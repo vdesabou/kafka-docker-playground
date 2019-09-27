caa780ca81a9a564c9eaae27465f3a5c28150789
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-09-27 14:33:54 +0200

    added AVRO Converter Example
    Former-commit-id: a1c5524b02e303385a8acd0802e631324a4400b1

---
+++ connect-http-sink/http_avro_basic_auth.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+${DIR}/reset-cluster.sh
+
+echo "Sending messages to topic avro-topic"
+seq -f "{\"f1\": \"value%g\"}" 10 | docker container exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic avro-topic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+echo "-------------------------------------"
+echo "Running AVRO Converter Example"
+echo "-------------------------------------"
+
+echo "Creating AvroHttpSink connector"
+docker-compose exec connect \
+     curl -X POST \
+     -H "Content-Type: application/json" \
+     --data '{
+          "name": "AvroHttpSink",
+          "config": {
+               "topics": "avro-topic",
+               "tasks.max": "1",
+               "connector.class": "io.confluent.connect.http.HttpSinkConnector",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url":"http://schema-registry:8081",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1",
+               "http.api.url": "http://http-service-basic-auth:8080/api/messages",
+               "auth.type": "BASIC",
+               "connection.user": "admin",
+               "connection.password": "password"
+          }}' \
+     http://localhost:8083/connectors | jq .
+
+
+sleep 10
+
+echo "Confirm that the data was sent to the HTTP endpoint."
+curl admin:password@localhost:9080/api/messages | jq .
