cfd18a3015d6c7c7ad431edb944c64bc9e23091a
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-10-01 16:51:37 +0200

    Working GCS Sink with SSL authentication
    Former-commit-id: 3e192227a46226a2df9259e4dc2b267038b63c46

--- connect-gcs-sink/gcs-sink-sasl-ssl.sh
+++ connect-gcs-sink/gcs-sink-sasl-ssl.sh
@@ -12,45 +12,19 @@ ${DIR}/../scripts/reset-cluster-sasl-ssl.sh
 
 echo "Removing existing objects in GCS, if applicable"
 set +e
-gsutil rm -r gs://$BUCKET_NAME/topics/gcs_topic
+gsutil rm -r gs://$BUCKET_NAME/topics/gcs_topic-ssl
+gsutil rm -r gs://$BUCKET_NAME/topics/gcs_topic-sasl-ssl
 set -e
 
-seq -f "{\"f1\": \"value%g\"}" 10 | docker container exec -i connect kafka-avro-console-producer --broker-list kafka1:9091 --topic gcs_topic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}' --property schema.registry.url=https://schemaregistry:8085 --producer.config /etc/kafka/secrets/client_without_interceptors.config
 
+echo "########"
+echo "##  SSL authentication"
+echo "########"
 
-echo "Creating GCS Sink connector"
-# docker-compose exec -e BUCKET_NAME="$BUCKET_NAME" connect \
-#      curl -X POST \
-#      --cert /etc/kafka/secrets/connect.certificate.pem --key /etc/kafka/secrets/connect.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt \
-#      -H "Content-Type: application/json" \
-#      --data '{
-#                "name": "gcs",
-#                "config": {
-#                     "connector.class": "io.confluent.connect.gcs.GcsSinkConnector",
-#                     "tasks.max" : "1",
-#                     "topics" : "gcs_topic",
-#                     "gcs.bucket.name" : "'"$BUCKET_NAME"'",
-#                     "gcs.part.size": "5242880",
-#                     "flush.size": "3",
-#                     "gcs.credentials.path": "/root/keyfile.json",
-#                     "storage.class": "io.confluent.connect.gcs.storage.GcsStorage",
-#                     "format.class": "io.confluent.connect.gcs.format.avro.AvroFormat",
-#                     "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
-#                     "schema.compatibility": "NONE",
-#                     "confluent.topic.bootstrap.servers": "kafka1:11091",
-#                     "confluent.topic.replication.factor": "2",
-#                     "confluent.topic.ssl.keystore.location" : "/etc/kafka/secrets/kafka.connect.keystore.jks",
-#                     "confluent.topic.ssl.keystore.password" : "confluent",
-#                     "confluent.topic.ssl.key.password" : "confluent",
-#                     "confluent.topic.ssl.truststore.location" : "/etc/kafka/secrets/kafka.connect.truststore.jks",
-#                     "confluent.topic.ssl.truststore.password" : "confluent",
-#                     "confluent.topic.ssl.keystore.type" : "JKS",
-#                     "confluent.topic.ssl.truststore.type" : "JKS",
-#                     "confluent.topic.security.protocol" : "SSL",
-#                     "confluent.topic.ssl.principal.mapping.rules" : "RULE:^CN=(.*?),OU=TEST.*$/$1/,DEFAULT"
-#           }}' \
-#      https://localhost:8083/connectors | jq .
+echo "Sending messages to topic gcs_topic-ssl"
+seq -f "{\"f1\": \"This is a message sent with SSL authentication %g\"}" 10 | docker container exec -i connect kafka-avro-console-producer --broker-list kafka1:9091 --topic gcs_topic-ssl --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}' --property schema.registry.url=https://schemaregistry:8085 --producer.config /etc/kafka/secrets/client_without_interceptors.config
 
+echo "Creating GCS Sink connector with SSL authentication"
 docker-compose exec -e BUCKET_NAME="$BUCKET_NAME" connect \
      curl -X POST \
      --cert /etc/kafka/secrets/connect.certificate.pem --key /etc/kafka/secrets/connect.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt \
@@ -60,7 +34,61 @@ docker-compose exec -e BUCKET_NAME="$BUCKET_NAME" connect \
                "config": {
                     "connector.class": "io.confluent.connect.gcs.GcsSinkConnector",
                     "tasks.max" : "1",
-                    "topics" : "gcs_topic",
+                    "topics" : "gcs_topic-ssl",
+                    "gcs.bucket.name" : "'"$BUCKET_NAME"'",
+                    "gcs.part.size": "5242880",
+                    "flush.size": "3",
+                    "gcs.credentials.path": "/root/keyfile.json",
+                    "storage.class": "io.confluent.connect.gcs.storage.GcsStorage",
+                    "format.class": "io.confluent.connect.gcs.format.avro.AvroFormat",
+                    "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
+                    "schema.compatibility": "NONE",
+                    "confluent.topic.bootstrap.servers": "kafka1:11091",
+                    "confluent.topic.replication.factor": "2",
+                    "confluent.topic.ssl.keystore.location" : "/etc/kafka/secrets/kafka.connect.keystore.jks",
+                    "confluent.topic.ssl.keystore.password" : "confluent",
+                    "confluent.topic.ssl.key.password" : "confluent",
+                    "confluent.topic.ssl.truststore.location" : "/etc/kafka/secrets/kafka.connect.truststore.jks",
+                    "confluent.topic.ssl.truststore.password" : "confluent",
+                    "confluent.topic.ssl.keystore.type" : "JKS",
+                    "confluent.topic.ssl.truststore.type" : "JKS",
+                    "confluent.topic.security.protocol" : "SSL"
+          }}' \
+     https://localhost:8083/connectors | jq .
+
+
+sleep 10
+
+echo "Listing objects of in GCS"
+gsutil ls gs://$BUCKET_NAME/topics/gcs_topic-ssl/partition=0/
+
+echo "Doing gsutil authentication"
+gcloud auth activate-service-account --key-file ./keyfile.json
+
+echo "Getting one of the avro files locally and displaying content with avro-tools"
+gsutil cp gs://$BUCKET_NAME/topics/gcs_topic-ssl/partition=0/gcs_topic-ssl+0+0000000000.avro /tmp/
+
+# brew install avro-tools
+avro-tools tojson /tmp/gcs_topic-ssl+0+0000000000.avro
+
+echo "########"
+echo "##  SASL_SSL authentication"
+echo "########"
+
+echo "Sending messages to topic gcs_topic-sasl-ssl"
+seq -f "{\"f1\": \"This is a message sent with SASL_SSL authentication %g\"}" 10 | docker container exec -i connect kafka-avro-console-producer --broker-list kafka1:9091 --topic gcs_topic-sasl-ssl --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}' --property schema.registry.url=https://schemaregistry:8085 --producer.config /etc/kafka/secrets/client_without_interceptors.config
+
+echo "Creating GCS Sink connector with SASL_SSL authentication"
+docker-compose exec -e BUCKET_NAME="$BUCKET_NAME" connect \
+     curl -X POST \
+     --cert /etc/kafka/secrets/connect.certificate.pem --key /etc/kafka/secrets/connect.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt \
+     -H "Content-Type: application/json" \
+     --data '{
+               "name": "gcs-sink-sasl-ssl",
+               "config": {
+                    "connector.class": "io.confluent.connect.gcs.GcsSinkConnector",
+                    "tasks.max" : "1",
+                    "topics" : "gcs_topic-sasl-ssl",
                     "gcs.bucket.name" : "'"$BUCKET_NAME"'",
                     "gcs.part.size": "5242880",
                     "flush.size": "3",
@@ -85,13 +113,13 @@ docker-compose exec -e BUCKET_NAME="$BUCKET_NAME" connect \
 sleep 10
 
 echo "Listing objects of in GCS"
-gsutil ls gs://$BUCKET_NAME/topics/gcs_topic/partition=0/
+gsutil ls gs://$BUCKET_NAME/topics/gcs_topic-sasl-ssl/partition=0/
 
 echo "Doing gsutil authentication"
 gcloud auth activate-service-account --key-file ./keyfile.json
 
 echo "Getting one of the avro files locally and displaying content with avro-tools"
-gsutil cp gs://$BUCKET_NAME/topics/gcs_topic/partition=0/gcs_topic+0+0000000000.avro /tmp/
+gsutil cp gs://$BUCKET_NAME/topics/gcs_topic-sasl-ssl/partition=0/gcs_topic-sasl-ssl+0+0000000000.avro /tmp/
 
 # brew install avro-tools
-avro-tools tojson /tmp/gcs_topic+0+0000000000.avro
+avro-tools tojson /tmp/gcs_topic-sasl-ssl+0+0000000000.avro
