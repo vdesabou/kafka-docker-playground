0bfd4c8c57883a4feca594386ff42a04846b0f3b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-10-02 17:52:32 +0200

    GCS connector with kerberos

---
+++ connect-gcs-sink/gcs-sink-kerberos.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+BUCKET_NAME=${1:-test-gcs-playground} 
+
+export CUSTOM_VOLUME_MAPPING_1="$PWD/keyfile.json:/root/keyfile.json:ro"
+
+${DIR}/../scripts/reset-cluster-kerberos.sh "FALSE" "TRUE"
+
+echo "Removing existing objects in GCS, if applicable"
+set +e
+gsutil rm -r gs://$BUCKET_NAME/topics/gcs_topic-kerberos
+set -e
+
+
+echo "########"
+echo "##  Kerberos GSSAPI authentication"
+echo "########"
+
+echo "Sending messages to topic gcs_topic-kerberos"
+docker container exec -i client kinit -k -t /var/lib/secret/kafka-client.key kafka_producer
+seq -f "{\"f1\": \"This is a message sent with Kerberos GSSAPI authentication %g\"}" 10 | docker container exec -i client kafka-avro-console-producer --broker-list kafka:9093 --topic gcs_topic-kerberos --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}' --property schema.registry.url=http://schema-registry:8081 --producer.config /etc/kafka/producer.properties
+
+echo "Creating GCS Sink connector with Kerberos GSSAPI authentication"
+docker container exec -e BUCKET_NAME="$BUCKET_NAME" connect \
+     curl -X POST \
+     -H "Content-Type: application/json" \
+     --data '{
+               "name": "gcs-sink-kerberos",
+               "config": {
+                    "connector.class": "io.confluent.connect.gcs.GcsSinkConnector",
+                    "tasks.max" : "1",
+                    "topics" : "gcs_topic-kerberos",
+                    "gcs.bucket.name" : "'"$BUCKET_NAME"'",
+                    "gcs.part.size": "5242880",
+                    "flush.size": "3",
+                    "gcs.credentials.path": "/root/keyfile.json",
+                    "storage.class": "io.confluent.connect.gcs.storage.GcsStorage",
+                    "format.class": "io.confluent.connect.gcs.format.avro.AvroFormat",
+                    "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
+                    "schema.compatibility": "NONE",
+                    "confluent.topic.bootstrap.servers": "kafka:9093",
+                    "confluent.topic.replication.factor": "1",
+                    "confluent.topic.sasl.mechanism": "GSSAPI",
+                    "confluent.topic.sasl.kerberos.service.name": "kafka",
+                    "confluent.topic.sasl.jaas.config" : "com.sun.security.auth.module.Krb5LoginModule required useKeyTab=true storeKey=true keyTab=\"/var/lib/secret/kafka-connect.key\" principal=\"connect@TEST.CONFLUENT.IO\";",
+                    "confluent.topic.security.protocol" : "SASL_PLAINTEXT"
+          }}' \
+     http://localhost:8083/connectors | jq .
+
+sleep 10
+
+echo "Listing objects of in GCS"
+gsutil ls gs://$BUCKET_NAME/topics/gcs_topic-kerberos/partition=0/
+
+echo "Doing gsutil authentication"
+gcloud auth activate-service-account --key-file ./keyfile.json
+
+echo "Getting one of the avro files locally and displaying content with avro-tools"
+gsutil cp gs://$BUCKET_NAME/topics/gcs_topic-kerberos/partition=0/gcs_topic-kerberos+0+0000000000.avro /tmp/
+
+# brew install avro-tools
+avro-tools tojson /tmp/gcs_topic-kerberos+0+0000000000.avro
+
---
+++ scripts/reset-cluster-kerberos.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+IGNORE_CONNECT_STARTUP=${1:-FALSE}
+IGNORE_CONTROL_CENTER_STARTUP=${2:-FALSE}
+
+verify_installed()
+{
+  local cmd="$1"
+  if [[ $(type $cmd 2>&1) =~ "not found" ]]; then
+    echo -e "\nERROR: This script requires '$cmd'. Please install '$cmd' and run again.\n"
+    exit 1
+  fi
+}
+verify_installed "jq"
+verify_installed "docker-compose"
+
+
+# Starting kerberos,
+# Avoiding starting up all services at the begining to generate the keytab first
+cd ../kerberos
+docker-compose down -v
+docker-compose build kdc
+docker-compose up -d kdc
+
+### Create the required identities:
+# Kafka service principal:
+docker exec -ti kdc kadmin.local -w password -q "add_principal -randkey kafka/kafka.kerberos-demo.local@TEST.CONFLUENT.IO"  > /dev/null
+
+# Zookeeper service principal:
+docker exec -ti kdc kadmin.local -w password -q "add_principal -randkey zookeeper/zookeeper.kerberos-demo.local@TEST.CONFLUENT.IO"  > /dev/null
+
+# Create a principal with which to connect to Zookeeper from brokers - NB use the same credential on all brokers!
+docker exec -ti kdc kadmin.local -w password -q "add_principal -randkey zkclient@TEST.CONFLUENT.IO"  > /dev/null
+
+# Create client principals to connect in to the cluster:
+docker exec -ti kdc kadmin.local -w password -q "add_principal -randkey kafka_producer@TEST.CONFLUENT.IO"  > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "add_principal -randkey kafka_producer/instance_demo@TEST.CONFLUENT.IO"  > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "add_principal -randkey kafka_consumer@TEST.CONFLUENT.IO"  > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "add_principal -randkey connect@TEST.CONFLUENT.IO"  > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "add_principal -randkey schemaregistry@TEST.CONFLUENT.IO"  > /dev/null
+
+
+# Create an admin principal for the cluster, which we'll use to setup ACLs.
+# Look after this - its also declared a super user in broker config.
+docker exec -ti kdc kadmin.local -w password -q "add_principal -randkey admin/for-kafka@TEST.CONFLUENT.IO"  > /dev/null
+
+# Create keytabs to use for Kafka
+docker exec -ti kdc rm -f /var/lib/secret/kafka.key 2>&1 > /dev/null
+docker exec -ti kdc rm -f /var/lib/secret/zookeeper.key 2>&1 > /dev/null
+docker exec -ti kdc rm -f /var/lib/secret/zookeeper-client.key 2>&1 > /dev/null
+docker exec -ti kdc rm -f /var/lib/secret/kafka-client.key 2>&1 > /dev/null
+docker exec -ti kdc rm -f /var/lib/secret/kafka-admin.key 2>&1 > /dev/null
+docker exec -ti kdc rm -f /var/lib/secret/kafka-connect.key 2>&1 > /dev/null
+docker exec -ti kdc rm -f /var/lib/secret/kafka-schemaregistry.key 2>&1 > /dev/null
+
+docker exec -ti kdc kadmin.local -w password -q "ktadd  -k /var/lib/secret/kafka.key -norandkey kafka/kafka.kerberos-demo.local@TEST.CONFLUENT.IO " > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "ktadd  -k /var/lib/secret/zookeeper.key -norandkey zookeeper/zookeeper.kerberos-demo.local@TEST.CONFLUENT.IO " > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "ktadd  -k /var/lib/secret/zookeeper-client.key -norandkey zkclient@TEST.CONFLUENT.IO " > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "ktadd  -k /var/lib/secret/kafka-client.key -norandkey kafka_producer@TEST.CONFLUENT.IO " > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "ktadd  -k /var/lib/secret/kafka-client.key -norandkey kafka_producer/instance_demo@TEST.CONFLUENT.IO " > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "ktadd  -k /var/lib/secret/kafka-client.key -norandkey kafka_consumer@TEST.CONFLUENT.IO " > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "ktadd  -k /var/lib/secret/kafka-admin.key -norandkey admin/for-kafka@TEST.CONFLUENT.IO " > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "ktadd  -k /var/lib/secret/kafka-connect.key -norandkey connect@TEST.CONFLUENT.IO " > /dev/null
+docker exec -ti kdc kadmin.local -w password -q "ktadd  -k /var/lib/secret/kafka-schemaregistry.key -norandkey schemaregistry@TEST.CONFLUENT.IO " > /dev/null
+
+# Starting zookeeper and kafka now that the keytab has been created with the required credentials and services
+docker-compose up -d
+
+# Adding ACLs for consumer and producer user:
+docker exec client bash -c "kinit -k -t /var/lib/secret/kafka-admin.key admin/for-kafka && kafka-acls --bootstrap-server kafka:9093 --command-config /etc/kafka/command.properties --add --allow-principal User:kafka_producer --producer --topic=*"
+docker exec client bash -c "kinit -k -t /var/lib/secret/kafka-admin.key admin/for-kafka && kafka-acls --bootstrap-server kafka:9093 --command-config /etc/kafka/command.properties --add --allow-principal User:kafka_consumer --consumer --topic=* --group=*"
+# Adding ACLs for connect user:
+docker exec client bash -c "kinit -k -t /var/lib/secret/kafka-admin.key admin/for-kafka && kafka-acls --bootstrap-server kafka:9093 --command-config /etc/kafka/command.properties --add --allow-principal User:connect --consumer --topic=* --group=*"
+docker exec client bash -c "kinit -k -t /var/lib/secret/kafka-admin.key admin/for-kafka && kafka-acls --bootstrap-server kafka:9093 --command-config /etc/kafka/command.properties --add --allow-principal User:connect --producer --topic=*"
+# schemaregistry is super user
+
+# Output example usage:
+echo "Example configuration to access kafka:"
+echo "-> docker-compose exec client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_producer && kafka-console-producer --broker-list kafka:9093 --topic test --producer.config /etc/kafka/producer.properties'"
+echo "-> docker-compose exec client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_consumer && kafka-console-consumer --bootstrap-server kafka:9093 --topic test --consumer.config /etc/kafka/consumer.properties --from-beginning'"
+
+if [ "${IGNORE_CONNECT_STARTUP}" == "FALSE" ]
+then
+  # Verify Kafka Connect has started within MAX_WAIT seconds
+  MAX_WAIT=120
+  CUR_WAIT=0
+  echo "Waiting up to $MAX_WAIT seconds for Kafka Connect to start"
+  while [[ ! $(docker-compose logs connect) =~ "Finished starting connectors and tasks" ]]; do
+    sleep 10
+    CUR_WAIT=$(( CUR_WAIT+10 ))
+    if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+      echo -e "\nERROR: The logs in connect container do not show 'Finished starting connectors and tasks' after $MAX_WAIT seconds. Please troubleshoot with 'docker-compose ps' and 'docker-compose logs'.\n"
+      exit 1
+    fi
+  done
+  echo "Connect has started!"
+fi
+
+if [ "${IGNORE_CONTROL_CENTER_STARTUP}" == "FALSE" ]
+then
+  # Verify Confluent Control Center has started within MAX_WAIT seconds
+  MAX_WAIT=300
+  CUR_WAIT=0
+  echo "Waiting up to $MAX_WAIT seconds for Confluent Control Center to start"
+  while [[ ! $(docker-compose logs control-center) =~ "Started NetworkTrafficServerConnector" ]]; do
+    sleep 10
+    CUR_WAIT=$(( CUR_WAIT+10 ))
+    if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+      echo -e "\nERROR: The logs in control-center container do not show 'Started NetworkTrafficServerConnector' after $MAX_WAIT seconds. Please troubleshoot with 'docker-compose ps' and 'docker-compose logs'.\n"
+      exit 1
+    fi
+  done
+  echo "Control Center has started!"
+fi
+
+# Verify Docker containers started
+if [[ $(docker-compose ps) =~ "Exit 137" ]]; then
+  echo -e "\nERROR: At least one Docker container did not start properly, see 'docker-compose ps'. Did you remember to increase the memory available to Docker to at least 8GB (default is 2GB)?\n"
+  exit 1
+fi
+
+cd -
