996f5edf7755e7c295445b83e81744f074c11fd9
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-10-09 11:54:27 +0200

    minor change

--- connect-tibco-source/tibco-ems.sh
+++ connect-tibco-source/tibco-ems.sh
@@ -37,7 +37,6 @@ ${DIR}/../plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
 docker container exec tibco-ems bash -c '
 cd /opt/tibco/ems/8.5/samples/java
-echo $PWD
 export TIBEMS_JAVA=/opt/tibco/ems/8.5/lib
 CLASSPATH=${TIBEMS_JAVA}/jms-2.0.jar:${CLASSPATH}
 CLASSPATH=.:${TIBEMS_JAVA}/tibjms.jar:${TIBEMS_JAVA}/tibjmsadmin.jar:${CLASSPATH}
