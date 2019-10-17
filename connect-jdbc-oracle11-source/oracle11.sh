5f135281fdc367daf9cc91b9e721690e9d5afdc8
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-10-17 13:14:50 +0200

    added more notes, giving up
    Former-commit-id: 4f0caf1392ce2962016f504a599690d021879752

--- connect-jdbc-oracle11-source/oracle11.sh
+++ connect-jdbc-oracle11-source/oracle11.sh
@@ -11,6 +11,9 @@ fi
 
 ${DIR}/../plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
+# same issue as https://github.com/confluentinc/kafka-connect-jdbc/issues/654
+
+# [2019-10-17 07:40:50,462] WARN [Worker clientId=connect-1, groupId=connect] This member will leave the group because consumer poll timeout has expired. This means the time between subsequent calls to poll() was longer than the configured max.poll.interval.ms, which typically implies that the poll loop is spending too much time processing messages. You can address this either by increasing max.poll.interval.ms or by reducing the maximum size of batches returned in poll() with max.poll.records. (org.apache.kafka.clients.consumer.internals.AbstractCoordinator)
 
 # installing instantclient (sqlplus) on connect:
 # Version 11.2.0.4.0
@@ -42,6 +45,57 @@ ${DIR}/../plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
 # 16-OCT-2019 16:02:13 * (CONNECT_DATA=(SERVICE_NAME=XE)(CID=(PROGRAM=sqlplus64)(HOST=connect)(USER=root))) * (ADDRESS=(PROTOCOL=tcp)(HOST=172.30.0.6)(PORT=39520)) * establish * XE * 0
 
+
+# Testing with simple Java program is also working
+# root@connect:~/instantclient# javac -classpath /usr/share/confluent-hub-components/confluentinc-kafka-connect-jdbc/lib/ojdbc6.jar OracleSample.java
+# root@connect:~/instantclient# java -classpath "/usr/share/confluent-hub-components/confluentinc-kafka-connect-jdbc/lib/ojdbc6.jar:." OracleSample
+# Current Date from Oracle is : 2019-10-17
+
+
+# // Example Java Program - Oracle Database Connectivity
+# import java.sql.Connection;
+# import java.sql.Date;
+# import java.sql.DriverManager;
+# import java.sql.ResultSet;
+# import java.sql.SQLException;
+# import java.sql.Statement;
+
+# // javac -classpath /usr/share/confluent-hub-components/confluentinc-kafka-connect-jdbc/lib/ojdbc6.jar OracleSample.java
+# // java -classpath "/usr/share/confluent-hub-components/confluentinc-kafka-connect-jdbc/lib/ojdbc6.jar:." OracleSample
+# public class OracleSample {
+
+#     public static final String DBURL = "jdbc:oracle:thin:@oracle:1521/XE";
+#     public static final String DBUSER = "myuser";
+#     public static final String DBPASS = "mypassword";
+
+#     public static void main(String[] args) throws SQLException {
+
+#         // Load Oracle JDBC Driver
+#         DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+
+#         // Connect to Oracle Database
+#         Connection con = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+
+#         Statement statement = con.createStatement();
+
+#         // Execute a SELECT query on Oracle Dummy DUAL Table. Useful for retrieving system values
+#         // Enables us to retrieve values as if querying from a table
+#         ResultSet rs = statement.executeQuery("SELECT SYSDATE FROM DUAL");
+
+
+#         if (rs.next()) {
+#             Date currentDate = rs.getDate(1); // get first column returned
+#             System.out.println("Current Date from Oracle is : "+currentDate);
+#         }
+#         rs.close();
+#         statement.close();
+#         con.close();
+#     }
+# }
+
+# 17-OCT-2019 08:41:35 * (CONNECT_DATA=(CID=(PROGRAM=JDBC Thin Client)(HOST=__jdbc__)(USER=root))(SERVICE_NAME=XE)(CID=(PROGRAM=JDBC Thin Client)(HOST=__jdbc__)(USER=root))) * (ADDRESS=(PROTOCOL=tcp)(HOST=172.19.0.6)(PORT=49788)) * establish * XE * 0
+
+
 exit 0
 
 # FIXTHIS: not working:
@@ -52,10 +106,10 @@ exit 0
 # }
 echo "Creating Oracle source connector"
 docker container exec connect \
-     curl -X POST \
+     curl -X POST --max-time 90000 \
      -H "Content-Type: application/json" \
      --data '{
-               "name": "oracle-source",
+               "name": "oracle-source3",
                "config": {
                     "connector.class":"io.confluent.connect.jdbc.JdbcSourceConnector",
                     "tasks.max":"1",
@@ -67,14 +121,16 @@ docker container exec connect \
                     "timestamp.column.name":"UPDATE_TS",
                     "incrementing.column.name":"ID",
                     "topic.prefix":"oracle-",
+                    "consumer.max.poll.interval.ms": 900000000,
+                    "max.poll.interval.ms": 900000000,
                     "errors.log.enable": "true",
                     "errors.log.include.messages": "true"
           }}' \
      http://localhost:8083/connectors | jq .
 
-# Wed Oct 16 15:28:10 2019
-#16-OCT-2019 15:28:10 * (CONNECT_DATA=(CID=(PROGRAM=JDBC Thin Client)(HOST=__jdbc__)(USER=root))(SERVICE_NAME=xe)(CID=(PROGRAM=JDBC Thin Client)(HOST=__jdbc__)(USER=root))) * (ADDRESS=(PROTOCOL=tcp)(HOST=172.28.0.6)(PORT=39094)) * establish * xe * 0
-# 172.28.0.6 is ip of connect
+
+#17-OCT-2019 08:45:27 * (CONNECT_DATA=(CID=(PROGRAM=JDBC Thin Client)(HOST=__jdbc__)(USER=root))(SERVICE_NAME=XE)(CID=(PROGRAM=JDBC Thin Client)(HOST=__jdbc__)(USER=root))) * (ADDRESS=(PROTOCOL=tcp)(HOST=172.19.0.6)(PORT=49980)) * establish * XE * 0
+# 172.19.0.6 is ip of connect
 
 # HOST=__jdbc__: This hostname has been hard-coded as a false hostname inside the driver to force an IP address lookup
 
