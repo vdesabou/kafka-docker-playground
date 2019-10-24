b75bf48e669711a8961204e3276943a05e844070
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-10-24 15:40:42 +0200

    added ActiveMQ sink

---
+++ connect-active-mq-sink/active-mq-sink.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+${DIR}/../plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+
+echo "Sending messages to topic sink-messages"
+docker container exec -i broker kafka-console-producer --broker-list broker:9092 --topic sink-messages << EOF
+This is my message
+EOF
+
+echo "Creating ActiveMQ sink connector"
+docker container exec connect \
+     curl -X POST \
+     -H "Content-Type: application/json" \
+     --data '{
+               "name": "active-mq-sink",
+               "config": {
+                    "connector.class": "io.confluent.connect.jms.ActiveMqSinkConnector",
+                    "topics": "sink-messages",
+                    "activemq.url": "tcp://activemq:61616",
+                    "activemq.username": "admin",
+                    "activemq.password": "admin",
+                    "jms.destination.name": "DEV.QUEUE.1",
+                    "jms.destination.type": "queue",
+                    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                    "value.converter": "org.apache.kafka.connect.storage.StringConverter",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }}' \
+     http://localhost:8083/connectors | jq .
+
+sleep 5
+
+echo "Get messages from DEV.QUEUE.1 JMS queue:"
+curl -XGET -u admin:admin -d "body=message" http://localhost:8161/api/message/DEV.QUEUE.1?type=queue
+
