41426d8c7872c56f7817a85d07e221bd17a169bf
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-11-04 10:57:43 +0100

    added TSV
    Former-commit-id: f6392accf0dd23da58ab6b923054d459199def00

--- connect-spool-dir-source/csv.sh
+++ connect-spool-dir-source/csv.sh
@@ -6,6 +6,10 @@ BUCKET_NAME=${1:-kafka-docker-playground}
 
 ${DIR}/../plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
+mkdir -p ${DIR}/data/input
+mkdir -p ${DIR}/data/error
+mkdir -p ${DIR}/data/finished
+
 if [ ! -f "${DIR}/data/input/csv-spooldir-source.csv" ]
 then
      echo "Generating data"
@@ -17,7 +21,7 @@ docker exec connect \
      curl -X POST \
      -H "Content-Type: application/json" \
      --data '{
-               "name": "CsvSchemaSpoolDir5",
+               "name": "CsvSchemaSpoolDir",
                "config": {
                     "tasks.max": "1",
                     "connector.class": "com.github.jcustenborder.kafka.connect.spooldir.SpoolDirCsvSourceConnector",
@@ -26,7 +30,7 @@ docker exec connect \
                     "error.path": "/root/data/error",
                     "finished.path": "/root/data/finished",
                     "halt.on.error": "false",
-                    "topic": "spooldir-testing-topic",
+                    "topic": "spooldir-csv-topic",
                     "csv.first.row.as.header": "true",
                     "key.schema": "{\n  \"name\" : \"com.example.users.UserKey\",\n  \"type\" : \"STRUCT\",\n  \"isOptional\" : false,\n  \"fieldSchemas\" : {\n    \"id\" : {\n      \"type\" : \"INT64\",\n      \"isOptional\" : false\n    }\n  }\n}",
                     "value.schema": "{\n  \"name\" : \"com.example.users.User\",\n  \"type\" : \"STRUCT\",\n  \"isOptional\" : false,\n  \"fieldSchemas\" : {\n    \"id\" : {\n      \"type\" : \"INT64\",\n      \"isOptional\" : false\n    },\n    \"first_name\" : {\n      \"type\" : \"STRING\",\n      \"isOptional\" : true\n    },\n    \"last_name\" : {\n      \"type\" : \"STRING\",\n      \"isOptional\" : true\n    },\n    \"email\" : {\n      \"type\" : \"STRING\",\n      \"isOptional\" : true\n    },\n    \"gender\" : {\n      \"type\" : \"STRING\",\n      \"isOptional\" : true\n    },\n    \"ip_address\" : {\n      \"type\" : \"STRING\",\n      \"isOptional\" : true\n    },\n    \"last_login\" : {\n      \"type\" : \"STRING\",\n      \"isOptional\" : true\n    },\n    \"account_balance\" : {\n      \"name\" : \"org.apache.kafka.connect.data.Decimal\",\n      \"type\" : \"BYTES\",\n      \"version\" : 1,\n      \"parameters\" : {\n        \"scale\" : \"2\"\n      },\n      \"isOptional\" : true\n    },\n    \"country\" : {\n      \"type\" : \"STRING\",\n      \"isOptional\" : true\n    },\n    \"favorite_color\" : {\n      \"type\" : \"STRING\",\n      \"isOptional\" : true\n    }\n  }\n}"
@@ -36,5 +40,5 @@ docker exec connect \
 
 sleep 5
 
-echo "Verify we have received the data in spooldir-testing-topic topic"
-docker exec schema-registry kafka-avro-console-consumer -bootstrap-server broker:9092 --topic spooldir-testing-topic --property schema.registry.url=http://schema-registry:8081 --from-beginning --max-messages 10
+echo "Verify we have received the data in spooldir-csv-topic topic"
+docker exec schema-registry kafka-avro-console-consumer -bootstrap-server broker:9092 --topic spooldir-csv-topic --property schema.registry.url=http://schema-registry:8081 --from-beginning --max-messages 10
