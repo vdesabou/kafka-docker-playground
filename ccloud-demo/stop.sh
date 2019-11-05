44c62f426c4bb702063c38608beed7eaf08dccda
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-11-05 17:29:02 +0100

    reworked Confluent cloud demo

--- ccloud-demo/stop.sh
+++ ccloud-demo/stop.sh
@@ -2,4 +2,18 @@
 
 set -e
 
+#!/bin/bash
+
+CONFIG_FILE=~/.ccloud/config
+
+set -eu
+
+./ccloud-generate-env-vars.sh $CONFIG_FILE
+source delta_configs/env.delta
+
+# Delete topic in Confluent Cloud
+echo "Delete topic customer-avro"
+kafka-topics --bootstrap-server `grep "^\s*bootstrap.server" $CONFIG_FILE | tail -1` --command-config $CONFIG_FILE --topic customer-avro --delete 2>/dev/null || true
+
+
 docker-compose down -v
