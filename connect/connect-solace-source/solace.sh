16f2e6be28842a74ca46e5eca687a1097bd4ad1f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-11-07 14:17:38 +0100

    automatically download sol-jms-10.6.3.jar

--- connect/connect-solace-source/solace.sh
+++ connect/connect-solace-source/solace.sh
@@ -3,6 +3,12 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+if [ ! -f ${DIR}/sol-jms-10.6.3.jar ]
+then
+     echo "Downloading sol-jms-10.6.3.jar"
+     wget http://central.maven.org/maven2/com/solacesystems/sol-jms/10.6.3/sol-jms-10.6.3.jar
+fi
+
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
 echo "Wait for Solace to be up and running"
