b499a65100f884c34b85fb1032c205af0b3bbc87
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-11-07 12:33:03 +0100

    added Service Account and ACLs example
    Former-commit-id: 6f194abbb59ebfcce36d24a2c5573bce41f5f469

---
+++ ccloud/ccloud-demo/producer-acl/src/main/java/com/github/vdesabou/model/DataRecord.java
@@ -0,0 +1,22 @@
+package com.github.vdesabou.model;
+
+public class DataRecord {
+
+    Long count;
+
+    public DataRecord() {
+    }
+
+    public DataRecord(Long count) {
+        this.count = count;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public String toString() {
+        return new com.google.gson.Gson().toJson(this);
+    }
+
+}
