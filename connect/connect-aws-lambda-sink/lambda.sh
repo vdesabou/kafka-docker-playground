be5c25f8a116ac89566b6c6722daa27baf6f0a2f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-11-27 12:23:31 +0100

    Added AWS Lambda Sink
    Former-commit-id: 056c262eca4153b86dea229c26e803afd273b690 [formerly 6585ed23404cfc44fd30eb00205d72bb94fc62a9 [formerly 1186128bf363c845a8334d34cb6b13cb32849a91]]
    Former-commit-id: deac9998548d009d5e6932edb61e75f468137281
    Former-commit-id: baca185056df09f962dbbabbbb2026e57d709351

--- connect/connect-aws-lambda-sink/lambda.sh
+++ connect/connect-aws-lambda-sink/lambda.sh
@@ -5,10 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
-echo "Sending messages to topic sink-messages"
+echo "Sending messages to topic add-topic"
 seq -f "{\"a\": %g,\"b\": 1}" 10 | docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic add-topic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"a","type":"int"},{"name":"b","type":"int"}]}'
 
-echo "Creating AWS CloudWatch Logs Source connector"
+echo "Creating AWS Lambda Sink connector"
 docker exec connect \
      curl -X POST \
      -H "Content-Type: application/json" \
@@ -22,9 +22,17 @@ docker exec connect \
                     "aws.lambda.invocation.type" : "sync",
                     "aws.lambda.batch.size" : "50",
                     "behavior.on.error" : "fail",
+                    "aws.lambda.response.topic": "add-topic-response",
+                    "aws.lambda.response.bootstrap.servers": "broker:9092",
+                    "aws.lambda.response.client.id": "add-topic-response-client",
                     "confluent.license": "",
                     "confluent.topic.bootstrap.servers": "broker:9092",
                     "confluent.topic.replication.factor": "1"
           }}' \
      http://localhost:8083/connectors | jq .
 
+
+sleep 10
+
+echo "Verifying topic add-topic-response"
+docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic add-topic-response --from-beginning --max-messages 10
