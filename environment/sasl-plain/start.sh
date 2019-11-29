2c73a456afc97a1f66de421085cff277a92131ab
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-11-29 11:02:00 +0100

    added sasl plain example

---
+++ environment/sasl-plain/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+verify_installed()
+{
+  local cmd="$1"
+  if [[ $(type $cmd 2>&1) =~ "not found" ]]; then
+    echo -e "\nERROR: This script requires '$cmd'. Please install '$cmd' and run again.\n"
+    exit 1
+  fi
+}
+verify_installed "docker-compose"
+
+DOCKER_COMPOSE_FILE_OVERRIDE=$1
+if [ -f "${DOCKER_COMPOSE_FILE_OVERRIDE}" ]
+then
+  echo "Using ${DOCKER_COMPOSE_FILE_OVERRIDE}"
+  docker-compose -f ../../environment/sasl-plain/docker-compose.yml -f ${DOCKER_COMPOSE_FILE_OVERRIDE} down -v
+  docker-compose -f ../../environment/sasl-plain/docker-compose.yml -f ${DOCKER_COMPOSE_FILE_OVERRIDE} up -d
+else
+  docker-compose down -v
+  docker-compose up -d
+fi
+
+../../WaitForConnectAndControlCenter.sh
