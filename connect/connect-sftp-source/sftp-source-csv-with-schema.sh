b456f3c5aaac0f8b7a8103b75a4dd3435cd30e2d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-11-29 18:08:04 +0100

    with JSON (schema, no schema), TSV and CSV (schema,no schema)

---
+++ connect/connect-sftp-source/sftp-source-csv-with-schema.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+mkdir -p ${DIR}/upload/input
+mkdir -p ${DIR}/upload/error
+mkdir -p ${DIR}/upload/finished
+
+echo $'id,first_name,last_name,email,gender,ip_address,last_login,account_balance,country,favorite_color\n1,Salmon,Baitman,sbaitman0@feedburner.com,Male,120.181.75.98,2015-03-01T06:01:15Z,17462.66,IT,#f09bc0\n2,Debby,Brea,dbrea1@icio.us,Female,153.239.187.49,2018-10-21T12:27:12Z,14693.49,CZ,#73893a' > ${DIR}/upload/input/csv-sftp-source.csv
+
+echo "Creating CSV SFTP Source connector"
+docker exec connect \
+     curl -X POST \
+     -H "Content-Type: application/json" \
+     --data '{
+        "name": "sftp-source-csv-with-schema",
+        "config": {
+               "topics": "test_sftp_sink",
+               "tasks.max": "1",
+               "connector.class": "io.confluent.connect.sftp.SftpCsvSourceConnector",
+               "cleanup.policy":"NONE",
+               "behavior.on.error":"IGNORE",
+               "input.path": "/upload/input",
+               "error.path": "/upload/error",
+               "finished.path": "/upload/finished",
+               "input.file.pattern": "csv-sftp-source.csv",
+               "sftp.username":"foo",
+               "sftp.password":"pass",
+               "sftp.host":"sftp-server",
+               "sftp.port":"22",
+               "kafka.topic": "sftp-testing-topic",
+               "csv.first.row.as.header": "true",
+               "key.schema": "{\"name\" : \"com.example.users.UserKey\",\"type\" : \"STRUCT\",\"isOptional\" : false,\"fieldSchemas\" : {\"id\" : {\"type\" : \"INT64\",\"isOptional\" : false}}}",
+               "value.schema": "{\"name\" : \"com.example.users.User\",\"type\" : \"STRUCT\",\"isOptional\" : false,\"fieldSchemas\" : {\"id\" : {\"type\" : \"INT64\",\"isOptional\" : false},\"first_name\" : {\"type\" : \"STRING\",\"isOptional\" : true},\"last_name\" : {\"type\" : \"STRING\",\"isOptional\" : true},\"email\" : {\"type\" : \"STRING\",\"isOptional\" : true},\"gender\" : {\"type\" : \"STRING\",\"isOptional\" : true},\"ip_address\" : {\"type\" : \"STRING\",\"isOptional\" : true},\"last_login\" : {\"type\" : \"STRING\",\"isOptional\" : true},\"account_balance\" : {\"name\" : \"org.apache.kafka.connect.data.Decimal\",\"type\" : \"BYTES\",\"version\" : 1,\"parameters\" : {\"scale\" : \"2\"},\"isOptional\" : true},\"country\" : {\"type\" : \"STRING\",\"isOptional\" : true},\"favorite_color\" : {\"type\" : \"STRING\",\"isOptional\" : true}}}"
+          }}' \
+     http://localhost:8083/connectors | jq .
+
+sleep 5
+
+echo "Verifying topic sftp-testing-topic"
+docker exec schema-registry kafka-avro-console-consumer -bootstrap-server broker:9092 --topic sftp-testing-topic --from-beginning --max-messages 2
---
+++ connect/connect-sftp-source/sftp-source-json.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+mkdir -p ${DIR}/upload/input
+mkdir -p ${DIR}/upload/error
+mkdir -p ${DIR}/upload/finished
+
+echo $'{"id":1,"first_name":"Roscoe","last_name":"Brentnall","email":"rbrentnall0@mediafire.com","gender":"Male","ip_address":"202.84.142.254","last_login":"2018-02-12T06:26:23Z","account_balance":1450.68,"country":"CZ","favorite_color":"#4eaefa"}\n{"id":2,"first_name":"Gregoire","last_name":"Fentem","email":"gfentem1@nsw.gov.au","gender":"Male","ip_address":"221.159.106.63","last_login":"2015-03-27T00:29:56Z","account_balance":1392.37,"country":"ID","favorite_color":"#e8f686"}' > ${DIR}/upload/input/json-sftp-source.json
+
+echo "Creating JSON (no schema) SFTP Source connector"
+docker exec connect \
+     curl -X POST \
+     -H "Content-Type: application/json" \
+     --data '{
+        "name": "sftp-source-json",
+        "config": {
+               "topics": "test_sftp_sink",
+               "tasks.max": "1",
+               "connector.class": "io.confluent.connect.sftp.SftpSchemaLessJsonSourceConnector",
+               "behavior.on.error":"IGNORE",
+               "input.path": "/upload/input",
+               "error.path": "/upload/error",
+               "finished.path": "/upload/finished",
+               "input.file.pattern": "json-sftp-source.json",
+               "sftp.username":"foo",
+               "sftp.password":"pass",
+               "sftp.host":"sftp-server",
+               "sftp.port":"22",
+               "kafka.topic": "sftp-testing-topic",
+               "value.converter": "org.apache.kafka.connect.storage.StringConverter"
+          }}' \
+     http://localhost:8083/connectors | jq .
+
+sleep 5
+
+echo "Verifying topic sftp-testing-topic"
+docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic sftp-testing-topic --from-beginning --max-messages 2
