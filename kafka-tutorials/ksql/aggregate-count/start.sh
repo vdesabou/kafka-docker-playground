6ffafefcf566039fffd1ec32678c2e4b5583b830
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-12-04 18:19:34 +0100

    adding examples
    Former-commit-id: fafdf3dd202c0d62a15fd040d347e383484092ef [formerly 75b6aa5c1232ea34ca1cf35c09fa96e3fc8f7521 [formerly fd0ff80f3b25f71790a9efe19483320dc2c28eb0]]
    Former-commit-id: 401292d9d875055402274cfa6fd13d1b9f47fca5
    Former-commit-id: 91ffc2f08919a3fc0b5c7d7022ea24e9c15871dd

---
+++ kafka-tutorials/ksql/aggregate-count/start.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+verify_installed()
+{
+  local cmd="$1"
+  if [[ $(type $cmd 2>&1) =~ "not found" ]]; then
+    echo -e "\nERROR: This script requires '$cmd'. Please install '$cmd' and run again.\n"
+    exit 1
+  fi
+}
+verify_installed "docker-compose"
+
+docker-compose down -v
+docker-compose up -d
+
+echo "Invoke manual steps"
+docker exec -i ksql-cli bash -c 'echo -e "\n\n⏳ Waiting for KSQL to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksql-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksql-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksql-server:8088' << EOF
+
+CREATE STREAM MOVIE_TICKET_SALES (title VARCHAR, sale_ts VARCHAR, ticket_total_value INT)
+    WITH (KAFKA_TOPIC='movie-ticket-sales',
+          PARTITIONS=1,
+          VALUE_FORMAT='avro');
+
+INSERT INTO MOVIE_TICKET_SALES (title, sale_ts, ticket_total_value) VALUES ('Die Hard', '2019-07-18T10:00:00Z', 12);
+INSERT INTO MOVIE_TICKET_SALES (title, sale_ts, ticket_total_value) VALUES ('Die Hard', '2019-07-18T10:01:00Z', 12);
+INSERT INTO MOVIE_TICKET_SALES (title, sale_ts, ticket_total_value) VALUES ('The Godfather', '2019-07-18T10:01:31Z', 12);
+INSERT INTO MOVIE_TICKET_SALES (title, sale_ts, ticket_total_value) VALUES ('Die Hard', '2019-07-18T10:01:36Z', 24);
+INSERT INTO MOVIE_TICKET_SALES (title, sale_ts, ticket_total_value) VALUES ('The Godfather', '2019-07-18T10:02:00Z', 18);
+INSERT INTO MOVIE_TICKET_SALES (title, sale_ts, ticket_total_value) VALUES ('The Big Lebowski', '2019-07-18T11:03:21Z', 12);
+INSERT INTO MOVIE_TICKET_SALES (title, sale_ts, ticket_total_value) VALUES ('The Big Lebowski', '2019-07-18T11:03:50Z', 12);
+INSERT INTO MOVIE_TICKET_SALES (title, sale_ts, ticket_total_value) VALUES ('The Godfather', '2019-07-18T11:40:00Z', 36);
+INSERT INTO MOVIE_TICKET_SALES (title, sale_ts, ticket_total_value) VALUES ('The Godfather', '2019-07-18T11:40:09Z', 18);
+
+SET 'auto.offset.reset' = 'earliest';
+SET 'ksql.streams.cache.max.bytes.buffering' = '10000000';
+
+SELECT TITLE,
+       COUNT(TICKET_TOTAL_VALUE) AS TICKETS_SOLD
+FROM MOVIE_TICKET_SALES
+GROUP BY TITLE
+LIMIT 3;
+
+CREATE TABLE MOVIE_TICKETS_SOLD AS
+    SELECT TITLE,
+           COUNT(TICKET_TOTAL_VALUE) AS TICKETS_SOLD
+    FROM MOVIE_TICKET_SALES
+    GROUP BY TITLE;
+
+PRINT 'MOVIE_TICKETS_SOLD' FROM BEGINNING LIMIT 3;
+EOF
+
+
+echo "Invoke the tests"
+docker exec ksql-cli ksql-test-runner -i /opt/app/test/input.json -s opt/app/src/statements.sql -o /opt/app/test/output.json
