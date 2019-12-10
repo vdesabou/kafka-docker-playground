c0499e74a25c77e9fed181fdcacf95594fdcde82
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-12-10 15:44:50 +0100

    remove unrequired stop script
    Former-commit-id: 592bee25540cf97aee2c2bc648386d71dae733c7 [formerly 050f190fa83d191e98f08d0f6259986accc66c08 [formerly cd178f9fe901f3d070fea8013a4479f1ad419f1f]]
    Former-commit-id: ea424e03e1711831a8eb602f084bd6937f2a4b6f
    Former-commit-id: 652af6f012b6383d2e3bd9ea239d4793a86c9ff4

--- connect/connect-rabbitmq-source/stop.sh
+++ connect/connect-rabbitmq-source/stop.sh
@@ -5,5 +5,3 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 ${DIR}/../../environment/plaintext/stop.sh "${PWD}/docker-compose.plaintext.yml"
-${DIR}/../../environment/sasl-ssl/stop.sh "${PWD}/docker-compose.sasl-ssl.yml"
-${DIR}/../../environment/kerberos/stop.sh "${PWD}/docker-compose.kerberos.yml"
