e03cd2d8b3a94c14bf188268a51cf8c49bc5a7bd
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-12-16 16:37:17 +0100

    adding tests for CC-7247
    Former-commit-id: f7039fde4bc48edde3688b7c6a9aec9e9b5ae9bd [formerly e0d3f006a202e0d5c7260381ab7e51d6ccdac0f6 [formerly c3a4b589b501f646ff176b9cd66f5dbdb2b4a2b1]]
    Former-commit-id: d6f661ee90d1f48b8a6f17ad66a54506899c0649
    Former-commit-id: 4e4fbac706c1778131e1175c862b25dcc841b65d

---
+++ connect/connect-hdfs-sink/hdfs-CC-7247-apple.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext-CC-7247.yml"
+
+
+# Note in this simple example, if you get into an issue with permissions at the local HDFS level, it may be easiest to unlock the permissions unless you want to debug that more.
+docker exec hadoop bash -c "/usr/local/hadoop/bin/hdfs dfs -chmod 777  /"
+
+echo "Creating HDFS Sink connector"
+docker exec connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class":"io.confluent.connect.hdfs.HdfsSinkConnector",
+               "tasks.max":"1",
+               "topics":"apple.pie",
+               "hdfs.url":"hdfs://hadoop:9000",
+               "flush.size":"3",
+               "hadoop.conf.dir":"/usr/local/hadoop-2.7.1/etc/hadoop/",
+               "partitioner.class":"io.confluent.connect.hdfs.partitioner.FieldPartitioner",
+               "partition.field.name":"f1",
+               "rotate.interval.ms":"120000",
+               "hadoop.home":"/usr/local/hadoop-2.7.1/share/hadoop/common/",
+               "logs.dir":"/tmp",
+               "topics.dir":"${topic}/${2}/some/path/${1}pie",
+               "topic.regex.capture.group": "[a-zA-Z]*",
+               "key.converter":"org.apache.kafka.connect.storage.StringConverter",
+               "value.converter":"io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url":"http://schema-registry:8081",
+               "schema.compatibility":"BACKWARD"
+          }' \
+     http://localhost:8083/connectors/hdfs-sink/config | jq .
+
+
+echo "Sending messages to topic apple.pie "
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic apple.pie --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+sleep 10
+
+echo "Listing content of /apple.pie/pie/some/path/applepie/ in HDFS"
+docker exec hadoop bash -c "/usr/local/hadoop/bin/hdfs dfs -ls /apple.pie/pie/some/path/applepie/"
+
+# drwxrwxrwx   - root supergroup          0 2019-12-16 07:21 /apple.pie/pie/some/path/applepie/+tmp
+# drwxr-xr-x   - root supergroup          0 2019-12-16 07:21 /apple.pie/pie/some/path/applepie/apple.pie
---
+++ connect/connect-hdfs-sink/hdfs-CC-7247-client.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext-CC-7247.yml"
+
+
+# Note in this simple example, if you get into an issue with permissions at the local HDFS level, it may be easiest to unlock the permissions unless you want to debug that more.
+docker exec hadoop bash -c "/usr/local/hadoop/bin/hdfs dfs -chmod 777  /"
+
+echo "Creating HDFS Sink connector"
+docker exec connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class":"io.confluent.connect.hdfs.HdfsSinkConnector",
+               "tasks.max":"1",
+               "topics":"tracking.mmtro.navigation.client.clientA.enabled,tracking.mmtro.navigation.client.clientB.enabled,tracking.mmtro.navigation.client.client123.enabled,tracking.mmtro.navigation.client.client-C.enabled",
+               "hdfs.url":"hdfs://hadoop:9000",
+               "flush.size":"3",
+               "hadoop.conf.dir":"/usr/local/hadoop-2.7.1/etc/hadoop/",
+               "partitioner.class":"io.confluent.connect.hdfs.partitioner.FieldPartitioner",
+               "partition.field.name":"f1",
+               "rotate.interval.ms":"120000",
+               "hadoop.home":"/usr/local/hadoop-2.7.1/share/hadoop/common/",
+               "logs.dir":"/tmp",
+               "topics.dir":"${5}/landing/behavioral",
+               "topic.regex.capture.group": "[a-zA-Z0-9-]*",
+               "key.converter":"org.apache.kafka.connect.storage.StringConverter",
+               "value.converter":"io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url":"http://schema-registry:8081",
+               "schema.compatibility":"BACKWARD"
+          }' \
+     http://localhost:8083/connectors/hdfs-sink/config | jq .
+
+
+echo "Sending messages to topic tracking.mmtro.navigation.client.clientA.enabled "
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic tracking.mmtro.navigation.client.clientA.enabled --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+echo "Sending messages to topic tracking.mmtro.navigation.client.clientB.enabled "
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic tracking.mmtro.navigation.client.clientB.enabled --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+echo "Sending messages to topic tracking.mmtro.navigation.client.client123.enabled "
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic tracking.mmtro.navigation.client.client123.enabled --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+echo "Sending messages to topic tracking.mmtro.navigation.client.client-C.enabled "
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic tracking.mmtro.navigation.client.client-C.enabled --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+sleep 10
+
+echo "Listing content of /clientA/landing/behavioral in HDFS"
+docker exec hadoop bash -c "/usr/local/hadoop/bin/hdfs dfs -ls /clientA/landing/behavioral"
+
+
+# Found 2 items
+# drwxrwxrwx   - root supergroup          0 2019-12-16 07:23 /clientA/landing/behavioral/+tmp
+# drwxr-xr-x   - root supergroup          0 2019-12-16 07:23 /clientA/landing/behavioral/tracking.mmtro.navigation.client.clientA.enabled
+
+echo "Listing content of /clientB/landing/behavioral in HDFS"
+docker exec hadoop bash -c "/usr/local/hadoop/bin/hdfs dfs -ls /clientB/landing/behavioral"
+
+# Found 2 items
+# drwxrwxrwx   - root supergroup          0 2019-12-16 07:23 /clientB/landing/behavioral/+tmp
+# drwxr-xr-x   - root supergroup          0 2019-12-16 07:23 /clientB/landing/behavioral/tracking.mmtro.navigation.client.clientB.enabled
+
+echo "Listing content of /client123/landing/behavioral in HDFS"
+docker exec hadoop bash -c "/usr/local/hadoop/bin/hdfs dfs -ls /client123/landing/behavioral"
+
+# Found 2 items
+# drwxrwxrwx   - root supergroup          0 2019-12-16 10:10 /client123/landing/behavioral/+tmp
+# drwxr-xr-x   - root supergroup          0 2019-12-16 10:10 /client123/landing/behavioral/tracking.mmtro.navigation.client.client123.enabled
+
+echo "Listing content of /client-C/landing/behavioral in HDFS"
+docker exec hadoop bash -c "/usr/local/hadoop/bin/hdfs dfs -ls /client-C/landing/behavioral"
+
+# Found 2 items
+# drwxrwxrwx   - root supergroup          0 2019-12-16 10:35 /client-C/landing/behavioral/+tmp
+# drwxr-xr-x   - root supergroup          0 2019-12-16 10:35 /client-C/landing/behavioral/tracking.mmtro.navigation.client.client-C.enabled
