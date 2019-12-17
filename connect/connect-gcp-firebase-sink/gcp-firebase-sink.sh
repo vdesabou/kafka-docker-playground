7e83fb4847f66028c11da726d165dd50a9e5d65a
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-12-17 10:32:42 +0100

    Added google Firebase sink

---
+++ connect/connect-gcp-firebase-sink/gcp-firebase-sink.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+PROJECT=${1:-vincent-de-saboulin-lab}
+
+KEYFILE="${DIR}/keyfile.json"
+if [ ! -f ${KEYFILE} ]
+then
+     echo "ERROR: the file ${KEYFILE} file is not present!"
+     exit 1
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+echo "Creating GCP Firebase Sink connector"
+docker exec -e PROJECT="$PROJECT" connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class" : "io.confluent.connect.firebase.FirebaseSinkConnector",
+                    "tasks.max" : "1",
+                    "topics":"artists,songs",
+                    "gcp.firebase.credentials.path": "/root/keyfiles/keyfile.json",
+                    "gcp.firebase.database.reference": "https://'"$PROJECT"'.firebaseio.com/musicBlog",
+                    "insert.mode":"update",
+                    "key.converter" : "io.confluent.connect.avro.AvroConverter",
+                    "key.converter.schema.registry.url":"http://schema-registry:8081",
+                    "value.converter" : "io.confluent.connect.avro.AvroConverter",
+                    "value.converter.schema.registry.url":"http://schema-registry:8081",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/firebase-sink/config | jq .
+
+
+echo "Produce Avro data to topic artists"
+docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic artists --property parse.key=true --property key.schema='{"type":"string"}' --property "key.separator=:" --property value.schema='{"type":"record","name":"artists","fields":[{"name":"name","type":"string"},{"name":"genre","type":"string"}]}' << EOF
+"artistId1":{"name":"Michael Jackson","genre":"Pop"}
+"artistId2":{"name":"Bob Dylan","genre":"American folk"}
+"artistId3":{"name":"Freddie Mercury","genre":"Rock"}
+EOF
+
+echo "Produce Avro data to topic songs"
+docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic songs --property parse.key=true --property key.schema='{"type":"string"}' --property "key.separator=:" --property value.schema='{"type":"record","name":"songs","fields":[{"name":"title","type":"string"},{"name":"artist","type":"string"}]}' << EOF
+"songId1":{"title":"billie jean","artist":"Michael Jackson"}
+"songId2":{"title":"hurricane","artist":"Bob Dylan"}
+"songId3":{"title":"bohemian rhapsody","artist":"Freddie Mercury"}
+EOF
+
+echo "Follow README to verify data is in Firebase"
