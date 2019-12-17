5018fa40d68130a8ebf523454c7df01552879a94
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-12-17 10:07:09 +0100

    Adding Google Firebase source
    Former-commit-id: 3ad406579227a2b052e2ace37558b6c7366e7e97 [formerly fef861cd6d78e12ab52dd2448dcf60372bc60416 [formerly 7f3aac96abb96a46da96080337662c05116ee3dd]]
    Former-commit-id: f7318c992e22eb98184afe32919e4efd1e57d2bb
    Former-commit-id: 2309ae59f97e206ac440cb891c8f6faf811ca133

---
+++ connect/connect-gcp-firebase-source/gcp-firebase-source.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+PROJECT=${1:-vincent-de-saboulin-lab}
+
+KEYFILE="${DIR}/keyfile.json"
+if [ ! -f ${KEYFILE} ]
+then
+     echo "ERROR: the file ${KEYFILE} file is not present!"
+     exit 1
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+echo "Creating GCP Firebase Source connector"
+docker exec -e PROJECT="$PROJECT" connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class" : "io.confluent.connect.firebase.FirebaseSourceConnector",
+                    "tasks.max" : "1",
+                    "gcp.firebase.credentials.path": "/root/keyfiles/keyfile.json",
+                    "gcp.firebase.database.reference": "https://'"$PROJECT"'.firebaseio.com/musicBlog",
+                    "gcp.firebase.snapshot":"true",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/firebase-source/config | jq .
+
+sleep 10
+
+echo "Verify messages are in topic artists"
+docker exec schema-registry kafka-avro-console-consumer -bootstrap-server broker:9092 --topic artists --from-beginning --max-messages 3
+
+echo "Verify messages are in topic songs"
+docker exec schema-registry kafka-avro-console-consumer -bootstrap-server broker:9092 --topic songs --from-beginning --max-messages 3
