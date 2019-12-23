b3db02459f42218365df148d207405a7305ccc45
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2019-12-23 14:14:55 +0100

    update
    Former-commit-id: 40bf8d418fa57ae2ae6ea75a18d9139806f8743f [formerly d2d44db910e49ffdc9f1b78f45ecdb835914c17e [formerly dfd8ab61438dc2719d2b93c3f133aa9e14e1e94f]]
    Former-commit-id: b7d36e60e4011d0dead50b926a0bc58c4fced264
    Former-commit-id: 615266fb875c9b2bd1438930044ecee7525b91ca

--- other/jms-client/jms-client-sasl-ssl-with-license.sh
+++ other/jms-client/jms-client-sasl-ssl-with-license.sh
@@ -43,8 +43,7 @@ echo -e "\033[0;33mBlocking communication between jms-client and zookeeper\033[0
 block_host jms-client $ZOOKEEPER_IP
 
 echo -e "\033[0;33mSending messages to topic test-queue using JMS client\033[0m"
-docker exec -e BOOTSTRAP_SERVERS="broker:9091" -e USERNAME="client" -e PASSWORD="client-secret" -e CONFLUENT_LICENSE="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ2aW5jZW50X2RlX3NhYm91bGluIiwiZXhwIjoxNjEwMzIzMjAwLCJpYXQiOjE1NDcxNjQ4MDAsImlzcyI6IkNvbmZsdWVudCIsIm1vbml0b3JpbmciOnRydWUsIm5iNCI6MTU3MzA1ODA0OCwic3ViIjoiY29udHJvbC1jZW50ZXIifQ.Om532E5DYdWd9Fw0_0hJvWvmVpcHlTxbZWQeozjJkDsTg45iqwN2mCT3ULyoyTV28z9qCn8YwEwyMFzwzzFRDSXpeWrTwnzrFjZfF00tZdw70-IEA7skpZ1PI5chS-Zdq20apxiWkIJR1qNRRwlapzKlPj7Um7UkPwmwLPK5cjIv7B-o01DtgqVgeGt_VMDBEJUcGcUa92ntawgI3x5y6BLpNbe246WrtjcA_I0URDwT64j6xvN2erNiC90Bu0zX8OM-g09Nac3p5Rg1HVdXdDSYOOy6_74qgB8t-7J5RHKuF2SiZI-kIFLU_wG-9-ECVMBsQNSr9a8k5z2-A-5NwA
-" jms-client bash -c "java -jar jms-client-1.0.0-jar-with-dependencies.jar"
+docker exec -e BOOTSTRAP_SERVERS="broker:9091" -e USERNAME="client" -e PASSWORD="client-secret" -e CONFLUENT_LICENSE="put your license here" jms-client bash -c "java -jar jms-client-1.0.0-jar-with-dependencies.jar"
 
 echo -e "\033[0;33mRemoving network partition between jms-client and zookeeper\033[0m"
 remove_partition jms-client zookeeper
