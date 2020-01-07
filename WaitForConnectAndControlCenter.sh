a6d103bc13e816cd36d021cea45b9054c3a680bc
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-07 15:38:30 +0100

    Starting to work on multiple data centers environments (plaintext + sasl plain)

--- WaitForConnectAndControlCenter.sh
+++ WaitForConnectAndControlCenter.sh
@@ -10,30 +10,34 @@ while getopts "h?ab" opt; do
         exit 0
         ;;
     a)  IGNORE_CONNECT_STARTUP="TRUE"
+        shift
         ;;
     b)  IGNORE_CONTROL_CENTER_STARTUP="FALSE"
+        shift
         ;;
     esac
 done
 
+CONNECT_CONTAINER=${1:-connect}
+CONTROL_CENTER_CONTAINER=${1:-"control-center"}
 
 if [ "${IGNORE_CONNECT_STARTUP}" == "FALSE" ]
 then
   # Verify Kafka Connect has started within MAX_WAIT seconds
   MAX_WAIT=240
   CUR_WAIT=0
-  echo -e "\033[0;33mWaiting up to $MAX_WAIT seconds for Kafka Connect to start\033[0m"
-  docker container logs connect > /tmp/out.txt 2>&1
+  echo -e "\033[0;33mWaiting up to $MAX_WAIT seconds for Kafka Connect ${CONNECT_CONTAINER} to start\033[0m"
+  docker container logs ${CONNECT_CONTAINER} > /tmp/out.txt 2>&1
   while [[ ! $(cat /tmp/out.txt) =~ "Finished starting connectors and tasks" ]]; do
     sleep 10
-    docker container logs connect > /tmp/out.txt 2>&1
+    docker container logs ${CONNECT_CONTAINER} > /tmp/out.txt 2>&1
     CUR_WAIT=$(( CUR_WAIT+10 ))
     if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
-      echo -e "\nERROR: The logs in connect container do not show 'Finished starting connectors and tasks' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+      echo -e "\nERROR: The logs in ${CONNECT_CONTAINER} container do not show 'Finished starting connectors and tasks' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
       exit 1
     fi
   done
-  echo -e "\033[0;33mConnect has started!\033[0m"
+  echo -e "\033[0;33mConnect ${CONNECT_CONTAINER} has started!\033[0m"
 fi
 
 
@@ -42,18 +46,18 @@ then
   # Verify Confluent Control Center has started within MAX_WAIT seconds
   MAX_WAIT=300
   CUR_WAIT=0
-  echo -e "\033[0;33mWaiting up to $MAX_WAIT seconds for Confluent Control Center to start\033[0m"
-  docker container logs control-center > /tmp/out.txt 2>&1
+  echo -e "\033[0;33mWaiting up to $MAX_WAIT seconds for Confluent Control Center ${CONTROL_CENTER_CONTAINER} to start\033[0m"
+  docker container logs ${CONTROL_CENTER_CONTAINER} > /tmp/out.txt 2>&1
   while [[ ! $(cat /tmp/out.txt) =~ "Started NetworkTrafficServerConnector" ]]; do
     sleep 10
-    docker container logs control-center > /tmp/out.txt 2>&1
+    docker container logs ${CONTROL_CENTER_CONTAINER} > /tmp/out.txt 2>&1
     CUR_WAIT=$(( CUR_WAIT+10 ))
     if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
-      echo -e "\nERROR: The logs in control-center container do not show 'Started NetworkTrafficServerConnector' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+      echo -e "\nERROR: The logs in ${CONTROL_CENTER_CONTAINER} container do not show 'Started NetworkTrafficServerConnector' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
       exit 1
     fi
   done
-  echo -e "\033[0;33mControl Center has started!\033[0m"
+  echo -e "\033[0;33mControl Center ${CONTROL_CENTER_CONTAINER} has started!\033[0m"
 fi
 
 # Verify Docker containers started
