321fe59c49bc53aa908f67fcdae3eb64cb21b3eb
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-07 09:09:59 +0100

    Added ksql example + minor updates

--- other/write-logs-to-files/start.sh
+++ other/write-logs-to-files/start.sh
@@ -3,4 +3,12 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+
+rm -f ${DIR}/broker/logs/*
+rm -f ${DIR}/zookeeper/logs/*
+rm -f ${DIR}/connect/logs/*
+rm -f ${DIR}/schema-registry/logs/*
+rm -f ${DIR}/control-center/logs/*
+rm -f ${DIR}/ksql-server/logs/*
+
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml" -a
