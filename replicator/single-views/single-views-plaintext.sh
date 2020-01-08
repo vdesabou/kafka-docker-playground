f0857d3057a07794033cb92858049244f17fe233
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-08 13:44:54 +0100

    curl -X PUT instead of POST

--- replicator/single-views/single-views-plaintext.sh
+++ replicator/single-views/single-views-plaintext.sh
@@ -14,11 +14,9 @@ seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i broker-us kafka-cons
 echo Consolidating all sales in the US
 
 docker container exec connect-us \
-     curl -X POST \
+     curl -X PUT \
      -H "Content-Type: application/json" \
      --data '{
-        "name": "replicate-europe-to-us",
-        "config": {
           "connector.class":"io.confluent.connect.replicator.ReplicatorSourceConnector",
           "key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
           "value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
@@ -32,30 +30,28 @@ docker container exec connect-us \
           "topic.auto.create": false,
           "topic.preserve.partitions": false,
           "topic.whitelist": "sales_EUROPE"
-          }}' \
-     http://localhost:8083/connectors | jq .
+          }' \
+     http://localhost:8083/connectors/replicate-europe-to-us/config | jq .
 
 
 echo Consolidating all sales in Europe
 
 docker container exec connect-europe \
-     curl -X POST \
+     curl -X PUT \
      -H "Content-Type: application/json" \
      --data '{
-        "name": "replicate-us-to-europe",
-        "config": {
           "connector.class":"io.confluent.connect.replicator.ReplicatorSourceConnector",
           "key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
           "value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
           "header.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
-          "src.consumer.group.id": "replicate-us",
+          "src.consumer.group.id": "replicate-us-to-europe",
           "src.kafka.bootstrap.servers": "broker-us:9092",
           "dest.kafka.bootstrap.servers": "broker-europe:9092",
           "confluent.topic.replication.factor": 1,
           "provenance.header.enable": true,
           "topic.whitelist": "sales_US"
-          }}' \
-     http://localhost:8083/connectors | jq .
+          }' \
+     http://localhost:8083/connectors/replicate-us-to-europe/config | jq .
 
 
 echo "Verify we have received the data in all the sales_ topics in EUROPE"
