4461ae26f927ca708a8f9fb46e4e3a613a908bc2
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-09 13:16:28 +0100

    Added sasl-plaintext + fixes
    Former-commit-id: 1b9cf231532ae295de41ddb15a2cfd64b13ce4ed [formerly 7e3e25b0a7bdf717183ade5bbfdbc73e5e3e2099 [formerly 021f2410f35f35a32e5e63ca75e7776c750307ea]]
    Former-commit-id: dc2bb9cc81a5f8d07e312e930a18238c4e3748ce
    Former-commit-id: ab894d059777ddbea516ae0bf69f6c11992e9cf5

--- replicator/executable/executable-plaintext.sh
+++ replicator/executable/executable-plaintext.sh
@@ -11,22 +11,25 @@ seq -f "european_sale_%g ${RANDOM}" 10 | docker container exec -i broker-europe 
 echo "Sending sales in US cluster"
 seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i broker-us kafka-console-producer --broker-list localhost:9092 --topic sales_US
 
-echo Consolidating all sales in the US
+echo "Consolidating all sales in the US (logs are in /tmp/replicator.log):"
 
 # run in detach mode -d
-docker exec -d connect-us bash -c 'replicator --consumer.config /etc/kafka/consumer-us.properties --producer.config /etc/kafka/producer-us.properties  --replication.config /etc/kafka/replication-us.properties  --cluster.id replicate-europe-to-us --whitelist sales_EUROPE'
+docker exec -d connect-us bash -c 'replicator --consumer.config /etc/kafka/consumer-us.properties --producer.config /etc/kafka/producer-us.properties  --replication.config /etc/kafka/replication-us.properties  --cluster.id replicate-europe-to-us --whitelist sales_EUROPE > /tmp/replicator.log 2>&1'
 
-echo Consolidating all sales in Europe
+echo "Consolidating all sales in Europe (logs are in /tmp/replicator.log):"
 
 # run in detach mode -d
-docker exec -d connect-europe bash -c 'replicator --consumer.config /etc/kafka/consumer-europe.properties --producer.config /etc/kafka/producer-europe.properties  --replication.config /etc/kafka/replication-europe.properties  --cluster.id replicate-us-to-europe --whitelist sales_US'
+docker exec -d connect-europe bash -c 'replicator --consumer.config /etc/kafka/consumer-europe.properties --producer.config /etc/kafka/producer-europe.properties  --replication.config /etc/kafka/replication-europe.properties  --cluster.id replicate-us-to-europe --whitelist sales_US  > /tmp/replicator.log 2>&1'
 
 echo "sleeping 120 seconds"
 sleep 120
 
 echo "Verify we have received the data in all the sales_ topics in EUROPE"
-docker container exec broker-europe kafka-console-consumer --bootstrap-server broker-europe:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000
+docker container exec broker-europe kafka-console-consumer --bootstrap-server localhost:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000
 
 echo "Verify we have received the data in all the sales_ topics in the US"
-docker container exec broker-europe kafka-console-consumer --bootstrap-server broker-europe:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000
+docker container exec broker-us kafka-console-consumer --bootstrap-server localhost:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000
 
+echo "Copying replicator logs to /tmp"
+docker cp connect-europe:/tmp/replicator.log /tmp/replicator-europe.log
+docker cp connect-us:/tmp/replicator.log /tmp/replicator-us.log
