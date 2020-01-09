1709184ae71f409aae07b6e5089459915b88e5d4
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-09 17:44:11 +0100

    block_host() in utils
    Former-commit-id: 0345d267298acc048d39d8c7e63c6f52bb811c61 [formerly aa4dfa33b8a26ba24e945b0e5d806029aab62656 [formerly cb5c3705240bd21e1cf61397440894a3e084352a]]
    Former-commit-id: df786ecfa3a727a0e04675098668167c2043ef78
    Former-commit-id: cd85f3c728ed57cae0abd09d2c4067d82440d54d

--- other/jms-client/jms-client-sasl-ssl-with-a-valid-license.sh
+++ other/jms-client/jms-client-sasl-ssl-with-a-valid-license.sh
@@ -4,40 +4,8 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-interface=eth0
-
-
-container_to_ip() {
-    name=$1
-    echo $(docker exec $name hostname -I)
-}
-
-block_host() {
-    name=$1
-    shift 1
-
-    # https://serverfault.com/a/906499
-    docker exec --privileged -t $name bash -c "tc qdisc add dev $interface root handle 1: prio" 2>&1
-
-    for ip in $@; do
-        docker exec --privileged -t $name bash -c "tc filter add dev $interface protocol ip parent 1: prio 1 u32 match ip dst $ip flowid 1:1" 2>&1
-    done
-
-    docker exec --privileged -t $name bash -c "tc filter add dev $interface protocol all parent 1: prio 2 u32 match ip dst 0.0.0.0/0 flowid 1:2" 2>&1
-    docker exec --privileged -t $name bash -c "tc filter add dev $interface protocol all parent 1: prio 2 u32 match ip protocol 1 0xff flowid 1:2" 2>&1
-    docker exec --privileged -t $name bash -c "tc qdisc add dev $interface parent 1:1 handle 10: netem loss 100%" 2>&1
-    docker exec --privileged -t $name bash -c "tc qdisc add dev $interface parent 1:2 handle 20: sfq" 2>&1
-}
-
-
-remove_partition() {
-    for name in $@; do
-        docker exec --privileged -t $name bash -c "tc qdisc del dev $interface root" 2>&1 > /dev/null
-    done
-}
-
 ${DIR}/../../environment/sasl-ssl/start.sh "${PWD}/docker-compose.sasl-ssl.yml" -a -b
-interface=eth0
+
 ZOOKEEPER_IP=$(container_to_ip zookeeper)
 
 log "Blocking communication between jms-client and zookeeper"
