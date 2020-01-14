cbf7645672ed981ca59350fcbe93e4eb60f98a7b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-14 09:42:34 +0100

    typo
    Former-commit-id: 31b14ca5cc88b716b6037c316e6ab11d1ff462ee [formerly 2ea6c44e9ad23cf6c854cf58109b245d1116a6cd [formerly 5014e7bf49c51e70a4def1a863a4bff2a2e3e9d5]]
    Former-commit-id: 68c5e22993210cf8d3f8e840b14646df22b5643b
    Former-commit-id: 0948d3972a201da30577f2e6560f6b547a2c62c5

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,13 +1,13 @@
 function log() {
   YELLOW='\033[0;33m'
   NC='\033[0m' # No Color
-  echo -e "$YELLOW$@1$NC"
+  echo -e "$YELLOW$@$NC"
 }
 
 function logerror() {
   RED='\033[0;31m'
   NC='\033[0m' # No Color
-  echo -e "$RED$@1$NC"
+  echo -e "$RED$@$NC"
 }
 
 function verify_installed()
