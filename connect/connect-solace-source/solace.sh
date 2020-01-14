0d0de2b8c9d2bab36369af12ed3a490203d24c54
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-14 10:11:08 +0100

    using sol-jms-10.6.4.jar
    Former-commit-id: ca9e0ecb1d6dad2256ef7cb8c6c263e086f52587 [formerly 77040ab06518f6c2f5951e11036f4de3b94c0749 [formerly f6f40ee10bbf40af377adb7a57aae5772aebe6d0]]
    Former-commit-id: d61d55c13ebbf0a4043516c739e045abe46e10c0
    Former-commit-id: 29e74819029a9dfa6be5496a23cb9e4baa8741df

--- connect/connect-solace-source/solace.sh
+++ connect/connect-solace-source/solace.sh
@@ -4,10 +4,10 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-if [ ! -f ${DIR}/sol-jms-10.6.0.jar ]
+if [ ! -f ${DIR}/sol-jms-10.6.4.jar ]
 then
-     log "Downloading sol-jms-10.6.0.jar"
-     wget http://central.maven.org/maven2/com/solacesystems/sol-jms/10.6.3/sol-jms-10.6.0.jar
+     log "Downloading sol-jms-10.6.4.jar"
+     wget http://central.maven.org/maven2/com/solacesystems/sol-jms/10.6.3/sol-jms-10.6.4.jar
 fi
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
