0667cafdb2f94c60c3a46508b7e493a727eb9c86
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-15 07:54:16 +0100

    fixed path for utils.sh

--- kafka-tutorials/kafka-streams/join-stream-and-table/start.sh
+++ kafka-tutorials/kafka-streams/join-stream-and-table/start.sh
@@ -3,7 +3,7 @@
 # https://kafka-tutorials.confluent.io/create-stateful-aggregation-sum/kstreams.html
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-source ${DIR}/../../scripts/utils.sh
+source ${DIR}/../../../scripts/utils.sh
 verify_installed "docker-compose"
 
 docker-compose down -v
