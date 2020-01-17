cda59f67d7f75a138187e110e946ecfedd1259cf
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-17 10:11:48 +0100

    added

---
+++ connect/connect-vertica-sink/vertica-sink-testing-batch-size-error.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f ${DIR}/vertica-jdbc.jar ]
+then
+     # install deps
+     log "Getting vertica-jdbc.jar from vertica-client-9.3.1-0.x86_64.tar.gz"
+     wget https://www.vertica.com/client_drivers/9.3.x/9.3.1-0/vertica-client-9.3.1-0.x86_64.tar.gz
+     tar xvfz ${DIR}/vertica-client-9.3.1-0.x86_64.tar.gz
+     cp ${DIR}/opt/vertica/java/lib/vertica-jdbc.jar ${DIR}/
+     rm -rf ${DIR}/opt
+     rm -f ${DIR}/vertica-client-9.3.1-0.x86_64.tar.gz
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+###
+# batch.size = 3000 (default)
+###
+
+log "Create the table mytabledefaultbatchsize and insert data."
+docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
+create table mytabledefaultbatchsize(f1 int);
+EOF
+
+sleep 2
+
+log "Sending messages to topic mytabledefaultbatchsize"
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic mytabledefaultbatchsize --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+log "Creating JDBC Vertica sink connector - default batch.size (3000)"
+docker exec connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class" : "io.confluent.vertica.VerticaSinkConnector",
+                    "tasks.max" : "1",
+                    "vertica.database": "docker",
+                    "vertica.host": "vertica",
+                    "vertica.port": "5433",
+                    "vertica.username": "dbadmin",
+                    "vertica.password": "",
+                    "topics": "mytabledefaultbatchsize",
+                    "errors.tolerance": "all",
+                    "errors.log.enable":true,
+                    "errors.log.include.messages":true,
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/vertica-sink/config | jq_docker_cli .
+
+sleep 10
+
+log "Check data is in Vertica"
+docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
+select * from mytabledefaultbatchsize;
+EOF
+
+
+
+###
+# batch.size = 1
+###
+
+log "Create the table mytablebatchsizeone and insert data."
+docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
+create table mytablebatchsizeone(f1 int);
+EOF
+
+sleep 2
+
+log "Sending messages to topic mytablebatchsizeone"
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic mytablebatchsizeone --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+log "Creating JDBC Vertica sink connector - batch.size (1)"
+docker exec connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class" : "io.confluent.vertica.VerticaSinkConnector",
+                    "tasks.max" : "1",
+                    "vertica.database": "docker",
+                    "vertica.host": "vertica",
+                    "vertica.port": "5433",
+                    "vertica.username": "dbadmin",
+                    "vertica.password": "",
+                    "topics": "mytablebatchsizeone",
+                    "batch.size": "1",
+                    "errors.tolerance": "all",
+                    "errors.log.enable":true,
+                    "errors.log.include.messages":true,
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/vertica-sink/config | jq_docker_cli .
+
+sleep 10
+
+log "Check data is in Vertica"
+docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
+select * from mytablebatchsizeone;
+EOF
