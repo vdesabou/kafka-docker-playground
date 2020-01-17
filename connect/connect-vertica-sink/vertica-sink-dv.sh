95ff4eb3aa902ecf7260139b607d5a6fe5786626
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-17 18:19:46 +0100

    wip
    Former-commit-id: cc92325d280b440a8c8a547e25e619f372095b34 [formerly 443cb233cdd51f02a14272cb3b980cd204cf0e4a [formerly cbc45b73f2cb67737565281efabb7ab836c0fd44]]
    Former-commit-id: a31d059a580f99c42801af93f8fd7bac8c0f7b32
    Former-commit-id: dfb6751f1dd13e12df49c224894c8dd821a67862

---
+++ connect/connect-vertica-sink/vertica-sink-dv.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f ${DIR}/vertica-jdbc.jar ]
+then
+     # install deps
+     log "Getting vertica-jdbc.jar from vertica-client-9.3.1-0.x86_64.tar.gz"
+     wget https://www.vertica.com/client_drivers/9.3.x/9.3.1-0/vertica-client-9.3.1-0.x86_64.tar.gz
+     tar xvfz ${DIR}/vertica-client-9.3.1-0.x86_64.tar.gz
+     cp ${DIR}/opt/vertica/java/lib/vertica-jdbc.jar ${DIR}/
+     rm -rf ${DIR}/opt
+     rm -f ${DIR}/vertica-client-9.3.1-0.x86_64.tar.gz
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+
+log "Create the table and insert data."
+docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
+CREATE TABLE url_override_normalized
+(
+    dwhCreationDate timestamp DEFAULT (statement_timestamp())::timestamp,
+    kafkaId int NOT NULL,
+    ListID int,
+    KafkaKeyIsDeleted boolean DEFAULT true
+);
+EOF
+
+#     NormalizedUrlHash int,
+#     URL varchar(80),
+sleep 2
+
+log "Sending messages to topic url_override_normalized"
+
+seq -f "{\"ListID\": 1,\"kafkaId\": 1}" 10 | docker exec -i schema-registry kafka-avro-console-producer --broker-list broker:9092 --topic url_override_normalized --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"ListID","type":"int"},{"name":"kafkaId","type":"int"}]}'
+
+log "Creating Vertica sink connector"
+docker exec connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class" : "io.confluent.vertica.VerticaSinkConnector",
+                    "tasks.max" : "1",
+                    "vertica.database": "docker",
+                    "vertica.host": "vertica",
+                    "vertica.port": "5433",
+                    "vertica.username": "dbadmin",
+                    "vertica.password": "",
+                    "vertica.load.method": "DIRECT",
+                    "errors.log.enable": "true",
+                    "errors.log.include.messages": "true",
+                    "topics": "url_override_normalized",
+                    "pk.mode": "record_key",
+                    "pk.fields": "kafkaId",
+                    "transforms": "insert,castboolean,insertcreationdate",
+                    "transforms.insert.type": "org.apache.kafka.connect.transforms.InsertField$Value",
+                    "transforms.insert.static.field": "KafkaKeyIsDeleted",
+                    "transforms.insert.static.value": "0",
+                    "transforms.castboolean.type": "org.apache.kafka.connect.transforms.Cast$Value",
+                    "transforms.castboolean.spec": "KafkaKeyIsDeleted:boolean",
+                    "transforms.insertcreationdate.type": "org.apache.kafka.connect.transforms.InsertField$Value",
+                    "transforms.insertcreationdate.timestamp.field": "dwhCreationDate",
+                    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                    "value.converter" : "Avro",
+                    "value.converter.schema.registry.url":"http://schema-registry:8081",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/vertica-sink/config | jq_docker_cli .
+
+                    # "transforms.timestampconverter.type": "org.apache.kafka.connect.transforms.TimestampConverter$Value",
+                    # "transforms.timestampconverter.field": "dwhCreationDate",
+                    # "transforms.timestampconverter.target.type": "Timestamp",
+
+sleep 10
+
+log "Check data is in Vertica"
+docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
+select * from public.url_override_normalized;
+EOF
+
+#      dwhCreationDate     | kafkaId | ListID | KafkaKeyIsDeleted
+# -------------------------+---------+--------+-------------------
+#  2020-01-17 16:26:53.087 |       1 |      1 | f
+#  2020-01-17 16:26:53.102 |       1 |      1 | f
+#  2020-01-17 16:26:53.103 |       1 |      1 | f
+#  2020-01-17 16:26:53.103 |       1 |      1 | f
+#  2020-01-17 16:26:53.103 |       1 |      1 | f
+#  2020-01-17 16:26:53.103 |       1 |      1 | f
+#  2020-01-17 16:26:53.103 |       1 |      1 | f
+#  2020-01-17 16:26:53.104 |       1 |      1 | f
+#  2020-01-17 16:26:53.104 |       1 |      1 | f
+#  2020-01-17 16:26:53.104 |       1 |      1 | f
+# (10 rows)
