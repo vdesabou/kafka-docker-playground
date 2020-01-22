34945b31a30d14788dc03540a8a59a1f1edd113a
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-22 09:11:29 +0100

    missing env
    Former-commit-id: b6d61dcdad10bcd7b29f9ddbf4620864a2ef2a3d [formerly 254926970ab6299edee7766e5bdf9e1556961cdb [formerly d68d357577c3dbc3411ef5f7d90827dc8cd943e1]]
    Former-commit-id: d41fc98b4e22e53e96593a765e43a6797faed530
    Former-commit-id: 44cb0318025c70691371db6508554b820858178f

--- connect/connect-jdbc-oracle12-sink/stop.sh
+++ connect/connect-jdbc-oracle12-sink/stop.sh
@@ -5,4 +5,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+export ORACLE_IMAGE="oracle/database:12.2.0.1-ee"
+if [ ! -z "$TRAVIS" ]
+then
+     # if this is travis build, use private image.
+     export ORACLE_IMAGE="vdesabou/oracle12"
+fi
+
 ${DIR}/../../environment/plaintext/stop.sh "${PWD}/docker-compose.plaintext.yml"
