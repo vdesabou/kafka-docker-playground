308939032479970a191c1be2ad6a7a1205554709
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-31 13:48:27 +0100

    WIP
    Former-commit-id: a1ae617344c53a192d73ef7084d70182a47c23d1 [formerly c0fbb85ac37da526b1cf9e684cc76ed2c1ea7ea2 [formerly db3584f4694124051a870fdbe6b37651c2b9f9aa]]
    Former-commit-id: 788cb5b94cf9c5ea2c7bde42a63d67d996d8e333
    Former-commit-id: 3583c48e7dbebaf0bcd8de0cc053c30559687239

--- other/cp-ansible-playground/start.sh
+++ other/cp-ansible-playground/start.sh
@@ -8,10 +8,32 @@ verify_installed "git"
 verify_installed "ansible"
 verify_installed "ansible-playbook"
 
+if [ -z "$1" ]
+  then
+    logerror "ERROR: No argument supplied. Version must be provided."
+    exit 1
+fi
+
+export TAG=$1
+
+if [ "$TAG" = "5.3.1" ]
+then
+  GIT_BRANCH="5.3.1-post"
+elif [ "$TAG" = "5.4.0" ]
+then
+  GIT_BRANCH="5.4.0-post"
+else
+    logerror "ERROR: Version $TAG not supported. Only 5.3.1 and 5.4.0 are supported"
+    exit 1
+fi
+
+cd ${DIR}
 if [ ! -d ${DIR}/cp-ansible ]
 then
-    log "INFO: Getting cp-ansible from Github."
+    log "Getting cp-ansible from Github (branch $GIT_BRANCH)"
     git clone https://github.com/confluentinc/cp-ansible
+    cd ${DIR}/cp-ansible
+    git checkout "${GIT_BRANCH}"
 fi
 
 # copy custom files
@@ -28,9 +50,14 @@ cd ${DIR}
 # ls /etc/systemd/system/
 log "INFO: Restarting everything."
 docker exec zookeeper1 systemctl restart confluent-zookeeper
-docker exec broker1 systemctl restart confluent-kafka
-docker exec broker2 systemctl restart confluent-kafka
-docker exec broker3 systemctl restart confluent-kafka
+brokerservice="confluent-kafka"
+if [ "$TAG" = "5.4.0" ]
+then
+  brokerservice="confluent-server"
+fi
+docker exec broker1 systemctl restart $brokerservice
+docker exec broker2 systemctl restart $brokerservice
+docker exec broker3 systemctl restart $brokerservice
 docker exec schema-registry systemctl restart confluent-schema-registry
 docker exec connect systemctl restart confluent-kafka-connect
 docker exec ksql-server systemctl restart confluent-ksql
@@ -41,6 +68,3 @@ docker exec control-center systemctl restart confluent-control-center
 
 log "INFO: Now you can modify the playbooks and run ansible-playbook -i hosts.yml all.yml"
 #ansible-playbook -i hosts.yml all.yml
-
-# if it fails, try to re-run this command
-# ansible-playbook -vvvv -i hosts.yml all.yml
