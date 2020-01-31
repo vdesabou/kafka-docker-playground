a3b8308961d32154004db84f4607824ac07915a8
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-31 10:49:48 +0100

    timeout for KSQL CLI

--- kafka-tutorials/ksql/rekey-with-function/start.sh
+++ kafka-tutorials/ksql/rekey-with-function/start.sh
@@ -11,7 +11,7 @@ docker-compose up -d
 
 # notice the "EOF" -> https://unix.stackexchange.com/a/490970
 log "Invoke manual steps"
-docker exec -i ksql-cli bash -c 'echo -e "\n\n⏳ Waiting for KSQL to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksql-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksql-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksql-server:8088' << "EOF"
+timeout 120 docker exec -i ksql-cli bash -c 'echo -e "\n\n⏳ Waiting for KSQL to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksql-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksql-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksql-server:8088' << "EOF"
 
 SHOW FUNCTIONS;
 DESCRIBE FUNCTION REGEXREPLACE;
