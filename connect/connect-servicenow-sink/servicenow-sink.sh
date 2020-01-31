46a7a45b126763844be127e2ea1bbf8dcb787b65
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-31 16:15:00 +0100

    Added ServiceNow sink connector
    Former-commit-id: eff1e221d2ebab323b7733bb72db4fd9250856e2 [formerly 0d486fee8602ffcbc1ee4435ad92de3cb43819ec [formerly 2eb1ea4513fb597882ba60956e95117ecd005a88]]
    Former-commit-id: c05876289aa3c2d755b77a6fe3ea9029c9bdf79e
    Former-commit-id: e7d1b6762ddd8d5c9fd09f20846bd2d01ff7668b

---
+++ connect/connect-servicenow-sink/servicenow-sink.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+SERVICENOW_URL=${SERVICENOW_URL:-$1}
+SERVICENOW_PASSWORD=${SERVICENOW_PASSWORD:-$2}
+
+if [ -z "$SERVICENOW_URL" ]
+then
+     logerror "SERVICENOW_URL is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SERVICENOW_PASSWORD" ]
+then
+     logerror "SERVICENOW_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Sending messages to topic test_table"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_table --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"u_name","type":"string"},
+{"name":"u_price", "type": "float"}, {"name":"u_quantity", "type": "int"}]}' << EOF
+{"u_name": "scissors", "u_price": 2.75, "u_quantity": 3}
+{"u_name": "tape", "u_price": 0.99, "u_quantity": 10}
+{"u_name": "notebooks", "u_price": 1.99, "u_quantity": 5}
+EOF
+
+log "Creating ServiceNow Sink connector"
+docker exec -e SERVICENOW_URL="$SERVICENOW_URL" -e SERVICENOW_PASSWORD="$SERVICENOW_PASSWORD" -e TODAY="$TODAY" connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "tasks.max": "1",
+                    "connector.class": "io.confluent.connect.servicenow.ServiceNowSinkConnector",
+                    "topics": "test_table",
+                    "servicenow.url": "'"$SERVICENOW_URL"'",
+                    "tasks.max": "1",
+                    "servicenow.table": "u_test_table",
+                    "servicenow.user": "admin",
+                    "servicenow.password": "'"$SERVICENOW_PASSWORD"'",
+                    "key.converter": "io.confluent.connect.avro.AvroConverter",
+                    "key.converter.schema.registry.url": "http://schema-registry:8081",
+                    "value.converter": "io.confluent.connect.avro.AvroConverter",
+                    "value.converter.schema.registry.url": "http://schema-registry:8081",
+                    "reporter.bootstrap.servers": "broker:9092",
+                    "reporter.error.topic.name": "test-error",
+                    "reporter.error.topic.replication.factor": 1,
+                    "reporter.error.topic.key.format": "string",
+                    "reporter.error.topic.value.format": "string",
+                    "reporter.result.topic.name": "test-result",
+                    "reporter.result.topic.key.format": "string",
+                    "reporter.result.topic.value.format": "string",
+                    "reporter.result.topic.replication.factor": 1,
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/servicenow-sink/config | jq .
+
+
+sleep 5
+
+log "Confirm that the messages were delivered to the ServiceNow table"
+docker exec -e SERVICENOW_URL="$SERVICENOW_URL" -e SERVICENOW_PASSWORD="$SERVICENOW_PASSWORD" connect \
+   curl -X GET \
+    "${SERVICENOW_URL}/api/now/table/u_test_table" \
+    --user admin:"$SERVICENOW_PASSWORD" \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -H 'cache-control: no-cache'
