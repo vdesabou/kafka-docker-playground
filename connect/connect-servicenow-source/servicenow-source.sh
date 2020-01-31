2b8b7637e7fd9b1b636834d870457ed809f63439
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-31 15:41:21 +0100

    Added ServiceNow source connector

---
+++ connect/connect-servicenow-source/servicenow-source.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+SERVICENOW_URL=${SERVICENOW_URL:-$1}
+SERVICENOW_PASSWORD=${SERVICENOW_PASSWORD:-$2}
+
+if [ -z "$SERVICENOW_URL" ]
+then
+     logerror "SERVICENOW_URL is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SERVICENOW_PASSWORD" ]
+then
+     logerror "SERVICENOW_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+TODAY=$(date '+%Y-%m-%d')
+
+log "Creating ServiceNow Source connector"
+docker exec -e SERVICENOW_URL="$SERVICENOW_URL" -e SERVICENOW_PASSWORD="$SERVICENOW_PASSWORD" -e TODAY="$TODAY" connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "tasks.max": "1",
+                    "connector.class": "io.confluent.connect.servicenow.ServiceNowSourceConnector",
+                    "kafka.topic": "topic-servicenow",
+                    "servicenow.url": "'"$SERVICENOW_URL"'",
+                    "tasks.max": "1",
+                    "servicenow.table": "incident",
+                    "servicenow.user": "admin",
+                    "servicenow.password": "'"$SERVICENOW_PASSWORD"'",
+                    "servicenow.since": "'"$TODAY"'",
+                    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/servicenow-source/config | jq .
+
+
+sleep 10
+
+log "Create one record to ServiceNow"
+docker exec -e SERVICENOW_URL="$SERVICENOW_URL" -e SERVICENOW_PASSWORD="$SERVICENOW_PASSWORD" connect \
+   curl -X POST \
+    "${SERVICENOW_URL}/api/now/table/incident" \
+    --user admin:"$SERVICENOW_PASSWORD" \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -H 'cache-control: no-cache' \
+    -d '{"short_description": "This is test"}'
+
+sleep 5
+
+log "Verify we have received the data in topic-servicenow topic"
+timeout 60 docker exec connect kafka-console-consumer -bootstrap-server broker:9092 --topic topic-servicenow --from-beginning --max-messages 1
