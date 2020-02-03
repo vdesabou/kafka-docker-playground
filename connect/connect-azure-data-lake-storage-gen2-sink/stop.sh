2db6745cbe421c19a9d32e7caa96c3eedba99d66
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-02-03 12:21:43 +0100

    Fixed stop command
    Former-commit-id: bf1e544717ae648b7fa5859b09287d01cbd0f730 [formerly 7a253074af481c5257ddae28bb5479f444fed93b [formerly d1d25077945f21195c01ffa48a7c1ff8c471b4fa]]
    Former-commit-id: 36deda4ba8b1726d43518244cc148e02cf4fadc8
    Former-commit-id: 527e2cf8ae12a4e89c22c8be40b04852907de075

--- connect/connect-azure-data-lake-storage-gen2-sink/stop.sh
+++ connect/connect-azure-data-lake-storage-gen2-sink/stop.sh
@@ -6,3 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../environment/plaintext/stop.sh "${PWD}/docker-compose.plaintext.yml"
+${DIR}/../../environment/2way-ssl/stop.sh "${PWD}/docker-compose.2way-ssl.yml"
