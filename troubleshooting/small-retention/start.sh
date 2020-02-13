d7440313d78011dea74777bf1a4653c1afed77ce
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-02-13 12:03:32 +0100

    WIP
    Former-commit-id: 497095ea015cbd6d4680f4e25c364f8444f39b65 [formerly 1f55f0eb4a992c7249097f3bce93c9bfa1eb4814 [formerly bbe80dcb95bc7e5ab221a9355f9250f4cd238506]]
    Former-commit-id: d97b18b34737fecf066561818c85ae8456ef2db1
    Former-commit-id: 4759d4f56bc1dd0d7e0910a95e7fba79e60597cf

--- troubleshooting/small-retention/start.sh
+++ troubleshooting/small-retention/start.sh
@@ -2,7 +2,10 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+export TAG=5.3.1
 source ${DIR}/../../scripts/utils.sh
+
+log "Using CP 5.3.1"
 
 if [ ! -f ${DIR}/producer/target/producer-1.0.0-jar-with-dependencies.jar ]
 then
@@ -10,7 +13,14 @@ then
      docker run -it --rm -e TAG=$TAG -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/producer":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/producer/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-8 mvn package
 fi
 
+if [ ! -f ${DIR}/consumer/target/consumer-1.0.0-jar-with-dependencies.jar ]
+then
+     log "Building jar for consumer"
+     docker run -it --rm -e TAG=$TAG -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/consumer":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/consumer/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-8 mvn package
+fi
+
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.yml"
+
 
 log "create a topic testtopic with 30 seconds retention and 15 seconds segment"
 docker exec broker kafka-topics --create --topic testtopic --partitions 1 --replication-factor 1 --zookeeper zookeeper:2181 --config segment.ms=15000 --config retention.ms=30000 --config message.timestamp.type=CreateTime
@@ -21,8 +31,10 @@ docker exec broker kafka-topics --create --topic testtopic --partitions 1 --repl
 
 sleep 1
 
-log "Run the Java producer"
-LOG1="/tmp/log.1"
-docker exec producer bash -c "java -jar producer-1.0.0-jar-with-dependencies.jar" > $LOG1 2>&1
+log "Run the Java producer, logs are in producer.log."
+docker exec producer bash -c "java -jar producer-1.0.0-jar-with-dependencies.jar" > producer.log 2>&1 &
 
-# docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic testtopic --property print.key=true --property print.timestamp=true
+log "Run the Java consumer"
+docker exec consumer bash -c "java -jar consumer-1.0.0-jar-with-dependencies.jar"
+
+# docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic testtopic --from-beginning --property print.key=true --property print.timestamp=true
