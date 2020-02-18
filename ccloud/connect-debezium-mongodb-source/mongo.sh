aa2082057d1d9fc07926de7b5f34263014040041
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-02-18 12:06:42 +0100

    Added debezium mongoDB example with ccloud

---
+++ ccloud/connect-debezium-mongodb-source/mongo.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+#############
+${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose.yml"
+
+if [ -f /tmp/delta_configs/env.delta ]
+then
+     source /tmp/delta_configs/env.delta
+else
+     logerror "ERROR: /tmp/delta_configs/env.delta has not been generated"
+     exit 1
+fi
+#############
+
+log "Creating topic in Confluent Cloud (auto.create.topics.enable=false)"
+set +e
+create_topic dbserver1.inventory.customers
+set -e
+
+log "Initialize MongoDB replica set"
+docker exec -it mongodb mongo --eval 'rs.initiate({_id: "debezium", members:[{_id: 0, host: "mongodb:27017"}]})'
+
+sleep 5
+
+log "Create a user profile"
+docker exec -i mongodb mongo << EOF
+use admin
+db.createUser(
+{
+user: "debezium",
+pwd: "dbz",
+roles: ["dbOwner"]
+}
+)
+EOF
+
+sleep 2
+
+log "Insert a record"
+docker exec -i mongodb mongo << EOF
+use inventory
+db.customers.insert([
+{ _id : 1006, first_name : 'Bob', last_name : 'Hopper', email : 'thebob@example.com' }
+]);
+EOF
+
+log "View record"
+docker exec -i mongodb mongo << EOF
+use inventory
+db.customers.find().pretty();
+EOF
+
+log "Creating Debezium MongoDB source connector"
+docker exec connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class" : "io.debezium.connector.mongodb.MongoDbConnector",
+               "tasks.max" : "1",
+               "mongodb.hosts" : "debezium/mongodb:27017",
+               "mongodb.name" : "dbserver1",
+               "mongodb.user" : "debezium",
+               "mongodb.password" : "dbz",
+               "database.history.kafka.bootstrap.servers": "'"$BOOTSTRAP_SERVERS"'",
+               "database.history.kafka.ssl.endpoint.identification.algorithm" : "https",
+               "database.history.kafka.sasl.mechanism" : "PLAIN",
+               "database.history.kafka.sasl.jaas.config" : "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"'$CLOUD_KEY'\" password=\"'$CLOUD_SECRET'\";",
+               "database.history.kafka.security.protocol" : "SASL_SSL"
+          }' \
+     http://localhost:8083/connectors/debezium-mongodb-source/config | jq .
+
+
+sleep 5
+
+log "Verifying topic dbserver1.inventory.customers"
+docker exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e BASIC_AUTH_CREDENTIALS_SOURCE="$BASIC_AUTH_CREDENTIALS_SOURCE" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" connect bash -c 'kafka-avro-console-consumer --topic dbserver1.inventory.customers --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=$BASIC_AUTH_CREDENTIALS_SOURCE --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --from-beginning --max-messages 1'
