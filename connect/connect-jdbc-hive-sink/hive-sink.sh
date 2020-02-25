555be2fd3983a7ae259306aa907aec3f21ee5226
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-02-25 13:34:18 +0100

    update

--- connect/connect-jdbc-hive-sink/hive-sink.sh
+++ connect/connect-jdbc-hive-sink/hive-sink.sh
@@ -10,8 +10,6 @@ then
      wget https://repo1.maven.org/maven2/org/apache/hive/hive-jdbc/3.1.2/hive-jdbc-3.1.2-standalone.jar
 fi
 
-# https://documentation.progress.com/output/DataDirect/jdbcquickstarts/hivejdbc_quickstart/index.html#page/jdbchivequick%2Fdownloading-the-driver.html%23wwID0EB2AG
-
 if [ ! -f ${DIR}/presto.jar ]
 then
      log "Getting presto-cli-0.183-executable.jar"
@@ -19,7 +17,6 @@ then
      mv presto-cli-0.183-executable.jar presto.jar
      chmod +x presto.jar
 fi
-
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
@@ -29,8 +26,6 @@ log "Create table in hive"
 docker exec -i hive-server /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 << EOF
 CREATE TABLE pokes (foo INT, bar STRING);
 EOF
-# LOAD DATA LOCAL INPATH '/opt/hive/examples/files/kv1.txt' OVERWRITE INTO TABLE pokes;
-
 
 docker exec -i hive-server /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 << EOF
 show databases
@@ -38,8 +33,6 @@ EOF
 
 log "Sending messages to topic pokes"
 seq -f "{\"foo\": %g,\"bar\": \"a string\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic pokes --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"foo","type":"int"},{"name":"bar","type":"string"}]}'
-
-
 
 log "Creating JDBC Hive sink connector"
 docker exec connect \
