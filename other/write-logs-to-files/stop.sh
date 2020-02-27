c5cf8a10beffbfe04196d12f4cb1baf601fe4a78
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-02-27 16:11:56 +0100

    added example with template

--- other/write-logs-to-files/stop.sh
+++ other/write-logs-to-files/stop.sh
@@ -5,4 +5,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-${DIR}/../../environment/plaintext/stop.sh "${PWD}/docker-compose.plaintext.yml"
+${DIR}/../../environment/plaintext/stop.sh "${PWD}/docker-compose.custom-log4j.yml"
+${DIR}/../../environment/plaintext/stop.sh "${PWD}/docker-compose.template-log4j.yml"
