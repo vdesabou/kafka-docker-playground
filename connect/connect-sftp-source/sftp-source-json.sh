3d27e0aaefa3bbfe0386372ebdd53b63fd1a9d53
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-03-06 18:27:53 +0100

    Fix "Couldn't create directory: Failure"
    getting weird "Couldn't create directory: Failure", so created image from this PR https://github.com/atmoz/sftp/pull/164
    https://github.com/amaysim-au/docker-sftp/tree/chroot-user
    docker build -t vdesabou/sftp .
    docker push vdesabou/sftp

--- connect/connect-sftp-source/sftp-source-json.sh
+++ connect/connect-sftp-source/sftp-source-json.sh
@@ -7,18 +7,16 @@ source ${DIR}/../../scripts/utils.sh
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
 docker exec -t sftp-server bash -c "
-mkdir -p /home/foo/upload/input
-mkdir -p /home/foo/upload/error
-mkdir -p /home/foo/upload/finished
+mkdir -p /chroot/home/foo/upload/input
+mkdir -p /chroot/home/foo/upload/error
+mkdir -p /chroot/home/foo/upload/finished
 
-chown -R foo /home/foo/upload/input
-chown -R foo /home/foo/upload/error
-chown -R foo /home/foo/upload/finished
+chown -R foo /chroot/home/foo/upload
 "
 
 echo $'{"id":1,"first_name":"Roscoe","last_name":"Brentnall","email":"rbrentnall0@mediafire.com","gender":"Male","ip_address":"202.84.142.254","last_login":"2018-02-12T06:26:23Z","account_balance":1450.68,"country":"CZ","favorite_color":"#4eaefa"}\n{"id":2,"first_name":"Gregoire","last_name":"Fentem","email":"gfentem1@nsw.gov.au","gender":"Male","ip_address":"221.159.106.63","last_login":"2015-03-27T00:29:56Z","account_balance":1392.37,"country":"ID","favorite_color":"#e8f686"}' > json-sftp-source.json
 
-docker cp json-sftp-source.json sftp-server:/home/foo/upload/input/
+docker cp json-sftp-source.json sftp-server:/chroot/home/foo/upload/input/
 rm -f json-sftp-source.json
 
 log "Creating JSON (no schema) SFTP Source connector"
@@ -30,9 +28,9 @@ docker exec connect \
                "tasks.max": "1",
                "connector.class": "io.confluent.connect.sftp.SftpSchemaLessJsonSourceConnector",
                "behavior.on.error":"IGNORE",
-               "input.path": "/upload/input",
-               "error.path": "/upload/error",
-               "finished.path": "/upload/finished",
+               "input.path": "/home/foo/upload/input",
+               "error.path": "/home/foo/upload/error",
+               "finished.path": "/home/foo/upload/finished",
                "input.file.pattern": "json-sftp-source.json",
                "sftp.username":"foo",
                "sftp.password":"pass",
--- connect/connect-sftp-source/sftp-source-repro-connection-leak2.sh
+++ connect/connect-sftp-source/sftp-source-repro-connection-leak2.sh
@@ -7,13 +7,11 @@ source ${DIR}/../../scripts/utils.sh
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.repro.yml"
 
 docker exec -t sftp-server bash -c "
-mkdir -p /home/foo/upload/input
-mkdir -p /home/foo/upload/error
-mkdir -p /home/foo/upload/finished
+mkdir -p /chroot/home/foo/upload/input
+mkdir -p /chroot/home/foo/upload/error
+mkdir -p /chroot/home/foo/upload/finished
 
-chown -R foo /home/foo/upload/input
-chown -R foo /home/foo/upload/error
-chown -R foo /home/foo/upload/finished
+chown -R foo /chroot/home/foo/upload
 "
 
 log "Installing netstat"
@@ -39,9 +37,9 @@ docker exec connect \
                "connector.class": "io.confluent.connect.sftp.SftpCsvSourceConnector",
                "cleanup.policy":"MOVE",
                "behavior.on.error":"LOG",
-               "input.path": "/upload/input",
-               "error.path": "/upload/error",
-               "finished.path": "/upload/finished",
+               "input.path": "/home/foo/upload/input",
+               "error.path": "/home/foo/upload/error",
+               "finished.path": "/home/foo/upload/finished",
                "input.file.pattern": "csv-sftp-source(.*).csv",
                "sftp.username":"foo",
                "sftp.password":"pass",
@@ -60,7 +58,7 @@ docker exec -t sftp-server bash -c "chown -R root /home/foo/upload"
 
 log "Process a file csv-sftp-source.csv"
 echo $'id,first_name,last_name,email,gender,ip_address,last_login,account_balance,country,favorite_color\n1,Salmon,Baitman,sbaitman0@feedburner.com,Male,120.181.75.98,2015-03-01T06:01:15Z,17462.66,IT,#f09bc0\n2,Debby,Brea,dbrea1@icio.us,Female,153.239.187.49,2018-10-21T12:27:12Z,14693.49,CZ,#73893a' > csv-sftp-source.csv
-docker cp csv-sftp-source.csv sftp-server:/home/foo/upload/input/
+docker cp csv-sftp-source.csv sftp-server:/chroot/home/foo/upload/input/
 rm -f csv-sftp-source.csv
 
 sleep 5
