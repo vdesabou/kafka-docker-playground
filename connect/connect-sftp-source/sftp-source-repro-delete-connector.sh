a4e1a7f5b0691c89d9d3a88e30ddab7e4f9d3a2b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-03-06 18:27:53 +0100

    Fix "Couldn't create directory: Failure"
    getting weird "Couldn't create directory: Failure", so created image from this PR https://github.com/atmoz/sftp/pull/164
    https://github.com/amaysim-au/docker-sftp/tree/chroot-user
    docker build -t vdesabou/sftp .
    docker push vdesabou/sftp

--- connect/connect-sftp-source/sftp-source-repro-delete-connector.sh
+++ connect/connect-sftp-source/sftp-source-repro-delete-connector.sh
@@ -7,13 +7,11 @@ source ${DIR}/../../scripts/utils.sh
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.repro.yml"
 
 docker exec -t sftp-server bash -c "
-mkdir -p /home/foo/upload/input
-mkdir -p /home/foo/upload/error
-mkdir -p /home/foo/upload/finished
+mkdir -p /chroot/home/foo/upload/input
+mkdir -p /chroot/home/foo/upload/error
+mkdir -p /chroot/home/foo/upload/finished
 
-chown -R foo /home/foo/upload/input
-chown -R foo /home/foo/upload/error
-chown -R foo /home/foo/upload/finished
+chown -R foo /chroot/home/foo/upload
 "
 
 for i in $(seq 1 5)
@@ -47,7 +45,7 @@ do
 
      log "Process a file csv-sftp-source$i.csv"
      echo $'id,first_name,last_name,email,gender,ip_address,last_login,account_balance,country,favorite_color\n1,Salmon,Baitman,sbaitman0@feedburner.com,Male,120.181.75.98,2015-03-01T06:01:15Z,17462.66,IT,#f09bc0\n2,Debby,Brea,dbrea1@icio.us,Female,153.239.187.49,2018-10-21T12:27:12Z,14693.49,CZ,#73893a' > csv-sftp-source$i.csv
-     docker cp csv-sftp-source$i.csv sftp-server:/home/foo/upload/input/
+     docker cp csv-sftp-source$i.csv sftp-server:/chroot/home/foo/upload/input/
      rm -f csv-sftp-source$i.csv
 
      sleep 5
--- other/connect-override-policy-sftp-source/sftp-source-csv.sh
+++ other/connect-override-policy-sftp-source/sftp-source-csv.sh
@@ -7,18 +7,16 @@ source ${DIR}/../../scripts/utils.sh
 ${DIR}/../../environment/sasl-plain/start.sh "${PWD}/docker-compose.sasl-plain.yml"
 
 docker exec -t sftp-server bash -c "
-mkdir -p /home/foo/upload/input
-mkdir -p /home/foo/upload/error
-mkdir -p /home/foo/upload/finished
+mkdir -p /chroot/home/foo/upload/input
+mkdir -p /chroot/home/foo/upload/error
+mkdir -p /chroot/home/foo/upload/finished
 
-chown -R foo /home/foo/upload/input
-chown -R foo /home/foo/upload/error
-chown -R foo /home/foo/upload/finished
+chown -R foo /chroot/home/foo/upload
 "
 
 echo $'id,first_name,last_name,email,gender,ip_address,last_login,account_balance,country,favorite_color\n1,Salmon,Baitman,sbaitman0@feedburner.com,Male,120.181.75.98,2015-03-01T06:01:15Z,17462.66,IT,#f09bc0\n2,Debby,Brea,dbrea1@icio.us,Female,153.239.187.49,2018-10-21T12:27:12Z,14693.49,CZ,#73893a' > csv-sftp-source.csv
 
-docker cp csv-sftp-source.csv sftp-server:/home/foo/upload/input/
+docker cp csv-sftp-source.csv sftp-server:/chroot/home/foo/upload/input/
 rm -f csv-sftp-source.csv
 
 # Principal = User:sftp is Denied Operation = Describe from host = 192.168.208.6 on resource = Topic:LITERAL:sftp-testing-topic (kafka.authorizer.logger)
@@ -40,9 +38,9 @@ docker exec connect \
                "connector.class": "io.confluent.connect.sftp.SftpCsvSourceConnector",
                "cleanup.policy":"NONE",
                "behavior.on.error":"IGNORE",
-               "input.path": "/upload/input",
-               "error.path": "/upload/error",
-               "finished.path": "/upload/finished",
+               "input.path": "/home/foo/upload/input",
+               "error.path": "/home/foo/upload/error",
+               "finished.path": "/home/foo/upload/finished",
                "input.file.pattern": "csv-sftp-source.csv",
                "sftp.username":"foo",
                "sftp.password":"pass",
