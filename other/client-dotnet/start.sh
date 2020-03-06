871f9c04b84e810fc96a3f32d2ef257ee6aeb0be
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-03-06 13:50:56 +0100

    minor

--- other/client-dotnet/start.sh
+++ other/client-dotnet/start.sh
@@ -8,5 +8,3 @@ ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.yml" -a -b
 
 log "Starting producer"
 docker exec -i client-dotnet bash -c "dotnet DotNet.dll broker:9092 dotnet-basic-producer"
-
-Control Center is reachable at [http://127.0.0.1:9021](http://127.0.0.1:9021])
