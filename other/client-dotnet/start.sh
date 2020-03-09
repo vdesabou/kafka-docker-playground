71598dc5d1d0a7bd5eaffdac6725c24a80936e73
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-03-09 13:43:54 +0100

    Add possibility to use Core .NET 3.1  or 2.2

--- other/client-dotnet/start.sh
+++ other/client-dotnet/start.sh
@@ -4,6 +4,21 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+CORE_DOT_VERSION=${1:-2.2}
+
+if [[ "$CORE_DOT_VERSION" = "3.1" ]]
+then
+     log "Using .NET Core version 3.1"
+     export CORE_RUNTIME_TAG="3.1.2-bionic"
+     export CORE_SDK_TAG="3.1.102-bionic"
+     export CSPROJ_FILE="DotNet3.1.csproj"
+else
+     log "Using .NET Core version 2.2"
+     export CORE_RUNTIME_TAG="2.2-stretch-slim"
+     export CORE_SDK_TAG="2.2-stretch"
+     export CSPROJ_FILE="DotNet2.2.csproj"
+fi
+
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.yml" -a -b
 
 log "Starting producer"
