8dae111801779b18283c8bbd7b075ab91ab4ec3f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-03-10 09:16:24 +0100

    Same as ccloud example
    Former-commit-id: 207bb358c6fa80fa738815c3c2c3a5c7ace25207 [formerly c914781e87ea3deb7aadc3ca62781fdb4861838f [formerly b0cb5c55dad98aad14f26b999239427d0b7b84b2]]
    Former-commit-id: 225486dadbf9a42aafaac4ea848f01fdb7baeec8
    Former-commit-id: 15f88009a7b0449533ddc552cf21cb540ed138f9

--- other/client-dotnet/Program.cs
+++ other/client-dotnet/Program.cs
@@ -1,114 +1,192 @@
-// Copyright 2016-2017 Confluent Inc., 2015-2016 Andreas Heider
-//
+// Copyright 2019 Confluent Inc.
+
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+
 // http://www.apache.org/licenses/LICENSE-2.0
-//
+
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// Derived from: rdkafka-dotnet, licensed under the 2-clause BSD License.
-//
-// Refer to LICENSE for more information.
 
 using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 
-namespace Confluent.Kafka.Examples.ProducerExample
+namespace CCloud
 {
-    public class Program
+    class Program
     {
-        public static async Task Main(string[] args)
+        static async Task CreateTopicMaybe(string name, int numPartitions, short replicationFactor, ClientConfig cloudConfig)
         {
-            if (args.Length != 2)
+            using (var adminClient = new AdminClientBuilder(cloudConfig).Build())
             {
-                Console.WriteLine("Usage: .. brokerList topicName");
-                return;
-            }
-
-            string brokerList = args[0];
-            string topicName = args[1];
-
-            var config = new ProducerConfig { BootstrapServers = brokerList };
-
-            using (var producer = new ProducerBuilder<string, string>(config).Build())
-            {
-                Console.WriteLine("\n-----------------------------------------------------------------------");
-                Console.WriteLine($"Producer {producer.Name} producing on topic {topicName}.");
-                Console.WriteLine("-----------------------------------------------------------------------");
-                Console.WriteLine("To create a kafka message with UTF-8 encoded key and value:");
-                Console.WriteLine("> key value<Enter>");
-                Console.WriteLine("To create a kafka message with a null key and UTF-8 encoded value:");
-                Console.WriteLine("> value<enter>");
-                Console.WriteLine("Ctrl-C to quit.\n");
-
-                var cancelled = false;
-                Console.CancelKeyPress += (_, e) => {
-                    e.Cancel = true; // prevent the process from terminating.
-                    cancelled = true;
-                };
-
-                while (!cancelled)
+                try
                 {
-                    Console.Write("> ");
-
-                    string text;
-                    try
+                    await adminClient.CreateTopicsAsync(new List<TopicSpecification> {
+                        new TopicSpecification { Name = name, NumPartitions = numPartitions, ReplicationFactor = replicationFactor } });
+                }
+                catch (CreateTopicsException e)
+                {
+                    if (e.Results[0].Error.Code != ErrorCode.TopicAlreadyExists)
                     {
-                        text = Console.ReadLine();
+                        Console.WriteLine($"An error occured creating topic {name}: {e.Results[0].Error.Reason}");
                     }
-                    catch (IOException)
+                    else
                     {
-                        // IO exception is thrown when ConsoleCancelEventArgs.Cancel == true.
-                        break;
-                    }
-                    if (text == null)
-                    {
-                        // Console returned null before
-                        // the CancelKeyPress was treated
-                        break;
-                    }
-
-                    string key = null;
-                    string val = text;
-
-                    // split line if both key and value specified.
-                    int index = text.IndexOf(" ");
-                    if (index != -1)
-                    {
-                        key = text.Substring(0, index);
-                        val = text.Substring(index + 1);
-                    }
-
-                    try
-                    {
-                        // Note: Awaiting the asynchronous produce request below prevents flow of execution
-                        // from proceeding until the acknowledgement from the broker is received (at the
-                        // expense of low throughput).
-                        var deliveryReport = await producer.ProduceAsync(
-                            topicName, new Message<string, string> { Key = key, Value = val });
-
-                        Console.WriteLine($"delivered to: {deliveryReport.TopicPartitionOffset}");
-                    }
-                    catch (ProduceException<string, string> e)
-                    {
-                        Console.WriteLine($"failed to deliver message: {e.Message} [{e.Error.Code}]");
+                        Console.WriteLine("Topic already exists");
                     }
                 }
+            }
+        }
 
-                // Since we are producing synchronously, at this point there will be no messages
-                // in-flight and no delivery reports waiting to be acknowledged, so there is no
-                // need to call producer.Flush before disposing the producer.
+        static void Produce(string topic, ClientConfig config)
+        {
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                int numProduced = 0;
+                int numMessages = 100;
+                for (int i=0; i<numMessages; ++i)
+                {
+                    var key = "alice";
+                    var val = JObject.FromObject(new { count = i }).ToString(Formatting.None);
+
+                    Console.WriteLine($"Producing record: {key} {val}");
+
+                    producer.Produce(topic, new Message<string, string> { Key = key, Value = val },
+                        (deliveryReport) =>
+                        {
+                            if (deliveryReport.Error.Code != ErrorCode.NoError)
+                            {
+                                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Produced message to: {deliveryReport.TopicPartitionOffset}");
+                                numProduced += 1;
+                            }
+                        });
+                }
+
+                producer.Flush(TimeSpan.FromSeconds(10));
+
+                Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+            }
+        }
+
+        static void Consume(string topic, ClientConfig config)
+        {
+            var consumerConfig = new ConsumerConfig(config);
+            consumerConfig.GroupId = "dotnet-example-group-1";
+            consumerConfig.AutoOffsetReset = AutoOffsetReset.Earliest;
+            consumerConfig.EnableAutoCommit = false;
+            consumerConfig.FetchMinBytes = 100;
+            consumerConfig.MaxPartitionFetchBytes = 400000;
+            consumerConfig.SessionTimeoutMs = 10000;
+            consumerConfig.PartitionAssignmentStrategy = PartitionAssignmentStrategy.Range;
+            consumerConfig.ClientId = "IntTests";
+            consumerConfig.HeartbeatIntervalMs = 300;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            using (var consumer = new ConsumerBuilder<string, string>(consumerConfig)
+                // Note: All handlers are called on the main .Consume thread.
+                .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+                .SetStatisticsHandler((_, json) => Console.WriteLine($"Statistics: {json}"))
+                .SetPartitionsAssignedHandler((c, partitions) =>
+                {
+                    Console.WriteLine($"Assigned partitions: [{string.Join(", ", partitions)}]");
+                    // possibly manually specify start offsets or override the partition assignment provided by
+                    // the consumer group by returning a list of topic/partition/offsets to assign to, e.g.:
+                    //
+                    // return partitions.Select(tp => new TopicPartitionOffset(tp, externalOffsets[tp]));
+                })
+                .SetPartitionsRevokedHandler((c, partitions) =>
+                {
+                    Console.WriteLine($"Revoking assignment: [{string.Join(", ", partitions)}]");
+                }).Build())
+            {
+                consumer.Subscribe(topic);
+                var totalCount = 0;
+                try
+                {
+                    while (true)
+                    {
+                        var cr = consumer.Consume(cts.Token);
+
+                        if (cr.IsPartitionEOF)
+                        {
+                            Console.WriteLine(
+                                $"Reached end of topic {cr.Topic}, partition {cr.Partition}, offset {cr.Offset}.");
+
+                            continue;
+                        }
+                        totalCount += JObject.Parse(cr.Value).Value<int>("count");
+                        Console.WriteLine($"Consumed record with key {cr.Key} and value {cr.Value}, and updated total count to {totalCount}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ctrl-C was pressed.
+                }
+                catch (ConsumeException e)
+                {
+                    Console.WriteLine($"Consume error: {e.Error.Reason}");
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("usage: .. produce|consume <topic>");
+            System.Environment.Exit(1);
+        }
+
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 2) { PrintUsage(); }
+
+            var mode = args[0];
+            var topic = args[1];
+
+            var config = new ClientConfig
+                {
+                    BootstrapServers = "broker:9092",
+                    ApiVersionRequest=true,
+                    ApiVersionFallbackMs=0,
+                };
+
+            switch (mode)
+            {
+                case "produce":
+                    await CreateTopicMaybe(topic, 1, 1, config);
+                    Produce(topic, config);
+                    break;
+                case "consume":
+                    Consume(topic, config);
+                    break;
+                default:
+                    PrintUsage();
+                    break;
             }
         }
     }
