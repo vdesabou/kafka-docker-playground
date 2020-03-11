47f47aecc80563b6d515ebe9079acc7c8911b0e0
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-03-11 10:24:02 +0100

    update

--- kafka-tutorials/ksql/join-stream-and-table/src/statements.sql
+++ kafka-tutorials/ksql/join-stream-and-table/src/statements.sql
@@ -1,13 +1,13 @@
-CREATE TABLE movies (id INT, title VARCHAR, release_year INT)
-    WITH (kafka_topic='movies', key='id', partitions=1, value_format='avro');
+CREATE TABLE movies (ROWKEY INT KEY, title VARCHAR, release_year INT)
+    WITH (kafka_topic='movies', partitions=1, value_format='avro');
 
-CREATE STREAM ratings (id INT, rating DOUBLE)
+CREATE STREAM ratings (ROWKEY INT KEY, rating DOUBLE)
     WITH (kafka_topic='ratings', partitions=1, value_format='avro');
 
 CREATE STREAM rated_movies
     WITH (kafka_topic='rated_movies',
           partitions=1,
           value_format='avro') AS
-    SELECT ratings.id AS id, title, release_year, rating
+    SELECT ratings.rowkey AS id, title, release_year, rating
     FROM ratings
-    LEFT JOIN movies ON ratings.id = movies.id;
+    LEFT JOIN movies ON ratings.rowkey = movies.rowkey;
