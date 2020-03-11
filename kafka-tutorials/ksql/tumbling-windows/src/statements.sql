75239606fd4719a1135984b71156701082312464
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-03-11 10:31:21 +0100

    update

--- kafka-tutorials/ksql/tumbling-windows/src/statements.sql
+++ kafka-tutorials/ksql/tumbling-windows/src/statements.sql
@@ -10,8 +10,8 @@ CREATE TABLE rating_count
     WITH (kafka_topic='rating_count') AS
     SELECT title,
            COUNT(*) AS rating_count,
-           WINDOWSTART() AS window_start,
-           WINDOWEND() AS window_end
+           WINDOWSTART AS window_start,
+           WINDOWEND AS window_end
     FROM ratings
     WINDOW TUMBLING (SIZE 6 HOURS)
     GROUP BY title;
