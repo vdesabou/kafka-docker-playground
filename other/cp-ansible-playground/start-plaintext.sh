36554c7f0d2a25bd21caa7b2b255a8ba17dded64
Vincent de Saboulin <vsaboulin@confluent.io>
2020-03-12 16:51:24 +0100

    update

--- other/cp-ansible-playground/start-plaintext.sh
+++ other/cp-ansible-playground/start-plaintext.sh
@@ -16,19 +16,26 @@ then
 elif [ "$TAG" = "5.4.0" ]
 then
   GIT_BRANCH="5.4.0-post"
+elif [ "$TAG" = "5.4.1" ]
+then
+  GIT_BRANCH="5.4.1-post"
 else
-    logerror "ERROR: Version $TAG not supported. Only 5.3.1 and 5.4.0 are supported"
+    logerror "ERROR: Version $TAG not supported. Only 5.3.1, 5.4.0 or 5.4.1 are supported"
     exit 1
 fi
 
-cd ${DIR}
-if [ ! -d ${DIR}/cp-ansible ]
+if [ -d ${DIR}/cp-ansible ]
 then
-    log "Getting cp-ansible from Github (branch $GIT_BRANCH)"
-    git clone https://github.com/confluentinc/cp-ansible
-    cd ${DIR}/cp-ansible
-    git checkout "${GIT_BRANCH}"
+  logwarn "${DIR}/cp-ansible already exists ! It will be deleted !"
+  check_if_continue
 fi
+
+rm -rf ${DIR}/cp-ansible
+log "Getting cp-ansible from Github (branch $GIT_BRANCH)"
+cd ${DIR}
+git clone https://github.com/confluentinc/cp-ansible
+cd ${DIR}/cp-ansible
+git checkout "${GIT_BRANCH}"
 
 # copy custom files
 cp ${DIR}/${HOSTS_FILE} ${DIR}/cp-ansible/
