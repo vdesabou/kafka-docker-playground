ddf3ea0cd1f79ca57be9ab91372e5471eb8ebb61
Vincent de Saboulin <vsaboulin@confluent.io>
2020-03-13 16:30:56 +0100

    Added Azure Event Hubs Source connector

---
+++ connect/connect-azure-event-hubs-source/simple-send/src/main/java/com/microsoft/azure/eventhubs/samples/SimpleSend/SimpleSend.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+package com.microsoft.azure.eventhubs.samples.SimpleSend;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.microsoft.azure.eventhubs.ConnectionStringBuilder;
+import com.microsoft.azure.eventhubs.EventData;
+import com.microsoft.azure.eventhubs.EventHubClient;
+import com.microsoft.azure.eventhubs.EventHubException;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.time.Instant;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class SimpleSend {
+
+    public static void main(String[] args)
+            throws EventHubException, ExecutionException, InterruptedException, IOException {
+
+        final ConnectionStringBuilder connStr = new ConnectionStringBuilder()
+                .setNamespaceName(System.getenv("AZURE_EVENT_HUBS_NAMESPACE")) // to target National clouds - use .setEndpoint(URI)
+                .setEventHubName(System.getenv("AZURE_EVENT_HUBS_NAME"))
+                .setSasKeyName(System.getenv("AZURE_SAS_KEYNAME"))
+                .setSasKey(System.getenv("AZURE_SAS_KEY"));
+
+        final Gson gson = new GsonBuilder().create();
+
+        // The Executor handles all asynchronous tasks and this is passed to the EventHubClient instance.
+        // This enables the user to segregate their thread pool based on the work load.
+        // This pool can then be shared across multiple EventHubClient instances.
+        // The following sample uses a single thread executor, as there is only one EventHubClient instance,
+        // handling different flavors of ingestion to Event Hubs here.
+        final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(4);
+
+        // Each EventHubClient instance spins up a new TCP/SSL connection, which is expensive.
+        // It is always a best practice to reuse these instances. The following sample shows this.
+        final EventHubClient ehClient = EventHubClient.createFromConnectionStringSync(connStr.toString(), executorService);
+
+        try {
+            for (int i = 0; i < 100; i++) {
+
+                String payload = "Message " + Integer.toString(i);
+                //PayloadEvent payload = new PayloadEvent(i);
+                byte[] payloadBytes = gson.toJson(payload).getBytes(Charset.defaultCharset());
+                EventData sendEvent = EventData.create(payloadBytes);
+
+                // Send - not tied to any partition
+                // Event Hubs service will round-robin the events across all Event Hubs partitions.
+                // This is the recommended & most reliable way to send to Event Hubs.
+                ehClient.sendSync(sendEvent);
+            }
+
+            System.out.println(Instant.now() + ": Send Complete...");
+            System.out.println("Press Enter to stop.");
+            System.in.read();
+        } finally {
+            ehClient.closeSync();
+            executorService.shutdown();
+        }
+    }
+}
