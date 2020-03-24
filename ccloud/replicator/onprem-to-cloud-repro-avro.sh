f06e06a4b9e0b792720c19c5c27386509ba84f8f
Vincent de Saboulin <vsaboulin@confluent.io>
2020-03-24 17:54:14 +0100

    Added README and avro converter example
    Former-commit-id: 71b9ebc7fa3dccdc48cfa5ebf648df379d4ddbb4 [formerly 9edd62ef7759a20ffb6f70c787785f5458c32bf6 [formerly 0f6e4bc4b75f018d6a242b5d985fba3cc1257599]]
    Former-commit-id: 61e5830443166726cd3fbdacd1f1abb5d2ba6bc8
    Former-commit-id: c9dceef42b624f9ee3ca8c58997b9ac92eddd78f

--- ccloud/replicator/onprem-to-cloud-repro-avro.sh
+++ ccloud/replicator/onprem-to-cloud-repro-avro.sh
@@ -18,27 +18,32 @@ fi
 
 log "Creating topic in Confluent Cloud (auto.create.topics.enable=false)"
 set +e
-create_topic products
+create_topic products-avro
 set -e
 
-log "Sending messages to topic products on source OnPREM cluster"
-docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic products --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"name","type":"string"},
+log "Sending messages to topic products-avro on source OnPREM cluster"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic products-avro --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"name","type":"string"},
 {"name":"price", "type": "float"}, {"name":"quantity", "type": "int"}]}' << EOF
 {"name": "scissors", "price": 2.75, "quantity": 3}
 {"name": "tape", "price": 0.99, "quantity": 10}
 {"name": "notebooks", "price": 1.99, "quantity": 5}
 EOF
 
-docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e CLOUD_KEY="$CLOUD_KEY" -e CLOUD_SECRET="$CLOUD_SECRET" -e BOOTSTRAP_SERVERS_SRC="$BOOTSTRAP_SERVERS_SRC" -e CLOUD_KEY_SRC="$CLOUD_KEY_SRC" -e CLOUD_SECRET_SRC="$CLOUD_SECRET_SRC" connect \
+docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e CLOUD_KEY="$CLOUD_KEY" -e CLOUD_SECRET="$CLOUD_SECRET" -e BOOTSTRAP_SERVERS_SRC="$BOOTSTRAP_SERVERS_SRC" -e CLOUD_KEY_SRC="$CLOUD_KEY_SRC" -e CLOUD_SECRET_SRC="$CLOUD_SECRET_SRC" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" connect \
      curl -X PUT \
      -H "Content-Type: application/json" \
      --data '{
           "connector.class":"io.confluent.connect.replicator.ReplicatorSourceConnector",
-          "key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
-          "value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
-          "header.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
           "src.consumer.group.id": "replicate-onprem-to-cloud",
+          "src.value.converter": "io.confluent.connect.avro.AvroConverter",
+          "src.value.converter.schema.registry.url": "http://schema-registry:8081",
           "src.kafka.bootstrap.servers": "broker:9092",
+
+          "value.converter": "io.confluent.connect.avro.AvroConverter",
+          "value.converter.schema.registry.url": "'"$SCHEMA_REGISTRY_URL"'",
+          "value.converter.basic.auth.user.info": "'"$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO"'",
+          "value.converter.basic.auth.credentials.source": "USER_INFO",
+
           "dest.kafka.ssl.endpoint.identification.algorithm":"https",
           "dest.kafka.bootstrap.servers": "'"$BOOTSTRAP_SERVERS"'",
           "dest.kafka.security.protocol" : "SASL_SSL",
@@ -53,12 +58,12 @@ docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e CLOUD_KEY="$C
           "confluent.topic.security.protocol" : "SASL_SSL",
           "confluent.topic.replication.factor": "3",
           "provenance.header.enable": true,
-          "topic.whitelist": "products",
+          "topic.whitelist": "products-avro",
           "topic.config.sync": false,
           "topic.auto.create": false
           }' \
      http://localhost:8083/connectors/replicate-onprem-to-cloud/config | jq .
 
 
-log "Verify we have received the data in products topic"
-timeout 60 docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e BASIC_AUTH_CREDENTIALS_SOURCE="$BASIC_AUTH_CREDENTIALS_SOURCE" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" connect bash -c 'kafka-avro-console-consumer --topic products --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=$BASIC_AUTH_CREDENTIALS_SOURCE --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --from-beginning --max-messages 3'
+log "Verify we have received the data in products-avro topic"
+timeout 60 docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e BASIC_AUTH_CREDENTIALS_SOURCE="$BASIC_AUTH_CREDENTIALS_SOURCE" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" connect bash -c 'kafka-avro-console-consumer --topic products-avro --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=$BASIC_AUTH_CREDENTIALS_SOURCE --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --from-beginning --max-messages 3'
