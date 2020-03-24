346db60fe0ad67342e6a74799638773a660ce84b
Vincent de Saboulin <vsaboulin@confluent.io>
2020-03-24 17:27:28 +0100

    onprem to cloud example

--- ccloud/replicator/stop.sh
+++ ccloud/replicator/stop.sh
@@ -6,3 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-cloud-to-cloud.yml"
+${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-onprem-to-cloud.yml"
