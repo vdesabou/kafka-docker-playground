a9b97fdfaa616e8a4537c964d1ca95364d031577
Vincent de Saboulin <vsaboulin@confluent.io>
2020-03-26 17:26:08 +0100

    adding example with TopicRecordNameStrategy
    Former-commit-id: d194c3e148b71eeffe18470e8da76eea6d033efe [formerly 25646fb946e5d018463d69396f7c9051957f14fe [formerly 0a41fc5f77e1978108c63ef640f7d4fd61082b16]]
    Former-commit-id: 562cdc9373a44d3084f2d56acbc7a3d679f8987e
    Former-commit-id: 15eaeab3963db3dc30db4a1e9028fa2d6186b586

---
+++ ccloud/replicator/producer/src/main/java/com/github/vdesabou/SimpleProducer.java
@@ -0,0 +1,110 @@
+package com.github.vdesabou;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.config.SaslConfigs;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import com.github.vdesabou.Customer;
+import java.util.Date;
+
+public class SimpleProducer {
+
+    private static final String TOPIC = "executable-products-avro";
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Properties props = new Properties();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("BOOTSTRAP_SERVERS"));
+
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 20000);
+        props.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 500);
+        props.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
+
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+
+        // Schema Registry specific settings
+        props.put("schema.registry.url", System.getenv("SCHEMA_REGISTRY_URL"));
+
+        props.put("value.subject.name.strategy", "io.confluent.kafka.serializers.subject.TopicRecordNameStrategy");
+
+        System.out.println("Sending data to `customer` topic. Properties: " + props.toString());
+
+        try (Producer<String, Customer> producer = new KafkaProducer<>(props)) {
+            long i = 0;
+
+            while (true) {
+                String key = "" + i;
+
+                ProducerRecord<String, Customer> record;
+                if(i==2) {
+                    record = new ProducerRecord<>(TOPIC, key, null);
+
+                } else if(i==3) {
+                    // test with default values
+                    Customer customer = Customer.newBuilder()
+                    .setListID(null)
+                    .setNormalizedHashItemID(null)
+                    .setURL(null)
+                    .setMyTable("customer1")
+                    .setMyFloatValue(null)
+                    .setMyTimestamp(new Date().getTime())
+                    .build();
+                    record = new ProducerRecord<>(TOPIC, key, customer);
+                } else if(i==4) {
+                    Customer customer = Customer.newBuilder()
+                    .setListID(i)
+                    .setNormalizedHashItemID(i)
+                    .setURL("ultralongurlultralongurlultralongurlultralongurlultralongurlultralongurlultralongurultralongurl")
+                    .setMyTable("customer1")
+                    .setMyFloatValue(0.28226356681351483)
+                    .setMyTimestamp(new Date().getTime())
+                    .build();
+                    record = new ProducerRecord<>(TOPIC, key, customer);
+                } else {
+                    String tableName;
+                    if(i % 2 == 0) {
+                        tableName = "customer1";
+                    } else {
+                        tableName = "customer2";
+                    }
+
+                    Customer customer = Customer.newBuilder()
+                    .setListID(i)
+                    .setNormalizedHashItemID(i)
+                    .setURL("url")
+                    .setMyTable(tableName)
+                    .setMyFloatValue(0.28226356681351483)
+                    .setMyTimestamp(new Date().getTime())
+                    .build();
+                    record = new ProducerRecord<>(TOPIC, key, customer);
+                }
+
+                System.out.println("Sending " + record.key() + " " + record.value());
+                producer.send(record, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        if (exception == null) {
+                            System.out.printf("Produced record to topic %s partition [%d] @ offset %d%n", metadata.topic(), metadata.partition(), metadata.offset());
+                        } else {
+                            exception.printStackTrace();
+                        }
+                    }
+                });
+                producer.flush();
+                i++;
+                //TimeUnit.MILLISECONDS.sleep(1);
+            }
+        }
+    }
+}
