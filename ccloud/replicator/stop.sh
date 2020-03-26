03c968ec51fad783813e13648d5b49f36f59a1a8
Vincent de Saboulin <vsaboulin@confluent.io>
2020-03-26 11:09:37 +0100

    updates

--- ccloud/replicator/stop.sh
+++ ccloud/replicator/stop.sh
@@ -6,4 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-cloud-to-cloud.yml"
-${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-onprem-to-cloud.yml"
+${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-connect-onprem-to-cloud.yml"
+${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-executable-onprem-to-cloud.yml"
