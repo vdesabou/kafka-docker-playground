0a41fc5f77e1978108c63ef640f7d4fd61082b16
Vincent de Saboulin <vsaboulin@confluent.io>
2020-03-26 17:26:08 +0100

    adding example with TopicRecordNameStrategy

---
+++ ccloud/replicator/executable-onprem-to-cloud-repro-avro-issue-topicrecordnamestrategy.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f ${DIR}/producer/target/producer-1.0.0-jar-with-dependencies.jar ]
+then
+     log "Building jar for producer"
+     docker run -it --rm -e TAG=$TAG_BASE -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/producer":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/producer/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-8 mvn package
+fi
+
+#############
+${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose-executable-onprem-to-cloud-topicrecordnamestrategy.yml" -a -b
+
+if [ -f /tmp/delta_configs/env.delta ]
+then
+     source /tmp/delta_configs/env.delta
+else
+     logerror "ERROR: /tmp/delta_configs/env.delta has not been generated"
+     exit 1
+fi
+#############
+
+# generate executable-onprem-to-cloud-producer-repro-avro.properties config
+sed -e "s|:BOOTSTRAP_SERVERS:|$BOOTSTRAP_SERVERS|g" \
+    -e "s|:CLOUD_KEY:|$CLOUD_KEY|g" \
+    -e "s|:CLOUD_SECRET:|$CLOUD_SECRET|g" \
+    ${DIR}/executable-onprem-to-cloud-producer-repro-avro.properties > ${DIR}/tmp
+mv ${DIR}/tmp ${DIR}/executable-onprem-to-cloud-producer-repro-avro.properties
+
+# generate executable-onprem-to-cloud-replicator-repro-avro-topicrecordnamestrategy.properties config
+sed -e "s|:SCHEMA_REGISTRY_URL:|$SCHEMA_REGISTRY_URL|g" \
+    -e "s|:SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO:|$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO|g" \
+    ${DIR}/executable-onprem-to-cloud-replicator-repro-avro-topicrecordnamestrategy.properties > ${DIR}/tmp
+mv ${DIR}/tmp ${DIR}/executable-onprem-to-cloud-replicator-repro-avro-topicrecordnamestrategy.properties
+
+log "Creating topic in Confluent Cloud (auto.create.topics.enable=false)"
+set +e
+delete_topic executable-products-avro
+sleep 3
+create_topic executable-products-avro
+delete_topic connect-onprem-to-cloud-avro.offsets
+delete_topic connect-onprem-to-cloud-avro.status
+delete_topic connect-onprem-to-cloud-avro.config
+set -e
+
+log "Delete schema for topic"
+set +e
+ccloud schema-registry schema delete --subject executable-products-avro-value --version latest
+set -e
+
+# Avoid java.lang.OutOfMemoryError: Java heap space
+docker container restart connect
+sleep 5
+
+log "Sending messages to topic executable-products-avro (done using JAVA producer)"
+
+log "Starting replicator executable (logs are in /tmp/replicator.log):"
+# run in detach mode -d
+docker exec -d connect bash -c 'replicator --consumer.config /etc/kafka/executable-onprem-to-cloud-consumer-repro-avro.properties --producer.config /etc/kafka/executable-onprem-to-cloud-producer-repro-avro.properties  --replication.config /etc/kafka/executable-onprem-to-cloud-replicator-repro-avro-topicrecordnamestrategy.properties  --cluster.id executable-onprem-to-cloud --whitelist executable-products-avro > /tmp/replicator.log 2>&1'
+
+# docker exec connect bash -c 'replicator --consumer.config /etc/kafka/executable-onprem-to-cloud-consumer-repro-avro.properties --producer.config /etc/kafka/executable-onprem-to-cloud-producer-repro-avro.properties  --replication.config /etc/kafka/executable-onprem-to-cloud-replicator-repro-avro-topicrecordnamestrategy.properties  --cluster.id executable-onprem-to-cloud --whitelist executable-products-avro'
+
+sleep 50
+log "Verify we have received the data in executable-products-avro topic"
+timeout 60 docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e BASIC_AUTH_CREDENTIALS_SOURCE="$BASIC_AUTH_CREDENTIALS_SOURCE" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" connect bash -c 'kafka-avro-console-consumer --topic executable-products-avro --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=$BASIC_AUTH_CREDENTIALS_SOURCE --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --from-beginning --max-messages 3'
+
+log "Copying replicator logs to /tmp/replicator.log"
+docker cp connect:/tmp/replicator.log /tmp/replicator.log
