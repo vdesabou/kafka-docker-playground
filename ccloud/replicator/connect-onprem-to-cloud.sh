f2942d7d1d099fda94b4c1697cf290bdbf706a77
Vincent de Saboulin <vsaboulin@confluent.io>
2020-03-26 11:09:37 +0100

    updates
    Former-commit-id: 7378000b489f961b4f85e5d59bf1bfee3b564c92 [formerly 842a05b071ce38f381543c12e60585e3eda1706d [formerly 03c968ec51fad783813e13648d5b49f36f59a1a8]]
    Former-commit-id: ac4aee2deb84d5c874b985ac6238d9d427a643c3
    Former-commit-id: 69d2ebee0d8b537d0d2c8719404e566e306c3e09

--- ccloud/replicator/connect-onprem-to-cloud.sh
+++ ccloud/replicator/connect-onprem-to-cloud.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
 #############
-${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose-onprem-to-cloud.yml"
+${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose-connect-onprem-to-cloud.yml"
 
 if [ -f /tmp/delta_configs/env.delta ]
 then
