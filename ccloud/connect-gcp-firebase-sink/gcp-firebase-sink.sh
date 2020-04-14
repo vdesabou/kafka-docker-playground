222eb757d13019ccd1e1f3488c73546c6783d863
Vincent de Saboulin <vsaboulin@confluent.io>
2020-04-14 18:01:37 +0200

    Added GCP Firebase sink example with CCloud
    Former-commit-id: 21bb8b0379744aa7045827a6c3bbf3eae82e6dd0 [formerly 344f1697e2b5ca55dc44ad4bfddab45fab922f84 [formerly 49e422bfd2172dec9eebc4dcef847a0efe26484d]]
    Former-commit-id: 173b9cfb1e50f0665219ae6b35a289bd12ce9101
    Former-commit-id: ccf3f29b4b08bbdd84e2fef0be7b0e6112be0c1b

---
+++ ccloud/connect-gcp-firebase-sink/gcp-firebase-sink.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+PROJECT=${1:-vincent-de-saboulin-lab}
+
+KEYFILE="${DIR}/keyfile.json"
+if [ ! -f ${KEYFILE} ]
+then
+     logerror "ERROR: the file ${KEYFILE} file is not present!"
+     exit 1
+fi
+
+#############
+${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose.yml"
+
+if [ -f /tmp/delta_configs/env.delta ]
+then
+     source /tmp/delta_configs/env.delta
+else
+     logerror "ERROR: /tmp/delta_configs/env.delta has not been generated"
+     exit 1
+fi
+#############
+
+log "Creating topic in Confluent Cloud (auto.create.topics.enable=false)"
+set +e
+create_topic artists
+create_topic songs
+set -e
+
+log "Creating GCP Firebase Sink connector"
+docker exec -e PROJECT="$PROJECT" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class" : "io.confluent.connect.firebase.FirebaseSinkConnector",
+                    "tasks.max" : "1",
+                    "topics":"artists,songs",
+                    "gcp.firebase.credentials.path": "/root/keyfiles/keyfile.json",
+                    "gcp.firebase.database.reference": "https://'"$PROJECT"'.firebaseio.com/musicBlog",
+                    "insert.mode":"update",
+                    "key.converter" : "io.confluent.connect.avro.AvroConverter",
+                    "key.converter.schema.registry.url": "'"$SCHEMA_REGISTRY_URL"'",
+                    "key.converter.basic.auth.user.info": "'"$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO"'",
+                    "key.converter.basic.auth.credentials.source": "USER_INFO",
+                    "value.converter" : "io.confluent.connect.avro.AvroConverter",
+                    "value.converter.schema.registry.url": "'"$SCHEMA_REGISTRY_URL"'",
+                    "value.converter.basic.auth.user.info": "'"$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO"'",
+                    "value.converter.basic.auth.credentials.source": "USER_INFO",
+                    "confluent.topic.ssl.endpoint.identification.algorithm" : "https",
+                    "confluent.topic.sasl.mechanism" : "PLAIN",
+                    "confluent.topic.bootstrap.servers": "'"$BOOTSTRAP_SERVERS"'",
+                    "confluent.topic.sasl.jaas.config" : "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"'$CLOUD_KEY'\" password=\"'$CLOUD_SECRET'\";",
+                    "confluent.topic.security.protocol" : "SASL_SSL",
+                    "confluent.topic.replication.factor": "3"
+          }' \
+     http://localhost:8083/connectors/firebase-sink/config | jq .
+
+
+log "Produce Avro data to topic artists"
+docker exec -i -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e BASIC_AUTH_CREDENTIALS_SOURCE="$BASIC_AUTH_CREDENTIALS_SOURCE" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" connect kafka-avro-console-producer --broker-list $BOOTSTRAP_SERVERS --producer-property ssl.endpoint.identification.algorithm=https --producer-property sasl.mechanism=PLAIN --producer-property security.protocol=SASL_SSL --producer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=$BASIC_AUTH_CREDENTIALS_SOURCE --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --topic artists --property parse.key=true --property key.schema='{"type":"string"}' --property "key.separator=:" --property value.schema='{"type":"record","name":"artists","fields":[{"name":"name","type":"string"},{"name":"genre","type":"string"}]}' << EOF
+"artistId1":{"name":"Michael Jackson","genre":"Pop"}
+"artistId2":{"name":"Bob Dylan","genre":"American folk"}
+"artistId3":{"name":"Freddie Mercury","genre":"Rock"}
+"artistId4":{"name":"Vincent McMorrow","genre":"Other"}
+EOF
+
+log "Produce Avro data to topic songs"
+docker exec -i -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e BASIC_AUTH_CREDENTIALS_SOURCE="$BASIC_AUTH_CREDENTIALS_SOURCE" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" connect kafka-avro-console-producer --broker-list $BOOTSTRAP_SERVERS --producer-property ssl.endpoint.identification.algorithm=https --producer-property sasl.mechanism=PLAIN --producer-property security.protocol=SASL_SSL --producer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=$BASIC_AUTH_CREDENTIALS_SOURCE --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --topic songs --property parse.key=true --property key.schema='{"type":"string"}' --property "key.separator=:" --property value.schema='{"type":"record","name":"songs","fields":[{"name":"title","type":"string"},{"name":"artist","type":"string"}]}' << EOF
+"songId1":{"title":"billie jean","artist":"Michael Jackson"}
+"songId2":{"title":"hurricane","artist":"Bob Dylan"}
+"songId3":{"title":"bohemian rhapsody","artist":"Freddie Mercury"}
+EOF
+
+log "Follow README to verify data is in Firebase"
