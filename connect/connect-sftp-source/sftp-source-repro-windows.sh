7dea24150d0b41b08a87a73b1e2644afcb4df71f
Vincent de Saboulin <vsaboulin@confluent.io>
2020-04-17 10:32:04 +0200

    using windows server hosted on EC2

---
+++ connect/connect-sftp-source/sftp-source-repro-windows.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.repro-windows.yml"
+
+log "Creating CSV file"
+echo $'id,first_name,last_name,email,gender,ip_address,last_login,account_balance,country,favorite_color\n1,Salmon,Baitman,sbaitman0@feedburner.com,Male,120.181.75.98,2015-03-01T06:01:15Z,17462.66,IT,#f09bc0\n2,Debby,Brea,dbrea1@icio.us,Female,153.239.187.49,2018-10-21T12:27:12Z,14693.49,CZ,#73893a' > csv-sftp-source1.csv
+
+log "Creating CSV SFTP Source connector"
+docker exec connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "topics": "test_sftp_sink",
+               "tasks.max": "1",
+               "connector.class": "io.confluent.connect.sftp.SftpCsvSourceConnector",
+               "cleanup.policy":"MOVE",
+               "behavior.on.error":"LOG",
+               "input.path": "/",
+               "error.path": "/error",
+               "finished.path": "/finished",
+               "input.file.pattern": "csv-sftp-source(.*).csv",
+               "sftp.username":"xxx",
+               "sftp.password":"xxxx",
+               "sftp.host":"x.x.x.x",
+               "sftp.port":"2222",
+               "kafka.topic": "sftp-testing-topic",
+               "csv.first.row.as.header": "true",
+               "schema.generation.enabled": "true"
+          }' \
+     http://localhost:8083/connectors/sftp-source-csv/config | jq .
+
+log "Use your SFTP client to copy csv-sftp-source1.csv"
+
+exit 0
+
+sleep 5
+
+log "Verifying topic sftp-testing-topic"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic sftp-testing-topic --from-beginning --max-messages 2
