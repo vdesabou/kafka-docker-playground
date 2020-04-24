d0f279e8a4149a8371f6bc2365df1e31e137fe8d
Vincent de Saboulin <vsaboulin@confluent.io>
2020-04-24 14:52:34 +0200

    jdk11
    Former-commit-id: c5f6caec1ccea8e02bafa65ef8edcd32ce6d1370 [formerly 5d1c217e01131fa640324d076c348cc78a81b8e1 [formerly 5f98d492403fdbd37b94823719b0a7685d4e8714]]
    Former-commit-id: c775b2d43703b5db73f623f618e747da1e0a2048
    Former-commit-id: a5749b4fc3eaa04560a70da6a529bb81dfd255f6

--- connect/connect-vertica-sink/vertica-sink-repro-dv.sh
+++ connect/connect-vertica-sink/vertica-sink-repro-dv.sh
@@ -40,21 +40,21 @@ fi
 if [ ! -f ${DIR}/producer/target/producer-1.0.0-jar-with-dependencies.jar ]
 then
      log "Building jar for producer"
-     docker run -it --rm -e TAG=$TAG_BASE -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/producer":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/producer/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-8 mvn package
+     docker run -it --rm -e TAG=$TAG_BASE -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/producer":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/producer/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
 fi
 
 if [ ! -f ${DIR}/KeyToValue/target/KeyToValue-1.0.0-SNAPSHOT.jar ]
 then
      # build KeyToValue transform
      log "Build KeyToValue transform"
-     docker run -it --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/KeyToValue":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/KeyToValue/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-8 mvn package
+     docker run -it --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/KeyToValue":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/KeyToValue/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
 fi
 
 if [ ! -f ${DIR}/TombstoneToNull/target/TombstoneToNull-1.0.0-SNAPSHOT.jar ]
 then
      # build TombstoneToNull transform
      log "Build TombstoneToNull transform"
-     docker run -it --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/TombstoneToNull":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/TombstoneToNull/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-8 mvn package
+     docker run -it --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/TombstoneToNull":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/TombstoneToNull/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
 fi
 
 if [ ! -f ${DIR}/vertica-stream-writer/target/vertica-stream-writer-0.0.1-SNAPSHOT.jar ]
@@ -62,7 +62,7 @@ then
      log "Build vertica-stream-writer-0.0.1-SNAPSHOT.jar"
      git clone https://github.com/jcustenborder/vertica-stream-writer.git
      cp ${DIR}/QueryBuilder.java vertica-stream-writer/src/main/java/com/github/jcustenborder/vertica/QueryBuilder.java
-     docker run -it --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/vertica-stream-writer":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/vertica-stream-writer/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-8 mvn package
+     docker run -it --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/vertica-stream-writer":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/vertica-stream-writer/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
 fi
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext-dv.yml"
