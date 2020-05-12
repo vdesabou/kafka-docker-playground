1f01e340fe574500cf71fd1e924b098a8b2f9f0f
Vincent de Saboulin <vsaboulin@confluent.io>
2020-05-12 14:45:19 +0200

    adding rest proxy Sasl SSL example with security plugin
    Former-commit-id: 75efe1f02c7498a6242d55f7eeb880a285c59cd0 [formerly 7eb12dd340a6e6455e32a91791cff163f1c43b2d [formerly 297918024a96d29921165f884754aa07bd2ba2f7]]
    Former-commit-id: 5898ddbad91396fdde278f1ee493c9762528348e
    Former-commit-id: 537c7909d48cd4efdfe5104119e77782257592fe

---
+++ other/rest-proxy-security-plugin/start-sasl-ssl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+
+${DIR}/../../environment/sasl-ssl/start.sh "${PWD}/docker-compose.sasl-ssl.yml"
+
+
+
+docker exec restproxy curl -X POST --cert /etc/kafka/secrets/restproxy.certificate.pem --key /etc/kafka/secrets/restproxy.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://localhost:8086/topics/jsontest"
