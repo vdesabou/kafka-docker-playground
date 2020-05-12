1ec21c787e29f8401e6276a04d1cbf11ce62471b
Vincent de Saboulin <vsaboulin@confluent.io>
2020-05-12 17:33:18 +0200

    Use clientrestproxy principal to make it clearer
    Former-commit-id: 840d70a12c237258c579d5d9231c70feaf5b7405 [formerly c29feb994b4b329cb2f51d4356908c8c234a15fc [formerly 76f337887faacb3e97cc809cd9663862547274ad]]
    Former-commit-id: e6ec38f328a6f8f331ba38a70a543b4c30d6044c
    Former-commit-id: 4f704ffcb813497ab21366ea0b69cf097dbf46a2

--- other/rest-proxy-security-plugin/start-sasl-ssl.sh
+++ other/rest-proxy-security-plugin/start-sasl-ssl.sh
@@ -7,5 +7,8 @@ source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../environment/sasl-ssl/start.sh "${PWD}/docker-compose.sasl-ssl.yml"
 
-log "HTTP client using restproxy principal"
-docker exec restproxy curl -X POST --cert /etc/kafka/secrets/restproxy.certificate.pem --key /etc/kafka/secrets/restproxy.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://localhost:8086/topics/jsontest"
+log "HTTP client using clientrestproxy principal"
+docker exec restproxy curl -X POST --cert /etc/kafka/secrets/clientrestproxy.certificate.pem --key /etc/kafka/secrets/clientrestproxy.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://localhost:8086/topics/jsontest"
+
+log "Verify principal clientrestproxy is used"
+docker container logs broker | grep Write | grep jsontest
