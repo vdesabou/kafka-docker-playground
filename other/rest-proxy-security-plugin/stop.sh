297918024a96d29921165f884754aa07bd2ba2f7
Vincent de Saboulin <vsaboulin@confluent.io>
2020-05-12 14:45:19 +0200

    adding rest proxy Sasl SSL example with security plugin

---
+++ other/rest-proxy-security-plugin/stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/sasl-ssl/stop.sh "${PWD}/docker-compose.sasl-ssl.yml"
+${DIR}/../../environment/2way-ssl/stop.sh "${PWD}/docker-compose.2way-ssl.yml"
