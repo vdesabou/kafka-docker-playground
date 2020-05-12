76f337887faacb3e97cc809cd9663862547274ad
Vincent de Saboulin <vsaboulin@confluent.io>
2020-05-12 17:33:18 +0200

    Use clientrestproxy principal to make it clearer

--- environment/sasl-ssl/security/certs-verify.sh
+++ environment/sasl-ssl/security/certs-verify.sh
@@ -5,7 +5,7 @@ set -o nounset \
     -o verbose
 
 # See what is in each keystore and truststore
-for i in broker client schema-registry restproxy connect control-center
+for i in broker broker2 client schema-registry restproxy connect control-center clientrestproxy
 do
         log "------------------------------- $i keystore -------------------------------"
 	keytool -list -v -keystore kafka.$i.keystore.jks -storepass confluent | grep -e Alias -e Entry
