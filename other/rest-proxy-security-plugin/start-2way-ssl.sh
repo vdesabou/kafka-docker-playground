b341bc108d2e3342cd89f844bd8b5f8ba9410bfd
Vincent de Saboulin <vsaboulin@confluent.io>
2020-05-13 08:05:50 +0200

    2way ssl wip
    Former-commit-id: d14638635ab2911e582a018b9c9057241ec52f1f [formerly 4e91d9653a77bfa6a1ff3ad672127ea973a5079d [formerly 2e29cb6699bfdafafa4a6b38d8e29ec70c034250]]
    Former-commit-id: c7468191fee1ceff9c0417eee15c08efdb206823
    Former-commit-id: 0cd7cdd1b6bbf06515cbe575803c7513fa7a3c64

---
+++ other/rest-proxy-security-plugin/start-2way-ssl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+
+${DIR}/../../environment/2way-ssl/start.sh "${PWD}/docker-compose.2way-ssl.yml"
+
+
+log "HTTP client using clientrestproxy principal"
+docker exec restproxy curl -X POST --cert /etc/kafka/secrets/clientrestproxy.certificate.pem --key /etc/kafka/secrets/clientrestproxy.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://localhost:8086/topics/jsontest"
+
+log "Verify principal clientrestproxy is used"
+docker container logs broker | grep Write | grep jsontest
