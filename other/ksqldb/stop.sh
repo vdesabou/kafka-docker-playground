750b98012e7d31e358aca39ac5fb7eb7f89a48d1
Vincent de Saboulin <vsaboulin@confluent.io>
2020-06-12 17:54:31 +0200

    Environment to test ksqlDB
    Former-commit-id: fff4e6046570020fb0ad44bc919847230dd5bd8c [formerly c2431fc31001023653b208cdf6dcbbccae259c39 [formerly 8052b1ed10fe33402562185c808d72e0409b3c02]]
    Former-commit-id: 63f3f8aae76894688ade6debd6b34856a8f58cab
    Former-commit-id: 58a8920772f705d1f51d16ec3b50d0c29a25a1b6

---
+++ other/ksqldb/stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/stop.sh "${PWD}/docker-compose.yml"
