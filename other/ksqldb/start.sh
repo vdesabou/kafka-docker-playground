bf7a43fbfee9f0d0053f158029581b959fe20f10
Vincent de Saboulin <vsaboulin@confluent.io>
2020-06-12 17:54:31 +0200

    Environment to test ksqlDB

---
+++ other/ksqldb/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.yml"
