b5d18e2bf8696cfd8a975c6cd3bc405c205cc0f2
Vincent de Saboulin <vsaboulin@confluent.io>
2020-06-15 16:55:43 +0200

    Added validation
    https://github.com/vdesabou/kafka-docker-playground/issues/32

--- connect/connect-splunk-sink/splunk-sink.sh
+++ connect/connect-splunk-sink/splunk-sink.sh
@@ -47,4 +47,8 @@ log "Sleeping 60 seconds"
 sleep 60
 
 log "Verify data is in splunk"
-docker exec splunk bash -c 'sudo /opt/splunk/bin/splunk search "source=\"http:splunk_hec_token\"" -auth "admin:password"'
+output=$(docker exec splunk bash -c 'sudo /opt/splunk/bin/splunk search "source=\"http:splunk_hec_token\"" -auth "admin:password"')
+if [[ ! $output == "This is a test with Splunk"* ]]; then
+  logerror "Fail: something is wrong !"
+  exit 1
+fi
