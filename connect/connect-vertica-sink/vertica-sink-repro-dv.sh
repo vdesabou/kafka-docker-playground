89b465a6c80b17ec5bb35ff3aee858bf69d34e5f
Vincent de Saboulin <vsaboulin@confluent.io>
2020-06-18 17:28:19 +0200

    updated for 1.2.0
    Former-commit-id: 4a1a2c9469faf50c1aab8162a5a9612b9bf5337e [formerly 0a75f72cd1af5a2d2d4d7b396d847544450e293a [formerly f32d5e5a19dc6147d45b14ed62e312a2b0fd0bbe]]
    Former-commit-id: ec5b748e32a7008b1b93d6a0df65a98d5d0644e5
    Former-commit-id: 28a6b6e3483385a18f122ed25e7176a34589004f

--- connect/connect-vertica-sink/vertica-sink-repro-dv.sh
+++ connect/connect-vertica-sink/vertica-sink-repro-dv.sh
@@ -15,87 +15,16 @@ then
      rm -f ${DIR}/vertica-client-9.3.1-0.x86_64.tar.gz
 fi
 
-# if [ ! -f ${DIR}/vertica-jdbc.jar ]
-# then
-#      # install deps
-#      log "Getting vertica-jdbc.jar from vertica-client-9.2.1-0.x86_64.tar.gz"
-#      wget https://www.vertica.com/client_drivers/9.2.x/9.2.1-0/vertica-client-9.2.1-0.x86_64.tar.gz
-#      tar xvfz ${DIR}/vertica-client-9.2.1-0.x86_64.tar.gz
-#      cp ${DIR}/opt/vertica/java/lib/vertica-jdbc.jar ${DIR}/
-#      rm -rf ${DIR}/opt
-#      rm -f ${DIR}/vertica-client-9.2.1-0.x86_64.tar.gz
-# fi
-
-# if [ ! -f ${DIR}/vertica-jdbc.jar ]
-# then
-#      # install deps
-#      log "Getting vertica-jdbc.jar from vertica-client-7.2.3-0.x86_64.tar.gz"
-#      wget https://www.vertica.com/client_drivers/7.2.x/7.2.3-0/vertica-client-7.2.3-0.x86_64.tar.gz
-#      tar xvfz ${DIR}/vertica-client-7.2.3-0.x86_64.tar.gz
-#      cp ${DIR}/opt/vertica/java/lib/vertica-jdbc.jar ${DIR}/
-#      rm -rf ${DIR}/opt
-#      rm -f ${DIR}/vertica-client-7.2.3-0.x86_64.tar.gz
-# fi
-
 if [ ! -f ${DIR}/producer/target/producer-1.0.0-jar-with-dependencies.jar ]
 then
      log "Building jar for producer"
      docker run -it --rm -e TAG=$TAG_BASE -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/producer":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/producer/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
 fi
 
-if [ ! -f ${DIR}/KeyToValue/target/KeyToValue-1.0.0-SNAPSHOT.jar ]
-then
-     # build KeyToValue transform
-     log "Build KeyToValue transform"
-     docker run -it --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/KeyToValue":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/KeyToValue/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
-fi
 
-if [ ! -f ${DIR}/TombstoneToNull/target/TombstoneToNull-1.0.0-SNAPSHOT.jar ]
-then
-     # build TombstoneToNull transform
-     log "Build TombstoneToNull transform"
-     docker run -it --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/TombstoneToNull":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/TombstoneToNull/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
-fi
-
-if [ ! -f ${DIR}/vertica-stream-writer/target/vertica-stream-writer-0.0.1-SNAPSHOT.jar ]
-then
-     log "Build vertica-stream-writer-0.0.1-SNAPSHOT.jar"
-     git clone https://github.com/jcustenborder/vertica-stream-writer.git
-     cp ${DIR}/QueryBuilder.java vertica-stream-writer/src/main/java/com/github/jcustenborder/vertica/QueryBuilder.java
-     docker run -it --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/vertica-stream-writer":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/vertica-stream-writer/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
-fi
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext-dv.yml"
 
-log "Create the table customer1"
-docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
-CREATE TABLE public.customer1
-(
-    dwhCreationDate timestamp DEFAULT (statement_timestamp())::timestamp,
-    kafkaId int,
-    ListID int,
-    NormalizedHashItemID int,
-    KafkaKeyIsDeleted boolean DEFAULT true,
-    MyFloatValue float,
-    MyTimestamp timestamp
-);
-EOF
-
-log "Create the table customer2"
-docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
-CREATE TABLE public.customer2
-(
-    dwhCreationDate timestamp DEFAULT (statement_timestamp())::timestamp,
-    kafkaId int,
-    ListID int,
-    NormalizedHashItemID int,
-    KafkaKeyIsDeleted boolean DEFAULT true,
-    MyFloatValue float,
-    MyTimestamp timestamp
-);
-EOF
-
-sleep 2
 
 log "Sending messages to topic customer (done using JAVA producer)"
 
@@ -113,37 +42,31 @@ docker exec connect \
                     "vertica.port": "5433",
                     "vertica.username": "dbadmin",
                     "vertica.password": "",
-                    "vertica.load.method": "DIRECT",
+                    "config.action.reload": "restart",
                     "errors.log.enable": "true",
                     "errors.log.include.messages": "true",
                     "topics": "customer",
-                    "transforms": "TombstoneToNull, insert_dwhCreateDate, KeyToValue, cast_kafkaId_toInt, mapMyTableFieldToTopic,MyTimestamp_convert",
-                    "transforms.TombstoneToNull.type": "com.github.vdesabou.kafka.connect.transforms.TombstoneToNull",
-                    "transforms.insert_dwhCreateDate.type": "org.apache.kafka.connect.transforms.InsertField$Value",
-                    "transforms.insert_dwhCreateDate.timestamp.field": "dwhCreationDate",
-                    "transforms.KeyToValue.type": "com.github.vdesabou.kafka.connect.transforms.KeyToValue",
-                    "transforms.KeyToValue.key.field.name":"kafkaId",
-                    "transforms.cast_kafkaId_toInt.type": "org.apache.kafka.connect.transforms.Cast$Value",
-                    "transforms.cast_kafkaId_toInt.spec": "kafkaId:int64",
-                    "transforms.mapMyTableFieldToTopic.type": "io.confluent.connect.transforms.ExtractTopic$Value",
-                    "transforms.mapMyTableFieldToTopic.field": "MyTable",
-                    "transforms.MyTimestamp_convert.type": "org.apache.kafka.connect.transforms.TimestampConverter$Value",
-                    "transforms.MyTimestamp_convert.field": "MyTimestamp",
-                    "transforms.MyTimestamp_convert.target.type": "Timestamp",
+                    "rejected.record.logging.mode": "table",
+                    "rejected.record.table.schema": "public",
+                    "table.name.format": "customer1",
+                    "pk.mode": "record_key",
+                    "pk.fields": "ID",
+                    "auto.create": true,
+                    "auto.evolve": false,
+                    "delete.enabled": true,
                     "key.converter": "org.apache.kafka.connect.storage.StringConverter",
                     "value.converter" : "io.confluent.connect.avro.AvroConverter",
                     "value.converter.schema.registry.url":"http://schema-registry:8081",
-                    "vertica.load.method": "DIRECT",
                     "confluent.topic.bootstrap.servers": "broker:9092",
-                    "confluent.topic.replication.factor": "1",
-                    "consumer.override.max.poll.records": "10000",
-                    "consumer.override.fetch.max.wait.ms": "30000",
-                    "consumer.override.fetch.min.bytes": "10000000",
-                    "consumer.override.fetch.max.bytes": "100000000",
-                    "consumer.override.request.timeout.ms": "60000"
+                    "confluent.topic.replication.factor": "1"
           }' \
      http://localhost:8083/connectors/vertica-sink/config | jq .
 
+                    # "consumer.override.max.poll.records": "10000",
+                    # "consumer.override.fetch.max.wait.ms": "30000",
+                    # "consumer.override.fetch.min.bytes": "10000000",
+                    # "consumer.override.fetch.max.bytes": "100000000",
+                    # "consumer.override.request.timeout.ms": "60000"
 sleep 10
 
 log "Check data is in Vertica for customer1"
@@ -151,19 +74,9 @@ docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
 select * from public.customer1;
 EOF
 
-log "Check data is in Vertica for customer2"
-docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
-select * from public.customer2;
-EOF
-
 log "Check for rejected data for customer1"
 docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
-select * from public.customer1_rej;
-EOF
-
-log "Check for rejected data for customer2"
-docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
-select * from public.customer2_rej;
+select * from public.customer1_rejected;
 EOF
 
 # docker exec -i vertica /opt/vertica/bin/vsql -hlocalhost -Udbadmin << EOF
