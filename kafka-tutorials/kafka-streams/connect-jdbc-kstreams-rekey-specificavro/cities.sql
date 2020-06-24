9ad57f75010dbbc6aac0a499d4a52cb8198c71ae
Vincent de Saboulin <vsaboulin@confluent.io>
2020-06-24 09:52:25 +0200

    Removing kafka-tutorials as local tests can be run with harness runner
    More details will be added soon to  https://github.com/confluentinc/kafka-tutorials
    
    
    Former-commit-id: ea725a27831d279637790dac3e673d03e8a5a4a5 [formerly d3b6b0916845962792b9149712353f5501149739 [formerly 8e0182a4eeb27628da276ac5161db48c0a34dc8e]]
    Former-commit-id: 389c6e5acea45c04d66caee9b313246618f98175
    Former-commit-id: df6e3662842f180d05a3d3a0b7881258cebc72f9

--- kafka-tutorials/kafka-streams/connect-jdbc-kstreams-rekey-specificavro/cities.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS cities;
-CREATE TABLE cities (city_id INTEGER KEY NOT NULL, name VARCHAR(255), state VARCHAR(255));
-INSERT INTO cities (city_id, name, state) VALUES (1, 'Raleigh', 'NC');
-INSERT INTO cities (city_id, name, state) VALUES (2, 'Mountain View', 'CA');
-INSERT INTO cities (city_id, name, state) VALUES (3, 'Knoxville', 'TN');
-INSERT INTO cities (city_id, name, state) VALUES (4, 'Houston', 'TX');
-INSERT INTO cities (city_id, name, state) VALUES (5, 'Olympia', 'WA');
-INSERT INTO cities (city_id, name, state) VALUES (6, 'Bismarck', 'ND');
