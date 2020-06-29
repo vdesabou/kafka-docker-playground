c3ed575971e7c4d3a47568f7c5e3408749b721c5
Vincent de Saboulin <vsaboulin@confluent.io>
2020-06-29 14:52:45 +0200

    Update to 5.5.1

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -23,7 +23,7 @@ function version_gt() {
 
 function set_kafka_client_tag()
 {
-    if [ "$TAG_BASE" = "5.5.0" ]
+    if [ "$TAG_BASE" = "5.5.1" ] || [ "$TAG_BASE" = "5.5.0" ]
     then
       export KAFKA_CLIENT_TAG="2.5.0"
     fi
@@ -84,7 +84,7 @@ function set_kafka_client_tag()
 if [ -z "$TAG" ]
 then
     # TAG is not set, use defaults:
-    export TAG=5.5.0
+    export TAG=5.5.1
     # to handle ubi8 images
     export TAG_BASE=$TAG
     if [ -z "$CP_KAFKA_IMAGE" ]
