e4a658461a2d3719e4373ecbc5fd0065e3f3663d
Vincent de Saboulin <vsaboulin@confluent.io>
2020-07-22 16:50:04 +0200

    Added RabbitMQ sink example
    Former-commit-id: 74e7cd98e3be61ea13f633083e324fcc65ad99d1 [formerly c52b2aba84967e098b46cfa02a0d28ee991a446c [formerly af5093e631eb5c4a448569ff8722bbd4d03fe1f1]]
    Former-commit-id: 2bb707582af524a2068865b8aa375e8d66496c33
    Former-commit-id: adee14b5337d99b7ce149599280eb588fd6d0af9

---
+++ connect/connect-rabbitmq-sink/rabbitmq.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+
+log "Create RabbitMQ exchange, queue and binding"
+docker exec -it rabbitmq rabbitmqadmin -u myuser -p mypassword -V / declare exchange name=exchange1 type=direct
+docker exec -it rabbitmq rabbitmqadmin -u myuser -p mypassword -V / declare queue name=queue1 durable=true
+docker exec -it rabbitmq rabbitmqadmin -u myuser -p mypassword -V / declare binding source=exchange1 destination=queue1 routing_key=rkey1
+
+
+log "Sending messages to topic rabbitmq-messages"
+seq 10 | docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic rabbitmq-messages
+
+log "Creating RabbitMQ Source connector"
+docker exec connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class" : "io.confluent.connect.rabbitmq.sink.RabbitMQSinkConnector",
+               "tasks.max" : "1",
+               "topics": "rabbitmq-messages",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "org.apache.kafka.connect.converters.ByteArrayConverter",
+               "rabbitmq.queue" : "myqueue",
+               "rabbitmq.host" : "rabbitmq",
+               "rabbitmq.username" : "myuser",
+               "rabbitmq.password" : "mypassword",
+               "rabbitmq.exchange": "exchange1",
+               "rabbitmq.routing.key": "rkey1",
+               "rabbitmq.delivery.mode": "PERSISTENT",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/rabbitmq-sink/config | jq .
+
+
+sleep 5
+
+log "Check messages received in RabbitMQ"
+docker exec -it rabbitmq rabbitmqadmin -u myuser -p mypassword get queue=queue1 count=10
