62424a35690525c1b94bc8ac6a0b7e4e9d8dac9a
Vincent de Saboulin <vsaboulin@confluent.io>
2020-07-24 15:04:15 +0200

    Added Salesforce Bulk API source
    Former-commit-id: 71f9dbc9ad4e84b37b8008b1aae62701cade8790 [formerly fb20f2aa7ac7a246fcbb9883662e3eabe25f6757 [formerly 2d07ee35386f2e081a9f07a4628ebb5fd2ea4931]]
    Former-commit-id: 0b874b2e80c3b25df7830c21beb0fb039b116e17
    Former-commit-id: e61c7e99bc9ec53e212b09463df0d214a9cf607d

---
+++ connect/connect-salesforce-bulkapi-source/salesforce-bulkapi-source.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+SALESFORCE_USERNAME=${SALESFORCE_USERNAME:-$1}
+SALESFORCE_PASSWORD=${SALESFORCE_PASSWORD:-$2}
+CONSUMER_KEY=${CONSUMER_KEY:-$3}
+CONSUMER_PASSWORD=${CONSUMER_PASSWORD:-$4}
+SECURITY_TOKEN=${SECURITY_TOKEN:-$5}
+
+if [ -z "$SALESFORCE_USERNAME" ]
+then
+     logerror "SALESFORCE_USERNAME is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_PASSWORD" ]
+then
+     logerror "SALESFORCE_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+
+if [ -z "$CONSUMER_KEY" ]
+then
+     logerror "CONSUMER_KEY is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$CONSUMER_PASSWORD" ]
+then
+     logerror "CONSUMER_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SECURITY_TOKEN" ]
+then
+     logerror "SECURITY_TOKEN is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Creating Salesforce Bulk API Source connector"
+docker exec -e SALESFORCE_USERNAME="$SALESFORCE_USERNAME" -e SALESFORCE_PASSWORD="$SALESFORCE_PASSWORD" -e CONSUMER_KEY="$CONSUMER_KEY" -e CONSUMER_PASSWORD="$CONSUMER_PASSWORD" -e SECURITY_TOKEN="$SECURITY_TOKEN" connect \
+     curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+                    "connector.class": "io.confluent.connect.salesforce.SalesforceBulkApiSourceConnector",
+                    "kafka.topic": "sfdc-bulkapi-leads",
+                    "tasks.max": "1",
+                    "curl.logging": "true",
+                    "salesforce.object" : "Lead",
+                    "salesforce.username" : "'"$SALESFORCE_USERNAME"'",
+                    "salesforce.password" : "'"$SALESFORCE_PASSWORD"'",
+                    "salesforce.password.token" : "'"$SECURITY_TOKEN"'",
+                    "salesforce.consumer.key" : "'"$CONSUMER_KEY"'",
+                    "salesforce.consumer.secret" : "'"$CONSUMER_PASSWORD"'",
+                    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/salesforce-bulkapi-source/config | jq .
+
+
+
+sleep 10
+
+log "Verify we have received the data in sfdc-bulkapi-leads topic"
+timeout 60 docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic sfdc-bulkapi-leads --from-beginning --max-messages 1
