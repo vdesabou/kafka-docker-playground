7103f95239af1a1fad6b77c8f41f82b80e19225e
Vincent de Saboulin <vsaboulin@confluent.io>
2020-10-07 08:59:30 +0200

    Cleanup Azure resource groups before starting Travis tests #123
    Former-commit-id: e676d48052c05876ffac16174fd2f926ef26761c [formerly 242564a1d2cb754de17deeab1f97b7cc73178507 [formerly 66cae3bda3893e61038420bf4fc949331cc79216]]
    Former-commit-id: 36ca83fe20c971bf96d306297378a6614ac04bed
    Former-commit-id: 4cb357d041d6b69d9bd35754591f0d1485818626

---
+++ scripts/cleanup-resources.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+function log() {
+  YELLOW='\033[0;33m'
+  NC='\033[0m' # No Color
+  echo -e "$YELLOW$@$NC"
+}
+
+function logerror() {
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+  echo -e "$RED$@$NC"
+}
+
+function logwarn() {
+  PURPLE='\033[0;35m'
+  NC='\033[0m' # No Color
+  echo -e "$PURPLE$@$NC"
+}
+
+log "Cleanup Azure Resource groups"
+for group in $(az group list --query [].name --output tsv)
+do
+  if [[ $group = playgroundtravis* ]]
+  then
+    log "Deleting resource group $group"
+    az group delete --name $group --yes
+  fi
+done
