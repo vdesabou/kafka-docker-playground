fc62340cd659a899607fa05218c721e4aeb075f9
Vincent de Saboulin <vsaboulin@confluent.io>
2020-10-12 15:51:47 +0200

    Add Google Cloud BigTable Sink Connector example #133
    Former-commit-id: 113125f6458d5305a9762e7398345125dfb61314 [formerly 92c13699c9570921d0a98506e45a6dce94fce790 [formerly 2170c99aaf522ec625b9ed6ca0620e60c935b3bb]]
    Former-commit-id: 424c872ac5a39eb030a01f52d55f868dd003d7e8
    Former-commit-id: 13ce0d3344fca9062bc69037982638047cace6fe

---
+++ connect/connect-gcp-bigtable-sink/gcp-bigtable.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+PROJECT=${1:-vincent-de-saboulin-lab}
+INSTANCE=${2:-test-instance}
+
+KEYFILE="${DIR}/keyfile.json"
+if [ ! -f ${KEYFILE} ]
+then
+     logerror "ERROR: the file ${KEYFILE} file is not present!"
+     exit 1
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Doing gsutil authentication"
+set +e
+docker rm -f gcloud-config
+set -e
+docker run -ti -v ${KEYFILE}:/tmp/keyfile.json --name gcloud-config google/cloud-sdk:latest gcloud auth activate-service-account --key-file /tmp/keyfile.json
+
+set +e
+log "Deleting instance, if required"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud bigtable instances delete $INSTANCE --project $PROJECT  << EOF
+Y
+EOF
+set -e
+log "Create a BigTable Instance and Database"
+docker run -ti --volumes-from gcloud-config google/cloud-sdk:latest gcloud bigtable instances create $INSTANCE --project $PROJECT --cluster $INSTANCE --cluster-zone=us-east1-c --display-name="playground-bigtable-instance" --instance-type=DEVELOPMENT
+
+log "Sending messages to topic stats"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic stats --property parse.key=true --property key.separator=, --property key.schema='{"type" : "string", "name" : "id"}' --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"users","type":{"name":"columnfamily","type":"record","fields":[{"name": "name", "type": "string"},{"name": "friends", "type": "string"}]}}]}' << EOF
+"simple-key-1", {"users": {"name":"Bob","friends": "1000"}}
+"simple-key-2", {"users": {"name":"Jess","friends": "10000"}}
+"simple-key-3", {"users": {"name":"John","friends": "10000"}}
+EOF
+
+
+log "Creating GCP BigTbale Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.gcp.bigtable.BigtableSinkConnector",
+               "tasks.max" : "1",
+               "topics" : "stats",
+               "auto.create" : "true",
+               "gcp.bigtable.credentials.path": "/root/keyfiles/keyfile.json",
+               "gcp.bigtable.instance.id": "'"$INSTANCE"'",
+               "gcp.bigtable.project.id": "'"$PROJECT"'",
+               "auto.create.tables": "true",
+               "auto.create.column.families": "true",
+               "table.name.format" : "kafka_${topic}",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/gcp-bigtable-sink/config | jq .
+
+sleep 30
+
+log "Verify data is in GCP BigTable"
+docker run -ti --volumes-from gcloud-config google/cloud-sdk:latest cbt -project $PROJECT -instance $INSTANCE read kafka_stats
+
+log "Delete table"
+docker run -ti --volumes-from gcloud-config google/cloud-sdk:latest cbt -project $PROJECT -instance $INSTANCE deletetable kafka_stats
+
+log "Deleting instance"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud bigtable instances delete $INSTANCE --project $PROJECT  << EOF
+Y
+EOF
+
+docker rm -f gcloud-config
