81b8f4d34f60d143f6c61f45a2f55ff92ca6e24d
Vincent de Saboulin <vsaboulin@confluent.io>
2020-10-13 11:05:01 +0200

    wip

--- connect/connect-azure-functions-sink/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    context.res = {
-        status: 200,
-        body: req.body
-    };
-};
