24dce45edf09c5f7622a86e9035184b0c6255e24
Vincent de Saboulin <vsaboulin@confluent.io>
2020-10-22 23:01:13 +0200

    pass env

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -401,7 +401,7 @@ function aws() {
       fi
     fi
 
-    docker run --rm -iv $HOME/.aws:/root/.aws -v $(pwd):/aws -v /tmp:/tmp mikesir87/aws-cli:v1 aws "$@"
+    docker run --rm -iv $HOME/.aws:/root/.aws -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -v $(pwd):/aws -v /tmp:/tmp mikesir87/aws-cli:v1 aws "$@"
 }
 
 function timeout() {
