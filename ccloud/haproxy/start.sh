18e75cb950499a55373ab6bce2bd93dab7ba534d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-11-06 10:49:11 +0100

    Add HAProxy example for Confluent Cloud Networking #146
    Former-commit-id: 95a7e1028a453c76eff94aa6833ae0ac8140a94b [formerly 474789ff30d3685ea3b3bff1e3e9237b229cd7a1 [formerly 7f081942e4da089c406b1a9748b41b59a80d05b4]]
    Former-commit-id: 4f12e44f8fb131c80dab951c5c446492eb048009
    Former-commit-id: 2de95860c085c3890c0e9d20a99a76b0ba5da72f

---
+++ ccloud/haproxy/start.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+function generate_haproxy_config() {
+    TMP_FILE=${DIR}/haproxy/haproxy_tmp.cfg
+    rm -f ${TMP_FILE}
+
+    echo " " >> $TMP_FILE
+    echo "    # define acl depending certificate name" >> $TMP_FILE
+    echo "    acl is_bootstrap req.ssl_sni -i $BOOTSTRAP_SERVER" >> $TMP_FILE
+    for (( i=0; i<$nb_broker; i++ ))
+    do
+        echo "    acl is_kafka$i req.ssl_sni -i b$i-$BOOTSTRAP_SERVER" >> $TMP_FILE
+    done
+
+    echo " " >> $TMP_FILE
+
+    echo "    # depending name rule to route to specified backend" >> $TMP_FILE
+    echo "    use_backend bootstrap if is_bootstrap" >> $TMP_FILE
+    for (( i=0; i<$nb_broker; i++ ))
+    do
+        echo "    use_backend kafka$i if is_kafka$i" >> $TMP_FILE
+    done
+
+    echo " " >> $TMP_FILE
+    echo "# backend definitions" >> $TMP_FILE
+    echo "backend bootstrap" >> $TMP_FILE
+    echo "    mode tcp" >> $TMP_FILE
+    echo "    server bootstrap $BOOTSTRAP_SERVERS check" >> $TMP_FILE
+    for (( i=0; i<$nb_broker; i++ ))
+    do
+        echo "backend kafka$i" >> $TMP_FILE
+        echo "    mode tcp" >> $TMP_FILE
+        echo "    server kafka$i b$i-$BOOTSTRAP_SERVERS check" >> $TMP_FILE
+    done
+    echo " " >> $TMP_FILE
+
+    rm -f ${DIR}/haproxy/haproxy.cfg
+    cat ${DIR}/haproxy/haproxy-template.cfg $TMP_FILE >> ${DIR}/haproxy/haproxy.cfg
+}
+
+BOOTSTRAP_SERVER=${BOOTSTRAP_SERVER:-$1}
+CLOUD_KEY=${CLOUD_KEY:-$2}
+CLOUD_SECRET=${CLOUD_SECRET:-$3}
+
+if [ -z "$BOOTSTRAP_SERVER" ]
+then
+     logerror "BOOTSTRAP_SERVER is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [[ "$BOOTSTRAP_SERVER" = *2 ]]
+then
+    logerror "BOOTSTRAP_SERVER is the pkc endpoint without ':9092'"
+    exit 1
+fi
+
+if [ -z "$CLOUD_KEY" ]
+then
+     logerror "CLOUD_KEY is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$CLOUD_SECRET" ]
+then
+     logerror "CLOUD_SECRET is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVER:9092"
+
+log "Checking with Kafkacat before using HAProxy"
+docker run confluentinc/cp-kafkacat:${TAG} kafkacat -b $BOOTSTRAP_SERVERS -L -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN -X sasl.username=$CLOUD_KEY -X sasl.password=$CLOUD_SECRET | grep "broker"
+
+nb_broker=$(docker run confluentinc/cp-kafkacat:${TAG} kafkacat -b $BOOTSTRAP_SERVERS -L -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN -X sasl.username=$CLOUD_KEY -X sasl.password=$CLOUD_SECRET | grep "pkc" | grep " at " | wc -l)
+if [ $nb_broker -eq 0 ]
+then
+    logerror "ERROR: No broker could be discovered using Kafkacat"
+    exit 1
+fi
+
+log "Generating haproxy file based on Kafkacat results"
+generate_haproxy_config
+
+docker-compose -f "${PWD}/docker-compose.yml" down
+docker-compose -f "${PWD}/docker-compose.yml" build
+docker-compose -f "${PWD}/docker-compose.yml" up -d
+
+log "Using iptables on client to block $BOOTSTRAP_SERVER and all broker IPs"
+for ip in $(docker exec -i -e BOOTSTRAP_SERVER=$BOOTSTRAP_SERVER client bash -c 'nslookup $BOOTSTRAP_SERVER | grep Address | grep -v "#" | cut -d " " -f 2')
+do
+    log "Blocking IP address $ip corresponding to bootstrap server $BOOTSTRAP_SERVER"
+    docker exec -i --privileged --user root -e ip=$ip client bash -c 'iptables -I INPUT -p tcp -s $ip -j DROP'
+done
+for (( i=0; i<$nb_broker; i++ ))
+do
+    BROKER="b$i-$BOOTSTRAP_SERVER"
+    ip=$(docker exec -i -e BROKER=$BROKER client bash -c 'nslookup $BROKER | grep Address | grep -v "#" | cut -d " " -f 2')
+    log "Blocking IP address $ip corresponding to broker $BROKER"
+    docker exec -i --privileged --user root -e ip=$ip client bash -c 'iptables -I INPUT -p tcp -s $ip -j DROP'
+done
+
+log "Verify it is no more working, as expected"
+set +e
+docker exec -i -e BOOTSTRAP_SERVERS=$BOOTSTRAP_SERVERS -e CLOUD_KEY=$CLOUD_KEY -e CLOUD_SECRET=$CLOUD_SECRET client bash -c 'kafkacat -b $BOOTSTRAP_SERVERS -L -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN -X sasl.username=$CLOUD_KEY -X sasl.password=$CLOUD_SECRET | grep "broker"'
+set -e
+
+log "Modifying /etc/hosts on client container, so that haproxy is used"
+HAPROXY_IP=$(container_to_ip haproxy)
+docker exec -i --privileged --user root -e BOOTSTRAP_SERVER=$BOOTSTRAP_SERVER -e HAPROXY_IP=$HAPROXY_IP client bash -c 'echo "$HAPROXY_IP $BOOTSTRAP_SERVER" >> /etc/hosts'
+for (( i=0; i<$nb_broker; i++ ))
+do
+    docker exec -i --privileged --user root -e BOOTSTRAP_SERVER=$BOOTSTRAP_SERVER -e HAPROXY_IP=$HAPROXY_IP -e i=$i client bash -c 'echo "$HAPROXY_IP b$i-$BOOTSTRAP_SERVER" >> /etc/hosts'
+done
+docker exec -i client bash -c 'cat /etc/hosts'
+
+log "Verifying we can connect to $BOOTSTRAP_SERVER using HAProxy and netcat"
+docker exec -i -e BOOTSTRAP_SERVER=$BOOTSTRAP_SERVER client bash -c 'nc -zv $BOOTSTRAP_SERVER 9092'
+
+log "Verifying we can connect to $BOOTSTRAP_SERVERS using openssl"
+docker exec -i -e BOOTSTRAP_SERVERS=$BOOTSTRAP_SERVERS client bash -c 'echo QUIT | openssl s_client -connect $BOOTSTRAP_SERVERS'
+
+log "Verifying we can use Kafkacat using HAProxy"
+docker exec -i -e BOOTSTRAP_SERVERS=$BOOTSTRAP_SERVERS -e CLOUD_KEY=$CLOUD_KEY -e CLOUD_SECRET=$CLOUD_SECRET client bash -c 'kafkacat -b $BOOTSTRAP_SERVERS -L -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN -X sasl.username=$CLOUD_KEY -X sasl.password=$CLOUD_SECRET | grep "broker"'
