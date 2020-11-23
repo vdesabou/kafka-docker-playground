b57cbeba02b8363dd4142fd4774cb03b6cd119ae
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-11-23 11:07:16 +0100

    fix name

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,4 +2,4 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_ENCRYPTION_PASSWORD" --output secrets.tgz secrets.tar.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_ENCRYPTION_PASSWORD" --output secrets.tar secrets.tar.gpg
