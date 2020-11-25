624364b4105497032493605bead60c5a881ea2a2
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-11-25 15:34:23 +0100

    added
    Former-commit-id: d1c877dafe5a3c2c6601a8d32bc0f3d9677b7b72 [formerly 53fe58fc81ff41109781e406cd0879c5e64e39b7 [formerly dda2e83e4b65e526d1ff0e56eda55d35f3169d4b]]
    Former-commit-id: a5c16ad75d1e064b0e2b586f8d55c84f8da1915e
    Former-commit-id: 10398084ee6ecaebc290263ccf3d308981e05d6f

---
+++ connect/connect-jdbc-sqlserver-sink/sqlserver-repro-json-schema-ksqldb.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../ksqldb/environment/start.sh  "${PWD}/docker-compose.plaintext-jtds.yml"
+
+log "Sending AVRO messages to topic orders"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic orders --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"id","type":"int"},{"name":"product", "type": "string"}, {"name":"quantity", "type": "int"}, {"name":"price",
+"type": "float"}]}' << EOF
+{"id": 999, "product": "foo", "quantity": 100, "price": 50}
+EOF
+
+
+log "Create the ksqlDB calls stream in JSON"
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE STREAM ORDERS WITH (
+    kafka_topic = 'orders',
+    VALUE_FORMAT='AVRO'
+);
+EOF
+
+
+log "Create the table"
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE TABLE orders_count
+     WITH(VALUE_FORMAT='JSON_SR', KAFKA_TOPIC='orders_count')
+     AS SELECT
+     id,
+     COUNT_DISTINCT(product) as product_count
+     FROM ORDERS
+     GROUP BY id;
+EOF
+
+# if VALUE_FORMAT='JSON', we get
+
+# [2020-11-25 14:30:50,307] ERROR WorkerSinkTask{id=sqlserver-sink-0} Task threw an uncaught and unrecoverable exception (org.apache.kafka.connect.runtime.WorkerTask)
+# org.apache.kafka.connect.errors.ConnectException: Tolerance exceeded in error handler
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndHandleError(RetryWithToleranceOperator.java:196)
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execute(RetryWithToleranceOperator.java:122)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertAndTransformRecord(WorkerSinkTask.java:495)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertMessages(WorkerSinkTask.java:472)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.poll(WorkerSinkTask.java:322)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.iteration(WorkerSinkTask.java:226)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:198)
+#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:185)
+#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:235)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:834)
+# Caused by: org.apache.kafka.connect.errors.DataException: Converting byte[] to Kafka Connect data failed due to serialization error:
+#         at io.confluent.connect.json.JsonSchemaConverter.toConnectData(JsonSchemaConverter.java:111)
+#         at org.apache.kafka.connect.storage.Converter.toConnectData(Converter.java:87)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.lambda$convertAndTransformRecord$1(WorkerSinkTask.java:495)
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndRetry(RetryWithToleranceOperator.java:146)
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndHandleError(RetryWithToleranceOperator.java:180)
+#         ... 13 more
+# Caused by: org.apache.kafka.common.errors.SerializationException: Error deserializing JSON message for id -1
+# Caused by: org.apache.kafka.common.errors.SerializationException: Unknown magic byte!
+# [2020-11-25 14:30:50,310] ERROR WorkerSinkTask{id=sqlserver-sink-0} Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask)
+# [2020-11-25 14:30:50,310] INFO Stopping task (io.confluent.connect.jdbc.sink.JdbcSinkTask)
+
+
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic orders --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"id","type":"int"},{"name":"product", "type": "string"}, {"name":"quantity", "type": "int"}, {"name":"price",
+"type": "float"}]}' << EOF
+{"id": 1000, "product": "foo2", "quantity": 1000, "price": 50}
+{"id": 1001, "product": "foo2", "quantity": 1000, "price": 50}
+{"id": 1002, "product": "foo2", "quantity": 1000, "price": 50}
+{"id": 1003, "product": "foo2", "quantity": 1000, "price": 50}
+EOF
+
+log "Creating JDBC SQL Server (with JTDS driver) sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+                    "tasks.max": "1",
+                    "connection.url": "jdbc:jtds:sqlserver://sqlserver:1433",
+                    "connection.user": "sa",
+                    "connection.password": "Password!",
+                    "value.converter": "io.confluent.connect.json.JsonSchemaConverter",
+                    "value.converter.schema.registry.url": "http://schema-registry:8081",
+                    "topics": "orders_count",
+                    "auto.create": "true"
+          }' \
+     http://localhost:8083/connectors/sqlserver-sink/config | jq .
+
+
+
+sleep 15
+
+log "Show content of ORDERS_COUNT table:"
+docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! << EOF
+select * from ORDERS_COUNT
+GO
+EOF
