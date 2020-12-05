5a96d0dc3abf82c349cd0955028101c761a9369e
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-12-05 21:37:05 +0100

    CI: connect-cassandra-sink is failing #164
    Former-commit-id: ae24288dfbe0c241e88b9e5a5a1e151302d7493e [formerly 8037beb067832a23ed97d200b4e8157e312d3f67 [formerly 13874619c8475d027acabe409fd11b9f2e94b0cc]]
    Former-commit-id: 106f4152e68936edf52b08014313c3c619a28e00
    Former-commit-id: 1e91c04a0f83d049a61144886018a5fb380a7f64

--- connect/connect-cassandra-sink/cassandra.sh
+++ connect/connect-cassandra-sink/cassandra.sh
@@ -7,6 +7,9 @@ source ${DIR}/../../scripts/utils.sh
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
 
+log "Getting value for cassandra.local.datacenter (2.0.x only), see https://docs.confluent.io/kafka-connect-cassandra/current/index.html#upgrading-to-version-2-0-x"
+DATACENTER=$(docker exec cassandra cqlsh -e 'SELECT data_center FROM system.local;' | head -4 | tail -1 | tr -d ' ')
+
 log "Sending messages to topic topic1"
 seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic topic1 --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
 
@@ -20,6 +23,7 @@ curl -X PUT \
                     "cassandra.contact.points" : "cassandra",
                     "cassandra.keyspace" : "test",
                     "cassandra.consistency.level": "ONE",
+                    "cassandra.local.datacenter":"'"$DATACENTER"'",
                     "confluent.license": "",
                     "confluent.topic.bootstrap.servers": "broker:9092",
                     "confluent.topic.replication.factor": "1",
