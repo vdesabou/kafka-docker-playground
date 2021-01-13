4777c13a213e3e498050d2f35d39b7fe9ec77ea0
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-01-13 10:15:17 +0100

    add sleep
    Former-commit-id: 834905fc8943ab24ac1a14ae32d2d40decdc3bdd [formerly cc3608cacaccb84a9155161a7889d4fcaf7a134c [formerly a6409673b3a8b5da7b94a2b4e0ccf8328b77b9a5]]
    Former-commit-id: ee83fa75548acb1833c21dd6686ee5978c1eb923
    Former-commit-id: e5ddf24fe5f0945522ee21ed354eb7af30751959

--- other/filebeat-to-kafka/start.sh
+++ other/filebeat-to-kafka/start.sh
@@ -6,8 +6,8 @@ source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml" -a
 
-log "Sleep 60 seconds"
-sleep 60
+log "Sleep 90 seconds"
+sleep 90
 
 log "Verify we have received the data in syslog topic"
 docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic topic-log --from-beginning --max-messages 100
