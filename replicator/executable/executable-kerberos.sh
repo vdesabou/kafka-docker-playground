2fba0d1dc93c4ab75e9ad5d9a7d669c291caa9be
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-01-25 10:06:14 +0100

    CI: replicator/executable are failing #191
    Former-commit-id: edfc1e7f63d2a96511747d414a59c7a3834ed7df [formerly d31c7cbd91df2a9860c6b3acd1d02ed233b99348 [formerly dd730259f1a25a4b4a299480abfd11877358077c]]
    Former-commit-id: ff7a107906851770fba0a6e3035235468d9bbe97
    Former-commit-id: 3ce78412376363b59601bfad9a903760b124b6ba

--- replicator/executable/executable-kerberos.sh
+++ replicator/executable/executable-kerberos.sh
@@ -7,10 +7,10 @@ source ${DIR}/../../scripts/utils.sh
 ${DIR}/../../environment/mdc-kerberos/start.sh "${PWD}/docker-compose.kerberos.yml"
 
 log "Sending sales in Europe cluster"
-seq -f "european_sale_%g ${RANDOM}" 10 | docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_producer && kafka-console-producer --broker-list broker-europe:9092 --topic sales_EUROPE --producer.config /etc/replicator/producer.properties'
+seq -f "european_sale_%g ${RANDOM}" 10 | docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_producer && kafka-console-producer --broker-list broker-europe:9092 --topic sales_EUROPE --producer.config /etc/kafka/producer-europe.properties'
 
 log "Sending sales in US cluster"
-seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_producer && kafka-console-producer --broker-list broker-us:9092 --topic sales_US --producer.config /etc/replicator/producer.properties'
+seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_producer && kafka-console-producer --broker-list broker-us:9092 --topic sales_US --producer.config /etc/kafka/producer-us.properties'
 
 log "Starting replicator instances"
 docker-compose -f ../../environment/mdc-plaintext/docker-compose.yml -f ../../environment/mdc-kerberos/docker-compose.kerberos.yml -f docker-compose.replicator.kerberos.yml up -d
@@ -22,7 +22,7 @@ docker container exec -i replicator-europe bash -c 'kinit -k -t /var/lib/secret/
 ../../scripts/wait-for-connect-and-controlcenter.sh replicator-europe $@
 
 log "Verify we have received the data in all the sales_ topics in EUROPE"
-timeout 60 docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_consumer && kafka-console-consumer --bootstrap-server broker-europe:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/replicator/consumer.properties'
+timeout 60 docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_consumer && kafka-console-consumer --bootstrap-server broker-europe:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/kafka/consumer-europe.properties'
 
 log "Verify we have received the data in all the sales_ topics in the US"
-timeout 60 docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_consumer && kafka-console-consumer --bootstrap-server broker-us:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/replicator/consumer.properties'
+timeout 60 docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_consumer && kafka-console-consumer --bootstrap-server broker-us:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/kafka/consumer-us.properties'
