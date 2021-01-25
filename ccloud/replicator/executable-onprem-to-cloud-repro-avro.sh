27c15fcc0467af7586ee59a4a6e8a5dec5dbe8e4
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-01-25 15:23:24 +0100

    WIP moving to cp-enterprise-replicator-executable image
    Former-commit-id: 3be201fe5dd93568fe9a957e8d7af154fa02ad58 [formerly 85cecd23f25a9e1bfa1f2411c3cbb19a3a61745d [formerly a4ad6e49336f7f5ca8b9d2ae32a63a2bf66d029b]]
    Former-commit-id: e8fdb05f05d0a68cf470f60a2b1e2880207ac4b2
    Former-commit-id: da1d7f608263f926f08e336c63035c96a209ef3e

--- ccloud/replicator/executable-onprem-to-cloud-repro-avro.sh
+++ ccloud/replicator/executable-onprem-to-cloud-repro-avro.sh
@@ -41,12 +41,8 @@ set -e
 
 log "Delete schema for topic"
 set +e
-ccloud schema-registry schema delete --subject executable-products-avro-value --version latest
+curl -X DELETE -u $SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO $SCHEMA_REGISTRY_URL/subjects/executable-products-avro-value
 set -e
-
-# Avoid java.lang.OutOfMemoryError: Java heap space
-docker container restart connect
-sleep 5
 
 log "Sending messages to topic executable-products-avro on source OnPREM cluster"
 docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic executable-products-avro --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"name","type":"string"},
@@ -56,15 +52,11 @@ docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --p
 {"name": "notebooks", "price": 1.99, "quantity": 5}
 EOF
 
-log "Starting replicator executable (logs are in /tmp/replicator.log):"
-# run in detach mode -d
-docker exec -d connect bash -c 'export CLASSPATH=/etc/kafka-connect/jars/replicator-rest-extension-*.jar; replicator --consumer.config /etc/kafka/executable-onprem-to-cloud-consumer-repro-avro.properties --producer.config /etc/kafka/executable-onprem-to-cloud-producer-repro-avro.properties  --replication.config /etc/kafka/executable-onprem-to-cloud-replicator-repro-avro.properties  --cluster.id executable-onprem-to-cloud --whitelist executable-products-avro > /tmp/replicator.log 2>&1'
-
-# docker exec connect bash -c 'export CLASSPATH=/etc/kafka-connect/jars/replicator-rest-extension-*.jar; replicator --consumer.config /etc/kafka/executable-onprem-to-cloud-consumer-repro-avro.properties --producer.config /etc/kafka/executable-onprem-to-cloud-producer-repro-avro.properties  --replication.config /etc/kafka/executable-onprem-to-cloud-replicator-repro-avro.properties  --cluster.id executable-onprem-to-cloud --whitelist executable-products-avro'
+log "Starting replicator executable"
+docker-compose -f ../../ccloud/environment/docker-compose.yml -f ${PWD}/docker-compose-executable-onprem-to-cloud.yml -f docker-compose-executable-onprem-to-cloud-repro-avro-replicator.yml up -d
+../../scripts/wait-for-connect-and-controlcenter.sh replicator $@
 
 sleep 50
 log "Verify we have received the data in executable-products-avro topic"
 timeout 60 docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" connect bash -c 'kafka-avro-console-consumer --topic executable-products-avro --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=USER_INFO --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --from-beginning --max-messages 3'
 
-log "Copying replicator logs to /tmp/replicator.log"
-docker cp connect:/tmp/replicator.log /tmp/replicator.log
