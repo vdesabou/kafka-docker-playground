629437789cf805a5cf4bc0cdc4996627c2407963
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-01-26 15:17:33 +0100

    fix file name

--- ccloud/replicator/stop.sh
+++ ccloud/replicator/stop.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-executable-onprem-to-cloud-replicator.yml"
-${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-repro-cloud-to-cloud.yml"
+${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-cloud-to-cloud.yml"
 ${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-connect-onprem-to-cloud.yml"
 ${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-executable-onprem-to-cloud.yml"
 ${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose-executable-onprem-to-cloud-topicrecordnamestrategy.yml"
