ce24af30ed64c45aaed0d1f87ac5c4e522fe0a33
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-01-29 10:40:29 +0100

    Adding cp-helm-charts example connector to Confluent Cloud #198
    Former-commit-id: 794cf017fdb4f3c3592e001479e6cd53ca96b365 [formerly f1b3d23c3788fcb3596e88e87b9e05225473db07 [formerly df2f0155b681473131d0c1ed0584489984eba1dd]]
    Former-commit-id: 05705c1e5119b826172205c4af09a9873083ace2
    Former-commit-id: 68a29fe5dbe0e1453f3cb58200b865af445ff0f4

--- ccloud/cp-helm-chart/start.sh
+++ ccloud/cp-helm-chart/start.sh
@@ -10,13 +10,6 @@ verify_installed "kubectl"
 verify_installed "minikube"
 verify_installed "helm"
 
-# FIXTHIS: helm 3 is not supported;
-# brew install helm@2
-# export PATH="/usr/local/opt/helm@2/bin:$PATH" >> ~/.bash_profile
-# helm init --stable-repo-url=https://charts.helm.sh/stable --client-only
-# helm repo add stable https://charts.helm.sh/stable
-# helm repo update
-
 CONFIG_FILE=~/.ccloud/config
 
 if [ ! -f ${CONFIG_FILE} ]
@@ -55,9 +48,71 @@ minikube dashboard &
 
 #helm repo add stable https://charts.helm.sh/stable
 #helm repo update
-helm repo add confluentinc https://confluentinc.github.io/cp-helm-charts/
+#helm repo add confluentinc https://confluentinc.github.io/cp-helm-charts/
 
-helm install demo -f values.yaml --set cp-schema-registry.enabled=false,cp-kafka-rest.enabled=false,cp-ksql-server.enabled=false confluentinc/cp-helm-charts
+git clone https://github.com/confluentinc/cp-helm-charts.git
+
+log "Create the Kubernetes namespace to install cp-helm-charts"
+kubectl create namespace cp-helm-charts
+
+SR_USERNAME=$(echo $SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO | cut -d ":" -f 1)
+SR_SECRET=$(echo $SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO | cut -d ":" -f 2)
+
+exit 0
+
+log "Install connect"
+helm upgrade --install \
+   connect \
+    ${DIR}/cp-helm-charts/charts/cp-kafka-connect \
+  --values ${DIR}/cp-helm-charts/charts/cp-kafka-connect/values.yaml \
+  --namespace cp-helm-charts \
+  --set kafka.bootstrapServers="${BOOTSTRAP_SERVERS}" \
+  --set configurationOverrides."ssl\.endpoint\.identification\.algorithm"=https \
+  --set configurationOverrides."security\.protocol"=SASL_SSL \
+  --set configurationOverrides."sasl\.mechanism"=PLAIN \
+  --set configurationOverrides."sasl\.jaas\.config"="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${CLOUD_KEY}\" password=\"${CLOUD_SECRET}\";" \
+  --set configurationOverrides."request\.timeout\.ms"=20000 \
+  --set configurationOverrides."retry\.backoff\.ms"=500 \
+  --set configurationOverrides."producer\.bootstrap\.servers"="${BOOTSTRAP_SERVERS}" \
+  --set configurationOverrides."producer\.ssl\.endpoint\.identification\.algorithm"=https \
+  --set configurationOverrides."producer\.security\.protocol"=SASL_SSL \
+  --set configurationOverrides."producer\.sasl\.mechanism"=PLAIN \
+  --set configurationOverrides."producer\.sasl\.jaas\.config"="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${CLOUD_KEY}\" password=\"${CLOUD_SECRET}\";" \
+  --set configurationOverrides."producer\.request\.timeout\.ms"=20000 \
+  --set configurationOverrides."producer\.retry\.backoff\.ms"=500 \
+  --set configurationOverrides."consumer\.bootstrap\.servers"="${BOOTSTRAP_SERVERS}" \
+  --set configurationOverrides."consumer\.ssl\.endpoint\.identification\.algorithm"=https \
+  --set configurationOverrides."consumer\.security\.protocol"=SASL_SSL \
+  --set configurationOverrides."consumer\.sasl\.mechanism"=PLAIN \
+  --set configurationOverrides."consumer\.sasl\.jaas\.config"="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${CLOUD_KEY}\" password=\"${CLOUD_SECRET}\";" \
+  --set configurationOverrides."consumer\.request\.timeout\.ms"=20000 \
+  --set configurationOverrides."consumer\.retry\.backoff\.ms"=500 \
+  --set configurationOverrides."value\.converter\.basic\.auth\.credentials\.source"=USER_INFO \
+  --set configurationOverrides."value\.converter\.schema\.registry\.basic\.auth\.user\.info"="${SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO}" \
+  --set configurationOverrides."value\.converter\.schema\.registry\.url"="${SCHEMA_REGISTRY_URL}"
+
+
+log "Sleep 60 seconds to let pods being started"
+sleep 60
+
+set +e
+# Verify Kafka Connect has started within MAX_WAIT seconds
+MAX_WAIT=480
+CUR_WAIT=0
+POD_NAME=$(kubectl get pods -n cp-helm-charts --selector=app=cp-kafka-connect -o jsonpath="{.items[0].metadata.name}")
+log "Waiting up to $MAX_WAIT seconds for Kafka Connect $POD_NAME to start"
+kubectl logs -n cp-helm-charts $POD_NAME -c cp-kafka-connect-server > /tmp/out.txt 2>&1
+while [[ ! $(cat /tmp/out.txt) =~ "Finished starting connectors and tasks" ]]; do
+  sleep 10
+  kubectl logs -n cp-helm-charts $POD_NAME -c cp-kafka-connect-server > /tmp/out.txt 2>&1
+  CUR_WAIT=$(( CUR_WAIT+10 ))
+  if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+    echo -e "\nERROR: The logs in $POD_NAME container do not show 'Finished starting connectors and tasks' after $MAX_WAIT seconds. Please troubleshoot'.\n"
+    exit 1
+  fi
+done
+log "Connect $POD_NAME has started!"
+set -e
 
 # https://github.com/confluentinc/cp-helm-charts#monitoring
 log "Install Prometheus"
