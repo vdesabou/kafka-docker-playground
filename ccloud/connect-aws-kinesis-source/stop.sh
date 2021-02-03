7bd5cf0c9e2057b3af26ceb33903913647e9bd81
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-02-03 13:27:19 +0100

    fix

--- ccloud/connect-aws-kinesis-source/stop.sh
+++ ccloud/connect-aws-kinesis-source/stop.sh
@@ -5,4 +5,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-${DIR}/../../environment/plaintext/stop.sh "${PWD}/docker-compose.plaintext.yml"
+${DIR}/../../environment/plaintext/stop.sh "${PWD}/docker-compose.yml"
