ffae2d061b8c4f76c28b980dce9b8e8abc8d9f11
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-02-04 10:57:52 +0100

    Added self-balancing example with Operator
    Former-commit-id: 118b67677941b47bdae55795a97d2afb6837bad4 [formerly 01f7e7a0aba8dc1024db19f3696a8e3d89b3bc7a [formerly efb70c8804b3fc21d4a9286863d724da446c6dd3]]
    Former-commit-id: 403d0225c10b46799164f05dfadfe61676313445
    Former-commit-id: 95bda412ac7c076ead0cce6fc6cb60757b6e5157

---
+++ operator/self-balancing/README.md
@@ -0,0 +1,52 @@
+# Test Self Balancing with operator
+
+Quickly test [self-balancing](https://docs.confluent.io/operator/current/co-management.html#scale-ak-clusters-and-balance-data) with replicator by following this [demo](https://github.com/confluentinc/demo-scene/tree/master/self-balancing).
+
+Create a topic sbk, We are forcing the topic to not create replicas in broker 2 to create an uneven load
+
+```bash
+$ kubectl cp ${DIR}/kafka.properties operator/kafka-0:/tmp/config
+$ kubectl -n operator exec -i kafka-0 -- bash -c 'kafka-topics --create --topic sbk --bootstrap-server kafka:9071 --command-config /tmp/config --replica-assignment 0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1'
+```
+
+Produce Data
+
+```bash
+$ kubectl -n operator exec -i kafka-0 -- bash -c 'kafka-producer-perf-test --producer-props bootstrap.servers=kafka:9071 --producer.config /tmp/config --topic sbk --record-size 1000 --throughput 100000 --num-records 3600000'
+```
+
+![disk usage](screenshot1.jpg)
+
+Enable self balancing
+
+```bash
+$ helm upgrade --install \
+  kafka \
+  ${DIR}/confluent-operator/helm/confluent-operator/ \
+  --values $VALUES_FILE \
+  --namespace operator \
+  --set kafka.enabled=true \
+  --set kafka.replicas=3 \
+  --set kafka.metricReporter.enabled=true \
+  --set kafka.metricReporter.bootstrapEndpoint="kafka:9071" \
+  --set kafka.oneReplicaPerNode=false \
+  --set 'kafka.configOverrides.server[0]=confluent.license.topic.replication.factor=1' \
+  --set 'kafka.configOverrides.server[1]=confluent.balancer.enable=true' \
+  --set 'kafka.configOverrides.server[2]=confluent.balancer.heal.uneven.load.trigger=ANY_UNEVEN_LOAD'
+```
+
+* Wait for Self-Balancing to start the rebalance
+
+Self-Balancing samples data about disk use, leader count, partition count, network throughput and other variables. It then aggregates this data to make informed decisions about how to reassign partitions. It needs between 10 and 20 minutes from startup to collect enough samples to generate a rebalacing plan (if one is needed). Self-Balancing also invalidates previous samples when the number of partitions in the cluster changes materially since they may not accurately reflect the current state of the cluster.
+
+While Self-Balancing is still sampling, the following message will appear on the logs periodically: INFO Skipping proposal precomputing because load monitor does not have enough snapshots.
+
+* Watch Self-Balancing rebalance the cluster
+
+Once Self-balacing is ready to compute reassignment plans, the following message will appear: INFO Finished the precomputation proposal candidates in * ms (com.linkedin.kafka.cruisecontrol.analyzer.GoalOptimizer)
+
+Self-Balancing should then start the rebalancing process. Monitor the logs and the watch kafka-topics command to observe the changes as they happen.
+
+After about 20 minutes:
+
+![disk usage](screenshot2.jpg)
---
+++ operator/self-balancing/start.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+verify_memory
+verify_installed "kubectl"
+verify_installed "minikube"
+verify_installed "helm"
+
+set +e
+log "Stop minikube if required"
+minikube delete
+set -e
+log "Start minikube"
+minikube start --cpus=8 --disk-size='50gb' --memory=16384
+log "Launch minikube dashboard in background"
+minikube dashboard &
+
+log "Download Confluent Operator in ${DIR}/confluent-operator"
+
+rm -rf ${DIR}/confluent-operator
+mkdir ${DIR}/confluent-operator
+cd ${DIR}/confluent-operator
+wget https://platform-ops-bin.s3-us-west-1.amazonaws.com/operator/confluent-operator-1.6.1-for-confluent-platform-6.0.0.tar.gz
+tar xvfz confluent-operator-1.6.1-for-confluent-platform-6.0.0.tar.gz
+cd -
+
+
+log "Extend Kubernetes with first class CP primitives"
+kubectl apply --filename ${DIR}/confluent-operator/resources/crds/
+
+log "Create the Kubernetes namespaces to install Operator and clusters"
+
+kubectl create namespace operator
+
+# Use most basic values file and override it with --set
+VALUES_FILE="${DIR}/../../operator/private.yaml"
+
+log "installing operator"
+helm upgrade --install \
+  operator \
+  ${DIR}/confluent-operator/helm/confluent-operator/ \
+  --values $VALUES_FILE \
+  --namespace operator \
+  --set operator.enabled=true
+
+log "install operator cluster"
+helm upgrade --install \
+  zookeeper \
+  ${DIR}/confluent-operator/helm/confluent-operator/ \
+  --values $VALUES_FILE \
+  --namespace operator \
+  --set zookeeper.enabled=true
+
+helm upgrade --install \
+  kafka \
+  ${DIR}/confluent-operator/helm/confluent-operator/ \
+  --values $VALUES_FILE \
+  --namespace operator \
+  --set kafka.enabled=true \
+  --set kafka.replicas=3 \
+  --set kafka.metricReporter.enabled=true \
+  --set kafka.metricReporter.bootstrapEndpoint="kafka:9071" \
+  --set kafka.oneReplicaPerNode=false
+
+
+  # --set 'kafka.configOverrides.server[0]=confluent.license.topic.replication.factor=1' \
+  # --set 'kafka.configOverrides.server[1]=confluent.balancer.enable=true' \
+  # --set 'kafka.configOverrides.server[2]=confluent.balancer.heal.uneven.load.trigger=ANY_UNEVEN_LOAD'
+
+
+helm upgrade --install \
+  replicator \
+    ${DIR}/confluent-operator/helm/confluent-operator/ \
+  --values $VALUES_FILE \
+  --namespace operator \
+  --set replicator.enabled=true \
+  --set replicator.dependencies.kafka.brokerCount=3
+
+helm upgrade --install \
+  controlcenter \
+    ${DIR}/confluent-operator/helm/confluent-operator/ \
+  --values $VALUES_FILE \
+  --namespace operator \
+  --set controlcenter.enabled=true
+
+
+# kubectl -n operator exec -it replicator-0 -- bash
+
+
+log "Sleep 60 seconds to let pods being started"
+sleep 60
+
+set +e
+# Verify Kafka Connect has started within MAX_WAIT seconds
+MAX_WAIT=480
+CUR_WAIT=0
+log "Waiting up to $MAX_WAIT seconds for Kafka Connect replicator-0 to start"
+kubectl logs -n operator replicator-0 > /tmp/out.txt 2>&1
+while [[ ! $(cat /tmp/out.txt) =~ "Finished starting connectors and tasks" ]]; do
+  sleep 10
+  kubectl logs -n operator replicator-0 > /tmp/out.txt 2>&1
+  CUR_WAIT=$(( CUR_WAIT+10 ))
+  if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+    echo -e "\nERROR: The logs in replicator-0 container do not show 'Finished starting connectors and tasks' after $MAX_WAIT seconds. Please troubleshoot'.\n"
+    exit 1
+  fi
+done
+log "Connect replicator-0 has started!"
+set -e
+
+log "Control Center is reachable at http://127.0.0.1:9021 (admin/Developer1)"
+kubectl -n operator port-forward controlcenter-0 9021:9021 &
+
+# https://github.com/confluentinc/demo-scene/tree/master/self-balancing
+log "Create a topic sbk, We are forcing the topic to not create replicas in broker 2 to create an uneven load"
+kubectl cp ${DIR}/kafka.properties operator/kafka-0:/tmp/config
+kubectl -n operator exec -i kafka-0 -- bash -c 'kafka-topics --create --topic sbk --bootstrap-server kafka:9071 --command-config /tmp/config --replica-assignment 0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1'
+
+log "Produce Data"
+kubectl -n operator exec -i kafka-0 -- bash -c 'kafka-producer-perf-test --producer-props bootstrap.servers=kafka:9071 --producer.config /tmp/config --topic sbk --record-size 1000 --throughput 100000 --num-records 3600000'
+
+log "enable self balancing"
+helm upgrade --install \
+  kafka \
+  ${DIR}/confluent-operator/helm/confluent-operator/ \
+  --values $VALUES_FILE \
+  --namespace operator \
+  --set kafka.enabled=true \
+  --set kafka.replicas=3 \
+  --set kafka.metricReporter.enabled=true \
+  --set kafka.metricReporter.bootstrapEndpoint="kafka:9071" \
+  --set kafka.oneReplicaPerNode=false \
+  --set 'kafka.configOverrides.server[0]=confluent.license.topic.replication.factor=1' \
+  --set 'kafka.configOverrides.server[1]=confluent.balancer.enable=true' \
+  --set 'kafka.configOverrides.server[2]=confluent.balancer.heal.uneven.load.trigger=ANY_UNEVEN_LOAD'
