354c06b6de5c6d8701ce94b01bf9045361311559
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-02-10 11:58:10 +0100

    ssl but with com.amazonaws.sdk.disableCertChecking as cert is expired
    https://github.com/mhart/kinesalite/issues/96
    
    Former-commit-id: 81e6461c7c256ccb8a4b263338b82f4c4dd1fd83 [formerly 5384e9d5fbfd30c55315dfba9e00159eab00561d [formerly b62482725736ae71f536ae3e6bfd71476c3c2ef8]]
    Former-commit-id: 650c9aa946005588b39bdbbd8e76061eab4e3d5b
    Former-commit-id: f5912a0ce24efaeca4f07d4c20f614cdbf828a79

--- connect/connect-aws-kinesis-source/kinesis-repro-local-instance.sh
+++ connect/connect-aws-kinesis-source/kinesis-repro-local-instance.sh
@@ -16,10 +16,25 @@ then
      exit 1
 fi
 
+# rm -f *.jks
+# # Create truststore and import the CA cert
+# docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG}  keytool -noprompt -keystore /tmp/my.truststore.jks -alias CARoot -import -file /tmp/server-crt.pem -storepass confluent -keypass confluent
+# docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -noprompt -destkeystore /tmp/my.truststore.jks -importkeystore -srckeystore /usr/lib/jvm/zulu11-ca/lib/security/cacerts -srcstorepass changeit -deststorepass confluent
+
+
+# docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} openssl pkcs12 -export -in /tmp/server-crt.pem -inkey /tmp/server-key.pem \
+#                -out /tmp/kinesis-server.p12 -name kinesis-server \
+#                -CAfile /tmp/ca-crt.pem -caname CARoot -passout pass:confluent
+
+# docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -importkeystore \
+#         -deststorepass confluent -destkeypass confluent -destkeystore /tmp/my.trustore.jks \
+#         -srckeystore /tmp/kinesis-server.p12 -srcstoretype PKCS12 -srcstorepass confluent \
+#         -alias kinesis-server
+
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext-repro-local-instance.yml"
 
 log "Create a Kinesis stream my_kinesis_stream"
-/usr/local/bin/aws kinesis --endpoint-url http://localhost:4567/ create-stream --stream-name my_kinesis_stream --shard-count 1
+/usr/local/bin/aws kinesis --endpoint-url https://localhost:4567/ create-stream --stream-name my_kinesis_stream --shard-count 1 --no-verify-ssl
 
 log "Sleep 10 seconds to let the Kinesis stream being fully started"
 sleep 10
@@ -32,7 +47,7 @@ curl -X PUT \
                "connector.class":"io.confluent.connect.kinesis.KinesisSourceConnector",
                "tasks.max": "1",
                "kafka.topic": "kinesis_topic",
-               "kinesis.base.url": "http://kinesis-local:4567",
+               "kinesis.base.url": "https://kinesis-local:4567",
                "kinesis.stream": "my_kinesis_stream",
                "kinesis.position": "AT_TIMESTAMP",
                "kinesis.shard.timestamp": "'"$TODAY"'",
@@ -45,7 +60,7 @@ curl -X PUT \
 
 log "Insert records in Kinesis stream"
 # The example shows that a record containing partition key 123 and data "test-message-1" is inserted into my_kinesis_stream.
-/usr/local/bin/aws kinesis --endpoint-url http://localhost:4567/ put-record --stream-name my_kinesis_stream --partition-key 123 --data test-message-1
+/usr/local/bin/aws kinesis --endpoint-url https://localhost:4567/ put-record --stream-name my_kinesis_stream --partition-key 123 --data test-message-1 --no-verify-ssl
 
 sleep 10
 
@@ -53,4 +68,4 @@ log "Verify we have received the data in kinesis_topic topic"
 timeout 60 docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic kinesis_topic --from-beginning --max-messages 1
 
 log "Delete the stream"
-/usr/local/bin/aws kinesis --endpoint-url http://localhost:4567/ delete-stream --stream-name my_kinesis_stream
+/usr/local/bin/aws kinesis --endpoint-url https://localhost:4567/ delete-stream --stream-name my_kinesis_stream --no-verify-ssl
