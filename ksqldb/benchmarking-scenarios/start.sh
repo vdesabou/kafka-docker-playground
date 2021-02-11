87b6444faaa0e7a5928c6397a40743a519dd6447
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-02-11 14:57:31 +0100

    benchmark scenarios

---
+++ ksqldb/benchmarking-scenarios/start.sh
@@ -0,0 +1,227 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Create topic orders"
+curl -s -X PUT \
+      -H "Content-Type: application/json" \
+      --data '{
+                "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
+                "kafka.topic": "orders",
+                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                "value.converter.schemas.enable": "false",
+                "max.interval": 1,
+                "iterations": "1000",
+                "tasks.max": "10",
+                "schema.filename" : "/tmp/schemas/orders.avro",
+                "schema.keyfield" : "orderid"
+            }' \
+      http://localhost:8083/connectors/datagen-orders/config | jq
+
+
+log "Create topic shipments"
+curl -s -X PUT \
+      -H "Content-Type: application/json" \
+      --data '{
+                "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
+                "kafka.topic": "shipments",
+                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                "value.converter.schemas.enable": "false",
+                "max.interval": 1,
+                "iterations": "1000",
+                "tasks.max": "10",
+                "schema.filename" : "/tmp/schemas/shipments.avro"
+            }' \
+      http://localhost:8083/connectors/datagen-shipments/config | jq
+
+log "Create topic products"
+curl -s -X PUT \
+      -H "Content-Type: application/json" \
+      --data '{
+                "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
+                "kafka.topic": "products",
+                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                "value.converter.schemas.enable": "false",
+                "max.interval": 1,
+                "iterations": "1000",
+                "tasks.max": "10",
+                "schema.filename" : "/tmp/schemas/products.avro",
+                "schema.keyfield" : "productid"
+            }' \
+      http://localhost:8083/connectors/datagen-products/config | jq
+
+log "Create topic customers"
+curl -s -X PUT \
+      -H "Content-Type: application/json" \
+      --data '{
+                "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
+                "kafka.topic": "customers",
+                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                "value.converter.schemas.enable": "false",
+                "max.interval": 1,
+                "iterations": "1000",
+                "tasks.max": "10",
+                "schema.filename" : "/tmp/schemas/customers.avro",
+                "schema.keyfield" : "customerid"
+            }' \
+      http://localhost:8083/connectors/datagen-customers/config | jq
+
+
+
+log "Create the ksqlDB tables and streams"
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE TABLE CUSTOMERS
+(
+    customerid varchar PRIMARY KEY,
+    firstname varchar,
+    lastname varchar, gender varchar,
+    random_data varchar,
+    address struct<street varchar, city varchar, state varchar, zipcode bigint>
+)
+WITH
+    (kafka_topic='customers', value_format='json');
+
+CREATE TABLE PRODUCTS
+(
+    productid varchar PRIMARY KEY,
+    name varchar,
+    category varchar,
+    description varchar
+)
+WITH
+    (kafka_topic='products', value_format='json');
+
+CREATE STREAM ORDERS
+(
+    ordertime bigint,
+    orderid bigint,
+    productid varchar,
+    orderunits integer,
+    customerid varchar
+)
+WITH
+    (kafka_topic= 'orders', value_format='json', timestamp='ordertime');
+
+CREATE STREAM SHIPMENTS
+(
+    SHIPMENT_TIME bigint,
+    SHIPMENTID bigint,
+    orderid bigint,
+    productid varchar,
+    customerid varchar
+)
+    WITH (kafka_topic= 'shipments', value_format='json', timestamp='shipment_time');
+EOF
+
+log "QUERY 1"
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE STREAM ENRICHED_O_C AS SELECT
+  UNIX_TIMESTAMP() JOINTIME,
+  O.ORDERTIME ORDERTIME,
+  O.ORDERID ORDERID,
+  O.PRODUCTID PRODUCTID,
+  O.ORDERUNITS ORDERUNITS,
+  O.CUSTOMERID CUSTOMERID,
+  CUSTOMERS.FIRSTNAME FIRSTNAME,
+  CUSTOMERS.LASTNAME LASTNAME,
+  CUSTOMERS.GENDER GENDER,
+  CUSTOMERS.RANDOM_DATA RANDOM_DATA,
+  CUSTOMERS.ADDRESS ADDRESS
+FROM
+  ORDERS O
+LEFT OUTER JOIN
+    CUSTOMERS CUSTOMERS
+    ON ((O.CUSTOMERID = CUSTOMERS.CUSTOMERID));
+EOF
+
+log "QUERY 2"
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE STREAM ENRICHED_O_C_P AS SELECT
+  UNIX_TIMESTAMP() JOINTIME,
+  ORDERTIME,
+  ORDERID,
+  P.PRODUCTID as PRODUCTID,
+  ORDERUNITS,
+  CUSTOMERID,
+  FIRSTNAME,
+  LASTNAME,
+  GENDER,
+  RANDOM_DATA,
+  ADDRESS,
+  NAME as ProductName,
+  category,
+  description
+FROM
+  ENRICHED_O_C o
+LEFT JOIN
+  PRODUCTS p
+ON O.PRODUCTID = P.PRODUCTID;
+EOF
+
+log "QUERY 3"
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE STREAM ORDERS_SHIPPED AS SELECT
+  UNIX_TIMESTAMP() JOINTIME,
+  O.ORDERID O_ORDERID,
+  S.ORDERID S_ORDERID,
+  S.SHIPMENTID SHIPMENTID,
+  O.PRODUCTID PRODUCTID,
+  O.CUSTOMERID CUSTOMERID,
+  O.ORDERUNITS ORDERUNITS,
+  PRODUCTNAME,
+  CATEGORY,
+  DESCRIPTION,
+  FIRSTNAME,
+  lastname,
+  gender,
+  RANDOM_DATA,
+  address
+FROM
+  ENRICHED_O_C_P o
+INNER JOIN SHIPMENTS S
+  WITHIN 2 HOURS
+ON O.ORDERID = S.ORDERID;
+EOF
+
+log "QUERY 4"
+# FIXTHIS
+# l8 is MAX(UNIX_TIMESTAMP()) - MIN(UNIX_TIMESTAMP())) TIMEDIFF
+# getting line 8:48: extraneous input ')' expecting {',', 'FROM'}
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE TABLE ORDERPER_PROD_CUST_AGG AS SELECT
+  os.PRODUCTID PRODUCTID,
+  os.CUSTOMERID CUSTOMERID,
+  COUNT(*) COUNTVAL,
+  SUM(eo.ORDERUNITS) ORDERSUM,
+  MIN(UNIX_TIMESTAMP()) MINTIME,
+  MAX(UNIX_TIMESTAMP()) MAXTIME,
+  MAX(UNIX_TIMESTAMP()) - MIN(UNIX_TIMESTAMP())) TIMEDIFF
+FROM
+  ORDERS_SHIPPED os
+WINDOW TUMBLING ( SIZE 1 MINUTES )
+GROUP BY
+  os.PRODUCTID, os.CUSTOMERID;
+EOF
