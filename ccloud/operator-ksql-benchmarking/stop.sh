5b0f9f8d43354b14ed352ba2a3e365fb027793a0
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-02-12 12:04:28 +0100

    WIP

--- ccloud/operator-ksql-benchmarking/stop.sh
+++ ccloud/operator-ksql-benchmarking/stop.sh
@@ -5,4 +5,34 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-minikube delete
+# read configuration files
+#
+if [ -r ${DIR}/test.properties ]
+then
+    . ${DIR}/test.properties
+else
+    logerror "Cannot read configuration file ${DIR}/test.properties"
+    exit 1
+fi
+
+if [ -r ${DIR}/ccloud-cluster.properties ]
+then
+    . ${DIR}/ccloud-cluster.properties
+else
+    logerror "Cannot read configuration file ${APP_HOME}/ccloud-cluster.properties"
+    exit 1
+fi
+
+if [ "${provider}" = "minikube" ]
+then
+    minikube delete
+elif [ "${provider}" = "aws" ]
+then
+    #######
+    # aws
+    #######
+    log "EKS"
+else
+    logerror "Provider ${provider} is not supported"
+    exit 1
+fi
