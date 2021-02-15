bc3a05b5681bfa9ea29e319934e7f4c18e1d02d9
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-02-15 12:21:02 +0100

    cleanup queries
    Former-commit-id: 10738e73eb54faaade7733fa211880d8fc2ce534 [formerly 43c9979a52aca1c58fe161aa3ff0c8dc933f21e4 [formerly 1b61bcd03d55f15e9c7a7c36fe7bdac19025a810]]
    Former-commit-id: 5d3bc283b97b1c7f484bce09678f1554b4476d30
    Former-commit-id: 9ebe16bcb0f141140ed356ede37ee01346b10fd0

---
+++ ccloud/operator-ksql-benchmarking/06-cleanup-queries.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+# read configuration files
+#
+if [ -r ${DIR}/test.properties ]
+then
+    . ${DIR}/test.properties
+else
+    logerror "Cannot read configuration file ${DIR}/test.properties"
+    exit 1
+fi
+
+if [ -r ${DIR}/ccloud-cluster.properties ]
+then
+    . ${DIR}/ccloud-cluster.properties
+else
+    logerror "Cannot read configuration file ${APP_HOME}/ccloud-cluster.properties"
+    exit 1
+fi
+
+verify_installed "kubectl"
+verify_installed "helm"
+
+set +e
+# https://rmoff.net/2019/03/25/terminate-all-ksql-queries/
+log "TERMINATE all queries, if applicable"
+kubectl exec -i connectors-0 -- bash -c "curl -s -X \"POST\" \"http://ksql:9088/ksql\" \
+         -H \"Content-Type: application/vnd.ksql.v1+json; charset=utf-8\" \
+         -d '{\"ksql\": \"SHOW QUERIES;\"}' | \
+  jq '.[].queries[].id' | \
+  xargs -Ifoo curl -s -X \"POST\" \"http://ksql:9088/ksql\" \
+           -H \"Content-Type: application/vnd.ksql.v1+json; charset=utf-8\" \
+           -d '{\"ksql\": \"TERMINATE 'foo';\"}'"
+log "DROP all streams, if applicable"
+kubectl exec -i connectors-0 -- bash -c "curl -s -X \"POST\" \"http://ksql:9088/ksql\" \
+           -H \"Content-Type: application/vnd.ksql.v1+json; charset=utf-8\" \
+           -d '{\"ksql\": \"SHOW STREAMS;\"}' | \
+    jq '.[].streams[].name' | \
+    xargs -Ifoo curl -s -X \"POST\" \"http://ksql:9088/ksql\" \
+             -H \"Content-Type: application/vnd.ksql.v1+json; charset=utf-8\" \
+             -d '{\"ksql\": \"DROP STREAM 'foo';\"}'"
+log "DROP all tables, if applicable"
+kubectl exec -i connectors-0 -- bash -c "curl -s -X \"POST\" \"http://ksql:9088/ksql\" \
+             -H \"Content-Type: application/vnd.ksql.v1+json; charset=utf-8\" \
+             -d '{\"ksql\": \"SHOW TABLES;\"}' | \
+      jq '.[].tables[].name' | \
+      xargs -Ifoo curl -s -X \"POST\" \"http://ksql:9088/ksql\" \
+               -H \"Content-Type: application/vnd.ksql.v1+json; charset=utf-8\" \
+               -d '{\"ksql\": \"DROP TABLE 'foo';\"}'"
+set -e
