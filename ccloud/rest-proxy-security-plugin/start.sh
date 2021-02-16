9f291e4ffb271d16de3c7ef05fcb69acd50649c3
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-02-16 16:15:46 +0100

    rest-proxy-security-plugin broken with CP 6.1 #211
    Former-commit-id: d1a797669b6c45306f6640285352c9c949c41a25 [formerly 3e94e4e6d31bde0beee2772fa69bc16355790f8f [formerly 44dcfe83d4f66923bf9d09c275d6583c7f70f4a1]]
    Former-commit-id: a33c286ccfccce9f3ac6e9ee1536924866b46778
    Former-commit-id: 4ae1155dfd1dfa2e22f3342bbcfd28719ffe42ad

--- ccloud/rest-proxy-security-plugin/start.sh
+++ ccloud/rest-proxy-security-plugin/start.sh
@@ -57,6 +57,6 @@ set -e
 
 sleep 15
 
-# run as root for linux case where key is ownaed by root user
+# run as root for linux case where key is owned by root user
 log "HTTP client using $REST_KEY principal"
 docker exec -e REST_KEY=$REST_KEY --privileged --user root restproxy curl -X POST --cert /etc/kafka/secrets/$REST_KEY.certificate.pem --key /etc/kafka/secrets/$REST_KEY.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://localhost:8082/topics/rest-proxy-security-plugin"
--- other/rest-proxy-security-plugin/start-2way-ssl.sh
+++ other/rest-proxy-security-plugin/start-2way-ssl.sh
@@ -8,8 +8,12 @@ source ${DIR}/../../scripts/utils.sh
 ${DIR}/../../environment/2way-ssl/start.sh "${PWD}/docker-compose.2way-ssl.yml"
 
 
+# log "HTTP client using clientrestproxy principal"
+# curl -X POST --cert ../../environment/2way-ssl/security/clientrestproxy.certificate.pem --key ../../environment/2way-ssl/security/clientrestproxy.key --tlsv1.2 --cacert ../../environment/2way-ssl/security/snakeoil-ca-1.crt -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://localhost:8086/topics/jsontest"
+
+# run as root for linux case where key is owned by root user
 log "HTTP client using clientrestproxy principal"
-curl -X POST --cert ../../environment/2way-ssl/security/clientrestproxy.certificate.pem --key ../../environment/2way-ssl/security/clientrestproxy.key --tlsv1.2 --cacert ../../environment/2way-ssl/security/snakeoil-ca-1.crt -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://localhost:8086/topics/jsontest"
+docker exec --privileged --user root restproxy curl -X POST --cert /etc/kafka/secrets/clientrestproxy.certificate.pem --key /etc/kafka/secrets/clientrestproxy.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://localhost:8086/topics/jsontest"
 
 log "Verify principal clientrestproxy is used"
 docker container logs broker | grep Write | grep jsontest
