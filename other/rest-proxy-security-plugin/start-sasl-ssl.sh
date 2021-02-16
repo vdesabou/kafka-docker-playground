06d9448cf0d3b64d1b3ab4c5173338735b2920ab
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-02-16 16:36:55 +0100

    sleep 3

--- other/rest-proxy-security-plugin/start-sasl-ssl.sh
+++ other/rest-proxy-security-plugin/start-sasl-ssl.sh
@@ -11,5 +11,7 @@ ${DIR}/../../environment/sasl-ssl/start.sh "${PWD}/docker-compose.sasl-ssl.yml"
 log "HTTP client using clientrestproxy principal"
 docker exec --privileged --user root restproxy curl -X POST --cert /etc/kafka/secrets/clientrestproxy.certificate.pem --key /etc/kafka/secrets/clientrestproxy.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://localhost:8086/topics/jsontest"
 
+sleep 3
+
 log "Verify principal clientrestproxy is used"
 docker container logs broker | grep Write | grep jsontest
