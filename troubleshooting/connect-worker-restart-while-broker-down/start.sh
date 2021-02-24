f44ad3f7340552dd4d0546d9ca5d30c04c65732c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-02-24 15:56:31 +0100

    using replicator and eager
    Former-commit-id: 50e81ac36536a5a78515b5c1d8589ce2921846cf [formerly 6887904403ab902f4ce45b027332d9c2f9e26c49 [formerly db89c3d04f31f131ed84e63a2fd3409bbb5cee7e]]
    Former-commit-id: ce0ebd4eade5503fc98973da3d0896f0127015ee
    Former-commit-id: 182e79765737d9ffb0f53ab55aecf0ee4284aa16

--- troubleshooting/connect-worker-restart-while-broker-down/start.sh
+++ troubleshooting/connect-worker-restart-while-broker-down/start.sh
@@ -9,40 +9,47 @@ docker-compose up -d
 ${DIR}/../../scripts/wait-for-connect-and-controlcenter.sh "connect1"
 ${DIR}/../../scripts/wait-for-connect-and-controlcenter.sh "connect2"
 
+docker exec broker1 kafka-topics --create --topic test-topic --partitions 10 --replication-factor 3 --zookeeper zookeeper:2181
 
-log "Creating SFTP Sink connector with 4 tasks"
+log "Sending messages to topic test-topic"
+seq 10 | docker exec -i broker1 kafka-console-producer --broker-list broker1:9092 --topic test-topic
+
+log "Creating Replicator connector"
 curl -X PUT \
-     -H "Content-Type: application/json" \
-     --data '{
-        "topics": "test_sftp_sink",
-               "tasks.max": "4",
-               "connector.class": "io.confluent.connect.sftp.SftpSinkConnector",
-               "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
-               "schema.generator.class": "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator",
-               "flush.size": "3",
-               "schema.compatibility": "NONE",
-               "format.class": "io.confluent.connect.sftp.sink.format.avro.AvroFormat",
-               "storage.class": "io.confluent.connect.sftp.sink.storage.SftpSinkStorage",
-               "sftp.host": "sftp-server",
-               "sftp.port": "22",
-               "sftp.username": "foo",
-               "sftp.password": "pass",
-               "sftp.working.dir": "/upload",
-               "confluent.license": "",
-               "confluent.topic.bootstrap.servers": "broker:9092",
-               "confluent.topic.replication.factor": "1"
-          }' \
-     http://localhost:8083/connectors/sftp-sink/config | jq .
+      -H "Content-Type: application/json" \
+      --data '{
+              "tasks.max": "10",
+              "connector.class":"io.confluent.connect.replicator.ReplicatorSourceConnector",
+               "key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+               "value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+               "header.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+               "src.consumer.group.id": "duplicate-topic",
+               "confluent.topic.replication.factor": 3,
+               "provenance.header.enable": true,
+               "topic.whitelist": "test-topic",
+               "topic.rename.format": "test-topic-duplicate",
+               "dest.kafka.bootstrap.servers": "broker1:9092,broker2:9092,broker3:9092",
+               "src.kafka.bootstrap.servers": "broker1:9092,broker2:9092,broker3:9092"
+           }' \
+      http://localhost:8083/connectors/replicator/config | jq .
+
+sleep 10
+
+log "Verify we have received the data in test-topic-duplicate topic"
+timeout 60 docker exec broker1 kafka-console-consumer --bootstrap-server broker1:9092 --topic test-topic-duplicate --from-beginning --max-messages 10
 
 sleep 5
 
 log "Getting tasks placement"
+
 curl --request GET \
-  --url http://localhost:8083/connectors/sftp-sink/status \
+  --url http://localhost:8083/connectors/replicator/status \
   --header 'accept: application/json' | jq
 
-log "Stop broker 1 (first in the connect worker bootstrap.servers list)"
-docker container stop broker1
+
+log "Stop broker 1"
+#docker container stop broker1
+docker container kill broker1
 # if broker 2 or 3 is down, no problem
 # docker container stop broker2
 
@@ -50,15 +57,17 @@ docker container stop connect2
 
 log "Getting tasks placement"
 curl --request GET \
-  --url http://localhost:8083/connectors/sftp-sink/status \
+  --url http://localhost:8083/connectors/replicator/status \
   --header 'accept: application/json' | jq
 
 docker container start connect2
 
-log "sleep 5 minutes (scheduled.rebalance.max.delay.ms), after this time all tasks should be RUNNING (no more UNASSIGNED)"
-sleep 310
+# commented as eager is used
+# log "sleep 5 minutes (scheduled.rebalance.max.delay.ms), after this time all tasks should be RUNNING (no more UNASSIGNED)"
+# sleep 310
+sleep 60
 
 log "Getting tasks placement"
 curl --request GET \
-  --url http://localhost:8083/connectors/sftp-sink/status \
+  --url http://localhost:8083/connectors/replicator/status \
   --header 'accept: application/json' | jq
