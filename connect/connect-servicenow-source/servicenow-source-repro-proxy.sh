b4cd55a5f5ffda8be87e9cdfb512a7502f620bb5
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-02-26 14:55:10 +0100

    Verify with curl version 7.38.0
    Former-commit-id: 732c6aa0a705d35b50f4bcc2f2b541fb39f03b90 [formerly 9364c601abaa8065b3ee485ab75a084a572d94c1 [formerly 5ca5f97a0a7b1d2151246f037a476798a565d182]]
    Former-commit-id: 6e493324b4fe3a2ed66aea941a9ba090b8284fc9
    Former-commit-id: edf5872a6d1bb7d6710718a17422adfb066db704

--- connect/connect-servicenow-source/servicenow-source-repro-proxy.sh
+++ connect/connect-servicenow-source/servicenow-source-repro-proxy.sh
@@ -32,72 +32,87 @@ export HTTPS_PROXY=127.0.0.1:8888
 log "Verify forward proxy is working correctly"
 curl -v ${SERVICENOW_URL}api/now/table/incident?sysparm_limit=1 -u "admin:$SERVICENOW_PASSWORD" | jq .
 
-# * Uses proxy env variable HTTPS_PROXY == '127.0.0.1:8888'
-# *   Trying 127.0.0.1...
-# * TCP_NODELAY set
-# * Connected to 127.0.0.1 (127.0.0.1) port 8888 (#0)
-# * allocate connect buffer!
+log "Verify with curl version 7.38.0"
+docker exec  -e SERVICENOW_URL=$SERVICENOW_URL -e SERVICENOW_PASSWORD=$SERVICENOW_PASSWORD oldcurl bash -c "export HTTP_PROXY=nginx_proxy:8888 && export HTTPS_PROXY=nginx_proxy:8888 && curl  -v ${SERVICENOW_URL}api/now/table/incident?sysparm_limit=1 -u \"admin:$SERVICENOW_PASSWORD\""
+
+# * Hostname was NOT found in DNS cache
+#   % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+#                                  Dload  Upload   Total   Spent    Left  Speed
+#   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0*   Trying 172.28.0.2...
+# * Connected to nginx_proxy (172.28.0.2) port 8888 (#0)
 # * Establish HTTP proxy tunnel to dev86373.service-now.com:443
+# * Server auth using Basic with user 'admin'
 # > CONNECT dev86373.service-now.com:443 HTTP/1.1
 # > Host: dev86373.service-now.com:443
-# > User-Agent: curl/7.64.1
+# > User-Agent: curl/7.38.0
 # > Proxy-Connection: Keep-Alive
 # >
 # < HTTP/1.1 200 Connection Established
 # < Proxy-agent: nginx
 # <
-# * Proxy replied 200 to CONNECT request
-# * CONNECT phase completed!
-# * ALPN, offering h2
-# * ALPN, offering http/1.1
+# * Proxy replied OK to CONNECT request
 # * successfully set certificate verify locations:
-# *   CAfile: /etc/ssl/cert.pem
-#   CApath: none
-# * TLSv1.2 (OUT), TLS handshake, Client hello (1):
-# * CONNECT phase completed!
-# * CONNECT phase completed!
-# * TLSv1.2 (IN), TLS handshake, Server hello (2):
-# * TLSv1.2 (IN), TLS handshake, Certificate (11):
-# * TLSv1.2 (IN), TLS handshake, Server key exchange (12):
-# * TLSv1.2 (IN), TLS handshake, Server finished (14):
-# * TLSv1.2 (OUT), TLS handshake, Client key exchange (16):
-# * TLSv1.2 (OUT), TLS change cipher, Change cipher spec (1):
-# * TLSv1.2 (OUT), TLS handshake, Finished (20):
-# * TLSv1.2 (IN), TLS change cipher, Change cipher spec (1):
-# * TLSv1.2 (IN), TLS handshake, Finished (20):
+# *   CAfile: none
+#   CApath: /etc/ssl/certs
+# * SSLv3, TLS handshake, Client hello (1):
+# } [data not shown]
+# * SSLv3, TLS handshake, Server hello (2):
+# { [data not shown]
+# * SSLv3, TLS handshake, CERT (11):
+# { [data not shown]
+# * SSLv3, TLS handshake, Server key exchange (12):
+# { [data not shown]
+# * SSLv3, TLS handshake, Server finished (14):
+# { [data not shown]
+# * SSLv3, TLS handshake, Client key exchange (16):
+# } [data not shown]
+# * SSLv3, TLS change cipher, Client hello (1):
+# } [data not shown]
+# * SSLv3, TLS handshake, Finished (20):
+# } [data not shown]
+# * SSLv3, TLS change cipher, Client hello (1):
+# { [data not shown]
+# * SSLv3, TLS handshake, Finished (20):
+# { [data not shown]
 # * SSL connection using TLSv1.2 / ECDHE-RSA-AES128-GCM-SHA256
-# * ALPN, server did not agree to a protocol
 # * Server certificate:
-# *  subject: C=US; ST=California; L=San Diego; O=ServiceNow, Inc.; CN=*.service-now.com
-# *  start date: Jul 22 23:55:53 2020 GMT
-# *  expire date: Apr  1 23:55:53 2021 GMT
-# *  subjectAltName: host "dev86373.service-now.com" matched cert's "*.service-now.com"
-# *  issuer: C=US; O=Entrust, Inc.; OU=See www.entrust.net/legal-terms; OU=(c) 2012 Entrust, Inc. - for authorized use only; CN=Entrust Certification Authority - L1K
-# *  SSL certificate verify ok.
+# *        subject: C=US; ST=California; L=San Diego; O=ServiceNow, Inc.; CN=*.service-now.com
+# *        start date: 2020-07-22 23:55:53 GMT
+# *        expire date: 2021-04-01 23:55:53 GMT
+# *        subjectAltName: dev86373.service-now.com matched
+# *        issuer: C=US; O=Entrust, Inc.; OU=See www.entrust.net/legal-terms; OU=(c) 2012 Entrust, Inc. - for authorized use only; CN=Entrust Certification Authority - L1K
+# *        SSL certificate verify ok.
+# * Server auth using Basic with user 'admin'
 # > GET /api/now/table/incident?sysparm_limit=1 HTTP/1.1
+# > Authorization: Basic YWRtaW46Nkd2S0s1bW5hSGpZ
+# > User-Agent: curl/7.38.0
 # > Host: dev86373.service-now.com
-# > User-Agent: curl/7.64.1
 # > Accept: */*
 # >
-# < HTTP/1.1 401 Unauthorized
-# < Set-Cookie: JSESSIONID=4C37481E1E5C3C37AEF09AD9442887DF; Path=/; HttpOnly;Secure
+#   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0< HTTP/1.1 200 OK
+# < Set-Cookie: JSESSIONID=1460AB6D7BDED090BEC1AAF63751AA35; Path=/; HttpOnly;Secure
 # < Set-Cookie: glide_user=; Max-Age=0; Expires=Thu, 01-Jan-1970 00:00:10 GMT; Path=/; HttpOnly;Secure
 # < Set-Cookie: glide_user_session=; Max-Age=0; Expires=Thu, 01-Jan-1970 00:00:10 GMT; Path=/; HttpOnly;Secure
-# < WWW-Authenticate: BASIC realm="Service-now"
+# < Set-Cookie: glide_user_route=glide.163db7335e7500df6267a1f3113f00cf; Max-Age=2147483647; Expires=Wed, 16-Mar-2089 17:03:55 GMT; Path=/; HttpOnly;Secure
+# < X-Is-Logged-In: true
+# < X-Transaction-ID: 79aee3f52fa2
+# < Set-Cookie: glide_session_store=35AEE3F52FA22010FC759BACF699B601; Max-Age=1800; Expires=Fri, 26-Feb-2021 14:19:48 GMT; Path=/; HttpOnly;Secure
+# < Link: <https://dev86373.service-now.com/api/now/table/incident?sysparm_limit=1&sysparm_offset=0>;rel="first",<https://dev86373.service-now.com/api/now/table/incident?sysparm_limit=1&sysparm_offset=-1>;rel="prev",<https://dev86373.service-now.com/api/now/table/incident?sysparm_limit=1&sysparm_offset=1>;rel="next",<https://dev86373.service-now.com/api/now/table/incident?sysparm_limit=1&sysparm_offset=72>;rel="last"
+# < X-Total-Count: 73
 # < Pragma: no-store,no-cache
 # < Cache-control: no-cache,no-store,must-revalidate,max-age=-1
 # < Expires: 0
 # < Content-Type: application/json;charset=UTF-8
 # < Transfer-Encoding: chunked
-# < Date: Fri, 26 Feb 2021 11:40:33 GMT
+# < Date: Fri, 26 Feb 2021 13:49:48 GMT
+# * Server ServiceNow is not blacklisted
 # < Server: ServiceNow
 # < Set-Cookie: BIGipServerpool_dev86373=2843957002.9537.0000; path=/; Httponly; Secure
 # < Strict-Transport-Security: max-age=63072000; includeSubDomains
-# < Connection: close
 # <
-# * Closing connection 0
-# * TLSv1.2 (OUT), TLS alert, close notify (256):
-# {"error":{"detail":"Required to provide Auth information","message":"User Not Authenticated"},"status":"failure"}
+# { [data not shown]
+# 100  3362    0  3362    0     0   5886      0 --:--:-- --:--:-- --:--:--  5877
+# * Connection #0 to host nginx_proxy left intact
 
 TODAY=$(date '+%Y-%m-%d')
 
