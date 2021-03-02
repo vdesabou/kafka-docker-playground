ad0a5920a18a54b56608cc0b5ab657cf944598a7
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-03-02 10:05:04 +0100

    can use any tag
    Former-commit-id: d5b7401b4e0658c8f9a6bdce7f53e7d19a5ab644 [formerly 40a22d0d736a7c28cfe0e76b43637a198c654f5d [formerly 6a4a6af24d5665e22424f238253b7db1dec80243]]
    Former-commit-id: 3ea979a58f7762010df57712bc21f7fdab6bf984
    Former-commit-id: 9445032e773e60081bd61e5d1b87f8b255d892a0

--- troubleshooting/admin-client-issue-when-broker-down/start.sh
+++ troubleshooting/admin-client-issue-when-broker-down/start.sh
@@ -4,10 +4,9 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-if [ ! -f ${DIR}/adminclient/target/adminclient-1.0.0-jar-with-dependencies.jar ]
+if [ ! -f ${DIR}/adminclient/target/adminclient-${TAG}-jar-with-dependencies.jar ]
 then
      log "Building jar for adminclient"
-     set -x
      docker run -i --rm -e TAG=$TAG_BASE -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/adminclient":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/adminclient/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
 fi
 
@@ -18,14 +17,14 @@ docker-compose up -d
 sleep 30
 
 log "run adminclient to check it works"
-docker exec adminclient bash -c "java -jar adminclient-1.0.0-jar-with-dependencies.jar"
+docker exec adminclient bash -c "java -jar adminclient-${TAG}-jar-with-dependencies.jar"
 
 
 log "Pausing broker 1"
 docker container pause broker1
 
 for((i=0;i<5;i++)); do
-  docker exec adminclient bash -c "java -jar adminclient-1.0.0-jar-with-dependencies.jar"
+  docker exec adminclient bash -c "java -jar adminclient-${TAG}-jar-with-dependencies.jar"
 done
 
 # FIXTHIS: 6.1.0
@@ -33,3 +32,10 @@ done
 # [2021-03-02 08:53:35,820] INFO [AdminClient clientId=adminclient-1] Metadata update failed (org.apache.kafka.clients.admin.internals.AdminMetadataManager)
 # org.apache.kafka.common.errors.TimeoutException: Call(callName=fetchMetadata, deadlineMs=1614675215819, tries=1, nextAllowedTryMs=1614675215920) timed out at 1614675215820 after 1 attempt(s)
 # Caused by: org.apache.kafka.common.errors.TimeoutException: Timed out waiting to send the call. Call: fetchMetadata
+
+# 5.5.3
+
+# [2021-03-02 09:00:36,983] INFO Kafka startTimeMs: 1614675636982 (org.apache.kafka.common.utils.AppInfoParser)
+# [2021-03-02 09:01:06,989] INFO [AdminClient clientId=adminclient-1] Metadata update failed (org.apache.kafka.clients.admin.internals.AdminMetadataManager)
+# org.apache.kafka.common.errors.TimeoutException: Call(callName=fetchMetadata, deadlineMs=1614675666986) timed out at 1614675666987 after 1 attempt(s)
+# Caused by: org.apache.kafka.common.errors.TimeoutException: Timed out waiting to send the call.
