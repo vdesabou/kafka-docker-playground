1477b441a5b03877211ba4107d4d8fcacad38994
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-03-02 09:54:33 +0100

    inc-rcca-2655-connect-worker-cannot-restart-when-one-of-their-3-brokers-is-down
    Former-commit-id: 672911d9d5a0d336550ea3c579cc21fdb4e68945 [formerly 93bc260ed1bc8e4312fab15f9051d6057cbed540 [formerly f9b2d08a952d7a908abe58d22016c82ae46a6f84]]
    Former-commit-id: 21b720aa7d3629206736077523150e4c09f15c71
    Former-commit-id: a8cc3bdd14ebe1d3e899f310554f67bcf19066b4

---
+++ troubleshooting/admin-client-issue-when-broker-down/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f ${DIR}/adminclient/target/adminclient-1.0.0-jar-with-dependencies.jar ]
+then
+     log "Building jar for adminclient"
+     set -x
+     docker run -i --rm -e TAG=$TAG_BASE -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -v "${DIR}/adminclient":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/adminclient/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
+fi
+
+docker-compose down -v --remove-orphans
+docker-compose build
+docker-compose up -d
+
+sleep 30
+
+log "run adminclient to check it works"
+docker exec adminclient bash -c "java -jar adminclient-1.0.0-jar-with-dependencies.jar"
+
+
+log "Pausing broker 1"
+docker container pause broker1
+
+for((i=0;i<5;i++)); do
+  docker exec adminclient bash -c "java -jar adminclient-1.0.0-jar-with-dependencies.jar"
+done
+
+# FIXTHIS: 6.1.0
+
+# [2021-03-02 08:53:35,820] INFO [AdminClient clientId=adminclient-1] Metadata update failed (org.apache.kafka.clients.admin.internals.AdminMetadataManager)
+# org.apache.kafka.common.errors.TimeoutException: Call(callName=fetchMetadata, deadlineMs=1614675215819, tries=1, nextAllowedTryMs=1614675215920) timed out at 1614675215820 after 1 attempt(s)
+# Caused by: org.apache.kafka.common.errors.TimeoutException: Timed out waiting to send the call. Call: fetchMetadata
