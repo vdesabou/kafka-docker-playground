a7b8f7e5bd048729c40e3d49cf9eb7a8a7e5b193
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-03-07 15:46:42 +0100

    adding example with SFTP key auth
    Former-commit-id: caaa0f4baca1376a37da3c5f27eef73cda2552d6 [formerly 9d1d6edef218105cc395e73de62fac84b83b24f4 [formerly e95b1ee483724006ff97d8678fea54976833c078]]
    Former-commit-id: 2a48d5a6f92ceb162b5bccc4f9d3ea7cfebb4420
    Former-commit-id: 1bdf480deaa1c52f0456269033850923202dae79

---
+++ connect/connect-sftp-sink/sftp-sink-ssh-key.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+log "generate ssh host key"
+rm -f ssh_host*
+ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -P "mypassword"
+
+# needs to be a RSA key
+ssh-keygen -p -f ssh_host_rsa_key -m pem -P mypassword -N mypassword -b 2048
+
+RSA_PUBLIC_KEY=$(cat ssh_host_rsa_key.pub)
+RSA_PRIVATE_KEY=$(awk '{printf "%s\\r\\n", $0}' ssh_host_rsa_key)
+
+log "RSA_PUBLIC_KEY=$RSA_PUBLIC_KEY"
+log "RSA_PRIVATE_KEY=$RSA_PRIVATE_KEY"
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Creating SFTP Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+        "topics": "test_sftp_sink",
+               "tasks.max": "1",
+               "connector.class": "io.confluent.connect.sftp.SftpSinkConnector",
+               "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
+               "schema.generator.class": "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator",
+               "flush.size": "3",
+               "schema.compatibility": "NONE",
+               "format.class": "io.confluent.connect.sftp.sink.format.csv.CsvFormat",
+               "storage.class": "io.confluent.connect.sftp.sink.storage.SftpSinkStorage",
+               "sftp.host": "sftp-server",
+               "sftp.port": "22",
+               "sftp.username": "foo",
+               "sftp.password": "",
+               "tls.private.key": "'"$RSA_PRIVATE_KEY"'",
+               "tls.public.key": "'"$RSA_PUBLIC_KEY"'",
+               "tls.passphrase": "mypassword",
+               "sftp.working.dir": "/upload",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/sftp-sink/config | jq .
+
+
+log "Sending messages to topic test_sftp_sink"
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_sftp_sink --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+sleep 10
+
+log "Listing content of ./upload/topics/test_sftp_sink/partition\=0/"
+docker exec sftp-server bash -c "ls /home/foo/upload/topics/test_sftp_sink/partition\=0/"
+
+docker cp sftp-server:/home/foo/upload/topics/test_sftp_sink/partition\=0/test_sftp_sink+0+0000000000.csv  /tmp/
+
+docker run -v /tmp:/tmp actions/ -tools tojson /tmp/test_sftp_sink+0+0000000000.csv
