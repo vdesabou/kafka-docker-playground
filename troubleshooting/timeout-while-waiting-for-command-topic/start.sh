6be9410d8a527d7e659d39c97bed3ede23c8129e
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-03-12 14:34:58 +0100

    update
    Former-commit-id: 16f15db8a9bcee3515b9b37a7f147ccd4b8c1dbe [formerly cb04264f06d910d33eb302c46028fa1c4c329527 [formerly 4da543a8d132f52a121e01441f0705a6636d6241]]
    Former-commit-id: 8e858ef8e205b4fb50efe9e08c15a3a8a26b59d7
    Former-commit-id: d705cc560150224e5bb31bc6d2acb3e5bd143b15

--- troubleshooting/timeout-while-waiting-for-command-topic/start.sh
+++ troubleshooting/timeout-while-waiting-for-command-topic/start.sh
@@ -1,236 +1,68 @@
 #!/bin/bash
 set -e
 
+# https://github.com/confluentinc/ksql/issues/5503
+export TAG=6.0.0
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+${DIR}/../../environment/plaintext/start.sh
 
-function wait_for_stream_to_finish () {
-  stream="$1"
+# docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic users << EOF
+# {"ROWKEY": "foo", "namesp":"n1", "email":"email@dot.org"}
+# EOF
 
-  set +e
-  MAX_WAIT=3600
-  CUR_WAIT=0
-  nb_streams_finished=0
-  while [[ ! "${nb_streams_finished}" = "1" ]]
-  do
-    throughput=$(curl -s -X "POST" "http://localhost:8088/ksql" \
-        -H "Accept: application/vnd.ksql.v1+json" \
-        -d $"{
-      "ksql": "DESCRIBE EXTENDED ${stream};",
-      "streamsProperties": {}
-    }" | jq -r '.[].sourceDescription.statistics' | grep -Eo '(^|\s)messages-per-sec:\s*\d*\.*\d*' | cut -d":" -f 2 | sed 's/ //g')
-    if [ "$throughput" = "0" ]
-    then
-      let "nb_streams_finished++"
-    elif [ "$throughput" = "" ]
-    then
-      log "⚠️ Stream $stream has not started to process messages"
-      continue
-    else
-      log "⏳ Stream $stream currently processing $throughput messages-per-sec"
-    fi
+# docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic pageviews << EOF
+# {"userid": "foo", "namesp":"n1", "pageid":"1"}
+# EOF
 
-    sleep 5
-    CUR_WAIT=$(( CUR_WAIT+5 ))
-    if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
-      logerror "❗❗❗ ERROR: Please troubleshoot"
-      exit 1
-    fi
-  done
-  set -e
-}
+log "Create stream and table"
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
 
-function throughtput () {
-  stream="$1"
-  duration="$2"
+create table users (
+    rowkey varchar primary key,
+    namesp varchar,
+    email varchar
+  ) with (
+    kafka_topic='users',
+    value_format='json',partitions=1
+  );
 
-  MAX_WAIT=600
-  CUR_WAIT=0
-  totalmessages=""
-  while [[ "${totalmessages}" = "" ]]
-  do
-    totalmessages=$(curl -s -X "POST" "http://localhost:8088/ksql" \
-        -H "Accept: application/vnd.ksql.v1+json" \
-        -d $"{
-      "ksql": "DESCRIBE EXTENDED $stream;",
-      "streamsProperties": {}
-    }" | jq -r '.[].sourceDescription.statistics' | grep -Eo '(^|\s)total-messages:\s*\d*\.*\d*' | cut -d":" -f 2 | sed 's/ //g')
+  create stream pageviews (
+    userid varchar,
+    namesp varchar,
+    pageid varchar
+  ) with (
+    kafka_topic='pageviews',
+    value_format='json',partitions=1
+  );
 
-    sleep 5
-    CUR_WAIT=$(( CUR_WAIT+5 ))
-    if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
-      logerror "❗❗❗ ERROR: Please troubleshoot"
-      exit 1
-    fi
-  done
-
-  throughput=$(echo $((totalmessages / duration)))
-  log "🚀 Stream $stream has processed $totalmessages messages. Took $duration seconds. Throughput=$throughput msg/s"
-}
-
-NOW="$(date +%s)000"
-sed -e "s|:NOW:|$NOW|g" \
-    ${DIR}/schemas/orders-template.avro > ${DIR}/schemas/orders.avro
-sed -e "s|:NOW:|$NOW|g" \
-    ${DIR}/schemas/shipments-template.avro > ${DIR}/schemas/shipments.avro
-
-${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
-
-log "Create topic orders"
-curl -s -X PUT \
-      -H "Content-Type: application/json" \
-      --data '{
-                "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
-                "kafka.topic": "orders",
-                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-                "value.converter.schemas.enable": "false",
-                "max.interval": 1,
-                "iterations": "1000",
-                "tasks.max": "10",
-                "schema.filename" : "/tmp/schemas/orders.avro",
-                "schema.keyfield" : "orderid"
-            }' \
-      http://localhost:8083/connectors/datagen-orders/config | jq
-
-wait_for_datagen_connector_to_inject_data "orders" "10"
-
-log "Create topic shipments"
-curl -s -X PUT \
-      -H "Content-Type: application/json" \
-      --data '{
-                "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
-                "kafka.topic": "shipments",
-                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-                "value.converter.schemas.enable": "false",
-                "max.interval": 1,
-                "iterations": "1000",
-                "tasks.max": "10",
-                "schema.filename" : "/tmp/schemas/shipments.avro"
-            }' \
-      http://localhost:8083/connectors/datagen-shipments/config | jq
-
-wait_for_datagen_connector_to_inject_data "shipments" "10"
-
-log "Create topic products"
-curl -s -X PUT \
-      -H "Content-Type: application/json" \
-      --data '{
-                "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
-                "kafka.topic": "products",
-                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-                "value.converter.schemas.enable": "false",
-                "max.interval": 1,
-                "iterations": "100",
-                "tasks.max": "10",
-                "schema.filename" : "/tmp/schemas/products.avro",
-                "schema.keyfield" : "productid"
-            }' \
-      http://localhost:8083/connectors/datagen-products/config | jq
-
-wait_for_datagen_connector_to_inject_data "products" "10"
-
-log "Create topic customers"
-curl -s -X PUT \
-      -H "Content-Type: application/json" \
-      --data '{
-                "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
-                "kafka.topic": "customers",
-                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-                "value.converter.schemas.enable": "false",
-                "max.interval": 1,
-                "iterations": "1000",
-                "tasks.max": "10",
-                "schema.filename" : "/tmp/schemas/customers.avro",
-                "schema.keyfield" : "customerid"
-            }' \
-      http://localhost:8083/connectors/datagen-customers/config | jq
-
-wait_for_datagen_connector_to_inject_data "customers" "10"
-
-for((i=0;i<10;i++))
-do
-  ./create-queries.sh
-  sleep 5
-  ./cleanup-queries.sh
-  while [ $? -ne 0 ]
-  do
-      sleep 10
-      logerror "Retrying ./cleanup-queries.sh"
-      ./cleanup-queries.sh
-  done
-done
+INSERT INTO users (rowkey,namesp,email) VALUES ('foo', 'n1', 'email');
+INSERT INTO pageviews (userid,namesp,pageid) VALUES ('foo', 'n1', '1');
+EOF
 
 
+log "Problematic join"
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
 
-# 01:48:26 Cannot terminate all queries, check the errors below:
-# [
-#   {
-#     "@type": "currentStatus",
-#     "statementText": "TERMINATE CSAS_ENRICHED_O_C_105;",
-#     "commandId": "terminate/CSAS_ENRICHED_O_C_105/execute",
-#     "commandStatus": {
-#       "status": "SUCCESS",
-#       "message": "Query terminated.",
-#       "queryId": null
-#     },
-#     "commandSequenceNumber": 114,
-#     "warnings": []
-#   }
-# ]
-# [
-#   {
-#     "@type": "currentStatus",
-#     "statementText": "TERMINATE CSAS_FILTERED_STREAM_103;",
-#     "commandId": "terminate/CSAS_FILTERED_STREAM_103/execute",
-#     "commandStatus": {
-#       "status": "SUCCESS",
-#       "message": "Query terminated.",
-#       "queryId": null
-#     },
-#     "commandSequenceNumber": 116,
-#     "warnings": []
-#   }
-# ]
-# [
-#   {
-#     "@type": "currentStatus",
-#     "statementText": "TERMINATE CSAS_ENRICHED_O_C_P_107;",
-#     "commandId": "terminate/CSAS_ENRICHED_O_C_P_107/execute",
-#     "commandStatus": {
-#       "status": "SUCCESS",
-#       "message": "Query terminated.",
-#       "queryId": null
-#     },
-#     "commandSequenceNumber": 118,
-#     "warnings": []
-#   }
-# ]
-# [
-#   {
-#     "@type": "currentStatus",
-#     "statementText": "TERMINATE CTAS_ORDERPER_PROD_CUST_AGG_111;",
-#     "commandId": "terminate/CTAS_ORDERPER_PROD_CUST_AGG_111/execute",
-#     "commandStatus": {
-#       "status": "EXECUTING",
-#       "message": "Executing statement",
-#       "queryId": null
-#     },
-#     "commandSequenceNumber": 120,
-#     "warnings": []
-#   }
-# ]
-# {
-#   "@type": "statement_error",
-#   "error_code": 50000,
-#   "message": "Could not write the statement 'TERMINATE CSAS_ORDERS_SHIPPED_109;' into the command topic.\nCaused by: Timeout while waiting for command topic consumer to process command\n\ttopic",
-#   "statementText": "TERMINATE CSAS_ORDERS_SHIPPED_109;",
-#   "entities": []
-# }
+SET 'auto.offset.reset' = 'earliest';
 
-# [2021-02-25 00:48:20,574] WARN Unregistering query that has not terminated. This may happen when streams threads are hung. State: RUNNING (io.confluent.ksql.engine.EngineContext:264)
-# [2021-02-25 00:48:25,712] INFO Processed unsuccessfully: KsqlRequest{ksql='TERMINATE CSAS_ORDERS_SHIPPED_109;', configOverrides={}, requestProperties={}, commandSequenceNumber=Optional.empty}, reason: Could not write the statement 'TERMINATE CSAS_ORDERS_SHIPPED_109;' into the command topic. (io.confluent.ksql.rest.server.resources.KsqlResource:301)
+create stream joined as
+  select
+    p.*
+  from pageviews p
+    join users u on p.userid=u.rowkey
+  where p.namesp='n1'
+  emit changes;
+EOF
+
+docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic users --from-beginning --max-messages 1
+docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic pageviews --from-beginning --max-messages 1
+docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic JOINED --from-beginning --max-messages 1
+
+log "Try to create another stream"
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+
+create table users2 (rowkey varchar primary key, namesp varchar, email varchar) with (kafka_topic='users',value_format='json');
+EOF
