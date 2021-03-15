5f3b851d5c02aeb56cde27a2910ed230f397a60c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-03-15 11:19:13 +0100

    Updated as per https://kafka-tutorials.confluent.io/handling-deserialization-errors/ksql.html
    Former-commit-id: 74155593795e0a046fbff43e2fad307df734af71 [formerly 509d650be28a0c4263109ad3b50f0504f7fd892b [formerly 8fbf663108e9410fad494b19fe768d438747a00f]]
    Former-commit-id: 2720fdf115c70492624b4742083d2f29c4c49273
    Former-commit-id: 9874354c50f5767eb2b9dd9b4a07be3e37fd7c0e

--- troubleshooting/timeout-while-waiting-for-command-topic/start.sh
+++ troubleshooting/timeout-while-waiting-for-command-topic/start.sh
@@ -9,60 +9,52 @@ source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../environment/plaintext/start.sh
 
-# docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic users << EOF
-# {"ROWKEY": "foo", "namesp":"n1", "email":"email@dot.org"}
-# EOF
-
-# docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic pageviews << EOF
-# {"userid": "foo", "namesp":"n1", "pageid":"1"}
-# EOF
-
-log "Create stream and table"
-timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
-
-create table users (
-    rowkey varchar primary key,
-    namesp varchar,
-    email varchar
-  ) with (
-    kafka_topic='users',
-    value_format='json',partitions=1
-  );
-
-  create stream pageviews (
-    userid varchar,
-    namesp varchar,
-    pageid varchar
-  ) with (
-    kafka_topic='pageviews',
-    value_format='json',partitions=1
-  );
-
-INSERT INTO users (rowkey,namesp,email) VALUES ('foo', 'n1', 'email');
-INSERT INTO pageviews (userid,namesp,pageid) VALUES ('foo', 'n1', '1');
+# https://kafka-tutorials.confluent.io/handling-deserialization-errors/ksql.html
+docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic SENSORS_RAW << EOF
+{"id": "e7f45046-ad13-404c-995e-1eca16742801", "timestamp": "2020-01-15 02:20:30", "enabled": true}
+{"id": "835226cf-caf6-4c91-a046-359f1d3a6e2e", "timestamp": "2020-01-15 02:25:30", "enabled": true}
+{"id": "1a076a64-4a84-40cb-a2e8-2190f3b37465", "timestamp": "2020-01-15 02:30:30", "enabled": "true"}
 EOF
 
 
-log "Problematic join"
+log "Create streams"
+timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+
+CREATE STREAM SENSORS_RAW (id VARCHAR, timestamp VARCHAR, enabled BOOLEAN)
+    WITH (KAFKA_TOPIC = 'SENSORS_RAW',
+          VALUE_FORMAT = 'JSON',
+          TIMESTAMP = 'TIMESTAMP',
+          TIMESTAMP_FORMAT = 'yyyy-MM-dd HH:mm:ss',
+          PARTITIONS = 1,
+          REPLICAS=1);
+
+CREATE STREAM SENSORS AS
+    SELECT
+        ID, TIMESTAMP, ENABLED
+    FROM SENSORS_RAW
+    PARTITION BY ID;
+EOF
+
+
+log "Check Stream"
 timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
 
 SET 'auto.offset.reset' = 'earliest';
 
-create stream joined as
-  select
-    p.*
-  from pageviews p
-    join users u on p.userid=u.rowkey
-  where p.namesp='n1'
-  emit changes;
-EOF
+SELECT
+    ID,
+    TIMESTAMP,
+    ENABLED
+FROM SENSORS EMIT CHANGES LIMIT 2;
 
-docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic users --from-beginning --max-messages 1
-docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic pageviews --from-beginning --max-messages 1
-docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic JOINED --from-beginning --max-messages 1
 
-log "Try to create another stream"
-timeout 120 docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+SELECT
+    message->deserializationError->errorMessage,
+    encode(message->deserializationError->RECORDB64, 'base64', 'utf8') AS MSG,
+    message->deserializationError->cause
+  FROM KSQL_PROCESSING_LOG
+  EMIT CHANGES
+  LIMIT 1;
 
-create table users2 (rowkey varchar primary key, namesp varchar, email varchar) with (kafka_topic='users',value_format='json');
+PRINT ksql_processing_log FROM BEGINNING LIMIT 1;
 EOF
