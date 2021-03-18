d4652e198fecdb3331cd6f708dd78991fa399edb
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-03-18 17:41:01 +0100

    example with localstack
    Former-commit-id: 1f765f9b7b51c9ce5f3260c717032fed166e146c [formerly 65b8d02567c09db36c5c9968c8c869565d86b03a [formerly bcfe02e1e24aaf8aaa215c6a57e3ebe156ff1710]]
    Former-commit-id: 340fe069f0b85e16e7545c07661f3cd61c6344a3
    Former-commit-id: 0bcb7e8af954440d5e773e53da6d3fb27f734481

---
+++ connect/connect-aws-kinesis-source/kinesis-repro-localstack.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+# This is required even though we use a local instance !
+if [ ! -f $HOME/.aws/config ]
+then
+     logerror "ERROR: $HOME/.aws/config is not set"
+     exit 1
+fi
+if [ ! -f $HOME/.aws/credentials ]
+then
+     logerror "ERROR: $HOME/.aws/credentials is not set"
+     exit 1
+fi
+
+# rm -f *.jks
+# # Create truststore and import the CA cert
+# docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG}  keytool -noprompt -keystore /tmp/my.truststore.jks -alias CARoot -import -file /tmp/server-crt.pem -storepass confluent -keypass confluent
+# docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -noprompt -destkeystore /tmp/my.truststore.jks -importkeystore -srckeystore /usr/lib/jvm/zulu11-ca/lib/security/cacerts -srcstorepass changeit -deststorepass confluent
+
+
+# docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} openssl pkcs12 -export -in /tmp/server-crt.pem -inkey /tmp/server-key.pem \
+#                -out /tmp/kinesis-server.p12 -name kinesis-server \
+#                -CAfile /tmp/ca-crt.pem -caname CARoot -passout pass:confluent
+
+# docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -importkeystore \
+#         -deststorepass confluent -destkeypass confluent -destkeystore /tmp/my.trustore.jks \
+#         -srckeystore /tmp/kinesis-server.p12 -srcstoretype PKCS12 -srcstorepass confluent \
+#         -alias kinesis-server
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext-repro-localstack.yml"
+
+log "Create a Kinesis stream my_kinesis_stream"
+/usr/local/bin/aws kinesis --endpoint-url https://localhost:4566/ create-stream --stream-name my_kinesis_stream --shard-count 1 --no-verify-ssl
+
+log "Sleep 10 seconds to let the Kinesis stream being fully started"
+sleep 10
+
+TODAY=$(date +%s)
+log "Creating Kinesis Source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class":"io.confluent.connect.kinesis.KinesisSourceConnector",
+               "tasks.max": "1",
+               "kafka.topic": "kinesis_topic",
+               "kinesis.base.url": "https://kinesis-local:4566",
+               "kinesis.stream": "my_kinesis_stream",
+               "kinesis.region": "eu-west-3",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/kinesis-source-local/config | jq .
+
+log "Insert records in Kinesis stream"
+# The example shows that a record containing partition key 123 and data "test-message-1" is inserted into my_kinesis_stream.
+/usr/local/bin/aws kinesis --endpoint-url https://localhost:4566/ put-record --stream-name my_kinesis_stream --partition-key 123 --data test-message-1 --no-verify-ssl
+
+sleep 10
+
+log "Verify we have received the data in kinesis_topic topic"
+timeout 60 docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic kinesis_topic --from-beginning --max-messages 1
+
+log "Delete the stream"
+/usr/local/bin/aws kinesis --endpoint-url https://localhost:4566/ delete-stream --stream-name my_kinesis_stream --no-verify-ssl
