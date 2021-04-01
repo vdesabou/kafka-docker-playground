8ce6bf4a989459262e78cebcbb2ce2ba43670f9f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-04-02 00:04:17 +0200

    wip

--- troubleshooting/forward-compatibility-and-specific-records/start.sh
+++ troubleshooting/forward-compatibility-and-specific-records/start.sh
@@ -15,6 +15,8 @@ done
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.yml"
 
+docker exec broker kafka-topics --create --topic customer-avro --partitions 1 --replication-factor 1 --zookeeper zookeeper:2181
+
 log "Set global compatibility mode to FORWARD"
 curl --request PUT \
   --url http://localhost:8081/config \
@@ -27,19 +29,26 @@ log "Verify global compatibility mode"
 curl --request GET \
   --url http://localhost:8081/config
 
-# log "Run the Java consumer. Logs are in consumer.log."
-# docker exec consumer-v1 bash -c "java -jar consumer-v1-1.0.0-jar-with-dependencies.jar" > consumer.log 2>&1 &
+log "Run the Java consumer. Logs are in consumer.log."
+docker exec consumer-v1 bash -c "java -jar consumer-v1-1.0.0-jar-with-dependencies.jar" > consumer.log 2>&1 &
+
+sleep 15
 
 log "Run the Java producer-v1"
 docker exec producer-v1 bash -c "java -jar producer-v1-1.0.0-jar-with-dependencies.jar"
 
-docker exec consumer-v1 bash -c "java -jar consumer-v1-1.0.0-jar-with-dependencies.jar"
-
 log "Run the Java producer-v2, it adds a mandatory field country"
 docker exec producer-v2 bash -c "java -jar producer-v2-1.0.0-jar-with-dependencies.jar"
 
+log "stop the consumer-v1"
+docker restart consumer-v1
 
+sleep 5
 
+log "reset offset for consumer-v1"
 docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group customer-avro-app --describe
-docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group customer-avro-app --to-earliest --topic customer --reset-offsets --dry-run
-docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group customer-avro-app --to-earliest --topic customer --reset-offsets --execute
+docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group customer-avro-app --to-earliest --topic customer-avro --reset-offsets --dry-run
+docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group customer-avro-app --to-earliest --topic customer-avro --reset-offsets --execute
+
+log "Run the Java consumer-v1"
+docker exec consumer-v1 bash -c "java -jar consumer-v1-1.0.0-jar-with-dependencies.jar" > consumer_after_reset.log 2>&1 &
