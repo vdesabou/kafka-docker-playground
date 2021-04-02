68b4b0f92f915855fab530d73ec0251b220f1227
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-04-02 17:33:55 +0200

    Add MYSQL JDBC Source and Sink example with SSL (SSL encryption only + mTLS) #813

--- connect/connect-jdbc-mysql-source/README.md
+++ connect/connect-jdbc-mysql-source/README.md
@@ -11,11 +11,114 @@ Quickly test [JDBC Source](https://docs.confluent.io/current/connect/kafka-conne
 
 ## How to run
 
-Simply run:
+Without SSL:
 
 ```
 $ ./mysql.sh
 ```
 
+with SSL encryption:
+
+```
+$ ./mysql-ssl.sh
+```
+
+with SSL encryption + Mutual TLS authentication:
+
+```
+$ ./mysql-mtls.sh
+```
+
+### Without SSL
+
+Creating MySQL sink connector
+
+```bash
+$ curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class":"io.confluent.connect.jdbc.JdbcSourceConnector",
+                    "tasks.max":"10",
+                    "connection.url":"jdbc:mysql://mysql:3306/db?user=user&password=password&useSSL=false",
+                    "table.whitelist":"application",
+                    "mode":"timestamp+incrementing",
+                    "timestamp.column.name":"last_modified",
+                    "incrementing.column.name":"id",
+                    "topic.prefix":"mysql-"
+          }' \
+     http://localhost:8083/connectors/mysql-source/config | jq .
+```
+
+Sending messages to topic orders
+
+```bash
+$ docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic mysql-application --from-beginning --max-messages 2
+```
+
+Results:
+
+```json
+{"id":1,"name":"kafka","team_email":"kafka@apache.org","last_modified":1617377438000}
+{"id":2,"name":"another","team_email":"another@apache.org","last_modified":1617377478000}
+```
+### With SSL
+
+`mysql` container is started first in order to get generated `.pem` certificates from `/var/lib/mysql`
+
+`keystore.jks` and `truststore.jks` are generated from these `.pem` certificates (see explanations [here](https://dev.mysql.com/doc/connector-j/5.1/en/connector-j-reference-using-ssl.html)):
+
+```
+keytool -importcert -alias MySQLCACert -noprompt -file /tmp/ca.pem -keystore /tmp/truststore.jks -storepass mypassword
+# Convert the client key and certificate files to a PKCS #12 archive
+openssl pkcs12 -export -in /tmp/client-cert.pem -inkey /tmp/client-key.pem -name "mysqlclient" -passout pass:mypassword -out /tmp/client-keystore.p12
+# Import the client key and certificate into a Java keystore:
+eytool -importkeystore -srckeystore /tmp/client-keystore.p12 -srcstoretype pkcs12 -srcstorepass mypassword -destkeystore /tmp/keystore.jks -deststoretype JKS -deststorepass mypassword
+```
+
+#### With SSL encryption
+
+We use db user `userssl` to be sure SSL encryption is required:
+
+```sql
+-- used for ssl case
+GRANT ALL PRIVILEGES ON *.* TO 'userssl'@'%' IDENTIFIED BY 'password' REQUIRE SSL;
+```
+
+`connect` container is configured with truststore:
+
+```
+      KAFKA_OPTS: -Djavax.net.ssl.trustStore=/etc/kafka/secrets/truststore.jks
+                  -Djavax.net.ssl.trustStorePassword=mypassword
+```
+
+connection.url is:
+
+```json
+"connection.url": "jdbc:mysql://mysql:3306/db?user=userssl&password=password&verifyServerCertificate=true&useSSL=true&requireSSL=true"
+```
+
+#### With SSL encryption + Mutual TLS auth
+
+We use db user `usermtls` to be sure client certificate is required:
+
+```sql
+-- used for mtls case
+GRANT ALL PRIVILEGES ON *.* TO 'usermtls'@'%' IDENTIFIED BY 'password' REQUIRE X509;
+```
+
+`connect` container is configured with truststore **and** keystore:
+
+```
+      KAFKA_OPTS: -Djavax.net.ssl.trustStore=/etc/kafka/secrets/truststore.jks
+                  -Djavax.net.ssl.trustStorePassword=mypassword
+                  -Djavax.net.ssl.keyStore=/etc/kafka/secrets/keystore.jks
+                  -Djavax.net.ssl.keyStorePassword=mypassword
+```
+
+connection.url is:
+
+```json
+"connection.url": "jdbc:mysql://mysql:3306/db?user=usermtls&password=password&verifyServerCertificate=true&useSSL=true&requireSSL=true"
+```
 
 N.B: Control Center is reachable at [http://127.0.0.1:9021](http://127.0.0.1:9021])
---
+++ connect/connect-jdbc-mysql-source/mysql-ssl.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f ${DIR}/mysql-connector-java-5.1.45.jar ]
+then
+     log "Downloading mysql-connector-java-5.1.45.jar"
+     wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/5.1.45/mysql-connector-java-5.1.45.jar
+fi
+
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/docker-compose.plaintext-ssl.yml" down -v --remove-orphans
+log "Starting up mysql container to get generated certs from /var/lib/mysql"
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/docker-compose.plaintext-ssl.yml" up -d mysql
+
+sleep 5
+
+log "Getting certs from mysql container and transform them to JKS"
+mkdir -p ${PWD}/security/
+rm -rf ${PWD}/security/*
+# https://dev.mysql.com/doc/connector-j/5.1/en/connector-j-reference-using-ssl.html
+docker cp mysql:/var/lib/mysql/ca.pem ${PWD}/security/
+docker cp mysql:/var/lib/mysql/client-key.pem ${PWD}/security/
+docker cp mysql:/var/lib/mysql/client-cert.pem ${PWD}/security/
+
+log "Creating JKS from pem files"
+cd ${PWD}/security/
+docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -importcert -alias MySQLCACert -noprompt -file /tmp/ca.pem -keystore /tmp/truststore.jks -storepass mypassword
+# Convert the client key and certificate files to a PKCS #12 archive
+docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} openssl pkcs12 -export -in /tmp/client-cert.pem -inkey /tmp/client-key.pem -name "mysqlclient" -passout pass:mypassword -out /tmp/client-keystore.p12
+# Import the client key and certificate into a Java keystore:
+docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -importkeystore -srckeystore /tmp/client-keystore.p12 -srcstoretype pkcs12 -srcstorepass mypassword -destkeystore /tmp/keystore.jks -deststoretype JKS -deststorepass mypassword
+cd -
+
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/docker-compose.plaintext-ssl.yml" up -d
+../../scripts/wait-for-connect-and-controlcenter.sh
+
+
+log "Describing the application table in DB 'db':"
+docker exec mysql bash -c "mysql --user=usermtls --password=password --ssl-mode=VERIFY_CA --ssl-ca=/var/lib/mysql/ca.pem --ssl-cert=/var/lib/mysql/client-cert.pem --ssl-key=/var/lib/mysql/client-key.pem --database=db -e 'describe application'"
+
+log "Show content of application table:"
+docker exec mysql bash -c "mysql --user=usermtls --password=password --ssl-mode=VERIFY_CA --ssl-ca=/var/lib/mysql/ca.pem --ssl-cert=/var/lib/mysql/client-cert.pem --ssl-key=/var/lib/mysql/client-key.pem --database=db -e 'select * from application'"
+
+log "Adding an element to the table"
+docker exec mysql mysql --user=usermtls --password=password --ssl-mode=VERIFY_CA --ssl-ca=/var/lib/mysql/ca.pem --ssl-cert=/var/lib/mysql/client-cert.pem --ssl-key=/var/lib/mysql/client-key.pem --database=db -e "
+INSERT INTO application (   \
+  id,   \
+  name, \
+  team_email,   \
+  last_modified \
+) VALUES (  \
+  2,    \
+  'another',  \
+  'another@apache.org',   \
+  NOW() \
+); "
+
+log "Show content of application table:"
+docker exec mysql bash -c "mysql --user=usermtls --password=password --ssl-mode=VERIFY_CA --ssl-ca=/var/lib/mysql/ca.pem --ssl-cert=/var/lib/mysql/client-cert.pem --ssl-key=/var/lib/mysql/client-key.pem --database=db -e 'select * from application'"
+
+log "Creating MySQL source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class":"io.confluent.connect.jdbc.JdbcSourceConnector",
+               "tasks.max":"10",
+               "connection.url": "jdbc:mysql://mysql:3306/db?user=usermtls&password=password&verifyServerCertificate=true&useSSL=true&requireSSL=true",
+               "table.whitelist":"application",
+               "mode":"timestamp+incrementing",
+               "timestamp.column.name":"last_modified",
+               "incrementing.column.name":"id",
+               "topic.prefix":"mysql-"
+          }' \
+     http://localhost:8083/connectors/mysql-source/config | jq .
+
+sleep 5
+
+log "Verifying topic mysql-application"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic mysql-application --from-beginning --max-messages 2
+
+
