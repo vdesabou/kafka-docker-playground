69a1f6bbb9f1bc1ee0445c5af74f75805775fc39
Daniel Petisme <daniel.petisme@gmail.com>
2021-04-06 23:17:37 +0200

    Add USE_SPECIFIC_AVRO property

--- troubleshooting/forward-compatibility-and-specific-records/start.sh
+++ troubleshooting/forward-compatibility-and-specific-records/start.sh
@@ -4,7 +4,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-for component in producer-v1 producer-v2 consumer-v1
+for component in producer-v1 producer-v2 consumer-v1 consumer-v2
 do
      if [ ! -f ${DIR}/${component}/target/${component}-1.0.0-jar-with-dependencies.jar ]
      then
@@ -40,15 +40,17 @@ docker exec producer-v1 bash -c "java -jar producer-v1-1.0.0-jar-with-dependenci
 log "Run the Java producer-v2, it adds a mandatory field country"
 docker exec producer-v2 bash -c "java -jar producer-v2-1.0.0-jar-with-dependencies.jar"
 
-log "stop the consumer-v1"
-docker restart consumer-v1
+log "Stop the consumer-v1"
+docker stop consumer-v1
 
 sleep 5
 
-log "reset offset for consumer-v1"
+log "Reset offset for customer-avro-app"
+
 docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group customer-avro-app --describe
 docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group customer-avro-app --to-earliest --topic customer-avro --reset-offsets --dry-run
 docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group customer-avro-app --to-earliest --topic customer-avro --reset-offsets --execute
+docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group customer-avro-app --describe
 
-log "Run the Java consumer-v1"
-docker exec consumer-v1 bash -c "java -jar consumer-v1-1.0.0-jar-with-dependencies.jar" > consumer_after_reset.log 2>&1 &
+log "Run the Java consumer-v2"
+docker exec consumer-v2 bash -c "java -jar consumer-v2-1.0.0-jar-with-dependencies.jar" > consumer_after_reset.log 2>&1 &
