404e86ae3b5daf8284a07970348153def46ccdd5
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-04-15 22:39:10 +0200

    align with mtls

--- connect/connect-jdbc-postgresql-source/postgres-ssl.sh
+++ connect/connect-jdbc-postgresql-source/postgres-ssl.sh
@@ -9,45 +9,17 @@ cd ${DIR}/ssl
 rm -f server.crt
 rm -f server.csr
 rm -f server.key
-rm -f rootCA.crt
-rm -f rootCA.csr
-rm -f rootCA.key
-rm -f rootCA.srl
+rm -f ca.crt
+rm -f ca.key
 
-# generate a key for our root CA certificate
-log "Generating key for root CA certificate"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl genrsa -des3 -passout pass:confluent -out /tmp/rootCA.pass.key 2048
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl rsa -passin pass:confluent -in /tmp/rootCA.pass.key -out /tmp/rootCA.key
-rm rootCA.pass.key
+#https://blog.crunchydata.com/blog/ssl-certificate-authentication-postgresql-docker-containers
+log " Creating a Root Certificate Authority (CA)"
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl req -new -x509 -days 365 -nodes -out /tmp/ca.crt -keyout /tmp/ca.key -subj "/CN=root-ca"
 
-# create and self sign the root CA certificate
-log "Creating self-signed root CA certificate"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl req -x509 -new -nodes -key /tmp/rootCA.key -sha256 -days 1024 -out  /tmp/rootCA.crt -subj "/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US"
-log "Self-signed root CA certificate (rootCA.crt) is:"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl x509 -in  /tmp/rootCA.crt -text -noout
-
-# generate a key for our server certificate
-log "Generating key for server certificate"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl genrsa -des3 -passout pass:confluent -out  /tmp/server.pass.key 2048
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl rsa -passin pass:confluent -in  /tmp/server.pass.key -out  /tmp/server.key
-rm server.pass.key
-
-# create a certificate request for our server. This includes a subject alternative name so either localhost or postgres can be used to address it
-log "Creating server certificate"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl req -new -key  /tmp/server.key -out  /tmp/server.csr -subj "/CN=postgres/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US" -addext "subjectAltName=DNS:postgres,DNS:localhost"
-log "Server certificate signing request (server.csr) is:"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl req -verify -in /tmp/server.csr -text -noout
-
-# use our CA certificate and key to create a signed version of the server certificate
-log "Signing server certificate using our root CA certificate and key"
-cat << EOF > extfile
-[SAN]
-subjectAltName=DNS:postgres,DNS:localhost
-EOF
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl x509 -req -sha256 -days 365 -in /tmp/server.csr -CA /tmp/rootCA.crt -CAkey /tmp/rootCA.key -CAcreateserial -out /tmp/server.crt -extensions SAN -extfile /tmp/extfile
-chmod og-rwx server.key
-log "Server certificate signed with our root CA certificate (server.crt) is:"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl x509 -in /tmp/server.crt -text -noout
+log "Generate the PostgreSQL server key and certificate"
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl req -new -nodes -out /tmp/server.csr -keyout /tmp/server.key -subj "/CN=postgres"
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl x509 -req -in /tmp/server.csr -days 365 -CA /tmp/ca.crt -CAkey /tmp/ca.key -CAcreateserial -out /tmp/server.crt
+rm server.csr
 
 cd -
 
@@ -69,7 +41,7 @@ curl -X PUT \
      --data '{
                "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
                     "tasks.max": "1",
-                    "connection.url": "jdbc:postgresql://postgres/postgres?user=postgres&password=postgres&sslmode=verify-full&sslrootcert=/tmp/root.crt",
+                    "connection.url": "jdbc:postgresql://postgres/postgres?user=postgres&password=postgres&sslmode=verify-full&sslrootcert=/tmp/ca.crt",
                     "table.whitelist": "customers",
                     "mode": "timestamp+incrementing",
                     "timestamp.column.name": "update_ts",
