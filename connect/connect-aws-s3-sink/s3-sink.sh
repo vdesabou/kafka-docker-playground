a7049471c1d2f440e31cbee4c52702538e2b1670
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-04-18 09:54:16 +0200

    creating bucket might fail if already existing

--- connect/connect-aws-s3-sink/s3-sink.sh
+++ connect/connect-aws-s3-sink/s3-sink.sh
@@ -33,7 +33,9 @@ ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml
 
 AWS_REGION=$(aws configure get region | tr '\r' '\n')
 log "Creating bucket name <$AWS_BUCKET_NAME>, if required"
+set +e
 aws s3api create-bucket --bucket $AWS_BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+set -e
 
 log "Creating S3 Sink connector with bucket name <$AWS_BUCKET_NAME>"
 curl -X PUT \
