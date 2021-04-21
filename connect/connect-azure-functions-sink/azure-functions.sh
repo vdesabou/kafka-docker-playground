86dbb8cd788bf198636f9c5244fce879abfd609d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-04-21 16:36:21 +0200

    #876 --runtime-version 12

--- connect/connect-azure-functions-sink/azure-functions.sh
+++ connect/connect-azure-functions-sink/azure-functions.sh
@@ -46,7 +46,7 @@ log "Creating local functions project with HTTP trigger"
 docker run -v $PWD/LocalFunctionProj:/LocalFunctionProj mcr.microsoft.com/azure-functions/node:3.0-node12-core-tools bash -c "func init LocalFunctionProj --javascript && cd LocalFunctionProj && func new --name HttpExample --template \"HTTP trigger\""
 
 log "Creating functions app $AZURE_FUNCTIONS_NAME"
-az functionapp create --consumption-plan-location $AZURE_REGION --name $AZURE_FUNCTIONS_NAME --resource-group $AZURE_RESOURCE_GROUP --runtime node --storage-account $AZURE_STORAGE_NAME --runtime-version 10 --functions-version 3
+az functionapp create --consumption-plan-location $AZURE_REGION --name $AZURE_FUNCTIONS_NAME --resource-group $AZURE_RESOURCE_GROUP --runtime node --storage-account $AZURE_STORAGE_NAME --runtime-version 12 --functions-version 3
 
 log "Publishing functions app, it will take a while"
 max_attempts="10"
