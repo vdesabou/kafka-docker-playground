0f81e57c7eccae7db42e416e02c871bb1d134b7a
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-04-21 17:50:19 +0200

    added examples with more HBase versions
    Former-commit-id: 9ce83cfc28767a79c9b9398bd4133b21e499ce25 [formerly 1e2ab95b4610db589cf3c113ec8b9308342f1f94 [formerly bfea167c55f5878246ca90cce945f0cfba925fee]]
    Former-commit-id: 7a1a89201cb6fc4940348cb7db3233e77beeb4d1
    Former-commit-id: 6442a432c5b198ec697c1963f856d16f652fbc53

---
+++ connect/connect-hbase-sink/hbase-sink-1.2.0.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.1.2.0.yml"
+
+
+log "Sending messages to topic hbase-test"
+docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic hbase-test --property parse.key=true --property key.separator=, << EOF
+key1,value1
+key2,value2
+key3,value3
+EOF
+
+log "Creating HBase sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.hbase.HBaseSinkConnector",
+                    "tasks.max": "1",
+                    "key.converter":"org.apache.kafka.connect.storage.StringConverter",
+                    "value.converter":"org.apache.kafka.connect.storage.StringConverter",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor":1,
+                    "hbase.zookeeper.quorum": "hbase",
+                    "hbase.zookeeper.property.clientPort": "2181",
+                    "auto.create.tables": "true",
+                    "auto.create.column.families": "true",
+                    "table.name.format": "example_table",
+                    "topics": "hbase-test"
+          }' \
+     http://localhost:8083/connectors/hbase-sink/config | jq .
+
+log "Verify data is in HBase:"
+docker exec -i hbase hbase shell > /tmp/result.log <<-EOF
+scan 'example_table'
+EOF
+cat /tmp/result.log
+grep "key1" /tmp/result.log | grep "value=value1"
+grep "key2" /tmp/result.log | grep "value=value2"
+grep "key3" /tmp/result.log | grep "value=value3"
