1908eae746063e6273bf959891dc2052af7220b5
Daniel Petisme <daniel.petisme@gmail.com>
2021-04-27 15:25:49 +0200

    Include CDC examples

--- connect/connect-cdc-oracle12-source/cdc-oracle12-cdb-table-mtls-db-auth.sh
+++ connect/connect-cdc-oracle12-source/cdc-oracle12-cdb-table-mtls-db-auth.sh
@@ -204,7 +204,14 @@ curl -X PUT \
           }' \
      http://localhost:8083/connectors/cdc-oracle-source-cdb/config | jq .
 
-sleep 5
+log "Waiting 60s for cdc-oracle-source-cdb to read existing data"
+sleep 60
+
+log "Running SQL scripts"
+for script in ${DIR}/sample-sql-scripts/*
+do
+     $script "ORCLCDB"
+done
 
 log "Verifying topic ORCLCDB.C__MYUSER.CUSTOMERS"
 timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic ORCLCDB.C__MYUSER.CUSTOMERS --from-beginning --max-messages 2
