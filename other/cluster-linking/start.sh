0767852526b8f607f95c59068cd1c4bdec46511b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-04-30 17:52:34 +0200

    cleanup
    Former-commit-id: a23f7f698e9b09c11dc934e38f262a343ebb57ff [formerly 6e9d992d1f39accab70e8f8dc25b2ac7e4ec39dd [formerly ff413ef4a4135c8027e79119290e57452170fa41]]
    Former-commit-id: aeed0e55232a34c5b80db408b7973aa5ec6c9cf0
    Former-commit-id: 7f1b39b19736dcb448d3772549401a74eeb5cc50

--- other/cluster-linking/start.sh
+++ other/cluster-linking/start.sh
@@ -6,38 +6,29 @@ source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../environment/mdc-plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
-
-
+log "Create topic cluster-linking-demo"
 docker exec broker-europe kafka-topics --create --topic cluster-linking-demo --bootstrap-server broker-us:9092 --replication-factor 1 --partitions 1
 
-
 log "Sending sales in US cluster"
-seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "export CLASSPATH=/usr/share/java/monitoring-interceptors/monitoring-interceptors-${TAG_BASE}.jar; kafka-console-producer --broker-list broker-us:9092 --topic cluster-linking-demo --producer-property interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor --producer-property confluent.monitoring.interceptor.bootstrap.servers=broker-metrics:9092"
-
+seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "kafka-console-producer --broker-list broker-us:9092 --topic cluster-linking-demo"
 
 log "Verify we have received the data in all the sales_ topics in the US"
-docker container exec -i connect-us bash -c "export CLASSPATH=/usr/share/java/monitoring-interceptors/monitoring-interceptors-${TAG_BASE}.jar;  kafka-console-consumer --bootstrap-server broker-us:9092 --topic cluster-linking-demo --from-beginning --max-messages 10 --consumer-property interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor --consumer-property confluent.monitoring.interceptor.bootstrap.servers=broker-metrics:9092"
+docker container exec -i connect-us bash -c " kafka-console-consumer --bootstrap-server broker-us:9092 --topic cluster-linking-demo --from-beginning --max-messages 10 --consumer-property group.id=my-source-consumer"
 
-log "Create the cluster link on the destination cluster (with metadata.max.age.ms=5 minutes)"
-docker exec broker-europe kafka-cluster-links --bootstrap-server broker-europe:9092 --create --link-name demo-link --config bootstrap.servers=broker-us:9092,metadata.max.age.ms=5000
+log "Create the cluster link on the destination cluster (with metadata.max.age.ms=5 minutes + consumer.offset.sync.enable=true + consumer.offset.sync.ms=3000)"
+docker exec broker-europe kafka-cluster-links --bootstrap-server broker-europe:9092 --create --link-name demo-link --config bootstrap.servers=broker-us:9092,metadata.max.age.ms=5000,consumer.offset.sync.enable=true,consumer.offset.sync.ms=3000
 
 log "Initialize the topic mirror"
 docker exec broker-europe kafka-topics --create --topic cluster-linking-demo --mirror-topic cluster-linking-demo --link-name demo-link --bootstrap-server broker-europe:9092
 
 log "Consume from the mirror topic on the destination cluster to verify it"
-docker container exec -i connect-us bash -c "export CLASSPATH=/usr/share/java/monitoring-interceptors/monitoring-interceptors-${TAG_BASE}.jar;  kafka-console-consumer --bootstrap-server broker-europe:9092 --topic cluster-linking-demo --from-beginning --max-messages 10 --consumer-property interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor --consumer-property confluent.monitoring.interceptor.bootstrap.servers=broker-metrics:9092"
+docker container exec -i connect-us bash -c " kafka-console-consumer --bootstrap-server broker-europe:9092 --topic cluster-linking-demo --max-messages 10 --consumer-property group.id=my-source-consumer"
 
 log "Check the replica status on the destination"
 docker exec broker-europe kafka-replica-status --topics cluster-linking-demo --include-linked --bootstrap-server broker-europe:9092
 
-log "Sending data again in US cluster"
-seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "export CLASSPATH=/usr/share/java/monitoring-interceptors/monitoring-interceptors-${TAG_BASE}.jar; kafka-console-producer --broker-list broker-us:9092 --topic cluster-linking-demo --producer-property interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor --producer-property confluent.monitoring.interceptor.bootstrap.servers=broker-metrics:9092"
-
-log "Verify we have received the data in all the sales_ topics in the US"
-docker container exec -i connect-us bash -c "export CLASSPATH=/usr/share/java/monitoring-interceptors/monitoring-interceptors-${TAG_BASE}.jar;  kafka-console-consumer --bootstrap-server broker-us:9092 --topic cluster-linking-demo --from-beginning --max-messages 20 --consumer-property interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor --consumer-property confluent.monitoring.interceptor.bootstrap.servers=broker-metrics:9092"
-
 log "Verify that the topic mirror is read-only"
-seq -f "europe_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "export CLASSPATH=/usr/share/java/monitoring-interceptors/monitoring-interceptors-${TAG_BASE}.jar; kafka-console-producer --broker-list broker-europe:9092 --topic cluster-linking-demo --producer-property interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor --producer-property confluent.monitoring.interceptor.bootstrap.servers=broker-metrics:9092"
+seq -f "europe_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "kafka-console-producer --broker-list broker-europe:9092 --topic cluster-linking-demo"
 
 # [2021-04-29 14:03:24,363] ERROR Error when sending message to topic cluster-linking-demo with key: null, value: 19 bytes with error: (org.apache.kafka.clients.producer.internals.ErrorLoggingCallback)
 # org.apache.kafka.common.errors.InvalidRequestException: Cannot append records to read-only mirror topic 'cluster-linking-demo'
@@ -84,10 +75,10 @@ docker container exec -i connect-us kafka-topics --alter --topic cluster-linking
 log "Produce to both topics to verify divergence"
 
 log "Sending data again in US cluster"
-seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "export CLASSPATH=/usr/share/java/monitoring-interceptors/monitoring-interceptors-${TAG_BASE}.jar; kafka-console-producer --broker-list broker-us:9092 --topic cluster-linking-demo --producer-property interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor --producer-property confluent.monitoring.interceptor.bootstrap.servers=broker-metrics:9092"
+seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "kafka-console-producer --broker-list broker-us:9092 --topic cluster-linking-demo"
 
 log "Sending data in EUROPE cluster"
-seq -f "europe_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "export CLASSPATH=/usr/share/java/monitoring-interceptors/monitoring-interceptors-${TAG_BASE}.jar; kafka-console-producer --broker-list broker-europe:9092 --topic cluster-linking-demo --producer-property interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor --producer-property confluent.monitoring.interceptor.bootstrap.servers=broker-metrics:9092"
+seq -f "europe_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "kafka-console-producer --broker-list broker-europe:9092 --topic cluster-linking-demo"
 
 log "Delete the cluster link"
 docker container exec -i connect-us kafka-cluster-links --bootstrap-server broker-europe:9092 --delete --link-name demo-link
