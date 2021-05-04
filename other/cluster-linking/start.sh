313e7c8235234313627faad09180e9a61adfaf61
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-05-04 12:37:21 +0200

    WIP
    Former-commit-id: 2bc7887fd93fc543d3ef187593631bcaaa777dd2 [formerly 8ef8d4ee0a74395c46403df1b759cabd04304700 [formerly e6fd999ad4c6d164f3e7673a310a311d1a67d2c2]]
    Former-commit-id: 37350469f42182038aa3d4d5459561d41782b80a
    Former-commit-id: ab37fef16737c214cb9687bfb5fa31235e3e0b4d

--- other/cluster-linking/start.sh
+++ other/cluster-linking/start.sh
@@ -10,24 +10,68 @@ log "Create topic demo"
 docker exec broker-europe kafka-topics --create --topic demo --bootstrap-server broker-us:9092 --replication-factor 1 --partitions 1
 
 log "Sending sales in US cluster"
-seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "kafka-console-producer --broker-list broker-us:9092 --topic demo"
+seq -f "us_sale_%g ${RANDOM}" 20 | docker container exec -i connect-us bash -c "kafka-console-producer --broker-list broker-us:9092 --topic demo"
 
-log "Verify we have received the data in all the sales_ topics in the US"
-docker container exec -i connect-us bash -c "kafka-console-consumer --bootstrap-server broker-us:9092 --topic demo --from-beginning --max-messages 10"
+log "Verify we have received the data in source cluster using consumer group id my-consumer-group, we read only 5 messages"
+docker container exec -i connect-us bash -c "kafka-console-consumer --bootstrap-server broker-us:9092 --topic demo --from-beginning --max-messages 5 --consumer-property group.id=my-consumer-group"
 
 log "Create the cluster link on the destination cluster (with metadata.max.age.ms=5 minutes + consumer.offset.sync.enable=true + consumer.offset.sync.ms=3000)"
-docker exec broker-europe kafka-cluster-links --bootstrap-server broker-europe:9092 --create --link-name demo-link --config bootstrap.servers=broker-us:9092,metadata.max.age.ms=5000
-
-#,consumer.offset.sync.enable=true,consumer.offset.sync.ms=3000
+docker cp consumer.offset.sync.all.json broker-europe:/tmp/consumer.offset.sync.all.json
+docker exec broker-europe kafka-cluster-links --bootstrap-server broker-europe:9092 --create --link-name demo-link --config bootstrap.servers=broker-us:9092,metadata.max.age.ms=5000,consumer.offset.sync.enable=true,consumer.offset.sync.ms=3000 --consumer-group-filters-json-file /tmp/consumer.offset.sync.all.json
 
 log "Initialize the topic mirror"
 docker exec broker-europe kafka-topics --create --topic demo --mirror-topic demo --link-name demo-link --bootstrap-server broker-europe:9092
 
-log "Consume from the mirror topic on the destination cluster to verify it"
-docker container exec -i connect-us bash -c "kafka-console-consumer --bootstrap-server broker-europe:9092 --topic demo --from-beginning --max-messages 10"
-
 log "Check the replica status on the destination"
 docker exec broker-europe kafka-replica-status --topics demo --include-linked --bootstrap-server broker-europe:9092
+
+log "Wait 6 seconds for consumer.offset sync to happen (2 times consumer.offset.sync.ms=3000)"
+sleep 6
+
+log "Verify that current offset is consistent in source and destination"
+log "Source cluster"
+docker exec broker-europe kafka-consumer-groups --bootstrap-server broker-us:9092 --describe --group my-consumer-group
+log "Destination cluster"
+docker exec broker-europe kafka-consumer-groups --bootstrap-server broker-europe:9092 --describe --group my-consumer-group
+
+log "Consume from the mirror topic on the destination cluster and verify consumer offset is working, it should start at 6"
+docker container exec -i connect-us bash -c "kafka-console-consumer --bootstrap-server broker-europe:9092 --topic demo --max-messages 5 --consumer-property group.id=my-consumer-group"
+
+log "Destination cluster"
+docker exec broker-europe kafka-consumer-groups --bootstrap-server broker-europe:9092 --describe --group my-consumer-group
+
+log "Stop consumer offset sync for consumer group my-consumer-group"
+echo "consumer.offset.group.filters={\"groupFilters\": [ \
+  { \
+    \"name\": \"*\", \
+    \"patternType\": \"LITERAL\", \
+    \"filterType\": \"INCLUDE\" \
+  }, \
+  { \
+    \"name\": \"my-consumer-group\", \
+    \"patternType\": \"LITERAL\", \
+    \"filterType\": \"EXCLUDE\" \
+  } \
+]}" > newFilters.properties
+docker cp newFilters.properties broker-europe:/tmp/newFilters.properties
+docker exec broker-europe kafka-configs --bootstrap-server broker-europe:9092 --alter --cluster-link demo-link --add-config-file /tmp/newFilters.properties
+
+log "Destination cluster"
+docker exec broker-europe kafka-consumer-groups --bootstrap-server broker-europe:9092 --describe --group my-consumer-group
+
+sleep 6
+
+log "Consume from the source cluster another 10 messages, up to 15"
+docker container exec -i connect-us bash -c "kafka-console-consumer --bootstrap-server broker-us:9092 --topic demo --max-messages 10 --consumer-property group.id=my-consumer-group"
+
+
+
+log "Consume from the destination cluster, it will continue from it's last offset 11 FIXTHIS: it starts with 6 !"
+docker container exec -i connect-us bash -c "kafka-console-consumer --bootstrap-server broker-europe:9092 --topic demo --max-messages 5 --consumer-property group.id=my-consumer-group"
+
+# FIXTHIS
+exit 0
+
 
 log "Verify that the topic mirror is read-only"
 seq -f "europe_sale_%g ${RANDOM}" 10 | docker container exec -i connect-us bash -c "kafka-console-producer --broker-list broker-europe:9092 --topic demo"
