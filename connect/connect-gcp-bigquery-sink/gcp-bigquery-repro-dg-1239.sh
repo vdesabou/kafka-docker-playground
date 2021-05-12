e0279672f42c57db44da8422a910b162559c8894
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-05-12 14:35:33 +0200

    add schema

--- connect/connect-gcp-bigquery-sink/gcp-bigquery-repro-dg-1239.sh
+++ connect/connect-gcp-bigquery-sink/gcp-bigquery-repro-dg-1239.sh
@@ -32,6 +32,27 @@ set -e
 log "Create dataset $PROJECT.$DATASET"
 docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" mk --dataset --description "used by playground" "$DATASET"
 
+# schema
+# {
+#  "fields": [
+#    {
+#      "name": "id",
+#      "type": [
+#        "null",
+#        "string",
+#        {
+#          "name": "testenum",
+#           "symbols": ["A","B","C","D"],
+#           "type": "enum"
+#         }
+#       ],
+#       "default": null
+#     }
+#   ],
+#   "name": "EnumUnionTest",
+#   "namespace": "com",
+#   "type": "record"
+# }
 log "Send data to topic myavrotopic"
 docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic myavrotopic --property value.schema='{"fields":[{"name":"id","type":["null","string",{"name":"testenum","symbols":["A","B","C","D"],"type":"enum"}],"default":null}],"name":"EnumUnionTest","namespace":"com","type":"record"}' << EOF
 {"id":{"string":"anystring"}}
