f362ef1a2e8f3808e48f8256d35bc7c7f044d64d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-05-12 15:52:22 +0200

    test with string, null and enum

--- connect/connect-gcp-bigquery-sink/gcp-bigquery-repro-dg-1239.sh
+++ connect/connect-gcp-bigquery-sink/gcp-bigquery-repro-dg-1239.sh
@@ -34,42 +34,43 @@ docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_
 
 # schema
 # {
-#  "fields": [
-#    {
-#      "name": "id",
-#      "type": [
-#        "null",
-#        "string",
-#        {
-#          "name": "testenum",
-#           "symbols": ["A","B","C","D"],
-#           "type": "enum"
+#     "fields": [
+#         {
+#             "default": null,
+#             "name": "userType",
+#             "type": [
+#                 "null",
+#                 {
+#                     "type": "string",
+#                     "avro.java.string": "String"
+#                 },
+#                 {
+#                     "name": "UserType",
+#                     "symbols": [
+#                         "ANONYMOUS",
+#                         "REGISTERED"
+#                     ],
+#                     "type": "enum"
+#                 }
+#             ]
 #         }
-#       ],
-#       "default": null
-#     }
-#   ],
-#   "name": "EnumUnionTest",
-#   "namespace": "com",
-#   "type": "record"
+#     ],
+#     "name": "EnumStringUnion",
+#     "namespace": "com.connect.avro",
+#     "type": "record"
 # }
-log "Send data to topic myavrotopic"
-docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic myavrotopic --property value.schema='{"fields":[{"name":"id","type":["null","string",{"name":"testenum","symbols":["A","B","C","D"],"type":"enum"}],"default":null}],"name":"EnumUnionTest","namespace":"com","type":"record"}' << EOF
-{"id":{"string":"anystring"}}
-{"id":{"com.testenum":"A"}}
-{"id":null}
+log "Send userType as string to topic myavrotopic1"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic myavrotopic1 --property value.schema='{"fields":[{"default":null,"name":"userType","type":["null",{"type":"string","avro.java.string":"String"},{"name":"UserType","symbols":["ANONYMOUS","REGISTERED"],"type":"enum"}]}],"name":"EnumStringUnion","namespace":"com.connect.avro","type":"record"}' << EOF
+{"userType":{"string":"anystring"}}
 EOF
 
-log "Verifying topic myavrotopic"
-timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic myavrotopic --from-beginning --max-messages 3
-
-log "Creating GCP BigQuery Sink connector"
+log "Creating GCP BigQuery Sink connector gcp-bigquery-sink-1"
 curl -X PUT \
      -H "Content-Type: application/json" \
      --data '{
                "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
                "tasks.max" : "1",
-               "topics" : "myavrotopic",
+               "topics" : "myavrotopic1",
                "sanitizeTopics" : "true",
                "autoCreateTables" : "true",
                "autoUpdateSchemas" : "true",
@@ -84,74 +85,72 @@ curl -X PUT \
                "project" : "'"$PROJECT"'",
                "keyfile" : "/tmp/keyfile.json"
           }' \
-     http://localhost:8083/connectors/gcp-bigquery-sink/config | jq .
+     http://localhost:8083/connectors/gcp-bigquery-sink-1/config | jq .
 
-# FIXTHIS
-# [2021-05-12 12:05:51,704] ERROR WorkerSinkTask{id=gcp-bigquery-sink-0} Error converting message value in topic 'myavrotopic' partition 0 at offset 1 and timestamp 1620821141211: Did not find matching union field for data: A (org.apache.kafka.connect.runtime.WorkerSinkTask)
-# org.apache.kafka.connect.errors.DataException: Did not find matching union field for data: A
-#         at io.confluent.connect.avro.AvroData.toConnectData(AvroData.java:1466)
-#         at io.confluent.connect.avro.AvroData.toConnectData(AvroData.java:1226)
-#         at io.confluent.connect.avro.AvroData.toConnectData(AvroData.java:1484)
-#         at io.confluent.connect.avro.AvroData.toConnectData(AvroData.java:1226)
-#         at io.confluent.connect.avro.AvroData.toConnectData(AvroData.java:1222)
-#         at io.confluent.connect.avro.AvroConverter.toConnectData(AvroConverter.java:115)
-#         at org.apache.kafka.connect.storage.Converter.toConnectData(Converter.java:87)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertValue(WorkerSinkTask.java:545)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.lambda$convertAndTransformRecord$1(WorkerSinkTask.java:501)
-#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndRetry(RetryWithToleranceOperator.java:156)
-#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndHandleError(RetryWithToleranceOperator.java:190)
-#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execute(RetryWithToleranceOperator.java:132)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertAndTransformRecord(WorkerSinkTask.java:501)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertMessages(WorkerSinkTask.java:478)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.poll(WorkerSinkTask.java:328)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.iteration(WorkerSinkTask.java:232)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:201)
-#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:189)
-#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:238)
-#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
-#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
-#         at java.base/java.lang.Thread.run(Thread.java:829)
-# [2021-05-12 12:05:51,713] ERROR WorkerSinkTask{id=gcp-bigquery-sink-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask)
-# org.apache.kafka.connect.errors.ConnectException: Tolerance exceeded in error handler
-#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndHandleError(RetryWithToleranceOperator.java:206)
-#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execute(RetryWithToleranceOperator.java:132)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertAndTransformRecord(WorkerSinkTask.java:501)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertMessages(WorkerSinkTask.java:478)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.poll(WorkerSinkTask.java:328)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.iteration(WorkerSinkTask.java:232)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:201)
-#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:189)
-#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:238)
-#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
-#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
-#         at java.base/java.lang.Thread.run(Thread.java:829)
-# Caused by: org.apache.kafka.connect.errors.DataException: Did not find matching union field for data: A
-#         at io.confluent.connect.avro.AvroData.toConnectData(AvroData.java:1466)
-#         at io.confluent.connect.avro.AvroData.toConnectData(AvroData.java:1226)
-#         at io.confluent.connect.avro.AvroData.toConnectData(AvroData.java:1484)
-#         at io.confluent.connect.avro.AvroData.toConnectData(AvroData.java:1226)
-#         at io.confluent.connect.avro.AvroData.toConnectData(AvroData.java:1222)
-#         at io.confluent.connect.avro.AvroConverter.toConnectData(AvroConverter.java:115)
-#         at org.apache.kafka.connect.storage.Converter.toConnectData(Converter.java:87)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertValue(WorkerSinkTask.java:545)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.lambda$convertAndTransformRecord$1(WorkerSinkTask.java:501)
-#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndRetry(RetryWithToleranceOperator.java:156)
-#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndHandleError(RetryWithToleranceOperator.java:190)
-#         ... 13 more
+sleep 4
 
-log "Sleeping 125 seconds"
-sleep 125
+curl localhost:8083/connectors/gcp-bigquery-sink-1/status | jq
 
-log "Verify data is in GCP BigQuery:"
-docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" query "SELECT * FROM $DATASET.myavrotopic;" > /tmp/result.log  2>&1
-cat /tmp/result.log
-grep "value1" /tmp/result.log
+log "Send userType as enum to topic myavrotopic2"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic myavrotopic2 --property value.schema='{"fields":[{"default":null,"name":"userType","type":["null",{"type":"string","avro.java.string":"String"},{"name":"UserType","symbols":["ANONYMOUS","REGISTERED"],"type":"enum"}]}],"name":"EnumStringUnion","namespace":"com.connect.avro","type":"record"}' << EOF
+{"userType":{"com.connect.avro.UserType":"ANONYMOUS"}}
+EOF
 
-log "Drop dataset $DATASET"
-docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" rm -r -f -d "$DATASET"
+log "Creating GCP BigQuery Sink connector gcp-bigquery-sink-2"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
+               "tasks.max" : "1",
+               "topics" : "myavrotopic2",
+               "sanitizeTopics" : "true",
+               "autoCreateTables" : "true",
+               "autoUpdateSchemas" : "true",
+               "value.converter": "io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url": "http://schema-registry:8081",
+               "value.converter.enhanced.avro.schema.support": "true",
+               "defaultDataset" : "'"$DATASET"'",
+               "mergeIntervalMs": "5000",
+               "bufferSize": "100000",
+               "maxWriteSize": "10000",
+               "tableWriteWait": "1000",
+               "project" : "'"$PROJECT"'",
+               "keyfile" : "/tmp/keyfile.json"
+          }' \
+     http://localhost:8083/connectors/gcp-bigquery-sink-2/config | jq .
 
-docker rm -f gcloud-config
+sleep 4
+
+curl localhost:8083/connectors/gcp-bigquery-sink-2/status | jq
+
+log "Send userType as null to topic myavrotopic3"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic myavrotopic3 --property value.schema='{"fields":[{"default":null,"name":"userType","type":["null",{"type":"string","avro.java.string":"String"},{"name":"UserType","symbols":["ANONYMOUS","REGISTERED"],"type":"enum"}]}],"name":"EnumStringUnion","namespace":"com.connect.avro","type":"record"}' << EOF
+{"userType":null}
+EOF
+
+log "Creating GCP BigQuery Sink connector gcp-bigquery-sink-3"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
+               "tasks.max" : "1",
+               "topics" : "myavrotopic3",
+               "sanitizeTopics" : "true",
+               "autoCreateTables" : "true",
+               "autoUpdateSchemas" : "true",
+               "value.converter": "io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url": "http://schema-registry:8081",
+               "value.converter.enhanced.avro.schema.support": "true",
+               "defaultDataset" : "'"$DATASET"'",
+               "mergeIntervalMs": "5000",
+               "bufferSize": "100000",
+               "maxWriteSize": "10000",
+               "tableWriteWait": "1000",
+               "project" : "'"$PROJECT"'",
+               "keyfile" : "/tmp/keyfile.json"
+          }' \
+     http://localhost:8083/connectors/gcp-bigquery-sink-3/config | jq .
+
+sleep 4
+
+curl localhost:8083/connectors/gcp-bigquery-sink-3/status | jq
