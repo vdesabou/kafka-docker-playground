573b132f55ed0ab49bfb5628794f899c4217a713
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-05-19 17:37:07 +0200

    ldap_authorizer_sasl_plain -> ldap-authorizer-sasl-plain

---
+++ environment/ldap-authorizer-sasl-plain/stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+DOCKER_COMPOSE_FILE_OVERRIDE=$1
+if [ -f "${DOCKER_COMPOSE_FILE_OVERRIDE}" ]
+then
+
+  docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/sasl-plain/docker-compose.yml -f ../../environment/ldap-authorizer-sasl-plain/docker-compose.yml -f ${DOCKER_COMPOSE_FILE_OVERRIDE} down -v --remove-orphans
+else
+  docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/sasl-plain/docker-compose.yml -f ../../environment/ldap-authorizer-sasl-plain/docker-compose.yml down -v --remove-orphans
+fi
