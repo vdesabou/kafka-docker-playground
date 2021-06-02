3244a0e5ed38252b5b0a756881b707b7560c45b9
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-02 16:26:52 +0200

    Certificates creation is very slow #1002

--- environment/2way-ssl/security/certs-verify.sh
+++ environment/2way-ssl/security/certs-verify.sh
@@ -4,14 +4,11 @@ set -o nounset \
     -o errexit \
     -o verbose
 
-# workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
-chmod -R a+rw .
-
 # See what is in each keystore and truststore
 for i in broker broker2 client schema-registry restproxy connect control-center clientrestproxy
 do
         echo "------------------------------- $i keystore -------------------------------"
-	docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -list -v -keystore /tmp/kafka.$i.keystore.jks -storepass confluent | grep -e Alias -e Entry
+        keytool -list -v -keystore /tmp/kafka.$i.keystore.jks -storepass confluent | grep -e Alias -e Entry
         echo "------------------------------- $i truststore -------------------------------"
-	docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -list -v -keystore /tmp/kafka.$i.truststore.jks -storepass confluent | grep -e Alias -e Entry
+        keytool -list -v -keystore /tmp/kafka.$i.truststore.jks -storepass confluent | grep -e Alias -e Entry
 done
