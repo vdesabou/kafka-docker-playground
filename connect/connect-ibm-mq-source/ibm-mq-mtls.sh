5979f72ea9adfaa8292312f74ef0936c8d5c9908
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-03 18:33:55 +0200

    #816 get MTLS working as per https://developer.ibm.com/components/ibm-mq/tutorials/configuring-mutual-tls-authentication-java-messaging-app/
    Former-commit-id: 860cc3b7bf57a8171ae49bdcfbd2e5a854fdc871 [formerly 4d76378373059c01c80fe3a9cca4a1bd0a55433a [formerly 5413bf0ea109592f7964aefcca07a2ce6582776d]]
    Former-commit-id: 4fcc43d933f80de00737eb5cb67b80cd962e1203
    Former-commit-id: 750831fc295e9dd420eca20a0782814a831a207a

--- connect/connect-ibm-mq-source/ibm-mq-mtls.sh
+++ connect/connect-ibm-mq-source/ibm-mq-mtls.sh
@@ -24,80 +24,87 @@ then
      rm -rf ${DIR}/install
 fi
 
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/docker-compose.plaintext.mtls.yml" down -v
+
+log "Starting up ibmmq container to get generated cert from server"
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/docker-compose.plaintext.mtls.yml" up -d ibmmq
+
+# Verify IBM MQ has started within MAX_WAIT seconds
+MAX_WAIT=900
+CUR_WAIT=0
+log "Waiting up to $MAX_WAIT seconds for IBM MQ to start"
+docker container logs ibmmq > /tmp/out.txt 2>&1
+while [[ ! $(cat /tmp/out.txt) =~ "Started web server" ]]; do
+sleep 10
+docker container logs ibmmq > /tmp/out.txt 2>&1
+CUR_WAIT=$(( CUR_WAIT+10 ))
+if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+     logerror "ERROR: The logs in ibmmq container do not show 'Started web server' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+     exit 1
+fi
+done
+log "IBM MQ has started!"
+
+# https://developer.ibm.com/components/ibm-mq/tutorials/configuring-mutual-tls-authentication-java-messaging-app/
+rm -rf ${DIR}/mtls
 mkdir -p ${DIR}/mtls
 cd ${DIR}/mtls
-if [ -z "$CI" ]
-then
-    # not running with github actions
-    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
-    chmod -R a+rw .
-else
-    # docker is run as runneradmin user, need to use sudo
-    ls -lrt
-    sudo chmod -R a+rw .
-    ls -lrt
-fi
 
-rm -f server.crt
-rm -f server.csr
-rm -f server.key
-rm -f ca.crt
-rm -f ca.key
-rm -f truststore.jks
-
-log "Creating a Root Certificate Authority (CA)"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl req -new -x509 -days 365 -nodes -out /tmp/ca.crt -keyout /tmp/ca.key -subj "/CN=root-ca"
-log "Generate the IBM MQ server key and certificate"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl req -new -nodes -out /tmp/server.csr -keyout /tmp/server.key -subj "/CN=ibmmq"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl x509 -req -in /tmp/server.csr -days 365 -CA /tmp/ca.crt -CAkey /tmp/ca.key -CAcreateserial -out /tmp/server.crt
-
-log "Generate truststore.jks"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -noprompt -keystore /tmp/truststore.jks -alias CARoot -import -file /tmp/ca.crt -storepass confluent -keypass confluent
-
-if [ -z "$CI" ]
-then
-    # not running with github actions
-    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
-    chmod -R a+rw .
-else
-    # docker is run as runneradmin user, need to use sudo
-    ls -lrt
-    sudo chmod -R a+rw .
-    ls -lrt
-fi
-rm server.csr
-
-log "Generating the Client Key and Certificate"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl req -new -nodes -out /tmp/client.csr -keyout /tmp/client.key -subj "/CN=connect"
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl x509 -req -in /tmp/client.csr -days 365 -CA /tmp/ca.crt -CAkey /tmp/ca.key -CAcreateserial -out /tmp/client.crt
-
-# Sign and import the CA cert into the keystore
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -noprompt -keystore /tmp/keystore.jks -alias CARoot -import -file /tmp/ca.crt -storepass confluent -keypass confluent
-
-# Sign and import the host certificate into the keystore
-docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -noprompt -keystore /tmp/keystore.jks -alias $i -import -file /tmp/client.crt -storepass confluent -keypass confluent
-
-if [ -z "$CI" ]
-then
-    # not running with github actions
-    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
-    chmod -R a+rw .
-else
-    # docker is run as runneradmin user, need to use sudo
-    ls -lrt
-    sudo chmod -R a+rw .
-    ls -lrt
-fi
-rm client.csr
-
-cd -
-
-${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.mtls.yml"
-
-log "Verify TLS is active on IBM MQ: it should display SSLCIPH(ANY_TLS12)"
-docker exec -i ibmmq runmqsc QM1 << EOF
-DISPLAY CHANNEL(DEV.APP.SVRCONN)
+log "Create a keystore (a .kdb file) using the MQ security tool command runmqakm"
+docker exec -i ibmmq bash << EOF
+cd /var/mqm/qmgrs/QM1/ssl
+rm -f key.*
+rm -f QM.*
+# Create a keystore (a .kdb file) using the MQ security tool command runmqakm
+runmqakm -keydb -create -db key.kdb -pw confluent -stash
+chmod 640 *
+# create a self-signed certificate and private key and put them in the keystore
+runmqakm -cert -create -db key.kdb -stashed -dn "cn=qm,o=ibm,c=uk" -label ibmwebspheremqqm1
+# let’s extract the queue manager certificate, which we’ll then give to the client application.
+runmqakm -cert -extract -label ibmwebspheremqqm1 -db key.kdb -stashed -file QM.cert
 EOF
+
+log "Copy IBM MQ certificate"
+docker cp ibmmq:/var/mqm/qmgrs/QM1/ssl/QM.cert .
+
+log "Create client truststore.jks with server certificate"
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -importcert -alias server-certificate -noprompt -file /tmp/QM.cert -keystore /tmp/truststore.jks -storepass confluent
+
+log "Setting up mutual authentication"
+
+log "Set the channel authentication to required so that both the server and client will need to provide a trusted certificate"
+docker exec -i ibmmq runmqsc QM1 << EOF
+ALTER CHANNEL(DEV.APP.SVRCONN) CHLTYPE(SVRCONN) SSLCAUTH(REQUIRED)
+EXIT
+EOF
+
+log "Create client keystore.jks"
+rm -f keystore.jks
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -genkeypair -noprompt -keyalg RSA -alias client-key -keystore /tmp/keystore.jks -storepass confluent -keypass confluent -storetype pkcs12 -dname "CN=connect,OU=TEST,O=CONFLUENT,L=PaloAlto,S=Ca,C=US"
+
+log "Extract the client certificate to the file client.crt"
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -noprompt -export -alias client-key -file /tmp/client.crt -keystore /tmp/keystore.jks -storepass confluent -keypass confluent
+
+log "Copy client.crt to ibmmq container"
+docker cp client.crt ibmmq:/tmp/client.crt
+
+log "Add client certificate to the queue manager’s key repository, so the server knows that it can trust the client"
+docker exec -i ibmmq bash -c "cd /var/mqm/qmgrs/QM1/ssl && runmqakm -cert -add -db key.kdb -stashed -label ibmwebspheremqapp -file /tmp/client.crt"
+
+log "Force our queue manager to pick up these changes"
+docker exec -i ibmmq runmqsc QM1 << EOF
+REFRESH SECURITY(*) TYPE(SSL)
+EXIT
+EOF
+
+log "List the certificates in the key repository"
+docker exec -i ibmmq bash -c "cd /var/mqm/qmgrs/QM1/ssl && runmqakm -cert -list -db key.kdb -stashed"
+
+cd ${DIR}
+
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/docker-compose.plaintext.mtls.yml" up -d
+
+../../scripts/wait-for-connect-and-controlcenter.sh
 
 log "Creating IBM MQ source connector"
 curl -X PUT \
@@ -118,7 +125,6 @@ curl -X PUT \
                "mq.tls.truststore.password": "confluent",
                "mq.tls.keystore.location": "/tmp/keystore.jks",
                "mq.tls.keystore.password": "confluent",
-               "mq.ssl.cipher.suite":"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
                "confluent.license": "",
                "confluent.topic.bootstrap.servers": "broker:9092",
                "confluent.topic.replication.factor": "1"
