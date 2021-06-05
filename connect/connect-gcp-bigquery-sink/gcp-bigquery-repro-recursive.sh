5140fc9b1048d4a67f85519a46b8cf64ef8fdf7e
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-05 18:19:34 +0200

    update with older version
    Former-commit-id: e7ef646c22dad8817826adb3fbd836ba0eee8552 [formerly f2b3f2e9798a0c35daa36ad5f1edd50884427d18 [formerly 57ce62e1240a056bd8707a2bc652b4fa71e499d3]]
    Former-commit-id: a5aef356a729485aeeda66054fa9c3f43d199ba4
    Former-commit-id: d9e807aa3ed1034ff1283032de2d0b942f761add

--- connect/connect-gcp-bigquery-sink/gcp-bigquery-repro-recursive.sh
+++ connect/connect-gcp-bigquery-sink/gcp-bigquery-repro-recursive.sh
@@ -88,29 +88,58 @@ docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --p
 {"UnionField":{"com.example.InnerStruct2":{"InnerField":{"boolean":true}}}}
 EOF
 
-log "Creating GCP BigQuery Sink connector gcp-bigquery-sink-1"
-curl -X PUT \
-     -H "Content-Type: application/json" \
-     --data '{
-               "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
-               "tasks.max" : "1",
-               "topics" : "myavrotopic1",
-               "sanitizeTopics" : "true",
-               "autoCreateTables" : "true",
-               "autoUpdateSchemas" : "true",
-               "value.converter": "io.confluent.connect.avro.AvroConverter",
-               "value.converter.schema.registry.url": "http://schema-registry:8081",
-               "defaultDataset" : "'"$DATASET"'",
-               "mergeIntervalMs": "5000",
-               "bufferSize": "100000",
-               "maxWriteSize": "10000",
-               "tableWriteWait": "1000",
-               "sanitizeFieldNames": "true",
-               "project" : "'"$PROJECT"'",
-               "keyfile" : "/tmp/keyfile.json"
-          }' \
-     http://localhost:8083/connectors/gcp-bigquery-sink-1/config | jq .
+if version_gt $CONNECTOR_TAG "1.9.9"
+then
+     log "Creating GCP BigQuery Sink connector gcp-bigquery-sink-1"
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+                    "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
+                    "tasks.max" : "1",
+                    "topics" : "myavrotopic1",
+                    "sanitizeTopics" : "true",
+                    "autoCreateTables" : "true",
+                    "autoUpdateSchemas" : "true",
+                    "value.converter": "io.confluent.connect.avro.AvroConverter",
+                    "value.converter.schema.registry.url": "http://schema-registry:8081",
+                    "defaultDataset" : "'"$DATASET"'",
+                    "mergeIntervalMs": "5000",
+                    "bufferSize": "100000",
+                    "maxWriteSize": "10000",
+                    "tableWriteWait": "1000",
+                    "sanitizeFieldNames": "true",
+                    "project" : "'"$PROJECT"'",
+                    "keyfile" : "/tmp/keyfile.json"
+               }' \
+          http://localhost:8083/connectors/gcp-bigquery-sink-1/config | jq .
+else
+     log "Creating GCP BigQuery Sink connector gcp-bigquery-sink-1"
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+                    "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
+                    "tasks.max" : "1",
+                    "topics" : "myavrotopic46",
+                    "sanitizeTopics" : "true",
+                    "autoCreateTables" : "true",
+                    "autoUpdateSchemas" : "true",
+                    "schemaRetriever" : "com.wepay.kafka.connect.bigquery.schemaregistry.schemaretriever.SchemaRegistrySchemaRetriever",
+                    "schemaRegistryLocation": "http://schema-registry:8081",
+                    "value.converter": "io.confluent.connect.avro.AvroConverter",
+                    "value.converter.schema.registry.url": "http://schema-registry:8081",
+                    "mergeIntervalMs": "5000",
+                    "bufferSize": "100000",
+                    "maxWriteSize": "10000",
+                    "tableWriteWait": "1000",
+                    "sanitizeFieldNames": "true",
+                    "datasets" : ".*='"$DATASET"'",
+                    "project" : "'"$PROJECT"'",
+                    "keyfile" : "/tmp/keyfile.json"
+               }' \
+          http://localhost:8083/connectors/gcp-bigquery-sink-1/config | jq .
+fi
 
+# with connector version 2.1.3:
 # [2021-06-05 15:40:12,152] ERROR Task failed with com.wepay.kafka.connect.bigquery.exception.ConversionConnectException error: Kafka Connect schema contains cycle (com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor)
 # Exception in thread "pool-5-thread-1" com.wepay.kafka.connect.bigquery.exception.ConversionConnectException: Kafka Connect schema contains cycle
 #         at com.wepay.kafka.connect.bigquery.convert.BigQuerySchemaConverter.throwOnCycle(BigQuerySchemaConverter.java:129)
@@ -144,51 +173,10 @@ curl -X PUT \
 #         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
 #         at java.base/java.lang.Thread.run(Thread.java:829)
 
-sleep 4
 
-curl localhost:8083/connectors/gcp-bigquery-sink-1/status | jq
+# with connector version 1.5.2 and 1.6.1:
 
-# [2021-06-05 15:18:30,395] ERROR Task failed with com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException error: Failed to create table GenericData{classInfo=[datasetId, projectId, tableId], {datasetId=pgvsaboulinds611, tableId=myavrotopic44}} (com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor)
-# Exception in thread "pool-7-thread-1" com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException: Failed to create table GenericData{classInfo=[datasetId, projectId, tableId], {datasetId=pgvsaboulinds611, tableId=myavrotopic44}}
-# Caused by: Invalid field name "com.example.InnerStruct". Fields must contain only letters, numbers, and underscores, start with a letter or underscore, and be at most 300 characters long.
-#         at com.wepay.kafka.connect.bigquery.write.row.AdaptiveBigQueryWriter.attemptTableCreate(AdaptiveBigQueryWriter.java:170)
-#         at com.wepay.kafka.connect.bigquery.write.row.AdaptiveBigQueryWriter.performWriteRequest(AdaptiveBigQueryWriter.java:115)
-#         at com.wepay.kafka.connect.bigquery.write.row.BigQueryWriter.writeRows(BigQueryWriter.java:118)
-#         at com.wepay.kafka.connect.bigquery.write.batch.TableWriter.run(TableWriter.java:96)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
-#         at java.base/java.lang.Thread.run(Thread.java:829)
-# Caused by: com.google.cloud.bigquery.BigQueryException: Invalid field name "com.example.InnerStruct". Fields must contain only letters, numbers, and underscores, start with a letter or underscore, and be at most 300 characters long.
-#         at com.google.cloud.bigquery.spi.v2.HttpBigQueryRpc.translate(HttpBigQueryRpc.java:113)
-#         at com.google.cloud.bigquery.spi.v2.HttpBigQueryRpc.create(HttpBigQueryRpc.java:186)
-#         at com.google.cloud.bigquery.BigQueryImpl$2.call(BigQueryImpl.java:253)
-#         at com.google.cloud.bigquery.BigQueryImpl$2.call(BigQueryImpl.java:250)
-#         at com.google.api.gax.retrying.DirectRetryingExecutor.submit(DirectRetryingExecutor.java:105)
-#         at com.google.cloud.RetryHelper.run(RetryHelper.java:76)
-#         at com.google.cloud.RetryHelper.runWithRetries(RetryHelper.java:50)
-#         at com.google.cloud.bigquery.BigQueryImpl.create(BigQueryImpl.java:249)
-#         at com.wepay.kafka.connect.bigquery.SchemaManager.createTable(SchemaManager.java:229)
-#         at com.wepay.kafka.connect.bigquery.write.row.AdaptiveBigQueryWriter.attemptTableCreate(AdaptiveBigQueryWriter.java:168)
-#         ... 6 more
-# Caused by: com.google.api.client.googleapis.json.GoogleJsonResponseException: 400 Bad Request
-# POST https://www.googleapis.com/bigquery/v2/projects/vincent-de-saboulin-lab/datasets/pgvsaboulinds611/tables?prettyPrint=false
-# {
-#   "code" : 400,
-#   "errors" : [ {
-#     "domain" : "global",
-#     "message" : "Invalid field name \"com.example.InnerStruct\". Fields must contain only letters, numbers, and underscores, start with a letter or underscore, and be at most 300 characters long.",
-#     "reason" : "invalid"
-#   } ],
-#   "message" : "Invalid field name \"com.example.InnerStruct\". Fields must contain only letters, numbers, and underscores, start with a letter or underscore, and be at most 300 characters long.",
-#   "status" : "INVALID_ARGUMENT"
-# }
-#         at com.google.api.client.googleapis.json.GoogleJsonResponseException.from(GoogleJsonResponseException.java:149)
-#         at com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest.newExceptionOnError(AbstractGoogleJsonClientRequest.java:112)
-#         at com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest.newExceptionOnError(AbstractGoogleJsonClientRequest.java:39)
-#         at com.google.api.client.googleapis.services.AbstractGoogleClientRequest$1.interceptResponse(AbstractGoogleClientRequest.java:443)
-#         at com.google.api.client.http.HttpRequest.execute(HttpRequest.java:1108)
-#         at com.google.api.client.googleapis.services.AbstractGoogleClientRequest.executeUnparsed(AbstractGoogleClientRequest.java:541)
-#         at com.google.api.client.googleapis.services.AbstractGoogleClientRequest.executeUnparsed(AbstractGoogleClientRequest.java:474)
-#         at com.google.api.client.googleapis.services.AbstractGoogleClientRequest.execute(AbstractGoogleClientRequest.java:591)
-#         at com.google.cloud.bigquery.spi.v2.HttpBigQueryRpc.create(HttpBigQueryRpc.java:184)
-#         ... 14 more
+# [2021-06-05 15:54:20,566] INFO Putting 1 records in the sink. (com.wepay.kafka.connect.bigquery.BigQuerySinkTask)
+# [2021-06-05 15:54:21,735] ERROR Task failed with java.lang.StackOverflowError error: null (com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor)
+# Exception in thread "pool-4-thread-1" java.lang.StackOverflowError
+#         at com.wepay.kafka.connect.bigquery.convert.BigQuerySchemaConverter.convertField(BigQuerySchemaConverter.java:120)
