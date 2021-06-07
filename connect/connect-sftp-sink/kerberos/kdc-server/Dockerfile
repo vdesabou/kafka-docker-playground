031b9b14d538bf8affec4b8f0b74061acb19e2a8
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-07 18:29:06 +0200

    Add SFTP sink example with kerberos #1055

---
+++ connect/connect-sftp-sink/kerberos/kdc-server/Dockerfile
@@ -0,0 +1,17 @@
+FROM debian:jessie
+
+EXPOSE 749 88/udp
+
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get -qq update
+RUN apt-get -qq install locales krb5-kdc krb5-admin-server
+RUN apt-get -qq install vim
+RUN apt-get -qq clean
+
+ENV REALM ${REALM:-EXAMPLE.COM}
+ENV SUPPORTED_ENCRYPTION_TYPES ${SUPPORTED_ENCRYPTION_TYPES:-aes256-cts-hmac-sha1-96:normal}
+ENV KADMIN_PRINCIPAL ${KADMIN_PRINCIPAL:-kadmin/admin}
+ENV KADMIN_PASSWORD ${KADMIN_PASSWORD:-adminpassword}
+
+COPY init-script.sh /tmp/
+CMD /tmp/init-script.sh
---
+++ connect/connect-sftp-sink/sftp-sink-kerberos.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.kerberos.yml"
+
+# following https://www.confluent.io/blog/containerized-testing-with-kerberos-and-ssh/
+log "Add kerberos principals"
+docker exec -i kdc-server kadmin.local << EOF
+addprinc -randkey host/ssh-server.kerberos-demo.local@EXAMPLE.COM
+ktadd -k /sshserver.keytab host/ssh-server.kerberos-demo.local@EXAMPLE.COM
+addprinc -randkey sshuser@EXAMPLE.COM
+ktadd -k /sshuser.keytab sshuser@EXAMPLE.COM
+listprincs
+EOF
+
+log "Copy sshuser.keytab to connect container /home/appuser/sshuser.keytab"
+docker cp kdc-server:/sshuser.keytab .
+docker cp sshuser.keytab connect:/home/appuser/sshuser.keytab
+docker exec -u 0 connect chown appuser:appuser sshuser.keytab
+
+log "Copy sshserver.keytab to ssh server /etc/krb5.keytab"
+docker cp kdc-server:/sshserver.keytab .
+docker cp sshserver.keytab ssh-server:/etc/krb5.keytab
+docker exec -u 0 ssh-server chown root:root /etc/krb5.keytab
+
+log "Add sshuser"
+docker exec -i ssh-server adduser sshuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" << EOF
+confluent
+confluent
+EOF
+
+docker exec ssh-server bash -c "
+mkdir -p /home/sshuser/upload/input
+mkdir -p /home/sshuser/upload/error
+mkdir -p /home/sshuser/upload/finished
+
+chown -R sshuser /home/sshuser/upload
+"
+
+# FIXTHIS: it is required to do kinit manually
+docker exec connect kinit sshuser -k -t sshuser.keytab
+# if required to troubleshoot
+# docker exec -i --privileged --user root connect bash -c "yum update -y && yum install openssh-clients -y"
+
+log "Creating SFTP Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "topics": "test_sftp_sink",
+               "tasks.max": "1",
+               "connector.class": "io.confluent.connect.sftp.SftpSinkConnector",
+               "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
+               "schema.generator.class": "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator",
+               "flush.size": "3",
+               "schema.compatibility": "NONE",
+               "format.class": "io.confluent.connect.sftp.sink.format.avro.AvroFormat",
+               "storage.class": "io.confluent.connect.sftp.sink.storage.SftpSinkStorage",
+               "sftp.username":"sshuser",
+               "kerberos.keytab.path": "/home/appuser/sshuser.keytab",
+               "kerberos.user.principal": "sshuser",
+               "sftp.host":"ssh-server",
+               "sftp.port":"22",
+               "sftp.working.dir": "/home/sshuser/upload",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/sftp-sink-kerberos/config | jq .
+
+
+log "Sending messages to topic test_sftp_sink"
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_sftp_sink --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+sleep 10
+
+log "Listing content of ./upload/topics/test_sftp_sink/partition\=0/"
+docker exec ssh-server bash -c "ls /home/sshuser/upload/topics/test_sftp_sink/partition\=0/"
+
+docker cp ssh-server:/home/sshuser/upload/topics/test_sftp_sink/partition\=0/test_sftp_sink+0+0000000000.avro /tmp/
+
+docker run -v /tmp:/tmp actions/avro-tools tojson /tmp/test_sftp_sink+0+0000000000.avro
