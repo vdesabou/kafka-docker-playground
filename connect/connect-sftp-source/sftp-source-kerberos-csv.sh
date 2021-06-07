481132b4eb0a5e54b744d19c004f27af9d80cc08
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-07 18:14:23 +0200

    #1019 Now it is really working :)
    Former-commit-id: 5496b123cb3a65150d7a1804ae92118feed6ceb7 [formerly 4c3fcec59e9bb10e2726c35c0d2c431a8cd008e0 [formerly 0a1f8be7b612ab60246fc739865e145a165f0ef9]]
    Former-commit-id: 96a31acc43e7eadb80a427e43ad96d93cde65685
    Former-commit-id: 4abe6170a8b0ae309d66bcd37ba127702b699eb9

--- connect/connect-sftp-source/sftp-source-kerberos-csv.sh
+++ connect/connect-sftp-source/sftp-source-kerberos-csv.sh
@@ -9,21 +9,25 @@ ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.ker
 # following https://www.confluent.io/blog/containerized-testing-with-kerberos-and-ssh/
 log "Add kerberos principals"
 docker exec -i kdc-server kadmin.local << EOF
-addprinc -randkey host/ssh-server@EXAMPLE.COM
-ktadd -k /sshserver.keytab host/ssh-server@EXAMPLE.COM
-addprinc -randkey sshuser
+addprinc -randkey host/ssh-server.kerberos-demo.local@EXAMPLE.COM
+ktadd -k /sshserver.keytab host/ssh-server.kerberos-demo.local@EXAMPLE.COM
+addprinc -randkey sshuser@EXAMPLE.COM
 ktadd -k /sshuser.keytab sshuser@EXAMPLE.COM
+listprincs
 EOF
 
+log "Copy sshuser.keytab to connect container /home/appuser/sshuser.keytab"
 docker cp kdc-server:/sshuser.keytab .
 docker cp sshuser.keytab connect:/home/appuser/sshuser.keytab
 docker exec -u 0 connect chown appuser:appuser sshuser.keytab
 
+log "Copy sshserver.keytab to ssh server /etc/krb5.keytab"
 docker cp kdc-server:/sshserver.keytab .
 docker cp sshserver.keytab ssh-server:/etc/krb5.keytab
+docker exec -u 0 ssh-server chown root:root /etc/krb5.keytab
 
 log "Add sshuser"
-docker exec -i ssh-server adduser sshuser << EOF
+docker exec -i ssh-server adduser sshuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" << EOF
 confluent
 confluent
 EOF
@@ -36,12 +40,10 @@ mkdir -p /home/sshuser/upload/finished
 chown -R sshuser /home/sshuser/upload
 "
 
-docker container restart ssh-server
-
-# just to make sure we can kinit
+# FIXTHIS: it is required to do kinit manually
 docker exec connect kinit sshuser -k -t sshuser.keytab
 # if required to troubleshoot
-docker exec -i --privileged --user root connect bash -c "yum update -y && yum install openssh-clients -y"
+# docker exec -i --privileged --user root connect bash -c "yum update -y && yum install openssh-clients -y"
 
 echo $'id,first_name,last_name,email,gender,ip_address,last_login,account_balance,country,favorite_color\n1,Salmon,Baitman,sbaitman0@feedburner.com,Male,120.181.75.98,2015-03-01T06:01:15Z,17462.66,IT,#f09bc0\n2,Debby,Brea,dbrea1@icio.us,Female,153.239.187.49,2018-10-21T12:27:12Z,14693.49,CZ,#73893a' > csv-sftp-source.csv
 docker cp csv-sftp-source.csv ssh-server:/home/sshuser/upload/input/
