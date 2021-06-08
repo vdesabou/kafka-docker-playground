8de949f0e1c689d15ef1308c8dafd1cb0ef7fa76
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-08 14:07:13 +0200

    #1054 fix zip name
    Former-commit-id: 0bb9ffe6bb721390ee03e85ff0b512ff83eb5ce5 [formerly 080b64759360369e89840169acb4a28257c3615a [formerly 2d4c8e153ebfa8519788f57e2cac383a8df4f888]]
    Former-commit-id: 9dff3281f03ea1f1238e67b7eae96cea9bfe5ff7
    Former-commit-id: f0befbd43a452a3788ee98e520b7bd618e22bd79

--- connect/connect-cdc-oracle19-source/cdc-oracle19-cdb-table.sh
+++ connect/connect-cdc-oracle19-source/cdc-oracle19-cdb-table.sh
@@ -4,31 +4,28 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-if [ -z "$CI" ]
-then
-     # not running with github actions
-     if test -z "$(docker images -q oracle/database:19.3.0-ee)"
-     then
-          if [ ! -f ${DIR}/LINUX.X64_193000_db_home.zip ]
-          then
-               logerror "ERROR: ${DIR}/LINUX.X64_193000_db_home.zip is missing. It must be downloaded manually in order to acknowledge user agreement"
-               exit 1
-          fi
-          log "Building oracle/database:19.3.0-ee docker image..it can take a while...(more than 15 minutes!)"
-          OLDDIR=$PWD
-          rm -rf ${DIR}/docker-images
-          git clone https://github.com/oracle/docker-images.git
+export ORACLE_IMAGE="oracle/database:19.3.0-ee"
 
-          cp ${DIR}/LINUX.X64_193000_db_home.zip ${DIR}/docker-images/OracleDatabase/SingleInstance/dockerfiles/19.3.0/LINUX.X64_193000_db_home.zip
-          cd ${DIR}/docker-images/OracleDatabase/SingleInstance/dockerfiles
-          ./buildContainerImage.sh -v 19.3.0 -e
-          rm -rf ${DIR}/docker-images
-          cd ${OLDDIR}
-     fi
+if test -z "$(docker images -q oracle/database:19.3.0-ee)"
+then
+    if [ ! -f ${DIR}/LINUX.X64_193000_db_home.zip ]
+    then
+        logerror "ERROR: ${DIR}/LINUX.X64_193000_db_home.zip is missing. It must be downloaded manually in order to acknowledge user agreement"
+        exit 1
+    fi
+    log "Building oracle/database:19.3.0-ee docker image..it can take a while...(more than 15 minutes!)"
+    OLDDIR=$PWD
+    rm -rf ${DIR}/docker-images
+    git clone https://github.com/oracle/docker-images.git
+
+    cp ${DIR}/LINUX.X64_193000_db_home.zip ${DIR}/docker-images/OracleDatabase/SingleInstance/dockerfiles/19.3.0/LINUX.X64_193000_db_home.zip
+    cd ${DIR}/docker-images/OracleDatabase/SingleInstance/dockerfiles
+    ./buildContainerImage.sh -v 19.3.0 -e
+    rm -rf ${DIR}/docker-images
+    cd ${OLDDIR}
 fi
 
 export ORACLE_IMAGE="oracle/database:19.3.0-ee"
-
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.cdb-table.yml"
 
