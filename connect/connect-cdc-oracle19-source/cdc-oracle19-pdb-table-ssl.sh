7e87a215c3e44efa175a054fce12d9076e39d6f8
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-08 10:47:38 +0200

    #1054 skeleton

---
+++ connect/connect-cdc-oracle19-source/cdc-oracle19-pdb-table-ssl.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if test -z "$(docker images -q oracle/database:19.3.0-ee)"
+then
+    if [ ! -f ${DIR}/linuxx64_12201_database.zip ]
+    then
+        logerror "ERROR: ${DIR}/linuxx64_12201_database.zip is missing. It must be downloaded manually in order to acknowledge user agreement"
+        exit 1
+    fi
+    log "Building oracle/database:19.3.0-ee docker image..it can take a while...(more than 15 minutes!)"
+    OLDDIR=$PWD
+    rm -rf ${DIR}/docker-images
+    git clone https://github.com/oracle/docker-images.git
+
+    cp ${DIR}/linuxx64_12201_database.zip ${DIR}/docker-images/OracleDatabase/SingleInstance/dockerfiles/19.3.0/linuxx64_12201_database.zip
+    cd ${DIR}/docker-images/OracleDatabase/SingleInstance/dockerfiles
+    ./buildContainerImage.sh -v 19.3.0 -e
+    rm -rf ${DIR}/docker-images
+    cd ${OLDDIR}
+fi
+
+export ORACLE_IMAGE="oracle/database:19.3.0-ee"
+
+# required to make utils.sh script being able to work, do not remove:
+# ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.pdb-table.yml"
+log "Starting up oracle container to get generated cert from oracle server wallet"
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/docker-compose.plaintext.pdb-table-ssl.yml" up -d oracle
+
+# Verify Oracle DB has started within MAX_WAIT seconds
+MAX_WAIT=1800
+CUR_WAIT=0
+log "Waiting up to $MAX_WAIT seconds for Oracle DB to start"
+docker container logs oracle > /tmp/out.txt 2>&1
+while [[ ! $(cat /tmp/out.txt) =~ "DONE: Executing user defined scripts" ]]; do
+sleep 10
+docker container logs oracle > /tmp/out.txt 2>&1
+CUR_WAIT=$(( CUR_WAIT+10 ))
+if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+     logerror "ERROR: The logs in oracle container do not show 'DONE: Executing user defined scripts' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+     exit 1
+fi
+done
+log "Oracle DB has started!"
+
+log "Grant select on CUSTOMERS table"
+docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLPDB1 << EOF
+     ALTER SESSION SET CONTAINER=ORCLPDB1;
+     GRANT select on CUSTOMERS TO C##MYUSER;
+EOF
+
+log "Setting up SSL on oracle server..."
+# https://www.oracle.com/technetwork/topics/wp-oracle-jdbc-thin-ssl-130128.pdf
+log "Create a wallet for the test CA"
+
+docker exec oracle bash -c "orapki wallet create -wallet /tmp/root -pwd WalletPasswd123"
+# Add a self-signed certificate to the wallet
+docker exec oracle bash -c "orapki wallet add -wallet /tmp/root -dn CN=root_test,C=US -keysize 2048 -self_signed -validity 3650 -pwd WalletPasswd123"
+# Export the certificate
+docker exec oracle bash -c "orapki wallet export -wallet /tmp/root -dn CN=root_test,C=US -cert /tmp/root/b64certificate.txt -pwd WalletPasswd123"
+
+log "Create a wallet for the Oracle server"
+
+# Create an empty wallet with auto login enabled
+docker exec oracle bash -c "orapki wallet create -wallet /tmp/server -pwd WalletPasswd123 -auto_login"
+# Add a user In the wallet (a new pair of private/public keys is created)
+docker exec oracle bash -c "orapki wallet add -wallet /tmp/server -dn CN=server,C=US -pwd WalletPasswd123 -keysize 2048"
+# Export the certificate request to a file
+docker exec oracle bash -c "orapki wallet export -wallet /tmp/server -dn CN=server,C=US -pwd WalletPasswd123 -request /tmp/server/creq.txt"
+# Using the test CA, sign the certificate request
+docker exec oracle bash -c "orapki cert create -wallet /tmp/root -request /tmp/server/creq.txt -cert /tmp/server/cert.txt -validity 3650 -pwd WalletPasswd123"
+log "You now have the following files under the /tmp/server directory"
+docker exec oracle ls /tmp/server
+# View the signed certificate:
+docker exec oracle bash -c "orapki cert display -cert /tmp/server/cert.txt -complete"
+# Add the test CA's trusted certificate to the wallet
+docker exec oracle bash -c "orapki wallet add -wallet /tmp/server -trusted_cert -cert /tmp/root/b64certificate.txt -pwd WalletPasswd123"
+# add the user certificate to the wallet:
+docker exec oracle bash -c "orapki wallet add -wallet /tmp/server -user_cert -cert /tmp/server/cert.txt -pwd WalletPasswd123"
+
+cd ${DIR}/ssl
+if [ -z "$CI" ]
+then
+    # not running with github actions
+    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
+    chmod -R a+rw .
+else
+    # docker is run as runneradmin user, need to use sudo
+    ls -lrt
+    sudo chmod -R a+rw .
+    ls -lrt
+fi
+log "Create a JKS truststore"
+rm -f truststore.jks
+docker cp oracle:/tmp/root/b64certificate.txt b64certificate.txt
+if [ -z "$CI" ]
+then
+    # not running with github actions
+    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
+    chmod -R a+rw .
+else
+    # docker is run as runneradmin user, need to use sudo
+    ls -lrt
+    sudo chmod -R a+rw .
+    ls -lrt
+fi
+# We import the test CA certificate
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -import -v -alias testroot -file /tmp/b64certificate.txt -keystore /tmp/truststore.jks -storetype JKS -storepass 'welcome123' -noprompt
+log "Displaying truststore"
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -list -keystore /tmp/truststore.jks -storepass 'welcome123' -v
+
+cd ${DIR}
+
+log "Update listener.ora, sqlnet.ora and tnsnames.ora"
+docker cp ${PWD}/ssl/listener.ora oracle:/opt/oracle/oradata/dbconfig/ORCLCDB/listener.ora
+docker cp ${PWD}/ssl/sqlnet.ora oracle:/opt/oracle/oradata/dbconfig/ORCLCDB/sqlnet.ora
+docker cp ${PWD}/ssl/tnsnames.ora oracle:/opt/oracle/oradata/dbconfig/ORCLCDB/tnsnames.ora
+
+docker exec -i oracle lsnrctl << EOF
+reload
+stop
+start
+EOF
+
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/docker-compose.plaintext.pdb-table-ssl.yml" up -d
+
+../../scripts/wait-for-connect-and-controlcenter.sh
+
+docker exec connect kafka-topics --create --topic redo-log-topic --bootstrap-server broker:9092 --replication-factor 1 --partitions 1 --config cleanup.policy=delete --config retention.ms=120960000
+log "redo-log-topic is created"
+
+sleep 15
+
+log "Creating Oracle source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.oracle.cdc.OracleCdcSourceConnector",
+               "tasks.max":1,
+               "key.converter": "io.confluent.connect.avro.AvroConverter",
+               "key.converter.schema.registry.url": "http://schema-registry:8081",
+               "value.converter": "io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url": "http://schema-registry:8081",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1",
+               "oracle.server": "oracle",
+               "oracle.port": 1532,
+               "oracle.sid": "ORCLCDB",
+               "oracle.pdb.name": "ORCLPDB1",
+               "oracle.username": "C##MYUSER",
+               "oracle.password": "mypassword",
+               "oracle.ssl.truststore.file": "/tmp/truststore.jks",
+               "oracle.ssl.truststore.password": "welcome123",
+               "start.from":"snapshot",
+               "redo.log.topic.name": "redo-log-topic",
+               "redo.log.consumer.bootstrap.servers":"broker:9092",
+               "table.inclusion.regex": "ORCLPDB1[.].*[.]CUSTOMERS",
+               "table.topic.name.template": "${databaseName}.${schemaName}.${tableName}",
+               "connection.pool.max.size": 20,
+               "confluent.topic.replication.factor":1
+          }' \
+     http://localhost:8083/connectors/cdc-oracle-source-pdb/config | jq .
+
+log "Waiting 60s for cdc-oracle-source-cdb to read existing data"
+sleep 60
+
+log "Running SQL scripts"
+for script in ${DIR}/sample-sql-scripts/*
+do
+     $script "ORCLPDB1"
+done
+
+log "Verifying topic ORCLPDB1.C__MYUSER.CUSTOMERS"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic ORCLPDB1.C__MYUSER.CUSTOMERS --from-beginning --max-messages 2
---
+++ connect/connect-cdc-oracle19-source/stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+export ORACLE_IMAGE="oracle/database:19.3.0-ee"
+
+
+stop_all "$DIR"
