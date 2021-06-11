a759a29d2faa65e7756dc12b910c96834b73237e
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-11 16:26:07 +0200

    #938 Add SSL with JTDS example

---
+++ connect/connect-jdbc-sqlserver-sink/sqlserver-jtds-sink-ssl.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+cd ${DIR}/ssl
+if [ -z "$CI" ]
+then
+    # not running with github actions
+    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
+    chmod -R a+rw .
+else
+    # docker is run as runneradmin user, need to use sudo
+    ls -lrt
+    sudo chmod -R a+rw .
+    ls -lrt
+fi
+
+rm -f mssql.pem
+rm -f mssql.key
+
+#https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-docker-container-security?view=sql-server-ver15
+#https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-encrypted-connections?view=sql-server-ver15&preserve-view=true#client-initiated-encryption
+log "Create a self-signed certificate"
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl req -x509 -nodes -newkey rsa:2048 -subj '/CN=sqlserver' -keyout /tmp/mssql.key -out /tmp/mssql.pem -days 365
+
+if [ -z "$CI" ]
+then
+    # not running with github actions
+    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
+    chmod -R a+rw .
+else
+    # docker is run as runneradmin user, need to use sudo
+    ls -lrt
+    sudo chmod -R a+rw .
+    ls -lrt
+fi
+
+log "Creating JKS from pem files"
+rm -f truststore.jks
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -importcert -alias MSSQLCACert -noprompt -file /tmp/mssql.pem -keystore /tmp/truststore.jks -storepass confluent
+
+if [ -z "$CI" ]
+then
+    # not running with github actions
+    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
+    chmod -R a+rw .
+else
+    # docker is run as runneradmin user, need to use sudo
+    ls -lrt
+    sudo chmod -R a+rw .
+    ls -lrt
+fi
+
+cd -
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.jtds-ssl.yml"
+
+log "Creating JDBC SQL Server (with JTDS driver) sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+                    "tasks.max": "1",
+                    "connection.url": "jdbc:jtds:sqlserver://sqlserver:1433;ssl=require",
+                    "connection.user": "sa",
+                    "connection.password": "Password!",
+                    "topics": "orders",
+                    "auto.create": "true"
+          }' \
+     http://localhost:8083/connectors/sqlserver-sink-ssl/config | jq .
+
+log "Sending messages to topic orders"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic orders --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"id","type":"int"},{"name":"product", "type": "string"}, {"name":"quantity", "type": "int"}, {"name":"price",
+"type": "float"}]}' << EOF
+{"id": 999, "product": "foo", "quantity": 100, "price": 50}
+EOF
+
+sleep 5
+
+log "Show content of orders table:"
+docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! > /tmp/result.log  2>&1 <<-EOF
+select * from orders
+GO
+EOF
+cat /tmp/result.log
+grep "foo" /tmp/result.log
