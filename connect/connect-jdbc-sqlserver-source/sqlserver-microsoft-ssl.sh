0d9118c4e40eff8b2c5e7694485dab59a19fa4ef
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-11 16:07:38 +0200

    #938 example with microsoft driver

---
+++ connect/connect-jdbc-sqlserver-source/sqlserver-microsoft-ssl.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f ${DIR}/sqljdbc_7.4/enu/mssql-jdbc-7.4.1.jre8.jar ]
+then
+     log "Downloading Microsoft JDBC driver mssql-jdbc-7.4.1.jre8.jar"
+     wget https://download.microsoft.com/download/6/9/9/699205CA-F1F1-4DE9-9335-18546C5C8CBD/sqljdbc_7.4.1.0_enu.tar.gz
+     tar xvfz sqljdbc_7.4.1.0_enu.tar.gz
+     rm -f sqljdbc_7.4.1.0_enu.tar.gz
+fi
+
+cd ${DIR}/ssl
+if [ -z "$CI" ]
+then
+    # not running with github actions
+    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
+    chmod -R a+rw .
+else
+    # docker is run as runneradmin user, need to use sudo
+    ls -lrt
+    sudo chmod -R a+rw .
+    ls -lrt
+fi
+
+rm -f mssql.pem
+rm -f mssql.key
+
+#https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-docker-container-security?view=sql-server-ver15
+#https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-encrypted-connections?view=sql-server-ver15&preserve-view=true#client-initiated-encryption
+log "Create a self-signed certificate"
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} openssl req -x509 -nodes -newkey rsa:2048 -subj '/CN=sqlserver' -keyout /tmp/mssql.key -out /tmp/mssql.pem -days 365
+
+if [ -z "$CI" ]
+then
+    # not running with github actions
+    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
+    chmod -R a+rw .
+else
+    # docker is run as runneradmin user, need to use sudo
+    ls -lrt
+    sudo chmod -R a+rw .
+    ls -lrt
+fi
+
+log "Creating JKS from pem files"
+rm -f truststore.jks
+docker run --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} keytool -importcert -alias MSSQLCACert -noprompt -file /tmp/mssql.pem -keystore /tmp/truststore.jks -storepass confluent
+
+if [ -z "$CI" ]
+then
+    # not running with github actions
+    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
+    chmod -R a+rw .
+else
+    # docker is run as runneradmin user, need to use sudo
+    ls -lrt
+    sudo chmod -R a+rw .
+    ls -lrt
+fi
+
+cd -
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.microsoft-ssl.yml"
+
+# Removed pre-installed JTDS driver
+docker exec connect rm -f /usr/share/confluent-hub-components/confluentinc-kafka-connect-jdbc/lib/jtds-1.3.1.jar
+docker container restart connect
+
+log "sleeping 60 seconds"
+sleep 60
+
+log "Load inventory.sql to SQL Server"
+cat inventory.sql | docker exec -i sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P Password!'
+
+
+log "Creating JDBC SQL Server (with Microsoft driver) source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+                    "tasks.max": "1",
+                    "connection.url": "jdbc:sqlserver://sqlserver:1433;databaseName=testDB;encrypt=true;trustServerCertificate=false;",
+                    "connection.user": "sa",
+                    "connection.password": "Password!",
+                    "table.whitelist": "customers",
+                    "mode": "incrementing",
+                    "incrementing.column.name": "id",
+                    "topic.prefix": "sqlserver-",
+                    "validate.non.null":"false",
+                    "errors.log.enable": "true",
+                    "errors.log.include.messages": "true"
+          }' \
+     http://localhost:8083/connectors/sqlserver-source-ssl/config | jq .
+
+sleep 5
+
+docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! << EOF
+USE testDB;
+INSERT INTO customers(first_name,last_name,email) VALUES ('Pam','Thomas','pam@office.com');
+GO
+EOF
+
+log "Verifying topic sqlserver-customers"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic sqlserver-customers --from-beginning --max-messages 5
