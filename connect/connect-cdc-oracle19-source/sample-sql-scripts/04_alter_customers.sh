f6833f66a6e4f9c80f1f7432c428b4d46f442ba9
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-15 16:17:10 +0200

    Update Oracle 19c to not include ALTER TABLE ADD #1109
    https://github.com/vdesabou/kafka-docker-playground/issues/1109

--- connect/connect-cdc-oracle19-source/sample-sql-scripts/04_alter_customers.sh
+++ connect/connect-cdc-oracle19-source/sample-sql-scripts/04_alter_customers.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Update Oracle 19c to not include ALTER TABLE ADD #1109
+# https://github.com/vdesabou/kafka-docker-playground/issues/1109
+# FIXTHIS
+
+echo 'DDL is not fully supported for now'
+exit 0
+
 echo 'Altering CUSTOMERS table with an optional column'
 
 docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/$1 << EOF
