87889edf0200df4553477dc16ccef7b86c1af656
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-18 17:44:42 +0200

    Simplify file stream examples #1118
    missing commit
    
    
    Former-commit-id: d3b7d00560dc26f8861d0000f29fc35e6e83ff95 [formerly a888ef7aeff3d29a44cbd9f700c259adf22b7e8b [formerly 0f5df504eb70cb99e2388310b0d4e3a9f8c02a84]]
    Former-commit-id: 5e47f7a5254375eb3c734d23edd12d519cd157a9
    Former-commit-id: 27922e58458dc43f8733c5905f7b119955efb2d8

--- connect/connect-filestream-sink/filestream-sink.sh
+++ connect/connect-filestream-sink/filestream-sink.sh
@@ -4,19 +4,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-mkdir -p ${DIR}/data/ouput
-
-# workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
-chmod -R a+rw ${DIR}/data
-
-if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
-then
-     export CONNECT_CONTAINER_HOME_DIR="/home/appuser"
-else
-     export CONNECT_CONTAINER_HOME_DIR="/root"
-fi
-
-${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+${DIR}/../../environment/plaintext/start.sh
 
 log "Sending messages to topic filestream"
 docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic filestream << EOF
@@ -32,7 +20,7 @@ curl -X PUT \
                "tasks.max": "1",
                "connector.class": "FileStreamSink",
                "topics": "filestream",
-               "file": "'"$OUTPUT_FILE"'",
+               "file": "/tmp/output.json",
                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
                "value.converter.schemas.enable": "false"
@@ -43,4 +31,4 @@ curl -X PUT \
 sleep 5
 
 log "Verify we have received the data in file"
-docker exec connect cat $OUTPUT_FILE
+docker exec connect cat /tmp/output.json
--- connect/connect-filestream-source/filestream-source.sh
+++ connect/connect-filestream-source/filestream-source.sh
@@ -4,27 +4,10 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-mkdir -p ${DIR}/data/input
+${DIR}/../../environment/plaintext/start.sh
 
-# workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
-chmod -R a+rw ${DIR}/data
-
-if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
-then
-     export CONNECT_CONTAINER_HOME_DIR="/home/appuser"
-else
-     export CONNECT_CONTAINER_HOME_DIR="/root"
-fi
-
-${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
-
-if [ ! -f "${DIR}/data/input/file.json" ]
-then
-     log "Generating data"
-     curl "https://api.mockaroo.com/api/17c84440?count=500&key=25fd9c80" > "${DIR}/data/input/file.json"
-fi
-
-INPUT_FILE="${CONNECT_CONTAINER_HOME_DIR}/data/input/file.json"
+log "Generating data"
+docker exec -i connect bash -c "mkdir -p /tmp/kafka-connect/examples/ && curl -sSL 'https://api.mockaroo.com/api/17c84440?count=500&key=25fd9c80' -o /tmp/kafka-connect/examples/file.json"
 
 log "Creating FileStream Source connector"
 curl -X PUT \
@@ -33,7 +16,7 @@ curl -X PUT \
                "tasks.max": "1",
                "connector.class": "FileStreamSource",
                "topic": "filestream",
-               "file": "'"$INPUT_FILE"'",
+               "file": "/tmp/kafka-connect/examples/file.json",
                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
                "value.converter.schemas.enable": "false"
