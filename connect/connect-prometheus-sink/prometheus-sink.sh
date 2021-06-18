3c1ad5a98a7e5549007a44c20184b33b09e505a4
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-18 14:00:26 +0200

    FIXTHIS: if  we execute same command again, it will show no results....

--- connect/connect-prometheus-sink/prometheus-sink.sh
+++ connect/connect-prometheus-sink/prometheus-sink.sh
@@ -6,14 +6,6 @@ source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
-NOW=$(date +%s)
-log "Sending messages to topic test-topic"
-docker exec -i -e NOW=$NOW connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test-topic --property value.schema='{"name": "metric","type": "record","fields": [{"name": "name","type": "string"},{"name": "type","type": "string"},{"name": "timestamp","type": "long"},{"name": "values","type": {"name": "values","type": "record","fields": [{"name":"doubleValue", "type": "double"}]}}]}' << EOF
-{"name":"kafka_gaugeMetric1", "type":"gauge","timestamp": $NOW,"values": {"doubleValue": 5.639623848362502}}
-{"name":"kafka_gaugeMetric2", "type":"gauge","timestamp": $NOW,"values": {"doubleValue": 5.639623848362502}}
-{"name":"kafka_gaugeMetric3", "type":"gauge","timestamp": $NOW,"values": {"doubleValue": 5.639623848362502}}
-EOF
-
 log "Creating Prometheus sink connector"
 curl -X PUT \
      -H "Content-Type: application/json" \
@@ -35,9 +27,22 @@ curl -X PUT \
           }' \
      http://localhost:8083/connectors/prometheus-sink/config | jq .
 
-sleep 30
+NOW=$(date +%s)
+log "Sending messages to topic test-topic"
+docker exec -i -e NOW=$NOW connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test-topic --property value.schema='{"name": "metric","type": "record","fields": [{"name": "name","type": "string"},{"name": "type","type": "string"},{"name": "timestamp","type": "long"},{"name": "values","type": {"name": "values","type": "record","fields": [{"name":"doubleValue", "type": "double"}]}}]}' << EOF
+{"name":"kafka_gaugeMetric1", "type":"gauge","timestamp": $NOW,"values": {"doubleValue": 5.639623848362502}}
+{"name":"kafka_gaugeMetric2", "type":"gauge","timestamp": $NOW,"values": {"doubleValue": 5.639623848362502}}
+{"name":"kafka_gaugeMetric3", "type":"gauge","timestamp": $NOW,"values": {"doubleValue": 5.639623848362502}}
+{"name":"kafka_gaugeMetric4", "type":"gauge","timestamp": $NOW,"values": {"doubleValue": 5.639623848362502}}
+{"name":"kafka_gaugeMetric5", "type":"gauge","timestamp": $NOW,"values": {"doubleValue": 5.639623848362502}}
+{"name":"kafka_gaugeMetric6", "type":"gauge","timestamp": $NOW,"values": {"doubleValue": 5.639623848362502}}
+EOF
+
+sleep 6
 
 log "Verify data is in Prometheus"
 curl 'http://localhost:9090/api/v1/query?query=kafka_gaugeMetric1' > /tmp/result.log  2>&1
 cat /tmp/result.log
 grep "kafka_gaugeMetric1" /tmp/result.log
+
+# FIXTHIS: if  we execute same command again, it will show no results....
