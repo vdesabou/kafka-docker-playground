5ca4b69c68147c128a469b031ab13a144c36ddf7
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-24 17:18:56 +0200

    Provide an easy way to see container properties files #1138

---
+++ scripts/get-properties.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../scripts/utils.sh
+
+container="$1"
+
+if [ "$container" = "" ]
+then
+  logerror "ERROR: container name is not provided as argument!"
+  exit 1
+fi
+
+docker exec -i $container sh << EOF
+ps -ef | grep properties | grep java | grep -v grep | awk '{ print \$NF }' > /tmp/propertie_file
+propertie_file=\$(cat /tmp/propertie_file)
+if [ ! -f \$propertie_file ]
+then
+  echo 'ERROR: Could not determine properties file!'
+  exit 1
+fi
+cat \$propertie_file | grep -v None | grep . | sort
+EOF
+
