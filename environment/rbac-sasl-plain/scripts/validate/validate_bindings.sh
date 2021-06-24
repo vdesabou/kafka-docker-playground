f416c406d3466bd37496d6788938f41929ff920c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-24 15:49:15 +0200

    Add ksqldb to environment/rbac-sasl-plain #1130
    Former-commit-id: f850a4c4d201f2adcfbd416baf14b49f95ea601e [formerly 403013ad1680ce832b1156ece32eab1098a3b18b [formerly 12101765d27ccccc2ddfd16a0c70e2d2b6715ff9]]
    Former-commit-id: 0867b769ad4d39e7759a692b1cec3f97f2f94493
    Former-commit-id: 81d646564990b820621e26d3e424fd061a522a9e

--- environment/rbac-sasl-plain/scripts/validate/validate_bindings.sh
+++ environment/rbac-sasl-plain/scripts/validate/validate_bindings.sh
@@ -13,6 +13,7 @@ MDS_URL=http://broker:8091
 CONNECT=connect-cluster
 SR=schema-registry
 C3=c3-cluster
+KSQLDB=ksql-cluster
 
 SUPER_USER=superUser
 SUPER_USER_PASSWORD=superUser
@@ -21,13 +22,16 @@ CONNECT_ADMIN="User:connectAdmin"
 CONNECTOR_SUBMITTER="User:connectorSubmitter"
 CONNECTOR_PRINCIPAL="User:connectorSA"
 SR_PRINCIPAL="User:schemaregistryUser"
+KSQLDB_ADMIN="User:ksqlDBAdmin"
+KSQLDB_USER="User:ksqlDBUser"
+KSQLDB_SERVER="User:controlCenterAndKsqlDBServer"
 C3_ADMIN="User:controlcenterAdmin"
 CLIENT_AVRO_PRINCIPAL="User:clientAvroCli"
 
 ################################## Run through permutations #############################
 
-for p in $SUPER_USER_PRINCIPAL $CONNECT_ADMIN $CONNECTOR_SUBMITTER $CONNECTOR_PRINCIPAL $SR_PRINCIPAL $C3_ADMIN $CLIENT_AVRO_PRINCIPAL; do
-  for c in " " " --schema-registry-cluster-id $SR" " --connect-cluster-id $CONNECT"; do
+for p in $SUPER_USER_PRINCIPAL $CONNECT_ADMIN $CONNECTOR_SUBMITTER $CONNECTOR_PRINCIPAL $SR_PRINCIPAL $KSQLDB_ADMIN $KSQLDB_USER $KSQLDB_SERVER $C3_ADMIN $CLIENT_AVRO_PRINCIPAL; do
+  for c in " " " --schema-registry-cluster-id $SR" " --connect-cluster-id $CONNECT" " --ksql-cluster-id $KSQLDB"; do
     echo
     echo "Showing bindings for principal $p and --kafka-cluster-id $KAFKA_CLUSTER_ID $c"
     docker-compose -f ${DIR}/../../../../environment/plaintext/docker-compose.yml -f ${DIR}/../../../../environment/rbac-sasl-plain/docker-compose.yml exec tools confluent iam rolebinding list --principal $p --kafka-cluster-id $KAFKA_CLUSTER_ID $c
