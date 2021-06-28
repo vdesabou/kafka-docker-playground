0dcd7ae2f3b9d7348fe0185b664eac9066682777
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-28 17:58:37 +0200

    #1144 Adding consumer

---
+++ ccloud/client-kafkajs/consumer-template.js
@@ -0,0 +1,65 @@
+const { Kafka, CompressionTypes, logLevel } = require('kafkajs')            //npm install kafkajs
+
+const kafka = new Kafka({
+  clientId: 'my-kafkajs-consumer',
+  brokers: [':BOOTSTRAP_SERVERS:'],
+  ssl: true,
+  sasl: {
+    mechanism: 'plain',
+    username: ':CLOUD_KEY:',
+    password: ':CLOUD_SECRET:',
+    logLevel: logLevel.DEBUG
+  },
+})
+
+
+const topic = 'kafkajs'
+const consumer = kafka.consumer({ groupId: 'test-group' })
+const admin = kafka.admin()
+
+const run = async () => {
+  await admin.connect()
+  await admin.createTopics({
+    topics: [{ topic }],
+    waitForLeaders: true,
+  })
+  await consumer.connect()
+  await consumer.subscribe({ topic, fromBeginning: false })
+  await consumer.run({
+    // eachBatch: async ({ batch }) => {
+    //   console.log(batch)
+    // },
+    eachMessage: async ({ topic, partition, message }) => {
+      const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+      console.log(`- ${prefix} ${message.key}#${message.value}`)
+    },
+  })
+}
+
+run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
+
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.map(type => {
+  process.on(type, async e => {
+    try {
+      console.log(`process.on ${type}`)
+      console.error(e)
+      await consumer.disconnect()
+      process.exit(0)
+    } catch (_) {
+      process.exit(1)
+    }
+  })
+})
+
+signalTraps.map(type => {
+  process.once(type, async () => {
+    try {
+      await consumer.disconnect()
+    } finally {
+      process.kill(process.pid, type)
+    }
+  })
+})
