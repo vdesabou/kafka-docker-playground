a807a86aeff7e42b038f3b72b7cccf95e4ac65b4
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-06-29 14:33:04 +0200

    Add KafkaJS example (onPrem) #1149
    Former-commit-id: 6b42f9b3ce8b801add514aa3947e3a570ec8f355 [formerly 6d0d29a45baba59d556499fcde72599708a0d6f2 [formerly 56d6a3e01dce2aa7296c47e1821124dc5ce7fa50]]
    Former-commit-id: 3482fb6e7cbf59a2231271cd7d9874613153c89f
    Former-commit-id: 8783f869ca4afa82781b2a7c45b553781e89d6fc

---
+++ other/client-kafkajs/producer.js
@@ -0,0 +1,70 @@
+const { Kafka, CompressionTypes, logLevel } = require('kafkajs')            //npm install kafkajs
+const Chance = require('chance')                //npm install chance
+
+const kafka = new Kafka({
+  clientId: 'my-kafkajs-producer',
+  brokers: ['broker1:9092','broker2:9092','broker3:9092'],
+  connectionTimeout: 20000,
+})
+
+const producer = kafka.producer()
+const topic = 'kafkajs'
+// const admin = kafka.admin()
+
+const getRandomNumber = () => Math.round(Math.random(10) * 1000)
+const createMessage = num => ({
+  key: `key-${num}`,
+  value: `value-${num}-${new Date().toISOString()}`,
+})
+
+const sendMessage = () => {
+  return producer
+    .send({
+      topic,
+      // compression: CompressionTypes.GZIP,
+      messages: Array(getRandomNumber())
+        .fill()
+        .map(_ => createMessage(getRandomNumber())),
+    })
+    .then(console.log)
+    .catch(e => console.error(`[example/producer] ${e.message}`, e))
+}
+
+const run = async () => {
+  // await admin.connect()
+  // await admin.createTopics({
+  //   topics: [{ topic }],
+  //   waitForLeaders: true,
+  // })
+
+  // Producing
+  await producer.connect()
+  setInterval(sendMessage, 1000)
+}
+
+run().catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.map(type => {
+  process.on(type, async () => {
+    try {
+      console.log(`process.on ${type}`)
+      await producer.disconnect()
+      process.exit(0)
+    } catch (_) {
+      process.exit(1)
+    }
+  })
+})
+
+signalTraps.map(type => {
+  process.once(type, async () => {
+    try {
+      await producer.disconnect()
+    } finally {
+      process.kill(process.pid, type)
+    }
+  })
+})
