cb124b00bfde07436d7d509a90d7ec4eec94a6e7
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-07-05 16:41:21 +0200

    Add Splunk S2S Source example #1151
    Former-commit-id: 9ac26011c0cca83f08209069d5311726bdfdf014 [formerly 526d840c476e50a840f8114f6adddba9335cd9e3 [formerly 9722e30c3c3013c8b8a1577b4a5398f0fa496ac6]]
    Former-commit-id: f0e165898ba90b1cac0d7398d63a694c2e84466a
    Former-commit-id: 505feecc3de67199533213bd70b827673055fc52

---
+++ connect/connect-splunk-s2s-source/splunk-s2s-source.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Creating Splunk S2S source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.splunk.s2s.SplunkS2SSourceConnector",
+               "tasks.max": "1",
+               "kafka.topic": "splunk-s2s-events",
+               "splunk.collector.index.default": "default-index",
+               "splunk.s2s.port": "9997",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+               "value.converter.schemas.enable": "false",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/splunk-s2s-source/config | jq .
+
+sleep 5
+
+echo "log event 1" > splunk-s2s-test.log
+echo "log event 2" >> splunk-s2s-test.log
+echo "log event 3" >> splunk-s2s-test.log
+
+log "Copy the splunk-s2s-test.log file to the Splunk UF Docker container"
+docker cp splunk-s2s-test.log splunk-uf:/opt/splunkforwarder/splunk-s2s-test.log
+
+log "Configure the UF to monitor the splunk-s2s-test.log file"
+docker exec -i splunk-uf sudo ./bin/splunk add monitor -source /opt/splunkforwarder/splunk-s2s-test.log -auth admin:password
+
+log "Configure the UF to connect to Splunk S2S Source connector"
+docker exec -it splunk-uf sudo ./bin/splunk add forward-server connect:9997
+
+sleep 5
+
+log "Verifying topic splunk-s2s-events"
+set +e
+# rely on timeout command
+timeout 10 docker exec connect kafka-console-consumer -bootstrap-server broker:9092 --topic splunk-s2s-events --from-beginning > /tmp/result.log  2>&1
+set -e
+grep "log event 1" /tmp/result.log
+grep "log event 2" /tmp/result.log
+grep "log event 3" /tmp/result.log
+
