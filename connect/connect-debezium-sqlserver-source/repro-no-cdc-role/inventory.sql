f9f52c37e0399c74ee36b0c835667e551c6b419b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-07-16 14:12:13 +0200

    updated with same example as customer

--- connect/connect-debezium-sqlserver-source/repro-no-cdc-role/inventory.sql
+++ connect/connect-debezium-sqlserver-source/repro-no-cdc-role/inventory.sql
@@ -19,14 +19,14 @@ INSERT INTO customers(first_name,last_name,email)
   VALUES ('Edward','Walker','ed@walker.com');
 INSERT INTO customers(first_name,last_name,email)
   VALUES ('Anne','Kretchmar','annek@noanswer.org');
-EXEC sys.sp_cdc_enable_table @source_schema = 'dbo', @source_name = 'customers', @role_name = NULL, @supports_net_changes = 0;
+EXEC sys.sp_cdc_enable_table @source_schema = 'dbo', @source_name = 'customers', @role_name = 'CDCReader', @supports_net_changes = 0;
 
 
 CREATE LOGIN vincent WITH PASSWORD = 'Password!'
 IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'vincent')
 BEGIN
     CREATE USER [vincent] FOR LOGIN [vincent]
-    EXEC sp_addrolemember N'public', N'vincent'
+    EXEC sp_addrolemember N'db_datareader', N'vincent'
 END;
 
 EXEC sys.sp_cdc_help_change_data_capture
