85386babbf5562ca043ba7b437b20aa3af1cf4e4
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-07-20 17:21:35 +0200

    update

--- connect/connect-hdfs2-sink/hdfs2-sink-kerberos-repro-gss-exception.sh
+++ connect/connect-hdfs2-sink/hdfs2-sink-kerberos-repro-gss-exception.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export CONNECTOR_TAG=10.0.1
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
@@ -18,8 +20,11 @@ function wait_for_gss_exception () {
                echo -e "\nERROR: The logs in ${CONNECT_CONTAINER} container do not show 'Failed to find any Kerberos tgt' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
                exit 1
           fi
-          # send requests
-          seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_hdfs --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+          for((i=0;i<5;i++)); do
+               # send requests
+               seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_hdfs$i --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+          done
      done
      log "The problem has been reproduced !"
 }
@@ -54,11 +59,11 @@ docker exec hadoop bash -c "echo password | kinit && /usr/local/hadoop/bin/hdfs 
 log "Add connect kerberos principal"
 docker exec -i kdc kadmin.local << EOF
 addprinc -randkey connect/connect.kerberos-demo.local@EXAMPLE.COM
-modprinc -maxrenewlife 120days +allow_renewable connect/connect.kerberos-demo.local@EXAMPLE.COM
-modprinc -maxrenewlife 120days krbtgt/EXAMPLE.COM
-modprinc -maxlife 120days connect/connect.kerberos-demo.local@EXAMPLE.COM
+modprinc -maxrenewlife 604800 +allow_renewable connect/connect.kerberos-demo.local@EXAMPLE.COM
+modprinc -maxrenewlife 604800 krbtgt/EXAMPLE.COM
+modprinc -maxlife 604800 connect/connect.kerberos-demo.local@EXAMPLE.COM
 ktadd -k /connect.keytab connect/connect.kerberos-demo.local@EXAMPLE.COM
-listprincs
+getprinc connect/connect.kerberos-demo.local@EXAMPLE.COM
 EOF
 
 log "Copy connect.keytab to connect container /tmp/sshuser.keytab"
@@ -74,49 +79,39 @@ fi
 # docker exec connect klist
 
 
-log "Creating HDFS Sink connector"
-curl -X PUT \
-     -H "Content-Type: application/json" \
-     --data '{
-               "connector.class":"io.confluent.connect.hdfs.HdfsSinkConnector",
-               "tasks.max":"1",
-               "topics":"test_hdfs",
-               "store.url":"hdfs://hadoop.kerberos-demo.local:9000",
-               "flush.size":"3",
-               "hadoop.conf.dir":"/etc/hadoop/",
-               "partitioner.class":"io.confluent.connect.hdfs.partitioner.FieldPartitioner",
-               "partition.field.name":"f1",
-               "rotate.interval.ms":"120000",
-               "logs.dir":"/logs",
-               "hdfs.authentication.kerberos": "true",
-               "kerberos.ticket.renew.period.ms": "1000",
-               "connect.hdfs.principal": "connect/connect.kerberos-demo.local@EXAMPLE.COM",
-               "connect.hdfs.keytab": "/tmp/connect.keytab",
-               "hdfs.namenode.principal": "nn/hadoop.kerberos-demo.local@EXAMPLE.COM",
-               "confluent.license": "",
-               "confluent.topic.bootstrap.servers": "broker:9092",
-               "confluent.topic.replication.factor": "1",
-               "key.converter":"org.apache.kafka.connect.storage.StringConverter",
-               "value.converter":"io.confluent.connect.avro.AvroConverter",
-               "value.converter.schema.registry.url":"http://schema-registry:8081",
-               "schema.compatibility":"BACKWARD"
-          }' \
-     http://localhost:8083/connectors/hdfs-sink-kerberos/config | jq .
+for((i=0;i<5;i++)); do
 
-log "Sending messages to topic test_hdfs"
-seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_hdfs --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
-
-sleep 10
-
-log "Listing content of /topics/test_hdfs in HDFS"
-docker exec hadoop bash -c "/usr/local/hadoop/bin/hdfs dfs -ls /topics/test_hdfs"
-
-log "Getting one of the avro files locally and displaying content with avro-tools"
-docker exec hadoop bash -c "/usr/local/hadoop/bin/hadoop fs -copyToLocal /topics/test_hdfs/f1=value1/test_hdfs+0+0000000000+0000000000.avro /tmp"
-docker cp hadoop:/tmp/test_hdfs+0+0000000000+0000000000.avro /tmp/
-
-docker run -v /tmp:/tmp actions/avro-tools tojson /tmp/test_hdfs+0+0000000000+0000000000.avro
-
+     LOG_DIR="/logs$i"
+     TOPIC="test_hdfs$i"
+     log "Creating HDFS Sink connector $i"
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+                    "connector.class":"io.confluent.connect.hdfs.HdfsSinkConnector",
+                    "tasks.max":"1",
+                    "topics": "'"$TOPIC"'",
+                    "store.url":"hdfs://hadoop.kerberos-demo.local:9000",
+                    "flush.size":"3",
+                    "hadoop.conf.dir":"/etc/hadoop/",
+                    "partitioner.class":"io.confluent.connect.hdfs.partitioner.FieldPartitioner",
+                    "partition.field.name":"f1",
+                    "rotate.interval.ms":"120000",
+                    "logs.dir": "'"$LOG_DIR"'",
+                    "hdfs.authentication.kerberos": "true",
+                    "kerberos.ticket.renew.period.ms": "1000",
+                    "connect.hdfs.principal": "connect/connect.kerberos-demo.local@EXAMPLE.COM",
+                    "connect.hdfs.keytab": "/tmp/connect.keytab",
+                    "hdfs.namenode.principal": "nn/hadoop.kerberos-demo.local@EXAMPLE.COM",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1",
+                    "key.converter":"org.apache.kafka.connect.storage.StringConverter",
+                    "value.converter":"io.confluent.connect.avro.AvroConverter",
+                    "value.converter.schema.registry.url":"http://schema-registry:8081",
+                    "schema.compatibility":"BACKWARD"
+               }' \
+          http://localhost:8083/connectors/hdfs-sink-kerberos$i/config | jq .
+done
 
 wait_for_gss_exception
 
