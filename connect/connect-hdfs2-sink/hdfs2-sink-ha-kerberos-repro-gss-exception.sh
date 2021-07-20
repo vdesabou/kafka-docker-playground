cc8ebf9e8b98dd88cee34a61372b430b356a6141
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-07-20 13:34:30 +0200

    update

--- connect/connect-hdfs2-sink/hdfs2-sink-ha-kerberos-repro-gss-exception.sh
+++ connect/connect-hdfs2-sink/hdfs2-sink-ha-kerberos-repro-gss-exception.sh
@@ -66,7 +66,7 @@ log "Add connect kerberos principal"
 docker exec -i krb5 kadmin.local << EOF
 addprinc -randkey connect/connect.kerberos-demo.local@EXAMPLE.COM
 modprinc -maxrenewlife 600 +allow_renewable connect/connect.kerberos-demo.local@EXAMPLE.COM
-modprinc -maxrenewlife 600 krbtgt/EXAMPLE.COM
+modprinc -maxrenewlife 600 +allow_renewable krbtgt/EXAMPLE.COM
 modprinc -maxlife 600 connect/connect.kerberos-demo.local@EXAMPLE.COM
 ktadd -k /connect.keytab connect/connect.kerberos-demo.local@EXAMPLE.COM
 listprincs
