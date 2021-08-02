a80b2fa24721aceb44e5c88e386d37103091ed3a
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-08-02 15:05:03 +0200

    not working

---
+++ connect/connect-servicenow-source/servicenow-source-repro-read-timeout.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+function wait_for_end_of_hibernation () {
+     MAX_WAIT=1200
+     CUR_WAIT=0
+     log "Waiting up to $MAX_WAIT seconds for end of hibernation to happen (it can take several minutes)"
+     curl -X POST "${SERVICENOW_URL}/api/now/table/incident" --user admin:"$SERVICENOW_PASSWORD" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d '{"short_description": "This is test"}' > /tmp/out.txt 2>&1
+     while [[ $(cat /tmp/out.txt) =~ "Sign in to the site to wake your instance" ]]
+     do
+          sleep 10
+          curl -X POST "${SERVICENOW_URL}/api/now/table/incident" --user admin:"$SERVICENOW_PASSWORD" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d '{"short_description": "This is test"}' > /tmp/out.txt 2>&1
+          CUR_WAIT=$(( CUR_WAIT+10 ))
+          if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+               echo -e "\nERROR: The logs still show 'Sign in to the site to wake your instance' after $MAX_WAIT seconds.\n"
+               exit 1
+          fi
+     done
+     log "The instance is ready !"
+}
+
+SERVICENOW_URL=${SERVICENOW_URL:-$1}
+SERVICENOW_PASSWORD=${SERVICENOW_PASSWORD:-$2}
+
+if [ -z "$SERVICENOW_URL" ]
+then
+     logerror "SERVICENOW_URL is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [[ "$SERVICENOW_URL" != */ ]]
+then
+    logerror "SERVICENOW_URL does not end with "/" Example: https://dev12345.service-now.com/ "
+    exit 1
+fi
+
+if [ -z "$SERVICENOW_PASSWORD" ]
+then
+     logerror "SERVICENOW_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ ! -z "$CI" ]
+then
+     # this is github actions
+     set +e
+     log "Waking up servicenow instance..."
+     docker run -e USERNAME="$SERVICENOW_DEVELOPER_USERNAME" -e PASSWORD="$SERVICENOW_DEVELOPER_PASSWORD" ruthless/servicenow-instance-wakeup:latest
+     set -e
+     wait_for_end_of_hibernation
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-read-timeout.yml"
+
+# https://blog.jakubholy.net/2018/11/26/java-understanding-the-different-network-https-exceptions/
+# https://github.com/shopify/toxiproxy#toxics
+docker exec toxiproxy sh -c "/go/bin/toxiproxy-cli create servicenow -l 127.0.0.1:18888 -u service-now.com:443"
+docker exec toxiproxy sh -c "/go/bin/toxiproxy-cli toxic add servicenow -t latency -a latency=10"
+
+
+# docker exec toxiproxy sh -c "/go/bin/toxiproxy-cli toxic remove servicenow -n latency_downstream"
+# docker exec toxiproxy sh -c "/go/bin/toxiproxy-cli delete servicenow"
+# docker exec toxiproxy sh -c "/go/bin/toxiproxy-cli inspect servicenow"
+
+TODAY=$(date '+%Y-%m-%d')
+
+log "Creating ServiceNow Source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+                    "connector.class": "io.confluent.connect.servicenow.ServiceNowSourceConnector",
+                    "kafka.topic": "topic-servicenow",
+                    "proxy.url": "toxiproxy:18888",
+                    "servicenow.url": "'"$SERVICENOW_URL"'",
+                    "tasks.max": "1",
+                    "servicenow.table": "incident",
+                    "servicenow.user": "admin",
+                    "servicenow.password": "'"$SERVICENOW_PASSWORD"'",
+                    "servicenow.since": "'"$TODAY"'",
+                    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/servicenow-source-toxiproxy/config | jq .
+
+
+sleep 10
+
+log "Create one record to ServiceNow"
+docker exec -e SERVICENOW_URL="$SERVICENOW_URL" -e SERVICENOW_PASSWORD="$SERVICENOW_PASSWORD" connect bash -c "export HTTP_PROXY=toxiproxy:8888 && export HTTPS_PROXY=toxiproxy:8888 && \
+   curl -X POST \
+    \"${SERVICENOW_URL}api/now/table/incident\" \
+    --user admin:\"$SERVICENOW_PASSWORD\" \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -H 'cache-control: no-cache' \
+    -d '{\"short_description\": \"This is test\"}'"
+
+sleep 5
+
+log "Verify we have received the data in topic-servicenow topic"
+timeout 60 docker exec connect kafka-console-consumer -bootstrap-server broker:9092 --topic topic-servicenow --from-beginning --max-messages 1
