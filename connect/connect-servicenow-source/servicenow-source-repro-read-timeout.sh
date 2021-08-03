59f9fef111bdaf682dd0d27d8dc25c7e7cfbf8ae
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-08-03 16:14:28 +0200

    wip
    Former-commit-id: ce0f64ab42616834b8854e56cb9742689817c1a9 [formerly 99663b8fef2e4f143279006a940536ce8ef9c8b1 [formerly 6328ec0ec526a3ab0df633d879fb33d2cd38b041]]
    Former-commit-id: bc037712f043b8cb1062c2e116117cbea4f9e0a9
    Former-commit-id: 386f889d3632d55e04de799ace554f6523ce4496

--- connect/connect-servicenow-source/servicenow-source-repro-read-timeout.sh
+++ connect/connect-servicenow-source/servicenow-source-repro-read-timeout.sh
@@ -88,6 +88,8 @@ curl -X PUT \
                "servicenow.user": "admin",
                "servicenow.password": "'"$SERVICENOW_PASSWORD"'",
                "servicenow.since": "'"$TODAY"'",
+               "connection.timeout.ms": "50000",
+               "retry.max.times": 1,
                "key.converter": "org.apache.kafka.connect.json.JsonConverter",
                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
                "confluent.license": "",
@@ -114,16 +116,20 @@ sleep 5
 log "Verify we have received the data in topic-servicenow topic"
 timeout 60 docker exec connect kafka-console-consumer -bootstrap-server broker:9092 --topic topic-servicenow --from-beginning --max-messages 1
 
+log "install tcp dump"
+docker exec --privileged --user root connect bash -c 'curl http://mirror.centos.org/centos/8/AppStream/x86_64/os/Packages/tcpdump-4.9.3-1.el8.x86_64.rpm -o tcpdump-4.9.3-1.el8.x86_64.rpm  && rpm -Uvh tcpdump-4.9.3-1.el8.x86_64.rpm'
+
 log "Adding latency from nginx_proxy to connect to simulate a read timeout (hard-coded to 20 seconds)"
 # connect
 latency_put=$(get_latency nginx_proxy connect)
 log "Latency from nginx_proxy to nginx_proxy BEFORE traffic control: $latency_put ms"
 
-add_latency nginx_proxy connect 6000ms
+add_latency nginx_proxy connect 25000ms
 
 latency_put=$(get_latency nginx_proxy connect)
 log "Latency from nginx_proxy to nginx_proxy AFTER traffic control: $latency_put ms"
 
+docker exec --privileged --user root connect bash -c 'tcpdump -w tcpdump.pcap -i eth0 -s 0 port 8888'
 
 
 # with 2.1.1, no retry:
@@ -191,6 +197,7 @@ log "Latency from nginx_proxy to nginx_proxy AFTER traffic control: $latency_put
 
 
 
+# With default connection.timeout.ms (50 seconds) and 60 seconds latency between nginx_proxy and connect, we get a connect timeout error (SocketTimeoutException: connect timed out), which makes sense:
 
 # [2021-08-02 15:42:54,104] ERROR [servicenow-source|task-0] WorkerSourceTask{id=servicenow-source-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:184)
 # io.confluent.connect.utils.retry.RetryCountExceeded: Failed after 4 attempts to send request to ServiceNow: Connect to nginx_proxy:8888 [nginx_proxy/172.19.0.3] failed: connect timed out
@@ -238,3 +245,124 @@ log "Latency from nginx_proxy to nginx_proxy AFTER traffic control: $latency_put
 # 	at org.apache.http.conn.socket.PlainConnectionSocketFactory.connectSocket(PlainConnectionSocketFactory.java:75)
 # 	at org.apache.http.impl.conn.DefaultHttpClientConnectionOperator.connect(DefaultHttpClientConnectionOperator.java:142)
 # 	... 30 more
+
+# With default connection.timeout.ms (50 seconds) and latency between nginx_proxy and connect > 20 seconds (which is the default Google HTTP read timeout value), we this error:
+
+# [2021-08-02 18:17:30,818] ERROR [servicenow-source|task-0] WorkerSourceTask{id=servicenow-source-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:184)
+# io.confluent.connect.utils.retry.RetryCountExceeded: Failed after 4 attempts to send request to ServiceNow: Remote host terminated the handshake
+#         at io.confluent.connect.utils.retry.RetryPolicy.callWith(RetryPolicy.java:429)
+#         at io.confluent.connect.utils.retry.RetryPolicy.call(RetryPolicy.java:337)
+#         at io.confluent.connect.servicenow.rest.ServiceNowClientImpl.executeRequest(ServiceNowClientImpl.java:229)
+#         at io.confluent.connect.servicenow.rest.ServiceNowClientImpl.get(ServiceNowClientImpl.java:183)
+#         at io.confluent.connect.servicenow.rest.ServiceNowClientImpl.getObjects(ServiceNowClientImpl.java:146)
+#         at io.confluent.connect.servicenow.ServiceNowSourceTask.fetchRecordFromServiceNow(ServiceNowSourceTask.java:183)
+#         at io.confluent.connect.servicenow.ServiceNowSourceTask.poll(ServiceNowSourceTask.java:147)
+#         at org.apache.kafka.connect.runtime.WorkerSourceTask.poll(WorkerSourceTask.java:268)
+#         at org.apache.kafka.connect.runtime.WorkerSourceTask.execute(WorkerSourceTask.java:241)
+#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:182)
+#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:231)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:829)
+# Caused by: javax.net.ssl.SSLHandshakeException: Remote host terminated the handshake
+#         at java.base/sun.security.ssl.SSLSocketImpl.handleEOF(SSLSocketImpl.java:1598)
+#         at java.base/sun.security.ssl.SSLSocketImpl.decode(SSLSocketImpl.java:1426)
+#         at java.base/sun.security.ssl.SSLSocketImpl.readHandshakeRecord(SSLSocketImpl.java:1324)
+#         at java.base/sun.security.ssl.SSLSocketImpl.startHandshake(SSLSocketImpl.java:440)
+#         at java.base/sun.security.ssl.SSLSocketImpl.startHandshake(SSLSocketImpl.java:411)
+#         at org.apache.http.conn.ssl.SSLConnectionSocketFactory.createLayeredSocket(SSLConnectionSocketFactory.java:436)
+#         at org.apache.http.impl.conn.DefaultHttpClientConnectionOperator.upgrade(DefaultHttpClientConnectionOperator.java:191)
+#         at org.apache.http.impl.conn.PoolingHttpClientConnectionManager.upgrade(PoolingHttpClientConnectionManager.java:390)
+#         at org.apache.http.impl.execchain.MainClientExec.establishRoute(MainClientExec.java:428)
+#         at org.apache.http.impl.execchain.MainClientExec.execute(MainClientExec.java:236)
+#         at org.apache.http.impl.execchain.ProtocolExec.execute(ProtocolExec.java:186)
+#         at org.apache.http.impl.execchain.RetryExec.execute(RetryExec.java:89)
+#         at org.apache.http.impl.execchain.RedirectExec.execute(RedirectExec.java:110)
+#         at org.apache.http.impl.client.InternalHttpClient.doExecute(InternalHttpClient.java:185)
+#         at org.apache.http.impl.client.CloseableHttpClient.execute(CloseableHttpClient.java:83)
+#         at org.apache.http.impl.client.CloseableHttpClient.execute(CloseableHttpClient.java:108)
+#         at org.apache.http.impl.client.CloseableHttpClient.execute(CloseableHttpClient.java:56)
+#         at com.google.api.client.http.apache.v2.ApacheHttpRequest.execute(ApacheHttpRequest.java:71)
+#         at com.google.api.client.http.HttpRequest.execute(HttpRequest.java:996)
+#         at io.confluent.connect.servicenow.rest.ServiceNowClientImpl.lambda$executeRequest$2(ServiceNowClientImpl.java:230)
+#         at io.confluent.connect.utils.retry.RetryPolicy.lambda$call$1(RetryPolicy.java:337)
+#         at io.confluent.connect.utils.retry.RetryPolicy.callWith(RetryPolicy.java:417)
+#         ... 15 more
+# Caused by: java.io.EOFException: SSL peer shut down incorrectly
+#         at java.base/sun.security.ssl.SSLSocketInputRecord.read(SSLSocketInputRecord.java:483)
+#         at java.base/sun.security.ssl.SSLSocketInputRecord.readHeader(SSLSocketInputRecord.java:472)
+#         at java.base/sun.security.ssl.SSLSocketInputRecord.decode(SSLSocketInputRecord.java:160)
+#         at java.base/sun.security.ssl.SSLTransport.decode(SSLTransport.java:110)
+#         at java.base/sun.security.ssl.SSLSocketImpl.decode(SSLSocketImpl.java:1418)
+#         ... 35 more
+
+
+# In order to "reproduce", the read timeout, I've recompiled the connector with a ridiculously low read timeout:
+
+
+# Then I can see (with or without proxy):
+
+# [2021-08-03 13:22:53,383] ERROR [servicenow-source|task-0] WorkerSourceTask{id=servicenow-source-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:184)
+# org.apache.kafka.connect.errors.ConnectException: https://dev71747.service-now.com/api/now/table/incident?sysparm_limit=1 is not reachable. Please check the endpoint or network connection
+# at io.confluent.connect.servicenow.rest.ServiceNowClientImpl.checkConnectivity(ServiceNowClientImpl.java:178)
+# at io.confluent.connect.servicenow.rest.ServiceNowClientImpl.(ServiceNowClientImpl.java:91)
+# at io.confluent.connect.servicenow.rest.ServiceNowClientFactory.create(ServiceNowClientFactory.java:11)
+# at io.confluent.connect.servicenow.ServiceNowSourceTask.createServiceNowClient(ServiceNowSourceTask.java:348)
+# at io.confluent.connect.servicenow.ServiceNowSourceTask.start(ServiceNowSourceTask.java:109)
+# at org.apache.kafka.connect.runtime.WorkerSourceTask.execute(WorkerSourceTask.java:225)
+# at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:182)
+# at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:231)
+# at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+# at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+# at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+# at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+# at java.base/java.lang.Thread.run(Thread.java:829)
+# Caused by: io.confluent.connect.utils.retry.RetryCountExceeded: Failed after 4 attempts to send request to ServiceNow: Read timed out
+# at io.confluent.connect.utils.retry.RetryPolicy.callWith(RetryPolicy.java:429)
+# at io.confluent.connect.utils.retry.RetryPolicy.call(RetryPolicy.java:337)
+# at io.confluent.connect.servicenow.rest.ServiceNowClientImpl.executeRequest(ServiceNowClientImpl.java:230)
+# at io.confluent.connect.servicenow.rest.ServiceNowClientImpl.get(ServiceNowClientImpl.java:184)
+# at io.confluent.connect.servicenow.rest.ServiceNowClientImpl.checkConnectivity(ServiceNowClientImpl.java:176)
+# ... 12 more
+# Caused by: java.net.SocketTimeoutException: Read timed out
+# at java.base/java.net.SocketInputStream.socketRead0(Native Method)
+# at java.base/java.net.SocketInputStream.socketRead(SocketInputStream.java:115)
+# at java.base/java.net.SocketInputStream.read(SocketInputStream.java:168)
+# at java.base/java.net.SocketInputStream.read(SocketInputStream.java:140)
+# at java.base/sun.security.ssl.SSLSocketInputRecord.read(SSLSocketInputRecord.java:478)
+# at java.base/sun.security.ssl.SSLSocketInputRecord.readHeader(SSLSocketInputRecord.java:472)
+# at java.base/sun.security.ssl.SSLSocketInputRecord.bytesInCompletePacket(SSLSocketInputRecord.java:70)
+# at java.base/sun.security.ssl.SSLSocketImpl.readApplicationRecord(SSLSocketImpl.java:1364)
+# at java.base/sun.security.ssl.SSLSocketImpl$AppInputStream.read(SSLSocketImpl.java:973)
+# at org.apache.http.impl.io.SessionInputBufferImpl.streamRead(SessionInputBufferImpl.java:137)
+# at org.apache.http.impl.io.SessionInputBufferImpl.fillBuffer(SessionInputBufferImpl.java:153)
+# at org.apache.http.impl.io.SessionInputBufferImpl.readLine(SessionInputBufferImpl.java:280)
+# at org.apache.http.impl.conn.DefaultHttpResponseParser.parseHead(DefaultHttpResponseParser.java:138)
+# at org.apache.http.impl.conn.DefaultHttpResponseParser.parseHead(DefaultHttpResponseParser.java:56)
+# at org.apache.http.impl.io.AbstractMessageParser.parse(AbstractMessageParser.java:259)
+# at org.apache.http.impl.DefaultBHttpClientConnection.receiveResponseHeader(DefaultBHttpClientConnection.java:163)
+# at org.apache.http.impl.conn.CPoolProxy.receiveResponseHeader(CPoolProxy.java:157)
+# at org.apache.http.protocol.HttpRequestExecutor.doReceiveResponse(HttpRequestExecutor.java:273)
+# at org.apache.http.protocol.HttpRequestExecutor.execute(HttpRequestExecutor.java:125)
+# at org.apache.http.impl.execchain.MainClientExec.execute(MainClientExec.java:272)
+# at org.apache.http.impl.execchain.ProtocolExec.execute(ProtocolExec.java:186)
+# at org.apache.http.impl.execchain.RetryExec.execute(RetryExec.java:89)
+# at org.apache.http.impl.execchain.RedirectExec.execute(RedirectExec.java:110)
+# at org.apache.http.impl.client.InternalHttpClient.doExecute(InternalHttpClient.java:185)
+# at org.apache.http.impl.client.CloseableHttpClient.execute(CloseableHttpClient.java:83)
+# at org.apache.http.impl.client.CloseableHttpClient.execute(CloseableHttpClient.java:108)
+# at org.apache.http.impl.client.CloseableHttpClient.execute(CloseableHttpClient.java:56)
+# at com.google.api.client.http.apache.v2.ApacheHttpRequest.execute(ApacheHttpRequest.java:71)
+# at com.google.api.client.http.HttpRequest.execute(HttpRequest.java:996)
+# at io.confluent.connect.servicenow.rest.ServiceNowClientImpl.lambda$2(ServiceNowClientImpl.java:231)
+# at io.confluent.connect.utils.retry.RetryPolicy.lambda$call$1(RetryPolicy.java:337)
+# at io.confluent.connect.utils.retry.RetryPolicy.callWith(RetryPolicy.java:417)
+# ... 16 more
+
+# My conclusion:
+
+# * Prior to 2.3.x, there was no retry for read timeout error
+# * Starting from 2.3.x, retry.max.times is taken into account for read timeout error (but it is now capped to 5 retries (since 2.3.1) and retry backoff is maximum 300ms (since 2.3.0) with exponentialJitter, meaning that with a low number of retries, all retries are instantaneous.
+# * Read timeout is currently hard-coded to 20 seconds.
