883e62b168196b236845fe6d0b8fb307b02f3fe9
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-08-31 07:51:36 +0200

    #1225 Handling config name changes in 2.0

--- connect/connect-github-source/github-source.sh
+++ connect/connect-github-source/github-source.sh
@@ -14,28 +14,52 @@ fi
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
-log "Creating Github Source connector"
-curl -X PUT \
-     -H "Content-Type: application/json" \
-     --data '{
-                    "connector.class": "io.confluent.connect.github.GithubSourceConnector",
-                    "topic.name.pattern":"github-topic-${entityName}",
-                    "tasks.max": "1",
-                    "github.service.url":"https://api.github.com",
-                    "github.repositories":"apache/kafka",
-                    "github.tables":"stargazers",
-                    "github.since":"2019-01-01",
-                    "github.access.token": "'"$GITHUB_ACCESS_TOKEN"'",
-                    "key.converter": "io.confluent.connect.avro.AvroConverter",
-                    "key.converter.schema.registry.url":"http://schema-registry:8081",
-                    "value.converter": "io.confluent.connect.avro.AvroConverter",
-                    "value.converter.schema.registry.url":"http://schema-registry:8081",
-                    "confluent.license": "",
-                    "confluent.topic.bootstrap.servers": "broker:9092",
-                    "confluent.topic.replication.factor": "1"
-          }' \
-     http://localhost:8083/connectors/github-source/config | jq .
-
+if version_gt $CONNECTOR_TAG "1.9.9"
+then
+     log "Creating Github Source connector"
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+                         "connector.class": "io.confluent.connect.github.GithubSourceConnector",
+                         "topic.name.pattern":"github-topic-${resourceName}",
+                         "tasks.max": "1",
+                         "github.service.url":"https://api.github.com",
+                         "github.repositories":"apache/kafka",
+                         "github.resources":"stargazers",
+                         "github.since":"2019-01-01",
+                         "github.access.token": "'"$GITHUB_ACCESS_TOKEN"'",
+                         "key.converter": "io.confluent.connect.avro.AvroConverter",
+                         "key.converter.schema.registry.url":"http://schema-registry:8081",
+                         "value.converter": "io.confluent.connect.avro.AvroConverter",
+                         "value.converter.schema.registry.url":"http://schema-registry:8081",
+                         "confluent.license": "",
+                         "confluent.topic.bootstrap.servers": "broker:9092",
+                         "confluent.topic.replication.factor": "1"
+               }' \
+          http://localhost:8083/connectors/github-source/config | jq .
+else
+     log "Creating Github Source connector"
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+                         "connector.class": "io.confluent.connect.github.GithubSourceConnector",
+                         "topic.name.pattern":"github-topic-${entityName}",
+                         "tasks.max": "1",
+                         "github.service.url":"https://api.github.com",
+                         "github.repositories":"apache/kafka",
+                         "github.tables":"stargazers",
+                         "github.since":"2019-01-01",
+                         "github.access.token": "'"$GITHUB_ACCESS_TOKEN"'",
+                         "key.converter": "io.confluent.connect.avro.AvroConverter",
+                         "key.converter.schema.registry.url":"http://schema-registry:8081",
+                         "value.converter": "io.confluent.connect.avro.AvroConverter",
+                         "value.converter.schema.registry.url":"http://schema-registry:8081",
+                         "confluent.license": "",
+                         "confluent.topic.bootstrap.servers": "broker:9092",
+                         "confluent.topic.replication.factor": "1"
+               }' \
+          http://localhost:8083/connectors/github-source/config | jq .
+fi
 
 sleep 10
 
