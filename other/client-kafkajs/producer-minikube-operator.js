033a048e9866c4727e86d1b59524a86ad8a49b5f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-09-07 14:02:30 +0200

    updated with a lock mechanism

--- other/client-kafkajs/producer-minikube-operator.js
+++ other/client-kafkajs/producer-minikube-operator.js
@@ -1,25 +1,26 @@
-const { Kafka, CompressionTypes, logLevel } = require('kafkajs')            //npm install kafkajs
+const { Kafka, CompressionTypes, logLevel } = require('kafkajs')
+
 // add timestamps in front of log messages
 require('console-stamp')(console, '[HH:MM:ss.l]');
 
 const kafka = new Kafka({
-  clientId: 'my-kafkajs-producer',
-  brokers: ['kafka-0.kafka.confluent.svc.cluster.local:9071','kafka-1.kafka.confluent.svc.cluster.local:9071','kafka-2.kafka.confluent.svc.cluster.local:9071'],
-  sasl: {
-    mechanism: 'plain', // scram-sha-256 or scram-sha-512
-    username: 'test',
-    password: 'test123'
-  },
-  acks:1,
-  connectionTimeout: 20000,
-  enforceRequestTimeout: true,
-  // requestTimeout: 3000,
-  logLevel: logLevel.DEBUG,
-  // retry: {
-  //   initialRetryTime: 100,
-  //   retries: 1
-  // },
-})
+    clientId: 'my-kafkajs-producer',
+    brokers: ['kafka-0.kafka.confluent.svc.cluster.local:9071','kafka-1.kafka.confluent.svc.cluster.local:9071','kafka-2.kafka.confluent.svc.cluster.local:9071'],
+    sasl: {
+      mechanism: 'plain',
+      username: 'test',
+      password: 'test123'
+    },
+    acks:1,
+    connectionTimeout: 20000,
+    enforceRequestTimeout: true,
+    // requestTimeout: 3000,
+    // logLevel: logLevel.DEBUG,
+    // retry: {
+    //   initialRetryTime: 100,
+    //   retries: 1
+    // },
+  })
 
 const producer = kafka.producer()
 const topic = 'kafkajs'
@@ -30,51 +31,87 @@ producer.on(CONNECT, e => console.log(`Producer connected at ${e.timestamp}`));
 producer.on(DISCONNECT, e => console.log(`Producer disconnected at ${e.timestamp}`));
 producer.on(REQUEST_TIMEOUT, e => console.log(`Producer request timed out at ${e.timestamp}`, JSON.stringify(e.payload)));
 // producer.on(REQUEST_QUEUE_SIZE, e => console.log(`Request queue size at ${e.timestamp}`, JSON.stringify(e.payload)));
-//producer.logger().setLogLevel(logLevel.DEBUG)
+// producer.logger().setLogLevel(logLevel.DEBUG)
 
 let bigString = '';
-for (let i = 0; i < 10_0; i++) {
+for (let i = 0; i < 10; i++) {
   bigString += Math.random().toString(36);
 }
 
 const payload = new Array(500).fill({value: bigString});
 
-var outgoingMessages = 0;
+const batch = []
+var lock = false
 
 function successCallback(result) {
   console.log("send() finished ");
-  outgoingMessages--;
 }
 
 function exceptionCallback(result) {
-  // console.error(`[example/producer] ${e.message}`, e)
   console.log("send() failed!  " + result);
-  outgoingMessages--;
 }
 
-function sendData() {
-  const now = new Date();
-  const used = process.memoryUsage().heapUsed / 1024 / 1024;
-  console.log(`Memory: ${Math.round(used * 100) / 100} MB`);
-  console.log(`outgoingMessages ` + outgoingMessages);
+function addDataToQueue() {
+    if (batch.length < 500) {
+        batch.push({value: bigString})
+    } else {
+        console.log(`discard event`)
+    }
+}
 
-  if(outgoingMessages>100) {
-    console.log(`Refusing message as we have outgoing messages ` + outgoingMessages);
-    return;
+function sendData(dataArray) {
+    return producer.send({
+        topic: topic,
+        messages: dataArray,
+        acks: 1,
+      }).then(() => {
+        return {
+            count: dataArray.length,
+        }
+      }).catch(e => {
+        console.log('failed to send data', e);
+        throw ({
+            error: e,
+            count: dataArray.length
+        })
+      });
+}
+
+function splitQueue(queue) {
+    var i,j, tmp = [], chunk = 5;
+    for (i = 0, j = queue.length; i<j; i += chunk) {
+        tmp.push(queue.slice(i, i+chunk))
+    }
+    batch.length = 0
+    return tmp
+}
+
+function deQueueBatch() {
+  if  (!lock) {
+    lock = true
+    const now = new Date();
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`Memory: ${Math.round(used * 100) / 100} MB`);
+    console.log(`Queue size: ${batch.length}`)
+
+    var batches = splitQueue(batch)
+    var promises = batches.map(function (events) {
+        return sendData(events)
+          .catch(function(result) {
+              console.log(`Error in sending data`)
+              return result
+          }).then(function(result) {
+              console.log(`Success in sending data`)
+              return result
+          })
+    })
+
+    Promise.allSettled(promises).then(function(results) {
+        lock = false
+        console.log('lock released', {duration: new Date() - now});
+        //results.forEach((result) => console.log(result))
+    })
   }
-  outgoingMessages++;
-
-  producer.send({
-    topic: topic,
-    messages: payload,
-    acks: 1,
-  }).then(() => {
-    console.log('data sent', {messages: payload.length, duration: new Date() - now});
-    outgoingMessages--;
-  }).catch(e => {
-    console.log('failed to send data', e);
-    outgoingMessages--;
-  });
 }
 
 (async function main(){
@@ -82,11 +119,10 @@ function sendData() {
   // await admin.createTopics({
   //   topics: [{ topic }],
   //   waitForLeaders: true,
-  //   replicationFactor: 3,
-  //   numPartitions: 6
   // })
   await producer.connect().catch(e => {
     log.error("failed to producer.connect()", e);
   });
-  setInterval(sendData, 100);
+  setInterval(addDataToQueue, 10)
+  setInterval(deQueueBatch, 1000);
 })().catch(e => {throw e});
