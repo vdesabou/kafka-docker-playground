3364dd7ada0f18f757cd4d131a0746154b878855
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-09-07 14:27:21 +0200

    wip

---
+++ other/client-kafkajs/start-repro-timeout-63874.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+docker-compose -f docker-compose-repro-timeout-63874.yml down -v --remove-orphans
+docker-compose -f docker-compose-repro-timeout-63874.yml up -d --build
+
+${DIR}/../../scripts/wait-for-connect-and-controlcenter.sh -a -b
+
+log "Create a topic kafkajs"
+docker exec broker1 kafka-topics --create --topic kafkajs --partitions 3 --replication-factor 3 --zookeeper zookeeper:2181
+
+
+log "Starting consumer. Logs are in consumer.log."
+docker exec -i client-kafkajs node /usr/src/app/consumer.js > consumer.log 2>&1 &
+
+log "Starting producer. Logs are in producer.log."
+docker exec -i client-kafkajs node /usr/src/app/producer.js > producer.log 2>&1 &
+
+log "Sleeping 15 seconds"
+sleep 15
+
+ip=$(docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep client-kafkajs | cut -d " " -f 3)
+log "Simulate a 45 seconds network issue with broker1 by blocking output traffic from broker1 to kafkaJS producer container"
+docker exec -e ip=$ip --privileged --user root broker1 sh -c "iptables -A OUTPUT -p tcp -d $ip -j DROP"
+
+sleep 45
+
+log "Setting back traffic to normal"
+docker exec -e ip=$ip --privileged --user root broker1 sh -c "iptables -D OUTPUT -p tcp -d $ip -j DROP"
+
+log "let the test run 3 minutes"
+sleep 180
