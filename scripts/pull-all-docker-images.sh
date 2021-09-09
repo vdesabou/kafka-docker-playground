7b98254b004e9e45f18fd916b81ec305695b617a
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-09-09 12:57:50 +0200

    #1271 call to pull-all-docker-images.sh for all other images

---
+++ scripts/pull-all-docker-images.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../scripts/utils.sh
+
+OLDIFS="$IFS"
+IFS=$'\n' # bash specific
+for image in $(grep -h "image:\s.*\/.*" $(find ${DIR}/.. -name "*.yml"))
+do
+  image=$(echo $image | tr -s " " | cut -d " " -f 3)
+
+    # check for scripts containing "repro"
+  if [[ "$image" == *"\${"* ]]; then
+      continue
+  fi
+  if [[ "$image" == "home" ]]; then
+      continue
+  fi
+  echo "$image" >> /tmp/list.txt
+done
+IFS="$OLDIFS"
+
+OLDIFS="$IFS"
+IFS=$'\n' # bash specific
+for image in $(cat /tmp/list.txt | uniq)
+do
+  log "pulling image $image"
+  docker pull $image
+done
+IFS="$OLDIFS"
+
