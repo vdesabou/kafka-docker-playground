b6a111224d8bf20dcfafbc5a490435a0f5485fab
Daniel Petisme <daniel.petisme@gmail.com>
2021-09-21 08:57:03 +0200

    Sample UDF logging
    Former-commit-id: 8104bdae9b5260452ae3981d52a8a2e07e4f170c [formerly 41e28dd9e085c66a922eda7e1ac0976ee26722e8 [formerly 96eda9e11c2f6555acef54a92c888126d9fd8623]]
    Former-commit-id: 65617859fb7ca30d9027d822297132901670699b
    Former-commit-id: dd3e709d9e19ae0a8c204d582f909be52ffa72ee

---
+++ ksqldb/udf-logging/start.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f ${DIR}/udf-simple-logging/target/udf-simple-logging-1.0.0.jar ]
+then
+    log "Building udf-simple-logging jar"
+    docker run -i --rm -v "${DIR}/udf-simple-logging":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/udf-simple-logging/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
+fi
+
+if [ ! -f ${DIR}/udf-custom-logger/target/udf-custom-logger-1.0.0-jar-with-dependencies.jar ]
+then
+    log "Building udf-custom-logger jar"
+    docker run -i --rm -v "${DIR}/udf-custom-logger":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/udf-custom-logger/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn package
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Creating streams with UDF and inserting data..."
+docker exec -i ksqldb-cli bash -c 'echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"; while [[ $(curl -s -o /dev/null -w %{http_code} http://ksqldb-server:8088/) -eq 000 ]] ; do echo -e $(date) "KSQL Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http:/ksqldb-server:8088/) " (waiting for 200)" ; sleep 10 ; done; ksql http://ksqldb-server:8088' << EOF
+DESCRIBE FUNCTION formula_simple_log4j_logging;
+
+DESCRIBE FUNCTION formula_custom_log4j_logging_level;
+
+DESCRIBE FUNCTION formula_custom_logger;
+
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE STREAM s1 (
+    a VARCHAR KEY,
+    b INT,
+    c INT
+) WITH (
+    kafka_topic = 's1',
+    partitions = 1,
+    value_format = 'avro'
+);
+
+CREATE STREAM s2 AS
+    SELECT 
+        a, 
+        formula_simple_log4j_logging(b, c) AS x,
+        formula_custom_log4j_logging_level(b, c) AS y,
+        formula_custom_logger(b, c) AS z
+    FROM s1
+    EMIT CHANGES;
+
+INSERT INTO s1 (a, b, c) VALUES ('k1', 2, 3);
+INSERT INTO s1 (a, b, c) VALUES ('k2', 4, 6);
+INSERT INTO s1 (a, b, c) VALUES ('k3', 6, 9);
+EOF
+
+sleep 10
+
+log "Verify the UDF logs using KSQL SLF4J+Log4j1 logger and default log level..."
+docker container logs ksqldb-server 2>/dev/null | grep -E "INFO V1: \d+, V2: \d+ \(com\.example\.FormulaUdfSimpleLog4jLogging)"
+
+log "Verify the UDF logs using KSQL SLF4J+Log4j1 logger and custom log level..."
+docker exec ksqldb-server cat /etc/ksqldb-server/log4j.properties | grep "FormulaUdfLog4jCustomLoggingLevel"
+docker container logs ksqldb-server 2>/dev/null | grep -E "DEBUG V1: \d+, V2: \d+ \(com\.example\.FormulaUdfLog4jCustomLoggingLevel\)"
+
+log "Verify the UDF logs using a custom SLF4J+Logback logger, custom log level and custom message format..."
+docker container logs ksqldb-server 2>/dev/null | grep -E "INFO  com\.example\.FormulaUdfCustomLogger - V1: \d+, V2: \d+"
