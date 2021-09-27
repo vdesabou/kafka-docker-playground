dab3813a974b25508ef228a7acaeee79c0aaac2b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-09-27 08:00:21 +0000

    Update Oracle 19c CDC example with DDL (internal only) #1385
    Former-commit-id: af47084196f7f82449ab9a328fc30d3022b0248a [formerly 5ed82e276e36d3afad04fa040aae8e9117534a16 [formerly ea6f877a987ffbda3677ee44d29d78975df8a706]]
    Former-commit-id: fca11148b1f86f7bc405fa6f70d8bd9649d291ca
    Former-commit-id: d44e0ca8b72a4f4b4eac23436dbe29dbe1aae70d

--- connect/connect-cdc-oracle19-source/sample-sql-scripts/04_alter_customers.sh
+++ connect/connect-cdc-oracle19-source/sample-sql-scripts/04_alter_customers.sh
@@ -2,16 +2,14 @@
 
 # Update Oracle 19c to not include ALTER TABLE ADD #1109
 # https://github.com/vdesabou/kafka-docker-playground/issues/1109
-# FIXTHIS
-
-echo 'DDL is not fully supported for now'
-exit 0
 
 echo 'Altering CUSTOMERS table with an optional column'
 
 docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/$1 << EOF
+  EXECUTE DBMS_LOGMNR_D.BUILD(OPTIONS=>DBMS_LOGMNR_D.STORE_IN_REDO_LOGS);
   alter table CUSTOMERS add (
     country VARCHAR(50)
   );
+  EXECUTE DBMS_LOGMNR_D.BUILD(OPTIONS=>DBMS_LOGMNR_D.STORE_IN_REDO_LOGS);
   exit;
 EOF
