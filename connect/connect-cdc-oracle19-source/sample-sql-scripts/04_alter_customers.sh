7d530e992b94f9b84a2916d5c3ab67234f822f28
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-09-28 13:57:02 +0200

    #1386 Fix
    Former-commit-id: 2646aabc21967d00156f61b860b1e3bb74e1c710 [formerly fc9b84bd015581b2c64129d813c8b3dea966e8f4 [formerly 42a17ae4237f115e770e4c50c5d71cbea8d012e7]]
    Former-commit-id: c5d189ac7cdfc3daf95fbe12eda475fc4c9e700c
    Former-commit-id: c3636f289feec1c0ad91f2fd828438ae7f165418

--- connect/connect-cdc-oracle19-source/sample-sql-scripts/04_alter_customers.sh
+++ connect/connect-cdc-oracle19-source/sample-sql-scripts/04_alter_customers.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-# Update Oracle 19c to not include ALTER TABLE ADD #1109
-# https://github.com/vdesabou/kafka-docker-playground/issues/1109
-
 echo 'Altering CUSTOMERS table with an optional column'
 
+if [ "$1" = "ORCLPDB1" ]
+then
 docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/$1 << EOF
+  ALTER SESSION SET CONTAINER=CDB\$ROOT;
+  EXECUTE DBMS_LOGMNR_D.BUILD(OPTIONS=>DBMS_LOGMNR_D.STORE_IN_REDO_LOGS);
+  ALTER SESSION SET CONTAINER=ORCLPDB1;
+  alter table CUSTOMERS add (
+    country VARCHAR(50)
+  );
+  ALTER SESSION SET CONTAINER=CDB\$ROOT;
+  EXECUTE DBMS_LOGMNR_D.BUILD(OPTIONS=>DBMS_LOGMNR_D.STORE_IN_REDO_LOGS);
+  exit;
+EOF
+else
+docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/$1 << EOF
+  ALTER SESSION SET CONTAINER=CDB\$ROOT;
   EXECUTE DBMS_LOGMNR_D.BUILD(OPTIONS=>DBMS_LOGMNR_D.STORE_IN_REDO_LOGS);
   alter table CUSTOMERS add (
     country VARCHAR(50)
   );
+  ALTER SESSION SET CONTAINER=CDB\$ROOT;
   EXECUTE DBMS_LOGMNR_D.BUILD(OPTIONS=>DBMS_LOGMNR_D.STORE_IN_REDO_LOGS);
   exit;
 EOF
+fi
