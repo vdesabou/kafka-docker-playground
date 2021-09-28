8857618ebc295b1a662a722afb41db59ac1a6a7b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-09-28 18:28:54 +0200

    Update CP to 6.2.1 #1388

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -111,7 +111,7 @@ function choosejar()
 if [ -z "$TAG" ]
 then
     # TAG is not set, use default:
-    export TAG=6.2.0
+    export TAG=6.2.1
     # to handle ubi8 images
     export TAG_BASE=$TAG
     if [ -z "$CP_KAFKA_IMAGE" ]
