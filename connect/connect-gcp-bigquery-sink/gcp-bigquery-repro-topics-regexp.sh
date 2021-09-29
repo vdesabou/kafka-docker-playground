cbd95ede2923adb6d821922a8230a20c902739ae
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-09-29 17:05:08 +0200

    test with topics.regexp
    Former-commit-id: eca5bfe4115d4830f98b4ce9899c0893de47e737 [formerly f364047d6ebba96c68d08f844fe105f330baa1db [formerly ff456101a7e417938c33f00b264c1f3cb222320d]]
    Former-commit-id: 8989a7e2fc8f0cb1e3baeac381e05d3df4bc9cfe
    Former-commit-id: 5e6278c76e4996e351416e4e71ffbf57e38779dd

---
+++ connect/connect-gcp-bigquery-sink/gcp-bigquery-repro-topics-regexp.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+PROJECT=${1:-vincent-de-saboulin-lab}
+
+KEYFILE="${DIR}/keyfile.json"
+if [ ! -f ${KEYFILE} ]
+then
+     logerror "ERROR: the file ${KEYFILE} file is not present!"
+     exit 1
+fi
+
+DATASET=pg${USER}ds${GITHUB_RUN_NUMBER}${TAG}
+DATASET=${DATASET//[-._]/}
+
+log "Doing gsutil authentication"
+set +e
+docker rm -f gcloud-config
+set -e
+docker run -i -v ${KEYFILE}:/tmp/keyfile.json --name gcloud-config google/cloud-sdk:latest gcloud auth activate-service-account --project ${PROJECT} --key-file /tmp/keyfile.json
+
+set +e
+log "Drop dataset $DATASET, this might fail"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" rm -r -f -d "$DATASET"
+set -e
+
+log "Create dataset $PROJECT.$DATASET"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" mk --dataset --description "used by playground" "$DATASET"
+
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Sending messages to topic kcbq-quickstart1"
+seq -f "{\"f1\": \"value%g-`date`\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic kcbq-quickstart1 --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+log "Sending messages to topic kcbq-quickstart2"
+seq -f "{\"f1\": \"value%g-`date`\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic kcbq-quickstart2 --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+log "Sending messages to topic kcbq-quickstart3"
+seq -f "{\"f1\": \"value%g-`date`\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic kcbq-quickstart3 --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+
+if version_gt $CONNECTOR_TAG "1.9.9"
+then
+     log "Creating GCP BigQuery Sink connector"
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+                    "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
+                    "tasks.max" : "1",
+                    "topics.regex" : "kcbq-quickstart.*",
+                    "sanitizeTopics" : "true",
+                    "autoCreateTables" : "true",
+                    "defaultDataset" : "'"$DATASET"'",
+                    "mergeIntervalMs": "5000",
+                    "bufferSize": "100000",
+                    "maxWriteSize": "10000",
+                    "tableWriteWait": "1000",
+                    "project" : "'"$PROJECT"'",
+                    "keyfile" : "/tmp/keyfile.json"
+               }' \
+          http://localhost:8083/connectors/gcp-bigquery-sink/config | jq .
+else
+     log "Creating GCP BigQuery Sink connector"
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+                    "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
+                    "tasks.max" : "1",
+                    "topics.regex" : "kcbq-quickstart.*",
+                    "sanitizeTopics" : "true",
+                    "autoCreateTables" : "true",
+                    "autoUpdateSchemas" : "true",
+                    "schemaRetriever" : "com.wepay.kafka.connect.bigquery.schemaregistry.schemaretriever.SchemaRegistrySchemaRetriever",
+                    "schemaRegistryLocation": "http://schema-registry:8081",
+                    "datasets" : ".*='"$DATASET"'",
+                    "mergeIntervalMs": "5000",
+                    "bufferSize": "100000",
+                    "maxWriteSize": "10000",
+                    "tableWriteWait": "1000",
+                    "project" : "'"$PROJECT"'",
+                    "keyfile" : "/tmp/keyfile.json"
+               }' \
+          http://localhost:8083/connectors/gcp-bigquery-sink/config | jq .
+fi
+
+log "Sleeping 125 seconds"
+sleep 125
+
+log "Verify data is in GCP BigQuery for kcbq_quickstart1:"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" query "SELECT * FROM $DATASET.kcbq_quickstart1;" > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "value1" /tmp/result.log
+
+log "Verify data is in GCP BigQuery for kcbq_quickstart2:"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" query "SELECT * FROM $DATASET.kcbq_quickstart2;" > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "value1" /tmp/result.log
+
+log "Verify data is in GCP BigQuery for kcbq_quickstart3:"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" query "SELECT * FROM $DATASET.kcbq_quickstart3;" > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "value1" /tmp/result.log
+
+
+log "Drop dataset $DATASET"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" rm -r -f -d "$DATASET"
+
+docker rm -f gcloud-config
