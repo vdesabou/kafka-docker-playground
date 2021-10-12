00dc64ac706a168a893d578d4ee0834ee9807621
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-10-12 12:52:39 +0200

    #1407 Working now
    Former-commit-id: fb297aac28f003b8652b8bcea0b7e9b9baa209e7 [formerly a4b9ae92d1d6d85d366d39169248ab690956d2db [formerly 5f7c50d7228af7505d8d0f4bbc1a023599f0143a]]
    Former-commit-id: 94aaf1ec22c987ae58866a5e824e8f4a8570481a
    Former-commit-id: dc23bc5d5d1a862749db6434c6434cca07a93f6e

--- ccloud/mirrormaker2/mirrormaker2.sh
+++ ccloud/mirrormaker2/mirrormaker2.sh
@@ -26,6 +26,7 @@ delete_topic sales_A
 delete_topic mm2-configs.A.internal
 delete_topic mm2-offsets.A.internal
 delete_topic mm2-status.A.internal
+delete_topic .checkpoints.internal
 sleep 3
 create_topic sales_A
 set -e
@@ -40,20 +41,11 @@ sleep 30
 log "Sending messages in A cluster (OnPrem)"
 seq -f "A_sale_%g ${RANDOM}" 20 | docker container exec -i broker1 kafka-console-producer --broker-list localhost:9092 --topic sales_A
 
-log "Consumer with group my-consumer-group reads 10 messages in A cluster"
+log "Consumer with group my-consumer-group reads 10 messages in A cluster (OnPrem)"
 docker exec -i connect bash -c "kafka-console-consumer --bootstrap-server broker1:9092 --whitelist 'sales_A' --from-beginning --max-messages 10 --consumer-property group.id=my-consumer-group"
 
 log "sleeping 70 seconds"
 sleep 70
 
-log "Consumer with group my-consumer-group reads 10 messages in B cluster (Confluent Cloud), it should start from previous offset"
+log "Consumer with group my-consumer-group reads 10 messages in B cluster (Confluent Cloud), it should start from previous offset (sync.group.offsets.enabled = true)"
 timeout 60 docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" connect bash -c 'kafka-console-consumer --topic sales_A --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --max-messages 10 --consumer-property group.id=my-consumer-group'
-
-
-# FIXTHIS: not working getting:
-# [2021-10-12 09:23:39,996] INFO [Consumer clientId=consumer-my-consumer-group-1, groupId=my-consumer-group] Found no committed offset for partition sales_A-3 (org.apache.kafka.clients.consumer.internals.ConsumerCoordinator)
-# [2021-10-12 09:23:39,996] INFO [Consumer clientId=consumer-my-consumer-group-1, groupId=my-consumer-group] Found no committed offset for partition sales_A-2 (org.apache.kafka.clients.consumer.internals.ConsumerCoordinator)
-# [2021-10-12 09:23:39,996] INFO [Consumer clientId=consumer-my-consumer-group-1, groupId=my-consumer-group] Found no committed offset for partition sales_A-1 (org.apache.kafka.clients.consumer.internals.ConsumerCoordinator)
-# [2021-10-12 09:23:39,996] INFO [Consumer clientId=consumer-my-consumer-group-1, groupId=my-consumer-group] Found no committed offset for partition sales_A-0 (org.apache.kafka.clients.consumer.internals.ConsumerCoordinator)
-# [2021-10-12 09:23:39,996] INFO [Consumer clientId=consumer-my-consumer-group-1, groupId=my-consumer-group] Found no committed offset for partition sales_A-5 (org.apache.kafka.clients.consumer.internals.ConsumerCoordinator)
-# [2021-10-12 09:23:39,997] INFO [Consumer clientId=consumer-my-consumer-group-1, groupId=my-consumer-group] Found no committed offset for partition sales_A-4 (org.apache.kafka.clients.consumer.internals.ConsumerCoordinator)
