7098800a582bfb2629005366b514a923d2fa037f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-10-13 14:44:35 +0200

    now using bitnami kafka 1.1.1

--- ccloud/mirrormaker2/mirrormaker2-repro-77222.sh
+++ ccloud/mirrormaker2/mirrormaker2-repro-77222.sh
@@ -4,7 +4,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose.plaintext.yml" -a -b
+${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose.plaintext.repro-77222.yml" -a -b
 
 if [ -f /tmp/delta_configs/env.delta ]
 then
@@ -39,18 +39,22 @@ log "sleeping 30 seconds"
 sleep 30
 
 log "Sending messages in A cluster (OnPrem)"
-docker exec broker1 kafka-producer-perf-test --topic sales_A --num-records 200000 --record-size 1000 --throughput 100000 --producer-props bootstrap.servers=broker1:9092
+docker exec connect bash -c "export KAFKA_LOG4J_OPTS='';kafka-producer-perf-test --topic sales_A --num-records 200000 --record-size 1000 --throughput 100000 --producer-props bootstrap.servers=broker1:9092"
 
 log "Consumer with group my-consumer-group reads 10 messages in A cluster (OnPrem)"
-docker exec -i broker1 bash -c "kafka-console-consumer --bootstrap-server broker1:9092 --whitelist 'sales_A' --from-beginning --max-messages 10 --consumer-property group.id=my-consumer-group"
+docker exec -i connect bash -c "export KAFKA_LOG4J_OPTS='';kafka-console-consumer --bootstrap-server broker1:9092 --whitelist 'sales_A' --from-beginning --max-messages 10 --consumer-property group.id=my-consumer-group"
 
 log "sleeping 70 seconds"
 sleep 70
 
 log "Consumer with group my-consumer-group reads 10 messages in B cluster (Confluent Cloud), it should start from previous offset (sync.group.offsets.enabled = true)"
-timeout 60 docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" broker1 bash -c 'kafka-console-consumer --topic sales_A --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --max-messages 10 --consumer-property group.id=my-consumer-group'
+timeout 60 docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" connect bash -c 'kafka-console-consumer --topic sales_A --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --max-messages 10 --consumer-property group.id=my-consumer-group'
 
 tail -f mirrormaker.log | grep "ERROR"
+
+# https://confluentinc.atlassian.net/browse/CCDB-3071
+# https://issues.apache.org/jira/browse/KAFKA-12226
+
 
 # Repro:
 # [2021-10-12 11:00:54,454] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 30625 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
@@ -114,3 +118,14 @@ tail -f mirrormaker.log | grep "ERROR"
 
 # 2021-10-12 19:17:21,083] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 8913 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
 # [2021-10-12 19:17:21,140] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+
+
+# getting same issue with replicator:
+
+# [2021-10-13 10:31:40,576] INFO WorkerSourceTask{id=replicator-0} flushing 30721 outstanding messages for offset commit (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-13 10:31:45,576] ERROR WorkerSourceTask{id=replicator-0} Failed to flush, timed out while waiting for producer to flush outstanding 28666 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-13 10:31:45,578] ERROR WorkerSourceTask{id=replicator-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-13 10:32:43,265] INFO Found matching topics: [executable-products, __consumer_timestamps] (io.confluent.connect.replicator.NewTopicMonitorThread)
+# [2021-10-13 10:32:45,511] INFO WorkerSourceTask{id=replicator-0} flushing 30721 outstanding messages for offset commit (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-13 10:32:50,511] ERROR WorkerSourceTask{id=replicator-0} Failed to flush, timed out while waiting for producer to flush outstanding 28711 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-13 10:32:50,512] ERROR WorkerSourceTask{id=replicator-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
