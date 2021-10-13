4e1adc8a285ec8869a4989c7e9a848360c982d68
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-10-13 11:53:47 +0200

    adding results

--- ccloud/mirrormaker2/mirrormaker2-repro-77222.sh
+++ ccloud/mirrormaker2/mirrormaker2-repro-77222.sh
@@ -59,3 +59,58 @@ tail -f mirrormaker.log | grep "ERROR"
 # [2021-10-12 11:01:59,392] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
 # [2021-10-12 11:03:04,326] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 30769 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
 # [2021-10-12 11:03:04,327] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+
+# with
+# A.offset.flush.timeout.ms=45000
+# B.offset.flush.timeout.ms=45000
+# [2021-10-12 16:30:45,302] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 14225 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 16:30:45,312] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-12 16:32:30,244] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 13393 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 16:32:30,251] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-12 16:34:15,190] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 12689 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 16:34:15,197] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-12 16:36:00,130] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 14161 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 16:36:00,133] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+
+# With:
+
+# A.offset.flush.interval.ms=30000
+# B.offset.flush.interval.ms=30000
+# A.offset.flush.timeout.ms=45000
+# B.offset.flush.timeout.ms=45000
+
+# Processed a total of 10 messages
+# [2021-10-12 16:50:58,645] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 14545 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 16:50:58,652] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-12 16:52:13,620] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 13905 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 16:52:13,628] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-12 16:53:28,594] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 13713 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 16:53:28,602] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-12 16:54:43,569] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 14097 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 16:54:43,573] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-12 16:55:58,541] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 15185 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 16:55:58,542] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+
+
+# A.offset.flush.interval.ms=30000
+# B.offset.flush.interval.ms=30000
+# A.offset.flush.timeout.ms=60000
+# B.offset.flush.timeout.ms=60000
+
+# Processed a total of 10 messages
+# [2021-10-12 17:15:56,548] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 8545 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 17:15:56,561] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-12 17:17:26,529] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 6785 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 17:17:26,539] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-12 17:18:56,507] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 13953 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 17:18:56,516] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+# [2021-10-12 17:20:26,482] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 7297 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 17:20:26,488] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
+
+# A.offset.flush.interval.ms=30000
+# B.offset.flush.interval.ms=30000
+# A.offset.flush.timeout.ms=90000
+# B.offset.flush.timeout.ms=90000
+
+# 2021-10-12 19:17:21,083] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to flush, timed out while waiting for producer to flush outstanding 8913 messages (org.apache.kafka.connect.runtime.WorkerSourceTask)
+# [2021-10-12 19:17:21,140] ERROR WorkerSourceTask{id=MirrorSourceConnector-0} Failed to commit offsets (org.apache.kafka.connect.runtime.SourceTaskOffsetCommitter)
