5b32e74c07c36f0e013165bb6a4688ff3c826671
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-10-14 11:17:06 +0200

    wrong commit
    Former-commit-id: 6ee9e94571c3cf61367f9d1c0afd8681a3f4004f [formerly 6ee23be6f09af2e51d98b1e6c6d88d68ea894099 [formerly dc9b1aae845730840d0678c5f8c56b6b83f688dd]]
    Former-commit-id: f049131429671e674cd97f6f99a4552c0b4be124
    Former-commit-id: d0cf23c275e448cc9dbcb8c6ab2201396e2c5276

--- troubleshooting/ldap-ad/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-source ${DIR}/../../scripts/utils.sh
-
-docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/docker-compose.plaintext.yml" -f "${PWD}/docker-compose.add-connect-worker.yml" down -v --remove-orphans
