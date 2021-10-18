84ebff0c14dc523a0a1ef29d2248fcf7479eadad
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-10-18 10:28:54 +0200

    minor
    Former-commit-id: 1a7d2f98c95bdebd54d58f749acff3c28fcb3bbb [formerly 5d182e7c4959c703c4de36b8dbbac4eb2294f745 [formerly bcc59e17c6f2b90b1a5d7e7f890a911cc65700aa]]
    Former-commit-id: f0a8073b9d642aeec739478ba5fbeadf00838f51
    Former-commit-id: 17b5b999254aa20e18642c4e5014b52bfbc90b7d

--- connect/connect-cdc-oracle19-source/cdc-oracle19-cdb-table-mtls.sh
+++ connect/connect-cdc-oracle19-source/cdc-oracle19-cdb-table-mtls.sh
@@ -204,7 +204,7 @@ done
 
 log "Waiting 60s for connector to read new data"
 
-log "Verifying topic ORCLCDB.C__MYUSER.CUSTOMERS: there should be 13 records records"
+log "Verifying topic ORCLCDB.C__MYUSER.CUSTOMERS: there should be 13 records"
 set +e
 timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic ORCLCDB.C__MYUSER.CUSTOMERS --from-beginning --max-messages 13 > /tmp/result.log  2>&1
 set -e
