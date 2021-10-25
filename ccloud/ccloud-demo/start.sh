a381c5e26a0c027a13db1c812e49f752765f384a
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-10-25 11:36:59 +0200

    removed ACL example
    Former-commit-id: 635bdda7dabfad5472cea85f49563f659af25da8 [formerly 619196a792e07d2405e4e6ef68a9a56a2f234d4d [formerly e38d2818f3d1c244f537cea34629f866cbfdac42]]
    Former-commit-id: 2bb6081511c2b78d00cb59e5a4c8edd1a758097b
    Former-commit-id: c87e2167e39e2550c841a00902b900aec9acf601

--- ccloud/ccloud-demo/start.sh
+++ ccloud/ccloud-demo/start.sh
@@ -30,7 +30,7 @@ else
      exit 1
 fi
 
-for component in producer consumer streams producer-acl
+for component in producer consumer streams
 do
      if [ ! -f ${DIR}/${component}/target/${component}-1.0.0-jar-with-dependencies.jar ]
      then
@@ -213,115 +213,6 @@ then
      log "Check that the data is available in Elasticsearch"
      curl -XGET 'http://localhost:9200/mysql-application/_search?pretty'
 fi
-if [ -z "$CLOUDFORMATION" ]
-then
-     exit 0
-fi
-log "Now we will test Service Account and ACLs"
-check_if_continue
-
-######################
-## Service Account and ACLs
-######################
-
-##################################################
-# Create a Service Account and API key and secret
-# - A service account represents an application, and the service account name must be globally unique
-##################################################
-
-log "Create a new service account"
-RANDOM_NUM=$((1 + RANDOM % 1000000))
-SERVICE_NAME="my-java-producer-app-$RANDOM_NUM"
-log "ccloud service-account create $SERVICE_NAME --description $SERVICE_NAME"
-ccloud service-account create $SERVICE_NAME --description $SERVICE_NAME || true
-SERVICE_ACCOUNT_ID=$(ccloud service-account list | grep $SERVICE_NAME | awk '{print $1;}')
-
-CCLOUD_CLUSTER=$(ccloud prompt -f "%k")
-log "Create an API key and secret for the new service account"
-log "ccloud api-key create --service-account $SERVICE_ACCOUNT_ID --resource $CCLOUD_CLUSTER"
-OUTPUT=$(ccloud api-key create --service-account $SERVICE_ACCOUNT_ID --resource $CCLOUD_CLUSTER)
-API_KEY_SA=$(echo "$OUTPUT" | grep '| API Key' | awk '{print $5;}')
-API_SECRET_SA=$(echo "$OUTPUT" | grep '| Secret' | awk '{print $4;}')
-
-log "Wait 90 seconds for the user and service account key and secret to propagate"
-sleep 90
-
-CLIENT_CONFIG="/tmp/client.config"
-log "Create a local configuration file $CLIENT_CONFIG for the client to connect to Confluent Cloud with the newly created API key and secret"
-log "Write properties to $CLIENT_CONFIG:"
-cat <<EOF > $CLIENT_CONFIG
-ssl.endpoint.identification.algorithm=https
-sasl.mechanism=PLAIN
-security.protocol=SASL_SSL
-bootstrap.servers=${BOOTSTRAP_SERVERS}
-sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username\="${API_KEY_SA}" password\="${API_SECRET_SA}";
-EOF
-
-##################################################
-# Run a Java client: before and after ACLs
-#
-# When ACLs are enabled on your Confluent Cloud cluster,
-# by default no client applications are authorized.
-#
-# The following steps show the same Java producer failing at first due to
-# 'TopicAuthorizationException' and then passing once the appropriate
-# ACLs are configured
-##################################################
-
-TOPIC_ACL="demo-acl-topic"
-set +e
-create_topic $TOPIC_ACL
-
-log "By default, no ACLs are configured"
-log "ccloud kafka acl list --service-account $SERVICE_ACCOUNT_ID"
-ccloud kafka acl list --service-account $SERVICE_ACCOUNT_ID
-
-log "Run the Java producer to $TOPIC_ACL: before ACLs"
-LOG1="/tmp/log.1"
-docker cp $CLIENT_CONFIG producer-acl:/tmp/
-docker exec producer-acl bash -c "java -jar producer-acl-1.0.0-jar-with-dependencies.jar $CLIENT_CONFIG $TOPIC_ACL" > $LOG1 2>&1
-log "# Check logs for 'org.apache.kafka.common.errors.TopicAuthorizationException'"
-OUTPUT=$(grep "org.apache.kafka.common.errors.TopicAuthorizationException" $LOG1)
-if [[ ! -z $OUTPUT ]]; then
-  log "PASS: Producer failed due to org.apache.kafka.common.errors.TopicAuthorizationException (expected because there are no ACLs to allow this client application)"
-else
-  logerror "FAIL: Something went wrong, check $LOG1"
-fi
-
-log "Create ACLs for the service account"
-log "ccloud kafka acl create --allow --service-account $SERVICE_ACCOUNT_ID --operation WRITE --topic $TOPIC_ACL"
-ccloud kafka acl create --allow --service-account $SERVICE_ACCOUNT_ID --operation WRITE --topic $TOPIC_ACL
-log "ccloud kafka acl list --service-account $SERVICE_ACCOUNT_ID"
-ccloud kafka acl list --service-account $SERVICE_ACCOUNT_ID
-
-sleep 20
-
-log "Run the Java producer to $TOPIC_ACL: after ACLs"
-LOG2="/tmp/log.2"
-docker exec producer-acl bash -c "java -jar producer-acl-1.0.0-jar-with-dependencies.jar $CLIENT_CONFIG $TOPIC_ACL" > $LOG2 2>&1
-log "# Check logs for '10 messages were produced to topic'"
-OUTPUT=$(grep "10 messages were produced to topic" $LOG2)
-if [[ ! -z $OUTPUT ]]; then
-  log "PASS: Producer works"
-else
-  logerror "FAIL: Something went wrong, check $LOG2"
-fi
-cat $LOG2
-
-##################################################
-# Cleanup
-# - Delete the ACLs, API key, service account
-##################################################
-
-log "Delete ACLs"
-log "ccloud kafka acl delete --allow --service-account $SERVICE_ACCOUNT_ID --operation WRITE --topic $TOPIC_ACL"
-ccloud kafka acl delete --allow --service-account $SERVICE_ACCOUNT_ID --operation WRITE --topic $TOPIC_ACL
-
-log "ccloud service-account delete $SERVICE_ACCOUNT_ID"
-ccloud service-account delete $SERVICE_ACCOUNT_ID
-
-log "ccloud api-key delete $API_KEY_SA"
-ccloud api-key delete $API_KEY_SA 1>/dev/null
 
 # kafka-consumer-groups command for Confluent Cloud
 # https://support.confluent.io/hc/en-us/articles/360022562212-kafka-consumer-groups-command-for-Confluent-Cloud
