e212b4bd73a34da344b42f69f2beb22dd8d1c9f9
Daniel Petisme <daniel.petisme@gmail.com>
2021-10-25 23:09:40 +0200

    Showcase the difference between topic.regex and topic.whitelist when topic deleted
    Former-commit-id: d95862322d65510dc65e2df860129a14411cb376 [formerly 9bf326baa760aaf54e8e4d41f83c525c7a01f2ee [formerly ea410b3de38f46057a0657b4d4f3e39990042f52]]
    Former-commit-id: acc48f9d440687b8044461ed81393c61994eef80
    Former-commit-id: 6fdbd18f09749ed8e1af6e8ec6530dda00b727d9

--- replicator/connect/connect-plaintext-repro-deleted-source-topic.sh
+++ replicator/connect/connect-plaintext-repro-deleted-source-topic.sh
@@ -19,7 +19,7 @@ seq -f "north_european_sale_%g `date +%s`" 10 | docker container exec -i connect
 log "Sending 10 sales in sales_SEMEA topic in Europe cluster"
 seq -f "south_european_sale_%g `date +%s`" 10 | docker container exec -i connect-europe bash -c "kafka-console-producer --broker-list broker-europe:9092 --topic sales_SEMEA"
 
-log "Replicate in the US"
+log "Replicate sales_NEMEA in the US using topic.whitelist"
 
 docker container exec connect-us \
 curl -X PUT \
@@ -34,9 +34,28 @@ curl -X PUT \
           "dest.kafka.bootstrap.servers": "broker-us:9092",
           "confluent.topic.replication.factor": 1,
           "provenance.header.enable": true,
-          "topic.regex": "sales.*"
+          "topic.whitelist": "sales_NEMEA"
           }' \
-     http://localhost:8083/connectors/replicate-europe-to-us/config | jq .
+     http://localhost:8083/connectors/replicate-nemea-to-us/config | jq .
+
+log "Replicate sales_SEMEA in the US using topic.regex"
+docker container exec connect-us \
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+          "connector.class":"io.confluent.connect.replicator.ReplicatorSourceConnector",
+          "key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+          "value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+          "header.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+          "src.consumer.group.id": "replicate-europe-to-us",
+          "src.kafka.bootstrap.servers": "broker-europe:9092",
+          "dest.kafka.bootstrap.servers": "broker-us:9092",
+          "confluent.topic.replication.factor": 1,
+          "provenance.header.enable": true,
+          "topic.regex": "sales_SEMEA",
+          "topic.poll.interval.ms": "10000"
+          }' \
+     http://localhost:8083/connectors/replicate-semea-to-us/config | jq .
 
 sleep 60
 
@@ -49,29 +68,22 @@ docker container exec -i connect-europe bash -c "kafka-console-consumer --bootst
 log "Deleting topic sales_NEMEA in Europe cluster"
 docker exec broker-europe kafka-topics --delete --topic sales_NEMEA --bootstrap-server broker-europe:9092
 
-sleep 10
+sleep 5
 
 log "Verify topic is still present in US cluster"
 docker exec broker-us kafka-topics --describe --topic sales_NEMEA --bootstrap-server broker-us:9092
 
 log "Verify replicator can not find the sales_NEMEA topic anymore in th Europe cluster "
-docker logs connect-us | grep ".*WARN.*Received unknown topic or partition error in fetch for partition sales_NEMEA.*"
+docker logs connect-us | grep ".*WARN \[replicate-nemea-to-us.*\].*"
+# [replicate-nemea-to-us]
 
-log "Verify replication continues, sending 10 new sales in sales_SEMEA topic in Europe cluster"
-seq -f "south_european_sale_new_%g `date +%s`" 10 | docker container exec -i connect-europe bash -c "kafka-console-producer --broker-list broker-europe:9092 --topic sales_SEMEA"
-
-sleep 10
-
-log "Verify we have received the 10 new SEMEA sales in US cluster"
-docker container exec -i connect-europe bash -c "kafka-console-consumer --bootstrap-server broker-us:9092 --whitelist 'sales_SEMEA' --from-beginning --max-messages 20"
-
-log "Re-creating topic sales_NEMEA topic in Europe cluster"
-docker container exec -i broker-europe bash -c "kafka-topics --create --topic sales_NEMEA --bootstrap-server broker-europe:9092"
-
-log "Verify replication continues, sending 10 new sales in sales_NEMEA topic in Europe cluster"
-seq -f "north_european_sale_new_%g `date +%s`" 10 | docker container exec -i connect-europe bash -c "kafka-console-producer --broker-list broker-europe:9092 --topic sales_NEMEA"
+log "Deleting topic sales_SEMEA in Europe cluster"
+docker exec broker-europe kafka-topics --delete --topic sales_SEMEA --bootstrap-server broker-europe:9092
 
 sleep 10
 
-log "Verify we have received the 10 NEMEA sales in US cluster"
-docker container exec -i connect-europe bash -c "kafka-console-consumer --bootstrap-server broker-us:9092 --whitelist 'sales_NEMEA' --from-beginning --max-messages 20"
+log "Verify topic is still present in US cluster"
+docker exec broker-us kafka-topics --describe --topic sales_SEMEA --bootstrap-server broker-us:9092
+
+log "Verify replicator can not find the sales_SEMEA topic anymore in th Europe cluster "
+docker logs connect-us | grep ".*WARN \[replicate-semea-to-us.*\].*"
