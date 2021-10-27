bb51aa0186ebaa528850a68dceeb2b380f98c977
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-10-27 17:40:33 +0200

    Save CI time by not running KNOWN ISSUES
    Former-commit-id: 3b77e4cd6f52677041f227b22486059e81882a11 [formerly 0792661b25e2e3ef353e87081d3451221b1b27a9 [formerly 678b19d4ac2a5b7e72f681f7965aed20c039dbde]]
    Former-commit-id: 5559c028e9178589f7f198a181f62c1ec8ec0fd5
    Former-commit-id: 908f90fe0a2ea8b069d86271feba51513a7ad8bc

--- connect/connect-aws-s3-source/s3-source-with-assuming-iam-role.sh
+++ connect/connect-aws-s3-source/s3-source-with-assuming-iam-role.sh
@@ -4,6 +4,10 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+# KNOWN ISSUE
+logerror "💀 KNOWN ISSUE: https://confluentinc.atlassian.net/browse/CCMSG-1015"
+exit 1
+
 export AWS_CREDENTIALS_FILE_NAME=credentials-with-assuming-iam-role
 if [ ! -f $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME ]
 then
