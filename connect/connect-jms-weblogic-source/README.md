c68821bf576b4f7816e285a181aa2a37135de412
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-10-28 17:27:05 +0200

    Add Source Oracle Weblogic example with generic JMS connector #1470
    Former-commit-id: bac69e55e3116fee2f77be49de473453a4fe77b1 [formerly a4077a14704c098ecb6b08ce707ddffc84ae2684 [formerly b44845fc064564fb6dc6d0f2abe8ce079bbf45e2]]
    Former-commit-id: 256992dd1b6dfe89aa8cac310ed6648557ee2f25
    Former-commit-id: 17b8692080c02c1159061b1f7b21ea7ed772c0d6

---
+++ connect/connect-jms-weblogic-source/README.md
@@ -0,0 +1,72 @@
+# Oracle Weblogic Source with JMS connector
+
+## Objective
+
+Quickly test [Oracle Weblogic](https://docs.confluent.io/current/connect/kafka-connect-jms/sink/index.html) with JMS connector.
+
+Using Oracle WebLogic Server (Developer Tier, version `12.2.1.3`) Docker [image](https://hub.docker.com/_/oracle-weblogic-server-12c). You need to register to get the image.
+
+## How to run
+
+Simply run:
+
+```
+$ ./jms-weblogic-source.sh
+```
+
+N.B: Weblogic console is reachable at [http://127.0.0.1:7001/console](http://127.0.0.1:7001/console]) (`weblogic`/`welcome1`)
+
+You can use it to see all JMS resources:
+
+![console](screenshot1.jpg)
+
+## Details of what the script is doing
+
+### Queue example
+
+Creating JMS weblogic source connector
+
+```bash
+$ curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.jms.JmsSourceConnector",
+               "kafka.topic": "from-weblogic-messages",
+               "java.naming.factory.initial": "weblogic.jndi.WLInitialContextFactory",
+               "jms.destination.name": "myJMSServer/mySystemModule!myJMSServer@MyDistributedQueue",
+               "jms.destination.type": "QUEUE",
+               "java.naming.provider.url": "t3://weblogic-jms:7001",
+               "connection.factory.name": "myFactory",
+               "java.naming.security.principal": "weblogic",
+               "java.naming.security.credentials": "welcome1",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "tasks.max" : "1",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/weblogic-source/config | jq .
+```
+
+Sending one message in JMS queue myQueue:
+
+```bash
+$ docker exec jms-sender bash -c 'java -cp "/tmp/weblogic.jar:/tmp/wlthint3client.jar:/jms-sender-1.0.0.jar" com.sample.jms.toolkit.JMSSender'
+````
+
+Verify we have received the data in from-weblogic-messages topic
+
+```bash
+$ docker exec connect kafka-console-consumer -bootstrap-server broker:9092 --topic from-weblogic-messages --from-beginning --max-messages 1
+```
+
+Results:
+
+```
+Struct{messageID=ID:<704504.1635434551285.0>,messageType=text,timestamp=1635434551285,deliveryMode=2,destination=Struct{destinationType=queue,name=mySystemModule!myJMSServer@MyDistributedQueue},redelivered=false,expiration=0,priority=4,properties={JMS_BEA_DeliveryTime=Struct{propertyType=long,long=1635434551285}, JMSXDeliveryCount=Struct{propertyType=integer,integer=1}},text=Hello Queue World!}
+```
+
+
+N.B: Control Center is reachable at [http://127.0.0.1:9021](http://127.0.0.1:9021])
+
---
+++ connect/connect-jms-weblogic-source/jms-weblogic-source.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if test -z "$(docker images -q store/oracle/weblogic:12.2.1.3-dev-200127)"
+then
+     if [ ! -z "$CI" ]
+     then
+          # if this is github actions, pull the image
+          docker pull store/oracle/weblogic:12.2.1.3-dev-200127
+     else
+          logerror "Image store/oracle/weblogic:12.2.1.3-dev-200127 is not present. You must pull it from https://hub.docker.com/_/oracle-weblogic-server-12c"
+          exit 1
+     fi
+fi
+
+# https://github.com/oracle/docker-images/tree/main/OracleWebLogic/samples/12212-domain-online-config
+if test -z "$(docker images -q weblogic-jms:latest)"
+then
+     log "Building WebLogic JMS docker image..it can take a while..."
+     OLDDIR=$PWD
+     cd ${DIR}/docker-weblogic
+     docker build --build-arg ADMIN_PASSWORD="welcome1" -t 1213-domain ./1213-domain
+     docker build -t weblogic-jms:latest ./12212-domain-online-config -f ./12212-domain-online-config/Dockerfile
+     cd ${OLDDIR}
+fi
+
+if [ ! -f ${DIR}/jms-sender/lib/wlthint3client.jar ]
+then
+     docker run weblogic-jms:latest cat /u01/oracle/wlserver/server/lib/wlthint3client.jar > ${DIR}/jms-sender/lib/wlthint3client.jar
+fi
+
+if [ ! -f ${DIR}/jms-sender/lib/weblogic.jar ]
+then
+     docker run weblogic-jms:latest cat /u01/oracle/wlserver/server/lib/weblogic.jar > ${DIR}/jms-sender/lib/weblogic.jar
+fi
+
+for component in jms-sender
+do
+     set +e
+     log "🏗 Building jar for ${component}"
+     docker run -i --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -e TAG=$TAG_BASE -v "${DIR}/${component}":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/${component}/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn -Dkafka.tag=$TAG -Dkafka.client.tag=$KAFKA_CLIENT_TAG package > /tmp/result.log 2>&1
+     if [ $? != 0 ]
+     then
+          logerror "ERROR: failed to build java component $component"
+          tail -500 /tmp/result.log
+          exit 1
+     fi
+     set -e
+done
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+
+log "Creating JMS weblogic source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.jms.JmsSourceConnector",
+               "kafka.topic": "from-weblogic-messages",
+               "java.naming.factory.initial": "weblogic.jndi.WLInitialContextFactory",
+               "jms.destination.name": "myJMSServer/mySystemModule!myJMSServer@MyDistributedQueue",
+               "jms.destination.type": "QUEUE",
+               "java.naming.provider.url": "t3://weblogic-jms:7001",
+               "connection.factory.name": "myFactory",
+               "java.naming.security.principal": "weblogic",
+               "java.naming.security.credentials": "welcome1",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "tasks.max" : "1",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/weblogic-source/config | jq .
+
+
+sleep 5
+
+log "Sending one message in JMS queue myQueue"
+docker exec jms-sender bash -c 'java -cp "/tmp/weblogic.jar:/tmp/wlthint3client.jar:/jms-sender-1.0.0.jar" com.sample.jms.toolkit.JMSSender'
+
+sleep 5
+
+log "Verify we have received the data in from-weblogic-messages topic"
+timeout 60 docker exec connect kafka-console-consumer -bootstrap-server broker:9092 --topic from-weblogic-messages --from-beginning --max-messages 1
+
+
