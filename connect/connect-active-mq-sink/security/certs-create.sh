0399a5b2c261053ad84ded35a3634705f23eee1f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-03 14:39:36 +0000

    #1410
    Former-commit-id: ae16ee8bc0888fe5d919e0ce37c0c43c5a56d773 [formerly 7a453e4a0537aba6010aa8b9b08596b14121bd12 [formerly 5f0fb26990bc70f2966ec660573fab86a6377f7e]]
    Former-commit-id: 602e760b60953837c48db71522da632cc26697fa
    Former-commit-id: 501b639af2d0d8fb3a853bbe0fa3d520a3add8ac

--- connect/connect-active-mq-sink/security/certs-create.sh
+++ connect/connect-active-mq-sink/security/certs-create.sh
@@ -6,25 +6,17 @@
 #    -o xtrace
 
 # Cleanup files
-rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12 extfile
+rm -f /tmp/*.crt /tmp/*.csr /tmp/*_creds /tmp/*.jks /tmp/*.srl /tmp/*.key /tmp/*.pem /tmp/*.der /tmp/*.p12 /tmp/extfile
 
-if [[ "$OSTYPE" == "darwin"* ]]
-then
-    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
-    chmod -R a+rw .
-else
-    # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
-    sudo chmod -R a+rw .
-fi
 # Generate CA key
-docker run --rm -v $PWD:/tmp rmohr/activemq:5.15.9 openssl req -new -x509 -keyout /tmp/snakeoil-ca-1.key -out /tmp/snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US' -passin pass:confluent -passout pass:confluent
+openssl req -new -x509 -keyout /tmp/snakeoil-ca-1.key -out /tmp/snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
 for i in connect activemq
 do
     echo "------------------------------- $i -------------------------------"
 
     # Create host keystore
-    docker run --rm -v $PWD:/tmp rmohr/activemq:5.15.9 keytool -genkey -noprompt \
+    keytool -genkey -noprompt \
                  -alias $i \
                  -dname "CN=$i,OU=TEST,O=CONFLUENT,L=PaloAlto,S=Ca,C=US" \
                                  -ext "SAN=dns:$i,dns:localhost" \
@@ -35,10 +27,10 @@ do
                  -storetype pkcs12
 
     # Create the certificate signing request (CSR)
-    docker run --rm -v $PWD:/tmp rmohr/activemq:5.15.9 keytool -keystore /tmp/kafka.$i.keystore.jks -alias $i -certreq -file /tmp/$i.csr -storepass confluent -keypass confluent -ext "SAN=dns:$i,dns:localhost"
+    keytool -keystore /tmp/kafka.$i.keystore.jks -alias $i -certreq -file /tmp/$i.csr -storepass confluent -keypass confluent -ext "SAN=dns:$i,dns:localhost"
         #openssl req -in $i.csr -text -noout
 
-cat << EOF > extfile
+cat << EOF > /tmp/extfile
 [req]
 distinguished_name = req_distinguished_name
 x509_extensions = v3_req
@@ -52,20 +44,20 @@ DNS.1 = $i
 DNS.2 = localhost
 EOF
         # Sign the host certificate with the certificate authority (CA)
-        docker run --rm -v $PWD:/tmp rmohr/activemq:5.15.9 openssl x509 -req -CA /tmp/snakeoil-ca-1.crt -CAkey /tmp/snakeoil-ca-1.key -in /tmp/$i.csr -out /tmp/$i-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:confluent -extensions v3_req -extfile /tmp/extfile
+        openssl x509 -req -CA /tmp/snakeoil-ca-1.crt -CAkey /tmp/snakeoil-ca-1.key -in /tmp/$i.csr -out /tmp/$i-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:confluent -extensions v3_req -extfile /tmp/extfile
 
         #openssl x509 -noout -text -in $i-ca1-signed.crt
 
         # Sign and import the CA cert into the keystore
-    docker run --rm -v $PWD:/tmp rmohr/activemq:5.15.9 keytool -noprompt -keystore /tmp/kafka.$i.keystore.jks -alias CARoot -import -file /tmp/snakeoil-ca-1.crt -storepass confluent -keypass confluent
+    keytool -noprompt -keystore /tmp/kafka.$i.keystore.jks -alias CARoot -import -file /tmp/snakeoil-ca-1.crt -storepass confluent -keypass confluent
         #keytool -list -v -keystore kafka.$i.keystore.jks -storepass confluent
 
         # Sign and import the host certificate into the keystore
-    docker run --rm -v $PWD:/tmp rmohr/activemq:5.15.9  keytool -noprompt -keystore /tmp/kafka.$i.keystore.jks -alias $i -import -file /tmp/$i-ca1-signed.crt -storepass confluent -keypass confluent -ext "SAN=dns:$i,dns:localhost"
+     keytool -noprompt -keystore /tmp/kafka.$i.keystore.jks -alias $i -import -file /tmp/$i-ca1-signed.crt -storepass confluent -keypass confluent -ext "SAN=dns:$i,dns:localhost"
         #keytool -list -v -keystore kafka.$i.keystore.jks -storepass confluent
 
     # Create truststore and import the CA cert
-    docker run --rm -v $PWD:/tmp rmohr/activemq:5.15.9  keytool -noprompt -keystore /tmp/kafka.$i.truststore.jks -alias CARoot -import -file /tmp/snakeoil-ca-1.crt -storepass confluent -keypass confluent
+     keytool -noprompt -keystore /tmp/kafka.$i.truststore.jks -alias CARoot -import -file /tmp/snakeoil-ca-1.crt -storepass confluent -keypass confluent
 
     # Save creds
       echo  "confluent" > ${i}_sslkey_creds
