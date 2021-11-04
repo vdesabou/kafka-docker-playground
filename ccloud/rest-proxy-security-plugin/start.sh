0184278df22df76af9840aa110611255dc8b5f23
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-04 11:53:37 +0100

    dev null
    Former-commit-id: b46e9db0032b096843baf3783c8772527f500daf [formerly b49ba1f915a3c9dd7d1120e8bfd083b8410fcf5f [formerly d2a81c1b47236536b4e03434aa7605e563a1cb7a]]
    Former-commit-id: e39beb14ffbd692f62a93973fd8df10f32f2f3db
    Former-commit-id: f28f4b677a8252996816cb849139df4f70437507

--- ccloud/rest-proxy-security-plugin/start.sh
+++ ccloud/rest-proxy-security-plugin/start.sh
@@ -56,7 +56,7 @@ sed -e "s|:CCLOUD_REST_PROXY_SECURITY_PLUGIN_API_KEY:|$CCLOUD_REST_PROXY_SECURIT
 
 cd ${DIR}/security
 log "🔐 Generate keys and certificates used for SSL"
-docker run -u0 --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} bash -c "/tmp/certs-create.sh $CCLOUD_REST_PROXY_SECURITY_PLUGIN_API_KEY && chown -R $(id -u $USER):$(id -g $USER) /tmp/"
+docker run -u0 --rm -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} bash -c "/tmp/certs-create.sh > /dev/null 2>&1 $CCLOUD_REST_PROXY_SECURITY_PLUGIN_API_KEY && chown -R $(id -u $USER):$(id -g $USER) /tmp/"
 cd ${DIR}
 
 docker-compose -f "${PWD}/docker-compose.yml" up -d
