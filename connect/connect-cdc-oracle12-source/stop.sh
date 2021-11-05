07d0e668448c6513ce8238f0a90c0a456ffacfff
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-05 15:11:52 +0100

    Get rid of custom image vdesabou/oracle12 #1492
    Former-commit-id: f03f5778965c20c95892cdc9933a621a90965f2b [formerly 1b930f9d2ceaeed48128a4ac607316b50f3d6e5a [formerly 79440f6d778db076e93ea41761fb2ccc926fc026]]
    Former-commit-id: ea5d7fc6235e49055d02962e44f746c05dc15891
    Former-commit-id: fc58a6533d0ecba9aeca68d454790a11753bac0e

--- connect/connect-cdc-oracle12-source/stop.sh
+++ connect/connect-cdc-oracle12-source/stop.sh
@@ -6,10 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
 export ORACLE_IMAGE="oracle/database:12.2.0.1-ee"
-if [ ! -z "$CI" ]
-then
-     # if this is github actions, use private image.
-     export ORACLE_IMAGE="vdesabou/oracle12"
-fi
 
 stop_all "$DIR"
