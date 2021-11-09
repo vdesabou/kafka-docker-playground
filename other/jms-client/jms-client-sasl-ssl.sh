ef3fb442864d25e2e475f78b13ed94c8fcb2b738
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-09 12:35:31 +0100

    changed version
    Former-commit-id: 6c3a15a2b12b409860b03ef792c8fb13954a246c [formerly e5f7cedf24a6435b3303ec0da550f3ae5cbbba83 [formerly 39cee8e583a50ff3206328e7a2328336113aad6c]]
    Former-commit-id: fd469a4dbe77c474f202596834f499c54f55c12a
    Former-commit-id: 02cd74aa4d747166b7610768c616f599dcc8e251

--- other/jms-client/jms-client-sasl-ssl.sh
+++ other/jms-client/jms-client-sasl-ssl.sh
@@ -4,7 +4,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-if version_gt $TAG_BASE "5.3.2"; then
+if version_gt $TAG_BASE "5.3.99"; then
     logwarn "WARN: zookeeper.connect is deprecated. Use JMS client with confluent.license parameter only."
     exit 0
 fi
