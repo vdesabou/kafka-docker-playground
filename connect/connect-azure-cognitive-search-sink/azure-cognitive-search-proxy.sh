99640a8a2eb343e9ecbdc3dccd8782d7fca86f83
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-16 11:58:44 +0100

    Add Azure Cognitive Search Sink example with proxy #1669
    Former-commit-id: a03f9c7feb36069cbb9ab95ce3320ebf3993c03e [formerly 2e5637931cbcfb52d022fbe2d29d3fc55a933230 [formerly a232b60ec82a8951e2c0d1c40fa73a38b99468b9]]
    Former-commit-id: b379d30911d2b2e5edcb95a5a66375cd80c75286
    Former-commit-id: 08ca90bf4324d94e85a267a6a072e47b16dfa5d4

---
+++ connect/connect-azure-cognitive-search-sink/azure-cognitive-search-proxy.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -z "$CI" ]
+then
+     # running with github actions
+     if [ ! -f ../../secrets.properties ]
+     then
+          logerror "../../secrets.properties is not present!"
+          exit 1
+     fi
+     source ../../secrets.properties > /dev/null 2>&1
+fi
+
+if [ ! -z "$AZ_USER" ] && [ ! -z "$AZ_PASS" ]
+then
+    log "Logging to Azure using environment variables AZ_USER and AZ_PASS"
+    set +e
+    az logout
+    set -e
+    az login -u "$AZ_USER" -p "$AZ_PASS"
+else
+    log "Logging to Azure using browser"
+    az login
+fi
+
+AZURE_NAME=pg${USER}s${GITHUB_RUN_NUMBER}${TAG}
+AZURE_NAME=${AZURE_NAME//[-._]/}
+AZURE_RESOURCE_GROUP=$AZURE_NAME
+AZURE_SEARCH_SERVICE_NAME=$AZURE_NAME
+AZURE_REGION=westeurope
+
+set +e
+az group delete --name $AZURE_RESOURCE_GROUP --yes
+set -e
+
+log "Creating Azure Resource Group $AZURE_RESOURCE_GROUP"
+az group create \
+    --name $AZURE_RESOURCE_GROUP \
+    --location $AZURE_REGION
+log "Creating Azure Search service"
+az search service create \
+    --name $AZURE_SEARCH_SERVICE_NAME \
+    --resource-group $AZURE_RESOURCE_GROUP \
+    --location $AZURE_REGION \
+    --sku Basic
+AZURE_SEARCH_ADMIN_PRIMARY_KEY=$(az search admin-key show \
+    --resource-group $AZURE_RESOURCE_GROUP \
+    --service-name $AZURE_SEARCH_SERVICE_NAME | jq -r '.primaryKey')
+
+
+log "Creating Azure Search index"
+curl -X POST \
+"https://${AZURE_SEARCH_SERVICE_NAME}.search.windows.net/indexes?api-version=2019-05-06" \
+-H 'Accept: application/json' \
+-H 'Content-Type: application/json' \
+-H "api-key: $AZURE_SEARCH_ADMIN_PRIMARY_KEY" \
+-d '{
+  "name": "hotels-sample-index",
+  "fields": [
+    {"name": "HotelId", "type": "Edm.String", "key": true, "searchable": false, "sortable": false, "facetable": false},
+    {"name": "Description", "type": "Edm.String", "filterable": false, "sortable": false, "facetable": false},
+    {"name": "HotelName", "type": "Edm.String", "facetable": false}
+  ]
+}'
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.proxy.yml"
+
+log "Sending messages to topic hotels-sample"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic hotels-sample --property key.schema='{"type":"string"}' --property "parse.key=true" --property "key.separator=," --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"HotelName","type":"string"},{"name":"Description","type":"string"}]}' << EOF
+"marriottId",{"HotelName": "Marriott", "Description": "Marriott description"}
+"holidayinnId",{"HotelName": "HolidayInn", "Description": "HolidayInn description"}
+"motel8Id",{"HotelName": "Motel8", "Description": "motel8 description"}
+EOF
+
+
+log "Creating Azure Search Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+                "connector.class": "io.confluent.connect.azure.search.AzureSearchSinkConnector",
+                "tasks.max": "1",
+                "topics": "hotels-sample",
+                "key.converter": "io.confluent.connect.avro.AvroConverter",
+                "key.converter.schema.registry.url": "http://schema-registry:8081",
+                "value.converter": "io.confluent.connect.avro.AvroConverter",
+                "value.converter.schema.registry.url": "http://schema-registry:8081",
+                "azure.search.service.name": "'"$AZURE_SEARCH_SERVICE_NAME"'",
+                "azure.search.api.key": "'"$AZURE_SEARCH_ADMIN_PRIMARY_KEY"'",
+                "proxy.host": "nginx-proxy",
+                "proxy.port": "8888",
+                "index.name": "${topic}-index",
+                "confluent.license": "",
+                "confluent.topic.bootstrap.servers": "broker:9092",
+                "confluent.topic.replication.factor": "1",
+                "reporter.bootstrap.servers": "broker:9092",
+                "reporter.error.topic.name": "test-error",
+                "reporter.error.topic.replication.factor": 1,
+                "reporter.error.topic.key.format": "string",
+                "reporter.error.topic.value.format": "string",
+                "reporter.result.topic.name": "test-result",
+                "reporter.result.topic.key.format": "string",
+                "reporter.result.topic.value.format": "string",
+                "reporter.result.topic.replication.factor": 1
+          }' \
+     http://localhost:8083/connectors/azure-cognitive-search-proxy/config | jq .
+
+
+sleep 10
+
+log "Searching Azure Search index"
+curl -X GET \
+"https://${AZURE_SEARCH_SERVICE_NAME}.search.windows.net/indexes/hotels-sample-index/docs?api-version=2019-05-06&search=*" \
+-H 'Content-Type: application/json' \
+-H "api-key: $AZURE_SEARCH_ADMIN_PRIMARY_KEY" | jq > /tmp/result.log  2>&1
+
+cat /tmp/result.log
+grep "Marriott" /tmp/result.log
+grep "HolidayInn" /tmp/result.log
+grep "Motel8" /tmp/result.log
+
+log "Deleting resource group"
+az group delete --name $AZURE_RESOURCE_GROUP --yes --no-wait
