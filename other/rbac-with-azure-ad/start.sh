a412bca89d161da6107fb66bee113add84f63d9b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-17 14:52:52 +0100

    Add easy way to re-create containers #1695
    Former-commit-id: 048b49ebe58760ace250e6c3149f33030fa003f6 [formerly e3ffdba979f29ae0dde919fca42766c6db7c32c2 [formerly 48ff0e983017c85cee69c7d00f7f31fef3499b12]]
    Former-commit-id: 00ee08519447a36951365976ffc74272eda06953
    Former-commit-id: 0d64db52ec98561c2a47e1f27c90cf329959c71c

--- other/rbac-with-azure-ad/start.sh
+++ other/rbac-with-azure-ad/start.sh
@@ -70,8 +70,10 @@ docker exec -i tools bash -c "/tmp/helper/create-role-bindings.sh"
 
 docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/rbac-sasl-plain/docker-compose.yml -f "${PWD}/docker-compose.rbac-with-azure-ad.yml" ${profile_control_center_command} ${profile_ksqldb_command} up -d
 log "📝 To see the actual properties file, use ../../scripts/get-properties.sh <container>"
-log "🔃 If you modify a docker-compose file and want to re-create the container(s), use this command:"
-log "🔃 source ../../scripts/utils.sh && docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/rbac-sasl-plain/docker-compose.yml -f ${PWD}/docker-compose.rbac-with-azure-ad.yml ${profile_control_center_command} ${profile_ksqldb_command} up -d"
+command="source ../../scripts/utils.sh && docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/rbac-sasl-plain/docker-compose.yml -f ${PWD}/docker-compose.rbac-with-azure-ad.yml ${profile_control_center_command} ${profile_ksqldb_command} up -d"
+echo "$command" > /tmp/playground-command
+log "✨ If you modify a docker-compose file and want to re-create the container(s), run ../../scripts/recreate-containers.sh or use this command:"
+log "✨ $command"
 
 ../../scripts/wait-for-connect-and-controlcenter.sh
 
