b17af31d62ca8059d515a00548d777fcbd018d72
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-19 10:26:50 +0100

    script to tail all containers

---
+++ scripts/tail-logs-all-containers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../scripts/utils.sh
+
+if [ ! -f /tmp/playground-command ]
+then
+  logerror "File containing restart command /tmp/playground-command does not exist!"
+  exit 1
+fi
+
+sed -e "s|up -d|logs --tail=100 -f|g" \
+    /tmp/playground-command > /tmp/playground-command-debugging
+
+bash /tmp/playground-command-debugging
