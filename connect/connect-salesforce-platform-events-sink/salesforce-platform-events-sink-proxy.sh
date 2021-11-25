5f44cc7b750fd02125d59294b068b1fbcd1cde54
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-25 17:52:59 +0100

    Add proxy example for connect-salesforce-platform-events-sink #1710

---
+++ connect/connect-salesforce-platform-events-sink/salesforce-platform-events-sink-proxy.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -z "$CI" ]
+then
+     # running with github actions
+     if [ ! -f ../../secrets.properties ]
+     then
+          logerror "../../secrets.properties is not present!"
+          exit 1
+     fi
+     source ../../secrets.properties > /dev/null 2>&1
+fi
+
+SALESFORCE_USERNAME=${SALESFORCE_USERNAME:-$1}
+SALESFORCE_PASSWORD=${SALESFORCE_PASSWORD:-$2}
+CONSUMER_KEY=${CONSUMER_KEY:-$3}
+CONSUMER_PASSWORD=${CONSUMER_PASSWORD:-$4}
+SECURITY_TOKEN=${SECURITY_TOKEN:-$5}
+SALESFORCE_INSTANCE=${SALESFORCE_INSTANCE:-"https://login.salesforce.com"}
+
+if [ -z "$SALESFORCE_USERNAME" ]
+then
+     logerror "SALESFORCE_USERNAME is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_PASSWORD" ]
+then
+     logerror "SALESFORCE_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+
+if [ -z "$CONSUMER_KEY" ]
+then
+     logerror "CONSUMER_KEY is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$CONSUMER_PASSWORD" ]
+then
+     logerror "CONSUMER_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SECURITY_TOKEN" ]
+then
+     logerror "SECURITY_TOKEN is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.proxy.yml"
+
+log "Login with sfdx CLI"
+docker exec sfdx-cli sh -c "sfdx sfpowerkit:auth:login -u \"$SALESFORCE_USERNAME\" -p \"$SALESFORCE_PASSWORD\" -r \"$SALESFORCE_INSTANCE\" -s \"$SECURITY_TOKEN\""
+
+log "Send Platform Events"
+docker exec sfdx-cli sh -c "sfdx force:apex:execute  -u \"$SALESFORCE_USERNAME\" -f \"/tmp/event.apex\""
+
+DOMAIN=$(echo $SALESFORCE_INSTANCE | cut -d "/" -f 3)
+IP=$(nslookup $DOMAIN | grep Address | grep -v "#" | cut -d " " -f 2 | tail -1)
+log "Blocking $DOMAIN IP $IP to make sure proxy is used"
+docker exec --privileged --user root connect bash -c "iptables -A INPUT -p tcp -s $IP -j DROP"
+
+log "Creating Salesforce Platform Events Source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+                    "connector.class": "io.confluent.salesforce.SalesforcePlatformEventSourceConnector",
+                    "kafka.topic": "sfdc-platform-events",
+                    "tasks.max": "1",
+                    "curl.logging": "true",
+                    "salesforce.platform.event.name" : "MyPlatformEvent__e",
+                    "salesforce.instance" : "'"$SALESFORCE_INSTANCE"'",
+                    "salesforce.username" : "'"$SALESFORCE_USERNAME"'",
+                    "salesforce.password" : "'"$SALESFORCE_PASSWORD"'",
+                    "salesforce.password.token" : "'"$SECURITY_TOKEN"'",
+                    "salesforce.consumer.key" : "'"$CONSUMER_KEY"'",
+                    "salesforce.consumer.secret" : "'"$CONSUMER_PASSWORD"'",
+                    "http.proxy": "nginx-proxy:8888",
+                    "salesforce.initial.start" : "all",
+                    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/salesforce-platform-events-source-proxy/config | jq .
+
+
+
+sleep 10
+
+log "Verify we have received the data in sfdc-platform-events topic"
+timeout 60 docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic sfdc-platform-events --from-beginning --max-messages 2
+
+log "Creating Salesforce Platform Events Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+                    "connector.class": "io.confluent.salesforce.SalesforcePlatformEventSinkConnector",
+                    "topics": "sfdc-platform-events",
+                    "tasks.max": "1",
+                    "curl.logging": "true",
+                    "salesforce.platform.event.name" : "MyPlatformEvent__e",
+                    "salesforce.instance" : "'"$SALESFORCE_INSTANCE"'",
+                    "salesforce.username" : "'"$SALESFORCE_USERNAME"'",
+                    "salesforce.password" : "'"$SALESFORCE_PASSWORD"'",
+                    "salesforce.password.token" : "'"$SECURITY_TOKEN"'",
+                    "salesforce.consumer.key" : "'"$CONSUMER_KEY"'",
+                    "salesforce.consumer.secret" : "'"$CONSUMER_PASSWORD"'",
+                    "http.proxy": "nginx-proxy:8888",
+                    "salesforce.initial.start" : "all",
+                    "connection.max.message.size": "10048576",
+                    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "reporter.bootstrap.servers": "broker:9092",
+                    "reporter.error.topic.name": "error-responses",
+                    "reporter.error.topic.replication.factor": 1,
+                    "reporter.result.topic.name": "success-responses",
+                    "reporter.result.topic.replication.factor": 1,
+                    "transforms": "MaskField",
+                    "transforms.MaskField.type": "org.apache.kafka.connect.transforms.MaskField$Value",
+                    "transforms.MaskField.fields": "Message__c",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/salesforce-platform-events-sink-proxy/config | jq .
+
+
+sleep 10
+
+log "Verify topic success-responses"
+timeout 60 docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic success-responses --from-beginning --max-messages 2
+
+# log "Verify topic error-responses"
+# timeout 20 docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic error-responses --from-beginning --max-messages 1
