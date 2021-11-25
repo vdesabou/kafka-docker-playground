2ca5b89688cc6be5c8ffdf4b0f7715ec7c09c860
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-25 11:08:03 +0100

    Adding add_packet_corruption
    Former-commit-id: 3ad7430ef2e546d33b72d123f447971898797bd6 [formerly 58ac075c0274e387eec9cd452795bc645f78da48 [formerly 63fded927965d1261935cfce646940115edf7916]]
    Former-commit-id: 2f153fc275d510c83f3a1472465d3dff72dfe5d2
    Former-commit-id: 2467577aabe284edd1f9737f95b65712fd6c9fc1

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1046,19 +1046,37 @@ done
   log "JMX metrics are available in /tmp/jmx_metrics.log file"
 }
 
-container_to_name() {
+# https://www.linuxjournal.com/content/validating-ip-address-bash-script
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+function container_to_name() {
     container=$1
     echo "${PWD##*/}_${container}_1"
 }
 
-container_to_ip() {
+function container_to_ip() {
     if [ $# -lt 1 ]; then
         echo "Usage: container_to_ip container"
     fi
     echo $(docker inspect $1 -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
 }
 
-clear_traffic_control() {
+function clear_traffic_control() {
     if [ $# -lt 1 ]; then
         echo "Usage: clear_traffic_control src_container"
     fi
@@ -1068,10 +1086,10 @@ clear_traffic_control() {
     echo "Removing all traffic control settings on $src_container"
 
     # Delete the entry from the tc table so the changes made to tc do not persist
-    docker exec --privileged -u0 -t $src_container tc qdisc del dev eth0 root 2>&1
+    docker exec --privileged -u0 -t $src_container tc qdisc del dev eth0 root
 }
 
-get_latency() {
+function get_latency() {
     if [ $# -lt 2 ]; then
         echo "Usage: get_latency src_container dst_container"
     fi
@@ -1081,14 +1099,19 @@ get_latency() {
 }
 
 # https://serverfault.com/a/906499
-add_latency() {
+function add_latency() {
     if [ $# -lt 3 ]; then
-        echo "Usage: add_latency src_container dst_container latency"
-        echo "Exemple: add_latency container-1 container-2 100ms"
+        echo "Usage: add_latency src_container dst_container (or ip address) latency"
+        echo "Example: add_latency container-1 container-2 100ms"
     fi
 
     src_container=$1
-    dst_container_ip=$(container_to_ip $2)
+    if valid_ip $2
+    then 
+      dst_ip=$2
+    else 
+      dst_ip=$(container_to_ip $2)
+    fi
     latency=$3
 
     echo "Adding $latency from $src_container to $2"
@@ -1096,24 +1119,64 @@ add_latency() {
     # Add a classful priority queue which lets us differentiate messages.
     # This queue is named 1:.
     # Three children classes, 1:1, 1:2 and 1:3, are automatically created.
-    docker exec --privileged -u0 -t $src_container tc qdisc add dev eth0 root handle 1: prio 2>&1
+    docker exec --privileged -u0 -t $src_container tc qdisc add dev eth0 root handle 1: prio
 
 
     # Add a filter to the parent queue 1: (also called 1:0). The filter has priority 1 (if we had more filters this would make a difference).
-    # For all messages with the ip of dst_container_ip as their destination, it routes them to class 1:1, which
+    # For all messages with the ip of dst_ip as their destination, it routes them to class 1:1, which
     # subsequently sends them to its only child, queue 10: (All messages need to  "end up" in a queue).
-    docker exec --privileged -u0 -t $src_container tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst $dst_container_ip flowid 1:1 2>&1
+    docker exec --privileged -u0 -t $src_container tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst $dst_ip flowid 1:1
 
     # Route the rest of the of the packets without any control.
     # Add a filter to the parent queue 1:. The filter has priority 2.
-    docker exec --privileged -u0 -t $src_container tc filter add dev eth0 protocol all parent 1: prio 2 u32 match ip dst 0.0.0.0/0 flowid 1:2 2>&1
-    docker exec --privileged -u0 -t $src_container tc filter add dev eth0 protocol all parent 1: prio 2 u32 match ip protocol 1 0xff flowid 1:2 2>&1
+    docker exec --privileged -u0 -t $src_container tc filter add dev eth0 protocol all parent 1: prio 2 u32 match ip dst 0.0.0.0/0 flowid 1:2
+    docker exec --privileged -u0 -t $src_container tc filter add dev eth0 protocol all parent 1: prio 2 u32 match ip protocol 1 0xff flowid 1:2
 
     # Add a child queue named 10: under class 1:1. All outgoing packets that will be routed to 10: will have delay applied them.
-    docker exec --privileged -u0 -t $src_container tc qdisc add dev eth0 parent 1:1 handle 10: netem delay $latency 2>&1
+    docker exec --privileged -u0 -t $src_container tc qdisc add dev eth0 parent 1:1 handle 10: netem delay $latency
 
     # Add a child queue named 20: under class 1:2
-    docker exec --privileged -u0 -t $src_container tc qdisc add dev eth0 parent 1:2 handle 20: sfq 2>&1
+    docker exec --privileged -u0 -t $src_container tc qdisc add dev eth0 parent 1:2 handle 20: sfq
+}
+
+function add_packet_corruption() {
+    if [ $# -lt 3 ]; then
+        echo "Usage: add_packet_corruption src_container dst_container (or ip address) corrupt"
+        echo "Exemple: add_packet_corruption container-1 container-2 1%"
+    fi
+
+    src_container=$1
+    if valid_ip $2
+    then 
+      dst_ip=$2
+    else 
+      dst_ip=$(container_to_ip $2)
+    fi
+    corruption=$3
+
+    echo "Adding $corruption from $src_container to $2"
+
+    # Add a classful priority queue which lets us differentiate messages.
+    # This queue is named 1:.
+    # Three children classes, 1:1, 1:2 and 1:3, are automatically created.
+    docker exec --privileged -u0 -t $src_container tc qdisc add dev eth0 root handle 1: prio 
+
+
+    # Add a filter to the parent queue 1: (also called 1:0). The filter has priority 1 (if we had more filters this would make a difference).
+    # For all messages with the ip of dst_ip as their destination, it routes them to class 1:1, which
+    # subsequently sends them to its only child, queue 10: (All messages need to  "end up" in a queue).
+    docker exec --privileged -u0 -t $src_container tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst $dst_ip flowid 1:1 
+
+    # Route the rest of the of the packets without any control.
+    # Add a filter to the parent queue 1:. The filter has priority 2.
+    docker exec --privileged -u0 -t $src_container tc filter add dev eth0 protocol all parent 1: prio 2 u32 match ip dst 0.0.0.0/0 flowid 1:2 
+    docker exec --privileged -u0 -t $src_container tc filter add dev eth0 protocol all parent 1: prio 2 u32 match ip protocol 1 0xff flowid 1:2 
+
+    # Add a child queue named 10: under class 1:1. All outgoing packets that will be routed to 10: will have corrupt applied them.
+    docker exec --privileged -u0 -t $src_container tc qdisc add dev eth0 parent 1:1 handle 10: netem corrupt $corruption
+
+    # Add a child queue named 20: under class 1:2
+    docker exec --privileged -u0 -t $src_container tc qdisc add dev eth0 parent 1:2 handle 20: sfq 
 }
 
 function get_3rdparty_file () {
