c11cb2a1754319946fe91b0138ab95576ab450b3
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-29 13:36:24 +0000

    rcca-4937
    Former-commit-id: 82a28adbc017ee17e3b7c086235cdda45d0f26c3 [formerly bbd04fb2dff82676255e54ec507e0ddc9f7f917e [formerly a5f9813d1003286678cbbe2706cc074a4ff3aba3]]
    Former-commit-id: 2f07e896ee5195a09795222d05b47c16c3786d71
    Former-commit-id: 40314ca8679c42362fa07b4fd43abf957e964b8f

---
+++ connect/connect-elasticsearch-sink/elasticsearch-repro-rcca4937.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+function wait_for_end_of_injection () {
+     MAX_WAIT=2400
+     CUR_WAIT=0
+     log "Waiting up to $MAX_WAIT seconds for IT IS OVER MICHAEL to happen"
+     docker container logs producer-v1 > /tmp/out.txt 2>&1
+     while ! grep "IT IS OVER MICHAEL" /tmp/out.txt > /dev/null;
+     do
+          sleep 10
+          docker container logs producer-v1 > /tmp/out.txt 2>&1
+          CUR_WAIT=$(( CUR_WAIT+10 ))
+          if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+               echo -e "\nERROR: The logs in all producer-v1 containers do not show 'IT IS OVER MICHAEL' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+               exit 1
+          fi
+     done
+     log "The 3M RECORDS ARE THERE!"
+}
+
+for component in producer-v1
+do
+    set +e
+    log "🏗 Building jar for ${component}"
+    docker run -i --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -e TAG=$TAG_BASE -v "${DIR}/${component}":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/${component}/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn -Dkafka.tag=$TAG -Dkafka.client.tag=$KAFKA_CLIENT_TAG package > /tmp/result.log 2>&1
+    if [ $? != 0 ]
+    then
+        logerror "ERROR: failed to build java component $component"
+        tail -500 /tmp/result.log
+        exit 1
+    fi
+    set -e
+done
+
+
+# As of version 11.0.0, the connector uses the Elasticsearch High Level REST Client (version 7.0.1),
+# which means only Elasticsearch 7.x is supported.
+
+export ELASTIC_VERSION="6.8.3"
+if version_gt $CONNECTOR_TAG "10.9.9"
+then
+    log "Connector version is > 11.0.0, using Elasticsearch 7.x"
+    export ELASTIC_VERSION="7.12.0"
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-rcca-4937.yml"
+
+log "Run the Java producer-v1"
+docker exec -d producer-v1 bash -c "java -jar producer-v1-1.0.0-jar-with-dependencies.jar"
+
+wait_for_end_of_injection
+
+
+log "Creating Elasticsearch Sink connector (Elasticsearch version is $ELASTIC_VERSION"
+if version_gt $CONNECTOR_TAG "10.9.9"
+then
+     # 7.x
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+               "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+               "tasks.max": "1",
+               "topics": "test-elasticsearch-sink",
+               "key.ignore": "true",
+               "connection.url": "http://elasticsearch:9200"
+               }' \
+          http://localhost:8083/connectors/elasticsearch-sink/config | jq .
+else
+     # 6.x
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+               "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+               "tasks.max": "1",
+               "topics": "test-elasticsearch-sink",
+               "key.ignore": "true",
+               "connection.url": "http://elasticsearch:9200",
+               "type.name": "kafka-connect"
+               }' \
+          http://localhost:8083/connectors/elasticsearch-sink/config | jq .
+fi
+
+
+# log "Sending messages to topic test-elasticsearch-sink"
+# seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test-elasticsearch-sink --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+sleep 10
+
+log "Check that the data is available in Elasticsearch"
+curl -XGET 'http://localhost:9200/test-elasticsearch-sink/_search?pretty' > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "f1" /tmp/result.log | grep "value1"
+grep "f1" /tmp/result.log | grep "value10"
+
+
