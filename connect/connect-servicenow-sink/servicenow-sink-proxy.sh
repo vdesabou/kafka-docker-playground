1205e554d5c07c3828a73ed60310829c7b771b03
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-11-29 12:05:59 +0100

    Add service now sink example with PROXY #1722
    Former-commit-id: 96af66e5bdaa22d82272c20066c44490d4bcb768 [formerly 201aac9b2d764941d1f7ab25c1356dfa8b867216 [formerly d423b0168dbe9c49e6a3890146d906c5fa072643]]
    Former-commit-id: 58d2e23d7768e0966f405d9020ecbd98c0bde23d
    Former-commit-id: 23b004dbbf0700e9574543eeb90bc16262537874

---
+++ connect/connect-servicenow-sink/servicenow-sink-proxy.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -z "$CI" ]
+then
+     # running with github actions
+     if [ ! -f ../../secrets.properties ]
+     then
+          logerror "../../secrets.properties is not present!"
+          exit 1
+     fi
+     source ../../secrets.properties > /dev/null 2>&1
+fi
+
+function wait_for_end_of_hibernation () {
+     MAX_WAIT=600
+     CUR_WAIT=0
+     log "Waiting up to $MAX_WAIT seconds for end of hibernation to happen (it can take several minutes)"
+     curl -X POST "${SERVICENOW_URL}/api/now/table/incident" --user admin:"$SERVICENOW_PASSWORD" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d '{"short_description": "This is test"}' > /tmp/out.txt 2>&1
+     while [[ $(cat /tmp/out.txt) =~ "Sign in to the site to wake your instance" ]]
+     do
+          sleep 10
+          curl -X POST "${SERVICENOW_URL}/api/now/table/incident" --user admin:"$SERVICENOW_PASSWORD" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d '{"short_description": "This is test"}' > /tmp/out.txt 2>&1
+          CUR_WAIT=$(( CUR_WAIT+10 ))
+          if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+               echo -e "\nERROR: The logs still show 'Sign in to the site to wake your instance' after $MAX_WAIT seconds.\n"
+               exit 1
+          fi
+     done
+     log "The instance is ready !"
+}
+
+SERVICENOW_URL=${SERVICENOW_URL:-$1}
+SERVICENOW_PASSWORD=${SERVICENOW_PASSWORD:-$2}
+
+if [ -z "$SERVICENOW_URL" ]
+then
+     logerror "SERVICENOW_URL is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [[ "$SERVICENOW_URL" != */ ]]
+then
+    logerror "SERVICENOW_URL does not end with "/" Example: https://dev12345.service-now.com/ "
+    exit 1
+fi
+
+if [ -z "$SERVICENOW_PASSWORD" ]
+then
+     logerror "SERVICENOW_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ ! -z "$CI" ]
+then
+     # this is github actions
+     set +e
+     log "Waking up servicenow instance..."
+     docker run -e USERNAME="$SERVICENOW_DEVELOPER_USERNAME" -e PASSWORD="$SERVICENOW_DEVELOPER_PASSWORD" ruthless/servicenow-instance-wakeup:latest
+     set -e
+     wait_for_end_of_hibernation
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.proxy.yml"
+
+log "Sending messages to topic test_table"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_table --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"u_name","type":"string"},
+{"name":"u_price", "type": "float"}, {"name":"u_quantity", "type": "int"}]}' << EOF
+{"u_name": "scissors", "u_price": 2.75, "u_quantity": 3}
+{"u_name": "tape", "u_price": 0.99, "u_quantity": 10}
+{"u_name": "notebooks", "u_price": 1.99, "u_quantity": 5}
+EOF
+
+log "Creating ServiceNow Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+                    "connector.class": "io.confluent.connect.servicenow.ServiceNowSinkConnector",
+                    "topics": "test_table",
+                    "proxy.url": "nginx-proxy:8888",
+                    "servicenow.url": "'"$SERVICENOW_URL"'",
+                    "tasks.max": "1",
+                    "servicenow.table": "u_test_table",
+                    "servicenow.user": "admin",
+                    "servicenow.password": "'"$SERVICENOW_PASSWORD"'",
+                    "key.converter": "io.confluent.connect.avro.AvroConverter",
+                    "key.converter.schema.registry.url": "http://schema-registry:8081",
+                    "value.converter": "io.confluent.connect.avro.AvroConverter",
+                    "value.converter.schema.registry.url": "http://schema-registry:8081",
+                    "reporter.bootstrap.servers": "broker:9092",
+                    "reporter.error.topic.name": "test-error",
+                    "reporter.error.topic.replication.factor": 1,
+                    "reporter.error.topic.key.format": "string",
+                    "reporter.error.topic.value.format": "string",
+                    "reporter.result.topic.name": "test-result",
+                    "reporter.result.topic.key.format": "string",
+                    "reporter.result.topic.value.format": "string",
+                    "reporter.result.topic.replication.factor": 1,
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/servicenow-sink-proxy/config | jq .
+
+
+sleep 15
+
+log "Confirm that the messages were delivered to the ServiceNow table"
+docker exec -e SERVICENOW_URL="$SERVICENOW_URL" -e SERVICENOW_PASSWORD="$SERVICENOW_PASSWORD" connect bash -c "export HTTP_PROXY=nginx-proxy:8888 && export HTTPS_PROXY=nginx-proxy:8888 && \
+   curl -X GET \
+    \"${SERVICENOW_URL}/api/now/table/u_test_table\" \
+    --user admin:\"$SERVICENOW_PASSWORD\" \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -H 'cache-control: no-cache'" | jq . > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "u_name" /tmp/result.log | grep "notebooks"
