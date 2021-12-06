689d125c51b76cfdcd0fdd282a2e04b669371b57
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-12-06 10:00:21 +0100

    Add repro also with source
    Former-commit-id: 290550154b99158babd3953fdd7f87b6b246de6a [formerly 28265765c86579831c9c3e970146fa62b51fe72d [formerly 999e34344a2981685c6d2f1a449cd532bbae295f]]
    Former-commit-id: f47d7918b0c5ea8b7b0f99fc77d9ecfff1ad7b32
    Former-commit-id: 3d14edc35856365d9fb2539b2a4417eac5da41b7

---
+++ connect/connect-salesforce-bulkapi-source/salesforce-bulkapi-source-repro-rcca-4977.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+function wait_for_repro () {
+     MAX_WAIT=600
+     CUR_WAIT=0
+     log "Waiting up to $MAX_WAIT seconds for error invalid session ID exception to happen"
+     docker container logs connect > /tmp/out.txt 2>&1
+     while ! grep "invalid session ID exception" /tmp/out.txt > /dev/null;
+     do
+          sleep 10
+          docker container logs connect > /tmp/out.txt 2>&1
+          CUR_WAIT=$(( CUR_WAIT+10 ))
+          if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+               echo -e "\nERROR: The logs in all connect containers do not show 'invalid session ID exception' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+               exit 1
+          fi
+     done
+     log "The problem has been reproduced !"
+}
+
+if [ ! -z "$CI" ]
+then
+     # running with github actions
+     if [ ! -f ../../secrets.properties ]
+     then
+          logerror "../../secrets.properties is not present!"
+          exit 1
+     fi
+     source ../../secrets.properties > /dev/null 2>&1
+fi
+
+SALESFORCE_USERNAME=${SALESFORCE_USERNAME:-$1}
+SALESFORCE_PASSWORD=${SALESFORCE_PASSWORD:-$2}
+CONSUMER_KEY=${CONSUMER_KEY:-$3}
+CONSUMER_PASSWORD=${CONSUMER_PASSWORD:-$4}
+SECURITY_TOKEN=${SECURITY_TOKEN:-$5}
+SALESFORCE_INSTANCE=${SALESFORCE_INSTANCE:-"https://login.salesforce.com"}
+
+if [ -z "$SALESFORCE_USERNAME" ]
+then
+     logerror "SALESFORCE_USERNAME is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_PASSWORD" ]
+then
+     logerror "SALESFORCE_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SECURITY_TOKEN" ]
+then
+     logerror "SECURITY_TOKEN is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext-repro-rcca-4977.yml"
+
+log "Login with sfdx CLI"
+docker exec sfdx-cli sh -c "sfdx sfpowerkit:auth:login -u \"$SALESFORCE_USERNAME\" -p \"$SALESFORCE_PASSWORD\" -r \"$SALESFORCE_INSTANCE\" -s \"$SECURITY_TOKEN\""
+
+LEAD_FIRSTNAME=John_$RANDOM
+LEAD_LASTNAME=Doe_$RANDOM
+log "Add a Lead to Salesforce: $LEAD_FIRSTNAME $LEAD_LASTNAME"
+docker exec sfdx-cli sh -c "sfdx force:data:record:create  -u \"$SALESFORCE_USERNAME\" -s Lead -v \"FirstName='$LEAD_FIRSTNAME' LastName='$LEAD_LASTNAME' Company=Confluent\""
+
+log "Creating Salesforce Bulk API Source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+                    "connector.class": "io.confluent.connect.salesforce.SalesforceBulkApiSourceConnector",
+                    "kafka.topic": "sfdc-bulkapi-leads",
+                    "tasks.max": "1",
+                    "curl.logging": "true",
+                    "salesforce.object" : "Lead",
+                    "salesforce.instance" : "'"$SALESFORCE_INSTANCE"'",
+                    "salesforce.username" : "'"$SALESFORCE_USERNAME"'",
+                    "salesforce.password" : "'"$SALESFORCE_PASSWORD"'",
+                    "salesforce.password.token" : "'"$SECURITY_TOKEN"'",
+                    "connection.max.message.size": "10048576",
+                    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/salesforce-bulkapi-source/config | jq .
+
+
+
+sleep 10
+
+log "Verify we have received the data in sfdc-bulkapi-leads topic"
+timeout 60 docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic sfdc-bulkapi-leads --from-beginning --max-messages 1
+
+
+log "Creating Salesforce Bulk API Source connector 2"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+                    "connector.class": "io.confluent.connect.salesforce.SalesforceBulkApiSourceConnector",
+                    "kafka.topic": "sfdc-bulkapi-leads",
+                    "tasks.max": "1",
+                    "curl.logging": "true",
+                    "salesforce.object" : "Lead",
+                    "salesforce.instance" : "'"$SALESFORCE_INSTANCE"'",
+                    "salesforce.username" : "'"$SALESFORCE_USERNAME"'",
+                    "salesforce.password" : "'"$SALESFORCE_PASSWORD"'",
+                    "salesforce.password.token" : "'"$SECURITY_TOKEN"'",
+                    "connection.max.message.size": "10048576",
+                    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/salesforce-bulkapi-source2/config | jq .
+
+log "Add another lead"
+docker exec sfdx-cli sh -c "sfdx sfpowerkit:auth:login -u \"$SALESFORCE_USERNAME\" -p \"$SALESFORCE_PASSWORD\" -r \"$SALESFORCE_INSTANCE\" -s \"$SECURITY_TOKEN\""
+
+LEAD_FIRSTNAME=John_$RANDOM
+LEAD_LASTNAME=Doe_$RANDOM
+log "Add a Lead to Salesforce: $LEAD_FIRSTNAME $LEAD_LASTNAME"
+docker exec sfdx-cli sh -c "sfdx force:data:record:create  -u \"$SALESFORCE_USERNAME\" -s Lead -v \"FirstName='$LEAD_FIRSTNAME' LastName='$LEAD_LASTNAME' Company=Confluent\"" 
+
+
+wait_for_repro
