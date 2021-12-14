5006450d59e19557eb84a404167f2bee4358b84d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-12-14 18:51:37 +0100

    working i think

---
+++ other/ldap-authorizer-with-ldap-failover/bind/confluent.io
@@ -0,0 +1,20 @@
+$TTL    604800
+@       IN      SOA     bind.confluent.io. root.confluent.io. (
+                  3       ; Serial
+             604800     ; Refresh
+              86400     ; Retry
+            2419200     ; Expire
+             604800 )   ; Negative Cache TTL
+;
+; name servers - NS records
+     IN      NS      bind.confluent.io.
+
+; name servers - A records
+bind.confluent.io.          IN      A      172.28.1.1
+ldap.confluent.io.          IN      A      172.28.1.2
+ldap2.confluent.io.         IN      A      172.28.1.7
+ldap3.confluent.io.         IN      A      172.28.1.8
+
+_ldap._tcp.confluent.io. IN SRV 10 50 389 ldap.confluent.io.
+_ldap._tcp.confluent.io. IN SRV 10 50 389 ldap2.confluent.io.
+_ldap._tcp.confluent.io. IN SRV 20 75 389 ldap3.confluent.io.
--- other/ldap-authorizer-with-ldap-failover/start.sh
+++ other/ldap-authorizer-with-ldap-failover/start.sh
@@ -8,18 +8,18 @@ ${DIR}/../../environment/ldap-authorizer-sasl-plain/start.sh "${PWD}/docker-comp
 
 # docker exec --privileged --user root -i broker yum install bind-utils
 
-# nslookup -type=SRV _ldap._tcp.example.com
+# nslookup -type=SRV _ldap._tcp.confluent.io
 # Server:         127.0.0.11
 # Address:        127.0.0.11#53
 
-# _ldap._tcp.example.com  service = 10 50 389 ldap2.example.com.
-# _ldap._tcp.example.com  service = 10 50 389 ldap.example.com.
-# _ldap._tcp.example.com  service = 20 75 389 ldap3.example.com.
+# _ldap._tcp.confluent.io  service = 10 50 389 ldap2.confluent.io.
+# _ldap._tcp.confluent.io  service = 10 50 389 ldap.confluent.io.
+# _ldap._tcp.confluent.io  service = 20 75 389 ldap3.confluent.io.
 
 
-# [appuser@broker ~]$ dig SRV _ldap._tcp.example.com
+# [appuser@broker ~]$ dig SRV _ldap._tcp.confluent.io
 
-# ; <<>> DiG 9.11.26-RedHat-9.11.26-6.el8 <<>> SRV _ldap._tcp.example.com
+# ; <<>> DiG 9.11.26-RedHat-9.11.26-6.el8 <<>> SRV _ldap._tcp.confluent.io
 # ;; global options: +cmd
 # ;; Got answer:
 # ;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 35223
@@ -29,21 +29,21 @@ ${DIR}/../../environment/ldap-authorizer-sasl-plain/start.sh "${PWD}/docker-comp
 # ; EDNS: version: 0, flags:; udp: 4096
 # ; COOKIE: 6f8a2a347b2c1d044138bce361b8d3d6af6ba5f0aec87fc3 (good)
 # ;; QUESTION SECTION:
-# ;_ldap._tcp.example.com.                IN      SRV
+# ;_ldap._tcp.confluent.io.                IN      SRV
 
 # ;; ANSWER SECTION:
-# _ldap._tcp.example.com. 604800  IN      SRV     10 50 389 ldap.example.com.
-# _ldap._tcp.example.com. 604800  IN      SRV     20 75 389 ldap3.example.com.
-# _ldap._tcp.example.com. 604800  IN      SRV     10 50 389 ldap2.example.com.
+# _ldap._tcp.confluent.io. 604800  IN      SRV     10 50 389 ldap.confluent.io.
+# _ldap._tcp.confluent.io. 604800  IN      SRV     20 75 389 ldap3.confluent.io.
+# _ldap._tcp.confluent.io. 604800  IN      SRV     10 50 389 ldap2.confluent.io.
 
 # ;; AUTHORITY SECTION:
-# example.com.            604800  IN      NS      bind.example.com.
+# confluent.io.            604800  IN      NS      bind.confluent.io.
 
 # ;; ADDITIONAL SECTION:
-# ldap.example.com.       604800  IN      A       172.28.1.2
-# ldap2.example.com.      604800  IN      A       172.28.1.7
-# ldap3.example.com.      604800  IN      A       172.28.1.8
-# bind.example.com.       604800  IN      A       172.28.1.1
+# ldap.confluent.io.       604800  IN      A       172.28.1.2
+# ldap2.confluent.io.      604800  IN      A       172.28.1.7
+# ldap3.confluent.io.      604800  IN      A       172.28.1.8
+# bind.confluent.io.       604800  IN      A       172.28.1.1
 
 # ;; Query time: 3 msec
 # ;; SERVER: 127.0.0.11#53(127.0.0.11)
@@ -68,7 +68,7 @@ ${DIR}/../../environment/ldap-authorizer-sasl-plain/start.sh "${PWD}/docker-comp
 #         at kafka.server.KafkaServer.startup(KafkaServer.scala:553)
 #         at kafka.Kafka$.main(Kafka.scala:108)
 #         at kafka.Kafka.main(Kafka.scala)
-# Caused by: javax.naming.CommunicationException: example.com:389 [Root exception is java.net.UnknownHostException: example.com]
+# Caused by: javax.naming.CommunicationException: confluent.io:389 [Root exception is java.net.UnknownHostException: confluent.io]
 #         at java.naming/com.sun.jndi.ldap.Connection.<init>(Connection.java:252)
 #         at java.naming/com.sun.jndi.ldap.LdapClient.<init>(LdapClient.java:137)
 #         at java.naming/com.sun.jndi.ldap.LdapClient.getInstance(LdapClient.java:1616)
@@ -85,4 +85,4 @@ ${DIR}/../../environment/ldap-authorizer-sasl-plain/start.sh "${PWD}/docker-comp
 #         at java.naming/javax.naming.ldap.InitialLdapContext.<init>(InitialLdapContext.java:154)
 #         at io.confluent.security.auth.provider.ldap.LdapContextCreator.lambda$createLdapContext$0(LdapContextCreator.java:80)
 #         ... 15 more
-# Caused by: java.net.UnknownHostException: example.com
+# Caused by: java.net.UnknownHostException: confluent.io
