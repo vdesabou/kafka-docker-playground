25b08f058ab9abf99a39063a5f7959fcffeaee6b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-12-14 18:27:18 +0100

    dig results
    Former-commit-id: 0401c9a9d1336a64aa27447d781acd240b536f06 [formerly 3768d81b2dc62487ae5446e886a78bc70685cb91 [formerly cd0b5b6f22fb67bf3c9e138741c71fa48b98f6e6]]
    Former-commit-id: d0b12d1af32fd6fbcad0e7ccb9425857aa7799bf
    Former-commit-id: 16096803fa14e86ead26f84b0694664c438e9a66

--- other/ldap-authorizer-with-ldap-failover/start.sh
+++ other/ldap-authorizer-with-ldap-failover/start.sh
@@ -17,6 +17,39 @@ ${DIR}/../../environment/ldap-authorizer-sasl-plain/start.sh "${PWD}/docker-comp
 # _ldap._tcp.example.com  service = 20 75 389 ldap3.example.com.
 
 
+# [appuser@broker ~]$ dig SRV _ldap._tcp.example.com
+
+# ; <<>> DiG 9.11.26-RedHat-9.11.26-6.el8 <<>> SRV _ldap._tcp.example.com
+# ;; global options: +cmd
+# ;; Got answer:
+# ;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 35223
+# ;; flags: qr aa rd ra; QUERY: 1, ANSWER: 3, AUTHORITY: 1, ADDITIONAL: 5
+
+# ;; OPT PSEUDOSECTION:
+# ; EDNS: version: 0, flags:; udp: 4096
+# ; COOKIE: 6f8a2a347b2c1d044138bce361b8d3d6af6ba5f0aec87fc3 (good)
+# ;; QUESTION SECTION:
+# ;_ldap._tcp.example.com.                IN      SRV
+
+# ;; ANSWER SECTION:
+# _ldap._tcp.example.com. 604800  IN      SRV     10 50 389 ldap.example.com.
+# _ldap._tcp.example.com. 604800  IN      SRV     20 75 389 ldap3.example.com.
+# _ldap._tcp.example.com. 604800  IN      SRV     10 50 389 ldap2.example.com.
+
+# ;; AUTHORITY SECTION:
+# example.com.            604800  IN      NS      bind.example.com.
+
+# ;; ADDITIONAL SECTION:
+# ldap.example.com.       604800  IN      A       172.28.1.2
+# ldap2.example.com.      604800  IN      A       172.28.1.7
+# ldap3.example.com.      604800  IN      A       172.28.1.8
+# bind.example.com.       604800  IN      A       172.28.1.1
+
+# ;; Query time: 3 msec
+# ;; SERVER: 127.0.0.11#53(127.0.0.11)
+# ;; WHEN: Tue Dec 14 17:26:46 UTC 2021
+# ;; MSG SIZE  rcvd: 272
+
 # [2021-12-14 17:20:39,198] ERROR LDAP search failed, search will be retried. Groups from the last successful search will continue to be applied until the configured retry timeout or the next successful search. (io.confluent.security.auth.provider.ldap.LdapGroupManager)
 # io.confluent.security.auth.provider.ldap.LdapException: LDAP context could not be created with provided configs
 #         at io.confluent.security.auth.provider.ldap.LdapContextCreator.lambda$createLdapContext$0(LdapContextCreator.java:82)
