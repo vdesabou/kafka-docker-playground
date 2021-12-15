27cb78a2f9916c9423d7114350915bec59d3f0ae
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-12-15 17:33:26 +0100

    DEVX-2701
    Former-commit-id: 5c91562d154b6142d9bb53c86e4fbe961bb71939 [formerly 50f800d9be695b4670275270ceef9eaabe9e3270 [formerly 6c097748b6fa9351c5cd7f3f8fd7f079a456345d]]
    Former-commit-id: 7eeb93ce665c4c1bdf6aa2e02ea8e7391ef030a8
    Former-commit-id: 90efac68dd33219399e1d20d09f1bc2029fe72f1

---
+++ troubleshooting/control-center-topic-consumption-not-showing-up-devx-2701/README.md
@@ -0,0 +1,27 @@
+# Testing a potential workaround for RBAC/LDAP case sensitivity issue
+
+
+
+## How to run
+
+Simply run:
+
+```
+$ ./start-plaintext.sh
+```
+
+or
+
+```
+$ ./start-rbac.sh
+```
+
+## With PLAINTEXT
+
+![Plaintext](plaintext.jpg)
+
+## With RBAC
+
+`superUser/superUser`
+
+![RBAC](rbac.jpg)
---
+++ troubleshooting/control-center-topic-consumption-not-showing-up-devx-2701/start-rbac.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+
+${DIR}/../../environment/rbac-sasl-plain/start.sh "${PWD}/docker-compose.rbac-sasl-plain.yml"
+
+log "Create topic rbac_orders"
+curl -s -X PUT \
+      -H "Content-Type: application/json" \
+      -u connectorSubmitter:connectorSubmitter \
+      --data '{
+                "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
+                "kafka.topic": "rbac_orders",
+                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                "value.converter.schemas.enable": "false",
+                "max.interval": 500,
+                "iterations": "-1",
+                "tasks.max": "1",
+                "schema.filename" : "/tmp/schemas/orders.avro",
+                "schema.keyfield" : "orderid",
+                "producer.override.sasl.jaas.config": "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required username=\"connectorSA\" password=\"connectorSA\" metadataServerUrls=\"http://broker:8091\";"
+            }' \
+      http://localhost:8083/connectors/gcs-rbac-sink/config | jq .
+
+
+
+log "Checking messages from topic rbac_orders with MonitoringConsumerInterceptor in background"
+docker exec -i connect bash -c 'export CLASSPATH=/usr/share/java/monitoring-interceptors/*; kafka-console-consumer --bootstrap-server broker:9092 --topic rbac_orders --consumer.config /etc/kafka/secrets/client_sasl_plain.config --consumer-property interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor --consumer-property confluent.monitoring.interceptor.security.protocol=SASL_PLAINTEXT --consumer-property confluent.monitoring.interceptor.sasl.mechanism=PLAIN --consumer-property confluent.monitoring.interceptor.sasl.jaas.config="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"admin\" password=\"admin-secret\";" --consumer-property group.id=myapp --from-beginning > /dev/null 2>&1 &'
