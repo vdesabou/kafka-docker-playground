f59648bab9097194ed485fa485b5f5140f7c6332
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-12-15 19:43:00 +0100

    validate
    Former-commit-id: 6e837508361b8103fefc99ea74b16d2c49de6c34 [formerly 3e39e422e49b29d86cc684bd8e6e94ef336a2e0b [formerly 78b1119ffe3007dd0d3cc033ec215c00edc64c20]]
    Former-commit-id: 5d62fcd610c869b541bc2f2d50ab7e6395eb3207
    Former-commit-id: 4dced51a3d7b4d783e2cf3c791e9083c051c3936

--- environment/rbac-sasl-plain/scripts/helper/validate_bindings.sh
+++ environment/rbac-sasl-plain/scripts/helper/validate_bindings.sh
@@ -2,10 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-source ${DIR}/../../../../scripts/utils.sh
+. ${DIR}/functions.sh
 
 ################################## GET KAFKA CLUSTER ID ########################
-KAFKA_CLUSTER_ID=$(host_check_kafka_cluster_registered)
+KAFKA_CLUSTER_ID=$(get_kafka_cluster_id_from_container)
 echo "KAFKA_CLUSTER_ID: $KAFKA_CLUSTER_ID"
 
 ################################## SETUP VARIABLES #############################
@@ -28,13 +28,16 @@ KSQLDB_SERVER="User:controlCenterAndKsqlDBServer"
 C3_ADMIN="User:controlcenterAdmin"
 CLIENT_AVRO_PRINCIPAL="User:clientAvroCli"
 
+mds_login $MDS_URL ${SUPER_USER} ${SUPER_USER_PASSWORD} || exit 1
+
+
 ################################## Run through permutations #############################
 
 for p in $SUPER_USER_PRINCIPAL $CONNECT_ADMIN $CONNECTOR_SUBMITTER $CONNECTOR_PRINCIPAL $SR_PRINCIPAL $KSQLDB_ADMIN $KSQLDB_USER $KSQLDB_SERVER $C3_ADMIN $CLIENT_AVRO_PRINCIPAL; do
   for c in " " " --schema-registry-cluster-id $SR" " --connect-cluster-id $CONNECT" " --ksql-cluster-id $KSQLDB"; do
     echo
     echo "Showing bindings for principal $p and --kafka-cluster-id $KAFKA_CLUSTER_ID $c"
-    docker-compose -f ${DIR}/../../../../environment/plaintext/docker-compose.yml -f ${DIR}/../../../../environment/rbac-sasl-plain/docker-compose.yml exec tools confluent iam rolebinding list --principal $p --kafka-cluster-id $KAFKA_CLUSTER_ID $c
+    confluent iam rolebinding list --principal $p --kafka-cluster-id $KAFKA_CLUSTER_ID $c
     echo
   done
 done
