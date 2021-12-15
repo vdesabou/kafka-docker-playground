6c097748b6fa9351c5cd7f3f8fd7f079a456345d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-12-15 17:33:26 +0100

    DEVX-2701

---
+++ troubleshooting/control-center-topic-consumption-not-showing-up-devx-2701/start-plaintext.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Create topic orders"
+curl -s -X PUT \
+      -H "Content-Type: application/json" \
+      --data '{
+                "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
+                "kafka.topic": "orders",
+                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                "value.converter.schemas.enable": "false",
+                "max.interval": 500,
+                "iterations": "-1",
+                "tasks.max": "1",
+                "schema.filename" : "/tmp/schemas/orders.avro",
+                "schema.keyfield" : "orderid"
+            }' \
+      http://localhost:8083/connectors/datagen/config | jq .
+
+
+
+log "Checking messages from topic orders with MonitoringConsumerInterceptor in background"
+docker exec -i connect bash -c 'export CLASSPATH=/usr/share/java/monitoring-interceptors/*; kafka-console-consumer --bootstrap-server broker:9092 --topic orders --consumer-property interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor --consumer-property group.id=myapp --from-beginning > /dev/null 2>&1 &'
