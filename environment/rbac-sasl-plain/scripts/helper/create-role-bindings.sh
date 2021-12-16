498b76dab06e030f6cbd8b354afcc7520fb3e85d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-12-16 09:37:05 +0100

    renaming gcs-rbac-sink to my-rbac-connector

--- environment/rbac-sasl-plain/scripts/helper/create-role-bindings.sh
+++ environment/rbac-sasl-plain/scripts/helper/create-role-bindings.sh
@@ -121,14 +121,14 @@ echo "Creating role bindings for GCP GCS sink connector"
 confluent iam rolebinding create \
     --principal $CONNECTOR_SUBMITTER \
     --role ResourceOwner \
-    --resource Connector:gcs-rbac-sink \
+    --resource Connector:my-rbac-connector \
     --kafka-cluster-id $KAFKA_CLUSTER_ID \
     --connect-cluster-id $CONNECT
 
 confluent iam rolebinding create \
     --principal $CONNECTOR_PRINCIPAL \
     --role ResourceOwner \
-    --resource Group:connect-gcs-rbac-sink \
+    --resource Group:connect-my-rbac-connector \
     --kafka-cluster-id $KAFKA_CLUSTER_ID \
 
 confluent iam rolebinding create \
