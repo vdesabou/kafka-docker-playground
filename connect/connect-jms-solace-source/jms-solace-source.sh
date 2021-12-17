24b6b974ceb110b8e9b74798c20fa6b8fd4ca554
Akhilesh Dubey <adubey@confluent.io>
2021-12-17 15:51:46 +0000

    connect jms source

---
+++ connect/connect-jms-solace-source/jms-solace-source.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f ${DIR}/sol-jms-10.6.4.jar ]
+then
+     log "Downloading sol-jms-10.6.4.jar"
+     wget https://repo1.maven.org/maven2/com/solacesystems/sol-jms/10.6.4/sol-jms-10.6.4.jar
+fi
+
+if [ ! -f ${DIR}/commons-lang-2.6.jar ]
+then
+     log "Downloading commons-lang-2.6.jar"
+     wget https://repo1.maven.org/maven2/commons-lang/commons-lang/2.6/commons-lang-2.6.jar
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Wait 60 seconds for Solace to be up and running"
+sleep 60
+log "Solace UI is accessible at http://127.0.0.1:8080 (admin/admin)"
+
+log "Create the queue connector-quickstart in the default Message VPN using CLI"
+docker exec solace bash -c "/usr/sw/loads/currentload/bin/cli -A -s cliscripts/create_queue_cmd"
+
+log "Publish messages to the Solace queue using the REST endpoint"
+
+for i in 1000 1001 1002
+do
+     curl -X POST -d "m1" http://localhost:9000/Queue/connector-quickstart -H "Content-Type: text/plain" -H "Solace-Message-ID: $i"
+done
+
+log "Creating Solace source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.jms.JmsSourceConnector",
+                    "tasks.max": "1",
+                    "kafka.topic": "source-messages",
+                    "java.naming.factory.initial": "com.solacesystems.jndi.SolJNDIInitialContextFactory",
+                    "java.naming.provider.url": "smf://solace:55555",
+                    "java.naming.security.principal": "admin",
+                    "java.naming.security.credentials": "admin",
+                    "connection.factory.name": "/jms/cf/default",
+                    "Solace_JMS_VPN": "default",
+                    "jms.destination.type": "queue",
+                    "jms.destination.name": "connector-quickstart",
+                    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                    "value.converter": "org.apache.kafka.connect.storage.StringConverter",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/jms-solace-source/config | jq .
+
+sleep 10
+
+log "Verifying topic source-messages"
+timeout 60 docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic source-messages --from-beginning --max-messages 5
