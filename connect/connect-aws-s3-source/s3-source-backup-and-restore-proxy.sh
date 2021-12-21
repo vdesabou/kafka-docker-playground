e2188bad1b74230fbfd14a663e8a9ba07d53f3e6
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-12-21 12:34:18 +0100

    storage source connectors >= 2.0.0 do not support CP versions < 6.0.0 #2031

--- connect/connect-aws-s3-source/s3-source-backup-and-restore-proxy.sh
+++ connect/connect-aws-s3-source/s3-source-backup-and-restore-proxy.sh
@@ -4,6 +4,12 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+if ! version_gt $TAG_BASE "5.9.99" && version_gt $CONNECTOR_TAG "1.9.9"
+then
+    logwarn "WARN: connector version >= 2.0.0 do not support CP versions < 6.0.0"
+    exit 111
+fi
+
 if [ ! -f $HOME/.aws/config ]
 then
      logerror "ERROR: $HOME/.aws/config is not set"
--- connect/connect-aws-s3-source/s3-source-generalized.sh
+++ connect/connect-aws-s3-source/s3-source-generalized.sh
@@ -10,6 +10,12 @@ if ! version_gt $CONNECTOR_TAG "1.9.9"; then
     exit 111
 fi
 
+if ! version_gt $TAG_BASE "5.9.99" && version_gt $CONNECTOR_TAG "1.9.9"
+then
+    logwarn "WARN: connector version >= 2.0.0 do not support CP versions < 6.0.0"
+    exit 111
+fi
+
 if [ ! -f $HOME/.aws/config ]
 then
      logerror "ERROR: $HOME/.aws/config is not set"
