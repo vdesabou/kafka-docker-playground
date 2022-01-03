111b0cfd94bf356a8e0a6d40981593425eb0bf39
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-03 15:03:19 +0100

    Add Kraft environment #2049

---
+++ environment/kraft-plaintext/start.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if ! version_gt $TAG_BASE "6.1.99"; then
+    logwarn "WARN: KRaft (KIP-500) is available since CP 6.2 only"
+    exit 111
+fi
+
+verify_docker_and_memory
+verify_installed "docker-compose"
+check_docker_compose_version
+
+# https://docs.docker.com/compose/profiles/
+profile_control_center_command=""
+if [ -z "$DISABLE_CONTROL_CENTER" ]
+then
+  profile_control_center_command="--profile control-center"
+else
+  log "🛑 control-center is disabled"
+fi
+
+profile_ksqldb_command=""
+if [ -z "$DISABLE_KSQLDB" ]
+then
+  profile_ksqldb_command="--profile ksqldb"
+else
+  log "🛑 ksqldb is disabled"
+fi
+
+ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE=""
+DOCKER_COMPOSE_FILE_OVERRIDE=$1
+if [ -f "${DOCKER_COMPOSE_FILE_OVERRIDE}" ]
+then
+  ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE="-f ${DOCKER_COMPOSE_FILE_OVERRIDE}"
+fi
+
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/kraft-plaintext/docker-compose.yml ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} build
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/kraft-plaintext/docker-compose.yml ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} down -v --remove-orphans
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/kraft-plaintext/docker-compose.yml ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} ${profile_control_center_command} ${profile_ksqldb_command} up -d
+log "📝 To see the actual properties file, use ../../scripts/get-properties.sh <container>"
+command="source ../../scripts/utils.sh && docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/kraft-plaintext/docker-compose.yml ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} ${profile_control_center_command} ${profile_ksqldb_command} up -d"
+echo "$command" > /tmp/playground-command
+log "✨ If you modify a docker-compose file and want to re-create the container(s), run ../../scripts/recreate-containers.sh or use this command:"
+log "✨ $command"
+
+if [ "$#" -ne 0 ]
+then
+    shift
+fi
+../../scripts/wait-for-connect-and-controlcenter.sh $@
+
+display_jmx_info
