73cae3004d77e883216507236b0b4880476660d4
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-04 12:38:11 +0100

    Added to CI
    Former-commit-id: 298430eb77aefe354120ba7bcaa5d030d95c25bc [formerly 89cb21e26d0fab6fce3638809c46a8455d20b3e3 [formerly 6f5cfd2e27a82285d0c947658ec535f848c26892]]
    Former-commit-id: 3634cf50531f32ba099f6da00e7b7a0bd8975bb6
    Former-commit-id: 284231ed9463a91db98d7b8f42a39d1da761a680

---
+++ ccloud/connect-mqtt-source/mqtt-source.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
+then
+     export CONNECT_CONTAINER_HOME_DIR="/home/appuser"
+else
+     export CONNECT_CONTAINER_HOME_DIR="/root"
+fi
+
+${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose.yml"
+
+if [ -f /tmp/delta_configs/env.delta ]
+then
+     source /tmp/delta_configs/env.delta
+else
+     logerror "ERROR: /tmp/delta_configs/env.delta has not been generated"
+     exit 1
+fi
+#############
+
+if ! version_gt $TAG_BASE "5.9.9"; then
+     # note: for 6.x CONNECT_TOPIC_CREATION_ENABLE=true
+     log "Creating topic in Confluent Cloud (auto.create.topics.enable=false)"
+     set +e
+     create_topic mqtt-source-1
+     set -e
+fi
+
+log "Creating MQTT Source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.mqtt.MqttSourceConnector",
+               "tasks.max": "1",
+               "mqtt.server.uri": "tcp://mosquitto:1883",
+               "mqtt.topics":"my-mqtt-topic",
+               "kafka.topic":"mqtt-source-1",
+               "mqtt.qos": "2",
+               "mqtt.username": "myuser",
+               "mqtt.password": "mypassword",
+               "topic.creation.default.replication.factor": "-1",
+               "topic.creation.default.partitions": "-1",
+               "confluent.topic.ssl.endpoint.identification.algorithm" : "https",
+               "confluent.topic.sasl.mechanism" : "PLAIN",
+               "confluent.topic.bootstrap.servers": "${file:/data:bootstrap.servers}",
+               "confluent.topic.sasl.jaas.config" : "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${file:/data:sasl.username}\" password=\"${file:/data:sasl.password}\";",
+               "confluent.topic.security.protocol" : "SASL_SSL",
+               "confluent.topic.replication.factor": "3"
+          }' \
+     http://localhost:8083/connectors/mqtt-source/config | jq .
+
+sleep 5
+
+log "Send message to MQTT in my-mqtt-topic topic"
+docker exec mosquitto sh -c 'mosquitto_pub -h localhost -p 1883 -u "myuser" -P "mypassword" -t "my-mqtt-topic" -m "sample-msg-1"'
+
+sleep 5
+
+log "Verify we have received the data in mqtt-source-1 topic"
+timeout 60 docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" connect bash -c 'kafka-console-consumer --topic mqtt-source-1 --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=USER_INFO --from-beginning --max-messages 1'
