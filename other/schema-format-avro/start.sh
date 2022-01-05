f69e990d8fd18301853da0f609e4c1d74ebc6614
Vincent de Saboulin <vdesabou@users.noreply.github.com>
2022-01-05 23:55:26 +0100

    Remove wrong skip

--- other/schema-format-avro/start.sh
+++ other/schema-format-avro/start.sh
@@ -4,11 +4,6 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-if ! version_gt $TAG_BASE "5.4.99"; then
-    logwarn "WARN: JSON Schema is available since CP 5.5 only"
-    exit 111
-fi
-
 for component in producer
 do
     set +e
