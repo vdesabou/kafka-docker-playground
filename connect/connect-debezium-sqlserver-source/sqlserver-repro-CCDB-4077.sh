a6d449c1e761fd656054569086e7b526da13d11c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-06 11:15:30 +0100

    add logs
    Former-commit-id: c362e331133980124d3e9d8c2204662ac6448332 [formerly 848635c4ca78efd1fc9b1b9fb8375023bfce65ea [formerly 0d224c03a94dc9a1ff79021f357a0c3c0793fbaa]]
    Former-commit-id: be09ba630f41f4753e72058c27b69af0b64fb48a
    Former-commit-id: 8db88b983cc0db3a27b52d30789a670d26d1f1c8

--- connect/connect-debezium-sqlserver-source/sqlserver-repro-CCDB-4077.sh
+++ connect/connect-debezium-sqlserver-source/sqlserver-repro-CCDB-4077.sh
@@ -42,3 +42,22 @@ EOF
 
 log "Verifying topic server1.dbo.customers"
 timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081  --property basic.auth.credentials.source=USER_INFO --property schema.registry.basic.auth.user.info='admin:admin' --topic server1.dbo.customers --from-beginning --max-messages 5
+
+
+# [2022-01-06 10:11:02,053] INFO [debezium-sqlserver-source|task-0] Starting SqlServerConnectorTask with configuration: (io.debezium.connector.common.BaseSourceTask:127)
+# [2022-01-06 10:11:02,055] INFO [debezium-sqlserver-source|task-0]    connector.class = io.debezium.connector.sqlserver.SqlServerConnector (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,055] INFO [debezium-sqlserver-source|task-0]    database.user = sa (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,055] INFO [debezium-sqlserver-source|task-0]    database.dbname = testDB (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,055] INFO [debezium-sqlserver-source|task-0]    tasks.max = 1 (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,055] INFO [debezium-sqlserver-source|task-0]    database.history.kafka.bootstrap.servers = broker:9092 (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,055] INFO [debezium-sqlserver-source|task-0]    database.history.kafka.topic = schema-changes.inventory (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,055] INFO [debezium-sqlserver-source|task-0]    database.server.name = server1 (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,055] INFO [debezium-sqlserver-source|task-0]    database.port = 1433 (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,055] INFO [debezium-sqlserver-source|task-0]    value.converter.basic.auth.credentials.source = USER_INFO (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,056] INFO [debezium-sqlserver-source|task-0]    value.converter.schema.registry.url = http://schema-registry:8081 (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,056] INFO [debezium-sqlserver-source|task-0]    value.converter.basic.auth.user.info = admin:admin (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,056] INFO [debezium-sqlserver-source|task-0]    task.class = io.debezium.connector.sqlserver.SqlServerConnectorTask (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,056] INFO [debezium-sqlserver-source|task-0]    database.hostname = sqlserver (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,056] INFO [debezium-sqlserver-source|task-0]    database.password = ******** (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,056] INFO [debezium-sqlserver-source|task-0]    name = debezium-sqlserver-source (io.debezium.connector.common.BaseSourceTask:129)
+# [2022-01-06 10:11:02,056] INFO [debezium-sqlserver-source|task-0]    value.converter = io.confluent.connect.avro.AvroConverter (io.debezium.connector.common.BaseSourceTask:129)
