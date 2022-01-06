d581e58726d4c69c4840e8d27b3fde003d43df1c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-06 11:09:19 +0100

    wip

---
+++ connect/connect-debezium-sqlserver-source/sqlserver-repro-CCDB-4077.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.CCDB-4077.yml"
+
+
+log "Load inventory.sql to SQL Server"
+cat inventory.sql | docker exec -i sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P Password!'
+
+
+log "Creating Debezium SQL Server source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.debezium.connector.sqlserver.SqlServerConnector",
+                    "tasks.max": "1",
+                    "database.hostname": "sqlserver",
+                    "database.port": "1433",
+                    "database.user": "sa",
+                    "database.password": "Password!",
+                    "database.server.name": "server1",
+                    "database.dbname" : "testDB",
+                    "database.history.kafka.bootstrap.servers": "broker:9092",
+                    "database.history.kafka.topic": "schema-changes.inventory",
+                    "value.converter": "io.confluent.connect.avro.AvroConverter",
+                    "value.converter.schema.registry.url": "http://schema-registry:8081",
+                    "value.converter.basic.auth.credentials.source": "USER_INFO",
+                    "value.converter.basic.auth.user.info": "admin:admin"
+          }' \
+     http://localhost:8083/connectors/debezium-sqlserver-source/config | jq .
+
+sleep 5
+
+docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! << EOF
+USE testDB;
+INSERT INTO customers(first_name,last_name,email) VALUES ('Pam','Thomas','pam@office.com');
+GO
+EOF
+
+log "Verifying topic server1.dbo.customers"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081  --property basic.auth.credentials.source=USER_INFO --property schema.registry.basic.auth.user.info='admin:admin' --topic server1.dbo.customers --from-beginning --max-messages 5
