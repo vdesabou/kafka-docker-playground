05c219a75b65c48bdaca4ab34140593235c2f1c5
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-08 22:46:51 +0100

    cosmetic

--- scripts/wait-for-connect-and-controlcenter.sh
+++ scripts/wait-for-connect-and-controlcenter.sh
@@ -38,51 +38,49 @@ CONTROL_CENTER_CONTAINER=${1:-"control-center"}
 
 if [ "${IGNORE_CONNECT_STARTUP}" == "FALSE" ]
 then
-  # Verify Kafka Connect has started within MAX_WAIT seconds
   MAX_WAIT=480
   CUR_WAIT=0
-  log "⌛ Waiting up to $MAX_WAIT seconds for Kafka Connect ${CONNECT_CONTAINER} to start"
+  log "⌛ Waiting up to $MAX_WAIT seconds for ${CONNECT_CONTAINER} to start"
   docker container logs ${CONNECT_CONTAINER} > /tmp/out.txt 2>&1
   while [[ ! $(cat /tmp/out.txt) =~ "Finished starting connectors and tasks" ]]; do
     sleep 10
     docker container logs ${CONNECT_CONTAINER} > /tmp/out.txt 2>&1
     CUR_WAIT=$(( CUR_WAIT+10 ))
     if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
-      echo -e "\nERROR: The logs in ${CONNECT_CONTAINER} container do not show 'Finished starting connectors and tasks' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+      logerror "ERROR: The logs in ${CONNECT_CONTAINER} container do not show <Finished starting connectors and tasks> after $MAX_WAIT seconds. Please troubleshoot with <docker container ps> and 'docker container logs"
       exit 1
     fi
   done
-  log "Connect ${CONNECT_CONTAINER} has started!"
+  log "🚦 ${CONNECT_CONTAINER} is started!"
 fi
 
 
 if [ "${IGNORE_CONTROL_CENTER_STARTUP}" == "FALSE" ]
 then
-  # Verify Confluent Control Center has started within MAX_WAIT seconds
   MAX_WAIT=480
   CUR_WAIT=0
-  log "⌛ Waiting up to $MAX_WAIT seconds for Confluent Control Center ${CONTROL_CENTER_CONTAINER} to start"
+  log "⌛ Waiting up to $MAX_WAIT seconds for ${CONTROL_CENTER_CONTAINER} to start"
   docker container logs ${CONTROL_CENTER_CONTAINER} > /tmp/out.txt 2>&1
   while [[ ! $(cat /tmp/out.txt) =~ "Started NetworkTrafficServerConnector" ]]; do
     sleep 10
     docker container logs ${CONTROL_CENTER_CONTAINER} > /tmp/out.txt 2>&1
     CUR_WAIT=$(( CUR_WAIT+10 ))
     if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
-      echo -e "\nERROR: The logs in ${CONTROL_CENTER_CONTAINER} container do not show 'Started NetworkTrafficServerConnector' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+      logerror "ERROR: The logs in ${CONTROL_CENTER_CONTAINER} container do not show <Started NetworkTrafficServerConnector> after $MAX_WAIT seconds. Please troubleshoot with <docker container ps> and <docker container logs>"
       exit 1
     fi
   done
-  log "Control Center ${CONTROL_CENTER_CONTAINER} has started!"
+  log "🚦 ${CONTROL_CENTER_CONTAINER} is started!"
 fi
 
 # Verify Docker containers started
 if [[ $(docker container ps) =~ "Exit 137" ]]; then
-  echo -e "\nERROR: At least one Docker container did not start properly, see 'docker container ps'. Did you remember to increase the memory available to Docker to at least 8GB (default is 2GB)?\n"
+  logerror "ERROR: At least one Docker container did not start properly, see <docker container ps>. Did you remember to increase the memory available to Docker to at least 8GB (default is 2GB)"
   exit 1
 fi
 
 if [ -z "$DISABLE_KSQLDB" ]
 then
-  log "You can use ksqlDB with CLI using:"
+  log "🔧 You can use ksqlDB with CLI using:"
   log "docker exec -i ksqldb-cli ksql http://ksqldb-server:8088"
 fi
