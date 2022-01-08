e59ce33e4a0c71c3c04e014bef34f08c6ee262eb
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-08 16:31:36 +0100

    fix stop
    Former-commit-id: 8e98daa645698f8e45da400442ec9e3276243867 [formerly 1ab5af036a5b1feb92aff8415b167ed167956bd8 [formerly 97977538669b9f402d9d2caa803c2338bf73e9ae]]
    Former-commit-id: b0776fd9bb2519fa3fc76ae6d39e761cbab1c8b0
    Former-commit-id: 00771e1b21b96987b06de79e4b9d7b9b078f419a

--- ccloud/mirrormaker2/stop.sh
+++ ccloud/mirrormaker2/stop.sh
@@ -5,4 +5,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-stop_all "$DIR"
+${DIR}/../../ccloud/environment/stop.sh "${PWD}/docker-compose.plaintext.yml"
