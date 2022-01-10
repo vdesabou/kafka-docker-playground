93ad23272eba46d08cd938e10f7a46c0546746d3
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-10 15:48:58 +0100

    updated

--- connect/connect-elasticsearch-sink/elasticsearch-repro-87676.sh
+++ connect/connect-elasticsearch-sink/elasticsearch-repro-87676.sh
@@ -1,14 +1,64 @@
 #!/bin/bash
 set -e
 
+# export CONNECTOR_TAG=5.4.3
+# export ELASTIC_VERSION="7.12.0"
+
+# [2022-01-10 14:39:37,101] TRACE [elasticsearch-sink|task-0] Adding record to queue (io.confluent.connect.elasticsearch.bulk.BulkProcessor:335)
+# [2022-01-10 14:39:37,102] DEBUG [elasticsearch-sink|task-0] Farmer1 Submitting batch of 1 records; 1 unsent and 1 total in-flight records (io.confluent.connect.elasticsearch.bulk.BulkProcessor:189)
+# [2022-01-10 14:39:37,110] DEBUG [elasticsearch-sink|task-0] Farmer1 Processing next batch with 1 outstanding batch requests in flight (io.confluent.connect.elasticsearch.bulk.BulkProcessor:151)
+# [2022-01-10 14:39:37,115] TRACE [elasticsearch-sink|task-0] Thread1 Executing batch 1 of 1 records with attempt 1/6 (io.confluent.connect.elasticsearch.bulk.BulkProcessor:438)
+# [2022-01-10 14:39:37,164] DEBUG [elasticsearch-sink|task-0] Thread1 Bulk request failed; collecting error(s) (io.confluent.connect.elasticsearch.jest.JestElasticsearchClient:549)
+# [2022-01-10 14:39:37,167] DEBUG [elasticsearch-sink|task-0] Thread1 Encountered an illegal document error when executing batch 1 of 1 records. Ignoring and will not index record. Error was [{"type":"mapper_parsing_exception","reason":"failed to parse","caused_by":{"type":"not_x_content_exception","reason":"Compressor detection can only be called on some xcontent bytes or compressed xcontent bytes"}}] (io.confluent.connect.elasticsearch.bulk.BulkProcessor:490)
+
+
+# export CONNECTOR_TAG=5.4.3
+# export ELASTIC_VERSION="6.8.3"
+
+# [2022-01-10 13:39:05,179] TRACE [elasticsearch-sink|task-0] Adding record to queue (io.confluent.connect.elasticsearch.bulk.BulkProcessor:335)
+# [2022-01-10 13:39:05,179] DEBUG [elasticsearch-sink|task-0] Farmer1 Submitting batch of 1 records; 1 unsent and 1 total in-flight records (io.confluent.connect.elasticsearch.bulk.BulkProcessor:189)
+# [2022-01-10 13:39:05,180] DEBUG [elasticsearch-sink|task-0] Farmer1 Bulk request completed with status BulkResponse{succeeded=false, retriable=true, errorInfo='[{"type":"mapper_parsing_exception","reason":"failed to parse","caused_by":{"type":"not_x_content_exception","reason":"Compressor detection can only be called on some xcontent bytes or compressed xcontent bytes"}}]'} (io.confluent.connect.elasticsearch.bulk.BulkProcessor:146)
+# [2022-01-10 13:39:05,180] DEBUG [elasticsearch-sink|task-0] Farmer1 Processing next batch with 1 outstanding batch requests in flight (io.confluent.connect.elasticsearch.bulk.BulkProcessor:151)
+# [2022-01-10 13:39:05,180] TRACE [elasticsearch-sink|task-0] Thread2 Executing batch 2 of 1 records with attempt 1/6 (io.confluent.connect.elasticsearch.bulk.BulkProcessor:438)
+# [2022-01-10 13:39:05,194] DEBUG [elasticsearch-sink|task-0] Thread2 Bulk request failed; collecting error(s) (io.confluent.connect.elasticsearch.jest.JestElasticsearchClient:549)
+# [2022-01-10 13:39:05,194] DEBUG [elasticsearch-sink|task-0] Thread2 Encountered an illegal document error when executing batch 2 of 1 records. Ignoring and will not index record. Error was [{"type":"mapper_parsing_exception","reason":"failed to parse","caused_by":{"type":"not_x_content_exception","reason":"Compressor detection can only be called on some xcontent bytes or compressed xcontent bytes"}}] (io.confluent.connect.elasticsearch.bulk.BulkProcessor:490)
+# [2022-01-10 13:39:41,189] DEBUG [elasticsearch-sink|task-0] Putting 0 records to Elasticsearch (io.confluent.connect.elasticsearch.ElasticsearchSinkTask:125)
+
+# As of version 11.0.0, the connector uses the Elasticsearch High Level REST Client (version 7.0.1),
+# which means only Elasticsearch 7.x is supported.
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-export CONNECTOR_TAG=5.4.3
-export ELASTIC_VERSION="7.12.0"
+export ELASTIC_VERSION="6.8.3"
+if version_gt $CONNECTOR_TAG "10.9.9"
+then
+    log "Connector version is > 11.0.0, using Elasticsearch 7.x"
+    export ELASTIC_VERSION="7.12.0"
+fi
 
+# With 11.1.7 we get:
 
-# [2022-01-10 13:29:56,742] WARN [elasticsearch-sink3|task-0] Bulk request 1 failed (io.confluent.connect.elasticsearch.ElasticsearchClient:396)
+# [2022-01-10 14:46:23,299] ERROR [elasticsearch-sink|task-0] Failed to execute bulk request due to 'org.elasticsearch.common.compress.NotXContentException: Compressor detection can only be called on some xcontent bytes or compressed xcontent bytes' after 6 attempt(s) (io.confluent.connect.elasticsearch.RetryUtil:164)
+# org.elasticsearch.common.compress.NotXContentException: Compressor detection can only be called on some xcontent bytes or compressed xcontent bytes
+#         at org.elasticsearch.common.compress.CompressorFactory.compressor(CompressorFactory.java:54)
+#         at org.elasticsearch.common.xcontent.XContentHelper.createParser(XContentHelper.java:71)
+#         at org.elasticsearch.client.RequestConverters.bulk(RequestConverters.java:234)
+#         at org.elasticsearch.client.RestHighLevelClient.internalPerformRequest(RestHighLevelClient.java:1609)
+#         at org.elasticsearch.client.RestHighLevelClient.performRequest(RestHighLevelClient.java:1583)
+#         at org.elasticsearch.client.RestHighLevelClient.performRequestAndParseEntity(RestHighLevelClient.java:1553)
+#         at org.elasticsearch.client.RestHighLevelClient.bulk(RestHighLevelClient.java:533)
+#         at io.confluent.connect.elasticsearch.ElasticsearchClient.lambda$null$0(ElasticsearchClient.java:170)
+#         at io.confluent.connect.elasticsearch.RetryUtil.callWithRetries(RetryUtil.java:158)
+#         at io.confluent.connect.elasticsearch.RetryUtil.callWithRetries(RetryUtil.java:119)
+#         at io.confluent.connect.elasticsearch.ElasticsearchClient.callWithRetries(ElasticsearchClient.java:425)
+#         at io.confluent.connect.elasticsearch.ElasticsearchClient.lambda$null$1(ElasticsearchClient.java:168)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:829)
+# [2022-01-10 14:46:23,302] WARN [elasticsearch-sink|task-0] Bulk request 1 failed (io.confluent.connect.elasticsearch.ElasticsearchClient:396)
 # org.apache.kafka.connect.errors.ConnectException: Failed to execute bulk request due to 'org.elasticsearch.common.compress.NotXContentException: Compressor detection can only be called on some xcontent bytes or compressed xcontent bytes' after 6 attempt(s)
 #         at io.confluent.connect.elasticsearch.RetryUtil.callWithRetries(RetryUtil.java:165)
 #         at io.confluent.connect.elasticsearch.RetryUtil.callWithRetries(RetryUtil.java:119)
@@ -29,20 +79,6 @@ export ELASTIC_VERSION="7.12.0"
 #         at org.elasticsearch.client.RestHighLevelClient.bulk(RestHighLevelClient.java:533)
 #         at io.confluent.connect.elasticsearch.ElasticsearchClient.lambda$null$0(ElasticsearchClient.java:170)
 #         at io.confluent.connect.elasticsearch.RetryUtil.callWithRetries(RetryUtil.java:158)
-#         ... 8 more
-
-
-export CONNECTOR_TAG=5.4.3
-export ELASTIC_VERSION="6.8.3"
-
-# [2022-01-10 13:39:05,179] TRACE [elasticsearch-sink|task-0] Adding record to queue (io.confluent.connect.elasticsearch.bulk.BulkProcessor:335)
-# [2022-01-10 13:39:05,179] DEBUG [elasticsearch-sink|task-0] Farmer1 Submitting batch of 1 records; 1 unsent and 1 total in-flight records (io.confluent.connect.elasticsearch.bulk.BulkProcessor:189)
-# [2022-01-10 13:39:05,180] DEBUG [elasticsearch-sink|task-0] Farmer1 Bulk request completed with status BulkResponse{succeeded=false, retriable=true, errorInfo='[{"type":"mapper_parsing_exception","reason":"failed to parse","caused_by":{"type":"not_x_content_exception","reason":"Compressor detection can only be called on some xcontent bytes or compressed xcontent bytes"}}]'} (io.confluent.connect.elasticsearch.bulk.BulkProcessor:146)
-# [2022-01-10 13:39:05,180] DEBUG [elasticsearch-sink|task-0] Farmer1 Processing next batch with 1 outstanding batch requests in flight (io.confluent.connect.elasticsearch.bulk.BulkProcessor:151)
-# [2022-01-10 13:39:05,180] TRACE [elasticsearch-sink|task-0] Thread2 Executing batch 2 of 1 records with attempt 1/6 (io.confluent.connect.elasticsearch.bulk.BulkProcessor:438)
-# [2022-01-10 13:39:05,194] DEBUG [elasticsearch-sink|task-0] Thread2 Bulk request failed; collecting error(s) (io.confluent.connect.elasticsearch.jest.JestElasticsearchClient:549)
-# [2022-01-10 13:39:05,194] DEBUG [elasticsearch-sink|task-0] Thread2 Encountered an illegal document error when executing batch 2 of 1 records. Ignoring and will not index record. Error was [{"type":"mapper_parsing_exception","reason":"failed to parse","caused_by":{"type":"not_x_content_exception","reason":"Compressor detection can only be called on some xcontent bytes or compressed xcontent bytes"}}] (io.confluent.connect.elasticsearch.bulk.BulkProcessor:490)
-# [2022-01-10 13:39:41,189] DEBUG [elasticsearch-sink|task-0] Putting 0 records to Elasticsearch (io.confluent.connect.elasticsearch.ElasticsearchSinkTask:125)
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
@@ -54,7 +90,7 @@ curl --request PUT \
 	"level": "TRACE"
 }'
 
-log "Creating Elasticsearch Sink connector (Elasticsearch version is $ELASTIC_VERSION")
+log "Creating Elasticsearch Sink connector (Elasticsearch version is $ELASTIC_VERSION"
 if version_gt $CONNECTOR_TAG "10.9.9"
 then
      # 7.x
@@ -76,7 +112,7 @@ then
                "behavior.on.malformed.documents": "ignore",
                "errors.log.enable": "true"
                }' \
-          http://localhost:8083/connectors/elasticsearch-sink3/config | jq .
+          http://localhost:8083/connectors/elasticsearch-sink/config | jq .
 else
      # 6.x
      curl -X PUT \
