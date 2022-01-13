b258dabca45d182f3c0c0e7a30a6368ea7a1de0f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-13 11:36:11 +0000

    wip
    Former-commit-id: 851ad496f504616a341a2db564eea47a65cda004 [formerly 27cbaa677a665b0f60ef051a277e0af79e4d47fc [formerly ab8d1aaf49cb679d52425d32adaaa414f8f83b50]]
    Former-commit-id: 564693984596f7a0cd2c062e6a649b50aa5a1275
    Former-commit-id: 67ef81eb387d6af653b70340f247fbbc3550bbae

--- connect/connect-hdfs2-sink/hdfs2-sink-ha-kerberos-repro-rcca-5389.sh
+++ connect/connect-hdfs2-sink/hdfs2-sink-ha-kerberos-repro-rcca-5389.sh
@@ -4,45 +4,65 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+NB_CONNECTORS=10
+NB_TASK_PER_CONNECTOR=2
+CONNECT_KERBEROS_TICKET_LIFETIME=6000
+
 function wait_for_repro () {
-     MAX_WAIT=600
+     MAX_WAIT=43200
      CUR_WAIT=0
-     log "⌛ Waiting up to $MAX_WAIT seconds for error CannotObtainBlockLengthException to happen"
+     log "⌛ Waiting up to $MAX_WAIT seconds for CannotObtainBlockLengthException to happen (it can take several hours)"
      docker container logs connect > /tmp/out.txt 2>&1
+     docker container logs connect2 >> /tmp/out.txt 2>&1
+     docker container logs connect3 >> /tmp/out.txt 2>&1
      while ! grep "CannotObtainBlockLengthException" /tmp/out.txt > /dev/null;
      do
           sleep 10
           docker container logs connect > /tmp/out.txt 2>&1
+          docker container logs connect2 >> /tmp/out.txt 2>&1
+          docker container logs connect3 >> /tmp/out.txt 2>&1
           CUR_WAIT=$(( CUR_WAIT+10 ))
           if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
                echo -e "\nERROR: The logs in all connect containers do not show 'CannotObtainBlockLengthException' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
                exit 1
           fi
+
+          for((i=0;i<$NB_CONNECTORS;i++)); do
+               # send requests
+               seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_hdfs$i --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+          done
      done
      log "The problem has been reproduced !"
 
      log "Connector status should be running, but connector cannot recover by itself, the only way is to remove WAL manually"
-     curl --request GET \
-     --url http://localhost:8083/connectors/hdfs2-sink-ha-kerberos/status \
-     --header 'Accept: application/json' | jq .
+     for((i=0;i<$NB_CONNECTORS;i++)); do
+          curl --request GET \
+          --url http://localhost:8083/connectors/hdfs-sink-kerberos$i/status \
+          --header 'Accept: application/json' | jq .
+     done
 
-
-     log "Delete WAL log /logs/test_hdfs/0/log to unblock the connector ?"
+     log "Delete WAL log to unblock the connector ?"
      check_if_continue
-     docker exec namenode1 bash -c "kinit -kt /opt/hadoop/etc/hadoop/nn.keytab nn/namenode1.kerberos-demo.local && /opt/hadoop/bin/hadoop fs -rm /logs/test_hdfs/0/log"
+     set +e
+     for((i=0;i<$NB_CONNECTORS;i++)); do
+          docker exec namenode1 bash -c "kinit -kt /opt/hadoop/etc/hadoop/nn.keytab nn/namenode1.kerberos-demo.local && /opt/hadoop/bin/hadoop fs -rm /logs$i/test_hdfs$i/0/log"
+     done
+     set -e
 
      log "Sending messages to make sure connector is unblocked"
-     seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_hdfs --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
-
+     for((i=0;i<$NB_CONNECTORS;i++)); do
+          # send requests
+          seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_hdfs$i --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+     done
 }
 
-if [ ! -f ${DIR}/hadoop-2.7.4.tar.gz ]
+if [ ! -f ${DIR}/hadoop-2.7.3.tar.gz ]
 then
-     log "Getting hadoop-2.7.4.tar.gz"
-     wget https://archive.apache.org/dist/hadoop/common/hadoop-2.7.4/hadoop-2.7.4.tar.gz
+     log "Getting hadoop-2.7.3.tar.gz"
+     wget https://archive.apache.org/dist/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
 fi
 
-${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.ha-kerberos.yml"
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.ha-kerberos-repro-rcca-5389.yml"
 
 log "Wait 120 seconds while hadoop is installing"
 sleep 120
@@ -52,15 +72,16 @@ docker exec namenode1 bash -c "kinit -kt /opt/hadoop/etc/hadoop/nn.keytab nn/nam
 
 log "Add connect kerberos principal"
 docker exec -i krb5 kadmin.local << EOF
-addprinc -randkey connect/connect.kerberos-demo.local@EXAMPLE.COM
-modprinc -maxrenewlife 11days +allow_renewable connect/connect.kerberos-demo.local@EXAMPLE.COM
-modprinc -maxrenewlife 11days krbtgt/EXAMPLE.COM
-modprinc -maxlife 11days connect/connect.kerberos-demo.local@EXAMPLE.COM
-ktadd -k /connect.keytab connect/connect.kerberos-demo.local@EXAMPLE.COM
-listprincs
+addprinc -randkey connect@EXAMPLE.COM
+modprinc -maxrenewlife 604800 +allow_renewable connect@EXAMPLE.COM
+modprinc -maxrenewlife 604800 +allow_renewable krbtgt/EXAMPLE.COM
+modprinc -maxrenewlife 604800 +allow_renewable krbtgt/EXAMPLE.COM@EXAMPLE.COM
+modprinc -maxlife $CONNECT_KERBEROS_TICKET_LIFETIME connect@EXAMPLE.COM
+ktadd -k /connect.keytab connect@EXAMPLE.COM
+getprinc connect@EXAMPLE.COM
 EOF
 
-log "Copy connect.keytab to connect container /tmp/sshuser.keytab"
+log "Copy connect.keytab to connect container"
 docker cp krb5:/connect.keytab .
 docker cp connect.keytab connect:/tmp/connect.keytab
 if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
@@ -68,55 +89,70 @@ then
      docker exec -u 0 connect chown appuser:appuser /tmp/connect.keytab
 fi
 
-log "Creating HDFS Sink connector"
-curl -X PUT \
-     -H "Content-Type: application/json" \
-     --data '{
-               "connector.class":"io.confluent.connect.hdfs.HdfsSinkConnector",
-               "tasks.max":"1",
-               "topics":"test_hdfs",
-               "store.url":"hdfs://sh",
-               "flush.size":"3",
-               "hadoop.conf.dir":"/opt/hadoop/etc/hadoop/",
-               "partitioner.class":"io.confluent.connect.hdfs.partitioner.FieldPartitioner",
-               "partition.field.name":"f1",
-               "rotate.interval.ms":"120000",
-               "logs.dir":"/logs",
-               "hdfs.authentication.kerberos": "true",
-               "connect.hdfs.principal": "connect/connect.kerberos-demo.local@EXAMPLE.COM",
-               "connect.hdfs.keytab": "/tmp/connect.keytab",
-               "hdfs.namenode.principal": "nn/namenode1.kerberos-demo.local@EXAMPLE.COM",
-               "confluent.license": "",
-               "confluent.topic.bootstrap.servers": "broker:9092",
-               "confluent.topic.replication.factor": "1",
-               "key.converter":"org.apache.kafka.connect.storage.StringConverter",
-               "value.converter":"io.confluent.connect.avro.AvroConverter",
-               "value.converter.schema.registry.url":"http://schema-registry:8081",
-               "schema.compatibility":"BACKWARD"
-          }' \
-     http://localhost:8083/connectors/hdfs2-sink-ha-kerberos/config | jq .
+log "Copy connect.keytab to connect2 container"
+docker cp connect.keytab connect2:/tmp/connect.keytab
+if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
+then
+     docker exec -u 0 connect2 chown appuser:appuser /tmp/connect.keytab
+fi
+
+log "Copy connect.keytab to connect3 container"
+docker cp connect.keytab connect3:/tmp/connect.keytab
+if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
+then
+     docker exec -u 0 connect3 chown appuser:appuser /tmp/connect.keytab
+fi
 
 
-log "Sending messages to topic test_hdfs"
-seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_hdfs --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+for((i=0;i<$NB_CONNECTORS;i++)); do
+     LOG_DIR="/logs$i"
+     TOPIC="test_hdfs$i"
+     log "Creating HDFS Sink connector $i"
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+                    "connector.class":"io.confluent.connect.hdfs.HdfsSinkConnector",
+                    "tasks.max":"'"$NB_TASK_PER_CONNECTOR"'",
+                    "topics": "'"$TOPIC"'",
+                    "store.url":"hdfs://sh",
+                    "flush.size":"3",
+                    "hadoop.conf.dir":"/opt/hadoop/etc/hadoop/",
+                    "partitioner.class":"io.confluent.connect.hdfs.partitioner.FieldPartitioner",
+                    "partition.field.name":"f1",
+                    "rotate.interval.ms":"120000",
+                    "logs.dir": "'"$LOG_DIR"'",
+                    "hdfs.authentication.kerberos": "true",
+                    "kerberos.ticket.renew.period.ms": "5000",
+                    "connect.hdfs.principal": "connect@EXAMPLE.COM",
+                    "connect.hdfs.keytab": "/tmp/connect.keytab",
+                    "hdfs.namenode.principal": "nn/_HOST@EXAMPLE.COM",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1",
+                    "key.converter":"org.apache.kafka.connect.storage.StringConverter",
+                    "value.converter":"io.confluent.connect.avro.AvroConverter",
+                    "value.converter.schema.registry.url":"http://schema-registry:8081",
+                    "schema.compatibility":"BACKWARD"
+               }' \
+          http://localhost:8083/connectors/hdfs-sink-kerberos$i/config | jq .
+done
 
-sleep 10
+for((i=0;i<$NB_CONNECTORS;i++)); do
+     # send requests
+     seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_hdfs$i --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+done
 
-log "Listing content of /topics/test_hdfs in HDFS"
-docker exec namenode1 bash -c "kinit -kt /opt/hadoop/etc/hadoop/nn.keytab nn/namenode1.kerberos-demo.local && /opt/hadoop/bin/hdfs dfs -ls /topics/test_hdfs"
-
-log "Getting one of the avro files locally and displaying content with avro-tools"
-docker exec namenode1 bash -c "kinit -kt /opt/hadoop/etc/hadoop/nn.keytab nn/namenode1.kerberos-demo.local && /opt/hadoop/bin/hadoop fs -copyToLocal /topics/test_hdfs/f1=value1/test_hdfs+0+0000000000+0000000000.avro /tmp"
-docker cp namenode1:/tmp/test_hdfs+0+0000000000+0000000000.avro /tmp/
-
-docker run --rm -v /tmp:/tmp actions/avro-tools tojson /tmp/test_hdfs+0+0000000000+0000000000.avro
-
-log "Now restarting datanode container"
-docker restart datanode
+log "Now stopping datanode container"
+docker stop datanode
 
 sleep 30
 
-log "Sending messages to topic test_hdfs"
-seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_hdfs --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+for((i=0;i<$NB_CONNECTORS;i++)); do
+     # send requests
+     seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic test_hdfs$i --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+done
+
+log "Now starting datanode container"
+docker start datanode
 
 wait_for_repro
