4a11cbacf35c93b1facf08d2e133131c1f2f6b48
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-19 17:28:06 +0100

    simplify schema and use json.oneof.for.nullables false
    Former-commit-id: db4feaeb9f5ad6c8e305d6a1ccfc0ea2aec18a37 [formerly 4e506f39754aa1e215e34b8ac2e48eb6aa76aa08 [formerly 288271950dff3672a45bc7ff0a6a93a7f22f4dd7]]
    Former-commit-id: c32db56b18125dced61fc66fe45eb69016e9a9b4
    Former-commit-id: a02acaef187cb283e68f479239e22409529481a3

--- connect/connect-jdbc-mysql-sink/producer-88620/src/main/java/com/github/vdesabou/SimpleProducer.java
+++ connect/connect-jdbc-mysql-sink/producer-88620/src/main/java/com/github/vdesabou/SimpleProducer.java
@@ -99,7 +99,8 @@ public class SimpleProducer {
     private Map<String, String> defaultProps = Map.of(
             ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:9092",
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongSerializer",
-            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializer");
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializer",
+            "json.oneof.for.nullables", "false");
 
     private Properties buildProperties(Map<String, String> baseProps, Map<String, String> envProps, String prefix) {
         Map<String, String> systemProperties = envProps.entrySet()
