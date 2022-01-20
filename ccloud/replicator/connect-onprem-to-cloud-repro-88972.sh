1aeec7327d32ddeb618a78cc17effe860fe70baf
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-20 16:56:15 +0100

    repro
    Former-commit-id: 7ee7451ab7621c27706674a5bdbacf2260fe7143 [formerly 1c7b07eccd8887e4f439eedb32ccabef44b0913c [formerly 1f851b796d932fa837ae3c88beb41740fde67ea5]]
    Former-commit-id: d2f4c0fb4f6d25109efb0b09dc7c7ed53ac18b3f
    Former-commit-id: cc282f7b3b99e8240f828ee7c5f4b42f9ba869c5

---
+++ ccloud/replicator/connect-onprem-to-cloud-repro-88972.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+#############
+${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose-connect-onprem-to-cloud.yml"
+
+if [ -f /tmp/delta_configs/env.delta ]
+then
+     source /tmp/delta_configs/env.delta
+else
+     logerror "ERROR: /tmp/delta_configs/env.delta has not been generated"
+     exit 1
+fi
+#############
+
+log "Creating topic in Confluent Cloud (auto.create.topics.enable=false)"
+set +e
+delete_topic products
+sleep 3
+create_topic products
+set -e
+
+log "Sending messages to topic products on source OnPREM cluster"
+seq 10 | docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic products
+
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+          "connector.class":"io.confluent.connect.replicator.ReplicatorSourceConnector",
+          "key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+          "value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+          "header.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+          "src.consumer.group.id": "replicate-onprem-to-cloud",
+          "src.kafka.bootstrap.servers": "broker:9092",
+          "dest.kafka.ssl.endpoint.identification.algorithm":"https",
+          "dest.kafka.bootstrap.servers": "${file:/data:bootstrap.servers}",
+          "dest.kafka.security.protocol" : "SASL_SSL",
+          "dest.kafka.sasl.jaas.config": "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${file:/data:sasl.username}\" password=\"${file:/data:sasl.password}\";",
+          "dest.kafka.sasl.mechanism":"PLAIN",
+          "dest.kafka.request.timeout.ms":"20000",
+          "dest.kafka.retry.backoff.ms":"500",
+          "confluent.topic.ssl.endpoint.identification.algorithm" : "https",
+          "confluent.topic.sasl.mechanism" : "PLAIN",
+          "confluent.topic.bootstrap.servers": "${file:/data:bootstrap.servers}",
+          "confluent.topic.sasl.jaas.config" : "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${file:/data:sasl.username}\" password=\"${file:/data:sasl.password}\";",
+          "confluent.topic.security.protocol" : "SASL_SSL",
+          "confluent.topic.replication.factor": "3",
+          "provenance.header.enable": true,
+          "topic.whitelist": "products",
+          "topic.config.sync": true,
+          "topic.auto.create": false,
+
+          "schema.registry.url": "'"$SCHEMA_REGISTRY_URL"'",
+          "schema.registry.topic": "_schemas",
+          "schema.registry.client.basic.auth.user.info": "${file:/data:schema.registry.basic.auth.user.info}",
+          "schema.registry.client.basic.auth.credentials.source": "USER_INFO",
+          "producer.override.bootstrap.servers": "${file:/data:bootstrap.servers}",
+          "producer.override.enable.idempotence": "true",
+          "producer.override.acks": "all",
+          "producer.override.delivery.timeout.ms": "24000",
+          "producer.override.compression.type": "zstd",
+          "producer.override.request.timeout.ms": "20000",
+          "consumer.override.session.timeout.ms": "45000",
+          "tasks.max": "1"
+          }' \
+     http://localhost:8083/connectors/replicate-onprem-to-cloud/config | jq .
+
+
+log "Verify we have received the data in products topic"
+timeout 60 docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" connect bash -c 'kafka-console-consumer --topic products --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=$BASIC_AUTH_CREDENTIALS_SOURCE --from-beginning --max-messages 10'
