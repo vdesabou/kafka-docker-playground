d391be491b15860c6ae57442a128174c91b70eb3
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-21 17:18:25 +0100

    results with "decimal.handling.mode": "double"
    Former-commit-id: 655e8e2bc33e661e91604563a339a4f66c2b3f1d [formerly b8dadb9db3301228681811b7711d853a4b4736a7 [formerly 4e548b3bac33f07436257f767e4aa145f998a511]]
    Former-commit-id: c72fa69a3f601742c5e57424b7aca450062d949e
    Former-commit-id: 1c0ea2c2a88fb8c56246a7719fd97dadbecb50cb

--- connect/connect-debezium-postgresql-source/postgres-repro-81246.sh
+++ connect/connect-debezium-postgresql-source/postgres-repro-81246.sh
@@ -35,7 +35,8 @@ curl -X PUT \
                     "transforms": "addTopicSuffix",
                     "transforms.addTopicSuffix.type":"org.apache.kafka.connect.transforms.RegexRouter",
                     "transforms.addTopicSuffix.regex":"(.*)",
-                    "transforms.addTopicSuffix.replacement":"$1-raw"
+                    "transforms.addTopicSuffix.replacement":"$1-raw",
+                    "decimal.handling.mode": "double"
           }' \
      http://localhost:8083/connectors/debezium-postgres-source/config | jq .
 
@@ -46,21 +47,21 @@ sleep 5
 log "Verifying topic asgard.public.customers-raw"
 timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic asgard.public.customers-raw --from-beginning --property print.key=true --max-messages 5
 
-# With latest (1.7.1):  no Cannot parse column default value 'NULL::numeric' to type 'numeric', but "curr_amount":"x"
+# With latest (1.7.1):  no Cannot parse column default value 'NULL::numeric' to type 'numeric', but "curr_amount":"1.2"
 
 # [2022-01-21 15:01:15,758] WARN [debezium-postgres-source3|task-0] Cannot parse column default value 'CURRENT_TIMESTAMP' to type 'timestamp'. Expression evaluation is not supported. (io.debezium.connector.postgresql.connection.PostgresDefaultValueConverter:88)
 # [2022-01-21 15:01:15,758] WARN [debezium-postgres-source3|task-0] Cannot parse column default value 'CURRENT_TIMESTAMP' to type 'timestamp'. Expression evaluation is not supported. (io.debezium.connector.postgresql.connection.PostgresDefaultValueConverter:88)
 
 
-# {"id":1}        {"before":null,"after":{"asgard.public.customers.Value":{"id":1,"first_name":{"string":"Rica"},"last_name":{"string":"Blaisdell"},"email":{"string":"rblaisdell0@rambler.ru"},"gender":{"string":"Female"},"club_status":{"string":"bronze"},"comments":{"string":"Universal optimal hierarchy"},"create_ts":{"long":1642781028730102},"update_ts":{"long":1642781028730102},"curr_amount":"x"}},"source":{"version":"1.7.1.Final","connector":"postgresql","name":"asgard","ts_ms":1642781065106,"snapshot":{"string":"true"},"db":"postgres","sequence":{"string":"[null,\"24514616\"]"},"schema":"public","table":"customers","txId":{"long":580},"lsn":{"long":24514616},"xmin":null},"op":"r","ts_ms":{"long":1642781065111},"transaction":null}
+# {"id":1}        {"before":null,"after":{"asgard.public.customers.Value":{"id":1,"first_name":{"string":"Rica"},"last_name":{"string":"Blaisdell"},"email":{"string":"rblaisdell0@rambler.ru"},"gender":{"string":"Female"},"club_status":{"string":"bronze"},"comments":{"string":"Universal optimal hierarchy"},"create_ts":{"long":1642781519530266},"update_ts":{"long":1642781519530266},"curr_amount":1.2}},"source":{"version":"1.7.1.Final","connector":"postgresql","name":"asgard","ts_ms":1642781556064,"snapshot":{"string":"true"},"db":"postgres","sequence":{"string":"[null,\"24525144\"]"},"schema":"public","table":"customers","txId":{"long":580},"lsn":{"long":24525144},"xmin":null},"op":"r","ts_ms":{"long":1642781556069},"transaction":null}
 
-# with 1.6.3 -> "curr_amount":"x"
+# with 1.6.3 -> "curr_amount":"1.2"
 # [2022-01-21 16:01:53,106] WARN [debezium-postgres-source|task-0] Cannot parse column default value 'CURRENT_TIMESTAMP' to type 'timestamp'. Expression evaluation is not supported. (io.debezium.connector.postgresql.connection.PostgresDefaultValueConverter:88)
 # [2022-01-21 16:01:53,107] WARN [debezium-postgres-source|task-0] Cannot parse column default value 'CURRENT_TIMESTAMP' to type 'timestamp'. Expression evaluation is not supported. (io.debezium.connector.postgresql.connection.PostgresDefaultValueConverter:88)
 # [2022-01-21 16:01:53,107] WARN [debezium-postgres-source|task-0] Cannot parse column default value 'NULL::numeric' to type 'numeric'. Expression evaluation is not supported. (io.debezium.connector.postgresql.connection.PostgresDefaultValueConverter:88)
 
-# {"id":1}        {"before":null,"after":{"asgard.public.customers.Value":{"id":1,"first_name":{"string":"Rica"},"last_name":{"string":"Blaisdell"},"email":{"string":"rblaisdell0@rambler.ru"},"gender":{"string":"Female"},"club_status":{"string":"bronze"},"comments":{"string":"Universal optimal hierarchy"},"create_ts":{"long":1642780876781560},"update_ts":{"long":1642780876781560},"curr_amount":"x"}},"source":{"version":"1.6.3.Final","connector":"postgresql","name":"asgard","ts_ms":1642780913015,"snapshot":{"string":"true"},"db":"postgres","sequence":{"string":"[null,\"24524632\"]"},"schema":"public","table":"customers","txId":{"long":580},"lsn":{"long":24524632},"xmin":null},"op":"r","ts_ms":{"long":1642780913018},"transaction":null}
+# {"id":1}        {"before":null,"after":{"asgard.public.customers.Value":{"id":1,"first_name":{"string":"Rica"},"last_name":{"string":"Blaisdell"},"email":{"string":"rblaisdell0@rambler.ru"},"gender":{"string":"Female"},"club_status":{"string":"bronze"},"comments":{"string":"Universal optimal hierarchy"},"create_ts":{"long":1642781643548279},"update_ts":{"long":1642781643548279},"curr_amount":1.2}},"source":{"version":"1.6.3.Final","connector":"postgresql","name":"asgard","ts_ms":1642781679777,"snapshot":{"string":"true"},"db":"postgres","sequence":{"string":"[null,\"24522696\"]"},"schema":"public","table":"customers","txId":{"long":580},"lsn":{"long":24522696},"xmin":null},"op":"r","ts_ms":{"long":1642781679782},"transaction":null}
 
 # With 1.4.1: no Cannot parse column default value at all and "curr_amount":"1.2"
 
-# {"id":1}        {"before":null,"after":{"asgard.public.customers.Value":{"id":1,"first_name":{"string":"Rica"},"last_name":{"string":"Blaisdell"},"email":{"string":"rblaisdell0@rambler.ru"},"gender":{"string":"Female"},"club_status":{"string":"bronze"},"comments":{"string":"Universal optimal hierarchy"},"create_ts":{"long":1642780714611751},"update_ts":{"long":1642780714611751},"curr_amount":1.2}},"source":{"version":"1.4.1.Final","connector":"postgresql","name":"asgard","ts_ms":1642780750771,"snapshot":{"string":"true"},"db":"postgres","schema":"public","table":"customers","txId":{"long":580},"lsn":{"long":24530848},"xmin":null},"op":"r","ts_ms":{"long":1642780750774},"transaction":null}
+# {"id":1}        {"before":null,"after":{"asgard.public.customers.Value":{"id":1,"first_name":{"string":"Rica"},"last_name":{"string":"Blaisdell"},"email":{"string":"rblaisdell0@rambler.ru"},"gender":{"string":"Female"},"club_status":{"string":"bronze"},"comments":{"string":"Universal optimal hierarchy"},"create_ts":{"long":1642781817800503},"update_ts":{"long":1642781817800503},"curr_amount":1.2}},"source":{"version":"1.4.1.Final","connector":"postgresql","name":"asgard","ts_ms":1642781853914,"snapshot":{"string":"true"},"db":"postgres","schema":"public","table":"customers","txId":{"long":580},"lsn":{"long":24527280},"xmin":null},"op":"r","ts_ms":{"long":1642781853917},"transaction":null}
