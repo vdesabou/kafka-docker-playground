86687bc73ab3247497e8bfaae01c9931ad24e1eb
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-21 14:42:33 +0000

    wip no repro

---
+++ connect/connect-debezium-postgresql-source/postgres-repro-81246.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-81246.yml"
+
+
+log "Show content of CUSTOMERS table:"
+docker exec postgres bash -c "psql -U myuser -d postgres -c 'SELECT * FROM CUSTOMERS'"
+
+log "Adding an element to the table"
+docker exec postgres psql -U myuser -d postgres -c "insert into customers (id, first_name, last_name, email, gender, comments, curr_amount) values (21, 'Bernardo', 'Dudman', 'bdudmanb@lulu.com', 'Male', 'Robust bandwidth-monitored budgetary management', 1.4);"
+
+log "Show content of CUSTOMERS table:"
+docker exec postgres bash -c "psql -U myuser -d postgres -c 'SELECT * FROM CUSTOMERS'"
+
+log "Creating Debezium PostgreSQL source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+                    "tasks.max": "1",
+                    "database.hostname": "postgres",
+                    "database.port": "5432",
+                    "database.user": "myuser",
+                    "database.password": "mypassword",
+                    "database.dbname" : "postgres",
+                    "database.server.name": "asgard",
+                    "key.converter" : "io.confluent.connect.avro.AvroConverter",
+                    "key.converter.schema.registry.url": "http://schema-registry:8081",
+                    "value.converter" : "io.confluent.connect.avro.AvroConverter",
+                    "value.converter.schema.registry.url": "http://schema-registry:8081",
+                    "transforms": "addTopicSuffix",
+                    "transforms.addTopicSuffix.type":"org.apache.kafka.connect.transforms.RegexRouter",
+                    "transforms.addTopicSuffix.regex":"(.*)",
+                    "transforms.addTopicSuffix.replacement":"$1-raw"
+          }' \
+     http://localhost:8083/connectors/debezium-postgres-source/config | jq .
+
+
+
+sleep 5
+
+log "Verifying topic asgard.public.customers-raw"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic asgard.public.customers-raw --from-beginning --property print.key=true --max-messages 5
+
+
