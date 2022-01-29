ad5f7a95974629f1f3c1dc39ba2d7d4114663af3
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-29 16:09:05 +0100

    bulk delete all Leads with CI
    Former-commit-id: 13ac40461920a39d40f381a1f57f68c8ce0d6675 [formerly 0b2cc65f228328391acd5dc7a1659d6eb1a61481 [formerly 2c437c87196b6506990d8231bc8f85e81c8e1b08]]
    Former-commit-id: a8e5b28b09359e497fb26e7f4e3f2de5dd585be9
    Former-commit-id: a553d37350ad8c5cf0c886f2ce34da6dca94c82f

--- connect/connect-salesforce-pushtopics-source/stop.sh
+++ connect/connect-salesforce-pushtopics-source/stop.sh
@@ -5,4 +5,29 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+if [ ! -z "$CI" ]
+then
+     # running with github actions
+     if [ ! -f ../../secrets.properties ]
+     then
+          logerror "../../secrets.properties is not present!"
+          exit 1
+     fi
+     source ../../secrets.properties > /dev/null 2>&1
+
+
+    SALESFORCE_USERNAME=${SALESFORCE_USERNAME:-$1}
+    SALESFORCE_PASSWORD=${SALESFORCE_PASSWORD:-$2}
+    SECURITY_TOKEN=${SECURITY_TOKEN:-$5}
+    SALESFORCE_INSTANCE=${SALESFORCE_INSTANCE:-"https://login.salesforce.com"}
+
+    log "Login with sfdx CLI on the account #2"
+    docker exec sfdx-cli sh -c "sfdx sfpowerkit:auth:login -u \"$SALESFORCE_USERNAME\" -p \"$SALESFORCE_PASSWORD\" -r \"$SALESFORCE_INSTANCE\" -s \"$SECURITY_TOKEN\""
+
+    log "Bulk delete leads"
+    docker exec sfdx-cli sh -c "sfdx force:data:soql:query -u \"$SALESFORCE_USERNAME\" -q \"SELECT Id FROM Lead\" --resultformat csv" > /tmp/out.csv
+    docker cp /tmp/out.csv sfdx-cli:/tmp/out.csv
+    docker exec  sfdx-cli sh -c "sfdx force:data:bulk:delete -u \"$SALESFORCE_USERNAME\" -s Lead -f /tmp/out.csv"
+fi
+
 stop_all "$DIR"
