a9f1a11864866045f12548bea13facf08a6baece
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-02-02 13:51:38 +0100

    updated with latest findings
    Former-commit-id: 0e49fea627a6e63d31c7b5c35b5720f4583b3dfa [formerly 7b97fd32c8a85266ab515969e690ab8147ca7b14 [formerly 92614fd79c28c0bddbf7c5209af07e5b419aa247]]
    Former-commit-id: b144598b43583e783c61e0a7c2272babb2827b20
    Former-commit-id: 4a7a72539bc02dff2cda7320d31801d7418277ab

--- connect/connect-gcp-bigquery-sink/gcp-bigquery-repro-87895.sh
+++ connect/connect-gcp-bigquery-sink/gcp-bigquery-repro-87895.sh
@@ -47,8 +47,7 @@ then
      exit 1
 fi
 
-# pgrepro should exist with table created like this: https://github.com/vdesabou/kafka-docker-playground/blob/e8ee3b1cfd8b0704b70a669f147fe16e958ac14e/connect/connect-gcp-bigquery-sink/producer-87895/customer-avro.jpg
-DATASET=pgrepro
+DATASET=pgrepro87895
 DATASET=${DATASET//[-._]/}
 
 log "Doing gsutil authentication"
@@ -57,9 +56,25 @@ docker rm -f gcloud-config
 set -e
 docker run -i -v ${KEYFILE}:/tmp/keyfile.json --name gcloud-config google/cloud-sdk:latest gcloud auth activate-service-account --project ${PROJECT} --key-file /tmp/keyfile.json
 
+set +e
+log "Drop dataset $DATASET, this might fail"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" rm -r -f -d "$DATASET"
+set -e
+
+log "Create dataset $PROJECT.$DATASET"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" mk --dataset --description "used by playground" "$DATASET"
+
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-87895.yml"
 
-log "Creating GCP BigQuery Sink connector with autoCreateTables=true"
+curl --request PUT \
+  --url http://localhost:8083/admin/loggers/com.wepay.kafka.connect.bigquery \
+  --header 'Accept: application/json' \
+  --header 'Content-Type: application/json' \
+  --data '{
+	"level": "DEBUG"
+}'
+
+log "Creating GCP BigQuery Sink connector with autoCreateTables=false"
 curl -X PUT \
      -H "Content-Type: application/json" \
      --data '{
@@ -77,125 +92,40 @@ curl -X PUT \
                "keyfile" : "/tmp/keyfile.json",
                "deleteEnabled": "true",
                "upsertEnabled": "true",
+               "avroDataCacheSize": "100",
                "kafkaKeyFieldName": "KEY",
                "intermediateTableSuffix": "_intermediate",
                "key.converter" : "io.confluent.connect.avro.AvroConverter",
-               "key.converter.schema.registry.url" : "http://schema-registry:8081"
+               "key.converter.schema.registry.url" : "http://schema-registry:8081",
+               "consumer.override.max.poll.records":  "10000",
+               "consumer.override.fetch.min.bytes": "1048576",
+               "consumer.override.fetch.max.wait.ms": "1000",
+               "consumer.override.session.timeout.ms": "60000"
           }' \
-     http://localhost:8083/connectors/gcp-bigquery-sink-key-nullable-to-required/config | jq .
+     http://localhost:8083/connectors/gcp-bigquery-sink/config | jq .
 
+sleep 10
 
-log "Run the Java producer-87895 (KEY is REQUIRED)"
+log "Run the Java producer-87895 (10 records)"
 docker exec producer-87895 bash -c "java -jar producer-87895-1.0.0-jar-with-dependencies.jar"
 
-wait_for_repro "Field KEY.KEY has changed mode from NULLABLE to REQUIRED"
+sleep 30
 
-# [2022-01-11 16:59:38,234] INFO [gcp-bigquery-sink|task-0] Attempting to update table `pgrepro`.`customer-avro` with schema Schema{fields=[Field{name=count, type=INTEGER, mode=REQUIRED, description=null, policyTags=null}, Field{name=first_name, type=STRING, mode=REQUIRED, description=null, policyTags=null}, Field{name=last_name, type=STRING, mode=REQUIRED, description=null, policyTags=null}, Field{name=address, type=STRING, mode=REQUIRED, description=null, policyTags=null}, Field{name=KEY, type=RECORD, mode=NULLABLE, description=null, policyTags=null}]} (com.wepay.kafka.connect.bigquery.SchemaManager:255)
-# Exception in thread "pool-6-thread-3" com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException: Failed to create table GenericData{classInfo=[datasetId, projectId, tableId], {datasetId=pgrepro, tableId=customer_avro__intermediate_0_a3fe48e9_191e_42bb_88be_2334e52cfe7c_1641920361088}}
-# Caused by: Provided Schema does not match Table vincent-de-saboulin-lab:pgrepro.customer-avro. Field KEY.KEY has changed mode from NULLABLE to REQUIRED
-# [2022-01-11 16:59:38,686] ERROR [gcp-bigquery-sink|task-0] Task failed with com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException error: Failed to create table GenericData{classInfo=[datasetId, projectId, tableId], {datasetId=pgrepro, tableId=customer_avro__intermediate_0_a3fe48e9_191e_42bb_88be_2334e52cfe7c_1641920361088}} (com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor:70)
-#         at com.wepay.kafka.connect.bigquery.write.row.UpsertDeleteBigQueryWriter.attemptTableCreate(UpsertDeleteBigQueryWriter.java:89)
-#         at com.wepay.kafka.connect.bigquery.write.row.AdaptiveBigQueryWriter.performWriteRequest(AdaptiveBigQueryWriter.java:115)
-#         at com.wepay.kafka.connect.bigquery.write.row.BigQueryWriter.writeRows(BigQueryWriter.java:118)
-#         at com.wepay.kafka.connect.bigquery.write.batch.TableWriter.run(TableWriter.java:96)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
-#         at java.base/java.lang.Thread.run(Thread.java:829)
-# Caused by: com.google.cloud.bigquery.BigQueryException: Provided Schema does not match Table vincent-de-saboulin-lab:pgrepro.customer-avro. Field KEY.KEY has changed mode from NULLABLE to REQUIRED
-#         at com.google.cloud.bigquery.spi.v2.HttpBigQueryRpc.translate(HttpBigQueryRpc.java:113)
-#         at com.google.cloud.bigquery.spi.v2.HttpBigQueryRpc.patch(HttpBigQueryRpc.java:270)
-#         at com.google.cloud.bigquery.BigQueryImpl$14.call(BigQueryImpl.java:590)
-#         at com.google.cloud.bigquery.BigQueryImpl$14.call(BigQueryImpl.java:587)
-#         at com.google.api.gax.retrying.DirectRetryingExecutor.submit(DirectRetryingExecutor.java:105)
-#         at com.google.cloud.RetryHelper.run(RetryHelper.java:76)
-#         at com.google.cloud.RetryHelper.runWithRetries(RetryHelper.java:50)
-#         at com.google.cloud.bigquery.BigQueryImpl.update(BigQueryImpl.java:586)
-#         at com.wepay.kafka.connect.bigquery.SchemaManager.updateSchema(SchemaManager.java:257)
-#         at com.wepay.kafka.connect.bigquery.SchemaManager.createOrUpdateTable(SchemaManager.java:207)
-#         at com.wepay.kafka.connect.bigquery.write.row.UpsertDeleteBigQueryWriter.attemptTableCreate(UpsertDeleteBigQueryWriter.java:87)
-#         ... 6 more
-# Caused by: com.google.api.client.googleapis.json.GoogleJsonResponseException: 400 Bad Request
-# POST https://www.googleapis.com/bigquery/v2/projects/vincent-de-saboulin-lab/datasets/pgrepro/tables/customer-avro?prettyPrint=false
-# {
-#   "code" : 400,
-#   "errors" : [ {
-#     "domain" : "global",
-#     "message" : "Provided Schema does not match Table vincent-de-saboulin-lab:pgrepro.customer-avro. Field KEY.KEY has changed mode from NULLABLE to REQUIRED",
-#     "reason" : "invalid"
-#   } ],
-#   "message" : "Provided Schema does not match Table vincent-de-saboulin-lab:pgrepro.customer-avro. Field KEY.KEY has changed mode from NULLABLE to REQUIRED",
-#   "status" : "INVALID_ARGUMENT"
-# }
-#         at com.google.api.client.googleapis.json.GoogleJsonResponseException.from(GoogleJsonResponseException.java:149)
-#         at com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest.newExceptionOnError(AbstractGoogleJsonClientRequest.java:112)
-#         at com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest.newExceptionOnError(AbstractGoogleJsonClientRequest.java:39)
-#         at com.google.api.client.googleapis.services.AbstractGoogleClientRequest$1.interceptResponse(AbstractGoogleClientRequest.java:443)
-#         at com.google.api.client.http.HttpRequest.execute(HttpRequest.java:1108)
-#         at com.google.api.client.googleapis.services.AbstractGoogleClientRequest.executeUnparsed(AbstractGoogleClientRequest.java:541)
-#         at com.google.api.client.googleapis.services.AbstractGoogleClientRequest.executeUnparsed(AbstractGoogleClientRequest.java:474)
-#         at com.google.api.client.googleapis.services.AbstractGoogleClientRequest.execute(AbstractGoogleClientRequest.java:591)
-#         at com.google.cloud.bigquery.spi.v2.HttpBigQueryRpc.patch(HttpBigQueryRpc.java:268)
-#         ... 15 more
-
-log "Creating GCP BigQuery Sink connector with autoCreateTables=false"
-curl -X PUT \
-     -H "Content-Type: application/json" \
-     --data '{
-               "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
-               "tasks.max" : "1",
-               "topics" : "customer-avro2",
-               "sanitizeFieldNames": "true",
-               "autoCreateTables" : "false",
-               "defaultDataset" : "'"$DATASET"'",
-               "mergeIntervalMs": "5000",
-               "bufferSize": "100000",
-               "maxWriteSize": "10000",
-               "tableWriteWait": "1000",
-               "project" : "'"$PROJECT"'",
-               "keyfile" : "/tmp/keyfile.json",
-               "deleteEnabled": "true",
-               "upsertEnabled": "true",
-               "kafkaKeyFieldName": "KEY",
-               "intermediateTableSuffix": "_intermediate",
-               "key.converter" : "io.confluent.connect.avro.AvroConverter",
-               "key.converter.schema.registry.url" : "http://schema-registry:8081"
-          }' \
-     http://localhost:8083/connectors/gcp-bigquery-sink-tombstones3/config | jq .
-
-
-log "Run the Java producer-87895-2 (only composed of tombstones)"
+log "Run the Java producer-87895-2 (only one tombstone)"
 docker exec producer-87895-2 bash -c "java -jar producer-87895-2-1.0.0-jar-with-dependencies.jar"
 
-log "Creating GCP BigQuery Sink connector with autoCreateTables=false"
-curl -X PUT \
-     -H "Content-Type: application/json" \
-     --data '{
-               "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
-               "tasks.max" : "1",
-               "topics" : "customer-avro2",
-               "sanitizeFieldNames": "true",
-               "autoCreateTables" : "false",
-               "defaultDataset" : "'"$DATASET"'",
-               "mergeIntervalMs": "5000",
-               "bufferSize": "100000",
-               "maxWriteSize": "10000",
-               "tableWriteWait": "1000",
-               "project" : "'"$PROJECT"'",
-               "keyfile" : "/tmp/keyfile.json",
-               "deleteEnabled": "true",
-               "upsertEnabled": "true",
-               "kafkaKeyFieldName": "KEY",
-               "intermediateTableSuffix": "_intermediate",
-               "key.converter" : "io.confluent.connect.avro.AvroConverter",
-               "key.converter.schema.registry.url" : "http://schema-registry:8081"
-          }' \
-     http://localhost:8083/connectors/gcp-bigquery-sink-tombstones/config | jq .
 
 
-log "Run the Java producer-87895-2 (only composed of tombstones)"
-docker exec producer-87895-2 bash -c "java -jar producer-87895-2-1.0.0-jar-with-dependencies.jar"
 
-wait_for_repro "Failed to unionize schemas of records for the table"
+log "Reset offset for customer-avro"
+
+docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group connect-gcp-bigquery-sink --describe
+docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group connect-gcp-bigquery-sink --to-offset 54 --topic customer-avro --reset-offsets --dry-run
+docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group connect-gcp-bigquery-sink --to-offset 54 --topic customer-avro --reset-offsets --execute
+docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group connect-gcp-bigquery-sink --describe
+
+
+#wait_for_repro "Failed to unionize schemas of records for the table"
 
 # Exception in thread "pool-10-thread-1" com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException: Failed to unionize schemas of records for the table GenericData{classInfo=[datasetId, projectId, tableId], {datasetId=pgrepro, tableId=customer_avro2__intermediate_0_437011d3_a297_4d6d_a2ef_13c9e2d0edf1_1642152694641}}
 # Caused by: Could not convert to BigQuery schema with a batch of tombstone records.
