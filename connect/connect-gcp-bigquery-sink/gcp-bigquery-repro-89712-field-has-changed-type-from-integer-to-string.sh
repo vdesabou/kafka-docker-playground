ef3ed2d1db63536ec798f5a5389f5c4a40ced03d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-02-03 12:13:58 +0100

    wip

---
+++ connect/connect-gcp-bigquery-sink/gcp-bigquery-repro-89712-field-has-changed-type-from-integer-to-string.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+PROJECT=${1:-vincent-de-saboulin-lab}
+
+KEYFILE="${DIR}/keyfile.json"
+if [ ! -f ${KEYFILE} ]
+then
+     logerror "ERROR: the file ${KEYFILE} file is not present!"
+     exit 1
+fi
+
+DATASET=pg${USER}ds${GITHUB_RUN_NUMBER}${TAG}
+DATASET=${DATASET//[-._]/}
+
+log "Doing gsutil authentication"
+set +e
+docker rm -f gcloud-config
+set -e
+docker run -i -v ${KEYFILE}:/tmp/keyfile.json --name gcloud-config google/cloud-sdk:latest gcloud auth activate-service-account --project ${PROJECT} --key-file /tmp/keyfile.json
+
+set +e
+log "Drop dataset $DATASET, this might fail"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" rm -r -f -d "$DATASET"
+set -e
+
+log "Create dataset $PROJECT.$DATASET"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" mk --dataset --description "used by playground" "$DATASET"
+
+
+for component in producer-repro-89712 producer-repro-89712-2
+do
+    set +e
+    log "🏗 Building jar for ${component}"
+    docker run -i --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -e TAG=$TAG_BASE -v "${DIR}/${component}":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/${component}/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn -Dkafka.tag=$TAG -Dkafka.client.tag=$KAFKA_CLIENT_TAG package > /tmp/result.log 2>&1
+    if [ $? != 0 ]
+    then
+        logerror "ERROR: failed to build java component "
+        tail -500 /tmp/result.log
+        exit 1
+    fi
+    set -e
+done
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-89712-field-has-changed-type-from-integer-to-string.yml"
+
+if version_gt $CONNECTOR_TAG "1.9.9"
+then
+     log "Creating GCP BigQuery Sink connector"
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+                    "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
+                    "tasks.max" : "1",
+                    "topics" : "customer_avro",
+                    "sanitizeTopics" : "true",
+                    "autoCreateTables" : "true",
+                    "autoUpdateSchemas" : "false",
+                    "defaultDataset" : "'"$DATASET"'",
+                    "mergeIntervalMs": "5000",
+                    "bufferSize": "100000",
+                    "maxWriteSize": "10000",
+                    "tableWriteWait": "1000",
+                    "project" : "'"$PROJECT"'",
+                    "keyfile" : "/tmp/keyfile.json"
+               }' \
+          http://localhost:8083/connectors/gcp-bigquery-sink/config | jq .
+else
+     log "Creating GCP BigQuery Sink connector"
+     curl -X PUT \
+          -H "Content-Type: application/json" \
+          --data '{
+                    "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
+                    "tasks.max" : "1",
+                    "topics" : "customer_avro,
+                    "sanitizeTopics" : "true",
+                    "autoCreateTables" : "true",
+                    "autoUpdateSchemas" : "true",
+                    "schemaRetriever" : "com.wepay.kafka.connect.bigquery.schemaregistry.schemaretriever.SchemaRegistrySchemaRetriever",
+                    "schemaRegistryLocation": "http://schema-registry:8081",
+                    "datasets" : ".*='"$DATASET"'",
+                    "mergeIntervalMs": "5000",
+                    "bufferSize": "100000",
+                    "maxWriteSize": "10000",
+                    "tableWriteWait": "1000",
+                    "project" : "'"$PROJECT"'",
+                    "keyfile" : "/tmp/keyfile.json"
+               }' \
+          http://localhost:8083/connectors/gcp-bigquery-sink/config | jq .
+fi
+
+log "✨ Run the avro java producer which produces to topic customer_avro"
+docker exec producer-repro-89712 bash -c "java -jar producer-1.0.0-jar-with-dependencies.jar"
+
+log "Sleeping 125 seconds"
+sleep 125
+
+log "Verify data is in GCP BigQuery:"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" query "SELECT * FROM $DATASET.kcbq_quickstart1;" > /tmp/result.log  2>&1
+cat /tmp/result.log
+
+log "✨ Run the avro java producer which produces to topic customer_avro"
+docker exec producer-repro-89712-2 bash -c "java -jar producer-1.0.0-jar-with-dependencies.jar"
