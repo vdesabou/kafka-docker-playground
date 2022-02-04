65fac638389814d7ec8ad0767665806ade7e2b82
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-02-04 10:07:54 +0100

    wip

---
+++ ccloud/connect-centralized-license/mqtt-source.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
+then
+     export CONNECT_CONTAINER_HOME_DIR="/home/appuser"
+else
+     export CONNECT_CONTAINER_HOME_DIR="/root"
+fi
+
+${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose.yml"
+
+if [ -f /tmp/delta_configs/env.delta ]
+then
+     source /tmp/delta_configs/env.delta
+else
+     logerror "ERROR: /tmp/delta_configs/env.delta has not been generated"
+     exit 1
+fi
+#############
+
+MQTT_TOPIC=kafka_docker_pg_mqtt$TAG
+MQTT_TOPIC=${MQTT_TOPIC//[-.]/}
+
+set +e
+delete_topic $MQTT_TOPIC
+delete_topic my_license_topic
+set -e
+
+
+log "Creating MQTT Source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.mqtt.MqttSourceConnector",
+               "tasks.max": "1",
+               "mqtt.server.uri": "tcp://mosquitto:1883",
+               "mqtt.topics":"my-mqtt-topic",
+               "kafka.topic": "'"$MQTT_TOPIC"'",
+               "mqtt.qos": "2",
+               "mqtt.username": "myuser",
+               "mqtt.password": "mypassword",
+               "topic.creation.default.replication.factor": "-1",
+               "topic.creation.default.partitions": "-1"
+          }' \
+     http://localhost:8083/connectors/mqtt-source/config | jq .
+
+sleep 5
+
+log "Send message to MQTT in my-mqtt-topic topic"
+docker exec mosquitto sh -c 'mosquitto_pub -h localhost -p 1883 -u "myuser" -P "mypassword" -t "my-mqtt-topic" -m "sample-msg-1"'
+
+sleep 10
+
+docker container logs --tail=600 connect
+
+log "Verify we have received the data in $MQTT_TOPIC topic"
+timeout 60 docker container exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e MQTT_TOPIC="$MQTT_TOPIC" connect bash -c 'kafka-console-consumer --topic $MQTT_TOPIC --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=USER_INFO --from-beginning --max-messages 1'
