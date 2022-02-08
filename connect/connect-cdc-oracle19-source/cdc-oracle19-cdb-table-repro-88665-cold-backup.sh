168705cdcdc4b91ace31aff3a60dda3b9af2bdcc
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-02-08 00:02:37 +0000

    wip

---
+++ connect/connect-cdc-oracle19-source/cdc-oracle19-cdb-table-repro-88665-cold-backup.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+create_or_get_oracle_image "LINUX.X64_193000_db_home.zip" "$(pwd)/ora-setup-scripts-cdb-table"
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.cdb-table.repro-88665-cold-backup.yml"
+
+
+# Verify Oracle DB has started within MAX_WAIT seconds
+MAX_WAIT=2500
+CUR_WAIT=0
+log "⌛ Waiting up to $MAX_WAIT seconds for Oracle DB to start"
+docker container logs oracle > /tmp/out.txt 2>&1
+while [[ ! $(cat /tmp/out.txt) =~ "DONE: Executing user defined scripts" ]]; do
+sleep 10
+docker container logs oracle > /tmp/out.txt 2>&1
+CUR_WAIT=$(( CUR_WAIT+10 ))
+if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+     logerror "ERROR: The logs in oracle container do not show 'DONE: Executing user defined scripts' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+     exit 1
+fi
+done
+log "Oracle DB has started!"
+sleep 60
+
+# Create a redo-log-topic. Please make sure you create a topic with the same name you will use for "redo.log.topic.name": "redo-log-topic"
+# CC-13104
+docker exec connect kafka-topics --create --topic redo-log-topic --bootstrap-server broker:9092 --replication-factor 1 --partitions 1 --config cleanup.policy=delete --config retention.ms=120960000
+log "redo-log-topic is created"
+sleep 5
+
+
+log "Creating Oracle source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.oracle.cdc.OracleCdcSourceConnector",
+               "tasks.max":2,
+               "key.converter": "io.confluent.connect.avro.AvroConverter",
+               "key.converter.schema.registry.url": "http://schema-registry:8081",
+               "value.converter": "io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url": "http://schema-registry:8081",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1",
+               "oracle.server": "oracle",
+               "oracle.port": 1521,
+               "oracle.sid": "ORCLCDB",
+               "oracle.username": "C##MYUSER",
+               "oracle.password": "mypassword",
+               "start.from":"snapshot",
+               "redo.log.topic.name": "redo-log-topic",
+               "redo.log.consumer.bootstrap.servers":"broker:9092",
+               "table.inclusion.regex": ".*CUSTOMERS.*",
+               "table.topic.name.template": "${databaseName}.${schemaName}.${tableName}",
+               "numeric.mapping": "best_fit",
+               "connection.pool.max.size": 20,
+               "redo.log.row.fetch.size":1,
+               "oracle.dictionary.mode": "online"
+          }' \
+     http://localhost:8083/connectors/cdc-oracle-source-cdb/config | jq .
+
+log "Waiting 60s for connector to read existing data"
+sleep 60
+
+log "Running SQL scripts"
+for script in ${DIR}/sample-sql-scripts/*.sh
+do
+     $script "ORCLCDB"
+done
+
+log "Waiting 60s for connector to read new data"
+sleep 60
+
+log "Verifying topic ORCLCDB.C__MYUSER.CUSTOMERS: there should be 13 records"
+set +e
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic ORCLCDB.C__MYUSER.CUSTOMERS --from-beginning --max-messages 13 > /tmp/result.log  2>&1
+set -e
+cat /tmp/result.log
+log "Check there is 5 snapshots events"
+if [ $(grep -c "op_type\":{\"string\":\"R\"}" /tmp/result.log) -ne 5 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+log "Check there is 3 insert events"
+if [ $(grep -c "op_type\":{\"string\":\"I\"}" /tmp/result.log) -ne 3 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+log "Check there is 4 update events"
+if [ $(grep -c "op_type\":{\"string\":\"U\"}" /tmp/result.log) -ne 4 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+log "Check there is 1 delete events"
+if [ $(grep -c "op_type\":{\"string\":\"D\"}" /tmp/result.log) -ne 1 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+
+log "Verifying topic redo-log-topic: there should be 9 records"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic redo-log-topic --from-beginning --max-messages 9
+#https://www.support.dbagenesis.com/post/oracle-database-cold-backup-recovery
+log "doing a cold backup"
+docker exec -i oracle bash -c "ORACLE_SID=ORCLCDB;export ORACLE_SID;sqlplus /nolog" << EOF
+CONNECT sys/Admin123 AS SYSDBA
+
+select name from v\$datafile;
+select member from v\$logfile; 
+select name from v\$controlfile; 
+shutdown immediate
+
+  exit;
+EOF
+
+docker exec -i oracle bash << EOF
+mkdir -p /tmp/coldbkp
+cp /opt/oracle/oradata/ORCLCDB/*.dbf /tmp/coldbkp
+cp /opt/oracle/oradata/ORCLCDB/*.log /tmp/coldbkp
+cp /opt/oracle/oradata/ORCLCDB/*.ctl /tmp/coldbkp
+cp /opt/oracle/product/19c/dbhome_1/dbs/*.ora /tmp/coldbkp
+EOF
+
+docker exec -i oracle bash -c "ORACLE_SID=ORCLCDB;export ORACLE_SID;sqlplus /nolog" << EOF
+CONNECT sys/Admin123 AS SYSDBA
+startup 
+alter database backup controlfile to trace;
+  exit;
+EOF
+
+sleep 10
+
+log "restart failed task"
+curl --request POST \
+  --url 'http://localhost:8083/connectors/cdc-oracle-source-cdb/restart?includeTasks=true&onlyFailed=true'
+
+
+sleep 10
+
+log "Running SQL scripts"
+for script in ${DIR}/sample-sql-scripts/*.sh
+do
+     $script "ORCLCDB"
+done
+
+log "Waiting 60s for connector to read new data"
+sleep 60
+
+log "Verifying topic ORCLCDB.C__MYUSER.CUSTOMERS: there should be 13 records"
+set +e
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic ORCLCDB.C__MYUSER.CUSTOMERS --from-beginning --max-messages 13 > /tmp/result.log  2>&1
+set -e
+cat /tmp/result.log
