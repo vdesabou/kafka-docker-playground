1eb665cfa0e7816e38e39a5b2483530be469be4f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-02-10 16:49:19 +0000

    wip
    Former-commit-id: f7add21ad70ff2a81b1c85e90c0cc91bba79ef89 [formerly 469d7c0590625f82714e63ddf389607d72b3c8df [formerly b016009175283b0ca79250415e7bc2785bfc596e]]
    Former-commit-id: 4321c8f027c1a6debf0c7a23fd7483928b1196ef
    Former-commit-id: 4fbcf5f28dade65547c2dbae6476d1e7321abc8e

--- connect/connect-http-sink/http_no_auth-repro-92035-http-sink-connector-and-redirects.sh
+++ connect/connect-http-sink/http_no_auth-repro-92035-http-sink-connector-and-redirects.sh
@@ -30,58 +30,9 @@ curl -X PUT \
                "reporter.error.topic.replication.factor": 1,
                "reporter.result.topic.name": "success-responses",
                "reporter.result.topic.replication.factor": 1,
-               "http.api.url": "http://http-service-no-auth-302:8080/api/messages"
+               "http.api.url": "http://http-service-no-auth-302:8080/redirectPostToPost"
           }' \
      http://localhost:8083/connectors/http-sink/config | jq .
-
-
-# 2022-02-10 14:23:18,804] ERROR [http-sink|task-0] WorkerSinkTask{id=http-sink-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted. Error: Error while processing HTTP request with Url : http://http-service-no-auth-302:8080/api/messages, Status code : 302, Reason Phrase : , Response Content : {"id":1,"message":"1"},  (org.apache.kafka.connect.runtime.WorkerSinkTask:636)
-# org.apache.kafka.connect.errors.ConnectException: Error while processing HTTP request with Url : http://http-service-no-auth-302:8080/api/messages, Status code : 302, Reason Phrase : , Response Content : {"id":1,"message":"1"}, 
-#         at io.confluent.connect.http.writer.HttpWriterImpl.handleException(HttpWriterImpl.java:399)
-#         at io.confluent.connect.http.writer.HttpWriterImpl.sendBatch(HttpWriterImpl.java:282)
-#         at io.confluent.connect.http.writer.HttpWriterImpl.write(HttpWriterImpl.java:179)
-#         at io.confluent.connect.http.HttpSinkTask.put(HttpSinkTask.java:62)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.deliverMessages(WorkerSinkTask.java:604)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.poll(WorkerSinkTask.java:334)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.iteration(WorkerSinkTask.java:235)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:204)
-#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:199)
-#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:254)
-#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
-#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
-#         at java.base/java.lang.Thread.run(Thread.java:829)
-# Caused by: Error while processing HTTP request with Url : http://http-service-no-auth-302:8080/api/messages, Status code : 302, Reason Phrase : , Response Content : {"id":1,"message":"1"}, 
-#         at io.confluent.connect.http.writer.HttpWriterImpl.executeBatchRequest(HttpWriterImpl.java:370)
-#         at io.confluent.connect.http.writer.HttpWriterImpl.executeRequestWithBackOff(HttpWriterImpl.java:303)
-#         at io.confluent.connect.http.writer.HttpWriterImpl.sendBatch(HttpWriterImpl.java:277)
-#         ... 13 more
-# [2022-02-10 14:23:18,808] ERROR [http-sink|task-0] WorkerSinkTask{id=http-sink-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:206)
-# org.apache.kafka.connect.errors.ConnectException: Exiting WorkerSinkTask due to unrecoverable exception.
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.deliverMessages(WorkerSinkTask.java:638)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.poll(WorkerSinkTask.java:334)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.iteration(WorkerSinkTask.java:235)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:204)
-#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:199)
-#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:254)
-#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
-#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
-#         at java.base/java.lang.Thread.run(Thread.java:829)
-# Caused by: org.apache.kafka.connect.errors.ConnectException: Error while processing HTTP request with Url : http://http-service-no-auth-302:8080/api/messages, Status code : 302, Reason Phrase : , Response Content : {"id":1,"message":"1"}, 
-#         at io.confluent.connect.http.writer.HttpWriterImpl.handleException(HttpWriterImpl.java:399)
-#         at io.confluent.connect.http.writer.HttpWriterImpl.sendBatch(HttpWriterImpl.java:282)
-#         at io.confluent.connect.http.writer.HttpWriterImpl.write(HttpWriterImpl.java:179)
-#         at io.confluent.connect.http.HttpSinkTask.put(HttpSinkTask.java:62)
-#         at org.apache.kafka.connect.runtime.WorkerSinkTask.deliverMessages(WorkerSinkTask.java:604)
-#         ... 10 more
-# Caused by: Error while processing HTTP request with Url : http://http-service-no-auth-302:8080/api/messages, Status code : 302, Reason Phrase : , Response Content : {"id":1,"message":"1"}, 
-#         at io.confluent.connect.http.writer.HttpWriterImpl.executeBatchRequest(HttpWriterImpl.java:370)
-#         at io.confluent.connect.http.writer.HttpWriterImpl.executeRequestWithBackOff(HttpWriterImpl.java:303)
-#         at io.confluent.connect.http.writer.HttpWriterImpl.sendBatch(HttpWriterImpl.java:277)
-#         ... 13 more
 
 sleep 10
 
