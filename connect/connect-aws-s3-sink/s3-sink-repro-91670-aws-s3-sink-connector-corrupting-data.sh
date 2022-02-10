a965e1859f85010a3db19b7790d8a5e538008b19
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-02-10 21:14:40 +0100

    WIP

---
+++ connect/connect-aws-s3-sink/s3-sink-repro-91670-aws-s3-sink-connector-corrupting-data.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f $HOME/.aws/config ]
+then
+     logerror "ERROR: $HOME/.aws/config is not set"
+     exit 1
+fi
+if [ -z "$AWS_CREDENTIALS_FILE_NAME" ]
+then
+    export AWS_CREDENTIALS_FILE_NAME="credentials"
+fi
+if [ ! -f $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME ]
+then
+     logerror "ERROR: $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME is not set"
+     exit 1
+fi
+
+if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
+then
+     export CONNECT_CONTAINER_HOME_DIR="/home/appuser"
+else
+     export CONNECT_CONTAINER_HOME_DIR="/root"
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-91670-aws-s3-sink-connector-corrupting-data.yml"
+
+AWS_BUCKET_NAME=kafka-docker-playground-bucket-${USER}${TAG}
+AWS_BUCKET_NAME=${AWS_BUCKET_NAME//[-.]/}
+
+AWS_REGION=$(aws configure get region | tr '\r' '\n')
+log "Creating bucket name <$AWS_BUCKET_NAME>, if required"
+set +e
+aws s3api create-bucket --bucket $AWS_BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+set -e
+log "Empty bucket <$AWS_BUCKET_NAME>, if required"
+set +e
+aws s3 rm s3://$AWS_BUCKET_NAME --recursive --region $AWS_REGION
+set -e
+
+# Using a key
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic a-topic --property key.schema='{"type":"record","namespace": "io.confluent.connect.avro","name":"myrecordkey","fields":[{"name":"ID","type":"long"}]}' --property value.schema='{"type":"record","name":"myrecordvalue","fields":[{"name":"ID","type":"long"},{"name":"product", "type": "string"}, {"name":"quantity", "type": "int"}, {"name":"price",
+"type": "float"}]}'  --property parse.key=true --property key.separator="|" << EOF
+{"ID": 111}|{"ID": 111,"product": "foo", "quantity": 100, "price": 50}
+{"ID": 222}|{"ID": 222,"product": "bar", "quantity": 100, "price": 50}
+EOF
+
+log "Creating S3 Sink connector with bucket name <$AWS_BUCKET_NAME>"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.s3.S3SinkConnector",
+               "tasks.max": "1",
+               "topics": "a-topic",
+               "s3.region": "'"$AWS_REGION"'",
+               "s3.bucket.name": "'"$AWS_BUCKET_NAME"'",
+               "s3.part.size": 52428801,
+               "flush.size": "3",
+               "storage.class": "io.confluent.connect.s3.storage.S3Storage",
+               "format.class": "io.confluent.connect.s3.format.json.JsonFormat",
+               "schema.compatibility": "NONE",
+               "key.converter": "io.confluent.connect.avro.AvroConverter",
+               "key.converter.schema.registry.url": "http://schema-registry:8081",
+               "value.converter": "io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url": "http://schema-registry:8081"
+          }' \
+     http://localhost:8083/connectors/s3-sink/config | jq .
+
+sleep 10
+
+log "Listing objects of in S3"
+aws s3api list-objects --bucket "$AWS_BUCKET_NAME"
+
+log "Getting one of the avro files locally and displaying content with avro-tools"
+aws s3 cp --only-show-errors s3://$AWS_BUCKET_NAME/topics/a-topic/partition=0/a-topic+0+0000000000.json a-topic+0+0000000000.json
+
+
+cat a-topic+0+0000000000.json
+
+# {"ID":111,"product":"foo","quantity":100,"price":50.0}
+# {"ID":222,"product":"bar","quantity":100,"price":50.0}
+# {"ID":111,"product":"foo","quantity":100,"price":50.0}
