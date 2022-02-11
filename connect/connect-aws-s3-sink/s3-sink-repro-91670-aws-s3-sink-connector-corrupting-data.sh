d41eea1b08c6613ca71f58e74b4140945687b0c1
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-02-11 09:29:59 +0100

    example with bytes

--- connect/connect-aws-s3-sink/s3-sink-repro-91670-aws-s3-sink-connector-corrupting-data.sh
+++ connect/connect-aws-s3-sink/s3-sink-repro-91670-aws-s3-sink-connector-corrupting-data.sh
@@ -4,6 +4,20 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+for component in producer-repro-91670
+do
+    set +e
+    log "🏗 Building jar for ${component}"
+    docker run -i --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -e TAG=$TAG_BASE -v "${DIR}/${component}":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "${DIR}/${component}/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn -Dkafka.tag=$TAG -Dkafka.client.tag=$KAFKA_CLIENT_TAG package > /tmp/result.log 2>&1
+    if [ $? != 0 ]
+    then
+        logerror "ERROR: failed to build java component "
+        tail -500 /tmp/result.log
+        exit 1
+    fi
+    set -e
+done
+
 if [ ! -f $HOME/.aws/config ]
 then
      logerror "ERROR: $HOME/.aws/config is not set"
@@ -41,12 +55,8 @@ set +e
 aws s3 rm s3://$AWS_BUCKET_NAME --recursive --region $AWS_REGION
 set -e
 
-# Using a key
-docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic a-topic --property key.schema='{"type":"record","namespace": "io.confluent.connect.avro","name":"myrecordkey","fields":[{"name":"ID","type":"long"}]}' --property value.schema='{"type":"record","name":"myrecordvalue","fields":[{"name":"ID","type":"long"},{"name":"product", "type": "string"}, {"name":"quantity", "type": "int"}, {"name":"price",
-"type": "float"}]}'  --property parse.key=true --property key.separator="|" << EOF
-{"ID": 111}|{"ID": 111,"product": "foo", "quantity": 100, "price": 50}
-{"ID": 222}|{"ID": 222,"product": "bar", "quantity": 100, "price": 50}
-EOF
+log "✨ Run the avro java producer which produces to topic customer_avro"
+docker exec producer-repro-91670 bash -c "java -jar producer-1.0.0-jar-with-dependencies.jar"
 
 log "Creating S3 Sink connector with bucket name <$AWS_BUCKET_NAME>"
 curl -X PUT \
@@ -54,16 +64,14 @@ curl -X PUT \
      --data '{
                "connector.class": "io.confluent.connect.s3.S3SinkConnector",
                "tasks.max": "1",
-               "topics": "a-topic",
+               "topics": "customer_avro",
                "s3.region": "'"$AWS_REGION"'",
                "s3.bucket.name": "'"$AWS_BUCKET_NAME"'",
                "s3.part.size": 52428801,
-               "flush.size": "3",
+               "flush.size": "10",
                "storage.class": "io.confluent.connect.s3.storage.S3Storage",
                "format.class": "io.confluent.connect.s3.format.json.JsonFormat",
                "schema.compatibility": "NONE",
-               "key.converter": "io.confluent.connect.avro.AvroConverter",
-               "key.converter.schema.registry.url": "http://schema-registry:8081",
                "value.converter": "io.confluent.connect.avro.AvroConverter",
                "value.converter.schema.registry.url": "http://schema-registry:8081"
           }' \
@@ -75,11 +83,18 @@ log "Listing objects of in S3"
 aws s3api list-objects --bucket "$AWS_BUCKET_NAME"
 
 log "Getting one of the avro files locally and displaying content with avro-tools"
-aws s3 cp --only-show-errors s3://$AWS_BUCKET_NAME/topics/a-topic/partition=0/a-topic+0+0000000000.json a-topic+0+0000000000.json
+aws s3 cp --only-show-errors s3://$AWS_BUCKET_NAME/topics/customer_avro/partition=0/customer_avro+0+0000000000.json customer_avro+0+0000000000.json
 
 
-cat a-topic+0+0000000000.json
+cat customer_avro+0+0000000000.json
 
-# {"ID":111,"product":"foo","quantity":100,"price":50.0}
-# {"ID":222,"product":"bar","quantity":100,"price":50.0}
-# {"ID":111,"product":"foo","quantity":100,"price":50.0}
+# {"count":0,"first_name":"cc","last_name":"ccc","address":"AAAAAAAAAAAA"}
+# {"count":1,"first_name":"cc","last_name":"ccc","address":"AAAAAAAAAAAA"}
+# {"count":2,"first_name":"cc","last_name":"ccc","address":"AAAAAAAAAAAA"}
+# {"count":3,"first_name":"cc","last_name":"ccc","address":"AAAAAAAAAAAA"}
+# {"count":4,"first_name":"cc","last_name":"ccc","address":"AAAAAAAAAAAA"}
+# {"count":5,"first_name":"cc","last_name":"ccc","address":"AAAAAAAAAAAA"}
+# {"count":6,"first_name":"cc","last_name":"ccc","address":"AAAAAAAAAAAA"}
+# {"count":7,"first_name":"cc","last_name":"ccc","address":"AAAAAAAAAAAA"}
+# {"count":8,"first_name":"cc","last_name":"ccc","address":"AAAAAAAAAAAA"}
+# {"count":9,"first_name":"cc","last_name":"ccc","address":"AAAAAAAAAAAA"}
