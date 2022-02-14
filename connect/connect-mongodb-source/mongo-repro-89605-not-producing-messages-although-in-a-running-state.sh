ed0e7f054e081ce3571c5b824323e84852048bbc
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-02-14 09:21:59 +0000

    wip
    Former-commit-id: 88d7e9481f60c14c7f01a489e6e519647b37474f [formerly 2c954617fcd0e023a6ba21004f409c66eb48c36b [formerly d733dac596c11317c4e9472cbe74f07d4c3bbb26]]
    Former-commit-id: 7ae56c57c1dc0e17662f833d7492a36b3792246a
    Former-commit-id: 4717aa9d22b9ef32ff1c12c0fca184ef20a5efc7

--- connect/connect-mongodb-source/mongo-repro-89605-not-producing-messages-although-in-a-running-state.sh
+++ connect/connect-mongodb-source/mongo-repro-89605-not-producing-messages-although-in-a-running-state.sh
@@ -6,6 +6,14 @@ source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-89605-not-producing-messages-although-in-a-running-state.yml"
 
+curl --request PUT \
+  --url http://localhost:8083/admin/loggers/com.mongodb.kafka.connect \
+  --header 'Accept: application/json' \
+  --header 'Content-Type: application/json' \
+  --data '{
+	"level": "TRACE"
+}'
+
 log "Initialize MongoDB replica set"
 docker exec -i mongodb mongo --eval 'rs.initiate({_id: "myuser", members:[{_id: 0, host: "mongodb:27017"}]})'
 
@@ -39,19 +47,52 @@ curl -X PUT \
                     "topic.prefix":"mongo",
                     "offset.partition.name": "my-offset-partition-name-smt",
                     "heartbeat.interval.ms": "3000",
-                    "transforms": "filterHeartbeats",
-                    "transforms.filterHeartbeats.type": "org.apache.kafka.connect.transforms.Filter",
-                    "transforms.filterHeartbeats.predicate": "isHeartbeat",
                     "predicates": "isHeartbeat",
                     "predicates.isHeartbeat.pattern": "__mongodb_heartbeats",
-                    "predicates.isHeartbeat.type": "org.apache.kafka.connect.transforms.predicates.TopicNameMatches"
+                    "predicates.isHeartbeat.type": "org.apache.kafka.connect.transforms.predicates.TopicNameMatches",
+                    "transforms": "documentKeyToKey,flattenKey,extractIdKey",
+                    "transforms.documentKeyToKey.fields": "documentKey",
+                    "transforms.documentKeyToKey.type": "org.apache.kafka.connect.transforms.ValueToKey",
+                    "transforms.documentKeyToKey.predicate": "isHeartbeat",
+                    "transforms.documentKeyToKey.negate": "true",
+                    "transforms.flattenKey.delimiter": ".",
+                    "transforms.flattenKey.type": "org.apache.kafka.connect.transforms.Flatten$Key",
+                    "transforms.flattenKey.predicate": "isHeartbeat",
+                    "transforms.flattenKey.negate": "true",
+                    "transforms.extractIdKey.field": "documentKey._id",
+                    "transforms.extractIdKey.type": "org.apache.kafka.connect.transforms.ExtractField$Key",
+                    "transforms.extractIdKey.predicate": "isHeartbeat",
+                    "transforms.extractIdKey.negate": "true",
+                    "output.format.key": "schema",
+                    "output.format.value": "schema",
+                    "output.schema.infer.key": "true"
           }' \
-     http://localhost:8083/connectors/mongodb-source-smt/config | jq .
-
+     http://localhost:8083/connectors/mongodb-source-smt2/config | jq .
 
 sleep 5
 
 
+# [2022-02-14 08:37:05,366] ERROR [mongodb-source-smt|task-0] WorkerSourceTask{id=mongodb-source-smt-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:206)
+# org.apache.kafka.connect.errors.ConnectException: Tolerance exceeded in error handler
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndHandleError(RetryWithToleranceOperator.java:220)
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execute(RetryWithToleranceOperator.java:142)
+#         at org.apache.kafka.connect.runtime.TransformationChain.transformRecord(TransformationChain.java:70)
+#         at org.apache.kafka.connect.runtime.TransformationChain.apply(TransformationChain.java:50)
+#         at org.apache.kafka.connect.runtime.WorkerSourceTask.sendRecords(WorkerSourceTask.java:358)
+#         at org.apache.kafka.connect.runtime.WorkerSourceTask.execute(WorkerSourceTask.java:272)
+#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:199)
+#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:254)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:829)
+# Caused by: java.lang.IllegalArgumentException: Unknown field: documentKey._id
+#         at org.apache.kafka.connect.transforms.ExtractField.apply(ExtractField.java:65)
+#         at org.apache.kafka.connect.runtime.TransformationChain.lambda$transformRecord$0(TransformationChain.java:70)
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndRetry(RetryWithToleranceOperator.java:166)
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndHandleError(RetryWithToleranceOperator.java:200)
+#         ... 12 more
 
 log "Insert a record"
 docker exec -i mongodb mongo << EOF
