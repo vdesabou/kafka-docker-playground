65e3b6dc3ba30e69dcf1fcd29b053f23bde17e14
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-02-24 11:15:05 +0100

    https://serverfault.com/a/1093928
    Former-commit-id: ae0f4cdd8e2de248dbbae2e4a1e4ae96791e0eba [formerly 087867fa60cce3883da2dfbbc260fee809f11a77]
    Former-commit-id: 930838c2551bce8ee91da852514b17ff2490f14b
    Former-commit-id: 54646000ee4ef25cf2c3e201f430dff673b2ab48

--- connect/connect-hdfs2-sink/ha-kerberos/installer.sh
+++ connect/connect-hdfs2-sink/ha-kerberos/installer.sh
@@ -6,6 +6,8 @@ for INSTALLER in $(env | grep INSTALLER_); do
         cd /etc/yum.repos.d/
         sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
         sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+        # https://serverfault.com/a/1093928
+        sudo sed -i 's|baseurl=http://vault.centos.org|baseurl=http://vault.epel.cloud|g' /etc/yum.repos.d/CentOS-Linux-*
         # Centos 8 is EOL END
         sudo yum install -y rsync
         cd /
