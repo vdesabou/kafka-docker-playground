82796339872eaf5875999108e86fa2725659957e
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-03-03 14:30:53 +0100

    Add fully managed snowflake sink connector example #2183
    Former-commit-id: a9f963336c190fbd1b34c54c508ac22e5ac19d8b [formerly a6c1de5be4d769d9ce4180545eabe14dc4ce97ef]
    Former-commit-id: bd55cc2683cfa0a7d40d74df934cf5c096345ef3
    Former-commit-id: 03c0a5154e0612f1c807f3adfb822903da6261ba

---
+++ ccloud/fully-managed-connect-snowflake-sink/fully-managed-snowflake-sink.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -z "$CI" ]
+then
+     # running with github actions
+     if [ ! -f ../../secrets.properties ]
+     then
+          logerror "../../secrets.properties is not present!"
+          exit 1
+     fi
+     source ../../secrets.properties > /dev/null 2>&1
+fi
+
+PLAYGROUND_DB=PLAYGROUND_DB${TAG}
+PLAYGROUND_DB=${PLAYGROUND_DB//[-._]/}
+
+PLAYGROUND_WAREHOUSE=PLAYGROUND_WAREHOUSE${TAG}
+PLAYGROUND_WAREHOUSE=${PLAYGROUND_WAREHOUSE//[-._]/}
+
+PLAYGROUND_CONNECTOR_ROLE=PLAYGROUND_CONNECTOR_ROLE${TAG}
+PLAYGROUND_CONNECTOR_ROLE=${PLAYGROUND_CONNECTOR_ROLE//[-._]/}
+
+PLAYGROUND_USER=PLAYGROUND_USER${TAG}
+PLAYGROUND_USER=${PLAYGROUND_USER//[-._]/}
+
+SNOWFLAKE_ACCOUNT_NAME=${SNOWFLAKE_ACCOUNT_NAME:-$1}
+SNOWFLAKE_USERNAME=${SNOWFLAKE_USERNAME:-$2}
+SNOWFLAKE_PASSWORD=${SNOWFLAKE_PASSWORD:-$3}
+
+if [ -z "$SNOWFLAKE_ACCOUNT_NAME" ]
+then
+     logerror "SNOWFLAKE_ACCOUNT_NAME is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SNOWFLAKE_USERNAME" ]
+then
+     logerror "SNOWFLAKE_USERNAME is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SNOWFLAKE_PASSWORD" ]
+then
+     logerror "SNOWFLAKE_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+bootstrap_ccloud_environment
+
+if [ -f /tmp/delta_configs/env.delta ]
+then
+     source /tmp/delta_configs/env.delta
+else
+     logerror "ERROR: /tmp/delta_configs/env.delta has not been generated"
+     exit 1
+fi
+
+# https://<account_name>.<region_id>.snowflakecomputing.com:443
+SNOWFLAKE_URL="https://$SNOWFLAKE_ACCOUNT_NAME.snowflakecomputing.com"
+
+# Create encrypted Private key - keep this safe, do not share!
+openssl genrsa 2048 | openssl pkcs8 -topk8 -v2 aes256 -inform PEM -out snowflake_key.p8 -passout pass:confluent
+# Generate public key from private key. You can share your public key.
+openssl rsa -in snowflake_key.p8  -pubout -out snowflake_key.pub -passin pass:confluent
+
+
+RSA_PUBLIC_KEY=$(grep -v "BEGIN PUBLIC" snowflake_key.pub | grep -v "END PUBLIC"|tr -d '\n')
+RSA_PRIVATE_KEY=$(grep -v "BEGIN ENCRYPTED PRIVATE KEY" snowflake_key.p8 | grep -v "END ENCRYPTED PRIVATE KEY"|tr -d '\n')
+
+
+log "Create a Snowflake DB"
+docker run --rm -i -e SNOWSQL_PWD="$SNOWFLAKE_PASSWORD" -e RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY" kurron/snowsql --username $SNOWFLAKE_USERNAME -a $SNOWFLAKE_ACCOUNT_NAME << EOF
+DROP DATABASE IF EXISTS $PLAYGROUND_DB;
+CREATE OR REPLACE DATABASE $PLAYGROUND_DB COMMENT = 'Database for Docker Playground';
+EOF
+
+log "Create a Snowflake ROLE"
+docker run --rm -i -e SNOWSQL_PWD="$SNOWFLAKE_PASSWORD" -e RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY" kurron/snowsql --username $SNOWFLAKE_USERNAME -a $SNOWFLAKE_ACCOUNT_NAME << EOF
+USE ROLE SECURITYADMIN;
+DROP ROLE IF EXISTS $PLAYGROUND_CONNECTOR_ROLE;
+CREATE ROLE $PLAYGROUND_CONNECTOR_ROLE;
+GRANT USAGE ON DATABASE $PLAYGROUND_DB TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+GRANT USAGE ON DATABASE $PLAYGROUND_DB TO ACCOUNTADMIN;
+GRANT USAGE ON SCHEMA $PLAYGROUND_DB.PUBLIC TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+GRANT ALL ON FUTURE TABLES IN SCHEMA $PLAYGROUND_DB.PUBLIC TO $PLAYGROUND_CONNECTOR_ROLE;
+GRANT USAGE ON SCHEMA $PLAYGROUND_DB.PUBLIC TO ROLE ACCOUNTADMIN;
+GRANT CREATE TABLE ON SCHEMA $PLAYGROUND_DB.PUBLIC TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+GRANT CREATE STAGE ON SCHEMA $PLAYGROUND_DB.PUBLIC TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+GRANT CREATE PIPE ON SCHEMA $PLAYGROUND_DB.PUBLIC TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+EOF
+
+log "Create a Snowflake WAREHOUSE (for admin purpose as KafkaConnect is Serverless)"
+docker run --rm -i -e SNOWSQL_PWD="$SNOWFLAKE_PASSWORD" -e RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY" kurron/snowsql --username $SNOWFLAKE_USERNAME -a $SNOWFLAKE_ACCOUNT_NAME << EOF
+USE ROLE SYSADMIN;
+CREATE OR REPLACE WAREHOUSE $PLAYGROUND_WAREHOUSE
+  WAREHOUSE_SIZE = 'XSMALL'
+  WAREHOUSE_TYPE = 'STANDARD'
+  AUTO_SUSPEND = 60
+  AUTO_RESUME = TRUE
+  MIN_CLUSTER_COUNT = 1
+  MAX_CLUSTER_COUNT = 1
+  SCALING_POLICY = 'STANDARD'
+  INITIALLY_SUSPENDED = TRUE
+  COMMENT = 'Warehouse for Kafka Playground';
+GRANT USAGE ON WAREHOUSE $PLAYGROUND_WAREHOUSE TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+EOF
+
+log "Create a Snowflake USER"
+docker run --rm -i -e SNOWSQL_PWD="$SNOWFLAKE_PASSWORD" -e RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY" kurron/snowsql --username $SNOWFLAKE_USERNAME -a $SNOWFLAKE_ACCOUNT_NAME << EOF
+USE ROLE USERADMIN;
+DROP USER IF EXISTS $PLAYGROUND_USER;
+CREATE USER $PLAYGROUND_USER
+ PASSWORD = 'Password123!'
+ LOGIN_NAME = $PLAYGROUND_USER
+ DISPLAY_NAME = $PLAYGROUND_USER
+ DEFAULT_WAREHOUSE = $PLAYGROUND_WAREHOUSE
+ DEFAULT_ROLE = $PLAYGROUND_CONNECTOR_ROLE
+ DEFAULT_NAMESPACE = $PLAYGROUND_DB
+ MUST_CHANGE_PASSWORD = FALSE
+ RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY";
+USE ROLE SECURITYADMIN;
+GRANT ROLE $PLAYGROUND_CONNECTOR_ROLE TO USER $PLAYGROUND_USER;
+EOF
+
+
+log "Sending messages to topic test_table"
+docker run --rm -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" vdesabou/kafka-docker-playground-connect:${CONNECT_TAG}  kafka-avro-console-producer --broker-list $BOOTSTRAP_SERVERS --producer-property ssl.endpoint.identification.algorithm=https --producer-property sasl.mechanism=PLAIN --producer-property security.protocol=SASL_SSL --producer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=USER_INFO --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --topic test_table --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"u_name","type":"string"},
+{"name":"u_price", "type": "float"}, {"name":"u_quantity", "type": "int"}]}' << EOF
+{"u_name": "scissors", "u_price": 2.75, "u_quantity": 3}
+{"u_name": "tape", "u_price": 0.99, "u_quantity": 10}
+{"u_name": "notebooks", "u_price": 1.99, "u_quantity": 5}
+EOF
+
+cat << EOF > connector.json
+{
+     "connector.class": "SnowflakeSink",
+     "name": "SnowflakeSink",
+     "kafka.auth.mode": "KAFKA_API_KEY",
+     "kafka.api.key": "$CLOUD_KEY",
+     "kafka.api.secret": "$CLOUD_SECRET",
+     "input.data.format": "AVRO",
+     "topics": "test_table",
+     "snowflake.url.name": "$SNOWFLAKE_URL",
+     "snowflake.user.name": "$PLAYGROUND_USER",
+     "snowflake.user.role": "$PLAYGROUND_CONNECTOR_ROLE",
+     "snowflake.private.key":"$RSA_PRIVATE_KEY",
+     "snowflake.private.key.passphrase": "confluent",
+     "snowflake.database.name": "$PLAYGROUND_DB",
+     "snowflake.schema.name":"PUBLIC",
+     "tasks.max" : "1"
+}
+EOF
+
+log "Connector configuration is:"
+cat connector.json
+
+set +e
+log "Deleting fully managed connector, it might fail..."
+delete_ccloud_connector connector.json
+set -e
+
+log "Creating fully managed connector"
+create_ccloud_connector connector.json
+wait_for_ccloud_connector_up connector.json 300
+
+sleep 5
+
+sleep 120
+
+log "Confirm that the messages were delivered to the Snowflake table (logged as $PLAYGROUND_USER user)"
+docker run --rm -i -e SNOWSQL_PWD='Password123!' -e RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY" kurron/snowsql --username $PLAYGROUND_USER -a $SNOWFLAKE_ACCOUNT_NAME > /tmp/result.log  2>&1 <<-EOF
+USE ROLE $PLAYGROUND_CONNECTOR_ROLE;
+USE DATABASE $PLAYGROUND_DB;
+USE SCHEMA PUBLIC;
+USE WAREHOUSE $PLAYGROUND_WAREHOUSE;
+SELECT * FROM $PLAYGROUND_DB.PUBLIC.TEST_TABLE;
+EOF
+cat /tmp/result.log
+grep "scissors" /tmp/result.log
+
+log "Do you want to delete the fully managed connector ?"
+check_if_continue
+
+log "Deleting fully managed connector"
+delete_ccloud_connector connector.json
