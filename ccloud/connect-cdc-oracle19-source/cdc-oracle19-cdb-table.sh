575e5cae918a680248f648c35f043aee9056f85f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-03-03 09:56:07 +0100

    WIP
    Former-commit-id: e09ae316f699b6107b499c3f33988f15ef8a0568 [formerly 9a343b0d7821070729c22b4d9feae730ab4d26c7]
    Former-commit-id: e6a7fba3dbb92d1f2c46e610568ca2989a4b753c
    Former-commit-id: 73efef5ec33a6e057e3dc7c4a9dc51641c1d88cc

---
+++ ccloud/connect-cdc-oracle19-source/cdc-oracle19-cdb-table.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+create_or_get_oracle_image "LINUX.X64_193000_db_home.zip" "$(pwd)/ora-setup-scripts-cdb-table"
+
+${DIR}/../../ccloud/environment/start.sh "${PWD}/docker-compose.plaintext.cdb-table.yml"
+
+if [ -f /tmp/delta_configs/env.delta ]
+then
+     source /tmp/delta_configs/env.delta
+else
+     logerror "ERROR: /tmp/delta_configs/env.delta has not been generated"
+     exit 1
+fi
+#############
+
+# Verify Oracle DB has started within MAX_WAIT seconds
+MAX_WAIT=2500
+CUR_WAIT=0
+log "⌛ Waiting up to $MAX_WAIT seconds for Oracle DB to start"
+docker container logs oracle > /tmp/out.txt 2>&1
+while [[ ! $(cat /tmp/out.txt) =~ "DONE: Executing user defined scripts" ]]; do
+sleep 10
+docker container logs oracle > /tmp/out.txt 2>&1
+CUR_WAIT=$(( CUR_WAIT+10 ))
+if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+     logerror "ERROR: The logs in oracle container do not show 'DONE: Executing user defined scripts' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+     exit 1
+fi
+done
+log "Oracle DB has started!"
+sleep 60
+
+
+log "Creating Oracle source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.oracle.cdc.OracleCdcSourceConnector",
+               "tasks.max":2,
+               "key.converter" : "io.confluent.connect.avro.AvroConverter",
+               "key.converter.schema.registry.url": "'"$SCHEMA_REGISTRY_URL"'",
+               "key.converter.basic.auth.user.info": "${file:/data:schema.registry.basic.auth.user.info}",
+               "key.converter.basic.auth.credentials.source": "USER_INFO",
+               "value.converter" : "io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url": "'"$SCHEMA_REGISTRY_URL"'",
+               "value.converter.basic.auth.user.info": "${file:/data:schema.registry.basic.auth.user.info}",
+               "value.converter.basic.auth.credentials.source": "USER_INFO",
+
+               "confluent.topic.ssl.endpoint.identification.algorithm" : "https",
+               "confluent.topic.sasl.mechanism" : "PLAIN",
+               "confluent.topic.bootstrap.servers": "${file:/data:bootstrap.servers}",
+               "confluent.topic.sasl.jaas.config" : "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${file:/data:sasl.username}\" password=\"${file:/data:sasl.password}\";",
+               "confluent.topic.security.protocol" : "SASL_SSL",
+               "confluent.topic.replication.factor": "3",
+
+               "topic.creation.groups":"redo",
+               "topic.creation.redo.include":"redo-log-topic",
+               "topic.creation.redo.replication.factor":3,
+               "topic.creation.redo.partitions":1,
+               "topic.creation.redo.cleanup.policy":"delete",
+               "topic.creation.redo.retention.ms":1209600000,
+               "topic.creation.default.replication.factor":3,
+               "topic.creation.default.partitions":3,
+               "topic.creation.default.cleanup.policy":"compact",
+
+               "oracle.server": "oracle",
+               "oracle.port": 1521,
+               "oracle.sid": "ORCLCDB",
+               "oracle.username": "C##MYUSER",
+               "oracle.password": "mypassword",
+               "start.from":"snapshot",
+
+               "redo.log.topic.name": "redo-log-topic",
+               "redo.log.consumer.bootstrap.servers": "${file:/data:bootstrap.servers}",
+               "redo.log.consumer.sasl.jaas.config": "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${file:/data:sasl.username}\" password=\"${file:/data:sasl.password}\";",
+               "redo.log.consumer.security.protocol":"SASL_SSL",
+               "redo.log.consumer.sasl.mechanism":"PLAIN",
+
+               "table.inclusion.regex": ".*CUSTOMERS.*",
+               "table.topic.name.template": "${databaseName}.${schemaName}.${tableName}",
+               "numeric.mapping": "best_fit",
+               "connection.pool.max.size": 20,
+               "redo.log.row.fetch.size":1,
+               "oracle.dictionary.mode": "auto"
+          }' \
+     http://localhost:8083/connectors/cdc-oracle-source-cdb-cloud/config | jq .
+
+log "Waiting 60s for connector to read existing data"
+sleep 60
+
+log "Running SQL scripts"
+for script in ${DIR}/sample-sql-scripts/*.sh
+do
+     $script "ORCLCDB"
+done
+
+log "Waiting 60s for connector to read new data"
+sleep 60
+
+log "Verifying topic ORCLCDB.C__MYUSER.CUSTOMERS: there should be 13 records"
+set +e
+timeout 60 docker exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" connect bash -c 'kafka-avro-console-consumer --topic ORCLCDB.C__MYUSER.CUSTOMERS --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=USER_INFO --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --from-beginning --max-messages 13' > /tmp/result.log  2>&1
+set -e
+cat /tmp/result.log
+log "Check there is 5 snapshots events"
+if [ $(grep -c "op_type\":{\"string\":\"R\"}" /tmp/result.log) -ne 5 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+log "Check there is 3 insert events"
+if [ $(grep -c "op_type\":{\"string\":\"I\"}" /tmp/result.log) -ne 3 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+log "Check there is 4 update events"
+if [ $(grep -c "op_type\":{\"string\":\"U\"}" /tmp/result.log) -ne 4 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+log "Check there is 1 delete events"
+if [ $(grep -c "op_type\":{\"string\":\"D\"}" /tmp/result.log) -ne 1 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+
+log "Verifying topic redo-log-topic: there should be 9 records"
+timeout 60 docker exec -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" connect bash -c 'kafka-avro-console-consumer --topic redo-log-topic --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=USER_INFO --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --from-beginning --max-messages 9'
+
+log "🚚 If you're planning to inject more data, have a look at https://github.com/vdesabou/kafka-docker-playground/blob/master/connect/connect-cdc-oracle19-source/README.md#note-on-redologrowfetchsize"
---
+++ ccloud/connect-cdc-oracle19-source/cdc-oracle19-pdb-table.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -z "$GITHUB_RUN_NUMBER" ]
+then
+     # running with github actions
+     remove_cdb_oracle_image "LINUX.X64_193000_db_home.zip" "$(pwd)/ora-setup-scripts-cdb-table"
+fi
+
+create_or_get_oracle_image "LINUX.X64_193000_db_home.zip" "$(pwd)/ora-setup-scripts-pdb-table"
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.pdb-table.yml"
+
+
+# Verify Oracle DB has started within MAX_WAIT seconds
+MAX_WAIT=2500
+CUR_WAIT=0
+log "⌛ Waiting up to $MAX_WAIT seconds for Oracle DB to start"
+docker container logs oracle > /tmp/out.txt 2>&1
+while [[ ! $(cat /tmp/out.txt) =~ "DONE: Executing user defined scripts" ]]; do
+sleep 10
+docker container logs oracle > /tmp/out.txt 2>&1
+CUR_WAIT=$(( CUR_WAIT+10 ))
+if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+     logerror "ERROR: The logs in oracle container do not show 'DONE: Executing user defined scripts' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+     exit 1
+fi
+done
+log "Oracle DB has started!"
+sleep 60
+
+log "Grant select on CUSTOMERS table"
+docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLPDB1 << EOF
+     ALTER SESSION SET CONTAINER=ORCLPDB1;
+     GRANT select on CUSTOMERS TO C##MYUSER;
+EOF
+
+# Create a redo-log-topic. Please make sure you create a topic with the same name you will use for "redo.log.topic.name": "redo-log-topic"
+# CC-13104
+docker exec connect kafka-topics --create --topic redo-log-topic --bootstrap-server broker:9092 --replication-factor 1 --partitions 1 --config cleanup.policy=delete --config retention.ms=120960000
+log "redo-log-topic is created"
+sleep 5
+
+log "Creating Oracle source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.oracle.cdc.OracleCdcSourceConnector",
+               "tasks.max":2,
+               "key.converter": "io.confluent.connect.avro.AvroConverter",
+               "key.converter.schema.registry.url": "http://schema-registry:8081",
+               "value.converter": "io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url": "http://schema-registry:8081",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1",
+               "oracle.server": "oracle",
+               "oracle.port": 1521,
+               "oracle.sid": "ORCLCDB",
+               "oracle.pdb.name": "ORCLPDB1",
+               "oracle.username": "C##MYUSER",
+               "oracle.password": "mypassword",
+               "start.from":"snapshot",
+               "redo.log.topic.name": "redo-log-topic",
+               "redo.log.consumer.bootstrap.servers":"broker:9092",
+               "table.inclusion.regex": "ORCLPDB1[.].*[.]CUSTOMERS",
+               "table.topic.name.template": "${databaseName}.${schemaName}.${tableName}",
+               "numeric.mapping": "best_fit",
+               "connection.pool.max.size": 20,
+               "redo.log.row.fetch.size":1,
+               "oracle.dictionary.mode": "auto"
+          }' \
+     http://localhost:8083/connectors/cdc-oracle-source-pdb/config | jq .
+
+log "Waiting 60s for connector to read existing data"
+sleep 60
+
+log "Running SQL scripts"
+for script in ${DIR}/sample-sql-scripts/*.sh
+do
+     $script "ORCLPDB1"
+done
+
+log "Waiting 60s for connector to read new data"
+sleep 60
+
+log "Verifying topic ORCLPDB1.C__MYUSER.CUSTOMERS: there should be 13 records"
+set +e
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic ORCLPDB1.C__MYUSER.CUSTOMERS --from-beginning --max-messages 13 > /tmp/result.log  2>&1
+set -e
+cat /tmp/result.log
+log "Check there is 5 snapshots events"
+if [ $(grep -c "op_type\":{\"string\":\"R\"}" /tmp/result.log) -ne 5 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+log "Check there is 3 insert events"
+if [ $(grep -c "op_type\":{\"string\":\"I\"}" /tmp/result.log) -ne 3 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+log "Check there is 4 update events"
+if [ $(grep -c "op_type\":{\"string\":\"U\"}" /tmp/result.log) -ne 4 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+log "Check there is 1 delete events"
+if [ $(grep -c "op_type\":{\"string\":\"D\"}" /tmp/result.log) -ne 1 ]
+then
+     logerror "Did not get expected results"
+     exit 1
+fi
+
+log "Verifying topic redo-log-topic: there should be 9 records"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic redo-log-topic --from-beginning --max-messages 9
+
+log "🚚 If you're planning to inject more data, have a look at https://github.com/vdesabou/kafka-docker-playground/blob/master/connect/connect-cdc-oracle19-source/README.md#note-on-redologrowfetchsize"
+
