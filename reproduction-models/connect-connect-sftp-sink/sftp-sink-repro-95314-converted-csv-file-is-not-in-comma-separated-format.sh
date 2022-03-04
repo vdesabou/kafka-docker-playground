5348af809273688342bbbd80d7f05984dcf795b3
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-03-04 10:43:30 +0000

    repro
    Former-commit-id: a36f63dd7b9839e292af113c6c34bdc949bab1d6 [formerly 9ed9dee5808b133b9b7a1b8e1cd8506b23265f4e]
    Former-commit-id: 51e41de4e24a2c73358a4c4fba6c4a917f1c8bcb
    Former-commit-id: 3cad5ee03b19b55a6e363d2ef2df84f291f6a847

---
+++ reproduction-models/connect-connect-sftp-sink/sftp-sink-repro-95314-converted-csv-file-is-not-in-comma-separated-format.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-95314-converted-csv-file-is-not-in-comma-separated-format.yml"
+
+log "Creating SFTP Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "topics": "test_sftp_sink",
+               "tasks.max": "1",
+               "connector.class": "io.confluent.connect.sftp.SftpSinkConnector",
+               "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
+               "schema.generator.class": "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator",
+               "flush.size": "3",
+               "schema.compatibility": "NONE",
+               "format.class": "io.confluent.connect.sftp.sink.format.csv.CsvFormat",
+               "storage.class": "io.confluent.connect.sftp.sink.storage.SftpSinkStorage",
+               "sftp.host": "sftp-server",
+               "sftp.port": "22",
+               "sftp.username": "foo",
+               "sftp.password": "pass",
+               "sftp.working.dir": "/upload",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1",
+               "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+               "value.converter.schemas.enable": "false"
+          }' \
+     http://localhost:8083/connectors/sftp-sink-csv/config | jq .
+
+log "sending records"
+docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic test_sftp_sink << EOF
+{"u_name": "scissors", "u_price": 2.75, "u_quantity": 3}
+{"u_name": "tape", "u_price": 0.99, "u_quantity": 10}
+{"u_name": "notebooks", "u_price": 1.99, "u_quantity": 5}
+EOF
+
+sleep 10
+
+log "Listing content of ./upload/topics/test_sftp_sink/partition\=0/"
+docker exec sftp-server bash -c "ls /home/foo/upload/topics/test_sftp_sink/partition\=0/"
+
+docker cp sftp-server:/home/foo/upload/topics/test_sftp_sink/partition\=0/test_sftp_sink+0+0000000000.csv /tmp/
+
+cat /tmp/test_sftp_sink+0+0000000000.csv
+
+# {u_name=scissors, u_price=2.75, u_quantity=3}
+# {u_name=tape, u_price=0.99, u_quantity=10}
+# {u_name=notebooks, u_price=1.99, u_quantity=5}
