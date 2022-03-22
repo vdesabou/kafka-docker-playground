2b45232ec28d941a69dee4d4c4f13ab51e56a606
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-03-22 14:42:41 +0000

    fix name

--- reproduction-models/connect-connect-debezium-sqlserver-source/repro-97960/drop-old-capture.sql
+++ reproduction-models/connect-connect-debezium-sqlserver-source/repro-97960/drop-old-capture.sql
@@ -1,3 +1,3 @@
 USE testDB;
-EXEC sys.sp_cdc_disable_table @source_schema = 'dbo', @source_name = 'customers', @capture_instance = 'dbo_customers';
+EXEC sys.sp_cdc_disable_table @source_schema = 'dbo', @source_name = 'customers', @capture_instance = 'dbo_customers_v2';
 GO
