ed781ada410c7b0b9743610a13eed86f01969516
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-03-25 16:19:57 +0000

    working

---
+++ connect/connect-aws-dynamodb-sink/dynamodb-with-assuming-iam-role-config.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -z "$CI" ]
+then
+     # running with github actions
+     if [ ! -f ../../secrets.properties ]
+     then
+          logerror "../../secrets.properties is not present!"
+          exit 1
+     fi
+     source ../../secrets.properties > /dev/null 2>&1
+fi
+
+AWS_STS_ROLE_ARN=${AWS_STS_ROLE_ARN:-$1}
+
+if [ ! -f $HOME/.aws/config ]
+then
+     logerror "ERROR: $HOME/.aws/config is not set"
+     exit 1
+fi
+# this is only used for AWS CLI
+if [ -z "$AWS_CREDENTIALS_FILE_NAME" ]
+then
+    export AWS_CREDENTIALS_FILE_NAME="credentials_aws_account_with_assume_role"
+fi
+if [ ! -f $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME ]
+then
+     logerror "ERROR: $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME is not set"
+     exit 1
+fi
+
+if [ -z "$AWS_STS_ROLE_ARN" ]
+then
+     logerror "AWS_STS_ROLE_ARN is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_ACCESS_KEY_ID" ]
+then
+     logerror "AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_ACCESS_KEY_ID is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_SECRET_ACCESS_KEY" ]
+then
+     logerror "AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_SECRET_ACCESS_KEY is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+
+if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
+then
+     export CONNECT_CONTAINER_HOME_DIR="/home/appuser"
+else
+     export CONNECT_CONTAINER_HOME_DIR="/root"
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.with-assuming-iam-role-config.yml"
+
+log "Sending messages to topic a-topic"
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic a-topic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+AWS_REGION=$(aws configure get region | tr '\r' '\n')
+DYNAMODB_ENDPOINT="https://dynamodb.$AWS_REGION.amazonaws.com"
+
+log "Creating AWS DynamoDB Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.aws.dynamodb.DynamoDbSinkConnector",
+               "tasks.max": "1",
+               "topics": "a-topic",
+               "aws.dynamodb.region": "'"$AWS_REGION"'",
+               "aws.dynamodb.endpoint": "'"$DYNAMODB_ENDPOINT"'",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1",
+               "aws.dynamodb.credentials.provider.class": "io.confluent.connect.aws.dynamodb.auth.AwsAssumeRoleCredentialsProvider",
+               "aws.dynamodb.credentials.provider.sts.role.arn": "'"$AWS_STS_ROLE_ARN"'",
+               "aws.dynamodb.credentials.provider.sts.role.session.name": "session-name",
+               "aws.dynamodb.credentials.provider.sts.role.external.id": "123"
+          }' \
+     http://localhost:8083/connectors/dynamodb-sink/config | jq .
+
+log "Sleeping 120 seconds, waiting for table to be created"
+sleep 120
+
+log "Verify data is in DynamoDB"
+aws dynamodb scan --table-name a-topic --region $AWS_REGION  > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "value1" /tmp/result.log
