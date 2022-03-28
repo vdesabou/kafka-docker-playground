b11cd0c42794939954e1d1ef4c80c6dcd29a578a
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-03-28 15:08:16 +0000

    working

--- reproduction-models/connect-connect-replicator/replicator-repro-96655-rbac-and-centralized-license.sh
+++ reproduction-models/connect-connect-replicator/replicator-repro-96655-rbac-and-centralized-license.sh
@@ -9,23 +9,18 @@ if ! version_gt $TAG_BASE "5.3.99"; then
     exit 111
 fi
 
+log "Replace create-role-bindings.sh (in order to specify superAdmin user password)"
+cp $PWD/repro-96655-create-role-bindings.sh ../../environment/rbac-sasl-plain/scripts/helper/create-role-bindings.sh
+
 ${DIR}/../../environment/rbac-sasl-plain/start.sh "${PWD}/docker-compose.plaintext.repro-96655-rbac-and-centralized-license.yml"
 
 log "Sending messages to topic rbac_gcs_topic"
 seq -f "{\"f1\": \"This is a message sent with RBAC SASL/PLAIN authentication %g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic rbac_gcs_topic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}' --property schema.registry.url=http://schema-registry:8081 --property basic.auth.credentials.source=USER_INFO --property schema.registry.basic.auth.user.info=clientAvroCli:clientAvroCli --producer.config /etc/kafka/secrets/client_sasl_plain.config
 
-# for rbac, needed to add:
+# for rbac, needed to add (repro-96655-create-role-bindings.sh), see diff with regular environment/rbac-sasl-plain/create-role-bindings.sh:
 
 # declare -a ConnectResources=(
-#     "Topic:connect-configs"
-#     "Topic:connect-offsets"
-#     "Topic:connect-status"
-#     "Group:connect-cluster"
-#     "Topic:_confluent-monitoring"
-#     "Topic:_confluent-command"
 #     "Topic:rbac_gcs_topic-duplicate"
-#     "Topic:_confluent-secrets"
-#     "Group:secret-registry"
 # )
 
 # confluent iam rolebinding create \
@@ -34,11 +29,6 @@ seq -f "{\"f1\": \"This is a message sent with RBAC SASL/PLAIN authentication %g
 #     --resource Group:my-rbac-connector \
 #     --kafka-cluster-id $KAFKA_CLUSTER_ID \
 
-# confluent iam rolebinding create \
-#     --principal $CONNECTOR_PRINCIPAL \
-#     --role ResourceOwner \
-#     --resource Topic:_confluent-command \
-#     --kafka-cluster-id $KAFKA_CLUSTER_ID
 
 log "Creating Replicator connector"
 curl -X PUT \
@@ -64,8 +54,7 @@ curl -X PUT \
             "dest.kafka.sasl.jaas.config": "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required username=\"connectorSA\" password=\"connectorSA\" metadataServerUrls=\"http://broker:8091\";",
             "dest.kafka.sasl.mechanism": "OAUTHBEARER",
             "dest.kafka.sasl.login.callback.handler.class": "io.confluent.kafka.clients.plugins.auth.token.TokenUserLoginCallbackHandler",
-            "consumer.override.sasl.jaas.config": "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required username=\"connectorSA\" password=\"connectorSA\" metadataServerUrls=\"http://broker:8091\";",
-            "confluent.license": ""
+            "consumer.override.sasl.jaas.config": "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required username=\"connectorSA\" password=\"connectorSA\" metadataServerUrls=\"http://broker:8091\";"
            }' \
       http://localhost:8083/connectors/my-rbac-connector/config | jq .
 
@@ -93,55 +82,3 @@ docker exec -i connect kafka-avro-console-consumer --bootstrap-server broker:909
 #         ... 8 more
 # Caused by: org.apache.kafka.common.errors.TimeoutException: License topic could not be created
 # Caused by: org.apache.kafka.common.errors.TimeoutException: Timed out waiting for a node assignment. Call: createTopics
-
-
-# without confluent.topic.replication.factor and with:
-# CONNECT_CONFLUENT_TOPIC_BOOTSTRAP_SERVERS: "broker:9092"
-# CONNECT_CONFLUENT_TOPIC_SECURITY_PROTOCOL: "SASL_PLAINTEXT"
-# CONNECT_CONFLUENT_TOPIC_REPLICATION_FACTOR: "1"
-# CONNECT_CONFLUENT_TOPIC_SASL_MECHANISM: "PLAIN"
-# CONNECT_CONFLUENT_TOPIC_SASL_JAAS_CONFIG: "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"admin\" password=\"admin-secret\";"
-
-# [2022-03-25 09:41:54,597] ERROR [my-rbac-connector|worker] [Worker clientId=connect-1, groupId=connect-cluster] Failed to start connector 'my-rbac-connector' (org.apache.kafka.connect.runtime.distributed.DistributedHerder:1490)
-# org.apache.kafka.connect.errors.ConnectException: Failed to start connector: my-rbac-connector
-#         at org.apache.kafka.connect.runtime.distributed.DistributedHerder.lambda$startConnector$25(DistributedHerder.java:1461)
-#         at org.apache.kafka.connect.runtime.WorkerConnector.doTransitionTo(WorkerConnector.java:335)
-#         at org.apache.kafka.connect.runtime.WorkerConnector.doRun(WorkerConnector.java:141)
-#         at org.apache.kafka.connect.runtime.WorkerConnector.run(WorkerConnector.java:118)
-#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
-#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
-#         at java.base/java.lang.Thread.run(Thread.java:829)
-# Caused by: org.apache.kafka.connect.errors.ConnectException: Failed to transition connector my-rbac-connector to state STARTED
-#         ... 8 more
-# Caused by: org.apache.kafka.common.KafkaException: Failed to create new KafkaAdminClient
-#         at org.apache.kafka.clients.admin.KafkaAdminClient.createInternal(KafkaAdminClient.java:587)
-#         at org.apache.kafka.clients.admin.Admin.create(Admin.java:143)
-#         at org.apache.kafka.clients.admin.AdminClient.create(AdminClient.java:49)
-#         at io.confluent.license.LicenseStore.createTopicDescription(LicenseStore.java:370)
-#         at io.confluent.license.LicenseStore.<init>(LicenseStore.java:118)
-#         at io.confluent.license.LicenseStore.<init>(LicenseStore.java:100)
-#         at io.confluent.license.LicenseStore.<init>(LicenseStore.java:89)
-#         at io.confluent.license.LicenseManager.<init>(LicenseManager.java:151)
-#         at io.confluent.connect.replicator.ReplicatorSourceConnector.validateLicense(ReplicatorSourceConnector.java:197)
-#         at io.confluent.connect.replicator.ReplicatorSourceConnector.start(ReplicatorSourceConnector.java:91)
-#         at org.apache.kafka.connect.runtime.WorkerConnector.doStart(WorkerConnector.java:185)
-#         at org.apache.kafka.connect.runtime.WorkerConnector.start(WorkerConnector.java:210)
-#         at org.apache.kafka.connect.runtime.WorkerConnector.doTransitionTo(WorkerConnector.java:349)
-#         at org.apache.kafka.connect.runtime.WorkerConnector.doTransitionTo(WorkerConnector.java:332)
-#         ... 7 more
-# Caused by: org.apache.kafka.common.KafkaException: java.lang.IllegalArgumentException: Unexpected SASL mechanism: PLAIN
-#         at org.apache.kafka.common.network.SaslChannelBuilder.configure(SaslChannelBuilder.java:193)
-#         at org.apache.kafka.common.network.ChannelBuilders.create(ChannelBuilders.java:233)
-#         at org.apache.kafka.common.network.ChannelBuilders.clientChannelBuilder(ChannelBuilders.java:82)
-#         at org.apache.kafka.clients.ClientUtils.createChannelBuilder(ClientUtils.java:120)
-#         at org.apache.kafka.clients.admin.KafkaAdminClient.createInternal(KafkaAdminClient.java:555)
-#         ... 20 more
-# Caused by: java.lang.IllegalArgumentException: Unexpected SASL mechanism: PLAIN
-#         at io.confluent.kafka.clients.plugins.auth.token.AbstractTokenLoginCallbackHandler.jaasConfigDef(AbstractTokenLoginCallbackHandler.java:121)
-#         at io.confluent.kafka.clients.plugins.auth.token.AbstractTokenLoginCallbackHandler.configure(AbstractTokenLoginCallbackHandler.java:63)
-#         at org.apache.kafka.common.security.authenticator.LoginManager.<init>(LoginManager.java:73)
-#         at org.apache.kafka.common.security.authenticator.LoginManager.acquireLoginManager(LoginManager.java:123)
-#         at org.apache.kafka.common.network.SaslChannelBuilder.configure(SaslChannelBuilder.java:179)
-#         ... 24 more
---
+++ reproduction-models/connect-connect-replicator/repro-96655-create-role-bindings.sh
@@ -0,0 +1,405 @@
+#!/bin/bash -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+. ${DIR}/functions.sh
+
+################################## GET KAFKA CLUSTER ID ########################
+KAFKA_CLUSTER_ID=$(get_kafka_cluster_id_from_container)
+#echo "KAFKA_CLUSTER_ID: $KAFKA_CLUSTER_ID"
+
+################################## SETUP VARIABLES #############################
+MDS_URL=http://broker:8091
+CONNECT=connect-cluster
+SR=schema-registry
+KSQLDB=ksql-cluster
+C3=c3-cluster
+
+SUPER_USER=superUser
+SUPER_USER_PASSWORD=superUser
+SUPER_USER_PRINCIPAL="User:$SUPER_USER"
+CONNECT_ADMIN="User:connectAdmin"
+CONNECTOR_SUBMITTER="User:connectorSubmitter"
+CONNECTOR_PRINCIPAL="User:connectorSA"
+SR_PRINCIPAL="User:schemaregistryUser"
+C3_ADMIN="User:controlcenterAdmin"
+KSQLDB_ADMIN="User:ksqlDBAdmin"
+KSQLDB_USER="User:ksqlDBUser"
+KSQLDB_SERVER="User:controlCenterAndKsqlDBServer"
+CLIENT_AVRO_PRINCIPAL="User:clientAvroCli"
+LICENSE_RESOURCE="Topic:_confluent-license"
+
+mds_login $MDS_URL ${SUPER_USER} ${SUPER_USER_PASSWORD} || exit 1
+
+################################### SUPERUSER ###################################
+echo "Creating role bindings for Super User"
+
+confluent iam rolebinding create \
+    --principal $SUPER_USER_PRINCIPAL  \
+    --role SystemAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $SUPER_USER_PRINCIPAL \
+    --role SystemAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+confluent iam rolebinding create \
+    --principal $SUPER_USER_PRINCIPAL \
+    --role SystemAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --connect-cluster-id $CONNECT
+
+confluent iam rolebinding create \
+    --principal $SUPER_USER_PRINCIPAL \
+    --role SystemAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --ksql-cluster-id $KSQLDB
+
+################################### SCHEMA REGISTRY ###################################
+echo "Creating role bindings for Schema Registry"
+
+# SecurityAdmin on SR cluster itself
+confluent iam rolebinding create \
+    --principal $SR_PRINCIPAL \
+    --role SecurityAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+# ResourceOwner for groups and topics on broker
+for resource in Topic:_schemas Group:schema-registry
+do
+    confluent iam rolebinding create \
+        --principal $SR_PRINCIPAL \
+        --role ResourceOwner \
+        --resource $resource \
+        --kafka-cluster-id $KAFKA_CLUSTER_ID
+done
+
+for role in DeveloperRead DeveloperWrite
+do
+    confluent iam rolebinding create \
+        --principal $SR_PRINCIPAL \
+        --role $role \
+        --resource $LICENSE_RESOURCE \
+        --kafka-cluster-id $KAFKA_CLUSTER_ID
+done
+
+################################### CONNECT Admin ###################################
+echo "Creating role bindings for Connect Admin"
+
+# SecurityAdmin on the connect cluster itself
+confluent iam rolebinding create \
+    --principal $CONNECT_ADMIN \
+    --role SecurityAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --connect-cluster-id $CONNECT
+
+# ResourceOwner for groups and topics on broker
+declare -a ConnectResources=(
+    "Topic:connect-configs"
+    "Topic:connect-offsets"
+    "Topic:connect-status"
+    "Group:connect-cluster"
+    "Topic:_confluent-monitoring"
+    "Topic:_confluent-command"
+    "Topic:_confluent-secrets"
+    "Topic:rbac_gcs_topic-duplicate"
+    "Group:secret-registry"
+)
+for resource in ${ConnectResources[@]}
+do
+    confluent iam rolebinding create \
+        --principal $CONNECT_ADMIN \
+        --role ResourceOwner \
+        --resource $resource \
+        --kafka-cluster-id $KAFKA_CLUSTER_ID
+done
+
+################################### Connectors ###################################
+echo "Creating role bindings for GCP GCS sink connector"
+
+confluent iam rolebinding create \
+    --principal $C3_ADMIN \
+    --role ResourceOwner \
+    --resource Connector:my-rbac-connector \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --connect-cluster-id $CONNECT
+
+confluent iam rolebinding create \
+    --principal $CONNECTOR_SUBMITTER \
+    --role ResourceOwner \
+    --resource Connector:my-rbac-connector \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --connect-cluster-id $CONNECT
+
+confluent iam rolebinding create \
+    --principal $CONNECTOR_PRINCIPAL \
+    --role ResourceOwner \
+    --resource Group:connect-my-rbac-connector \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+
+confluent iam rolebinding create \
+    --principal $CONNECTOR_PRINCIPAL \
+    --role ResourceOwner \
+    --resource Group:my-rbac-connector \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+
+confluent iam rolebinding create \
+    --principal $CONNECTOR_PRINCIPAL \
+    --role ResourceOwner \
+    --resource Topic:rbac \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $CONNECTOR_PRINCIPAL \
+    --role ResourceOwner \
+    --resource Subject:rbac \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+################################### ksqlDB Admin ###################################
+echo "Creating role bindings for ksqlDB Admin"
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role ResourceOwner \
+    --resource KsqlCluster:$KSQLDB \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --ksql-cluster-id $KSQLDB
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role DeveloperRead \
+    --resource Group:_confluent-ksql-${KSQLDB} \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role ResourceOwner \
+    --resource Topic:_confluent-ksql-${KSQLDB} \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role ResourceOwner \
+    --resource Subject:_confluent-ksql-${KSQLDB} \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role ResourceOwner \
+    --resource Topic:_confluent-monitoring \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role ResourceOwner \
+    --resource Topic:${KSQLDB}ksql_processing_log \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role DeveloperRead \
+    --resource Topic:wikipedia.parsed \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role ResourceOwner \
+    --resource Topic:WIKIPEDIA \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role ResourceOwner \
+    --resource TransactionalId:${KSQLDB} \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role ResourceOwner \
+    --resource Subject:wikipedia \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role ResourceOwner \
+    --resource Subject:WIKIPEDIA \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role ResourceOwner \
+    --resource Subject:WIKIPEDIA \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+# enable.idempotence=true requires IdempotentWrite
+confluent iam rolebinding create \
+    --principal $KSQLDB_ADMIN \
+    --role DeveloperWrite \
+    --resource Cluster:kafka-cluster \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+################################### ksqlDB User ###################################
+echo "Creating role bindings for ksqlDB User"
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_USER \
+    --role DeveloperWrite \
+    --resource KsqlCluster:$KSQLDB \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --ksql-cluster-id $KSQLDB
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_USER \
+    --role DeveloperRead \
+    --resource Group:_confluent-ksql-${KSQLDB} \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_USER \
+    --role ResourceOwner \
+    --resource Topic:_confluent-ksql-${KSQLDB} \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_USER \
+    --role ResourceOwner \
+    --resource Subject:_confluent-ksql-${KSQLDB} \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_USER \
+    --role DeveloperRead \
+    --resource Topic:${KSQLDB}ksql_processing_log \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_USER \
+    --role DeveloperRead \
+    --resource Topic:wikipedia.parsed \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_USER \
+    --role ResourceOwner \
+    --resource Subject:wikipedia \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_USER \
+    --role ResourceOwner \
+    --resource Topic:WIKIPEDIA \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $KSQLDB_USER \
+    --role ResourceOwner \
+    --resource Subject:WIKIPEDIA \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+# enable.idempotence=true requires IdempotentWrite
+confluent iam rolebinding create \
+    --principal $KSQLDB_USER \
+    --role DeveloperWrite \
+    --resource Cluster:kafka-cluster \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+################################### KSQLDB Server #############################
+echo "Creating role bindings for ksqlDB Server (used for ksqlDB Processing Log)"
+confluent iam rolebinding create \
+    --principal $KSQLDB_SERVER \
+    --role ResourceOwner \
+    --resource Topic:${KSQLDB}ksql_processing_log \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+############################## Control Center ###############################
+echo "Creating role bindings for Control Center"
+
+# C3 only needs SystemAdmin on the kafka cluster itself
+confluent iam rolebinding create \
+    --principal $C3_ADMIN \
+    --role SystemAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+
+################################### Client Avro CLI ###################################
+echo "Creating role bindings for the Avro CLI"
+
+confluent iam rolebinding create \
+    --principal $CLIENT_AVRO_PRINCIPAL \
+    --role ResourceOwner \
+    --resource Group:clientAvro \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $CLIENT_AVRO_PRINCIPAL \
+    --role ResourceOwner \
+    --resource Topic:rbac \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $CLIENT_AVRO_PRINCIPAL \
+    --role ResourceOwner \
+    --resource Topic:_confluent-monitoring \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $CLIENT_AVRO_PRINCIPAL \
+    --role ResourceOwner \
+    --resource Subject:rbac \
+    --prefix \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+######################### Print #########################
+
+echo "Cluster IDs:"
+echo "    kafka cluster id: $KAFKA_CLUSTER_ID"
+echo "    connect cluster id: $CONNECT"
+echo "    schema registry cluster id: $SR"
+echo "    ksql cluster id: $KSQLDB"
+echo
+echo "Cluster IDs as environment variables:"
+echo "    export KAFKA_ID=$KAFKA_CLUSTER_ID ; export CONNECT_ID=$CONNECT ; export SR_ID=$SR ; export KSQLDB_ID=$KSQLDB"
+echo
+echo "Principals:"
+echo "    super user account: $SUPER_USER_PRINCIPAL"
+echo "    Schema Registry user: $SR_PRINCIPAL"
+echo "    Connect Admin: $CONNECT_ADMIN"
+echo "    Connector Submitter: $CONNECTOR_SUBMITTER"
+echo "    Connector Principal: $CONNECTOR_PRINCIPAL"
+echo "    ksqlDB Admin: $KSQLDB_ADMIN"
+echo "    ksqlDB User: $KSQLDB_USER"
+echo "    ksqlDB Server: $KSQLDB_SERVER"
+echo "    C3 Admin: $C3_ADMIN"
+echo "    Client Avro CLI Principal: $CLIENT_AVRO_PRINCIPAL"
+echo
+
+
