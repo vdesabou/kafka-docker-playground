9387bcd47ac39624fe6c22c76cfc7eb41e3a7c17
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-04-02 13:20:20 +0200

    use latest releases

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -144,7 +144,7 @@ function choosejar()
 if [ -z "$TAG" ]
 then
     # TAG is not set, use default:
-    export TAG=7.0.1
+    export TAG=7.0.2
     # to handle ubi8 images
     export TAG_BASE=$TAG
     if [ -z "$CP_KAFKA_IMAGE" ]
