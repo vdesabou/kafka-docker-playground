b335c450a48ade6126fffe0718eeaedeb409f27d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-04-04 13:03:59 +0000

    wip

---
+++ reproduction-models/connect-connect-snowflake-sink/snowflake-sink-repro-97543-wrong-json.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -z "$CI" ]
+then
+     # running with github actions
+     if [ ! -f ../../secrets.properties ]
+     then
+          logerror "../../secrets.properties is not present!"
+          exit 1
+     fi
+     source ../../secrets.properties > /dev/null 2>&1
+fi
+
+PLAYGROUND_DB=PLAYGROUND_DB${TAG}
+PLAYGROUND_DB=${PLAYGROUND_DB//[-._]/}
+
+PLAYGROUND_WAREHOUSE=PLAYGROUND_WAREHOUSE${TAG}
+PLAYGROUND_WAREHOUSE=${PLAYGROUND_WAREHOUSE//[-._]/}
+
+PLAYGROUND_CONNECTOR_ROLE=PLAYGROUND_CONNECTOR_ROLE${TAG}
+PLAYGROUND_CONNECTOR_ROLE=${PLAYGROUND_CONNECTOR_ROLE//[-._]/}
+
+PLAYGROUND_USER=PLAYGROUND_USER${TAG}
+PLAYGROUND_USER=${PLAYGROUND_USER//[-._]/}
+
+SNOWFLAKE_ACCOUNT_NAME=${SNOWFLAKE_ACCOUNT_NAME:-$1}
+SNOWFLAKE_USERNAME=${SNOWFLAKE_USERNAME:-$2}
+SNOWFLAKE_PASSWORD=${SNOWFLAKE_PASSWORD:-$3}
+
+if [ -z "$SNOWFLAKE_ACCOUNT_NAME" ]
+then
+     logerror "SNOWFLAKE_ACCOUNT_NAME is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SNOWFLAKE_USERNAME" ]
+then
+     logerror "SNOWFLAKE_USERNAME is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SNOWFLAKE_PASSWORD" ]
+then
+     logerror "SNOWFLAKE_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+# https://<account_name>.<region_id>.snowflakecomputing.com:443
+SNOWFLAKE_URL="https://$SNOWFLAKE_ACCOUNT_NAME.snowflakecomputing.com"
+
+# Create encrypted Private key - keep this safe, do not share!
+openssl genrsa 2048 | openssl pkcs8 -topk8 -v2 aes256 -inform PEM -out snowflake_key.p8 -passout pass:confluent
+# Generate public key from private key. You can share your public key.
+openssl rsa -in snowflake_key.p8  -pubout -out snowflake_key.pub -passin pass:confluent
+
+
+RSA_PUBLIC_KEY=$(grep -v "BEGIN PUBLIC" snowflake_key.pub | grep -v "END PUBLIC"|tr -d '\n')
+RSA_PRIVATE_KEY=$(grep -v "BEGIN ENCRYPTED PRIVATE KEY" snowflake_key.p8 | grep -v "END ENCRYPTED PRIVATE KEY"|tr -d '\n')
+
+
+log "Create a Snowflake DB"
+docker run --rm -i -e SNOWSQL_PWD="$SNOWFLAKE_PASSWORD" -e RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY" kurron/snowsql --username $SNOWFLAKE_USERNAME -a $SNOWFLAKE_ACCOUNT_NAME << EOF
+DROP DATABASE IF EXISTS $PLAYGROUND_DB;
+CREATE OR REPLACE DATABASE $PLAYGROUND_DB COMMENT = 'Database for Docker Playground';
+EOF
+
+log "Create a Snowflake ROLE"
+docker run --rm -i -e SNOWSQL_PWD="$SNOWFLAKE_PASSWORD" -e RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY" kurron/snowsql --username $SNOWFLAKE_USERNAME -a $SNOWFLAKE_ACCOUNT_NAME << EOF
+USE ROLE SECURITYADMIN;
+DROP ROLE IF EXISTS $PLAYGROUND_CONNECTOR_ROLE;
+CREATE ROLE $PLAYGROUND_CONNECTOR_ROLE;
+GRANT USAGE ON DATABASE $PLAYGROUND_DB TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+GRANT USAGE ON DATABASE $PLAYGROUND_DB TO ACCOUNTADMIN;
+GRANT USAGE ON SCHEMA $PLAYGROUND_DB.PUBLIC TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+GRANT ALL ON FUTURE TABLES IN SCHEMA $PLAYGROUND_DB.PUBLIC TO $PLAYGROUND_CONNECTOR_ROLE;
+GRANT USAGE ON SCHEMA $PLAYGROUND_DB.PUBLIC TO ROLE ACCOUNTADMIN;
+GRANT CREATE TABLE ON SCHEMA $PLAYGROUND_DB.PUBLIC TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+GRANT CREATE STAGE ON SCHEMA $PLAYGROUND_DB.PUBLIC TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+GRANT CREATE PIPE ON SCHEMA $PLAYGROUND_DB.PUBLIC TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+EOF
+
+log "Create a Snowflake WAREHOUSE (for admin purpose as KafkaConnect is Serverless)"
+docker run --rm -i -e SNOWSQL_PWD="$SNOWFLAKE_PASSWORD" -e RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY" kurron/snowsql --username $SNOWFLAKE_USERNAME -a $SNOWFLAKE_ACCOUNT_NAME << EOF
+USE ROLE SYSADMIN;
+CREATE OR REPLACE WAREHOUSE $PLAYGROUND_WAREHOUSE
+  WAREHOUSE_SIZE = 'XSMALL'
+  WAREHOUSE_TYPE = 'STANDARD'
+  AUTO_SUSPEND = 60
+  AUTO_RESUME = TRUE
+  MIN_CLUSTER_COUNT = 1
+  MAX_CLUSTER_COUNT = 1
+  SCALING_POLICY = 'STANDARD'
+  INITIALLY_SUSPENDED = TRUE
+  COMMENT = 'Warehouse for Kafka Playground';
+GRANT USAGE ON WAREHOUSE $PLAYGROUND_WAREHOUSE TO ROLE $PLAYGROUND_CONNECTOR_ROLE;
+EOF
+
+log "Create a Snowflake USER"
+docker run --rm -i -e SNOWSQL_PWD="$SNOWFLAKE_PASSWORD" -e RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY" kurron/snowsql --username $SNOWFLAKE_USERNAME -a $SNOWFLAKE_ACCOUNT_NAME << EOF
+USE ROLE USERADMIN;
+DROP USER IF EXISTS $PLAYGROUND_USER;
+CREATE USER $PLAYGROUND_USER
+ PASSWORD = 'Password123!'
+ LOGIN_NAME = $PLAYGROUND_USER
+ DISPLAY_NAME = $PLAYGROUND_USER
+ DEFAULT_WAREHOUSE = $PLAYGROUND_WAREHOUSE
+ DEFAULT_ROLE = $PLAYGROUND_CONNECTOR_ROLE
+ DEFAULT_NAMESPACE = $PLAYGROUND_DB
+ MUST_CHANGE_PASSWORD = FALSE
+ RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY";
+USE ROLE SECURITYADMIN;
+GRANT ROLE $PLAYGROUND_CONNECTOR_ROLE TO USER $PLAYGROUND_USER;
+EOF
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-97543-wrong-json.yml"
+
+log "Sending messages to topic test_table"
+#  Using JSON with schema (and key):
+docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic test_table --property parse.key=true --property key.separator=, << EOF
+1,{"schema":{"type":"struct","fields":[{"type":"string","optional":false,"field":"record"}]},"payload":{"record":"record1"}}
+2,{"schema":{"type":"struct","fields":[{"type":"string","optional":false,"field":"record"}]},"payload":{"record":"record2"}}
+3,{"schema":{"type":"struct","fields":[{"type":"string","optional":false,"field":"record"}]},"payload":{"record":"record3"}}
+EOF
+
+
+
+log "Creating Snowflake Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "com.snowflake.kafka.connector.SnowflakeSinkConnector",
+               "topics": "test_table",
+               "tasks.max": "1",
+               "snowflake.url.name": "'"$SNOWFLAKE_URL"'",
+               "snowflake.user.name": "'"$PLAYGROUND_USER"'",
+               "snowflake.user.role": "'"$PLAYGROUND_CONNECTOR_ROLE"'",
+               "snowflake.private.key":"'"$RSA_PRIVATE_KEY"'",
+               "snowflake.private.key.passphrase": "confluent",
+               "snowflake.database.name": "'"$PLAYGROUND_DB"'",
+               "snowflake.schema.name":"PUBLIC",
+               "buffer.count.records": "3",
+               "buffer.flush.time" : "10",
+               "key.converter":"org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+               "value.converter.schemas.enable": "true",
+               "errors.tolerance" : "all",
+               "errors.log.enable" : "true",
+               "errors.log.include.messages" : "true",
+               "errors.deadletterqueue.topic.name" : "dlq",
+               "errors.deadletterqueue.topic.replication.factor": "1",
+               "errors.deadletterqueue.context.headers.enable" : "true"
+          }' \
+     http://localhost:8083/connectors/snowflake-sink/config | jq .
+
+
+sleep 120
+
+log "Confirm that the messages were delivered to the Snowflake table (logged as $PLAYGROUND_USER user)"
+docker run --rm -i -e SNOWSQL_PWD='Password123!' -e RSA_PUBLIC_KEY="$RSA_PUBLIC_KEY" kurron/snowsql --username $PLAYGROUND_USER -a $SNOWFLAKE_ACCOUNT_NAME > /tmp/result.log  2>&1 <<-EOF
+USE ROLE $PLAYGROUND_CONNECTOR_ROLE;
+USE DATABASE $PLAYGROUND_DB;
+USE SCHEMA PUBLIC;
+USE WAREHOUSE $PLAYGROUND_WAREHOUSE;
+SELECT * FROM $PLAYGROUND_DB.PUBLIC.TEST_TABLE;
+EOF
+cat /tmp/result.log
+grep "record1" /tmp/result.log
+
+# docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group connect-snowflake-sink --describe
+
+# docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group connect-snowflake-sink --to-earliest --topic test_table --reset-offsets --dry-run
+# docker exec broker kafka-consumer-groups --bootstrap-server broker:9092 --group connect-snowflake-sink --to-earliest --topic test_table --reset-offsets --execute
+
+
+docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic test_table --property parse.key=true --property key.separator=, << EOF
+4,{"schema":{"type":"struct","fields":[{"type":"string","optional":false,"field":"record"}]},"payload":{"record":"record4"}}
+5,{"schema":{"type":"struct","fields":[{"type":"string","optional":false,"field":"record"}]},"payload":{"record":"record5"}}
+6,{"schema":{"type":"struct","fields":[{"type":"string","optional":false,"field":"record"}]},"payload":{"record":"record6}}
+EOF
+
+timeout 60 docker exec broker kafka-console-consumer --bootstrap-server broker:9092 --topic dlq --from-beginning --property print.headers=true
+# __connect.errors.topic:test_table,__connect.errors.partition:0,__connect.errors.offset:5,__connect.errors.connector.name:snowflake-sink,__connect.errors.task.id:0,__connect.errors.stage:VALUE_CONVERTER,__connect.errors.class.name:org.apache.kafka.connect.json.JsonConverter,__connect.errors.exception.class.name:org.apache.kafka.connect.errors.DataException,__connect.errors.exception.message:Converting byte[] to Kafka Connect data failed due to serialization error: ,__connect.errors.exception.stacktrace:org.apache.kafka.connect.errors.DataException: Converting byte[] to Kafka Connect data failed due to serialization error: 
+#         at org.apache.kafka.connect.json.JsonConverter.toConnectData(JsonConverter.java:324)
+#         at org.apache.kafka.connect.storage.Converter.toConnectData(Converter.java:87)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertValue(WorkerSinkTask.java:563)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.lambda$convertAndTransformRecord$5(WorkerSinkTask.java:519)
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndRetry(RetryWithToleranceOperator.java:166)
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execAndHandleError(RetryWithToleranceOperator.java:200)
+#         at org.apache.kafka.connect.runtime.errors.RetryWithToleranceOperator.execute(RetryWithToleranceOperator.java:142)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertAndTransformRecord(WorkerSinkTask.java:519)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.convertMessages(WorkerSinkTask.java:494)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.poll(WorkerSinkTask.java:333)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.iteration(WorkerSinkTask.java:235)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:204)
+#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:199)
+#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:254)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:829)
+# Caused by: org.apache.kafka.common.errors.SerializationException: Error deserializing message to JSON in topic test_table
+#         at org.apache.kafka.connect.json.JsonDeserializer.deserialize(JsonDeserializer.java:66)
+#         at org.apache.kafka.connect.json.JsonConverter.toConnectData(JsonConverter.java:322)
+#         ... 18 more
+#         {"schema":{"type":"struct","fields":[{"type":"string","optional":false,"field":"record"}]},"payload":{"record":"record6}}
