73595ef554d598c615db517a63ac2ed2daf34b79
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-04-07 08:35:52 +0000

    wip

---
+++ reproduction-models/connect-connect-jdbc-oracle19-source/oracle19-repro-100178-jdbc-source-connectors-do-not-destroy-sessions-when-restarted.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+create_or_get_oracle_image "LINUX.X64_193000_db_home.zip" "../../connect/connect-jdbc-oracle19-source/ora-setup-scripts"
+
+if [ ! -z "$CONNECTOR_TAG" ]
+then
+     JDBC_CONNECTOR_VERSION=$CONNECTOR_TAG
+else
+     JDBC_CONNECTOR_VERSION=$(docker run vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} cat /usr/share/confluent-hub-components/confluentinc-kafka-connect-jdbc/manifest.json | jq -r '.version')
+fi
+log "JDBC Connector version is $JDBC_CONNECTOR_VERSION"
+if ! version_gt $JDBC_CONNECTOR_VERSION "9.9.9"; then
+     get_3rdparty_file "ojdbc8.jar"
+     if [ ! -f ${DIR}/ojdbc8.jar ]
+     then
+          logerror "ERROR: ${DIR}/ojdbc8.jar is missing. It must be downloaded manually in order to acknowledge user agreement"
+          exit 1
+     fi
+     ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+else
+     log "ojdbc jar is shipped with connector (starting with 10.0.0)"
+     ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.no-ojdbc.repro-100178-jdbc-source-connectors-do-not-destroy-sessions-when-restarted.yml"
+fi
+
+# Verify Oracle DB has started within MAX_WAIT seconds
+MAX_WAIT=2500
+CUR_WAIT=0
+log "⌛ Waiting up to $MAX_WAIT seconds for Oracle DB to start"
+docker container logs oracle > /tmp/out.txt 2>&1
+while [[ ! $(cat /tmp/out.txt) =~ "DONE: Executing user defined scripts" ]]; do
+sleep 10
+docker container logs oracle > /tmp/out.txt 2>&1
+CUR_WAIT=$(( CUR_WAIT+10 ))
+if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+     logerror "ERROR: The logs in oracle container do not show 'DONE: Executing user defined scripts' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+     exit 1
+fi
+done
+log "Oracle DB has started!"
+
+log "Creating Oracle source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class":"io.confluent.connect.jdbc.JdbcSourceConnector",
+               "tasks.max":"1",
+               "connection.user": "myuser",
+               "connection.password": "mypassword",
+               "connection.url": "jdbc:oracle:thin:@oracle:1521/ORCLPDB1",
+               "numeric.mapping":"best_fit",
+               "mode":"timestamp",
+               "poll.interval.ms":"1000",
+               "validate.non.null":"false",
+               "table.whitelist":"CUSTOMERS",
+               "timestamp.column.name":"UPDATE_TS",
+               "topic.prefix":"oracle-",
+               "errors.log.enable": "true",
+               "errors.log.include.messages": "true"
+          }' \
+     http://localhost:8083/connectors/oracle-source/config | jq .
+
+
+sleep 5
+
+log "Verifying topic oracle-CUSTOMERS"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic oracle-CUSTOMERS --from-beginning --max-messages 2
+
+
+
+for((i=0;i<10;i++))
+do
+
+docker exec -i oracle sqlplus myuser/mypassword@//localhost:1521/ORCLPDB1 << EOF
+insert into CUSTOMERS (first_name, last_name, email, gender, club_status, comments) values ('Rica', 'Blaisdell', 'rblaisdell0@rambler.ru', 'Female', 'bronze', 'Universal optimal hierarchy');
+insert into CUSTOMERS (first_name, last_name, email, gender, club_status, comments) values ('Ruthie', 'Brockherst', 'rbrockherst1@ow.ly', 'Female', 'platinum', 'Reverse-engineered tangible interface');
+insert into CUSTOMERS (first_name, last_name, email, gender, club_status, comments) values ('Mariejeanne', 'Cocci', 'mcocci2@techcrunch.com', 'Female', 'bronze', 'Multi-tiered bandwidth-monitored capability');
+insert into CUSTOMERS (first_name, last_name, email, gender, club_status, comments) values ('Hashim', 'Rumke', 'hrumke3@sohu.com', 'Male', 'platinum', 'Self-enabling 24/7 firmware');
+insert into CUSTOMERS (first_name, last_name, email, gender, club_status, comments) values ('Hansiain', 'Coda', 'hcoda4@senate.gov', 'Male', 'platinum', 'Centralized full-range approach');
+EOF
+
+     log "Restarting task"
+     curl -X POST localhost:8083/connectors/oracle-source/tasks/0/restart
+
+     log "getting resources"
+docker exec -i oracle bash -c "ORACLE_SID=ORCLCDB;export ORACLE_SID;sqlplus /nolog" << EOF
+CONNECT sys/Admin123 AS SYSDBA
+SELECT RESOURCE_NAME,LIMIT_VALUE,CURRENT_UTILIZATION,MAX_UTILIZATION FROM v\$resource_limit where RESOURCE_NAME IN ('processes','sessions');
+exit;
+EOF
+
+     log "getting sesions"
+docker exec -i oracle bash -c "ORACLE_SID=ORCLCDB;export ORACLE_SID;sqlplus /nolog" << EOF
+CONNECT sys/Admin123 AS SYSDBA
+SELECT MACHINE, OSUSER, USERNAME, COUNT(*) AS SESSIONS_COUNT FROM "V\$SESSION" WHERE MACHINE LIKE 'connec%' GROUP BY MACHINE, OSUSER, USERNAME;
+exit;
+EOF
+done
