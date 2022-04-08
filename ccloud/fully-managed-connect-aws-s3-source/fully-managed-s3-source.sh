9a26be15c1bfc5d31f8ce3aa8a37bdbf18323791
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-04-08 09:26:46 +0200

    now working

--- ccloud/fully-managed-connect-aws-s3-source/fully-managed-s3-source.sh
+++ ccloud/fully-managed-connect-aws-s3-source/fully-managed-s3-source.sh
@@ -90,10 +90,10 @@ log "Creating fully managed connector"
 create_ccloud_connector connector.json
 wait_for_ccloud_connector_up connector.json 300
 
-sleep 120
+sleep 30
 
 log "Verifying topic quick-start-topic"
-timeout 60 docker run --rm -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} kafka-avro-console-consumer --topic quick-start-topic --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --from-beginning --max-messages 9
+timeout 60 docker run --rm -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} kafka-console-consumer --topic quick-start-topic --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --from-beginning --max-messages 9
 
 log "Do you want to delete the fully managed connector ?"
 check_if_continue
