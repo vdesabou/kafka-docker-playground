124f06a4c8e5292e06a7c1fe02078113886bdddb
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-04-13 13:05:32 +0000

    wip

--- reproduction-models/connect-connect-cdc-oracle12-source/cdc-oracle12-cdb-table-repro-98358-flashback-restore-point.sh
+++ reproduction-models/connect-connect-cdc-oracle12-source/cdc-oracle12-cdb-table-repro-98358-flashback-restore-point.sh
@@ -174,73 +174,46 @@ sleep 10
 log "restarting failed task"
 curl -X POST localhost:8083/connectors/cdc-oracle-source-cdb/tasks/0/restart
 
-
-# [2022-03-25 14:18:50,030] ERROR [cdc-oracle-source-cdb|task-0] WorkerSourceTask{id=cdc-oracle-source-cdb-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:191)
-# org.apache.kafka.connect.errors.ConnectException: Error while polling for records
-#         at io.confluent.connect.oracle.cdc.util.RecordQueue.poll(RecordQueue.java:372)
-#         at io.confluent.connect.oracle.cdc.OracleCdcSourceTask.poll(OracleCdcSourceTask.java:489)
-#         at org.apache.kafka.connect.runtime.WorkerSourceTask.poll(WorkerSourceTask.java:291)
-#         at org.apache.kafka.connect.runtime.WorkerSourceTask.execute(WorkerSourceTask.java:248)
-#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:189)
-#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:238)
-#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
-#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
-#         at java.base/java.lang.Thread.run(Thread.java:829)
-# Caused by: org.apache.kafka.connect.errors.ConnectException: org.apache.kafka.connect.errors.ConnectException: Failed on attempt 1 of 32768 to read redo log from jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle)(PORT=1521))(CONNECT_DATA=(SID=ORCLCDB))) with user 'C##MYUSER' (pool=oracle-cdc-source:cdc-oracle-source-cdb-0): ORA-01291: missing logfile
-# ORA-06512: at "SYS.DBMS_LOGMNR", line 58
-# ORA-06512: at line 1
-
-#         at io.confluent.connect.oracle.cdc.OracleRedoLogReader.readRedoLogs(OracleRedoLogReader.java:105)
-#         at io.confluent.connect.oracle.cdc.util.RecordQueue.lambda$createLoggingSupplier$0(RecordQueue.java:465)
-#         at java.base/java.util.concurrent.CompletableFuture$AsyncSupply.run(CompletableFuture.java:1700)
-#         ... 3 more
-# Caused by: org.apache.kafka.connect.errors.ConnectException: Failed on attempt 1 of 32768 to read redo log from jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle)(PORT=1521))(CONNECT_DATA=(SID=ORCLCDB))) with user 'C##MYUSER' (pool=oracle-cdc-source:cdc-oracle-source-cdb-0): ORA-01291: missing logfile
-# ORA-06512: at "SYS.DBMS_LOGMNR", line 58
-# ORA-06512: at line 1
-
-#         at io.confluent.connect.utils.retry.RetryPolicy.callWith(RetryPolicy.java:423)
-#         at io.confluent.connect.utils.retry.RetryPolicy.callWith(RetryPolicy.java:368)
-#         at io.confluent.connect.oracle.cdc.OracleDatabase.retry(OracleDatabase.java:564)
-#         at io.confluent.connect.oracle.cdc.OracleRedoLogReader.readRedoLogs(OracleRedoLogReader.java:89)
-#         ... 5 more
-# Caused by: java.sql.SQLException: ORA-01291: missing logfile
-# ORA-06512: at "SYS.DBMS_LOGMNR", line 58
-# ORA-06512: at line 1
-
-#         at oracle.jdbc.driver.T4CTTIoer11.processError(T4CTTIoer11.java:509)
-#         at oracle.jdbc.driver.T4CTTIoer11.processError(T4CTTIoer11.java:461)
-#         at oracle.jdbc.driver.T4C8Oall.processError(T4C8Oall.java:1104)
-#         at oracle.jdbc.driver.T4CTTIfun.receive(T4CTTIfun.java:550)
-#         at oracle.jdbc.driver.T4CTTIfun.doRPC(T4CTTIfun.java:268)
-#         at oracle.jdbc.driver.T4C8Oall.doOALL(T4C8Oall.java:655)
-#         at oracle.jdbc.driver.T4CStatement.doOall8(T4CStatement.java:229)
-#         at oracle.jdbc.driver.T4CStatement.doOall8(T4CStatement.java:41)
-#         at oracle.jdbc.driver.T4CStatement.executeForRows(T4CStatement.java:928)
-#         at oracle.jdbc.driver.OracleStatement.doExecuteWithTimeout(OracleStatement.java:1205)
-#         at oracle.jdbc.driver.OracleStatement.executeUpdateInternal(OracleStatement.java:1747)
-#         at oracle.jdbc.driver.OracleStatement.executeLargeUpdate(OracleStatement.java:1712)
-#         at oracle.jdbc.driver.OracleStatement.executeUpdate(OracleStatement.java:1699)
-#         at oracle.jdbc.driver.OracleStatementWrapper.executeUpdate(OracleStatementWrapper.java:285)
-#         at oracle.ucp.jdbc.proxy.oracle$1ucp$1jdbc$1proxy$1oracle$1StatementProxy$2oracle$1jdbc$1internal$1OracleStatement$$$Proxy.executeUpdate(Unknown Source)
-#         at io.confluent.connect.oracle.cdc.logging.LogUtils.executeUpdate(LogUtils.java:30)
-#         at io.confluent.connect.oracle.cdc.OracleDatabase.executeUpdate(OracleDatabase.java:1006)
-#         at io.confluent.connect.oracle.cdc.OracleDatabase.startLogMinerSession(OracleDatabase.java:444)
-#         at io.confluent.connect.oracle.cdc.OracleDatabase.startLogMinerSessionContinuousMine(OracleDatabase.java:416)
-#         at io.confluent.connect.oracle.cdc.mining.WithContinuousMining.mineRedoLogs(WithContinuousMining.java:62)
-#         at io.confluent.connect.oracle.cdc.OracleRedoLogReader.lambda$readRedoLogs$0(OracleRedoLogReader.java:91)
-#         at io.confluent.connect.utils.retry.RetryPolicy.callWith(RetryPolicy.java:417)
-#         ... 8 more
-# Caused by: Error : 1291, Position : 0, Sql = BEGIN DBMS_LOGMNR.START_LOGMNR(STARTSCN => 1447179, OPTIONS => DBMS_LOGMNR.DICT_FROM_REDO_LOGS + DBMS_LOGMNR.DDL_DICT_TRACKING + DBMS_LOGMNR.CONTINUOUS_MINE + DBMS_LOGMNR.SKIP_CORRUPTION + DBMS_LOGMNR.COMMITTED_DATA_ONLY); END;, OriginalSql = {CALL DBMS_LOGMNR.START_LOGMNR(STARTSCN => 1447179, OPTIONS => DBMS_LOGMNR.DICT_FROM_REDO_LOGS + DBMS_LOGMNR.DDL_DICT_TRACKING + DBMS_LOGMNR.CONTINUOUS_MINE + DBMS_LOGMNR.SKIP_CORRUPTION + DBMS_LOGMNR.COMMITTED_DATA_ONLY)}, Error Msg = ORA-01291: missing logfile
-# ORA-06512: at "SYS.DBMS_LOGMNR", line 58
-# ORA-06512: at line 1
-
-#         at oracle.jdbc.driver.T4CTTIoer11.processError(T4CTTIoer11.java:513)
-#         ... 29 more
-
 log "Running SQL scripts"
 for script in ../../connect/connect-cdc-oracle12-source/sample-sql-scripts/*.sh
 do
      $script "ORCLCDB"
 done
+
+
+# [2022-04-13 13:04:26,555] ERROR [cdc-oracle-source-cdb|task-1] WorkerSourceTask{id=cdc-oracle-source-cdb-1} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:206)
+# org.apache.kafka.connect.errors.ConnectException: Error while polling for records
+#         at io.confluent.connect.oracle.cdc.util.RecordQueue.poll(RecordQueue.java:372)
+#         at io.confluent.connect.oracle.cdc.OracleCdcSourceTask.poll(OracleCdcSourceTask.java:496)
+#         at org.apache.kafka.connect.runtime.WorkerSourceTask.poll(WorkerSourceTask.java:308)
+#         at org.apache.kafka.connect.runtime.WorkerSourceTask.execute(WorkerSourceTask.java:263)
+#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:199)
+#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:254)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:829)
+# Caused by: org.apache.kafka.connect.errors.ConnectException: Exception converting redo to change event. SQL: '  alter table CUSTOMERS add (
+#     country VARCHAR(50)
+#   );' INFO: USER DDL (PlSql=0 RecDep=0)
+#         at io.confluent.connect.oracle.cdc.record.OracleChangeEventSourceRecordConverter.convert(OracleChangeEventSourceRecordConverter.java:343)
+#         at io.confluent.connect.oracle.cdc.ChangeEventGenerator.processSingleRecord(ChangeEventGenerator.java:496)
+#         at io.confluent.connect.oracle.cdc.ChangeEventGenerator.lambda$doGenerateChangeEvent$2(ChangeEventGenerator.java:419)
+#         at java.base/java.util.stream.ReferencePipeline$3$1.accept(ReferencePipeline.java:195)
+#         at java.base/java.util.Spliterators$ArraySpliterator.forEachRemaining(Spliterators.java:948)
+#         at java.base/java.util.stream.AbstractPipeline.copyInto(AbstractPipeline.java:484)
+#         at java.base/java.util.stream.AbstractPipeline.wrapAndCopyInto(AbstractPipeline.java:474)
+#         at java.base/java.util.stream.ReduceOps$ReduceOp.evaluateSequential(ReduceOps.java:913)
+#         at java.base/java.util.stream.AbstractPipeline.evaluate(AbstractPipeline.java:234)
+#         at java.base/java.util.stream.ReferencePipeline.collect(ReferencePipeline.java:578)
+#         at io.confluent.connect.oracle.cdc.ChangeEventGenerator.doGenerateChangeEvent(ChangeEventGenerator.java:421)
+#         at io.confluent.connect.oracle.cdc.ChangeEventGenerator.execute(ChangeEventGenerator.java:221)
+#         at io.confluent.connect.oracle.cdc.util.RecordQueue.lambda$createLoggingSupplier$0(RecordQueue.java:465)
+#         at java.base/java.util.concurrent.CompletableFuture$AsyncSupply.run(CompletableFuture.java:1700)
+#         ... 3 more
+# Caused by: org.apache.kafka.connect.errors.SchemaBuilderException: Cannot create field because of field name duplication COUNTRY
+#         at org.apache.kafka.connect.data.SchemaBuilder.field(SchemaBuilder.java:330)
+#         at io.confluent.connect.oracle.cdc.OracleDdlParser.parseAndApply(OracleDdlParser.java:250)
+#         at io.confluent.connect.oracle.cdc.record.OracleChangeEventSourceRecordConverter.convert(OracleChangeEventSourceRecordConverter.java:318)
+#         ... 16 more
