db0be86f087698792c4d2a0d29a220b2c4b662da
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-04-20 12:50:06 +0000

    WIP

--- reproduction-models/connect-connect-debezium-sqlserver-source/sqlserver-repro-101931--implicit-transactions.sh
+++ reproduction-models/connect-connect-debezium-sqlserver-source/sqlserver-repro-101931--implicit-transactions.sh
@@ -8,7 +8,7 @@ ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.rep
 
 
 log "Load inventory.sql to SQL Server"
-cat inventory.sql | docker exec -i sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P Password!'
+cat ../../connect/connect-debezium-sqlserver-source/inventory.sql | docker exec -i sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P Password!'
 
 
 log "Creating Debezium SQL Server source connector"
@@ -24,7 +24,8 @@ curl -X PUT \
                     "database.server.name": "server1",
                     "database.dbname" : "testDB",
                     "database.history.kafka.bootstrap.servers": "broker:9092",
-                    "database.history.kafka.topic": "schema-changes.inventory"
+                    "database.history.kafka.topic": "schema-changes.inventory",
+                    "max.iteration.transactions": 1
           }' \
      http://localhost:8083/connectors/debezium-sqlserver-source/config | jq .
 
@@ -32,7 +33,7 @@ sleep 5
 
 docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! << EOF
 USE testDB;
-INSERT INTO customers(first_name,last_name,email) VALUES ('Pam2','Thomas','pam2@office.com');
+INSERT INTO customers(first_name,last_name,email) VALUES ('Pam','Thomas','pam4@office.com');
 GO
 EOF
 
@@ -60,7 +61,7 @@ cat /tmp/result.log
 #         55 2022-04-20 10:14:27.307                    NULL                           2 2022-04-20 12:08:47.920 2022-04-20 12:08:47.920
 #         61 2022-04-20 10:15:27.940                    NULL                           2 2022-04-20 12:08:27.920 2022-04-20 12:08:27.920
 
-log "Printing connecttions for Debezium (program_name=Microsoft JDBC Driver for SQL Server)"
+log "Printing connections for Debezium (program_name=Microsoft JDBC Driver for SQL Server)"
 docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! > /tmp/result.log  2>&1 <<-EOF
 USE testDB;
 
@@ -84,6 +85,93 @@ cat /tmp/result.log
 #         55 Microsoft JDBC Driver for SQL Server                                                                                             Microsoft JDBC Driver 7.2        SELECT MAX(start_lsn) FROM [testDB].cdc.lsn_time_mapping WHERE tran_id <> 0x00                                                                                                                                                                                  
 #         61 Microsoft JDBC Driver for SQL Server                                                                                             Microsoft JDBC Driver 7.2        IF @@TRANCOUNT > 0 COMMIT TRAN                                                                                                                                                                                                                                  
 #         56 SQLCMD                                                                                                                           ODBC                             USE testDB;
+
+
+log "Printing transactions for Debezium (program_name=Microsoft JDBC Driver for SQL Server)"
+docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! > /tmp/result.log  2>&1 <<-EOF
+USE testDB;
+
+SET NOCOUNT ON
+
+SELECT sys.dm_exec_sessions.session_id,
+       sys.dm_exec_sessions.program_name,
+       sys.dm_exec_sessions.client_interface_name,
+       sys.dm_tran_active_transactions.*
+FROM sys.dm_tran_active_transactions
+INNER JOIN sys.dm_tran_session_transactions ON sys.dm_tran_active_transactions.transaction_id = sys.dm_tran_session_transactions.transaction_id
+INNER JOIN sys.dm_exec_sessions ON sys.dm_tran_session_transactions.session_id = sys.dm_exec_sessions.session_id
+WHERE login_name = 'sa'
+ORDER BY client_interface_name ASC
+GO
+EOF
+cat /tmp/result.log
+
+
+
+docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! > /tmp/result.log  2>&1 <<-EOF
+USE testDB;
+
+SET NOCOUNT ON
+
+select transaction_id, name, transaction_begin_time
+ ,case transaction_type 
+    when 1 then '1 = Read/write transaction'
+    when 2 then '2 = Read-only transaction'
+    when 3 then '3 = System transaction'
+    when 4 then '4 = Distributed transaction'
+end as transaction_type 
+,case transaction_state 
+    when 0 then '0 = The transaction has not been completely initialized yet'
+    when 1 then '1 = The transaction has been initialized but has not started'
+    when 2 then '2 = The transaction is active'
+    when 3 then '3 = The transaction has ended. This is used for read-only transactions'
+    when 4 then '4 = The commit process has been initiated on the distributed transaction'
+    when 5 then '5 = The transaction is in a prepared state and waiting resolution'
+    when 6 then '6 = The transaction has been committed'
+    when 7 then '7 = The transaction is being rolled back'
+    when 8 then '8 = The transaction has been rolled back'
+end as transaction_state
+,case dtc_state 
+    when 1 then '1 = ACTIVE'
+    when 2 then '2 = PREPARED'
+    when 3 then '3 = COMMITTED'
+    when 4 then '4 = ABORTED'
+    when 5 then '5 = RECOVERED'
+end as dtc_state 
+,transaction_status, transaction_status2,dtc_status, dtc_isolation_level, filestream_transaction_id
+from sys.dm_tran_active_transactions 
+
+GO
+EOF
+cat /tmp/result.log
+
+
+docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! > /tmp/result.log  2>&1 <<-EOF
+USE testDB;
+
+SET NOCOUNT ON
+
+SELECT
+trans.session_id AS [SESSION ID],
+ESes.host_name AS [HOST NAME],login_name AS [Login NAME],
+trans.transaction_id AS [TRANSACTION ID],
+tas.name AS [TRANSACTION NAME],tas.transaction_begin_time AS [TRANSACTION 
+BEGIN TIME],
+tds.database_id AS [DATABASE ID],DBs.name AS [DATABASE NAME]
+FROM sys.dm_tran_active_transactions tas
+JOIN sys.dm_tran_session_transactions trans
+ON (trans.transaction_id=tas.transaction_id)
+LEFT OUTER JOIN sys.dm_tran_database_transactions tds
+ON (tas.transaction_id = tds.transaction_id )
+LEFT OUTER JOIN sys.databases AS DBs
+ON tds.database_id = DBs.database_id
+LEFT OUTER JOIN sys.dm_exec_sessions AS ESes
+ON trans.session_id = ESes.session_id
+WHERE ESes.session_id IS NOT NULL 
+
+GO
+EOF
+cat /tmp/result.log
 
 log "Printing all other information"
 docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! > /tmp/result.log  2>&1 <<-EOF
