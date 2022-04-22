58f4d249378fcccaa0fc804a296824b49190b16c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-04-22 09:39:18 +0000

    it fails

--- reproduction-models/connect-connect-gcp-bigquery-sink/gcp-bigquery-repro-102539-connector-hanging-when-sending-additional-field.sh
+++ reproduction-models/connect-connect-gcp-bigquery-sink/gcp-bigquery-repro-102539-connector-hanging-when-sending-additional-field.sh
@@ -92,7 +92,7 @@ docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic m
 "1",{"f1":"value1","f2":"value2"}
 EOF
 
-
+# [2022-04-22 09:33:08,245] DEBUG [gcp-bigquery-sink|task-0] Putting 0 records in the sink. (com.wepay.kafka.connect.bigquery.BigQuerySinkTask:238)
 # [2022-04-22 09:33:15,698] DEBUG [gcp-bigquery-sink|task-0] Putting 1 records in the sink. (com.wepay.kafka.connect.bigquery.BigQuerySinkTask:238)
 # [2022-04-22 09:33:15,699] DEBUG [gcp-bigquery-sink|task-0] writing 1 row to table GenericData{classInfo=[datasetId, projectId, tableId], {datasetId=pgds102539, tableId=mytable2$20220422}} (com.wepay.kafka.connect.bigquery.write.row.BigQueryWriter:101)
 # [2022-04-22 09:33:15,910] WARN [gcp-bigquery-sink|task-0] You may want to enable schema updates by specifying allowNewBigQueryFields=true or allowBigQueryRequiredFieldRelaxation=true in the properties file (com.wepay.kafka.connect.bigquery.write.row.SimpleBigQueryWriter:69)
@@ -111,3 +111,106 @@ EOF
 #         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
 #         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
 #         at java.base/java.lang.Thread.run(Thread.java:829)
+# [2022-04-22 09:34:08,246] ERROR [gcp-bigquery-sink|task-0] WorkerSinkTask{id=gcp-bigquery-sink-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted. Error: A write thread has failed with an unrecoverable error (org.apache.kafka.connect.runtime.WorkerSinkTask:636)
+# com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException: A write thread has failed with an unrecoverable error
+# Caused by: table insertion failed for the following rows:
+#         [row index 0] (location f2, reason: invalid): no such field: f2.
+#         at com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor.lambda$maybeThrowEncounteredError$0(KCBQThreadPoolExecutor.java:101)
+#         at java.base/java.util.Optional.ifPresent(Optional.java:183)
+#         at com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor.maybeThrowEncounteredError(KCBQThreadPoolExecutor.java:100)
+#         at com.wepay.kafka.connect.bigquery.BigQuerySinkTask.put(BigQuerySinkTask.java:236)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.deliverMessages(WorkerSinkTask.java:604)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.poll(WorkerSinkTask.java:334)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.iteration(WorkerSinkTask.java:235)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:204)
+#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:199)
+#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:254)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:829)
+# Caused by: com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException: table insertion failed for the following rows:
+#         [row index 0] (location f2, reason: invalid): no such field: f2.
+#         at com.wepay.kafka.connect.bigquery.write.row.BigQueryWriter.writeRows(BigQueryWriter.java:125)
+#         at com.wepay.kafka.connect.bigquery.write.batch.TableWriter.run(TableWriter.java:93)
+#         ... 3 more
+# [2022-04-22 09:34:08,247] WARN [gcp-bigquery-sink|task-0] WorkerSinkTask{id=gcp-bigquery-sink-0} Offset commit failed during close (org.apache.kafka.connect.runtime.WorkerSinkTask:408)
+# [2022-04-22 09:34:08,247] ERROR [gcp-bigquery-sink|task-0] WorkerSinkTask{id=gcp-bigquery-sink-0} Commit of offsets threw an unexpected exception for sequence number 8: null (org.apache.kafka.connect.runtime.WorkerSinkTask:270)
+# com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException: A write thread has failed with an unrecoverable error
+# Caused by: table insertion failed for the following rows:
+#         [row index 0] (location f2, reason: invalid): no such field: f2.
+#         at com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor.lambda$maybeThrowEncounteredError$0(KCBQThreadPoolExecutor.java:101)
+#         at java.base/java.util.Optional.ifPresent(Optional.java:183)
+#         at com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor.maybeThrowEncounteredError(KCBQThreadPoolExecutor.java:100)
+#         at com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor.awaitCurrentTasks(KCBQThreadPoolExecutor.java:90)
+#         at com.wepay.kafka.connect.bigquery.BigQuerySinkTask.flush(BigQuerySinkTask.java:159)
+#         at com.wepay.kafka.connect.bigquery.BigQuerySinkTask.preCommit(BigQuerySinkTask.java:175)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.commitOffsets(WorkerSinkTask.java:405)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.closePartitions(WorkerSinkTask.java:673)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.closeAllPartitions(WorkerSinkTask.java:668)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:205)
+#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:199)
+#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:254)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:829)
+# Caused by: com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException: table insertion failed for the following rows:
+#         [row index 0] (location f2, reason: invalid): no such field: f2.
+#         at com.wepay.kafka.connect.bigquery.write.row.BigQueryWriter.writeRows(BigQueryWriter.java:125)
+#         at com.wepay.kafka.connect.bigquery.write.batch.TableWriter.run(TableWriter.java:93)
+#         ... 3 more
+# [2022-04-22 09:34:08,247] ERROR [gcp-bigquery-sink|task-0] WorkerSinkTask{id=gcp-bigquery-sink-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:206)
+# org.apache.kafka.connect.errors.ConnectException: Exiting WorkerSinkTask due to unrecoverable exception.
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.deliverMessages(WorkerSinkTask.java:638)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.poll(WorkerSinkTask.java:334)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.iteration(WorkerSinkTask.java:235)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:204)
+#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:199)
+#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:254)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:829)
+# Caused by: com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException: A write thread has failed with an unrecoverable error
+# Caused by: table insertion failed for the following rows:
+#         [row index 0] (location f2, reason: invalid): no such field: f2.
+#         at com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor.lambda$maybeThrowEncounteredError$0(KCBQThreadPoolExecutor.java:101)
+#         at java.base/java.util.Optional.ifPresent(Optional.java:183)
+#         at com.wepay.kafka.connect.bigquery.write.batch.KCBQThreadPoolExecutor.maybeThrowEncounteredError(KCBQThreadPoolExecutor.java:100)
+#         at com.wepay.kafka.connect.bigquery.BigQuerySinkTask.put(BigQuerySinkTask.java:236)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.deliverMessages(WorkerSinkTask.java:604)
+#         ... 10 more
+# Caused by: com.wepay.kafka.connect.bigquery.exception.BigQueryConnectException: table insertion failed for the following rows:
+#         [row index 0] (location f2, reason: invalid): no such field: f2.
+#         at com.wepay.kafka.connect.bigquery.write.row.BigQueryWriter.writeRows(BigQueryWriter.java:125)
+#         at com.wepay.kafka.connect.bigquery.write.batch.TableWriter.run(TableWriter.java:93)
+#         ... 3 more
+# [2022-04-22 09:34:08,247] TRACE [gcp-bigquery-sink|task-0] Requesting shutdown for table write executor (com.wepay.kafka.connect.bigquery.BigQuerySinkTask:546)
+# [2022-04-22 09:34:08,248] TRACE [gcp-bigquery-sink|task-0] Awaiting termination of table write executor (com.wepay.kafka.connect.bigquery.BigQuerySinkTask:549)
+# [2022-04-22 09:34:08,251] TRACE [gcp-bigquery-sink|task-0] Shut down table write executor successfully (com.wepay.kafka.connect.bigquery.BigQuerySinkTask:551)
+# [2022-04-22 09:34:08,251] TRACE [gcp-bigquery-sink|task-0] task.stop() (com.wepay.kafka.connect.bigquery.BigQuerySinkTask:533)
+# [2022-04-22 09:34:08,252] INFO [gcp-bigquery-sink|task-0] [Consumer clientId=connector-consumer-gcp-bigquery-sink-0, groupId=connect-gcp-bigquery-sink] Revoke previously assigned partitions mytable2-0 (org.apache.kafka.clients.consumer.internals.ConsumerCoordinator:310)
+# [2022-04-22 09:34:08,252] INFO [gcp-bigquery-sink|task-0] [Consumer clientId=connector-consumer-gcp-bigquery-sink-0, groupId=connect-gcp-bigquery-sink] Member connector-consumer-gcp-bigquery-sink-0-7bd8b8e8-5739-46f4-9a43-decc682420b0 sending LeaveGroup request to coordinator broker:9092 (id: 2147483646 rack: null) due to the consumer is being closed (org.apache.kafka.clients.consumer.internals.ConsumerCoordinator:1048)
+# [2022-04-22 09:34:08,252] INFO [gcp-bigquery-sink|task-0] [Consumer clientId=connector-consumer-gcp-bigquery-sink-0, groupId=connect-gcp-bigquery-sink] Resetting generation due to: consumer pro-actively leaving the group (org.apache.kafka.clients.consumer.internals.ConsumerCoordinator:966)
+# [2022-04-22 09:34:08,253] INFO [gcp-bigquery-sink|task-0] [Consumer clientId=connector-consumer-gcp-bigquery-sink-0, groupId=connect-gcp-bigquery-sink] Request joining group due to: consumer pro-actively leaving the group (org.apache.kafka.clients.consumer.internals.ConsumerCoordinator:988)
+# [2022-04-22 09:34:08,254] INFO [gcp-bigquery-sink|task-0] Publish thread interrupted for client_id=connector-consumer-gcp-bigquery-sink-0 client_type=CONSUMER session= cluster=j4XAfrfyTCOfYnLEcV8q5Q group=connect-gcp-bigquery-sink (io.confluent.monitoring.clients.interceptor.MonitoringInterceptor:285)
+# [2022-04-22 09:34:08,254] INFO [gcp-bigquery-sink|task-0] Publishing Monitoring Metrics stopped for client_id=connector-consumer-gcp-bigquery-sink-0 client_type=CONSUMER session= cluster=j4XAfrfyTCOfYnLEcV8q5Q group=connect-gcp-bigquery-sink (io.confluent.monitoring.clients.interceptor.MonitoringInterceptor:297)
+# [2022-04-22 09:34:08,255] INFO [gcp-bigquery-sink|task-0] [Producer clientId=confluent.monitoring.interceptor.connector-consumer-gcp-bigquery-sink-0] Closing the Kafka producer with timeoutMillis = 9223372036854775807 ms. (org.apache.kafka.clients.producer.KafkaProducer:1217)
+# [2022-04-22 09:34:08,257] INFO [gcp-bigquery-sink|task-0] Metrics scheduler closed (org.apache.kafka.common.metrics.Metrics:676)
+# [2022-04-22 09:34:08,257] INFO [gcp-bigquery-sink|task-0] Closing reporter org.apache.kafka.common.metrics.JmxReporter (org.apache.kafka.common.metrics.Metrics:680)
+# [2022-04-22 09:34:08,258] INFO [gcp-bigquery-sink|task-0] Metrics reporters closed (org.apache.kafka.common.metrics.Metrics:686)
+# [2022-04-22 09:34:08,258] INFO [gcp-bigquery-sink|task-0] App info kafka.producer for confluent.monitoring.interceptor.connector-consumer-gcp-bigquery-sink-0 unregistered (org.apache.kafka.common.utils.AppInfoParser:83)
+# [2022-04-22 09:34:08,258] INFO [gcp-bigquery-sink|task-0] Closed monitoring interceptor for client_id=connector-consumer-gcp-bigquery-sink-0 client_type=CONSUMER session= cluster=j4XAfrfyTCOfYnLEcV8q5Q group=connect-gcp-bigquery-sink (io.confluent.monitoring.clients.interceptor.MonitoringInterceptor:320)
+# [2022-04-22 09:34:08,258] INFO [gcp-bigquery-sink|task-0] Metrics scheduler closed (org.apache.kafka.common.metrics.Metrics:676)
+# [2022-04-22 09:34:08,258] INFO [gcp-bigquery-sink|task-0] Closing reporter org.apache.kafka.common.metrics.JmxReporter (org.apache.kafka.common.metrics.Metrics:680)
+# [2022-04-22 09:34:08,258] INFO [gcp-bigquery-sink|task-0] Metrics reporters closed (org.apache.kafka.common.metrics.Metrics:686)
+# [2022-04-22 09:34:08,259] INFO [gcp-bigquery-sink|task-0] App info kafka.consumer for connector-consumer-gcp-bigquery-sink-0 unregistered (org.apache.kafka.common.utils.AppInfoParser:83)
+# [2022-04-22 09:34:08,259] INFO [gcp-bigquery-sink|task-0] [Producer clientId=connect-worker-producer] Closing the Kafka producer with timeoutMillis = 9223372036854775807 ms. (org.apache.kafka.clients.producer.KafkaProducer:1217)
+# [2022-04-22 09:34:08,261] INFO [gcp-bigquery-sink|task-0] Metrics scheduler closed (org.apache.kafka.common.metrics.Metrics:676)
+# [2022-04-22 09:34:08,261] INFO [gcp-bigquery-sink|task-0] Closing reporter org.apache.kafka.common.metrics.JmxReporter (org.apache.kafka.common.metrics.Metrics:680)
+# [2022-04-22 09:34:08,261] INFO [gcp-bigquery-sink|task-0] Metrics reporters closed (org.apache.kafka.common.metrics.Metrics:686)
+# [2022-04-22 09:34:08,261] INFO [gcp-bigquery-sink|task-0] App info kafka.producer for connect-worker-producer unregistered (org.apache.kafka.common.utils.AppInfoParser:83)
