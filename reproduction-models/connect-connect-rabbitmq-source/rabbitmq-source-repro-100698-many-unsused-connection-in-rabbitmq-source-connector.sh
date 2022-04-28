ff97fb9c28ec08759f80adf11e2fbfdb0c54be5e
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-04-28 07:12:07 +0000

    fixed with 1.7.3

--- reproduction-models/connect-connect-rabbitmq-source/rabbitmq-source-repro-100698-many-unsused-connection-in-rabbitmq-source-connector.sh
+++ reproduction-models/connect-connect-rabbitmq-source/rabbitmq-source-repro-100698-many-unsused-connection-in-rabbitmq-source-connector.sh
@@ -4,7 +4,6 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-100698-many-unsused-connection-in-rabbitmq-source-connector.yml"
 
 sleep 5
@@ -34,7 +33,7 @@ sleep 5
 log "Verify we have received the data in rabbitmq topic"
 timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic rabbitmq --from-beginning --max-messages 5
 
-
+set +e
 for((i=0;i<10;i++))
 do
      log "Display connections"
@@ -60,4 +59,9 @@ do
                     "confluent.topic.replication.factor": "1"
                }' \
           http://localhost:8083/connectors/rabbitmq-source/config | jq .
+     
+     sleep 2
+
+     log "Send message to RabbitMQ in myqueue"
+     docker exec rabbitmq_producer bash -c "python /producer.py myqueue 5"
 done
