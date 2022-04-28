8104ef728bd9ad020a5ea4889591ffae117e8ac2
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-04-28 16:34:48 +0000

    repro

---
+++ reproduction-models/connect-connect-jms-tibco-source/jms-tibco-repro-103276-xml-payload-only.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+# Need to create the TIBCO EMS image using https://github.com/mikeschippers/docker-tibco
+cd ../../connect/connect-jms-tibco-source/docker-tibco/
+get_3rdparty_file "TIB_ems-ce_8.5.1_linux_x86_64.zip"
+cd -
+if [ ! -f ../../connect/connect-jms-tibco-source/docker-tibco/TIB_ems-ce_8.5.1_linux_x86_64.zip ]
+then
+     logerror "ERROR: ../../connect/connect-jms-tibco-source/docker-tibco/ does not contain TIBCO EMS zip file TIB_ems-ce_8.5.1_linux_x86_64.zip"
+     exit 1
+fi
+
+if [ ! -f ../../connect/connect-jms-tibco-source/tibjms.jar ]
+then
+     log "../../connect/connect-jms-tibco-source/tibjms.jar missing, will get it from ../../connect/connect-jms-tibco-source/docker-tibco/TIB_ems-ce_8.5.1_linux_x86_64.zip"
+     rm -rf /tmp/TIB_ems-ce_8.5.1
+     unzip ../../connect/connect-jms-tibco-source/docker-tibco/TIB_ems-ce_8.5.1_linux_x86_64.zip -d /tmp/
+     tar xvfz /tmp/TIB_ems-ce_8.5.1/tar/TIB_ems-ce_8.5.1_linux_x86_64-java_client.tar.gz opt/tibco/ems/8.5/lib/tibjms.jar
+     cp opt/tibco/ems/8.5/lib/tibjms.jar ../../connect/connect-jms-tibco-source/
+     rm -rf ../../connect/connect-jms-tibco-source/opt
+fi
+
+if test -z "$(docker images -q tibems:latest)"
+then
+     log "Building TIBCO EMS docker image..it can take a while..."
+     OLDDIR=$PWD
+     cd ../../connect/connect-jms-tibco-source/docker-tibco
+     docker build -t tibbase:1.0.0 ./tibbase
+     docker build -t tibems:latest . -f ./tibems/Dockerfile
+     cd ${OLDDIR}
+fi
+
+if test -z "$(docker images -q tibems:latest)"
+then
+     log "Building TIBCO EMS docker image..it can take a while..."
+     OLDDIR=$PWD
+     cd ../../connect/connect-jms-tibco-source/docker-tibco
+     docker build -t tibbase:1.0.0 ./tibbase
+     docker build -t tibems:latest . -f ./tibems/Dockerfile
+     cd ${OLDDIR}
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-103276-xml-payload-only.yml"
+
+log "Sending EMS messages m1 m2 m3 m4 m5 in queue connector-quickstart"
+docker exec tibco-ems bash -c '
+cd /opt/tibco/ems/8.5/samples/java
+export TIBEMS_JAVA=/opt/tibco/ems/8.5/lib
+CLASSPATH=${TIBEMS_JAVA}/jms-2.0.jar:${CLASSPATH}
+CLASSPATH=.:${TIBEMS_JAVA}/tibjms.jar:${TIBEMS_JAVA}/tibjmsadmin.jar:${CLASSPATH}
+export CLASSPATH
+javac *.java
+java tibjmsMsgProducer -user admin -queue connector-quickstart "<Testing><XML><Values><ID value="DUNS">45454546</ID></Values></XML></Testing>"'
+
+
+log "Creating JMS TIBCO source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.jms.JmsSourceConnector",
+                    "tasks.max": "1",
+                    "kafka.topic": "from-tibco-messages",
+                    "java.naming.factory.initial": "com.tibco.tibjms.naming.TibjmsInitialContextFactory",
+                    "java.naming.provider.url": "tibjmsnaming://tibco-ems:7222",
+                    "jms.destination.type": "queue",
+                    "jms.destination.name": "connector-quickstart",
+                    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                    "value.converter": "org.apache.kafka.connect.storage.StringConverter",
+                    "confluent.license": "",
+                    "confluent.topic.bootstrap.servers": "broker:9092",
+                    "confluent.topic.replication.factor": "1",
+
+                    "transforms": "ExtractField",
+                    "transforms.ExtractField.type": "org.apache.kafka.connect.transforms.ExtractField$Value",
+                    "transforms.ExtractField.field": "text"
+          }' \
+     http://localhost:8083/connectors/jms-tibco-source/config | jq .
+
+sleep 5
+
+log "Verify we have received the data in from-tibco-messages topic"
+timeout 60 docker exec connect kafka-console-consumer -bootstrap-server broker:9092 --topic from-tibco-messages --from-beginning --max-messages 1
+
+# <Testing><XML><Values><ID value=DUNS>45454546</ID></Values></XML></Testing>
