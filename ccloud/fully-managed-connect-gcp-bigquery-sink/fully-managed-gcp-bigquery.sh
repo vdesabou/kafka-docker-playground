587480bd40541e03ac5ac1398b66da83124e81eb
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-05-09 10:36:20 +0000

    fully managed version

---
+++ ccloud/fully-managed-connect-gcp-bigquery-sink/fully-managed-gcp-bigquery.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+PROJECT=${1:-vincent-de-saboulin-lab}
+
+KEYFILE="${DIR}/keyfile.json"
+if [ ! -f ${KEYFILE} ]
+then
+     logerror "ERROR: the file ${KEYFILE} file is not present!"
+     exit 1
+fi
+
+KEYFILE_CONTENT=`cat keyfile.json | jq -aRs .`
+
+bootstrap_ccloud_environment
+
+if [ -f /tmp/delta_configs/env.delta ]
+then
+     source /tmp/delta_configs/env.delta
+else
+     logerror "ERROR: /tmp/delta_configs/env.delta has not been generated"
+     exit 1
+fi
+
+DATASET=pg${USER}ds${GITHUB_RUN_NUMBER}${TAG}
+DATASET=${DATASET//[-._]/}
+
+log "Doing gsutil authentication"
+set +e
+docker rm -f gcloud-config
+set -e
+docker run -i -v ${KEYFILE}:/tmp/keyfile.json --name gcloud-config google/cloud-sdk:latest gcloud auth activate-service-account --project ${PROJECT} --key-file /tmp/keyfile.json
+
+set +e
+log "Drop dataset $DATASET, this might fail"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" rm -r -f -d "$DATASET"
+set -e
+
+log "Create dataset $PROJECT.$DATASET"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" mk --dataset --description "used by playground" "$DATASET"
+
+log "Creating bqtopic topic in Confluent Cloud"
+set +e
+create_topic bqtopic
+set -e
+
+log "Sending messages to topic bqtopic"
+seq -f "{\"f1\": \"value%g\"}" 10 | docker run -i --rm -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" vdesabou/kafka-docker-playground-connect:${CONNECT_TAG}  kafka-avro-console-producer --broker-list $BOOTSTRAP_SERVERS --producer-property ssl.endpoint.identification.algorithm=https --producer-property sasl.mechanism=PLAIN --producer-property security.protocol=SASL_SSL --producer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=USER_INFO --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --topic bqtopic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+cat << EOF > connector.json
+{
+     "connector.class": "BigQuerySink",
+     "name": "BigQuerySink",
+     "kafka.auth.mode": "KAFKA_API_KEY",
+     "kafka.api.key": "$CLOUD_KEY",
+     "kafka.api.secret": "$CLOUD_SECRET",
+     "topics": "bqtopic",
+     "keyfile" : $KEYFILE_CONTENT,
+     "project" : "$PROJECT",
+     "datasets" : "$DATASET",
+     "input.data.format" : "AVRO",
+     "auto.create.tables" : "true",
+     "sanitize.topics" : "true",
+     "auto.update.schemas" : "true",
+     "sanitize.field.names" : "true",
+     "allow.schema.unionization" :"true",
+     "tasks.max" : "1"
+}
+EOF
+
+log "Connector configuration is:"
+cat connector.json
+
+set +e
+log "Deleting fully managed connector, it might fail..."
+delete_ccloud_connector connector.json
+set -e
+
+log "Creating fully managed connector"
+create_ccloud_connector connector.json
+wait_for_ccloud_connector_up connector.json 300
+
+log "Sleeping 120 seconds"
+sleep 120
+
+log "Verify data is in GCP BigQuery:"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" query "SELECT * FROM $DATASET.bqtopic;" > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "value1" /tmp/result.log
+
+log "Do you want to delete the fully managed connector ?"
+check_if_continue
+
+log "Deleting fully managed connector"
+delete_ccloud_connector connector.json
+
+log "Drop dataset $DATASET"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$PROJECT" rm -r -f -d "$DATASET"
+
+docker rm -f gcloud-config
