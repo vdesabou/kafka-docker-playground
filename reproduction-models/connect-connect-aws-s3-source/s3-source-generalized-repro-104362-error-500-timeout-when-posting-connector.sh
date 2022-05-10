4fd62b692f3f1fcc7c5f6a7552a8a9da5108fde7
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-05-10 11:25:45 +0200

    WIP

---
+++ reproduction-models/connect-connect-aws-s3-source/s3-source-generalized-repro-104362-error-500-timeout-when-posting-connector.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if ! version_gt $CONNECTOR_TAG "1.9.9"; then
+    # skipped
+    logwarn "WARN: skipped as it requires connector version 2.0.0"
+    exit 111
+fi
+
+if ! version_gt $TAG_BASE "5.9.99" && version_gt $CONNECTOR_TAG "1.9.9"
+then
+    logwarn "WARN: connector version >= 2.0.0 do not support CP versions < 6.0.0"
+    exit 111
+fi
+
+if [ ! -f $HOME/.aws/config ]
+then
+     logerror "ERROR: $HOME/.aws/config is not set"
+     exit 1
+fi
+if [ -z "$AWS_CREDENTIALS_FILE_NAME" ]
+then
+    export AWS_CREDENTIALS_FILE_NAME="credentials"
+fi
+if [ ! -f $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME ]
+then
+     logerror "ERROR: $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME is not set"
+     exit 1
+fi
+
+if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
+then
+     export CONNECT_CONTAINER_HOME_DIR="/home/appuser"
+else
+     export CONNECT_CONTAINER_HOME_DIR="/root"
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.generalized.repro-104362-error-500-timeout-when-posting-connector.yml"
+
+AWS_BUCKET_NAME=kafka-docker-playground-bucket-${USER}${TAG}
+AWS_BUCKET_NAME=${AWS_BUCKET_NAME//[-.]/}
+
+AWS_REGION=$(aws configure get region | tr '\r' '\n')
+
+log "Creating bucket name <$AWS_BUCKET_NAME>, if required"
+set +e
+aws s3api create-bucket --bucket $AWS_BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+set -e
+
+# mkdir -p /tmp/tmp
+# for((i=0;i<10000;i++)); do
+#      echo "${i}"
+#      cp generalized.quickstart.json /tmp/tmp/generalized.quickstart$i.json
+# done
+
+# exit 0
+
+# log "Copy generalized.quickstart.json to bucket $AWS_BUCKET_NAME/quickstart"
+# aws s3 cp generalized.quickstart.json s3://$AWS_BUCKET_NAME/quickstart/generalized.quickstart.json
+
+log "Creating Generalized S3 Source connector with bucket name <$AWS_BUCKET_NAME>"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "tasks.max": "1",
+               "connector.class": "io.confluent.connect.s3.source.S3SourceConnector",
+               "s3.region": "'"$AWS_REGION"'",
+               "s3.bucket.name": "'"$AWS_BUCKET_NAME"'",
+               "format.class": "io.confluent.connect.s3.format.json.JsonFormat",
+               "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+               "value.converter.schemas.enable": "false",
+               "confluent.license": "",
+               "mode": "GENERIC",
+               "topics.dir": "quickstart",
+               "topic.regex.list": "quick-start-topic:.*",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
+          }' \
+     http://localhost:8083/connectors/s3-source-generalized/config | jq .
+
+
+log "Verifying topic quick-start-topic"
+timeout 60 docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic quick-start-topic --from-beginning --max-messages 9
