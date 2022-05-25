da11afcf662ec0aca8471c2098efb7ee7d80f597
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-05-25 11:30:59 +0000

    wip

--- reproduction-models/connect-connect-gcp-gcs-sink/gcs-sink-repro-106925-schema-id-deleted.sh
+++ reproduction-models/connect-connect-gcp-gcs-sink/gcs-sink-repro-106925-schema-id-deleted.sh
@@ -45,6 +45,10 @@ log "HARD Delete version 1"
 curl --request DELETE \
   --url http://localhost:8081/subjects/gcs_topic-value/versions/1?permanent=true
 
+log "Show remaining version"
+curl --request GET \
+  --url http://localhost:8081/subjects/gcs_topic-value/versions 
+
 log "Sending messages to topic gcs_topic"
 seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic gcs_topic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string","doc": "toc"}]}'
 
@@ -65,7 +69,12 @@ curl -X PUT \
                "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
                "schema.compatibility": "NONE",
                "confluent.topic.bootstrap.servers": "broker:9092",
-               "confluent.topic.replication.factor": "1"
+               "confluent.topic.replication.factor": "1",
+
+               "value.converter": "io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url": "http://schema-registry:8081",
+               "value.converter.auto.register.schemas" : "false",
+               "value.converter.use.latest.version": "true"
           }' \
      http://localhost:8083/connectors/gcs-sink/config | jq .
 
