bc8c1c3e5859017981700e01f902c8676406b264
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-06-07 07:47:51 +0000

    WIP

--- reproduction-models/connect-connect-filestream-sink/filestream-sink-repro-107760-jsonparseexception-with-double-backslashs.sh
+++ reproduction-models/connect-connect-filestream-sink/filestream-sink-repro-107760-jsonparseexception-with-double-backslashs.sh
@@ -21,11 +21,14 @@ source ${DIR}/../../scripts/utils.sh
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-107760-jsonparseexception-with-double-backslashs.yml"
 
-#log "✨ Run the avro java producer which produces to topic a-topic"
+#log "✨ Run the avro java producer which produces to topic a-topic-1"
 #docker exec producer-repro-107760 bash -c "java ${JAVA_OPTS} -jar producer-1.0.0-jar-with-dependencies.jar"
 
-log "produce with kafkajs in background"
-docker exec -i client-kafkajs node /usr/src/app/producer.js > /dev/null 2>&1 &
+# log "produce with kafkajs in background"
+# docker exec -i client-kafkajs node /usr/src/app/producer.js > /dev/null 2>&1 &
+
+log "Sending messages to topic a-topic-1"
+cat repro-107760-payload-1.json | docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic a-topic-1
 
 OUTPUT_FILE="${CONNECT_CONTAINER_HOME_DIR}/data/ouput/file.json"
 
@@ -35,13 +38,13 @@ curl -X PUT \
      --data '{
                "tasks.max": "1",
                "connector.class": "FileStreamSink",
-               "topics": "a-topic",
+               "topics": "a-topic-1",
                "file": "/tmp/output.json",
                "key.converter": "org.apache.kafka.connect.storage.StringConverter",
                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
                "value.converter.schemas.enable": "false"
           }' \
-     http://localhost:8083/connectors/a-topic-sink/config | jq .
+     http://localhost:8083/connectors/a-topic-1-sink/config | jq .
 
 
 sleep 5
@@ -50,9 +53,78 @@ log "Verify we have received the data in file"
 docker exec connect cat /tmp/output.json
 
 log "Verify data with kafka-console-consumer"
-timeout 60 docker exec connect kafka-console-consumer --bootstrap-server broker:9092 --topic a-topic --from-beginning --max-messages 1
+timeout 60 docker exec connect kafka-console-consumer --bootstrap-server broker:9092 --topic a-topic-1 --from-beginning --max-messages 1
 # {"xpath":"href=\\#day"}
 
 log "Verify data with kafkacat"
-docker exec kafkacat kafkacat -b broker:9092 -t a-topic -o 0 -p 0 -c 1 -C
+docker exec kafkacat kafkacat -b broker:9092 -t a-topic-1 -o 0 -p 0 -c 1 -C -f '\nKey (%K bytes): %k\t\nValue (%S bytes): %s\nTimestamp: %T\tPartition: %p\tOffset: %o\n--\n'
+# {"xpath": "href=\\#day"}
+
+
+
+log "Sending messages to topic a-topic-2"
+cat repro-107760-payload-2.json | docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic a-topic-2
+
+OUTPUT_FILE="${CONNECT_CONTAINER_HOME_DIR}/data/ouput/file.json"
+
+log "Creating FileStream Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "tasks.max": "1",
+               "connector.class": "FileStreamSink",
+               "topics": "a-topic-2",
+               "file": "/tmp/output.json",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+               "value.converter.schemas.enable": "false"
+          }' \
+     http://localhost:8083/connectors/a-topic-2-sink/config | jq .
+
+
+sleep 5
+
+log "Verify we have received the data in file"
+docker exec connect cat /tmp/output.json
+
+log "Verify data with kafka-console-consumer"
+timeout 60 docker exec connect kafka-console-consumer --bootstrap-server broker:9092 --topic a-topic-2 --from-beginning --max-messages 1
+# {"xpath":"href=\\#day"}
+
+log "Verify data with kafkacat"
+docker exec kafkacat kafkacat -b broker:9092 -t a-topic-2 -o 0 -p 0 -c 1 -C -f '\nKey (%K bytes): %k\t\nValue (%S bytes): %s\nTimestamp: %T\tPartition: %p\tOffset: %o\n--\n'
+# {"xpath": "href=\\#day"}
+
+
+log "Sending messages to topic a-topic-3"
+cat repro-107760-payload-3.json | docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic a-topic-3
+
+OUTPUT_FILE="${CONNECT_CONTAINER_HOME_DIR}/data/ouput/file.json"
+
+log "Creating FileStream Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "tasks.max": "1",
+               "connector.class": "FileStreamSink",
+               "topics": "a-topic-3",
+               "file": "/tmp/output.json",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+               "value.converter.schemas.enable": "false"
+          }' \
+     http://localhost:8083/connectors/a-topic-3-sink/config | jq .
+
+
+sleep 5
+
+log "Verify we have received the data in file"
+docker exec connect cat /tmp/output.json
+
+log "Verify data with kafka-console-consumer"
+timeout 60 docker exec connect kafka-console-consumer --bootstrap-server broker:9092 --topic a-topic-3 --from-beginning --max-messages 1
+# {"xpath":"href=\\#day"}
+
+log "Verify data with kafkacat"
+docker exec kafkacat kafkacat -b broker:9092 -t a-topic-3 -o 0 -p 0 -c 1 -C -f '\nKey (%K bytes): %k\t\nValue (%S bytes): %s\nTimestamp: %T\tPartition: %p\tOffset: %o\n--\n'
 # {"xpath": "href=\\#day"}
