f173c7abed6a91904c3ac220331aa7075ca5d81a
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-06-07 12:49:18 +0200

    generate for broker3 and connect2 and connect3

--- environment/sasl-ssl/security/certs-verify.sh
+++ environment/sasl-ssl/security/certs-verify.sh
@@ -5,7 +5,7 @@ set -o nounset \
     -o verbose
 
 # See what is in each keystore and truststore
-for i in broker broker2 client schema-registry restproxy connect control-center clientrestproxy ksqldb-server
+for i in broker broker2 broker3 client schema-registry restproxy connect connect2 connect3 control-center clientrestproxy ksqldb-server
 do
         echo "------------------------------- $i keystore -------------------------------"
         keytool -list -v -keystore /tmp/kafka.$i.keystore.jks -storepass confluent | grep -e Alias -e Entry
