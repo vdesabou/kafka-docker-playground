d6f3278276d440ba953bd08084563acd94519792
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-06-14 12:39:45 +0000

    repro

--- reproduction-models/connect-connect-mongodb-sink/mongo-sink-repro-109617-only-struct-objects-supported-for-field-insertion-found:-java.lang.string.sh
+++ reproduction-models/connect-connect-mongodb-sink/mongo-sink-repro-109617-only-struct-objects-supported-for-field-insertion-found:-java.lang.string.sh
@@ -25,34 +25,34 @@ EOF
 
 sleep 2
 
-log "Sending messages to topic orders"
-docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic orders --property parse.key=true --property key.separator=, << EOF
-key1,value1
-key1,value2
-key2,value1
-EOF
+# log "Sending messages to topic orders"
+# docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic orders --property parse.key=true --property key.separator=, << EOF
+# key1,value1
+# key1,value2
+# key2,value1
+# EOF
 
-log "Creating MongoDB sink connector"
-curl -X PUT \
-     -H "Content-Type: application/json" \
-     --data '{
-               "connector.class" : "com.mongodb.kafka.connect.MongoSinkConnector",
-                    "tasks.max" : "1",
-                    "connection.uri" : "mongodb://myuser:mypassword@mongodb:27017",
+# log "Creating MongoDB sink connector"
+# curl -X PUT \
+#      -H "Content-Type: application/json" \
+#      --data '{
+#                "connector.class" : "com.mongodb.kafka.connect.MongoSinkConnector",
+#                     "tasks.max" : "1",
+#                     "connection.uri" : "mongodb://myuser:mypassword@mongodb:27017",
 
-                    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-                    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-                    "value.converter.schemas.enable": "false",
+#                     "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+#                     "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+#                     "value.converter.schemas.enable": "false",
                     
-                    "database":"inventory",
-                    "collection":"customers",
-                    "topics":"orders",
+#                     "database":"inventory",
+#                     "collection":"customers",
+#                     "topics":"orders",
 
-                    "transforms": "insert",
-                    "transforms.insert.type": "org.apache.kafka.connect.transforms.InsertField$Value",
-                    "transforms.insert.timestamp.field": "mytimestamp"
-          }' \
-     http://localhost:8083/connectors/mongodb-sink/config | jq .
+#                     "transforms": "insert",
+#                     "transforms.insert.type": "org.apache.kafka.connect.transforms.InsertField$Value",
+#                     "transforms.insert.timestamp.field": "mytimestamp"
+#           }' \
+#      http://localhost:8083/connectors/mongodb-sink/config | jq .
 
 
 # [2022-06-14 08:05:01,435] ERROR [mongodb-sink|task-0] WorkerSinkTask{id=mongodb-sink-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:207)
@@ -106,6 +106,8 @@ curl -X PUT \
                     "database":"inventory",
                     "collection":"customers",
                     "topics":"orders_json",
+                    "timeseries.timefield": "mytimestamp",
+                    "timeseries.metafield": "vinc",
 
                     "transforms": "insert",
                     "transforms.insert.type": "org.apache.kafka.connect.transforms.InsertField$Value",
@@ -120,6 +122,50 @@ docker exec -i mongodb mongosh << EOF
 use inventory
 db.customers.find().pretty();
 EOF
+
+
+# [2022-06-14 12:35:34,282] ERROR [mongodb-sink-json|task-0] WorkerSinkTask{id=mongodb-sink-json-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:207)
+# org.apache.kafka.connect.errors.ConnectException: Exiting WorkerSinkTask due to unrecoverable exception.
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.deliverMessages(WorkerSinkTask.java:618)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.poll(WorkerSinkTask.java:334)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.iteration(WorkerSinkTask.java:235)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:204)
+#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:200)
+#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:255)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:829)
+# Caused by: org.apache.kafka.connect.errors.DataException: com.mongodb.MongoBulkWriteException: Bulk write operation error on server mongodb:27017. Write errors: [BulkWriteError{index=0, code=2, message=''mytimestamp' must be present and contain a valid BSON UTC datetime value', details={}}]. 
+#         at com.mongodb.kafka.connect.sink.StartedMongoSinkTask.handleTolerableWriteException(StartedMongoSinkTask.java:168)
+#         at com.mongodb.kafka.connect.sink.StartedMongoSinkTask.bulkWriteBatch(StartedMongoSinkTask.java:111)
+#         at java.base/java.util.ArrayList.forEach(ArrayList.java:1541)
+#         at com.mongodb.kafka.connect.sink.StartedMongoSinkTask.put(StartedMongoSinkTask.java:76)
+#         at com.mongodb.kafka.connect.sink.MongoSinkTask.put(MongoSinkTask.java:90)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.deliverMessages(WorkerSinkTask.java:584)
+#         ... 10 more
+# Caused by: com.mongodb.MongoBulkWriteException: Bulk write operation error on server mongodb:27017. Write errors: [BulkWriteError{index=0, code=2, message=''mytimestamp' must be present and contain a valid BSON UTC datetime value', details={}}]. 
+#         at com.mongodb.internal.connection.BulkWriteBatchCombiner.getError(BulkWriteBatchCombiner.java:167)
+#         at com.mongodb.internal.connection.BulkWriteBatchCombiner.throwOnError(BulkWriteBatchCombiner.java:192)
+#         at com.mongodb.internal.connection.BulkWriteBatchCombiner.getResult(BulkWriteBatchCombiner.java:136)
+#         at com.mongodb.internal.operation.BulkWriteBatch.getResult(BulkWriteBatch.java:224)
+#         at com.mongodb.internal.operation.MixedBulkWriteOperation.executeBulkWriteBatch(MixedBulkWriteOperation.java:363)
+#         at com.mongodb.internal.operation.MixedBulkWriteOperation.lambda$execute$2(MixedBulkWriteOperation.java:260)
+#         at com.mongodb.internal.operation.OperationHelper.lambda$withSourceAndConnection$2(OperationHelper.java:575)
+#         at com.mongodb.internal.operation.OperationHelper.withSuppliedResource(OperationHelper.java:600)
+#         at com.mongodb.internal.operation.OperationHelper.lambda$withSourceAndConnection$3(OperationHelper.java:574)
+#         at com.mongodb.internal.operation.OperationHelper.withSuppliedResource(OperationHelper.java:600)
+#         at com.mongodb.internal.operation.OperationHelper.withSourceAndConnection(OperationHelper.java:573)
+#         at com.mongodb.internal.operation.MixedBulkWriteOperation.lambda$execute$3(MixedBulkWriteOperation.java:232)
+#         at com.mongodb.internal.async.function.RetryingSyncSupplier.get(RetryingSyncSupplier.java:65)
+#         at com.mongodb.internal.operation.MixedBulkWriteOperation.execute(MixedBulkWriteOperation.java:268)
+#         at com.mongodb.internal.operation.MixedBulkWriteOperation.execute(MixedBulkWriteOperation.java:84)
+#         at com.mongodb.client.internal.MongoClientDelegate$DelegateOperationExecutor.execute(MongoClientDelegate.java:212)
+#         at com.mongodb.client.internal.MongoCollectionImpl.executeBulkWrite(MongoCollectionImpl.java:443)
+#         at com.mongodb.client.internal.MongoCollectionImpl.bulkWrite(MongoCollectionImpl.java:423)
+#         at com.mongodb.kafka.connect.sink.StartedMongoSinkTask.bulkWriteBatch(StartedMongoSinkTask.java:104)
+#         ... 14 more
 
 # [
 #   {
