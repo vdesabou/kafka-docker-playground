58ba9cb0c1737a1a0124b5a77f860592fb5588a3
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-06-16 10:55:00 +0000

    wip (issue when restart)

--- reproduction-models/connect-connect-cdc-oracle19-source/107133-ora-startup-scripts-pdb-table/02_populate_customers.sh
+++ reproduction-models/connect-connect-cdc-oracle19-source/107133-ora-startup-scripts-pdb-table/02_populate_customers.sh
@@ -4,8 +4,8 @@ echo 'Populating CUSTOMERS table'
 
 sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLPDB1  <<- EOF
 
-insert into CUSTOMERS (RECID, XMLRECORD) values ('1',XMLType('<Warehouse whNo="100"> <Building>Owned</Building></Warehouse>'));
-insert into CUSTOMERS (RECID, XMLRECORD) values ('2',XMLType('<Warehouse whNo="100"> <Building>Owned</Building></Warehouse>'));
+insert into CUSTOMERS (RECID, XMLRECORD) values ('1',XMLType('<Warehouse whNo="1"> <Building>Owned</Building></Warehouse>'));
+insert into CUSTOMERS (RECID, XMLRECORD) values ('2',XMLType('<Warehouse whNo="2"> <Building>Owned</Building></Warehouse>'));
 
   exit;
 EOF
--- reproduction-models/connect-connect-cdc-oracle19-source/cdc-oracle19-pdb-table-repro-107133-npe-exception-processing-lob-column.sh
+++ reproduction-models/connect-connect-cdc-oracle19-source/cdc-oracle19-pdb-table-repro-107133-npe-exception-processing-lob-column.sh
@@ -44,6 +44,14 @@ docker exec connect kafka-topics --create --topic redo-log-topic --bootstrap-ser
 log "redo-log-topic is created"
 sleep 5
 
+curl --request PUT \
+  --url http://localhost:8083/admin/loggers/io.confluent.connect.oracle.cdc \
+  --header 'Accept: application/json' \
+  --header 'Content-Type: application/json' \
+  --data '{
+ "level": "TRACE"
+}'
+
 log "Creating Oracle source connector"
 curl -X PUT \
      -H "Content-Type: application/json" \
@@ -75,7 +83,7 @@ curl -X PUT \
 
                "behavior.on.dictionary.mismatch":"log",
                "behavior.on.unparsable.statement":"log",
-               "lob.topic.name.template": "LOB",
+               "lob.topic.name.template": "${tableName}-${columnName}",
                "redo.log.consumer.isolation.level": "read_committed",
                "enable.large.lob.object.support": "true",
                "error.deadletterqueue.topic.name": "dlq",
@@ -97,10 +105,62 @@ docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLPDB1 <
   exit;
 EOF
 
+docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLPDB1 << EOF
+  insert into CUSTOMERS (RECID, XMLRECORD) values ('4',XMLType('<Warehouse whNo="4"> <Building>Owned</Building></Warehouse>'));
+  exit;
+EOF
+
+docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLPDB1 << EOF
+  insert into CUSTOMERS (RECID, XMLRECORD) values ('5',XMLType('<Warehouse whNo="5"> <Building>Owned</Building></Warehouse>'));
+  exit;
+EOF
+
+docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLPDB1 << EOF
+  insert into CUSTOMERS (RECID, XMLRECORD) values ('6',XMLType('<Warehouse whNo="6"> <Building>Owned</Building></Warehouse>'));
+  exit;
+EOF
+
+docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLPDB1 << EOF
+  insert into CUSTOMERS (RECID, XMLRECORD) values ('7',XMLType('<Warehouse whNo="7"> <Building>Owned</Building></Warehouse>'));
+  exit;
+EOF
+
+docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLPDB1 << EOF
+  insert into CUSTOMERS (RECID, XMLRECORD) values ('9',XMLType('<Warehouse whNo="9"> <Building>Owned</Building></Warehouse>'));
+  exit;
+EOF
+
+docker exec -i oracle sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLPDB1 << EOF
+  insert into CUSTOMERS (RECID, XMLRECORD) values ('11',XMLType('<Warehouse whNo="10"> <Building>Owned</Building></Warehouse>'));
+  exit;
+EOF
+
 set +e
 timeout 20 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic ORCLPDB1.C__MYUSER.CUSTOMERS --from-beginning
 
 log "Verifying topic redo-log-topic: there should be 9 records"
 timeout 20 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic redo-log-topic --from-beginning
 
+timeout 20 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic CUSTOMERS-XMLRECORD --from-beginning
 
+
+exit 0
+
+# https://confluent.slack.com/archives/C03KEUWN8EL/p1655376778232009
+# curl -X POST localhost:8083/connectors/cdc-oracle-source-pdb/tasks/0/restart
+# curl -X POST localhost:8083/connectors/cdc-oracle-source-pdb/tasks/1/restart
+
+# After restart of tasks:
+
+# [2022-06-16 10:32:37,276] TRACE [cdc-oracle-source-pdb|task-1|changeEvent] Received record from redo log (io.confluent.connect.oracle.cdc.ChangeEventGenerator:373)
+# [2022-06-16 10:32:37,276] TRACE [cdc-oracle-source-pdb|task-1|changeEvent] Converting value of struct type (io.confluent.connect.oracle.cdc.ChangeEventGenerator:381)
+# [2022-06-16 10:32:37,277] TRACE [cdc-oracle-source-pdb|task-1|changeEvent] Redo log record received: Struct{SCN=2173963,START_SCN=2173962,COMMIT_SCN=2173966,TIMESTAMP=Thu Jun 16 10:32:04 GMT 2022,START_TIMESTAMP=Thu Jun 16 10:32:04 GMT 2022,COMMIT_TIMESTAMP=Thu Jun 16 10:32:04 GMT 2022,XIDUSN=10,XIDSLT=2,XIDSQN=650,XID=java.nio.HeapByteBuffer[pos=0 lim=8 cap=8],PXIDUSN=10,PXIDSLT=2,PXIDSQN=650,PXID=java.nio.HeapByteBuffer[pos=0 lim=8 cap=8],OPERATION=XML DOC WRITE,OPERATION_CODE=70,ROLLBACK=false,SEG_OWNER=C##MYUSER,SEG_NAME=CUSTOMERS,TABLE_NAME=CUSTOMERS,SEG_TYPE=2,SEG_TYPE_NAME=TABLE,TABLE_SPACE=USERS,ROW_ID=AAAAAAAAAAAAAAAAAA,USERNAME=C##MYUSER,OS_USERNAME=oracle,MACHINE_NAME=oracle,AUDIT_SESSIONID=10014,SESSION_NUM=367,SERIAL_NUM=37507,SESSION_INFO=login_username=C##MYUSER client_info= OS_username=oracle Machine_name=oracle OS_terminal=pts/0 OS_process_id=1099 OS_program_name=sqlplus@oracle (TNS V1-V3),THREAD_NUM=1,SEQUENCE_NUM=4,RBASQN=7,RBABLK=35242,RBABYTE=312,UBAFIL=11,UBABLK=0,UBAREC=0,UBASQN=0,ABS_FILE_NUM=0,REL_FILE_NUM=0,DATA_BLK_NUM=0,DATA_OBJ_NUM=72994,DATA_OBJV_NUM=2,DATA_OBJD_NUM=0,SQL_REDO=XML_REDO := '<Warehouse whNo="8"> <Building>Owned</Building></Warehouse>'
+#  amount: 59,RS_ID= 0x000007.000089aa.0138 ,SSN=0,CSF=false,INFO=XML sql_redo not re-executable,STATUS=2,REDO_VALUE=30,UNDO_VALUE=31,SAFE_RESUME_SCN=0,CSCN=2173966,SRC_CON_NAME=ORCLPDB1,SRC_CON_ID=3,SRC_CON_UID=280798746,SRC_CON_DBID=0,CON_ID=false} (io.confluent.connect.oracle.cdc.ChangeEventGenerator:394)
+# [2022-06-16 10:32:37,277] TRACE [cdc-oracle-source-pdb|task-1|changeEvent] Record is self-contained, returning verbatim (io.confluent.connect.oracle.cdc.record.RedoLogRecordCombiner:151)
+# [2022-06-16 10:32:37,277] TRACE [cdc-oracle-source-pdb|task-1|changeEvent] Processing combined multi-part record: Struct{SCN=2173963,START_SCN=2173962,COMMIT_SCN=2173966,TIMESTAMP=Thu Jun 16 10:32:04 GMT 2022,START_TIMESTAMP=Thu Jun 16 10:32:04 GMT 2022,COMMIT_TIMESTAMP=Thu Jun 16 10:32:04 GMT 2022,XIDUSN=10,XIDSLT=2,XIDSQN=650,XID=java.nio.HeapByteBuffer[pos=0 lim=8 cap=8],PXIDUSN=10,PXIDSLT=2,PXIDSQN=650,PXID=java.nio.HeapByteBuffer[pos=0 lim=8 cap=8],OPERATION=XML DOC WRITE,OPERATION_CODE=70,ROLLBACK=false,SEG_OWNER=C##MYUSER,SEG_NAME=CUSTOMERS,TABLE_NAME=CUSTOMERS,SEG_TYPE=2,SEG_TYPE_NAME=TABLE,TABLE_SPACE=USERS,ROW_ID=AAAAAAAAAAAAAAAAAA,USERNAME=C##MYUSER,OS_USERNAME=oracle,MACHINE_NAME=oracle,AUDIT_SESSIONID=10014,SESSION_NUM=367,SERIAL_NUM=37507,SESSION_INFO=login_username=C##MYUSER client_info= OS_username=oracle Machine_name=oracle OS_terminal=pts/0 OS_process_id=1099 OS_program_name=sqlplus@oracle (TNS V1-V3),THREAD_NUM=1,SEQUENCE_NUM=4,RBASQN=7,RBABLK=35242,RBABYTE=312,UBAFIL=11,UBABLK=0,UBAREC=0,UBASQN=0,ABS_FILE_NUM=0,REL_FILE_NUM=0,DATA_BLK_NUM=0,DATA_OBJ_NUM=72994,DATA_OBJV_NUM=2,DATA_OBJD_NUM=0,SQL_REDO=XML_REDO := '<Warehouse whNo="8"> <Building>Owned</Building></Warehouse>'
+#  amount: 59,RS_ID= 0x000007.000089aa.0138 ,SSN=0,CSF=false,INFO=XML sql_redo not re-executable,STATUS=2,REDO_VALUE=30,UNDO_VALUE=31,SAFE_RESUME_SCN=0,CSCN=2173966,SRC_CON_NAME=ORCLPDB1,SRC_CON_ID=3,SRC_CON_UID=280798746,SRC_CON_DBID=0,CON_ID=false} (io.confluent.connect.oracle.cdc.ChangeEventGenerator:485)
+# [2022-06-16 10:32:37,277] TRACE [cdc-oracle-source-pdb|task-1|changeEvent] Change event record convertor skipping XML records (io.confluent.connect.oracle.cdc.record.OracleChangeEventSourceRecordConverter:335)
+# [2022-06-16 10:32:37,277] TRACE [cdc-oracle-source-pdb|task-1|changeEvent] Change event records: [] (io.confluent.connect.oracle.cdc.ChangeEventGenerator:506)
+# [2022-06-16 10:32:37,277] TRACE [cdc-oracle-source-pdb|task-1|changeEvent] Table schema Schema{STRUCT} did not contain LOB fields, skipping LOB convertor for Struct{SCN=2173963,START_SCN=2173962,COMMIT_SCN=2173966,TIMESTAMP=Thu Jun 16 10:32:04 GMT 2022,START_TIMESTAMP=Thu Jun 16 10:32:04 GMT 2022,COMMIT_TIMESTAMP=Thu Jun 16 10:32:04 GMT 2022,XIDUSN=10,XIDSLT=2,XIDSQN=650,XID=java.nio.HeapByteBuffer[pos=0 lim=8 cap=8],PXIDUSN=10,PXIDSLT=2,PXIDSQN=650,PXID=java.nio.HeapByteBuffer[pos=0 lim=8 cap=8],OPERATION=XML DOC WRITE,OPERATION_CODE=70,ROLLBACK=false,SEG_OWNER=C##MYUSER,SEG_NAME=CUSTOMERS,TABLE_NAME=CUSTOMERS,SEG_TYPE=2,SEG_TYPE_NAME=TABLE,TABLE_SPACE=USERS,ROW_ID=AAAAAAAAAAAAAAAAAA,USERNAME=C##MYUSER,OS_USERNAME=oracle,MACHINE_NAME=oracle,AUDIT_SESSIONID=10014,SESSION_NUM=367,SERIAL_NUM=37507,SESSION_INFO=login_username=C##MYUSER client_info= OS_username=oracle Machine_name=oracle OS_terminal=pts/0 OS_process_id=1099 OS_program_name=sqlplus@oracle (TNS V1-V3),THREAD_NUM=1,SEQUENCE_NUM=4,RBASQN=7,RBABLK=35242,RBABYTE=312,UBAFIL=11,UBABLK=0,UBAREC=0,UBASQN=0,ABS_FILE_NUM=0,REL_FILE_NUM=0,DATA_BLK_NUM=0,DATA_OBJ_NUM=72994,DATA_OBJV_NUM=2,DATA_OBJD_NUM=0,SQL_REDO=XML_REDO := '<Warehouse whNo="8"> <Building>Owned</Building></Warehouse>'
+#  amount: 59,RS_ID= 0x000007.000089aa.0138 ,SSN=0,CSF=false,INFO=XML sql_redo not re-executable,STATUS=2,REDO_VALUE=30,UNDO_VALUE=31,SAFE_RESUME_SCN=0,CSCN=2173966,SRC_CON_NAME=ORCLPDB1,SRC_CON_ID=3,SRC_CON_UID=280798746,SRC_CON_DBID=0,CON_ID=false} (io.confluent.connect.oracle.cdc.record.OracleLobRecordConverter:85)
+# [2022-06-16 10:32:37,277] TRACE [cdc-oracle-source-pdb|task-1|changeEvent] LOB records: [] (io.confluent.connect.oracle.cdc.ChangeEventGenerator:513)
