beb6cad0c570a1a0801258b4dce5a7fd6546c7e1
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-06-17 14:15:23 +0000

    wip

---
+++ reproduction-models/connect-connect-aws-s3-source/s3-source-generalized-repro-110294-generalized-used-for-backup.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if ! version_gt $TAG_BASE "5.9.99" && version_gt $CONNECTOR_TAG "1.9.9"
+then
+    logwarn "WARN: connector version >= 2.0.0 do not support CP versions < 6.0.0"
+    exit 111
+fi
+
+if [ ! -f $HOME/.aws/config ]
+then
+     logerror "ERROR: $HOME/.aws/config is not set"
+     exit 1
+fi
+if [ -z "$AWS_CREDENTIALS_FILE_NAME" ]
+then
+    export AWS_CREDENTIALS_FILE_NAME="credentials"
+fi
+if [ ! -f $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME ]
+then
+     logerror "ERROR: $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME is not set"
+     exit 1
+fi
+
+if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
+then
+     export CONNECT_CONTAINER_HOME_DIR="/home/appuser"
+else
+     export CONNECT_CONTAINER_HOME_DIR="/root"
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.backup-and-restore.yml"
+
+AWS_BUCKET_NAME=kafka-docker-playground-bucket-${USER}${TAG}
+AWS_BUCKET_NAME=${AWS_BUCKET_NAME//[-.]/}
+
+AWS_REGION=$(aws configure get region | tr '\r' '\n')
+
+log "Creating bucket name <$AWS_BUCKET_NAME>, if required"
+set +e
+aws s3api create-bucket --bucket $AWS_BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+set -e
+log "Empty bucket <$AWS_BUCKET_NAME>, if required"
+set +e
+aws s3 rm s3://$AWS_BUCKET_NAME --recursive --region $AWS_REGION
+set -e
+
+log "Creating S3 Sink connector with bucket name <$AWS_BUCKET_NAME>"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.s3.S3SinkConnector",
+               "tasks.max": "1",
+               "topics": "s3_topic",
+               "s3.region": "'"$AWS_REGION"'",
+               "s3.bucket.name": "'"$AWS_BUCKET_NAME"'",
+               "s3.part.size": 5242880,
+               "flush.size": "3",
+               "storage.class": "io.confluent.connect.s3.storage.S3Storage",
+               "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
+               "partitioner.class": "io.confluent.connect.storage.partitioner.DailyPartitioner",
+               "rotate.schedule.interval.ms": "60000",
+               "locale": "en_US",
+               "timezone": "UTC",
+               "schema.compatibility": "NONE"
+          }' \
+     http://localhost:8083/connectors/s3-sink/config | jq .
+
+
+log "Sending messages to topic s3_topic"
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic s3_topic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+log "Sleep 10 seconds"
+sleep 10
+
+log "Listing objects of in S3"
+aws s3api list-objects --bucket "$AWS_BUCKET_NAME"
+
+# topics/s3_topic/year=2022/month=06/day=17/s3_topic+0+0000000521.avro
+
+log "Creating GENERIC S3 Source connector with bucket name <$AWS_BUCKET_NAME>"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "tasks.max": "1",
+               "connector.class": "io.confluent.connect.s3.source.S3SourceConnector",
+               "s3.region": "'"$AWS_REGION"'",
+               "s3.bucket.name": "'"$AWS_BUCKET_NAME"'",
+               "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1",
+               "mode": "GENERIC",
+               "topics.dir": "topics\\/s3_topic\\/year=\\d\\d\\d\\d\\/month=\\d\\d\\/day=\\d\\d\\/.*\\.avro",
+               "topic.regex.list": "copy_of_s3_topic:.*"
+          }' \
+     http://localhost:8083/connectors/s3-source/config | jq .
+
+
+log "Verifying topic copy_of_s3_topic"
+timeout 60 docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic copy_of_s3_topic --from-beginning --max-messages 9
