e853c499d1922c3aaf681a8f68c83b1337e54a30
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-06-24 15:51:00 +0000

    repro

---
+++ reproduction-models/connect-connect-aws-s3-sink/s3-sink-repro-111183-expected-compressionfilter-to-be-a-deflateroutputstream,-but-was-passed-an-instance-that-does-not-match-that-type.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f $HOME/.aws/config ]
+then
+     logerror "ERROR: $HOME/.aws/config is not set"
+     exit 1
+fi
+if [ -z "$AWS_CREDENTIALS_FILE_NAME" ]
+then
+    export AWS_CREDENTIALS_FILE_NAME="credentials"
+fi
+if [ ! -f $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME ]
+then
+     logerror "ERROR: $HOME/.aws/$AWS_CREDENTIALS_FILE_NAME is not set"
+     exit 1
+fi
+
+if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
+then
+     export CONNECT_CONTAINER_HOME_DIR="/home/appuser"
+else
+     export CONNECT_CONTAINER_HOME_DIR="/root"
+fi
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-111183-expected-compressionfilter-to-be-a-deflateroutputstream,-but-was-passed-an-instance-that-does-not-match-that-type.yml"
+
+AWS_BUCKET_NAME=kafka-docker-playground-bucket-${USER}${TAG}
+AWS_BUCKET_NAME=${AWS_BUCKET_NAME//[-.]/}
+
+AWS_REGION=$(aws configure get region | tr '\r' '\n')
+log "Creating bucket name <$AWS_BUCKET_NAME>, if required"
+set +e
+aws s3api create-bucket --bucket $AWS_BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+set -e
+log "Empty bucket <$AWS_BUCKET_NAME>, if required"
+set +e
+aws s3 rm s3://$AWS_BUCKET_NAME --recursive --region $AWS_REGION
+set -e
+
+log "Creating S3 Sink connector with bucket name <$AWS_BUCKET_NAME>"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "io.confluent.connect.s3.S3SinkConnector",
+               "tasks.max": "1",
+               "topics": "s3_topic",
+               "s3.region": "'"$AWS_REGION"'",
+               "s3.bucket.name": "'"$AWS_BUCKET_NAME"'",
+               "s3.part.size": 52428801,
+               "flush.size": "3",
+               "storage.class": "io.confluent.connect.s3.storage.S3Storage",
+               "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
+               "s3.compression.type": "gzip",
+               "s3.compression.level": "9",
+               "s3.part.retries" : "10000",
+               "s3.part.size": "5242880",               
+               "schema.compatibility": "NONE"
+          }' \
+     http://localhost:8083/connectors/s3-sink/config | jq .
+
+# [2022-06-24 15:47:59,922] ERROR [s3-sink|task-0] WorkerSinkTask{id=s3-sink-0} Task threw an uncaught and unrecoverable exception. Task is being killed and will not recover until manually restarted (org.apache.kafka.connect.runtime.WorkerTask:207)
+# org.apache.kafka.connect.errors.ConnectException: Exiting WorkerSinkTask due to unrecoverable exception.
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.deliverMessages(WorkerSinkTask.java:618)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.poll(WorkerSinkTask.java:334)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.iteration(WorkerSinkTask.java:235)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.execute(WorkerSinkTask.java:204)
+#         at org.apache.kafka.connect.runtime.WorkerTask.doRun(WorkerTask.java:200)
+#         at org.apache.kafka.connect.runtime.WorkerTask.run(WorkerTask.java:255)
+#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:515)
+#         at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
+#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
+#         at java.base/java.lang.Thread.run(Thread.java:829)
+# Caused by: org.apache.kafka.connect.errors.ConnectException: Expected compressionFilter to be a DeflaterOutputStream, but was passed an instance that does not match that type.
+#         at io.confluent.connect.s3.storage.CompressionType$1.finalize(CompressionType.java:77)
+#         at io.confluent.connect.s3.storage.S3OutputStream.commit(S3OutputStream.java:159)
+#         at io.confluent.connect.s3.format.avro.AvroRecordWriterProvider$1.commit(AvroRecordWriterProvider.java:96)
+#         at io.confluent.connect.s3.format.S3RetriableRecordWriter.commit(S3RetriableRecordWriter.java:55)
+#         at io.confluent.connect.s3.format.KeyValueHeaderRecordWriterProvider$1.commit(KeyValueHeaderRecordWriterProvider.java:129)
+#         at io.confluent.connect.s3.TopicPartitionWriter.commitFile(TopicPartitionWriter.java:627)
+#         at io.confluent.connect.s3.TopicPartitionWriter.commitFiles(TopicPartitionWriter.java:600)
+#         at io.confluent.connect.s3.TopicPartitionWriter.executeState(TopicPartitionWriter.java:257)
+#         at io.confluent.connect.s3.TopicPartitionWriter.write(TopicPartitionWriter.java:197)
+#         at io.confluent.connect.s3.S3SinkTask.put(S3SinkTask.java:234)
+#         at org.apache.kafka.connect.runtime.WorkerSinkTask.deliverMessages(WorkerSinkTask.java:584)
+#         ... 10 more
+
+log "Sending messages to topic s3_topic"
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic s3_topic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+sleep 10
+
+log "Listing objects of in S3"
+aws s3api list-objects --bucket "$AWS_BUCKET_NAME"
+
+log "Getting one of the avro files locally and displaying content with avro-tools"
+aws s3 cp --only-show-errors s3://$AWS_BUCKET_NAME/topics/s3_topic/partition=0/s3_topic+0+0000000000.avro s3_topic+0+0000000000.avro
+
+docker run --rm -v ${DIR}:/tmp actions/avro-tools tojson /tmp/s3_topic+0+0000000000.avro
+rm -f s3_topic+0+0000000000.avro
