8ac667570cf9630138a6c0cc362219e392db2a54
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-06-27 15:56:01 +0000

    update

---
+++ reproduction-models/connect-connect-jdbc-oracle19-source/oracle19-repro-106531-number-18-as-bytes.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+create_or_get_oracle_image "LINUX.X64_193000_db_home.zip" "../../connect/connect-jdbc-oracle19-source/ora-setup-scripts"
+
+if [ ! -z "$CONNECTOR_TAG" ]
+then
+     JDBC_CONNECTOR_VERSION=$CONNECTOR_TAG
+else
+     JDBC_CONNECTOR_VERSION=$(docker run vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} cat /usr/share/confluent-hub-components/confluentinc-kafka-connect-jdbc/manifest.json | jq -r '.version')
+fi
+log "JDBC Connector version is $JDBC_CONNECTOR_VERSION"
+if ! version_gt $JDBC_CONNECTOR_VERSION "9.9.9"; then
+     get_3rdparty_file "ojdbc8.jar"
+     if [ ! -f ${DIR}/ojdbc8.jar ]
+     then
+          logerror "ERROR: ${DIR}/ojdbc8.jar is missing. It must be downloaded manually in order to acknowledge user agreement"
+          exit 1
+     fi
+     ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+else
+     log "ojdbc jar is shipped with connector (starting with 10.0.0)"
+     ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.no-ojdbc.repro-106531-number-18-as-bytes.yml"
+fi
+
+# Verify Oracle DB has started within MAX_WAIT seconds
+MAX_WAIT=2500
+CUR_WAIT=0
+log "⌛ Waiting up to $MAX_WAIT seconds for Oracle DB to start"
+docker container logs oracle > /tmp/out.txt 2>&1
+while [[ ! $(cat /tmp/out.txt) =~ "DONE: Executing user defined scripts" ]]; do
+sleep 10
+docker container logs oracle > /tmp/out.txt 2>&1
+CUR_WAIT=$(( CUR_WAIT+10 ))
+if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+     logerror "ERROR: The logs in oracle container do not show 'DONE: Executing user defined scripts' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+     exit 1
+fi
+done
+log "Oracle DB has started!"
+
+log "Creating Oracle source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class":"io.confluent.connect.jdbc.JdbcSourceConnector",
+                    "tasks.max":"1",
+                    "connection.user": "myuser",
+                    "connection.password": "mypassword",
+                    "connection.url": "jdbc:oracle:thin:@oracle:1521/ORCLPDB1",
+                    "numeric.mapping":"best_fit",
+                    "mode":"timestamp",
+                    "poll.interval.ms":"1000",
+                    "validate.non.null":"false",
+                    "table.whitelist":"CUSTOMERS",
+                    "timestamp.column.name":"UPDATE_TS",
+                    "topic.prefix":"oracle-",
+                    "errors.log.enable": "true",
+                    "errors.log.include.messages": "true"
+          }' \
+     http://localhost:8083/connectors/oracle-source/config | jq .
+
+
+sleep 5
+
+log "Verifying topic oracle-CUSTOMERS"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic oracle-CUSTOMERS --from-beginning --max-messages 2
+
+# {"ID":1,"FIRST_NAME":{"string":"Rica"},"LAST_NAME":{"string":"Blaisdell"},"EMAIL":{"string":"rblaisdell0@rambler.ru"},"GENDER":{"string":"Female"},"CLUB_STATUS":{"string":"bronze"},"COMMENTS":{"string":"Universal optimal hierarchy"},"CREATE_TS":{"long":1656344865594},"UPDATE_TS":{"long":1656344865000},"MYNUMBER":{"long":1245169279}}
+# {"ID":2,"FIRST_NAME":{"string":"Ruthie"},"LAST_NAME":{"string":"Brockherst"},"EMAIL":{"string":"rbrockherst1@ow.ly"},"GENDER":{"string":"Female"},"CLUB_STATUS":{"string":"platinum"},"COMMENTS":{"string":"Reverse-engineered tangible interface"},"CREATE_TS":{"long":1656344865596},"UPDATE_TS":{"long":1656344865000},"MYNUMBER":{"long":1245169279}}
+
+#     {
+#       "default": null,
+#       "name": "MYNUMBER",
+#       "type": [
+#         "null",
+#         "long"
+#       ]
+#     }
