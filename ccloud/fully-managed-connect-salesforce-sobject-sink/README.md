e7f6b0d55903a80a87e36cdf55f363a9320726d2
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-07-22 13:22:40 +0200

    Added example

---
+++ ccloud/fully-managed-connect-salesforce-sobject-sink/README.md
@@ -0,0 +1,88 @@
+# Fully Managed Salesforce SObject Sink connector
+
+
+
+## Objective
+
+Quickly test [Salesforce SObject Sink](https://docs.confluent.io/cloud/current/connectors/cc-salesforce-SObjects-sink.html) connector.
+
+## Prerequisites
+
+All you have to do is to be already logged in with [confluent CLI](https://docs.confluent.io/confluent-cli/current/overview.html#confluent-cli-overview).
+
+By default, a new Confluent Cloud environment with a Cluster will be created.
+
+You can configure the cluster by setting environment variables:
+
+* `CLUSTER_CLOUD`: The Cloud provider (possible values: `aws`, `gcp` and `azure`, default `aws`)
+* `CLUSTER_REGION`: The Cloud region (use `confluent kafka region list` to get the list, default `eu-west-2`)
+* `CLUSTER_TYPE`: The type of cluster (possible values: `basic`, `standard` and `dedicated`, default `basic`)
+* `ENVIRONMENT` (optional): The environment id where want your new cluster (example: `env-xxxxx`) 
+
+In case you want to use your own existing cluster, you need to setup these environment variables:
+
+* `ENVIRONMENT`: The environment id where your cluster is located (example: `env-xxxxx`) 
+* `CLUSTER_NAME`: The cluster name
+* `CLUSTER_CLOUD`: The Cloud provider (possible values: `aws`, `gcp` and `azure`)
+* `CLUSTER_REGION`: The Cloud region (example `us-east-2`)
+* `CLUSTER_CREDS`: The Kafka api key and secret to use, it should be separated with semi-colon (example: `<API_KEY>:<API_KEY_SECRET>`)
+* `SCHEMA_REGISTRY_CREDS` (optional, if not set, new one will be created): The Schema Registry api key and secret to use, it should be separated with semi-colon (example: `<SR_API_KEY>:<SR_API_KEY_SECRET>`)
+
+## Register a test account
+
+Go to [Salesforce developer portal](https://developer.salesforce.com/signup/) and register an account.
+
+## Register another test account
+
+Go to [Salesforce developer portal](https://developer.salesforce.com/signup/) and register an account.
+
+## Salesforce Account
+
+### Create a new Connected App
+
+Full details available [here](https://docs.confluent.io/current/connect/kafka-connect-salesforce/pushtopics/salesforce_pushtopic_source_connector_quickstart.html#salesforce-account)
+
+Steps are:
+
+* Select the gear icon in the upper right hand corner and choose Setup.
+
+* Enter App in the Quick Find search box, and choose *App Manager* in the filtered results.
+
+* Click the *New Connected App* button in the upper right corner of the Setup panel.
+
+![Create a connected app](Screenshot2.png)
+
+* Supply a Connected App Name, API Name, and Contact Email.
+
+* Select *Enable OAuth Settings* checkbox and select the *Enable for Device Flow* checkbox. These selections enable the connector to use the Salesforce API.
+* Under the *Select OAuth Scopes* field, select all of the items under Available OAuth scopes and add them to the *Selected OAuth Scopes*.
+
+Example:
+
+![Create a connected app](Screenshot3.png)
+
+* Save the new app and press Continue at the prompt.
+* Look for the Consumer Key and Consumer Secret in the displayed form. Save these so you can put them in the configuration properties file for the Salesforce connect worker.
+
+### Find your Security token
+
+Find your Security Token (emailed to you from Salesforce.com). If you need to reset your token or view your profile on Salesforce.com, select `Settings->My Personal Information->Reset My Security Token` and follow the instructions.
+
+![security token](Screenshot1.png)
+
+## Create CustomId External ID
+
+Follow instructions [here](https://docs.confluent.io/current/connect/kafka-connect-salesforce/sobjects_sink/index.html#external-id), **for both accounts**.
+
+![custom PushTopic](Screenshot4.png)
+
+## How to run
+
+Simply run:
+
+```
+$ ./fully-managed-salesforce-sobject-sink.sh <SALESFORCE_USERNAME> <SALESFORCE_PASSWORD> <SALESFORCE_CONSUMER_KEY> <SALESFORCE_CONSUMER_PASSWORD> <SALESFORCE_SECURITY_TOKEN> <SALESFORCE_USERNAME_ACCOUNT2> <SALESFORCE_PASSWORD_ACCOUNT2> <SALESFORCE_SECURITY_TOKEN_ACCOUNT2> <SALESFORCE_CONSUMER_KEY_ACCOUNT2> <SALESFORCE_CONSUMER_PASSWORD_ACCOUNT2>
+```
+
+Note: you can also export these values as environment variable
+
---
+++ ccloud/fully-managed-connect-salesforce-sobject-sink/fully-managed-salesforce-sobject-sink.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+SALESFORCE_USERNAME=${SALESFORCE_USERNAME:-$1}
+SALESFORCE_PASSWORD=${SALESFORCE_PASSWORD:-$2}
+SALESFORCE_CONSUMER_KEY=${SALESFORCE_CONSUMER_KEY:-$3}
+SALESFORCE_CONSUMER_PASSWORD=${SALESFORCE_CONSUMER_PASSWORD:-$4}
+SALESFORCE_SECURITY_TOKEN=${SALESFORCE_SECURITY_TOKEN:-$5}
+SALESFORCE_INSTANCE=${SALESFORCE_INSTANCE:-"https://login.salesforce.com"}
+
+# second account (for SObject sink)
+SALESFORCE_USERNAME_ACCOUNT2=${SALESFORCE_USERNAME_ACCOUNT2:-$6}
+SALESFORCE_PASSWORD_ACCOUNT2=${SALESFORCE_PASSWORD_ACCOUNT2:-$7}
+SALESFORCE_SECURITY_TOKEN_ACCOUNT2=${SALESFORCE_SECURITY_TOKEN_ACCOUNT2:-$8}
+SALESFORCE_CONSUMER_KEY_ACCOUNT2=${SALESFORCE_CONSUMER_KEY_ACCOUNT2:-$9}
+SALESFORCE_CONSUMER_PASSWORD_ACCOUNT2=${SALESFORCE_CONSUMER_PASSWORD_ACCOUNT2:-$10}
+SALESFORCE_INSTANCE_ACCOUNT2=${SALESFORCE_INSTANCE_ACCOUNT2:-"https://login.salesforce.com"}
+
+if [ -z "$SALESFORCE_USERNAME" ]
+then
+     logerror "SALESFORCE_USERNAME is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_PASSWORD" ]
+then
+     logerror "SALESFORCE_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+
+if [ -z "$SALESFORCE_CONSUMER_KEY" ]
+then
+     logerror "SALESFORCE_CONSUMER_KEY is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_CONSUMER_PASSWORD" ]
+then
+     logerror "SALESFORCE_CONSUMER_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_SECURITY_TOKEN" ]
+then
+     logerror "SALESFORCE_SECURITY_TOKEN is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_USERNAME_ACCOUNT2" ]
+then
+     logerror "SALESFORCE_USERNAME_ACCOUNT2 is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_PASSWORD_ACCOUNT2" ]
+then
+     logerror "SALESFORCE_PASSWORD_ACCOUNT2 is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_SECURITY_TOKEN_ACCOUNT2" ]
+then
+     logerror "SALESFORCE_SECURITY_TOKEN_ACCOUNT2 is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_CONSUMER_KEY_ACCOUNT2" ]
+then
+     logerror "SALESFORCE_CONSUMER_KEY_ACCOUNT2 is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_CONSUMER_PASSWORD_ACCOUNT2" ]
+then
+     logerror "SALESFORCE_CONSUMER_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+PUSH_TOPICS_NAME=MyLeadPushTopics${TAG}
+PUSH_TOPICS_NAME=${PUSH_TOPICS_NAME//[-._]/}
+
+sed -e "s|:PUSH_TOPIC_NAME:|$PUSH_TOPICS_NAME|g" \
+    ${DIR}/MyLeadPushTopics-template.apex > ${DIR}/MyLeadPushTopics.apex
+
+bootstrap_ccloud_environment
+
+if [ -f /tmp/delta_configs/env.delta ]
+then
+     source /tmp/delta_configs/env.delta
+else
+     logerror "ERROR: /tmp/delta_configs/env.delta has not been generated"
+     exit 1
+fi
+
+set +e
+delete_topic sfdc-pushtopic-leads
+sleep 3
+create_topic sfdc-pushtopic-leads
+set -e
+
+docker-compose build
+docker-compose down -v --remove-orphans
+docker-compose up -d
+
+# the Salesforce PushTopic source connector is used to get data into Kafka and the Salesforce SObject sink connector is used to export data from Kafka to Salesforce
+
+log "Login with sfdx CLI"
+docker exec sfdx-cli sh -c "sfdx sfpowerkit:auth:login -u \"$SALESFORCE_USERNAME\" -p \"$SALESFORCE_PASSWORD\" -r \"$SALESFORCE_INSTANCE\" -s \"$SALESFORCE_SECURITY_TOKEN\""
+
+log "Delete $PUSH_TOPICS_NAME, if required"
+set +e
+docker exec -i sfdx-cli sh -c "sfdx force:apex:execute  -u \"$SALESFORCE_USERNAME\"" << EOF
+List<PushTopic> pts = [SELECT Id FROM PushTopic WHERE Name = '$PUSH_TOPICS_NAME'];
+Database.delete(pts);
+EOF
+set -e
+log "Create $PUSH_TOPICS_NAME"
+docker exec sfdx-cli sh -c "sfdx force:apex:execute  -u \"$SALESFORCE_USERNAME\" -f \"/tmp/MyLeadPushTopics.apex\""
+
+LEAD_FIRSTNAME=John_$RANDOM
+LEAD_LASTNAME=Doe_$RANDOM
+log "Add a Lead to Salesforce: $LEAD_FIRSTNAME $LEAD_LASTNAME"
+docker exec sfdx-cli sh -c "sfdx force:data:record:create  -u \"$SALESFORCE_USERNAME\" -s Lead -v \"FirstName='$LEAD_FIRSTNAME' LastName='$LEAD_LASTNAME' Company=Confluent\""
+
+log "Creating Salesforce PushTopics Source connector"
+cat << EOF > connector.json
+{
+     "connector.class": "SalesforcePushTopicSource",
+     "name": "SalesforcePushTopicSource",
+     "kafka.auth.mode": "KAFKA_API_KEY",
+     "kafka.api.key": "$CLOUD_KEY",
+     "kafka.api.secret": "$CLOUD_SECRET",
+     "kafka.topic": "sfdc-pushtopic-leads",
+     "salesforce.object" : "Lead",
+     "salesforce.push.topic.name" : "$PUSH_TOPICS_NAME",
+     "salesforce.instance" : "$SALESFORCE_INSTANCE",
+     "salesforce.username" : "$SALESFORCE_USERNAME",
+     "salesforce.password" : "$SALESFORCE_PASSWORD",
+     "salesforce.password.token" : "$SALESFORCE_SECURITY_TOKEN",
+     "salesforce.consumer.key" : "$SALESFORCE_CONSUMER_KEY",
+     "salesforce.consumer.secret" : "$SALESFORCE_CONSUMER_PASSWORD",
+     "salesforce.initial.start" : "all",
+     "output.data.format": "AVRO",
+     "tasks.max": "1"
+}
+EOF
+
+log "Connector configuration is:"
+cat connector.json
+
+set +e
+log "Deleting fully managed connector, it might fail..."
+delete_ccloud_connector connector.json
+set -e
+
+log "Creating fully managed connector"
+create_ccloud_connector connector.json
+wait_for_ccloud_connector_up connector.json 300
+
+
+sleep 10
+
+log "Verify we have received the data in sfdc-pushtopic-leads topic"
+timeout 60 docker run --rm -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" -e SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" -e SCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL" vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} kafka-avro-console-consumer --topic sfdc-pushtopic-leads --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --property basic.auth.credentials.source=USER_INFO --property schema.registry.basic.auth.user.info="$SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO" --property schema.registry.url=$SCHEMA_REGISTRY_URL --from-beginning --max-messages 1 
+
+log "Creating Salesforce SObject Sink connector"
+cat << EOF > connector2.json
+{
+     "connector.class": "SalesforceSObjectSink",
+     "name": "SalesforceSObjectSink",
+     "kafka.auth.mode": "KAFKA_API_KEY",
+     "kafka.api.key": "$CLOUD_KEY",
+     "kafka.api.secret": "$CLOUD_SECRET",
+     "topics": "sfdc-pushtopic-leads",
+     "salesforce.object" : "Lead",
+     "salesforce.instance" : "$SALESFORCE_INSTANCE_ACCOUNT2",
+     "salesforce.username" : "$SALESFORCE_USERNAME_ACCOUNT2",
+     "salesforce.password" : "$SALESFORCE_PASSWORD_ACCOUNT2",
+     "salesforce.password.token" : "$SALESFORCE_SECURITY_TOKEN_ACCOUNT2",
+     "salesforce.consumer.key" : "$SALESFORCE_CONSUMER_KEY_ACCOUNT2",
+     "salesforce.consumer.secret" : "$SALESFORCE_CONSUMER_PASSWORD_ACCOUNT2",
+     "salesforce.use.custom.id.field" : "true",
+     "salesforce.custom.id.field.name" : "CustomId__c",
+
+     "salesforce.ignore.fields" : "CleanStatus",
+     "salesforce.ignore.reference.fields" : "true",
+     "salesforce.object.override.event.type": "true",
+     "salesforce.sink.object.operation": "upsert",
+     "input.data.format": "AVRO",
+     "tasks.max": "1"
+}
+EOF
+
+log "Connector configuration is:"
+cat connector2.json
+
+set +e
+log "Deleting fully managed connector, it might fail..."
+delete_ccloud_connector connector2.json
+set -e
+
+log "Creating fully managed connector"
+create_ccloud_connector connector2.json
+wait_for_ccloud_connector_up connector2.json 300
+
+sleep 40
+
+connectorName=$(cat connector2.json| jq -r .name)
+connectorId=$(get_ccloud_connector_lcc $connectorName)
+
+log "Verifying topic success-$connectorId"
+timeout 60 docker run --rm -e BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" -e SASL_JAAS_CONFIG="$SASL_JAAS_CONFIG" vdesabou/kafka-docker-playground-connect:${CONNECT_TAG} kafka-console-consumer --topic success-$connectorId --bootstrap-server $BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG" --from-beginning --property print.key=true --max-messages 1
+
+log "Login with sfdx CLI on the account #2"
+docker exec sfdx-cli sh -c "sfdx sfpowerkit:auth:login -u \"$SALESFORCE_USERNAME_ACCOUNT2\" -p \"$SALESFORCE_PASSWORD_ACCOUNT2\" -r \"$SALESFORCE_INSTANCE_ACCOUNT2\" -s \"$SALESFORCE_SECURITY_TOKEN_ACCOUNT2\""
+
+log "Get the Lead created on account #2"
+docker exec sfdx-cli sh -c "sfdx force:data:record:get  -u \"$SALESFORCE_USERNAME_ACCOUNT2\" -s Lead -w \"FirstName='$LEAD_FIRSTNAME' LastName='$LEAD_LASTNAME' Company=Confluent\"" > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "$LEAD_FIRSTNAME" /tmp/result.log
