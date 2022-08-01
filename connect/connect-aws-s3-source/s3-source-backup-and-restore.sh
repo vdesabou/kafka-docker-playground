8865b7f42cd430927ec13e319b19db66bd601c75
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-08-01 15:38:57 +0200

    fix

--- connect/connect-aws-s3-source/s3-source-backup-and-restore.sh
+++ connect/connect-aws-s3-source/s3-source-backup-and-restore.sh
@@ -28,11 +28,14 @@ else
             export AWS_SECRET_ACCESS_KEY=$( grep "^aws_secret_access_key" $HOME/.aws/credentials| awk -F'=' '{print $2;}' ) 
         fi
     fi
-    AWS_REGION=$(aws configure get region | tr '\r' '\n')
     if [ -z "$AWS_REGION" ]
     then
-        logerror "ERROR: either the file $HOME/.aws/config is not present or environment variables AWS_REGION is not set!"
-        exit 1
+        AWS_REGION=$(aws configure get region | tr '\r' '\n')
+        if [ "$AWS_REGION" == "" ]
+        then
+            logerror "ERROR: either the file $HOME/.aws/config is not present or environment variables AWS_REGION is not set!"
+            exit 1
+        fi
     fi
 fi
 
