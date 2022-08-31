25321db1fe88bc6487a2f8e3e2fec4a5a508099c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-08-31 10:17:04 +0000

    fix

--- ccloud/fully-managed-connect-salesforce-platform-events-sink/stop.sh
+++ ccloud/fully-managed-connect-salesforce-platform-events-sink/stop.sh
@@ -5,6 +5,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+docker-compose down -v --remove-orphans
+
 log "Do you want to delete the fully managed connector ?"
 check_if_continue
 
