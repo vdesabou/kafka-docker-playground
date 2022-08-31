96451ac7992305dd4bd026f022253c355ec2a910
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-08-31 09:16:33 +0000

    remove pre-2.0.0 version

--- connect/connect-gcp-bigquery-sink/gcp-bigquery-sink.sh
+++ connect/connect-gcp-bigquery-sink/gcp-bigquery-sink.sh
@@ -41,49 +41,22 @@ docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_
 
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
-if version_gt $CONNECTOR_TAG "1.9.9"
-then
-     log "Creating GCP BigQuery Sink connector"
-     curl -X PUT \
-          -H "Content-Type: application/json" \
-          --data '{
-                    "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
-                    "tasks.max" : "1",
-                    "topics" : "kcbq-quickstart1",
-                    "sanitizeTopics" : "true",
-                    "autoCreateTables" : "true",
-                    "defaultDataset" : "'"$DATASET"'",
-                    "mergeIntervalMs": "5000",
-                    "bufferSize": "100000",
-                    "maxWriteSize": "10000",
-                    "tableWriteWait": "1000",
-                    "project" : "'"$PROJECT"'",
-                    "keyfile" : "/tmp/keyfile.json"
-               }' \
-          http://localhost:8083/connectors/gcp-bigquery-sink/config | jq .
-else
-     log "Creating GCP BigQuery Sink connector"
-     curl -X PUT \
-          -H "Content-Type: application/json" \
-          --data '{
-                    "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
-                    "tasks.max" : "1",
-                    "topics" : "kcbq-quickstart1",
-                    "sanitizeTopics" : "true",
-                    "autoCreateTables" : "true",
-                    "autoUpdateSchemas" : "true",
-                    "schemaRetriever" : "com.wepay.kafka.connect.bigquery.schemaregistry.schemaretriever.SchemaRegistrySchemaRetriever",
-                    "schemaRegistryLocation": "http://schema-registry:8081",
-                    "datasets" : ".*='"$DATASET"'",
-                    "mergeIntervalMs": "5000",
-                    "bufferSize": "100000",
-                    "maxWriteSize": "10000",
-                    "tableWriteWait": "1000",
-                    "project" : "'"$PROJECT"'",
-                    "keyfile" : "/tmp/keyfile.json"
-               }' \
-          http://localhost:8083/connectors/gcp-bigquery-sink/config | jq .
-fi
+
+log "Creating GCP BigQuery Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class": "com.wepay.kafka.connect.bigquery.BigQuerySinkConnector",
+               "tasks.max" : "1",
+               "topics" : "kcbq-quickstart1",
+               "sanitizeTopics" : "true",
+               "autoCreateTables" : "true",
+               "defaultDataset" : "'"$DATASET"'",
+               "project" : "'"$PROJECT"'",
+               "keyfile" : "/tmp/keyfile.json"
+          }' \
+     http://localhost:8083/connectors/gcp-bigquery-sink/config | jq .
+
 
 log "Sending messages to topic kcbq-quickstart1"
 seq -f "{\"f1\": \"value%g-`date`\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic kcbq-quickstart1 --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
