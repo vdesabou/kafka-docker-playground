93693c9600983f43ffe84175e22ef0e848b77af1
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-10-19 12:23:00 +0000

    remove external sql file

--- connect/connect-debezium-sqlserver-source/debezium-sqlserver-source.sh
+++ connect/connect-debezium-sqlserver-source/debezium-sqlserver-source.sh
@@ -7,9 +7,32 @@ source ${DIR}/../../scripts/utils.sh
 ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
 
 
-log "Load inventory.sql to SQL Server"
-cat ../../connect/connect-debezium-sqlserver-source/inventory.sql | docker exec -i sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P Password!'
+log "Create table"
+docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! << EOF
+-- Create the test database
+CREATE DATABASE testDB;
+GO
+USE testDB;
+EXEC sys.sp_cdc_enable_db;
 
+-- Create some customers ...
+CREATE TABLE customers (
+  id INTEGER IDENTITY(1001,1) NOT NULL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE
+);
+INSERT INTO customers(first_name,last_name,email)
+  VALUES ('Sally','Thomas','sally.thomas@acme.com');
+INSERT INTO customers(first_name,last_name,email)
+  VALUES ('George','Bailey','gbailey@foobar.com');
+INSERT INTO customers(first_name,last_name,email)
+  VALUES ('Edward','Walker','ed@walker.com');
+INSERT INTO customers(first_name,last_name,email)
+  VALUES ('Anne','Kretchmar','annek@noanswer.org');
+EXEC sys.sp_cdc_enable_table @source_schema = 'dbo', @source_name = 'customers', @role_name = NULL, @supports_net_changes = 0;
+GO
+EOF
 
 log "Creating Debezium SQL Server source connector"
 curl -X PUT \
