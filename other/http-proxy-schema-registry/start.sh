431d9c09e9c404029e04558396b4764bc01b0975
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-11-04 16:26:14 +0100

    wip producer

--- other/http-proxy-schema-registry/start.sh
+++ other/http-proxy-schema-registry/start.sh
@@ -49,3 +49,10 @@ sleep 5
 
 log "Verify we have received the data in file"
 docker exec connect cat /tmp/output.json
+
+
+log "Produce avro data using Java producer"
+docker exec producer bash -c "java -jar producer-1.0.0-jar-with-dependencies.jar"
+
+log "Verify data was sent to broker using --property proxy.host=nginx-proxy -property proxy.port=8888"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property proxy.host=nginx-proxy -property proxy.port=8888 --property schema.registry.url=http://schema-registry:8081 --topic customer_avro --from-beginning --max-messages 10
