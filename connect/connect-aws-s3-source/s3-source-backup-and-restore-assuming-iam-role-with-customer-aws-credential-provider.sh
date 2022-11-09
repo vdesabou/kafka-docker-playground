0a7ab4d48babcd72253dc9b8418496618956f553
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-11-09 10:12:36 +0000

    minor

--- connect/connect-aws-s3-source/s3-source-backup-and-restore-assuming-iam-role-with-customer-aws-credential-provider.sh
+++ connect/connect-aws-s3-source/s3-source-backup-and-restore-assuming-iam-role-with-customer-aws-credential-provider.sh
@@ -126,6 +126,50 @@ curl -X PUT \
      http://localhost:8083/connectors/s3-sink/config | jq .
 
 
+log "Sending messages to topic s3_topic"
+seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic s3_topic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
+
+sleep 10
+
+log "Listing objects of in S3"
+aws s3api list-objects --bucket "$AWS_BUCKET_NAME"
+
+log "Getting one of the avro files locally and displaying content with avro-tools"
+aws s3 cp --only-show-errors s3://$AWS_BUCKET_NAME/$TAG/s3_topic/partition=0/s3_topic+0+0000000000.avro s3_topic+0+0000000000.avro
+
+docker run --rm -v ${DIR}:/tmp actions/avro-tools tojson /tmp/s3_topic+0+0000000000.avro
+rm -f s3_topic+0+0000000000.avro
+
+log "Creating Backup and Restore S3 Source connector with bucket name <$AWS_BUCKET_NAME>"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "tasks.max": "1",
+               "connector.class": "io.confluent.connect.s3.source.S3SourceConnector",
+               "s3.region": "'"$AWS_REGION"'",
+               "s3.bucket.name": "'"$AWS_BUCKET_NAME"'",
+               "topics.dir": "'"$TAG"'",
+               "s3.credentials.provider.class": "com.github.vdesabou.AwsAssumeRoleCredentialsProvider",
+               "s3.credentials.provider.sts.role.arn": "'"$AWS_STS_ROLE_ARN"'",
+               "s3.credentials.provider.sts.role.session.name": "session-name",
+               "s3.credentials.provider.sts.role.external.id": "123",
+               "s3.credentials.provider.sts.aws.access.key.id": "'"$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_ACCESS_KEY_ID"'",
+               "s3.credentials.provider.sts.aws.secret.key.id": "'"$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_SECRET_ACCESS_KEY"'",
+               "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
+               "confluent.license": "",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1",
+               "transforms": "AddPrefix",
+               "transforms.AddPrefix.type": "org.apache.kafka.connect.transforms.RegexRouter",
+               "transforms.AddPrefix.regex": ".*",
+               "transforms.AddPrefix.replacement": "copy_of_$0"
+          }' \
+     http://localhost:8083/connectors/s3-source/config | jq .
+
+# https://stackoverflow.com/a/64566641/2381999
+
+# S3 sink: aws-java-sdk-core-1.12.268.jar 
+# S3 source: aws-java-sdk-core-1.11.725.jar
 # [2022-11-09 08:51:13,528] WARN /connectors/s3-source/config (org.eclipse.jetty.server.HttpChannel:776)
 # javax.servlet.ServletException: org.glassfish.jersey.server.ContainerException: java.lang.NoSuchFieldError: ENDPOINT_OVERRIDDEN
 #         at org.glassfish.jersey.servlet.WebComponent.serviceImpl(WebComponent.java:410)
@@ -215,47 +259,6 @@ curl -X PUT \
 #         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
 #         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
 #         ... 1 more
-
-log "Sending messages to topic s3_topic"
-seq -f "{\"f1\": \"value%g\"}" 10 | docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic s3_topic --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"f1","type":"string"}]}'
-
-sleep 10
-
-log "Listing objects of in S3"
-aws s3api list-objects --bucket "$AWS_BUCKET_NAME"
-
-log "Getting one of the avro files locally and displaying content with avro-tools"
-aws s3 cp --only-show-errors s3://$AWS_BUCKET_NAME/$TAG/s3_topic/partition=0/s3_topic+0+0000000000.avro s3_topic+0+0000000000.avro
-
-docker run --rm -v ${DIR}:/tmp actions/avro-tools tojson /tmp/s3_topic+0+0000000000.avro
-rm -f s3_topic+0+0000000000.avro
-
-log "Creating Backup and Restore S3 Source connector with bucket name <$AWS_BUCKET_NAME>"
-curl -X PUT \
-     -H "Content-Type: application/json" \
-     --data '{
-               "tasks.max": "1",
-               "connector.class": "io.confluent.connect.s3.source.S3SourceConnector",
-               "s3.region": "'"$AWS_REGION"'",
-               "s3.bucket.name": "'"$AWS_BUCKET_NAME"'",
-               "topics.dir": "'"$TAG"'",
-               "s3.credentials.provider.class": "com.github.vdesabou.AwsAssumeRoleCredentialsProvider",
-               "s3.credentials.provider.sts.role.arn": "'"$AWS_STS_ROLE_ARN"'",
-               "s3.credentials.provider.sts.role.session.name": "session-name",
-               "s3.credentials.provider.sts.role.external.id": "123",
-               "s3.credentials.provider.sts.aws.access.key.id": "'"$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_ACCESS_KEY_ID"'",
-               "s3.credentials.provider.sts.aws.secret.key.id": "'"$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_SECRET_ACCESS_KEY"'",
-               "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
-               "confluent.license": "",
-               "confluent.topic.bootstrap.servers": "broker:9092",
-               "confluent.topic.replication.factor": "1",
-               "transforms": "AddPrefix",
-               "transforms.AddPrefix.type": "org.apache.kafka.connect.transforms.RegexRouter",
-               "transforms.AddPrefix.regex": ".*",
-               "transforms.AddPrefix.replacement": "copy_of_$0"
-          }' \
-     http://localhost:8083/connectors/s3-source/config | jq .
-
 
 log "Verifying topic copy_of_s3_topic"
 timeout 60 docker exec broker kafka-console-consumer -bootstrap-server broker:9092 --topic copy_of_s3_topic --from-beginning --max-messages 9
