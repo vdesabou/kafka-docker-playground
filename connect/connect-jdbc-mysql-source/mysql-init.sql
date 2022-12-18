5aaa3e06a3d37db251f7e7fb31ac7bfcc605c4c7
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-12-18 09:47:45 +0000

    no more use sql

--- connect/connect-jdbc-mysql-source/mysql-init.sql
+++ connect/connect-jdbc-mysql-source/mysql-init.sql
@@ -6,23 +6,3 @@ USE db;
 GRANT ALL PRIVILEGES ON *.* TO 'userssl'@'%' IDENTIFIED BY 'password' REQUIRE SSL;
 -- used for mtls case: requires that clients present a valid certificate
 GRANT ALL PRIVILEGES ON *.* TO 'usermtls'@'%' IDENTIFIED BY 'password' REQUIRE X509;
-
-CREATE TABLE IF NOT EXISTS application (
-  id            INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name          VARCHAR(255) NOT NULL,
-  team_email    VARCHAR(255) NOT NULL,
-  last_modified DATETIME     NOT NULL
-);
-
-
-INSERT INTO application (
-  id,
-  name,
-  team_email,
-  last_modified
-) VALUES (
-  1,
-  'kafka',
-  'kafka@apache.org',
-  NOW()
-);
