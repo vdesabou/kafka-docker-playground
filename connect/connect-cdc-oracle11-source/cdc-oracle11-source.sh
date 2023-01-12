a224a916395dc5e4b4d03332466679e8470ce065
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-01-12 16:33:46 +0000

    fix

--- connect/connect-cdc-oracle11-source/cdc-oracle11-source.sh
+++ connect/connect-cdc-oracle11-source/cdc-oracle11-source.sh
@@ -31,12 +31,12 @@ MAX_WAIT=900
 CUR_WAIT=0
 log "⌛ Waiting up to $MAX_WAIT seconds for Oracle DB to start"
 docker container logs oracle > /tmp/out.txt 2>&1
-while [[ ! $(cat /tmp/out.txt) =~ "04_populate_customer.sh" ]]; do
+while [[ ! $(cat /tmp/out.txt) =~ "Grant succeeded." ]]; do
 sleep 10
 docker container logs oracle > /tmp/out.txt 2>&1
 CUR_WAIT=$(( CUR_WAIT+10 ))
 if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
-     logerror "ERROR: The logs in oracle container do not show '04_populate_customer.sh' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
+     logerror "ERROR: The logs in oracle container do not show 'Grant succeeded.' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'docker container logs'.\n"
      exit 1
 fi
 done
