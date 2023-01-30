aa14a1523d5178ae049a28754de61e946350ac51
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-01-30 16:12:13 +0000

    added

---
+++ ccloud/fully-managed-connect-elasticsearch-sink/stop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+
+docker-compose down -v --remove-orphans
+
+log "Do you want to delete the fully managed connector ?"
+check_if_continue
+
+log "Deleting fully managed connector"
+delete_ccloud_connector connector.json
+
+maybe_delete_ccloud_environment
