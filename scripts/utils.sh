c8a693f5462035cb09f37831224eeadd6032f499
Vincent de Saboulin <vdesabou@users.noreply.github.com>
2023-02-02 03:20:57 +0100

    CLI 3.0

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1709,7 +1709,7 @@ function bootstrap_ccloud_environment () {
   then
     # not running with CI
     verify_installed "confluent"
-    check_confluent_version 2.0.0 || exit 1
+    check_confluent_version 3.0.0 || exit 1
     verify_confluent_login  "confluent kafka cluster list"
   else
       log "Installing confluent CLI"
