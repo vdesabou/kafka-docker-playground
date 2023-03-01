18c966695ba644a0ac9b10d1e880ade04a621211
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-03-01 12:55:23 +0000

    trick to be faster

--- scripts/cli/src/get_topic_list_command.sh
+++ scripts/cli/src/get_topic_list_command.sh
@@ -1,1 +1,3 @@
-docker exec broker kafka-topics --bootstrap-server broker:9092 --list | grep -v "^_"
+#docker exec broker kafka-topics --bootstrap-server broker:9092 --list | grep -v "^_"
+# trick to be faster
+docker exec broker ls /var/lib/kafka/data | grep -v "checkpoint" | grep -v "meta.properties" | grep -v "^_" | sed 's/[^-]*$//' | sed 's/.$//' | sort | uniq
