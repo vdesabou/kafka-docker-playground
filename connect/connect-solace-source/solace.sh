f567833311abc07bc8a5ec7db8e7ce2ed43505fb
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-03-06 18:40:40 +0100

    Adapt playground to M1 mac #3488

--- connect/connect-solace-source/solace.sh
+++ connect/connect-solace-source/solace.sh
@@ -49,6 +49,17 @@ curl -X PUT \
      -H "Content-Type: application/json" \
      --data '{
                "connector.class": "io.confluent.connect.solace.SolaceSourceConnector",
+               "tasks.max": "1",
+               "kafka.topic": "from-solace-messages",
+               "solace.host": "smf://solace:55555",
+               "solace.username": "admin",
+               "solace.password": "admin",
+               "jms.destination.type": "queue",
+               "jms.destination.name": "connector-quickstart",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
                     "tasks.max": "1",
                     "kafka.topic": "from-solace-messages",
                     "solace.host": "smf://solace:55555",
@@ -60,6 +71,17 @@ curl -X PUT \
                     "value.converter": "org.apache.kafka.connect.storage.StringConverter",
                     "confluent.topic.bootstrap.servers": "broker:9092",
                     "confluent.topic.replication.factor": "1"
+               "tasks.max": "1",
+               "kafka.topic": "from-solace-messages",
+               "solace.host": "smf://solace:55555",
+               "solace.username": "admin",
+               "solace.password": "admin",
+               "jms.destination.type": "queue",
+               "jms.destination.name": "connector-quickstart",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "confluent.topic.bootstrap.servers": "broker:9092",
+               "confluent.topic.replication.factor": "1"
           }' \
      http://localhost:8083/connectors/solace-source/config | jq .
 
