c3594a1841304049db120e9dead62b032204f206
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-03-17 18:40:49 +0100

    add

--- scripts/cli/src/container_kill_all_command.sh
+++ scripts/cli/src/container_kill_all_command.sh
@@ -1,4 +1,2 @@
-echo "# this file is located in 'src/container_kill_all_command.sh'"
-echo "# code for 'playground container kill-all' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+log "Kill all docker containers"
+docker rm -f $(docker ps -qa)
