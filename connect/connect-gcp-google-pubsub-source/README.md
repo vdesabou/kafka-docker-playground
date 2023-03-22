80a91dc1c421074d1dd2fb6a4167ddd15cda1ed7
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-03-22 12:22:28 +0100

    https://github.com/googleapis/java-pubsub-group-kafka-connector

---
+++ connect/connect-gcp-google-pubsub-source/README.md
@@ -0,0 +1,105 @@
+# Google Cloud Pub/Sub Group Kafka Connector Source connector
+
+
+
+## Objective
+
+Quickly test [Google Cloud Pub/Sub Group Kafka Connector](https://github.com/googleapis/java-pubsub-group-kafka-connector) connector.
+
+* Active Google Cloud Platform (GCP) account with authorization to create resources
+
+## GCP Pub/Sub Setup
+
+* Create `Service Account` from IAM & Admin console:
+
+Set `Service account name`:
+
+![Service Account setup](Screenshot1.png)
+
+
+Choose permission `Pub/Sub`->`Pub/Sub Admin`
+
+![Service Account setup](Screenshot2.png)
+
+Create Key:
+
+![Service Account setup](Screenshot3.png)
+
+Download it as JSON:
+
+![Service Account setup](Screenshot4.png)
+
+Rename it to `keyfile.json`and place it in `./keyfile.json` or use environment variable `GCP_KEYFILE_CONTENT` with content generated with `GCP_KEYFILE_CONTENT=`cat keyfile.json | jq -aRs .`
+
+
+## How to run
+
+Simply run:
+
+```bash
+$ ./gcp-google-pubsub-source.sh <GCP_PROJECT>
+```
+
+## Details of what the script is doing
+
+Doing gsutil authentication
+
+```bash
+$ gcloud auth activate-service-account --key-file ${GCP_KEYFILE}
+```
+
+Create a Pub/Sub topic called topic-1
+
+```bash
+$ gcloud pubsub topics create topic-1
+```
+
+Create a Pub/Sub subscription called subscription-1
+
+```bash
+$ gcloud pubsub subscriptions create --topic topic-1 subscription-1
+```
+
+Publish three messages to topic-1
+
+```bash
+$ gcloud pubsub topics publish topic-1 --message "Peter"
+gcloud pubsub topics publish topic-1 --message "Megan"
+gcloud pubsub topics publish topic-1 --message "Erin"
+```
+
+Creating reating Google Cloud Pub/Sub Group Kafka Source connector
+
+```bash
+$ curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class" : "com.google.pubsub.kafka.source.CloudPubSubSourceConnector",
+               "tasks.max" : "1",
+               "kafka.topic" : "pubsub-topic",
+               "cps.project" : "'"$GCP_PROJECT"'",
+               "cps.topic" : "topic-1",
+               "cps.subscription" : "subscription-1",
+               "gcp.credentials.file.path" : "/tmp/keyfile.json",
+               "errors.tolerance": "all",
+               "errors.log.enable": "true",
+               "errors.log.include.messages": "true"
+          }' \
+     http://localhost:8083/connectors/pubsub-source/config | jq .
+```
+
+Verify messages are in topic `pubsub-topic`
+
+```bash
+$ docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic pubsub-topic --from-beginning --max-messages 3
+```
+
+Results:
+
+```
+"Peter"
+"Erin"
+"Megan"
+```
+
+N.B: Control Center is reachable at [http://127.0.0.1:9021](http://127.0.0.1:9021])
---
+++ connect/connect-gcp-google-pubsub-source/gcp-google-pubsub-source.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ -z "$GCP_PROJECT" ]
+then
+     logerror "GCP_PROJECT is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ ! -f ${DIR}/pubsub-group-kafka-connector-1.0.0.jar ]
+then
+     wget https://repo1.maven.org/maven2/com/google/cloud/pubsub-group-kafka-connector/1.0.0/pubsub-group-kafka-connector-1.0.0.jar
+fi
+
+cd ../../connect/connect-gcp-google-pubsub-source
+GCP_KEYFILE="${PWD}/keyfile.json"
+if [ ! -f ${GCP_KEYFILE} ] && [ -z "$GCP_KEYFILE_CONTENT" ]
+then
+     logerror "ERROR: either the file ${GCP_KEYFILE} is not present or environment variable GCP_KEYFILE_CONTENT is not set!"
+     exit 1
+else
+    if [ -f ${GCP_KEYFILE} ]
+    then
+        GCP_KEYFILE_CONTENT=`cat keyfile.json | jq -aRs .`
+    else
+        log "Creating ${GCP_KEYFILE} based on environment variable GCP_KEYFILE_CONTENT"
+        echo -e "$GCP_KEYFILE_CONTENT" | sed 's/\\"/"/g' > ${GCP_KEYFILE}
+    fi
+fi
+cd -
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+log "Doing gsutil authentication"
+set +e
+docker rm -f gcloud-config
+set -e
+docker run -i -v ${GCP_KEYFILE}:/tmp/keyfile.json --name gcloud-config google/cloud-sdk:latest gcloud auth activate-service-account --project ${GCP_PROJECT} --key-file /tmp/keyfile.json
+
+
+# cleanup if required
+set +e
+log "Delete topic and subscription, if required"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud pubsub --project ${GCP_PROJECT} topics delete topic-1
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud pubsub --project ${GCP_PROJECT} subscriptions delete subscription-1
+set -e
+
+log "Create a Pub/Sub topic called topic-1"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud pubsub --project ${GCP_PROJECT} topics create topic-1
+
+log "Create a Pub/Sub subscription called subscription-1"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud pubsub --project ${GCP_PROJECT} subscriptions create --topic topic-1 subscription-1
+
+log "Publish three messages to topic-1"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud pubsub --project ${GCP_PROJECT} topics publish topic-1 --message "Peter"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud pubsub --project ${GCP_PROJECT} topics publish topic-1 --message "Megan"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud pubsub --project ${GCP_PROJECT} topics publish topic-1 --message "Erin"
+
+sleep 10
+
+log "Creating Google Cloud Pub/Sub Group Kafka Source connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "connector.class" : "com.google.pubsub.kafka.source.CloudPubSubSourceConnector",
+               "tasks.max" : "1",
+               "kafka.topic" : "pubsub-topic",
+               "cps.project" : "'"$GCP_PROJECT"'",
+               "cps.topic" : "topic-1",
+               "cps.subscription" : "subscription-1",
+               "gcp.credentials.file.path" : "/tmp/keyfile.json",
+               "errors.tolerance": "all",
+               "errors.log.enable": "true",
+               "errors.log.include.messages": "true"
+          }' \
+     http://localhost:8083/connectors/pubsub-source/config | jq .
+
+sleep 10
+
+log "Verify messages are in topic pubsub-topic"
+timeout 60 docker exec connect kafka-avro-console-consumer -bootstrap-server broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic pubsub-topic --from-beginning --max-messages 3
+
+log "Delete topic and subscription"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud pubsub --project ${GCP_PROJECT} topics delete topic-1
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest gcloud pubsub --project ${GCP_PROJECT} subscriptions delete subscription-1
+
+docker rm -f gcloud-config
