2a09ede17af1b5b484687b0f1e8512ea67d051b2
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-03-27 09:21:47 +0200

    fix

--- connect/connect-solace-source/solace.sh
+++ connect/connect-solace-source/solace.sh
@@ -60,28 +60,6 @@ curl -X PUT \
                "value.converter": "org.apache.kafka.connect.storage.StringConverter",
                "confluent.topic.bootstrap.servers": "broker:9092",
                "confluent.topic.replication.factor": "1"
-                    "tasks.max": "1",
-                    "kafka.topic": "from-solace-messages",
-                    "solace.host": "smf://solace:55555",
-                    "solace.username": "admin",
-                    "solace.password": "admin",
-                    "jms.destination.type": "queue",
-                    "jms.destination.name": "connector-quickstart",
-                    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-                    "value.converter": "org.apache.kafka.connect.storage.StringConverter",
-                    "confluent.topic.bootstrap.servers": "broker:9092",
-                    "confluent.topic.replication.factor": "1"
-               "tasks.max": "1",
-               "kafka.topic": "from-solace-messages",
-               "solace.host": "smf://solace:55555",
-               "solace.username": "admin",
-               "solace.password": "admin",
-               "jms.destination.type": "queue",
-               "jms.destination.name": "connector-quickstart",
-               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-               "value.converter": "org.apache.kafka.connect.storage.StringConverter",
-               "confluent.topic.bootstrap.servers": "broker:9092",
-               "confluent.topic.replication.factor": "1"
           }' \
      http://localhost:8083/connectors/solace-source/config | jq .
 
