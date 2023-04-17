a1512355d6af11d3ce16d3542d3e1a1f3af45ca9
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-04-17 10:59:38 +0200

    wip

--- scripts/cli/src/connector_status_command.sh
+++ scripts/cli/src/connector_status_command.sh
@@ -5,49 +5,53 @@ security=$(echo "$ret" | cut -d "@" -f 2)
 
 connectors=$(curl -s $security "$connect_url/connectors/" | jq -r '.[]')
 
-printf "%-30s %-10s %-15s %-50s\n" "Connector Name" "Status" "Tasks" "Stack Trace"
-echo "----------------------------------------------------------------------------------------------"
+printf "%-30s %-10s %-20s %-20s %-50s\n" "Connector Name" "Status 🔥" "Tasks 👷‍♂️"  "State"   "Stack Trace"
+echo "------------------------------------------------------------------------------------------------------------------------"
 
 for connector in $connectors
 do
+    # Get the status of the connector and pretty-print using jq
     status=$(curl -s $security "$connect_url/connectors/$connector/status" | jq -r '.connector.state')
-    tasks=$(curl -s $security "$connect_url/connectors/$connector/status" | jq -r '.tasks[].state' | tr '\n' ',' | sed 's/,$/\n/')
-    stacktrace=$(curl -s $security "$connect_url/connectors/$connector/status" | jq -r '.connector.trace | select(length > 0)')
-    
+
     # Add emoji based on status
     if [ "$status" == "RUNNING" ]
     then
         status="✅ RUNNING"
-    elif [ "$status" == "FAILED" ]
-    then
-        status="🔥 FAILED"
     elif [ "$status" == "PAUSED" ]
     then
-        status="⏸️ PAUSED"
+        status="⏸ PAUSED"
+    elif [ "$status" == "FAILED" ]
+    then
+        status="❌ FAILED"
     else
         status="🤔 UNKNOWN"
     fi
     
+    tasks=$(curl -s $security "$connect_url/connectors/$connector/status" | jq -r '.tasks[] | "\(.id):\(.state)"' | tr '\n' ',' | sed 's/,$/\n/')
+    
     # Add emoji based on tasks
     if [[ "$tasks" == *"RUNNING"* ]]
     then
-        tasks="${tasks//RUNNING/🏃 RUNNING}"
-    elif [[ "$tasks" == *"FAILED"* ]]
-    then
-        tasks="${tasks//FAILED/🛑 FAILED}"
+        tasks="${tasks//RUNNING/🏃‍♂️ RUNNING}"
     elif [[ "$tasks" == *"PAUSED"* ]]
     then
         tasks="${tasks//PAUSED/⏸️ PAUSED}"
+    elif [[ "$tasks" == *"FAILED"* ]]
+    then
+        tasks="${tasks//FAILED/🛑 FAILED}"
     else
         tasks="🤔 N/A"
     fi
 
+    state=$(curl -s $security "$connect_url/connectors/$connector/status" | jq -r '.type')
+    
+    stacktrace=$(curl -s $security "$connect_url/connectors/$connector/status" | jq -r '.tasks[].trace | select(length > 0)')
+    
     if [ -z "$stacktrace" ]
     then
         stacktrace="-"
     fi
-    
-    printf "%-30s %-10s %-15s %-50s\n" "$connector" "$status" "$tasks" "$stacktrace"
 
-    echo "--------------------------------------------"
+    # Print the data for each connector in row format
+    printf "%-30s %-10s %-20s %-20s %-50s\n" "$connector" "$status" "$tasks" "$state" "$stacktrace"
 done
