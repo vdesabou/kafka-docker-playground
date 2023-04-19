b076be79e3ed253c4aabd8f17ff394edb2494450
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-04-20 00:33:27 +0200

    wip

--- scripts/cli/src/connector_plugins_command.sh
+++ scripts/cli/src/connector_plugins_command.sh
@@ -4,4 +4,4 @@ connect_url=$(echo "$ret" | cut -d "@" -f 1)
 security=$(echo "$ret" | cut -d "@" -f 2)
 
 log "🧩 Displaying all connector plugins installed"
-curl $security -s -X GET -H "Content-Type: application/json" "$connect_url/connector-plugins" | jq .
+curl $security -s -X GET -H "Content-Type: application/json" "$connect_url/connector-plugins" | jq -r '.[] | [.class , .version , .type] | @tsv' | column -t
