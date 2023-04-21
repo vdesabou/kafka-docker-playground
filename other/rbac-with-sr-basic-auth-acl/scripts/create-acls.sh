fabd8fa914c4680886849ceafe9f5f9a43f6e67c
Roman Schmitz <rschmitz@confluent.io>
2023-04-21 17:10:05 +0200

    Added follower schema registry and modified C3 to connect to this registry

--- other/rbac-with-sr-basic-auth-acl/scripts/create-acls.sh
+++ other/rbac-with-sr-basic-auth-acl/scripts/create-acls.sh
@@ -2,6 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+echo "Creating ACLs..."
+echo "*************"
+sr-acl-cli --config /etc/schema-registry/schema-registry.properties --add --subject '*' --principal sr-admin --operation '*'
+
+
 echo "Current ACLs:"
 echo "*************"
 sr-acl-cli --config /etc/schema-registry/schema-registry.properties --list
