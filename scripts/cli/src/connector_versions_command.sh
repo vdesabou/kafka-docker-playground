1ffb179e3451b44f704261f1e9f4d12407937bf6
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-04-28 23:09:52 +0200

    wip

--- scripts/cli/src/connector_versions_command.sh
+++ scripts/cli/src/connector_versions_command.sh
@@ -19,7 +19,7 @@ fi
 items=($connector)
 for connector in ${items[@]}
 do
-    log "Current version:"
+    log "Current version for $connector:"
     package=$(curl -s $security "$connect_url/connectors/$connector" | jq -r '.config."connector.class"')
     curl $security -s -X GET -H "Content-Type: application/json" "$connect_url/connector-plugins" | jq -r ".[] | select(.class == \"$package\").version"
 done
