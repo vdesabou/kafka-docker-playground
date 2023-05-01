c6eb7d80ef5cefa264854edf6c17623d1869ad84
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-05-01 17:51:23 +0200

    validate CP version

--- scripts/cli/src/lib/validations/validate_editor_exists.sh
+++ scripts/cli/src/lib/validations/validate_editor_exists.sh
@@ -2,7 +2,7 @@
 validate_editor_exists() {
   local cmd="$1"
   if [[ $(type $cmd 2>&1) =~ "not found" ]]; then
-    echo -e "\nERROR: This script requires '$cmd'. Please install '$cmd' and run again.\n"
+    logerror "This script requires $cmd. Please install $cmd and run again."
     exit 1
   fi
 }
