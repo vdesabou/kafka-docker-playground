15292f04a303b23ab7e2ec24f1f13019cc58b3e8
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-05-02 18:56:25 +0200

    minor

--- scripts/cli/src/lib/validations/validate_minimal_cp_version.sh
+++ scripts/cli/src/lib/validations/validate_minimal_cp_version.sh
@@ -1,10 +1,8 @@
 ## [@bashly-upgrade validations]
 validate_minimal_cp_version() {
   version="$1"
-
   if ! version_gt $version "4.9.99"
   then
-      logerror "CP version (--tag) must be > 5.0.0"
-      exit 1
+      echo "CP version (--tag) must be > 5.0.0"
   fi
 }
