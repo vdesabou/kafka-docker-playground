668b4ebe37b8a76aca27abf7eb592fb884d6faa5
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-05-02 18:55:39 +0200

    update

--- scripts/cli/src/lib/validations/validate_editor_exists.sh
+++ scripts/cli/src/lib/validations/validate_editor_exists.sh
@@ -1,9 +1,9 @@
 ## [@bashly-upgrade validations]
 validate_editor_exists() {
   local cmd="$1"
-  if [[ $(type $cmd 2>&1) =~ "not found" ]]; then
-    logerror "This script requires $cmd. Please install $cmd and run again."
-    exit 1
+  if [[ $(type $cmd 2>&1) =~ "not found" ]]
+  then
+    echo "this script requires $cmd. Please install $cmd and run again."
   fi
 }
 
