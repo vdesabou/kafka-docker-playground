715aad69a0a517f7267fe2482f900e30b455fe76
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-05-16 11:56:57 +0200

    Unknown magic byte

---
+++ academy/connect-connect-filestream-sink/filestream-sink-repro-000002-deserialization-error.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-000002-deserialization-error.yml"
+
+log "Sending messages to topic filestream"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic filestream --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"u_name","type":"string"},
+{"name":"u_price", "type": "float"}, {"name":"u_quantity", "type": "int"}]}' << EOF
+{"u_name": "scissors", "u_price": 2.75, "u_quantity": 3}
+{"u_name": "tape", "u_price": 0.99, "u_quantity": 10}
+{"u_name": "notebooks", "u_price": 1.99, "u_quantity": 5}
+EOF
+
+log "Sending again message to topic filestream"
+docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic filestream << EOF
+{"u_name": "poison pill", "u_price": 1.75, "u_quantity": 1}
+EOF
+
+log "Sending again messages to topic filestream"
+docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic filestream --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"u_name","type":"string"},
+{"name":"u_price", "type": "float"}, {"name":"u_quantity", "type": "int"}]}' << EOF
+{"u_name": "bottle", "u_price": 2.75, "u_quantity": 3}
+EOF
+
+log "Creating FileStream Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "tasks.max": "1",
+               "connector.class": "org.apache.kafka.connect.file.FileStreamSinkConnector",
+               "topics": "filestream",
+               "file": "/tmp/output.json",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "io.confluent.connect.avro.AvroConverter",
+               "value.converter.schema.registry.url": "http://schema-registry:8081"
+          }' \
+     http://localhost:8083/connectors/filestream-sink/config | jq .
+
+
+sleep 5
+
+playground connector status
+
+playground topic consume
+
+playground connector show-lag
+
+# log "Verify we have received the data in file"
+# docker exec connect cat /tmp/output.json
