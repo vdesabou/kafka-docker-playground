f65bac34e83ad9b5c1a38f677d4f9621415bf9bb
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-05-22 15:46:09 +0200

    wip

---
+++ academy/connect-connect-filestream-sink/filestream-sink-repro-000003-insertfield-smt-dataexception-only-struct-objects-supported.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.repro-000003-insertfield-smt-dataexception-only-struct-objects-supported.yml"
+
+log "Sending messages to topic filestream"
+docker exec -i broker kafka-console-producer --broker-list broker:9092 --topic filestream << EOF
+{"customer_name":"Ed", "complaint_type":"Dirty car", "trip_cost": 29.10, "new_customer": false, "number_of_rides": 22}
+EOF
+
+log "Creating FileStream Sink connector"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     --data '{
+               "tasks.max": "1",
+               "connector.class": "org.apache.kafka.connect.file.FileStreamSinkConnector",
+               "topics": "filestream",
+               "file": "/tmp/output.json",
+               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "value.converter": "org.apache.kafka.connect.storage.StringConverter",
+               "transforms": "InsertField",
+               "transforms.InsertField.type": "org.apache.kafka.connect.transforms.InsertField$Value",
+               "transforms.InsertField.static.field": "MessageSource",
+               "transforms.InsertField.static.value": "Kafka Connect framework"
+          }' \
+     http://localhost:8083/connectors/filestream-sink/config | jq .
+
+
+sleep 5
+
+log "Verify we have received the data in file"
+docker exec connect cat /tmp/output.json
+
+playground connector status
