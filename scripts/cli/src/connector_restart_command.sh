ebbf57c417f33b3d82540b3316975b448ea537fc
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-05-23 21:13:45 +0200

    handle restart before 7.0.0

--- scripts/cli/src/connector_restart_command.sh
+++ scripts/cli/src/connector_restart_command.sh
@@ -16,11 +16,28 @@ then
     fi
 fi
 
+DIR_CLI="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+dir1=$(echo ${DIR_CLI%/*})
+root_folder=$(echo ${dir1%/*})
+IGNORE_CHECK_FOR_DOCKER_COMPOSE=true
+source $root_folder/scripts/utils.sh
+
 items=($connector)
 for connector in ${items[@]}
 do
     log "🔄 Restarting connector $connector"
-    curl $security -s -X POST -H "Content-Type: application/json" "$connect_url/connectors/$connector/restart?includeTasks=true&onlyFailed=false" | jq .
+    if ! version_gt $TAG_BASE "6.9.9"
+    then
+        task_ids=$(curl $security -s -X GET "$connect_url/connectors/$connector/tasks" | jq -r '.[].id.task')
+
+        for task_id in $task_ids
+        do
+            log "🤹‍♂️ Restart task $task_id"
+            curl $security -s -X POST -H "Content-Type: application/json" "$connect_url/connectors/$connector/tasks/$task_id/restart"
+        done
+    else
+        curl $security -s -X POST -H "Content-Type: application/json" "$connect_url/connectors/$connector/restart?includeTasks=true&onlyFailed=false" | jq .
+    fi
 done
-sleep 2
+sleep 3
 playground connector status
