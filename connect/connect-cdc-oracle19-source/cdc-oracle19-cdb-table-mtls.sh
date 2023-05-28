5c59bb51e4a961610399ce4e145cafbcda9b7379
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-05-28 15:59:27 +0000

    fix

--- connect/connect-cdc-oracle19-source/cdc-oracle19-cdb-table-mtls.sh
+++ connect/connect-cdc-oracle19-source/cdc-oracle19-cdb-table-mtls.sh
@@ -221,6 +221,9 @@ log "Sleeping 60 seconds"
 sleep 60
 
 docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/docker-compose.plaintext.cdb-table-mtls.yml" up -d
+command="source ${DIR}/../../scripts/utils.sh && docker-compose -f ../../environment/plaintext/docker-compose.yml -f ${PWD}/docker-compose.plaintext.cdb-table-mtls.yml up -d ${profile_control_center_command} ${profile_ksqldb_command} ${profile_grafana_command} ${profile_kcat_command} up -d"
+echo "$command" > /tmp/playground-command
+log "✨ If you modify a docker-compose file and want to re-create the container(s), run cli command playground container recreate"
 
 ../../scripts/wait-for-connect-and-controlcenter.sh
 
