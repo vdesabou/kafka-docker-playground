aaa795630ca9e4b98ecbc81e5441571db0a457e2
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-05-30 21:24:27 +0200

    fix

--- multi-data-center/mirrormaker2/mirrormaker2-plaintext.sh
+++ multi-data-center/mirrormaker2/mirrormaker2-plaintext.sh
@@ -32,10 +32,10 @@ sleep 120
 # By default MM2 renames source topics to be prefixed with the source cluster name. e.g. if topic foo came from cluster A then it would be named A.foo on the destination. In the current release (5.4) MM2 does not support any different topic naming strategies out of the box.
 
 log "Verify we have received the data in topic US.sales_US in EUROPE"
-playground topic consume --topic "US.sales_US" --min-expected-messages 10 --timeout 60
+timeout 60 docker container exec broker-europe kafka-console-consumer --bootstrap-server localhost:9092 --topic "US.sales_US" --from-beginning --max-messages 10
 
 log "Verify we have received the data in topic EUROPE.sales_EUROPE topics in the US"
-playground topic consume --topic "EUROPE.sales_EUROPE" --min-expected-messages 10 --timeout 60
+timeout 60 docker container exec broker-us kafka-console-consumer --bootstrap-server localhost:9092 --topic "EUROPE.sales_EUROPE" --from-beginning --max-messages 10
 
 log "Copying mirrormaker logs to /tmp/mirrormaker.log"
 docker cp connect-us:/tmp/mirrormaker.log /tmp/mirrormaker.log
