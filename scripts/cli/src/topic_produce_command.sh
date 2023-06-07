d97260cd08d9feda559894a1c13d11d631ba4919
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-06-08 00:52:24 +0200

    wip with json and auto-detect schema

--- scripts/cli/src/topic_produce_command.sh
+++ scripts/cli/src/topic_produce_command.sh
@@ -89,9 +89,41 @@ then
   source $root_folder/scripts/utils.sh
 fi
 
-# value_type
-value_type=protobuf
+if grep -q "proto3" $schema_file
+then
+    log "schema was detected as protobuf"
+    value_type=protobuf
+elif grep -q "\$schema" $schema_file
+then
+    log "schema was detected as json schema"
+    value_type=json-schema
+elif grep -q "_meta" $schema_file
+then
+    log "schema was detected as json"
+    value_type=json
+elif grep -q "\"type\"\s*:\s*\"record\"" $schema_file
+then
+    log "schema was detected as avro"
+    value_type=avro
+else
+    logerror "Could not detect schema type for $schema_file"
+    cat $schema_file
+    exit 1
+fi
+
 case "${value_type}" in
+
+json)
+    # https://github.com/MaterializeInc/datagen
+    docker run --rm -i -v $schema_file:/app/schema.json materialize/datagen -s schema.json -n $nb_messages --dry-run | grep "Payload: " | awk '{print $2}' > $tmp_dir/out.json
+    #--record-size 104
+
+    log "payload is"
+    cat $tmp_dir/out.json
+
+    cat $tmp_dir/out.json | docker exec -i $container kafka-console-producer --broker-list $bootstrap_server --topic $topic $security
+;;
+
 avro)
     docker run --rm -v $tmp_dir:/tmp/ vdesabou/avro-tools random /tmp/out.avro --schema-file /tmp/value_schema --count $nb_messages
     docker run --rm -v $tmp_dir:/tmp/ vdesabou/avro-tools tojson /tmp/out.avro > $tmp_dir/out.json
@@ -106,7 +138,6 @@ avro-with-key)
 
 ;;
 json-schema)
-
     docker run --rm -v $tmp_dir:/tmp/ -e NB_MESSAGES=$nb_messages vdesabou/json-schema-faker > $tmp_dir/out.json
 
     log "payload is"
@@ -118,7 +149,6 @@ json-schema-with-key)
 
 ;;
 protobuf)
-
     # https://github.com/JasonkayZK/mock-protobuf.js
     docker run --rm -v $tmp_dir:/tmp/ -v $schema_file:/app/schema.proto -e NB_MESSAGES=$nb_messages vdesabou/protobuf-faker  > $tmp_dir/out.json
 
@@ -126,10 +156,6 @@ protobuf)
     cat $tmp_dir/out.json
 
     cat $tmp_dir/out.json | docker exec -e SCHEMA_REGISTRY_LOG4J_OPTS="-Dlog4j.configuration=file:/etc/kafka/tools-log4j.properties" -i $container kafka-$value_type-console-producer --broker-list $bootstrap_server --property schema.registry.url=$sr_url_cli --topic $topic $security --property value.schema="$(cat $schema_file)"
-    #log "payload is"
-    #cat $tmp_dir/mock-pb-output/*.json
-
-    # cat $tmp_dir/out.json | docker exec -e SCHEMA_REGISTRY_LOG4J_OPTS="-Dlog4j.configuration=file:/etc/kafka/tools-log4j.properties" -i $container kafka-$value_type-console-producer --broker-list $bootstrap_server --property schema.registry.url=$sr_url_cli --topic $topic $security --property value.schema="$(cat $schema_file)"
 ;;
 protobuf-with-key)
 
@@ -137,7 +163,7 @@ protobuf-with-key)
 none)
 ;;
 *)
-    logerror "producer name not valid ! Should be one of avro, avro-with-key, json-schema, json-schema-with-key, protobuf or protobuf-with-key"
+    logerror "producer name not valid ! Should be one of json, avro, avro-with-key, json-schema, json-schema-with-key, protobuf or protobuf-with-key"
     exit 1
 ;;
 esac
