d1fd25832ad85b17634fe6656fb1fc866f57bff7
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-06-08 22:48:28 +0200

    minor

--- connect/connect-jdbc-mysql-sink/mysql-sink-mtls.sh
+++ connect/connect-jdbc-mysql-sink/mysql-sink-mtls.sh
@@ -54,12 +54,12 @@ docker-compose -f ../../environment/plaintext/docker-compose.yml -f "${PWD}/dock
 log "Creating MySQL sink connector with server side Encrypted Connections (using <usermtls> user which requires SSL)"
 playground connector create-or-update --connector mysql-mtls-sink << EOF
 {
-               "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
-               "tasks.max": "1",
-               "connection.url": "jdbc:mysql://mysql:3306/db?user=usermtls&password=password&verifyServerCertificate=true&useSSL=true&requireSSL=true&enabledTLSProtocols=TLSv1,TLSv1.1,TLSv1.2,TLSv1.3",
-               "topics": "orders",
-               "auto.create": "true"
-          }
+    "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+    "tasks.max": "1",
+    "connection.url": "jdbc:mysql://mysql:3306/db?user=usermtls&password=password&verifyServerCertificate=true&useSSL=true&requireSSL=true&enabledTLSProtocols=TLSv1,TLSv1.1,TLSv1.2,TLSv1.3",
+    "topics": "orders",
+    "auto.create": "true"
+}
 EOF
 
 
--- connect/connect-jdbc-mysql-sink/mysql-sink.sh
+++ connect/connect-jdbc-mysql-sink/mysql-sink.sh
@@ -17,12 +17,12 @@ ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml
 log "Creating MySQL sink connector"
 playground connector create-or-update --connector mysql-sink << EOF
 {
-               "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
-                    "tasks.max": "1",
-                    "connection.url": "jdbc:mysql://mysql:3306/db?user=user&password=password&useSSL=false",
-                    "topics": "orders",
-                    "auto.create": "true"
-          }
+     "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+     "tasks.max": "1",
+     "connection.url": "jdbc:mysql://mysql:3306/db?user=user&password=password&useSSL=false",
+     "topics": "orders",
+     "auto.create": "true"
+}
 EOF
 
 
