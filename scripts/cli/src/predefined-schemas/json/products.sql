92588c872def76170027fe94f48bfae69ed61248
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-06-09 10:45:52 +0200

    wip

---
+++ scripts/cli/src/predefined-schemas/json/products.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "notused"."notused" (
+  "id" int PRIMARY KEY,
+  "name" varchar COMMENT 'faker.internet.userName()',
+  "merchant_id" int NOT NULL COMMENT 'faker.datatype.number()',
+  "price" int COMMENT 'faker.datatype.number()',
+  "status" int COMMENT 'faker.datatype.boolean()'
+);
