a7d08524db0c07db5a4e056cd3da19329478b33b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-06-09 17:42:51 +0200

    fix

--- connect/connect-github-source/github-source.sh
+++ connect/connect-github-source/github-source.sh
@@ -18,55 +18,51 @@ ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml
 
 if version_gt $CONNECTOR_TAG "1.9.9"
 then
-     log "Creating Github Source connector"
-     curl -X PUT \
-          -H "Content-Type: application/json" \
-          --data '{
-                    "connector.class": "io.confluent.connect.github.GithubSourceConnector",
-                    "topic.name.pattern":"github-topic-\${resourceName}",
-                    "tasks.max": "1",
-                    "github.service.url":"https://api.github.com",
-                    "github.repositories":"apache/kafka",
-                    "github.resources":"stargazers",
-                    "github.since":"2019-01-01",
-                    "github.access.token": "$CONNECTOR_GITHUB_ACCESS_TOKEN",
-                    "key.converter": "io.confluent.connect.avro.AvroConverter",
-                    "key.converter.schema.registry.url":"http://schema-registry:8081",
-                    "value.converter": "io.confluent.connect.avro.AvroConverter",
-                    "value.converter.schema.registry.url":"http://schema-registry:8081",
-                    "confluent.license": "",
-                    "confluent.topic.bootstrap.servers": "broker:9092",
-                    "confluent.topic.replication.factor": "1",
-                    "errors.tolerance": "all",
-                    "errors.log.enable": "true",
-                    "errors.log.include.messages": "true"
-               }' \
-          http://localhost:8083/connectors/github-source/config | jq .
+     playground connector create-or-update --connector github-source << EOF
+{
+     "connector.class": "io.confluent.connect.github.GithubSourceConnector",
+     "topic.name.pattern":"github-topic-\${resourceName}",
+     "tasks.max": "1",
+     "github.service.url":"https://api.github.com",
+     "github.repositories":"apache/kafka",
+     "github.resources":"stargazers",
+     "github.since":"2019-01-01",
+     "github.access.token": "$CONNECTOR_GITHUB_ACCESS_TOKEN",
+     "key.converter": "io.confluent.connect.avro.AvroConverter",
+     "key.converter.schema.registry.url":"http://schema-registry:8081",
+     "value.converter": "io.confluent.connect.avro.AvroConverter",
+     "value.converter.schema.registry.url":"http://schema-registry:8081",
+     "confluent.license": "",
+     "confluent.topic.bootstrap.servers": "broker:9092",
+     "confluent.topic.replication.factor": "1",
+     "errors.tolerance": "all",
+     "errors.log.enable": "true",
+     "errors.log.include.messages": "true"
+}
+EOF
 else
-     log "Creating Github Source connector"
-     curl -X PUT \
-          -H "Content-Type: application/json" \
-          --data '{
-                    "connector.class": "io.confluent.connect.github.GithubSourceConnector",
-                    "topic.name.pattern":"github-topic-${entityName}",
-                    "tasks.max": "1",
-                    "github.service.url":"https://api.github.com",
-                    "github.repositories":"apache/kafka",
-                    "github.tables":"stargazers",
-                    "github.since":"2019-01-01",
-                    "github.access.token": "$CONNECTOR_GITHUB_ACCESS_TOKEN",
-                    "key.converter": "io.confluent.connect.avro.AvroConverter",
-                    "key.converter.schema.registry.url":"http://schema-registry:8081",
-                    "value.converter": "io.confluent.connect.avro.AvroConverter",
-                    "value.converter.schema.registry.url":"http://schema-registry:8081",
-                    "confluent.license": "",
-                    "confluent.topic.bootstrap.servers": "broker:9092",
-                    "confluent.topic.replication.factor": "1",
-                    "errors.tolerance": "all",
-                    "errors.log.enable": "true",
-                    "errors.log.include.messages": "true"
-               }' \
-          http://localhost:8083/connectors/github-source/config | jq .
+     playground connector create-or-update --connector github-source << EOF
+{
+     "connector.class": "io.confluent.connect.github.GithubSourceConnector",
+     "topic.name.pattern":"github-topic-${entityName}",
+     "tasks.max": "1",
+     "github.service.url":"https://api.github.com",
+     "github.repositories":"apache/kafka",
+     "github.tables":"stargazers",
+     "github.since":"2019-01-01",
+     "github.access.token": "$CONNECTOR_GITHUB_ACCESS_TOKEN",
+     "key.converter": "io.confluent.connect.avro.AvroConverter",
+     "key.converter.schema.registry.url":"http://schema-registry:8081",
+     "value.converter": "io.confluent.connect.avro.AvroConverter",
+     "value.converter.schema.registry.url":"http://schema-registry:8081",
+     "confluent.license": "",
+     "confluent.topic.bootstrap.servers": "broker:9092",
+     "confluent.topic.replication.factor": "1",
+     "errors.tolerance": "all",
+     "errors.log.enable": "true",
+     "errors.log.include.messages": "true"
+}
+EOF
 fi
 
 sleep 10
