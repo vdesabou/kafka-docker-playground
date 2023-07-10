c06b12ad9b87f1c7966d5fc516cdf97b94bc083b
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-07-10 22:08:07 +0200

    playground topic produce

--- connect/connect-aws-cloudwatch-metrics-sink/cloudwatch-metrics-sink.sh
+++ connect/connect-aws-cloudwatch-metrics-sink/cloudwatch-metrics-sink.sh
@@ -44,10 +44,72 @@ ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml
 
 TIMESTAMP=`date +%s000`
 log "Sending messages to topic cloudwatch-metrics-topic"
-docker exec -i connect kafka-avro-console-producer --broker-list broker:9092 --property schema.registry.url=http://schema-registry:8081 --topic cloudwatch-metrics-topic --property parse.key=true --property key.separator=, --property key.schema='{"type":"string"}' --property value.schema='{"name": "myMetric","type": "record","fields": [{"name": "name","type": "string"},{"name": "type","type": "string"},{"name": "timestamp","type": "long"},{"name": "dimensions","type": {"name": "dimensions","type": "record","fields": [{"name": "dimensions1","type": "string"},{"name": "dimensions2","type": "string"}]}},{"name": "values","type": {"name": "values","type": "record","fields": [{"name":"count", "type": "double"},{"name":"oneMinuteRate", "type": "double"},{"name":"fiveMinuteRate", "type": "double"},{"name":"fifteenMinuteRate", "type": "double"},{"name":"meanRate", "type": "double"}]}}]}' << EOF
-"key1", {"name" : "test_meter","type" : "meter", "timestamp" : $TIMESTAMP, "dimensions" : {"dimensions1" : "InstanceID","dimensions2" : "i-aaba32d4"},"values" : {"count" : 32423.0,"oneMinuteRate" : 342342.2,"fiveMinuteRate" : 34234.2,"fifteenMinuteRate" : 2123123.1,"meanRate" : 2312312.1}}
+playground topic produce --topic cloudwatch-metrics-topic --nb-messages 1 --key "key1" --forced-value "{\"name\" : \"test_meter\",\"type\" : \"meter\", \"timestamp\" : $TIMESTAMP, \"dimensions\" : {\"dimensions1\" : \"InstanceID\",\"dimensions2\" : \"i-aaba32d4\"},\"values\" : {\"count\" : 32423.0,\"oneMinuteRate\" : 342342.2,\"fiveMinuteRate\" : 34234.2,\"fifteenMinuteRate\" : 2123123.1,\"meanRate\" : 2312312.1}}" << 'EOF'
+{
+  "name": "myMetric",
+  "type": "record",
+  "fields": [
+    {
+      "name": "name",
+      "type": "string"
+    },
+    {
+      "name": "type",
+      "type": "string"
+    },
+    {
+      "name": "timestamp",
+      "type": "long"
+    },
+    {
+      "name": "dimensions",
+      "type": {
+        "name": "dimensions",
+        "type": "record",
+        "fields": [
+          {
+            "name": "dimensions1",
+            "type": "string"
+          },
+          {
+            "name": "dimensions2",
+            "type": "string"
+          }
+        ]
+      }
+    },
+    {
+      "name": "values",
+      "type": {
+        "name": "values",
+        "type": "record",
+        "fields": [
+          {
+            "name": "count",
+            "type": "double"
+          },
+          {
+            "name": "oneMinuteRate",
+            "type": "double"
+          },
+          {
+            "name": "fiveMinuteRate",
+            "type": "double"
+          },
+          {
+            "name": "fifteenMinuteRate",
+            "type": "double"
+          },
+          {
+            "name": "meanRate",
+            "type": "double"
+          }
+        ]
+      }
+    }
+  ]
+}
 EOF
-
 
 CLOUDWATCH_METRICS_URL="https://monitoring.$AWS_REGION.amazonaws.com"
 
@@ -57,8 +119,6 @@ playground connector create-or-update --connector aws-cloudwatch-metrics-sink <<
     "tasks.max": "1",
     "topics": "cloudwatch-metrics-topic",
     "connector.class": "io.confluent.connect.aws.cloudwatch.metrics.AwsCloudWatchMetricsSinkConnector",
-    "key.converter": "io.confluent.connect.avro.AvroConverter",
-    "key.converter.schema.registry.url": "http://schema-registry:8081",
     "value.converter": "io.confluent.connect.avro.AvroConverter",
     "value.converter.schema.registry.url": "http://schema-registry:8081",
     "aws.cloudwatch.metrics.url": "$CLOUDWATCH_METRICS_URL",
