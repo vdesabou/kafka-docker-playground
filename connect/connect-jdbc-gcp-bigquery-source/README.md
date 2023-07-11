18ed58f5c8d444d30cf3cdb591f7d040c7b1f489
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-07-11 16:32:19 +0200

    wip

---
+++ connect/connect-jdbc-gcp-bigquery-source/README.md
@@ -0,0 +1,45 @@
+# JDBC GCP BigQuery Sink connector
+
+
+
+## Objective
+
+Quickly test [JDBC GCP BigQuery Source](https://docs.confluent.io/current/connect/kafka-connect-jdbc/source-connector/index.html#kconnect-long-jdbc-source-connector) connector.
+
+Driver used is https://cloud.google.com/bigquery/docs/reference/odbc-jdbc-drivers
+Docs: https://storage.googleapis.com/simba-bq-release/odbc/Simba%20Google%20BigQuery%20ODBC%20Connector%20Install%20and%20Configuration%20Guide-2.5.2.1004.pdf
+
+* Active Google Cloud Platform (GCP) account with authorization to create resources
+
+## GCP BigQuery Setup
+
+* Follow [Quickstart using the web UI in the GCP Console](https://cloud.google.com/bigquery/docs/quickstarts/quickstart-web-ui) to get familiar with GCP BigQuery
+* Create `Service Account` from IAM & Admin console:
+
+Set `Service account name`:
+
+![Service Account setup](Screenshot1.png)
+
+
+Choose permission `BigQuery`->`BigQuery Admin`:
+
+![Service Account setup](Screenshot2.png)
+
+Create Key:
+
+![Service Account setup](Screenshot3.png)
+
+Download it as JSON:
+
+![Service Account setup](Screenshot4.png)
+
+Rename it to `keyfile.json`and place it in `./keyfile.json` or use environment variable `GCP_KEYFILE_CONTENT` with content generated with `GCP_KEYFILE_CONTENT=`cat keyfile.json | jq -aRs .`
+
+
+## How to run
+
+Simply run:
+
+```bash
+$ playground run -f jdbc-gcp-bigquery<tab> <GCP_PROJECT>
+```
---
+++ connect/connect-jdbc-gcp-bigquery-source/jdbc-gcp-bigquery-source.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ -z "$GCP_PROJECT" ]
+then
+     logerror "GCP_PROJECT is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ ! -f ${DIR}/GoogleBigQueryJDBC42 ]
+then
+    mkdir -p GoogleBigQueryJDBC42
+    cd GoogleBigQueryJDBC42
+    wget https://storage.googleapis.com/simba-bq-release/jdbc/SimbaBigQueryJDBC42-1.3.2.1003.zip
+    unzip SimbaBigQueryJDBC42-1.3.2.1003.zip
+    cd -
+fi
+
+cd ../../connect/connect-jdbc-gcp-bigquery-source
+GCP_KEYFILE="${PWD}/keyfile.json"
+if [ ! -f ${GCP_KEYFILE} ] && [ -z "$GCP_KEYFILE_CONTENT" ]
+then
+     logerror "ERROR: either the file ${GCP_KEYFILE} is not present or environment variable GCP_KEYFILE_CONTENT is not set!"
+     exit 1
+else 
+    if [ -f ${GCP_KEYFILE} ]
+    then
+        GCP_KEYFILE_CONTENT=`cat keyfile.json | jq -aRs .`
+    else
+        log "Creating ${GCP_KEYFILE} based on environment variable GCP_KEYFILE_CONTENT"
+        echo -e "$GCP_KEYFILE_CONTENT" | sed 's/\\"/"/g' > ${GCP_KEYFILE}
+    fi
+fi
+cd -
+
+DATASET=pg${USER}ds${GITHUB_RUN_NUMBER}${TAG}
+DATASET=${DATASET//[-._]/}
+
+log "Doing gsutil authentication"
+set +e
+docker rm -f gcloud-config
+set -e
+docker run -i -v ${GCP_KEYFILE}:/tmp/keyfile.json --name gcloud-config google/cloud-sdk:latest gcloud auth activate-service-account --project ${GCP_PROJECT} --key-file /tmp/keyfile.json
+
+set +e
+log "Drop dataset $DATASET, this might fail"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$GCP_PROJECT" rm -r -f -d "$DATASET"
+sleep 1
+# https://github.com/GoogleCloudPlatform/terraform-google-secured-data-warehouse/issues/35
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$GCP_PROJECT" rm -r -f -d "$DATASET"
+set -e
+
+log "Create dataset $GCP_PROJECT.$DATASET"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$GCP_PROJECT" mk --dataset --description "used by playground" "$DATASET"
+
+log "Create table $GCP_PROJECT:$DATASET.customers"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq  mk --table --description "customers table" $GCP_PROJECT:$DATASET.customers id:INTEGER,first_name:STRING,last_name:STRING,email:STRING
+
+log "insert a row"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$GCP_PROJECT" query --nouse_legacy_sql "INSERT INTO $DATASET.customers(first_name,last_name,email) VALUES ('Sally','Thomas','sally.thomas@acme.com');" > /tmp/result.log  2>&1
+cat /tmp/result.log
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+
+log "Creating JDBC GCP BigQuery Source connector"
+playground connector create-or-update --connector jdbc-gcp-bigquery-source << EOF
+{
+    "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+    "tasks.max": "1",
+    "connection.url": "jdbc:bigquery://https://www.googleapis.com/bigquery/v2:443;ProjectId=$GCP_PROJECT;OAuthType=0;OAuthServiceAcctEmail=$SERVICE_ACCOUNT_EMAIL;OAuthPvtKeyPath=/tmp/keyfile.json;DefaultDataset=$DATASET;",
+    "table.whitelist": "customers",
+    "mode": "bulk",
+    "topic.prefix": "gcp-customers",
+    "validate.non.null":"false",
+    "errors.log.enable": "true",
+    "errors.log.include.messages": "true"
+}
+EOF
+
+# [2023-07-11 14:30:12,291] ERROR [jdbc-gcp-bigquery-source|task-0] SQL exception while running query for table: BulkTableQuerier{table='"vincent-de-saboulin-lab"."pgvsaboulinds740"."customers"', query='null', topicPrefix='gcp-customers'}, java.sql.SQLException: [Simba][BigQueryJDBCDriver](100032) Error executing query job. Message: EXEC_JOB_EXECUTION_ERR. Attempting retry 1 of -1 attempts. (io.confluent.connect.jdbc.source.JdbcSourceTask:455)
+
+playground topic consume
