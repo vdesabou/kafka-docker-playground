e579161a2f2e3844a4156006d5ad04c3d02d4f2e
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-07-12 18:10:37 +0200

    fix

--- connect/connect-ftps-sink/ftps-sink.sh
+++ connect/connect-ftps-sink/ftps-sink.sh
@@ -49,34 +49,16 @@ playground connector create-or-update --connector ftps-sink << EOF
 EOF
 
 log "Sending messages to topic test_ftps_sink"
-playground topic produce -t test_ftps_sink --nb-messages 10 << 'EOF'
+playground topic produce -t test_ftps_sink --nb-messages 10 --forced-value '{"f1":"value%g"}' << 'EOF'
 {
-    "type": "record",
-    "namespace": "com.github.vdesabou",
-    "name": "Customer",
-    "version": "1",
-    "fields": [
-        {
-            "name": "count",
-            "type": "long",
-            "doc": "count"
-        },
-        {
-            "name": "first_name",
-            "type": "string",
-            "doc": "First Name of Customer"
-        },
-        {
-            "name": "last_name",
-            "type": "string",
-            "doc": "Last Name of Customer"
-        },
-        {
-            "name": "address",
-            "type": "string",
-            "doc": "Address of Customer"
-        }
-    ]
+  "type": "record",
+  "name": "myrecord",
+  "fields": [
+    {
+      "name": "f1",
+      "type": "string"
+    }
+  ]
 }
 EOF
 
