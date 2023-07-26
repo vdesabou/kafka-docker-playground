78af1e2533d5bd7ac5937320c98549cccd98c02a
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-07-26 10:41:11 +0200

    fix

--- scripts/cli/src/topic_produce_command.sh
+++ scripts/cli/src/topic_produce_command.sh
@@ -300,8 +300,11 @@ then
             playground topic create --topic $topic
         fi
     else
-        log "--nb-partitions is set, creating topic with $nb_partitions partitions"
-        playground topic create --topic $topic --nb-partitions $nb_partitions
+        if [ "$nb_partitions" != "" ]
+        then
+            log "--nb-partitions is set, creating topic with $nb_partitions partitions"
+            playground topic create --topic $topic --nb-partitions $nb_partitions
+        fi
     fi
 else
     if [ "$nb_partitions" != "" ]
