f4801acde5107141b96251757b4a25e4719fd87c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-07-28 17:58:01 +0200

    fix for #4214

--- environment/kraft-plaintext/start.sh
+++ environment/kraft-plaintext/start.sh
@@ -63,11 +63,18 @@ then
   check_arm64_support "${DIR}" "${DOCKER_COMPOSE_FILE_OVERRIDE}"
 fi
 
-docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/kraft-plaintext/docker-compose.yml ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} build
-docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/kraft-plaintext/docker-compose.yml ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} down -v --remove-orphans
-docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/kraft-plaintext/docker-compose.yml ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} ${profile_control_center_command} ${profile_ksqldb_command} ${profile_grafana_command} ${profile_kcat_command} up -d
+if ! version_gt $TAG_BASE "7.3.99"
+then
+  KRAFT_DOCKER_COMPOSE="docker-compose.yml"
+else
+  KRAFT_DOCKER_COMPOSE="docker-compose.controller.yml"
+fi
+
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/kraft-plaintext/$KRAFT_DOCKER_COMPOSE ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} build
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/kraft-plaintext/$KRAFT_DOCKER_COMPOSE ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} down -v --remove-orphans
+docker-compose -f ../../environment/plaintext/docker-compose.yml -f ../../environment/kraft-plaintext/$KRAFT_DOCKER_COMPOSE ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} ${profile_control_center_command} ${profile_ksqldb_command} ${profile_grafana_command} ${profile_kcat_command} up -d
 log "📝 To see the actual properties file, use cli command playground get-properties -c <container>"
-command="source ${DIR}/../../scripts/utils.sh && docker-compose -f ${DIR}/../../environment/plaintext/docker-compose.yml -f ${DIR}/../../environment/kraft-plaintext/docker-compose.yml ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} ${profile_control_center_command} ${profile_ksqldb_command} ${profile_grafana_command} ${profile_kcat_command} up -d"
+command="source ${DIR}/../../scripts/utils.sh && docker-compose -f ${DIR}/../../environment/plaintext/docker-compose.yml -f ${DIR}/../../environment/kraft-plaintext/$KRAFT_DOCKER_COMPOSE ${ENABLE_DOCKER_COMPOSE_FILE_OVERRIDE} ${profile_control_center_command} ${profile_ksqldb_command} ${profile_grafana_command} ${profile_kcat_command} up -d"
 echo "$command" > /tmp/playground-command
 log "✨ If you modify a docker-compose file and want to re-create the container(s), run cli command playground container recreate"
 
--- environment/kraft-plaintext/stop.sh
+++ environment/kraft-plaintext/stop.sh
@@ -2,10 +2,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+if ! version_gt $TAG_BASE "7.3.99"
+then
+  KRAFT_DOCKER_COMPOSE="docker-compose.yml"
+else
+  KRAFT_DOCKER_COMPOSE="docker-compose.controller.yml"
+fi
+
 DOCKER_COMPOSE_FILE_OVERRIDE=$1
 if [ -f "${DOCKER_COMPOSE_FILE_OVERRIDE}" ]
 then
-  docker-compose -f ../../environment/plaintext/docker-compose.yml -f ${DOCKER_COMPOSE_FILE_OVERRIDE} down -v --remove-orphans
+  docker-compose -f ../../environment/plaintext/$$KRAFT_DOCKER_COMPOSE -f ${DOCKER_COMPOSE_FILE_OVERRIDE} down -v --remove-orphans
 else
-  docker-compose -f ../../environment/plaintext/docker-compose.yml down -v --remove-orphans
+  docker-compose -f ../../environment/plaintext/$$KRAFT_DOCKER_COMPOSE down -v --remove-orphans
 fi
