a0db7d24887913d1945d277c6241b34f17ca40dd
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-08-28 17:27:18 +0200

    Add MQTT Proxy example #3830

---
+++ other/mqtt-proxy/README.md
@@ -0,0 +1,22 @@
+# MQTT Proxy
+
+
+
+## Objective
+
+Quickly test [MQTT proxy](https://docs.confluent.io/platform/current/kafka-mqtt/intro.html).
+
+
+## How to run
+
+Simply run:
+
+```
+$ playground run -f mqtt-proxy<tab>
+```
+
+or with MTLS
+
+```
+$ playground run -f mqtt-souproxyrce-mtls<tab>
+```
---
+++ other/mqtt-proxy/mqtt-proxy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml"
+
+
+log "Send message to MQTT in car/engine/temperature topic"
+docker exec mosquitto sh -c 'mosquitto_pub -h mqtt-proxy -p 1883 -t "car/engine/temperature" -q 2 -m "190F"'
+
+sleep 5
+
+log "Verify we have received the data in temperature topic"
+playground topic consume --topic temperature --min-expected-messages 1 --timeout 60
