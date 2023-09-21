a6c8c9e1e3ba0ea3761e325e8b8bce24e596dfd3
jdrean <jocelyn.drean@gmail.com>
2023-09-21 15:59:08 +0200

    Add ksqlDb quickstart

---
+++ ksqldb/quickstart-example/start.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+# make sure ksqlDB is not disabled
+export ENABLE_KSQLDB=true
+
+${DIR}/../../environment/plaintext/start.sh
+
+log "Create the ksqlDB streams"
+timeout 120 docker exec -i ksqldb-cli ksql http://ksqldb-server:8088 << EOF
+
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE STREAM riderLocations (profileId VARCHAR, latitude DOUBLE, longitude DOUBLE)
+  WITH (kafka_topic='locations', value_format='json', partitions=1);
+
+CREATE TABLE currentLocation AS
+  SELECT profileId,
+         LATEST_BY_OFFSET(latitude) AS la,
+         LATEST_BY_OFFSET(longitude) AS lo
+  FROM riderlocations
+  GROUP BY profileId
+  EMIT CHANGES;
+
+CREATE TABLE ridersNearMountainView AS
+  SELECT ROUND(GEO_DISTANCE(la, lo, 37.4133, -122.1162), -1) AS distanceInMiles,
+         COLLECT_LIST(profileId) AS riders,
+         COUNT(*) AS count
+  FROM currentLocation
+  GROUP BY ROUND(GEO_DISTANCE(la, lo, 37.4133, -122.1162), -1);
+EOF
+
+log "Insert records to the stream"
+timeout 120 docker exec -i ksqldb-cli ksql http://ksqldb-server:8088 << EOF
+INSERT INTO riderLocations (profileId, latitude, longitude) VALUES ('c2309eec', 37.7877, -122.4205);
+INSERT INTO riderLocations (profileId, latitude, longitude) VALUES ('18f4ea86', 37.3903, -122.0643);
+INSERT INTO riderLocations (profileId, latitude, longitude) VALUES ('4ab5cbad', 37.3952, -122.0813);
+INSERT INTO riderLocations (profileId, latitude, longitude) VALUES ('8b6eae59', 37.3944, -122.0813);
+INSERT INTO riderLocations (profileId, latitude, longitude) VALUES ('4a7c7b41', 37.4049, -122.0822);
+INSERT INTO riderLocations (profileId, latitude, longitude) VALUES ('4ddad000', 37.7857, -122.4011);
+EOF
+
+# Wait for the stream to be initialized
+sleep 5
+
+log "Run a Pull query against the materialized view"
+timeout 120 docker exec -i ksqldb-cli ksql http://ksqldb-server:8088 << EOF
+SET 'auto.offset.reset' = 'earliest';
+
+SELECT * from ridersNearMountainView WHERE distanceInMiles <= 10;
+EOF
+
+log "We retrieved all the riders that are currently within 10 miles from Mountain View."
