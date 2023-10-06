11c4fb453b924142b8320fdc89c12017e4526922
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-10-06 17:32:58 +0200

    wip

--- scripts/cli/templates/shell-script-command-completion/render.rb
+++ scripts/cli/templates/shell-script-command-completion/render.rb
@@ -1,6 +1,14 @@
 # render script - shell-script-command-completion
 require 'gtx'
 
+require 'logger'
+
+# Create a new logger instance
+logger = Logger.new(STDOUT)
+
+# Set the logging level to INFO
+logger.level = Logger::INFO
+
 # Load the GTX template
 template = "#{source}/main.gtx"
 gtx = GTX.load_file template
@@ -16,20 +24,32 @@ template = "#{source}/subsubcommand.gtx"
 gtxsubsub = GTX.load_file template
 
 # Append to a file for each subcommand
-command.deep_commands.reject(&:private).each do |subcommand|
+processed_subcommands = []
 
+command.deep_commands.reject(&:private).each do |subcommand|
   if subcommand.commands.any?
     File.open("#{target}/playground.yaml", "a") do |file|
       file.write(gtxsub.parse(subcommand))
     end
     subcommand.commands.reject(&:private).each do |subsubcommand|
-      File.open("#{target}/playground.yaml", "a") do |file|
-        file.write(gtxsubsub.parse(subsubcommand))
+      unless processed_subcommands.include?(subsubcommand.full_name)
+        File.open("#{target}/playground.yaml", "a") do |file|
+          file.write(gtxsubsub.parse(subsubcommand))
+        end
+        processed_subcommands << subsubcommand.full_name
+        logger.info("process subcommand")
+        logger.info(subsubcommand.full_name)
       end
     end
+    processed_subcommands << subcommand.full_name
   else
-    # File.open("#{target}/playground.yaml", "a") do |file|
-    #   file.write(gtxsub.parse(subcommand))
-    # end
+    unless processed_subcommands.include?(subcommand.full_name)
+      File.open("#{target}/playground.yaml", "a") do |file|
+        file.write(gtxsub.parse(subcommand))
+      end
+      processed_subcommands << subcommand.full_name
+      logger.info("process command")
+      logger.info(subcommand.full_name)
+    end
   end
 end
