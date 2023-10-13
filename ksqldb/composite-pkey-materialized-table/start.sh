e56af49789fb97ae1a32b6af040999560460a529
jdrean <jocelyn.drean@gmail.com>
2023-10-13 12:31:05 +0200

    ksqldb - try with no composite where

--- ksqldb/composite-pkey-materialized-table/start.sh
+++ ksqldb/composite-pkey-materialized-table/start.sh
@@ -48,7 +48,37 @@ EMIT CHANGES LIMIT 4;
 
 EOF
 
-log "SELECT from this table"
+log "SELECT with a WHERE CLAUSE using ->"
+timeout 120 docker exec -i ksqldb-cli ksql http://ksqldb-server:8088 << EOF
+SET 'auto.offset.reset' = 'earliest';
+
+SELECT * FROM TRANSACTIONS
+WHERE TRANSACTION->CUSTOMER_ID=123 and TRANSACTION->PRODUCT_ID=10005.0 and TRANSACTION->TXN_ID=500005
+EMIT CHANGES LIMIT 1;
+
+EOF
+
+# expected output
+# +-----+-----+-----+
+# |TRANS|CREAT|DESCR|
+# |ACTIO|ED_AT|IPTIO|
+# |N    |     |N    |
+# +-----+-----+-----+
+# |{CUST|16971|buyin|
+# |OMER_|88928|g sto|
+# |ID=12|     |cks  |
+# |3, PR|     |     |
+# |ODUCT|     |     |
+# |_ID=1|     |     |
+# |0005.|     |     |
+# |0, TX|     |     |
+# |N_ID=|     |     |
+# |50000|     |     |
+# |5}   |     |     |
+# Limit Reached
+
+log "SELECT with a WHERE clause using STRUCT"
+#NOT WORKING
 timeout 120 docker exec -i ksqldb-cli ksql http://ksqldb-server:8088 << EOF
 SET 'auto.offset.reset' = 'earliest';
 
