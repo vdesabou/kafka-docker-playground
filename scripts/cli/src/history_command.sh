52530d2ba47ed031986e372d8ea8797c8ae09b81
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-12-02 14:40:40 +0100

    wip

--- scripts/cli/src/history_command.sh
+++ scripts/cli/src/history_command.sh
@@ -21,9 +21,17 @@ else
     fzf_option_rounded=""
 fi
 
-res=$(cat $root_folder/playground-run-history | sort -nr | uniq -u | fzf --margin=1%,1%,1%,1% $fzf_option_rounded --info=inline --prompt="🏰" --header="ctrl-c or esc to quit" --color="bg:-1,bg+:-1,info:#BDBB72,border:#FFFFFF,spinner:0,hl:#beb665,fg:#00f7f7,header:#5CC9F5,fg+:#beb665,pointer:#E12672,marker:#5CC9F5,prompt:#98BEDE" --delimiter / --with-nth "-3,-2,-1" $fzf_option_wrap $fzf_option_pointer)
+res=$(tac $root_folder/playground-run-history | sed '/^$/d' | fzf --margin=1%,1%,1%,1% $fzf_option_rounded --info=inline --prompt="🏰" --header="ctrl-c or esc to quit" --color="bg:-1,bg+:-1,info:#BDBB72,border:#FFFFFF,spinner:0,hl:#beb665,fg:#00f7f7,header:#5CC9F5,fg+:#beb665,pointer:#E12672,marker:#5CC9F5,prompt:#98BEDE" --delimiter / --with-nth "-3,-2,-1" $fzf_option_wrap $fzf_option_pointer)
 
-log "🚀 Are you sure you want to run:"
-echo "$res"
-check_if_continue
+# Prompt the user to edit the res variable
+read -e -p "" -i "$res" edited_res
+
+# Use the edited value if it is not empty
+if [[ -n "$edited_res" ]]; then
+  res="$edited_res"
+fi
+
+# log "🚀 Are you sure you want to run:"
+# echo "$res"
+#check_if_continue
 $res
