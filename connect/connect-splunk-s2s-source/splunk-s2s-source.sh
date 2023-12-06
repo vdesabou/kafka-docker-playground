6c2fac03a0f724252df2d70f50d3c2b1d073be2c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-12-06 09:48:09 +0100

    use CLI

--- connect/connect-splunk-s2s-source/splunk-s2s-source.sh
+++ connect/connect-splunk-s2s-source/splunk-s2s-source.sh
@@ -9,17 +9,17 @@ ${DIR}/../../environment/plaintext/start.sh "${PWD}/docker-compose.plaintext.yml
 log "Creating Splunk S2S source connector"
 playground connector create-or-update --connector splunk-s2s-source << EOF
 {
-               "connector.class": "io.confluent.connect.splunk.s2s.SplunkS2SSourceConnector",
-               "tasks.max": "1",
-               "kafka.topic": "splunk-s2s-events",
-               "splunk.collector.index.default": "default-index",
-               "splunk.s2s.port": "9997",
-               "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-               "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-               "value.converter.schemas.enable": "false",
-               "confluent.topic.bootstrap.servers": "broker:9092",
-               "confluent.topic.replication.factor": "1"
-          }
+     "connector.class": "io.confluent.connect.splunk.s2s.SplunkS2SSourceConnector",
+     "tasks.max": "1",
+     "kafka.topic": "splunk-s2s-events",
+     "splunk.collector.index.default": "default-index",
+     "splunk.s2s.port": "9997",
+     "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+     "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+     "value.converter.schemas.enable": "false",
+     "confluent.topic.bootstrap.servers": "broker:9092",
+     "confluent.topic.replication.factor": "1"
+}
 EOF
 
 sleep 5
@@ -40,11 +40,4 @@ docker exec -i splunk-uf sudo ./bin/splunk add forward-server connect:9997
 sleep 5
 
 log "Verifying topic splunk-s2s-events"
-set +e
-# rely on timeout command
-timeout 20 docker exec connect kafka-console-consumer -bootstrap-server broker:9092 --topic splunk-s2s-events --from-beginning > /tmp/result.log  2>&1
-set -e
-grep "log event 1" /tmp/result.log
-grep "log event 2" /tmp/result.log
-grep "log event 3" /tmp/result.log
-
+playground topic consume --topic splunk-s2s-events --timeout 20 --min-expected-messages 1 --grep "log event 1" --max-messages=1000
