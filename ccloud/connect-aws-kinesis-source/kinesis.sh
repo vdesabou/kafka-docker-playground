a430628f20fe4ca79bdcf89601840cce5a38dd5c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2023-12-23 18:23:44 +0100

    wip

--- ccloud/connect-aws-kinesis-source/kinesis.sh
+++ ccloud/connect-aws-kinesis-source/kinesis.sh
@@ -104,12 +104,10 @@ playground connector create-or-update --connector kinesis-source --environment "
     "confluent.license": "",
     "topic.creation.default.replication.factor": "-1",
     "topic.creation.default.partitions": "-1",
-    "confluent.topic.ssl.endpoint.identification.algorithm" : "https",
-    "confluent.topic.sasl.mechanism" : "PLAIN",
     "confluent.topic.bootstrap.servers": "\${file:/data:bootstrap.servers}",
     "confluent.topic.sasl.jaas.config" : "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"\${file:/data:sasl.username}\" password=\"\${file:/data:sasl.password}\";",
     "confluent.topic.security.protocol" : "SASL_SSL",
-    "confluent.topic.replication.factor": "3"
+    "confluent.topic.sasl.mechanism" : "PLAIN"
 }
 EOF
 
