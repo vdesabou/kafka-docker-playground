375632ae2d37da1dc5ba3cf22c4658014a4f3b75
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-01-04 20:07:55 +0000

    timeout change

--- multi-data-center/replicator-executable/executable-kerberos.sh
+++ multi-data-center/replicator-executable/executable-kerberos.sh
@@ -30,7 +30,7 @@ docker container exec -i replicator-europe bash -c 'kinit -k -t /var/lib/secret/
 sleep 120
 
 log "Verify we have received the data in all the sales_ topics in EUROPE"
-timeout 120 docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_consumer && kafka-console-consumer --bootstrap-server broker-europe:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/kafka/consumer-europe.properties'
+docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_consumer && timeout 120 kafka-console-consumer --bootstrap-server broker-europe:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/kafka/consumer-europe.properties'
 
 log "Verify we have received the data in all the sales_ topics in the US"
-timeout 120 docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_consumer && kafka-console-consumer --bootstrap-server broker-us:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/kafka/consumer-us.properties'
+docker container exec -i client bash -c 'kinit -k -t /var/lib/secret/kafka-client.key kafka_consumer && timeout 120 kafka-console-consumer --bootstrap-server broker-us:9092 --whitelist "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/kafka/consumer-us.properties'
