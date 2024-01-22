b4deba167ead4f9fe840afe84dccf4d4fccaba99
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-01-22 15:14:03 +0100

    limit: '10mb'

--- connect/connect-http-sink/httpserver-oauth2/app.js
+++ connect/connect-http-sink/httpserver-oauth2/app.js
@@ -6,8 +6,8 @@ var express = require('express'),
 
 var app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
--- connect/connect-http-sink/httpserver/server.js
+++ connect/connect-http-sink/httpserver/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 let errorCode = 200;
 let delay = 0; // response delay in ms
