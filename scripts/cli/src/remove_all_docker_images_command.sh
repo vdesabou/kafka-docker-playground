0f96c11fa420202f365702abc49880c31ec3e09d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-01-25 10:31:45 +0100

    fix

--- scripts/cli/src/remove_all_docker_images_command.sh
+++ scripts/cli/src/remove_all_docker_images_command.sh
@@ -1,6 +1,7 @@
 log "🧨 Remove all docker images (including docker volumes)"
 check_if_continue
 
+set +e
 playground container kill-all
 docker image rm $(docker image list | grep -v "oracle/database"  | grep -v "db-prebuilt" | awk 'NR>1 {print $3}') -f
 docker system prune -a -f
