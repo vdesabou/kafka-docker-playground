4a924beac4e4743c36b7f786b36c92aaf695e627
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-02-09 10:03:25 +0100

    added v2

---
+++ ccloud/fully-managed-connect-gcp-bigquery-v2-sink/README.md
@@ -0,0 +1,63 @@
+# Fully Managed GCP BigQuery Sink V2 connector
+
+## Objective
+
+Quickly test [GCP BigQuery Sink V2](https://docs.confluent.io/cloud/current/connectors/cc-gcp-bigquery-storage-sink.html) connector.
+
+* Active Google Cloud Platform (GCP) account with authorization to create resources
+
+## Prerequisites
+
+All you have to do is to be already logged in with [confluent CLI](https://docs.confluent.io/confluent-cli/current/overview.html#confluent-cli-overview).
+
+By default, a new Confluent Cloud environment with a Cluster will be created.
+
+You can configure the cluster by setting environment variables:
+
+* `CLUSTER_CLOUD`: The Cloud provider (possible values: `aws`, `gcp` and `azure`, default `aws`)
+* `CLUSTER_REGION`: The Cloud region (use `confluent kafka region list` to get the list, default `eu-west-2`)
+* `CLUSTER_TYPE`: The type of cluster (possible values: `basic`, `standard` and `dedicated`, default `basic`)
+* `ENVIRONMENT` (optional): The environment id where want your new cluster (example: `env-xxxxx`) 
+
+In case you want to use your own existing cluster, you need to setup these environment variables:
+
+* `ENVIRONMENT`: The environment id where your cluster is located (example: `env-xxxxx`) 
+* `CLUSTER_NAME`: The cluster name
+* `CLUSTER_CLOUD`: The Cloud provider (possible values: `aws`, `gcp` and `azure`)
+* `CLUSTER_REGION`: The Cloud region (example `us-east-2`)
+* `CLUSTER_CREDS`: The Kafka api key and secret to use, it should be separated with semi-colon (example: `<API_KEY>:<API_KEY_SECRET>`)
+* `SCHEMA_REGISTRY_CREDS` (optional, if not set, new one will be created): The Schema Registry api key and secret to use, it should be separated with semi-colon (example: `<SR_API_KEY>:<SR_API_KEY_SECRET>`)
+
+
+## GCP BigQuery Setup
+
+* Follow [Quickstart using the web UI in the GCP Console](https://cloud.google.com/bigquery/docs/quickstarts/quickstart-web-ui) to get familiar with GCP BigQuery
+* Create `Service Account` from IAM & Admin console:
+
+Set `Service account name`:
+
+![Service Account setup](Screenshot1.png)
+
+
+Choose permission `BigQuery`->`BigQuery Admin`:
+
+![Service Account setup](Screenshot2.png)
+
+Create Key:
+
+![Service Account setup](Screenshot3.png)
+
+Download it as JSON:
+
+![Service Account setup](Screenshot4.png)
+
+Rename it to `keyfile.json`and place it in `./keyfile.json` or use environment variable `GCP_KEYFILE_CONTENT` with content generated with `GCP_KEYFILE_CONTENT=`cat keyfile.json | jq -aRs .`
+
+
+## How to run
+
+Simply run:
+
+```bash
+$ playground run -f fully-managed-gcp-bigquery-v2<use tab key to activate fzf completion (see https://kafka-docker-playground.io/#/cli?id=%e2%9a%a1-setup-completion), otherwise use full path, or correct relative path> <GCP_PROJECT>
+```
---
+++ ccloud/fully-managed-connect-gcp-bigquery-v2-sink/fully-managed-gcp-bigquery-v2.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ -z "$GCP_PROJECT" ]
+then
+     logerror "GCP_PROJECT is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+GCP_KEYFILE="${DIR}/keyfile.json"
+if [ ! -f ${GCP_KEYFILE} ] && [ -z "$GCP_KEYFILE_CONTENT" ]
+then
+     logerror "ERROR: either the file ${GCP_KEYFILE} is not present or environment variable GCP_KEYFILE_CONTENT is not set!"
+     exit 1
+else 
+    if [ -f ${GCP_KEYFILE} ]
+    then
+        GCP_KEYFILE_CONTENT=`cat keyfile.json | jq -aRs .`
+    else
+        log "Creating ${GCP_KEYFILE} based on environment variable GCP_KEYFILE_CONTENT"
+        echo -e "$GCP_KEYFILE_CONTENT" | sed 's/\\"/"/g' > ${GCP_KEYFILE}
+    fi
+fi
+
+bootstrap_ccloud_environment
+
+
+
+DATASET=pg${USER}ds${GITHUB_RUN_NUMBER}${TAG}
+DATASET=${DATASET//[-._]/}
+
+log "Doing gsutil authentication"
+set +e
+docker rm -f gcloud-config
+set -e
+docker run -i -v ${GCP_KEYFILE}:/tmp/keyfile.json --name gcloud-config google/cloud-sdk:latest gcloud auth activate-service-account --project ${GCP_PROJECT} --key-file /tmp/keyfile.json
+
+set +e
+log "Drop dataset $DATASET, this might fail"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$GCP_PROJECT" rm -r -f -d "$DATASET"
+sleep 1
+# https://github.com/GoogleCloudPlatform/terraform-google-secured-data-warehouse/issues/35
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$GCP_PROJECT" rm -r -f -d "$DATASET"
+set -e
+
+log "Create dataset $GCP_PROJECT.$DATASET"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$GCP_PROJECT" mk --dataset --description "used by playground" "$DATASET"
+
+log "Creating bqtopic topic in Confluent Cloud"
+set +e
+playground topic create --topic bqtopic
+set -e
+
+log "Sending messages to topic bqtopic"
+playground topic produce -t bqtopic --nb-messages 10 --forced-value '{"f1":"value%g"}' << 'EOF'
+{
+  "type": "record",
+  "name": "myrecord",
+  "fields": [
+    {
+      "name": "f1",
+      "type": "string"
+    }
+  ]
+}
+EOF
+
+connector_name="BigQueryStorageSink"
+set +e
+log "Deleting fully managed connector $connector_name, it might fail..."
+playground connector delete --connector $connector_name
+set -e
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+  "connector.class": "BigQueryStorageSink",
+  "name": "BigQueryStorageSink",
+  "kafka.auth.mode": "KAFKA_API_KEY",
+  "kafka.api.key": "$CLOUD_KEY",
+  "kafka.api.secret": "$CLOUD_SECRET",
+  "topics": "bqtopic",
+  "keyfile" : $GCP_KEYFILE_CONTENT,
+  "project" : "$GCP_PROJECT",
+  "datasets" : "$DATASET",
+  "input.data.format" : "AVRO",
+  "auto.create.tables" : "NON-PARTITIONED",
+  "sanitize.topics" : "true",
+  "ingestion.mode" : "STREAMING",
+  "sanitize.field.names" : "true",
+  "tasks.max" : "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 300
+
+log "Sleeping 120 seconds"
+sleep 120
+
+log "Verify data is in GCP BigQuery:"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$GCP_PROJECT" query "SELECT * FROM $DATASET.bqtopic;" > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "value1" /tmp/result.log
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
+
+log "Drop dataset $DATASET"
+docker run -i --volumes-from gcloud-config google/cloud-sdk:latest bq --project_id "$GCP_PROJECT" rm -r -f -d "$DATASET"
+
+docker rm -f gcloud-config
