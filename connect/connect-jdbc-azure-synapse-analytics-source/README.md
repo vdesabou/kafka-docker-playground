2f3437fd83a388929350fb4efd948a1ff4be4e14
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-02-14 14:51:32 +0100

    Azure Synapse Analytics with JDBC source

---
+++ connect/connect-jdbc-azure-synapse-analytics-source/README.md
@@ -0,0 +1,15 @@
+# JDBC Azure Synapse Analytics Source connector
+
+
+
+## Objective
+
+Quickly test [JDBC Source](https://docs.confluent.io/current/connect/kafka-connect-jdbc/source-connector/index.html) connector with Azure Synapse Analytics.
+
+## How to run
+
+Simply run:
+
+```
+$ playground run -f jdbc-azure-synapse-analytics-source<use tab key to activate fzf completion (see https://kafka-docker-playground.io/#/cli?id=%e2%9a%a1-setup-completion), otherwise use full path, or correct relative path>
+```
---
+++ connect/connect-jdbc-azure-synapse-analytics-source/jdbc-azure-synapse-analytics-source.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+cd ../../connect/connect-jdbc-azure-synapse-analytics-source
+if [ ! -f ${PWD}/sqljdbc_12.2/enu/mssql-jdbc-12.2.0.jre11.jar ]
+then
+     log "Downloading Microsoft JDBC driver mssql-jdbc-12.2.0.jre11.jar"
+     curl -L https://go.microsoft.com/fwlink/?linkid=2222954 -o sqljdbc_12.2.0.0_enu.tar.gz
+     tar xvfz sqljdbc_12.2.0.0_enu.tar.gz
+     rm -f sqljdbc_12.2.0.0_enu.tar.gz
+fi
+cd -
+
+if [ ! -z "$AZ_USER" ] && [ ! -z "$AZ_PASS" ]
+then
+    log "Logging to Azure using environment variables AZ_USER and AZ_PASS"
+    set +e
+    az logout
+    set -e
+    az login -u "$AZ_USER" -p "$AZ_PASS" > /dev/null 2>&1
+else
+    log "Logging to Azure using browser"
+    az login
+fi
+
+AZURE_NAME=pg${USER}wh${GITHUB_RUN_NUMBER}${TAG}
+AZURE_NAME=${AZURE_NAME//[-._]/}
+AZURE_RESOURCE_GROUP=$AZURE_NAME
+AZURE_SQL_NAME=$AZURE_NAME
+AZURE_FIREWALL_RULL_NAME=$AZURE_NAME
+AZURE_DATA_WAREHOUSE_NAME=$AZURE_NAME
+AZURE_REGION=westeurope
+AZURE_SQL_URL="jdbc:sqlserver://$AZURE_SQL_NAME.database.windows.net:1433;databaseName=$AZURE_DATA_WAREHOUSE_NAME"
+PASSWORD=$(date +%s | cksum | base64 | head -c 32 ; echo)
+PASSWORD="${PASSWORD}1"
+
+set +e
+az group delete --name $AZURE_RESOURCE_GROUP --yes
+set -e
+
+log "Creating Azure Resource Group $AZURE_RESOURCE_GROUP"
+az group create \
+    --name $AZURE_RESOURCE_GROUP \
+    --location $AZURE_REGION \
+    --tags owner_email=$AZ_USER
+log "Creating SQL server instance $AZURE_SQL_NAME"
+az sql server create \
+    --name $AZURE_SQL_NAME \
+    --resource-group $AZURE_RESOURCE_GROUP \
+    --location $AZURE_REGION  \
+    --admin-user myadmin \
+    --admin-password $PASSWORD
+if [ ! -z "$GITHUB_RUN_NUMBER" ]
+then
+    # running with CI
+    # connect-jdbc-azure-synapse-analytics-source is failing #131
+    # allow applications from Azure IP addresses to connect to your Azure Database for MySQL server, provide the IP address 0.0.0.0 as the Start IP and End IP
+    az sql server firewall-rule create \
+    --name $AZURE_FIREWALL_RULL_NAME \
+    --resource-group $AZURE_RESOURCE_GROUP \
+    --server $AZURE_SQL_NAME \
+    --start-ip-address 0.0.0.0 \
+    --end-ip-address 0.0.0.0
+else
+    log "Enable a server-level firewall rule"
+    MY_IP=$(curl https://ipinfo.io/ip)
+    az sql server firewall-rule create \
+    --name $AZURE_FIREWALL_RULL_NAME \
+    --resource-group $AZURE_RESOURCE_GROUP \
+    --server $AZURE_SQL_NAME \
+    --start-ip-address $MY_IP \
+    --end-ip-address $MY_IP
+fi
+
+log "Create a SQL Data Warehouse instance"
+az sql dw create \
+    --name $AZURE_DATA_WAREHOUSE_NAME \
+    --resource-group $AZURE_RESOURCE_GROUP \
+    --server $AZURE_SQL_NAME
+
+# generate data file for externalizing secrets
+sed -e "s|:AZURE_SQL_URL:|$AZURE_SQL_URL|g" \
+    -e "s|:PASSWORD:|$PASSWORD|g" \
+    -e "s|:AZURE_DATA_WAREHOUSE_NAME:|$AZURE_DATA_WAREHOUSE_NAME|g" \
+    ../../connect/connect-jdbc-azure-synapse-analytics-source/data.template > ../../connect/connect-jdbc-azure-synapse-analytics-source/data
+
+PLAYGROUND_ENVIRONMENT=${PLAYGROUND_ENVIRONMENT:-"plaintext"}
+playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.yml"
+
+log "Create table"
+docker run -i fabiang/sqlcmd -S "$AZURE_SQL_NAME.database.windows.net,1433" -I -U "myadmin" -P "$PASSWORD" -d "$AZURE_DATA_WAREHOUSE_NAME" << EOF
+-- Create some customers ...
+
+DECLARE @Date DATETIME;
+SET @Date = GETDATE();
+
+CREATE TABLE customers (
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  last_update DATETIME
+);
+INSERT INTO customers(first_name,last_name,email,last_update)
+  VALUES ('Sally','Thomas','sally.thomas@acme.com',  @Date);
+INSERT INTO customers(first_name,last_name,email,last_update)
+  VALUES ('George','Bailey','gbailey@foobar.com',  @Date);
+INSERT INTO customers(first_name,last_name,email,last_update)
+  VALUES ('Edward','Walker','ed@walker.com',  @Date);
+INSERT INTO customers(first_name,last_name,email,last_update)
+  VALUES ('Anne','Kretchmar','annek@noanswer.org',  @Date);
+GO
+EOF
+
+log "Creating JDBC Azure Synapse Analytics Source connector"
+playground connector create-or-update --connector jdbc-synapse-source  << EOF
+{
+    "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+    "tasks.max": "1",
+    "connection.url": "\${file:/data:AZURE_SQL_URL}",
+    "connection.user": "myadmin",
+    "connection.password": "\${file:/data:PASSWORD}",
+    "table.whitelist": "customers",
+    "mode": "timestamp",
+    "timestamp.column.name": "last_update",
+    "topic.prefix": "synapse-",
+    "validate.non.null":"false",
+    "errors.log.enable": "true",
+    "errors.log.include.messages": "true"
+}
+EOF
+
+sleep 5
+
+playground topic consume --topic synapse-customers --min-expected-messages 4
+
+log "Deleting resource group"
+check_if_continue
+az group delete --name $AZURE_RESOURCE_GROUP --yes
