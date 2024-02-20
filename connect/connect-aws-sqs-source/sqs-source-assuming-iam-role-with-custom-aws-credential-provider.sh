51a1b0f4efc0f0c9ad2d95d41f222926be1920e5
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-02-20 08:57:46 +0100

    not working

---
+++ connect/connect-aws-sqs-source/sqs-source-assuming-iam-role-with-custom-aws-credential-provider.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+logwarn "⚠️ This example and associated custom code is not supported, use at your own risks !"
+
+AWS_STS_ROLE_ARN=${AWS_STS_ROLE_ARN:-$1}
+
+if [ -z "$AWS_STS_ROLE_ARN" ]
+then
+     logerror "AWS_STS_ROLE_ARN is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_ACCESS_KEY_ID" ]
+then
+     logerror "AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_ACCESS_KEY_ID is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_SECRET_ACCESS_KEY" ]
+then
+     logerror "AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_SECRET_ACCESS_KEY is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+for component in awscredentialsprovider
+do
+    set +e
+    log "🏗 Building jar for ${component}"
+    docker run -i --rm -e KAFKA_CLIENT_TAG=$KAFKA_CLIENT_TAG -e TAG=$TAG_BASE -v "${PWD}/${component}":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "$PWD/../../scripts/settings.xml:/tmp/settings.xml" -v "${PWD}/${component}/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven:3.6.1-jdk-11 mvn -s /tmp/settings.xml -Dkafka.tag=$TAG -Dkafka.client.tag=$KAFKA_CLIENT_TAG package > /tmp/result.log 2>&1
+    if [ $? != 0 ]
+    then
+        logerror "ERROR: failed to build java component "
+        tail -500 /tmp/result.log
+        exit 1
+    fi
+    set -e
+done
+
+if [ ! -f $HOME/.aws/credentials ] && ( [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] )
+then
+     logerror "ERROR: either the file $HOME/.aws/credentials is not present or environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are not set!"
+     exit 1
+else
+    if [ ! -z "$AWS_ACCESS_KEY_ID" ] && [ ! -z "$AWS_SECRET_ACCESS_KEY" ]
+    then
+        log "💭 Using environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY"
+        export AWS_ACCESS_KEY_ID
+        export AWS_SECRET_ACCESS_KEY
+    else
+        if [ -f $HOME/.aws/credentials ]
+        then
+            logwarn "💭 AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set based on $HOME/.aws/credentials"
+            export AWS_ACCESS_KEY_ID=$( grep "^aws_access_key_id" $HOME/.aws/credentials | head -1 | awk -F'=' '{print $2;}' )
+            export AWS_SECRET_ACCESS_KEY=$( grep "^aws_secret_access_key" $HOME/.aws/credentials | head -1 | awk -F'=' '{print $2;}' ) 
+        fi
+    fi
+    if [ -z "$AWS_REGION" ]
+    then
+        AWS_REGION=$(aws configure get region | tr '\r' '\n')
+        if [ "$AWS_REGION" == "" ]
+        then
+            logerror "ERROR: either the file $HOME/.aws/config is not present or environment variables AWS_REGION is not set!"
+            exit 1
+        fi
+    fi
+fi
+
+if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
+then
+     export CONNECT_CONTAINER_HOME_DIR="/home/appuser"
+else
+     export CONNECT_CONTAINER_HOME_DIR="/root"
+fi
+
+PLAYGROUND_ENVIRONMENT=${PLAYGROUND_ENVIRONMENT:-"plaintext"}
+playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.backup-and-restore-assuming-iam-role-with-custom-aws-credential-provider.yml"
+
+QUEUE_NAME=pg${USER}sqs${TAG}
+QUEUE_NAME=${QUEUE_NAME//[-._]/}
+
+QUEUE_URL_RAW=$(aws sqs create-queue --queue-name $QUEUE_NAME | jq .QueueUrl)
+AWS_ACCOUNT_NUMBER=$(echo "$QUEUE_URL_RAW" | cut -d "/" -f 4)
+# https://docs.amazonaws.cn/sdk-for-net/v3/developer-guide/how-to/sqs/QueueURL.html
+# https://{REGION_ENDPOINT}/queue.|api-domain|/{YOUR_ACCOUNT_NUMBER}/{YOUR_QUEUE_NAME}
+QUEUE_URL="https://sqs.$AWS_REGION.amazonaws.com/$AWS_ACCOUNT_NUMBER/$QUEUE_NAME"
+
+set +e
+log "Delete queue ${QUEUE_URL}"
+aws sqs delete-queue --queue-url ${QUEUE_URL}
+if [ $? -eq 0 ]
+then
+     # You must wait 60 seconds after deleting a queue before you can create another with the same name
+     log "Sleeping 60 seconds"
+     sleep 60
+fi
+set -e
+
+log "Create a FIFO queue $QUEUE_NAME"
+aws sqs create-queue --queue-name $QUEUE_NAME
+
+log "Sending messages to $QUEUE_URL"
+cd ../../connect/connect-aws-sqs-source
+aws sqs send-message-batch --queue-url $QUEUE_URL --entries file://send-message-batch.json
+cd -
+
+log "Creating SQS Source connector"
+playground connector create-or-update --connector sqs-source  << EOF
+{
+    "connector.class": "io.confluent.connect.sqs.source.SqsSourceConnector",
+    "tasks.max": "1",
+    "kafka.topic": "test-sqs-source",
+    "sqs.url": "$QUEUE_URL",
+    "sqs.credentials.provider.class": "com.github.vdesabou.AwsAssumeRoleCredentialsProvider",
+    "sqs.credentials.provider.sts.role.arn": "$AWS_STS_ROLE_ARN",
+    "sqs.credentials.provider.sts.role.session.name": "session-name",
+    "sqs.credentials.provider.sts.role.external.id": "123",
+    "sqs.credentials.provider.sts.aws.access.key.id": "$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_ACCESS_KEY_ID",
+    "sqs.credentials.provider.sts.aws.secret.key.id": "$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_SECRET_ACCESS_KEY",
+    "confluent.license": "",
+    "confluent.topic.bootstrap.servers": "broker:9092",
+    "confluent.topic.replication.factor": "1",
+    "errors.tolerance": "all",
+    "errors.log.enable": "true",
+    "errors.log.include.messages": "true"
+}
+EOF
+
+# FIXTHIS failing with
+# 08:54:15 🔥 Command failed with error code 400
+# 08:54:15 🔥 Connector configuration is invalid and contains the following 1 error(s):
+# Invalid value class com.github.vdesabou.AwsAssumeRoleCredentialsProvider for configuration sqs.credentials.provider.class: Class must extend: interface software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
+# You can also find the above list of errors at the endpoint `/connector-plugins/{connectorType}/config/validate`
+
+log "Verify we have received the data in test-sqs-source topic"
+playground topic consume --topic test-sqs-source --min-expected-messages 2 --timeout 60
+
+log "Delete queue ${QUEUE_URL}"
+aws sqs delete-queue --queue-url ${QUEUE_URL}
