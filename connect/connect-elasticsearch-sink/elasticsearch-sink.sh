b7764d23e0af49aa39b296282eaf4db328c76cd6
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-03-06 15:40:06 +0100

    simplified

--- connect/connect-elasticsearch-sink/elasticsearch-sink.sh
+++ connect/connect-elasticsearch-sink/elasticsearch-sink.sh
@@ -4,48 +4,8 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-# As of version 11.0.0, the connector uses the Elasticsearch High Level REST Client (version 7.0.1),
-# which means only Elasticsearch 7.x is supported.
-
-export ELASTIC_VERSION="6.8.3"
-if version_gt $CONNECTOR_TAG "10.9.9"
-then
-    log "Connector version is > 11.0.0, using Elasticsearch 7.x"
-    export ELASTIC_VERSION="7.12.0"
-fi
-
 PLAYGROUND_ENVIRONMENT=${PLAYGROUND_ENVIRONMENT:-"plaintext"}
 playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.yml"
-
-log "Creating Elasticsearch Sink connector (Elasticsearch version is $ELASTIC_VERSION)"
-if version_gt $CONNECTOR_TAG "10.9.9"
-then
-     # 7.x
-     curl -X PUT \
-          -H "Content-Type: application/json" \
-          --data '{
-               "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
-               "tasks.max": "1",
-               "topics": "test-elasticsearch-sink",
-               "key.ignore": "true",
-               "connection.url": "http://elasticsearch:9200"
-               }' \
-          http://localhost:8083/connectors/elasticsearch-sink/config | jq .
-else
-     # 6.x
-     curl -X PUT \
-          -H "Content-Type: application/json" \
-          --data '{
-               "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
-               "tasks.max": "1",
-               "topics": "test-elasticsearch-sink",
-               "key.ignore": "true",
-               "connection.url": "http://elasticsearch:9200",
-               "type.name": "kafka-connect"
-               }' \
-          http://localhost:8083/connectors/elasticsearch-sink/config | jq .
-fi
-
 
 log "Sending messages to topic test-elasticsearch-sink"
 playground topic produce -t test-elasticsearch-sink --nb-messages 10 --forced-value '{"f1":"value%g"}' << 'EOF'
@@ -61,6 +21,17 @@ playground topic produce -t test-elasticsearch-sink --nb-messages 10 --forced-va
 }
 EOF
 
+log "Creating Elasticsearch Sink connector"
+playground connector create-or-update --connector elasticsearch-sink  << EOF
+{
+     "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+     "tasks.max": "1",
+     "topics": "test-elasticsearch-sink",
+     "key.ignore": "true",
+     "connection.url": "http://elasticsearch:9200"
+}
+EOF
+
 sleep 10
 
 log "Check that the data is available in Elasticsearch"
@@ -68,5 +39,3 @@ curl -XGET 'http://localhost:9200/test-elasticsearch-sink/_search?pretty' > /tmp
 cat /tmp/result.log
 grep "f1" /tmp/result.log | grep "value1"
 grep "f1" /tmp/result.log | grep "value10"
-
-
