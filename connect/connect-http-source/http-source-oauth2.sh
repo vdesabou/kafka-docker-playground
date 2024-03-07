ce2f7e4c7e5576e0ca037c25cc48efe59cea11e0
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-03-07 14:53:00 +0100

    fix

--- connect/connect-http-source/http-source-oauth2.sh
+++ connect/connect-http-source/http-source-oauth2.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 set -e
 
+function get_3rdparty_file () {
+  file="$1"
+
+  if [ -f $file ]
+  then
+    echo  "$file already present, skipping"
+    return
+  fi
+
+  set +e
+  aws s3 ls s3://kafka-docker-playground/3rdparty/$file > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+      echo "Downloading <s3://kafka-docker-playground/3rdparty/$file> from S3 bucket"
+      aws s3 cp --only-show-errors "s3://kafka-docker-playground/3rdparty/$file" .
+      if [ $? -eq 0 ]; then
+        echo "📄 <s3://kafka-docker-playground/3rdparty/$file> was downloaded from S3 bucket"
+      fi
+      if [[ "$OSTYPE" == "darwin"* ]]
+      then
+          # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
+          chmod a+rw $file
+      else
+          # on CI, docker is run as runneradmin user, need to use sudo
+          sudo chmod a+rw $file
+      fi
+  fi
+  set -e
+}
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+cd ../../connect/connect-http-source
 HTTP_SOURCE_CONNECTOR_ZIP="confluentinc-kafka-connect-http-source-0.2.0-rc-f1cd5ff.zip"
 export CONNECTOR_ZIP="$PWD/$HTTP_SOURCE_CONNECTOR_ZIP"
-
-source ${DIR}/../../scripts/utils.sh
 
 get_3rdparty_file "$HTTP_SOURCE_CONNECTOR_ZIP"
 
@@ -15,6 +44,9 @@ then
      logerror "ERROR: ${PWD}/$HTTP_SOURCE_CONNECTOR_ZIP is missing. You must be a Confluent Employee to run this example !"
      exit 1
 fi
+cd -
+
+source ${DIR}/../../scripts/utils.sh
 
 PLAYGROUND_ENVIRONMENT=${PLAYGROUND_ENVIRONMENT:-"plaintext"}
 playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.oauth2.yml"
