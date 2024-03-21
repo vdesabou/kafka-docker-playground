3e22e5bb13dd9a036e14b059b6d26200084ab6d5
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-03-21 22:38:38 +0100

    fix

--- ccloud/fully-managed-connect-http-sink/stop.sh
+++ ccloud/fully-managed-connect-http-sink/stop.sh
@@ -5,6 +5,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-docker compose down -f docker-compose.oauth2.yml -v --remove-orphans
+docker compose -f docker-compose.oauth2.yml down -v --remove-orphans
 
 maybe_delete_ccloud_environment
