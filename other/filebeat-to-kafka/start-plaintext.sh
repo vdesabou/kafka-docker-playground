acae857d0c3d4f1e0a82f86132c17d0b7505737d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-03-26 16:12:14 +0100

    fix

--- other/filebeat-to-kafka/start-plaintext.sh
+++ other/filebeat-to-kafka/start-plaintext.sh
@@ -4,8 +4,11 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+# make sure control-center is not disabled
+export ENABLE_CONTROL_CENTER=true
+
 PLAYGROUND_ENVIRONMENT=${PLAYGROUND_ENVIRONMENT:-"plaintext"}
-playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.yml" -a
+playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.yml" --wait-for-control-center
 
 log "Sleep 90 seconds"
 sleep 90
