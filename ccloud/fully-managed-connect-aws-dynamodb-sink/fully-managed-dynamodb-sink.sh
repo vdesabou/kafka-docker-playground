ae41f804cccba39ffaee818fccf1faac4e248fc0
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-03 21:19:48 +0200

    added ccloud/fully-managed-connect-aws-dynamodb-sink

---
+++ ccloud/fully-managed-connect-aws-dynamodb-sink/fully-managed-dynamodb-sink.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if ! version_gt $TAG_BASE "5.9.99" && version_gt $CONNECTOR_TAG "1.2.99"
+then
+    logwarn "WARN: connector version >= 1.3.0 do not support CP versions < 6.0.0"
+    exit 111
+fi
+
+if [ ! -f $HOME/.aws/credentials ] && ( [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] )
+then
+     logerror "ERROR: either the file $HOME/.aws/credentials is not present or environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are not set!"
+     exit 1
+else
+    if [ ! -z "$AWS_ACCESS_KEY_ID" ] && [ ! -z "$AWS_SECRET_ACCESS_KEY" ]
+    then
+        log "💭 Using environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY"
+        export AWS_ACCESS_KEY_ID
+        export AWS_SECRET_ACCESS_KEY
+    else
+        if [ -f $HOME/.aws/credentials ]
+        then
+            logwarn "💭 AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set based on $HOME/.aws/credentials"
+            export AWS_ACCESS_KEY_ID=$( grep "^aws_access_key_id" $HOME/.aws/credentials | head -1 | awk -F'=' '{print $2;}' )
+            export AWS_SECRET_ACCESS_KEY=$( grep "^aws_secret_access_key" $HOME/.aws/credentials | head -1 | awk -F'=' '{print $2;}' ) 
+        fi
+    fi
+    if [ -z "$AWS_REGION" ]
+    then
+        AWS_REGION=$(aws configure get region | tr '\r' '\n')
+        if [ "$AWS_REGION" == "" ]
+        then
+            logerror "ERROR: either the file $HOME/.aws/config is not present or environment variables AWS_REGION is not set!"
+            exit 1
+        fi
+    fi
+fi
+
+set +e
+log "Delete table, this might fail"
+aws dynamodb delete-table --table-name mytable --region $AWS_REGION
+set -e
+
+bootstrap_ccloud_environment
+
+set +e
+playground topic delete --topic mytable
+sleep 3
+playground topic create --topic mytable --nb-partitions 1
+set -e
+
+log "Sending messages to topic mytable"
+playground topic produce -t mytable --nb-messages 10 --forced-value '{"f1":"value%g"}' << 'EOF'
+{
+  "type": "record",
+  "name": "myrecord",
+  "fields": [
+    {
+      "name": "f1",
+      "type": "string"
+    }
+  ]
+}
+EOF
+
+connector_name="DynamoDbSink_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+log "Creating AWS CloudWatch Logs Source connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+    "connector.class": "DynamoDbSink",
+    "name": "$connector_name",
+    "kafka.auth.mode": "KAFKA_API_KEY",
+    "kafka.api.key": "$CLOUD_KEY",
+    "kafka.api.secret": "$CLOUD_SECRET",
+    "aws.access.key.id" : "$AWS_ACCESS_KEY_ID",
+    "aws.secret.access.key": "$AWS_SECRET_ACCESS_KEY",
+    "input.data.format": "AVRO",
+    "topics": "mytable",
+    "aws.dynamodb.endpoint": "https://dynamodb.$AWS_REGION.amazonaws.com",
+    "tasks.max" : "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 600
+
+log "Sleeping 120 seconds, waiting for table to be created"
+sleep 120
+
+log "Verify data is in DynamoDB"
+aws dynamodb scan --table-name mytable --region $AWS_REGION  > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "value1" /tmp/result.log
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
