512b55877ccd4a365726f075fbf6f86a7e625a71
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-03 21:33:47 +0200

    ccloud/fully-managed-connect-aws-kinesis-source

---
+++ ccloud/fully-managed-connect-aws-kinesis-source/README.md
@@ -0,0 +1,57 @@
+# Fully Managed AWS Kinesis Source connector
+
+
+
+## Objective
+
+Quickly test [Fully Managed Kinesis Connector](https://docs.confluent.io/cloud/current/connectors/cc-kinesis-source.html) connector.
+
+## AWS Setup
+
+* Make sure you have an [AWS account](https://docs.aws.amazon.com/streams/latest/dev/before-you-begin.html#setting-up-sign-up-for-aws).
+* Set up [AWS Credentials](https://docs.confluent.io/current/connect/kafka-connect-kinesis/index.html#aws-credentials)
+
+You can either export environment variables `AWS_REGION`, `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` or set files `~/.aws/credentials` and `~/.aws/config`.
+
+Example:
+
+```
+[default]
+aws_access_key_id=xxx
+aws_secret_access_key=xxx
+region=eu-west-3
+```
+
+Make sure that region corresponds to the one used by the test (eu-west-3 by default), otherwise the connector will fail to start with `Stream does not exist`.
+
+## Prerequisites
+
+All you have to do is to be already logged in with [confluent CLI](https://docs.confluent.io/confluent-cli/current/overview.html#confluent-cli-overview).
+
+By default, a new Confluent Cloud environment with a Cluster will be created.
+
+You can configure the cluster by setting environment variables:
+
+* `CLUSTER_CLOUD`: The Cloud provider (possible values: `aws`, `gcp` and `azure`, default `aws`)
+* `CLUSTER_REGION`: The Cloud region (use `confluent kafka region list` to get the list, default `eu-west-2` for aws, `westeurope`for azure and `europe-west2` for gcp)
+* `CLUSTER_TYPE`: The type of cluster (possible values: `basic`, `standard` and `dedicated`, default `basic`)
+* `ENVIRONMENT` (optional): The environment id where want your new cluster (example: `txxxxx`) 
+
+In case you want to use your own existing cluster, you need to setup these environment variables:
+
+* `ENVIRONMENT`: The environment id where your cluster is located (example: `txxxxx`) 
+* `CLUSTER_NAME`: The cluster name
+* `CLUSTER_CLOUD`: The Cloud provider (possible values: `aws`, `gcp` and `azure`)
+* `CLUSTER_REGION`: The Cloud region (example `us-east-2`)
+* `CLUSTER_CREDS`: The Kafka api key and secret to use, it should be separated with colon (example: `<API_KEY>:<API_KEY_SECRET>`)
+* `SCHEMA_REGISTRY_CREDS` (optional, if not set, new one will be created): The Schema Registry api key and secret to use, it should be separated with colon (example: `<SR_API_KEY>:<SR_API_KEY_SECRET>`)
+
+
+## How to run
+
+Simply run:
+
+```
+$ just use <playground run> 
+```
+
---
+++ ccloud/fully-managed-connect-aws-kinesis-source/fully-managed-kinesis-source.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if [ ! -f $HOME/.aws/credentials ] && ( [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] )
+then
+     logerror "ERROR: either the file $HOME/.aws/credentials is not present or environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are not set!"
+     exit 1
+else
+    if [ ! -z "$AWS_ACCESS_KEY_ID" ] && [ ! -z "$AWS_SECRET_ACCESS_KEY" ]
+    then
+        log "💭 Using environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY"
+        export AWS_ACCESS_KEY_ID
+        export AWS_SECRET_ACCESS_KEY
+    else
+        if [ -f $HOME/.aws/credentials ]
+        then
+            logwarn "💭 AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set based on $HOME/.aws/credentials"
+            export AWS_ACCESS_KEY_ID=$( grep "^aws_access_key_id" $HOME/.aws/credentials | head -1 | awk -F'=' '{print $2;}' )
+            export AWS_SECRET_ACCESS_KEY=$( grep "^aws_secret_access_key" $HOME/.aws/credentials | head -1 | awk -F'=' '{print $2;}' ) 
+        fi
+    fi
+    if [ -z "$AWS_REGION" ]
+    then
+        AWS_REGION=$(aws configure get region | tr '\r' '\n')
+        if [ "$AWS_REGION" == "" ]
+        then
+            logerror "ERROR: either the file $HOME/.aws/config is not present or environment variables AWS_REGION is not set!"
+            exit 1
+        fi
+    fi
+fi
+
+bootstrap_ccloud_environment
+
+set +e
+playground topic delete --topic kinesis_topic
+sleep 3
+playground topic create --topic kinesis_topic --nb-partitions 1
+set -e
+
+KINESIS_STREAM_NAME=pg${USER}${TAG}
+KINESIS_STREAM_NAME=${KINESIS_STREAM_NAME//[-.]/}
+
+set +e
+log "Delete the stream"
+aws kinesis delete-stream --stream-name $KINESIS_STREAM_NAME --region $AWS_REGION
+set -e
+
+sleep 5
+
+log "Create a Kinesis stream $KINESIS_STREAM_NAME"
+aws kinesis create-stream --stream-name $KINESIS_STREAM_NAME --shard-count 1 --region $AWS_REGION
+
+log "Sleep 60 seconds to let the Kinesis stream being fully started"
+sleep 60
+
+log "Insert records in Kinesis stream".
+aws kinesis put-record --stream-name $KINESIS_STREAM_NAME --partition-key 123 --data test-message-1 --region $AWS_REGION
+
+
+connector_name="KinesisSource_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+    "connector.class": "KinesisSource",
+    "name": "$connector_name",
+    "kafka.auth.mode": "KAFKA_API_KEY",
+    "kafka.api.key": "$CLOUD_KEY",
+    "kafka.api.secret": "$CLOUD_SECRET",
+    "kafka.topic": "kinesis_topic",
+    "kinesis.stream": "$KINESIS_STREAM_NAME",
+    "kinesis.region": "$AWS_REGION",
+    "aws.access.key.id" : "$AWS_ACCESS_KEY_ID",
+    "aws.secret.key.id": "$AWS_SECRET_ACCESS_KEY",
+    "tasks.max" : "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 600
+
+sleep 10
+
+log "Verify we have received the data in kinesis_topic topic"
+playground topic consume --topic kinesis_topic --min-expected-messages 1 --timeout 60
+
+log "Delete the stream"
+check_if_continue
+aws kinesis delete-stream --stream-name $KINESIS_STREAM_NAME --region $AWS_REGION
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
