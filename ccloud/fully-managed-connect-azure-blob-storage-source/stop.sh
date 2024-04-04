c8c665d30df126304f4f7a2883178591d9f3e403
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-04 15:11:32 +0200

    ccloud/fully-managed-connect-azure-blob-storage-source

---
+++ ccloud/fully-managed-connect-azure-blob-storage-source/stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+AZURE_NAME=pg${USER}bs${GITHUB_RUN_NUMBER}${TAG}
+AZURE_NAME=${AZURE_NAME//[-._]/}
+AZURE_RESOURCE_GROUP=$AZURE_NAME
+
+
+log "Deleting resource group"
+az group delete --name $AZURE_RESOURCE_GROUP --yes --no-wait
+
+maybe_delete_ccloud_environment
