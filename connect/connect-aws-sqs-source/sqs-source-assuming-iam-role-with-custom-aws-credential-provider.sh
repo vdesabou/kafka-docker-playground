66494871857a87f390f833ba8626d58f0f5a60de
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-04 08:41:00 +0200

    check_if_continue

--- connect/connect-aws-sqs-source/sqs-source-assuming-iam-role-with-custom-aws-credential-provider.sh
+++ connect/connect-aws-sqs-source/sqs-source-assuming-iam-role-with-custom-aws-credential-provider.sh
@@ -143,4 +143,5 @@ log "Verify we have received the data in test-sqs-source topic"
 playground topic consume --topic test-sqs-source --min-expected-messages 2 --timeout 60
 
 log "Delete queue ${QUEUE_URL}"
+check_if_continue
 aws sqs delete-queue --queue-url ${QUEUE_URL}
