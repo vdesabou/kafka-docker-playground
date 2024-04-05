ce1118b2fff130c98ff5e642b0a597d641d91bce
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-05 17:32:41 +0200

    ccloud/fully-managed-connect-azure-cosmosdb-source

---
+++ ccloud/fully-managed-connect-azure-cosmosdb-source/stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+
+maybe_delete_ccloud_environment
+
+AZURE_NAME=pg${USER}ck${GITHUB_RUN_NUMBER}${TAG}
+AZURE_NAME=${AZURE_NAME//[-._]/}
+AZURE_RESOURCE_GROUP=$AZURE_NAME
+
+log "Deleting resource group"
+az group delete --name $AZURE_RESOURCE_GROUP --yes --no-wait
