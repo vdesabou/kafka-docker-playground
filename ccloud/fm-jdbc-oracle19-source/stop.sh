12a059588cfff287b0489c75e11085bd2c844784
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-10 21:38:31 +0200

    export ORACLE_IMAGE="oracle/database:19.3.0-ee"

--- ccloud/fm-jdbc-oracle19-source/stop.sh
+++ ccloud/fm-jdbc-oracle19-source/stop.sh
@@ -5,6 +5,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+export ORACLE_IMAGE="oracle/database:19.3.0-ee"
+
 docker compose down -v --remove-orphans
 
 
