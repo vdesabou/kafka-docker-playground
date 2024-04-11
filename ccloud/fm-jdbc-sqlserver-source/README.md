7aa19ddb49f13dd8e917b51731fe60b463b1f54d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-11 16:54:59 +0200

    fm-jdbc-sqlserver-source

---
+++ ccloud/fm-jdbc-sqlserver-source/README.md
@@ -0,0 +1,11 @@
+# Fully Managed JDBC SQL Server source connector
+
+
+
+## Objective
+
+Quickly test [Fully Managed JDBC SQL Server](https://docs.confluent.io/cloud/current/connectors/cc-microsoft-sql-server-source.html) connector.
+
+## Prerequisites
+
+See [here](https://kafka-docker-playground.io/#/how-to-use?id=%f0%9f%8c%a4%ef%b8%8f-confluent-cloud-examples)
---
+++ ccloud/fm-jdbc-sqlserver-source/fully-managed-sqlserver-source.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+NGROK_AUTH_TOKEN=${NGROK_AUTH_TOKEN:-$1}
+
+display_ngrok_warning
+
+bootstrap_ccloud_environment
+
+
+
+set +e
+playground topic delete --topic sqlserver_table
+set -e
+
+playground topic create --topic sqlserver_table
+
+docker compose build
+docker compose down -v --remove-orphans
+docker compose up -d --quiet-pull
+
+sleep 5
+
+log "Create table"
+docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! << EOF
+-- Create the test database
+CREATE DATABASE testDB;
+GO
+USE testDB;
+EXEC sys.sp_cdc_enable_db;
+
+-- Create some customers ...
+CREATE TABLE customers (
+  id INTEGER IDENTITY(1001,1) NOT NULL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
+);
+INSERT INTO customers(first_name,last_name,email)
+  VALUES ('Sally','Thomas','sally.thomas@acme.com');
+INSERT INTO customers(first_name,last_name,email)
+  VALUES ('George','Bailey','gbailey@foobar.com');
+INSERT INTO customers(first_name,last_name,email)
+  VALUES ('Edward','Walker','ed@walker.com');
+INSERT INTO customers(first_name,last_name,email)
+  VALUES ('Anne','Kretchmar','annek@noanswer.org');
+GO
+EOF
+
+log "Waiting for ngrok to start"
+while true
+do
+  container_id=$(docker ps -q -f name=ngrok)
+  if [ -n "$container_id" ]
+  then
+    status=$(docker inspect --format '{{.State.Status}}' $container_id)
+    if [ "$status" = "running" ]; then
+      break
+    fi
+  fi
+  log "Waiting for container ngrok to start..."
+  sleep 5
+done
+log "Getting ngrok hostname and port"
+NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+NGROK_HOSTNAME=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 1)
+NGROK_PORT=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 2)
+
+connector_name="MicrosoftSqlServerSource_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+     "connector.class": "MicrosoftSqlServerSource",
+     "name": "$connector_name",
+     "kafka.auth.mode": "KAFKA_API_KEY",
+     "kafka.api.key": "$CLOUD_KEY",
+     "kafka.api.secret": "$CLOUD_SECRET",
+     "output.data.format": "AVRO",
+     "connection.host": "$NGROK_HOSTNAME",
+     "connection.port": "$NGROK_PORT",
+     "connection.user": "sa",
+     "connection.password": "Password!",
+     "db.name": "testDB",
+     "table.whitelist": "customers",
+     "mode": "incrementing",
+     "incrementing.column.name": "id",
+     "topic.prefix": "sqlserver-",
+     "tasks.max": "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 600
+
+docker exec -i sqlserver /opt/mssql-tools/bin/sqlcmd -U sa -P Password! << EOF
+USE testDB;
+INSERT INTO customers(first_name,last_name,email) VALUES ('Pam','Thomas','pam@office.com');
+GO
+EOF
+
+log "Verifying topic sqlserver-customers"
+playground topic consume --topic sqlserver-customers --min-expected-messages 5 --timeout 60
+
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
