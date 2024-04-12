f008fd4cb25f5a8be001a71748aca923b4967b55
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-12 14:39:36 +0200

    ccloud/fm-salesforce-bulkapi-source

---
+++ ccloud/fm-salesforce-bulkapi-source/fully-managed-salesforce-bulkapi-source.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+SALESFORCE_USERNAME=${SALESFORCE_USERNAME:-$1}
+SALESFORCE_PASSWORD=${SALESFORCE_PASSWORD:-$2}
+SALESFORCE_CONSUMER_KEY=${SALESFORCE_CONSUMER_KEY:-$3}
+SALESFORCE_CONSUMER_PASSWORD=${SALESFORCE_CONSUMER_PASSWORD:-$4}
+SALESFORCE_SECURITY_TOKEN=${SALESFORCE_SECURITY_TOKEN:-$5}
+SALESFORCE_INSTANCE=${SALESFORCE_INSTANCE:-"https://login.salesforce.com"}
+
+if [ -z "$SALESFORCE_USERNAME" ]
+then
+     logerror "SALESFORCE_USERNAME is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_PASSWORD" ]
+then
+     logerror "SALESFORCE_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ -z "$SALESFORCE_SECURITY_TOKEN" ]
+then
+     logerror "SALESFORCE_SECURITY_TOKEN is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+bootstrap_ccloud_environment
+
+
+
+set +e
+playground topic delete --topic sfdc-bulkapi-leads
+sleep 3
+playground topic create --topic sfdc-bulkapi-leads
+set -e
+
+docker compose build
+docker compose down -v --remove-orphans
+docker compose up -d --quiet-pull
+
+connector_name="SalesforceBulkApiSource_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+TODAY=$(date -u '+%Y-%m-%d')
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+     "connector.class": "SalesforceBulkApiSource",
+     "name": "$connector_name",
+     "kafka.auth.mode": "KAFKA_API_KEY",
+     "kafka.api.key": "$CLOUD_KEY",
+     "kafka.api.secret": "$CLOUD_SECRET",
+     "kafka.topic": "sfdc-bulkapi-leads",
+     "salesforce.instance" : "$SALESFORCE_INSTANCE",
+     "salesforce.username": "$SALESFORCE_USERNAME",
+     "salesforce.password": "$SALESFORCE_PASSWORD",
+     "salesforce.password.token": "$SALESFORCE_SECURITY_TOKEN",
+     "salesforce.consumer.key": "$SALESFORCE_CONSUMER_KEY",
+     "salesforce.consumer.secret": "$SALESFORCE_CONSUMER_PASSWORD",
+     "salesforce.since": "$TODAY",
+     "salesforce.object" : "Lead",
+     "output.data.format": "JSON",
+     "tasks.max": "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 600
+
+log "Login with sfdx CLI"
+docker exec sfdx-cli sh -c "sfdx sfpowerkit:auth:login -u \"$SALESFORCE_USERNAME\" -p \"$SALESFORCE_PASSWORD\" -r \"$SALESFORCE_INSTANCE\" -s \"$SALESFORCE_SECURITY_TOKEN\""
+
+LEAD_FIRSTNAME=John_$RANDOM
+LEAD_LASTNAME=Doe_$RANDOM
+log "Add a Lead to Salesforce: $LEAD_FIRSTNAME $LEAD_LASTNAME"
+docker exec sfdx-cli sh -c "sfdx data:create:record  --target-org \"$SALESFORCE_USERNAME\" -s Lead -v \"FirstName='$LEAD_FIRSTNAME' LastName='$LEAD_LASTNAME' Company=Confluent\""
+
+sleep 10
+
+log "Verify we have received the data in sfdc-bulkapi-leads topic"
+playground topic consume --topic sfdc-bulkapi-leads --min-expected-messages 1 --timeout 60
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
