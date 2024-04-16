a43eeb7dc3ce74ca442d58d76959edef523845e2
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-16 17:58:35 +0200

    ccloud/fm-rabbitmq-sink

---
+++ ccloud/fm-rabbitmq-sink/README.md
@@ -0,0 +1,20 @@
+# Fully Managed RabbitMQ Sink connector
+
+
+
+## Objective
+
+Quickly test [Fully Managed RabbitMQ Sink](https://docs.confluent.io/cloud/current/connectors/cc-rabbitmq-sink.html) connector.
+
+
+## Prerequisites
+
+See [here](https://kafka-docker-playground.io/#/how-to-use?id=%f0%9f%8c%a4%ef%b8%8f-confluent-cloud-examples)
+
+## How to run
+
+Simply run:
+
+```
+$ just use <playground run> command and search for rabbitmq.sh in this folder
+```
---
+++ ccloud/fm-rabbitmq-sink/fully-managed-rabbitmq-sink.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+NGROK_AUTH_TOKEN=${NGROK_AUTH_TOKEN:-$1}
+
+display_ngrok_warning
+
+bootstrap_ccloud_environment
+
+
+set +e
+playground topic delete --topic rabbitmq-messages
+sleep 3
+playground topic create --topic rabbitmq-messages
+set -e
+
+docker compose build
+docker compose down -v --remove-orphans
+docker compose up -d --quiet-pull
+
+sleep 5
+
+log "Waiting for ngrok to start"
+while true
+do
+  container_id=$(docker ps -q -f name=ngrok)
+  if [ -n "$container_id" ]
+  then
+    status=$(docker inspect --format '{{.State.Status}}' $container_id)
+    if [ "$status" = "running" ]; then
+      break
+    fi
+  fi
+  log "Waiting for container ngrok to start..."
+  sleep 5
+done
+log "Getting ngrok hostname and port"
+NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+NGROK_HOSTNAME=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 1)
+NGROK_PORT=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 2)
+
+sleep 5
+
+log "Create RabbitMQ exchange, queue and binding"
+docker exec -i rabbitmq rabbitmqadmin -u myuser -p mypassword -V / declare exchange name=exchange1 type=direct
+docker exec -i rabbitmq rabbitmqadmin -u myuser -p mypassword -V / declare queue name=queue1 durable=true
+docker exec -i rabbitmq rabbitmqadmin -u myuser -p mypassword -V / declare binding source=exchange1 destination=queue1 routing_key=rkey1
+
+
+log "Sending messages to topic rabbitmq-messages"
+playground topic produce -t rabbitmq-messages --nb-messages 10 << 'EOF'
+%g
+EOF
+
+connector_name="RabbitMQSink_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+     "connector.class": "RabbitMQSink",
+     "name": "$connector_name",
+     "kafka.auth.mode": "KAFKA_API_KEY",
+     "kafka.api.key": "$CLOUD_KEY",
+     "kafka.api.secret": "$CLOUD_SECRET",
+     "rabbitmq.host" : "$NGROK_HOSTNAME",
+     "rabbitmq.port" : "$NGROK_PORT",
+     "rabbitmq.username": "myuser",
+     "rabbitmq.password" : "mypassword",
+     "rabbitmq.queue": "myqueue",
+     "rabbitmq.exchange": "exchange1",
+     "rabbitmq.routing.key": "rkey1",
+     "rabbitmq.delivery.mode": "persistent",
+     "topics" : "rabbitmq-messages",
+     "tasks.max" : "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 600
+
+sleep 5
+
+log "Check messages received in RabbitMQ"
+docker exec -i rabbitmq rabbitmqadmin -u myuser -p mypassword get queue=queue1 count=10 > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "rkey1" /tmp/result.log
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
