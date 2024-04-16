4e22481bfeef1bf5934da01f4b6259588bc5f25f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-16 18:32:30 +0200

    fix

--- connect/connect-jdbc-oracle19-sink/stop.sh
+++ connect/connect-jdbc-oracle19-sink/stop.sh
@@ -5,11 +5,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-export ORACLE_IMAGE="oracle/database:12.2.0.1-ee"
-if [ ! -z "$GITHUB_RUN_NUMBER" ]
-then
-     # if this is github actions, use private image.
-     export ORACLE_IMAGE="vdesabou/oracle19"
-fi
+export ORACLE_IMAGE="oracle/database:19.3.0-ee"
 
 stop_all "$DIR"
