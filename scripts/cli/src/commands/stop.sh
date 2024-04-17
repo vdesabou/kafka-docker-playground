42f3f365812f5be330a60f7e77eb518bd3c6777d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-17 19:06:52 +0200

    unknown flag: --quiet-pull #5566

--- scripts/cli/src/commands/stop.sh
+++ scripts/cli/src/commands/stop.sh
@@ -8,11 +8,17 @@ fi
 filename=$(basename -- "$test_file")
 test_file_directory="$(dirname "${test_file}")"
 
+tmp_dir=$(mktemp -d -t pg-XXXXXXXXXX)
+trap 'rm -rf $tmp_dir' EXIT
+
 log "🛑 Stopping example $filename in dir $test_file_directory"
 docker_command=$(playground state get run.docker_command)
-echo "$docker_command" > /tmp/tmp
+echo "$docker_command" > $tmp_dir/tmp
 
 sed -e "s|up -d|down -v --remove-orphans|g" \
-    /tmp/tmp > /tmp/playground-command-stop
+    $tmp_dir/tmp > $tmp_dir/tmp2
 
-bash /tmp/playground-command-stop
+sed -e "s|--quiet-pull||g" \
+    $tmp_dir/tmp2 > $tmp_dir/playground-command-stop
+
+bash $tmp_dir/playground-command-stop
