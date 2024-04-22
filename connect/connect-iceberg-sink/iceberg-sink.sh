f6852ed4a028a6c6a2b859ace868fb0b527b87ac
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-04-22 15:43:34 +0200

    added connect/connect-iceberg-sink

---
+++ connect/connect-iceberg-sink/iceberg-sink.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+PLAYGROUND_ENVIRONMENT=${PLAYGROUND_ENVIRONMENT:-"plaintext"}
+playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.yml"
+
+
+log "Sending messages to topic payments"
+playground topic produce -t payments --nb-messages $(wc -l <"../../connect/connect-iceberg-sink/data/transactions.json") --value ../../connect/connect-iceberg-sink/data/transactions.json
+
+log "Creating Iceberg sink connector"
+playground connector create-or-update --connector iceberg-sink  << EOF
+{
+  "tasks.max": "1",
+  "topics": "payments",
+  "connector.class": "io.tabular.iceberg.connect.IcebergSinkConnector",
+  "iceberg.catalog.s3.endpoint": "http://minio:9000",
+  "iceberg.catalog.s3.secret-access-key": "minioadmin",
+  "iceberg.catalog.s3.access-key-id": "minioadmin",
+  "iceberg.catalog.s3.path-style-access": "true",
+  "iceberg.catalog.uri": "http://rest:8181",
+  "iceberg.catalog.warehouse": "s3://warehouse/",
+  "iceberg.catalog.client.region": "eu-west-1",
+  "iceberg.catalog.type": "rest",
+  "iceberg.control.commitIntervalMs": "1000",
+  "iceberg.tables": "orders.payments",
+  "value.converter.schemas.enable": "false",
+  "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+  "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+  "schemas.enable": "false"
+}
+EOF
+
+sleep 10
+
+playground connector show-lag
+
+log "You can open the jupyter lab at http://localhost:8888/lab/tree/notebooks and use the sample notebook in notebooks/iceberg.ipynb to query the table"
+
+log "Verify data is in Iceberg"
+docker exec -i spark-iceberg spark-sql << EOF
+SELECT *
+FROM orders.payments
+LIMIT 10;
+EOF
