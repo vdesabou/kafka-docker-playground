5fef8b205194cc72a839cc7d253233a0b32fc260
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-05-28 06:39:43 +0200

    wip

---
+++ scripts/cli/src/commands/tcp-proxy/break.sh
@@ -0,0 +1,30 @@
+break_service_response="${args[--break-service-response]}"
+connection_id="${args[--connection-id]}"
+
+if [[ ! -n "$connection_id" ]]
+then
+    connection_id=$(playground get-zazkia-connection-list)
+    if [ "$connection_id" == "" ]
+    then
+        logerror "❌ No active Zazkia TCP connection found, make sure that Zazkia is being used!"
+        exit 1
+    fi
+fi
+
+items=($connection_id)
+length=${#items[@]}
+if ((length > 1))
+then
+    log "🧲 --connection-id flag was not provided, applying command to all active Zazkia TCP connections"
+fi
+for id in "${items[@]}"
+do
+    log "💔 Break sending the response to the client for $break_service_response % for connection id $id"
+    handle_onprem_connect_rest_api "curl -s -X POST -H \"Content-Type: application/json\" \"http://localhost:9191/links/$id/break-response?pct=$break_service_response\""
+
+    echo "$curl_output"
+
+    sleep 1
+
+    playground tcp-proxy get-connections --connection-id $connection_id
+done
