7f3142d9e1962a6feafb054b6e427cea272d74ca
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2024-12-20 14:58:15 +0100

    wip

--- scripts/cli/src/commands/update-docs.sh
+++ scripts/cli/src/commands/update-docs.sh
@@ -2,5 +2,5 @@ cd ${root_folder}/scripts/cli
 
 docker run --rm -i --user $(id -u):$(id -g) --volume "$PWD:/app" dannyben/bashly render templates/markdown docs
 
-cat ${root_folder}scripts/cli/docs/cli-template.md > ${root_folder}/docs/cli.md
+cat ${root_folder}scripts/cli/docs-template/cli-template.md > ${root_folder}/docs/cli.md
 cat ${root_folder}/scripts/cli/docs/index.md >> ${root_folder}/docs/cli.md
