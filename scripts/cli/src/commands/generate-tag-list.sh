c0830c18db9ba9981e923a53d90a507d64d29fde
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-01-16 15:12:11 +0100

    fix tag list

--- scripts/cli/src/commands/generate-tag-list.sh
+++ scripts/cli/src/commands/generate-tag-list.sh
@@ -4,16 +4,14 @@ function listAllTags() {
     [ -z "${repo}" ] && echo "Usage: listTags <repoName> [page_size]" 1>&2 && return 1 
     local base_url="https://registry.hub.docker.com/v2/repositories/${repo}/tags"
     local page=1 
-    local res=$(curl "${base_url}?page_size=${page_size}&page=${page}" 2>/dev/null) 
-    local tags=$(echo ${res} | jq --raw-output '.results[].name') 
-    local all_tags="${tags}" 
-    local tag_count=$(echo ${res} | jq '.count')   
-    ((page_count=(${tag_count}+${page_size}-1)/${page_size}))  # ceil(tag_count / page_size) 
-    for page in $(seq 2 $page_count); do 
-        tags=$(curl "${base_url}?page_size=${page_size}&page=${page}" 2>/dev/null | jq --raw-output '.results[].name') 
-        all_tags="${all_tags}${tags}" 
+    local res=$(curl "${base_url}?page_size=${page_size}&page=${page}" 2>/dev/null)
+    echo ${res} | jq --raw-output '.results[].name' > /tmp/all_tags
+    local tag_count=$(echo ${res} | jq '.count')
+    ((page_count=(${tag_count}+${page_size}-1)/${page_size}))  # ceil(tag_count / page_size)
+    for page in $(seq 2 $page_count); do
+        curl "${base_url}?page_size=${page_size}&page=${page}" 2>/dev/null | jq --raw-output '.results[].name' >> /tmp/all_tags
     done
-    echo "${all_tags}" | sort 
+    cat /tmp/all_tags | sort
 } 
 
 listAllTags "confluentinc/cp-server-connect-base" | grep -v "ubi8" | grep -v "arm64" | grep -v "amd64" | grep -v "latest" | grep -v "deb8" > $root_folder/scripts/cli/tag-list.txt
