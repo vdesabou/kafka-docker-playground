361be79b9a205d525199dc188c52bb6b7d63e5f3
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-02-11 10:15:25 +0100

    typo

--- connect/connect-jdbc-mysql-sink/mysql-sink.sh
+++ connect/connect-jdbc-mysql-sink/mysql-sink.sh
@@ -22,8 +22,7 @@ playground connector create-or-update --connector mysql-sink  << EOF
      "tasks.max": "1",
      "connection.url": "jdbc:mysql://mysql:3306/db?user=user&password=password&useSSL=false",
      "topics": "orders",
-     "auto.create": "true",
-     "consumer.override.enable.auto.commit": "false"
+     "auto.create": "true"
 }
 EOF
 
