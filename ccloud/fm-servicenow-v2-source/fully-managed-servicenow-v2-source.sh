cb5ec917298eaf58a825e38d41ca2142a1a3c7aa
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-02-18 15:32:21 +0100

    👾 Add Fully Managed ServiceNow Source V2 Connector example #6304

---
+++ ccloud/fm-servicenow-v2-source/fully-managed-servicenow-v2-source.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+
+
+function wait_for_end_of_hibernation () {
+     MAX_WAIT=600
+     CUR_WAIT=0
+     set +e
+     log "⌛ Waiting up to $MAX_WAIT seconds for end of hibernation to happen (it can take several minutes)"
+     curl -X POST "${SERVICENOW_URL}/api/now/table/incident" --user admin:"$SERVICENOW_PASSWORD" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d '{"short_description": "This is test"}' > /tmp/out.txt 2>&1
+     while [[ $(cat /tmp/out.txt) =~ "Sign in to the site to wake your instance" ]]
+     do
+          sleep 10
+          curl -X POST "${SERVICENOW_URL}/api/now/table/incident" --user admin:"$SERVICENOW_PASSWORD" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d '{"short_description": "This is test"}' > /tmp/out.txt 2>&1
+          CUR_WAIT=$(( CUR_WAIT+10 ))
+          if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
+               echo -e "\nERROR: The logs still show 'Sign in to the site to wake your instance' after $MAX_WAIT seconds.\n"
+               exit 1
+          fi
+     done
+     log "The instance is ready !"
+     set -e
+}
+
+SERVICENOW_URL=${SERVICENOW_URL:-$1}
+SERVICENOW_PASSWORD=${SERVICENOW_PASSWORD:-$2}
+
+if [ -z "$SERVICENOW_URL" ]
+then
+     logerror "SERVICENOW_URL is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [[ "$SERVICENOW_URL" != */ ]]
+then
+    logerror "SERVICENOW_URL does not end with "/" Example: https://dev12345.service-now.com/ "
+    exit 1
+fi
+
+if [ -z "$SERVICENOW_PASSWORD" ]
+then
+     logerror "SERVICENOW_PASSWORD is not set. Export it as environment variable or pass it as argument"
+     exit 1
+fi
+
+if [ ! -z "$GITHUB_RUN_NUMBER" ]
+then
+     # this is github actions
+     set +e
+     log "Waking up servicenow instance..."
+     docker run -e USERNAME="$SERVICENOW_DEVELOPER_USERNAME" -e PASSWORD="$SERVICENOW_DEVELOPER_PASSWORD" vdesabou/servicenowinstancewakeup:latest
+     set -e
+     wait_for_end_of_hibernation
+fi
+
+bootstrap_ccloud_environment
+
+set +e
+playground topic delete --topic topic-servicenow-incidents
+set -e
+
+playground topic create --topic topic-servicenow-incidents
+
+connector_name="ServiceNowSourceV2_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+TODAY=$(date -u '+%Y-%m-%d %H:%M:%S')
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+     "connector.class": "ServiceNowSourceV2",
+     "name": "$connector_name",
+     "kafka.auth.mode": "KAFKA_API_KEY",
+     "kafka.api.key": "$CLOUD_KEY",
+     "kafka.api.secret": "$CLOUD_SECRET",
+     "output.data.format": "AVRO",
+     "auth.type": "BASIC",
+     "servicenow.url": "$SERVICENOW_URL",
+     "connection.user": "admin",
+     "connection.password": "$SERVICENOW_PASSWORD",
+     "table1.name": "incident",
+     "table1.topic": "topic-servicenow-incidents",
+     "table1.start.timestamp": "$TODAY",
+     "table1.timestamp.field": "sys_updated_on",
+     "table1.display.value": "false",
+     "tasks.max" : "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 180
+
+sleep 10
+
+log "Create one record to ServiceNow"
+curl -X POST \
+    "${SERVICENOW_URL}/api/now/table/incident" \
+    --user admin:"$SERVICENOW_PASSWORD" \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -H 'cache-control: no-cache' \
+    -d '{"short_description": "This is test"}'
+
+sleep 5
+
+log "Verify we have received the data in topic-servicenow-incidents topic"
+playground topic consume --topic topic-servicenow-incidents --min-expected-messages 1 --timeout 60
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
