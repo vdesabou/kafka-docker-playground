6afb9c18bd185a17fac25ca5f93284f9e2e18bd0
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-02-20 10:31:24 +0100

    wip

--- scripts/cli/completions.bash
+++ scripts/cli/completions.bash
@@ -1265,7 +1265,7 @@ _playground_completions() {
       ;;
 
     *'ec2 open'*)
-      while read -r; do COMPREPLY+=("$REPLY"); done < <(compgen -W "$(_playground_completions_filter "--disable-sync-repro-folder --help --instance -h -i")" -- "$cur")
+      while read -r; do COMPREPLY+=("$REPLY"); done < <(compgen -W "$(_playground_completions_filter "--enable-sync-repro-folder --help --instance -h -i")" -- "$cur")
       ;;
 
     *'history'*)
