50c3c17c9729fcc8b8b96d8f84fafce86690a3dc
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-03-06 09:28:34 +0100

    keep duplicates

--- scripts/cli/src/commands/history.sh
+++ scripts/cli/src/commands/history.sh
@@ -17,18 +17,15 @@ else
     fzf_option_rounded=""
 fi
 
-awk '!seen[$0]++' $root_folder/playground-run-history > /tmp/tmp
-res=$(tac /tmp/tmp| sed '/^$/d' | fzf --margin=1%,1%,1%,1% $fzf_option_rounded --info=inline --cycle --prompt="🏰" --header="ctrl-c or esc to quit" --color="bg:-1,bg+:-1,info:#BDBB72,border:#FFFFFF,spinner:0,hl:#beb665,fg:#00f7f7,header:#5CC9F5,fg+:#beb665,pointer:#E12672,marker:#5CC9F5,prompt:#98BEDE" --delimiter "kafka-docker-playground" --with-nth "2,3,4" $fzf_option_wrap $fzf_option_pointer)
+res=$(tac $root_folder/playground-run-history | sed '/^$/d' | fzf --margin=1%,1%,1%,1% $fzf_option_rounded --info=inline --cycle --prompt="🏰" --header="ctrl-c or esc to quit" --color="bg:-1,bg+:-1,info:#BDBB72,border:#FFFFFF,spinner:0,hl:#beb665,fg:#00f7f7,header:#5CC9F5,fg+:#beb665,pointer:#E12672,marker:#5CC9F5,prompt:#98BEDE" --delimiter "kafka-docker-playground" --with-nth "2,3,4" $fzf_option_wrap $fzf_option_pointer)
 
 # Prompt the user to edit the res variable
 read -e -p "" -i "$res" edited_res
 
 # Use the edited value if it is not empty
-if [[ -n "$edited_res" ]]; then
+if [[ -n "$edited_res" ]]
+then
   res="$edited_res"
 fi
 
-# log "🚀 Are you sure you want to run:"
-# echo "$res"
-#check_if_continue
 $res
