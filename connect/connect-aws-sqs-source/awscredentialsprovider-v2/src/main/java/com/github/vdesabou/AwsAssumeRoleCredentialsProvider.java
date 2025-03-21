ddc2075744a6ab6f37d415e194bcb506c1fdde42
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-03-21 12:19:35 +0100

    🔐 Create SQS AWS custom credentials example with 2.x #5174

--- connect/connect-aws-sqs-source/awscredentialsprovider-v2/src/main/java/com/github/vdesabou/AwsAssumeRoleCredentialsProvider.java
+++ connect/connect-aws-sqs-source/awscredentialsprovider-v2/src/main/java/com/github/vdesabou/AwsAssumeRoleCredentialsProvider.java
@@ -15,109 +15,43 @@
 
 package com.github.vdesabou;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-//import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.sts.StsClient;
 import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
 import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
 
-import org.apache.kafka.common.Configurable;
-import org.apache.kafka.common.config.AbstractConfig;
-import org.apache.kafka.common.config.ConfigDef;
-// import com.amazonaws.auth.BasicAWSCredentials;
-import java.util.Map;
-// import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
-// import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import org.apache.kafka.common.config.types.Password;
-
-/**
- * AWS credentials provider that uses the AWS Security Token Service to assume a Role and create a
- * temporary, short-lived session to use for authentication.  This credentials provider does not
- * support refreshing the credentials in a background thread.
- */
-public class AwsAssumeRoleCredentialsProvider implements AwsCredentialsProvider, Configurable {
-
-  public static final String ROLE_EXTERNAL_ID_CONFIG = "sts.role.external.id";
-  public static final String ROLE_ARN_CONFIG = "sts.role.arn";
-  public static final String ROLE_SESSION_NAME_CONFIG = "sts.role.session.name";
-
-  public static final String ACCESS_KEY_ID_CONFIG = "sts.aws.access.key.id";
-  public static final String SECRET_KEY_ID_CONFIG = "sts.aws.secret.key.id";
-
-  private static final ConfigDef STS_CONFIG_DEF = new ConfigDef()
-      .define(
-          ROLE_EXTERNAL_ID_CONFIG,
-          ConfigDef.Type.STRING,
-          ConfigDef.Importance.MEDIUM,
-          "The role external ID used when retrieving session credentials under an assumed role."
-      ).define(
-          ROLE_ARN_CONFIG,
-          ConfigDef.Type.STRING,
-          ConfigDef.Importance.HIGH,
-          "Role ARN to use when starting a session."
-      ).define(
-          ROLE_SESSION_NAME_CONFIG,
-          ConfigDef.Type.STRING,
-          ConfigDef.Importance.HIGH,
-          "Role session name to use when starting a session"
-      ).define(
-          ACCESS_KEY_ID_CONFIG,
-          ConfigDef.Type.STRING,
-          ConfigDef.Importance.HIGH,
-          "The AWS access key that will be used"
-      ).define(
-          SECRET_KEY_ID_CONFIG,
-          ConfigDef.Type.PASSWORD,
-          ConfigDef.Importance.HIGH,
-          "The AWS access key that will be used"
-        );
+public class AwsAssumeRoleCredentialsProvider implements AwsCredentialsProvider {
 
   private String roleArn;
   private String roleExternalId;
   private String roleSessionName;
-  // private BasicAWSCredentials basicCredentials;
 
-  // STSAssumeRoleCredentialsProvider takes care of refreshing short-lived
-  // credentials 60 seconds before it's expiry
   private static StsAssumeRoleCredentialsProvider stsCredentialProvider;
 
-  @Override
-  public void configure(Map<String, ?> configs) {
-    AbstractConfig config = new AbstractConfig(STS_CONFIG_DEF, configs);
-    roleArn = config.getString(ROLE_ARN_CONFIG);
-    roleExternalId = config.getString(ROLE_EXTERNAL_ID_CONFIG);
-    roleSessionName = config.getString(ROLE_SESSION_NAME_CONFIG);
-    final String accessKeyId = (String) configs.get(ACCESS_KEY_ID_CONFIG);
-    final String secretKey = (String) configs.get(SECRET_KEY_ID_CONFIG);
-    if (!accessKeyId.equals("") && !secretKey.equals("")) {
-      //
-    } else {
-     // basicCredentials = null;
-      // stsCredentialProvider = new STSAssumeRoleSessionCredentialsProvider.Builder(roleArn, roleSessionName)
-      //     // default sts client will internally use default credentials chain provider
-      //     // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html#credentials-default
-      //     .withStsClient(AWSSecurityTokenServiceClientBuilder.defaultClient())
-      //     .withExternalId(roleExternalId)
-      //     .build();
+  public static AwsCredentialsProvider create() {
+    String accessKeyId = System.getenv("AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_ACCESS_KEY_ID");
+    String secretKey = System.getenv("AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_SECRET_ACCESS_KEY");
+    String roleArn = System.getenv("AWS_STS_ROLE_ARN");
+    String roleSessionName = System.getenv("AWS_STS_SESSION_NAME");
+    String roleExternalId = System.getenv("AWS_STS_EXTERNAL_ID");
 
-      stsCredentialProvider = StsAssumeRoleCredentialsProvider
+    AwsBasicCredentials basicCredentials = AwsBasicCredentials.create(accessKeyId, secretKey);
+      
+    return StsAssumeRoleCredentialsProvider
           .builder()
-          // .asyncCredentialUpdateEnabled(true)
-          .stsClient(StsClient.create())
-          .refreshRequest(() -> AssumeRoleRequest
-              .builder()
+          .stsClient(StsClient.builder()
+              .credentialsProvider(StaticCredentialsProvider.create(basicCredentials))
+              .build())
+          .refreshRequest(() -> AssumeRoleRequest.builder()
               .roleArn(roleArn)
               .roleSessionName(roleSessionName)
+              .externalId(roleExternalId)
               .build())
-          // .withExternalId(roleExternalId)
           .build();
-    }
   }
-
-  public static AwsCredentialsProvider create() {
-  return stsCredentialProvider;
-  } 
 
   @Override
   public AwsCredentials resolveCredentials() {
--- connect/connect-aws-sqs-source/sqs-source-assuming-iam-role-with-custom-aws-credential-provider.sh
+++ connect/connect-aws-sqs-source/sqs-source-assuming-iam-role-with-custom-aws-credential-provider.sh
@@ -96,11 +96,12 @@ playground connector create-or-update --connector sqs-source  << EOF
     "kafka.topic": "test-sqs-source",
     "sqs.url": "$QUEUE_URL",
     "sqs.credentials.provider.class": "com.github.vdesabou.AwsAssumeRoleCredentialsProvider",
+    "_comment": "The following sts parameters are not used when using v2 of the connector",
     "sqs.credentials.provider.sts.role.arn": "$AWS_STS_ROLE_ARN",
     "sqs.credentials.provider.sts.role.session.name": "session-name",
     "sqs.credentials.provider.sts.role.external.id": "123",
-    "_sqs.credentials.provider.sts.aws.access.key.id": "$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_ACCESS_KEY_ID",
-    "_sqs.credentials.provider.sts.aws.secret.key.id": "$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_SECRET_ACCESS_KEY",
+    "sqs.credentials.provider.sts.aws.access.key.id": "$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_ACCESS_KEY_ID",
+    "sqs.credentials.provider.sts.aws.secret.key.id": "$AWS_ACCOUNT_WITH_ASSUME_ROLE_AWS_SECRET_ACCESS_KEY",
     "confluent.license": "",
     "confluent.topic.bootstrap.servers": "broker:9092",
     "confluent.topic.replication.factor": "1",
@@ -112,75 +113,3 @@ EOF
 
 log "Verify we have received the data in test-sqs-source topic"
 playground topic consume --topic test-sqs-source --min-expected-messages 2 --timeout 60
-
-
-# [2025-03-21 09:11:30,583] WARN /connectors/sqs-source/config (org.eclipse.jetty.server.HttpChannel:776)
-# javax.servlet.ServletException: org.glassfish.jersey.server.ContainerException: java.lang.IllegalAccessError: class software.amazon.awssdk.awscore.client.builder.AwsDefaultClientBuilder tried to access private field software.amazon.awssdk.core.client.builder.SdkDefaultClientBuilder.overrideConfig (software.amazon.awssdk.awscore.client.builder.AwsDefaultClientBuilder and software.amazon.awssdk.core.client.builder.SdkDefaultClientBuilder are in unnamed module of loader org.apache.kafka.connect.runtime.isolation.PluginClassLoader @5f7b97da)
-#         at org.glassfish.jersey.servlet.WebComponent.serviceImpl(WebComponent.java:410)
-#         at org.glassfish.jersey.servlet.WebComponent.service(WebComponent.java:346)
-#         at org.glassfish.jersey.servlet.ServletContainer.service(ServletContainer.java:358)
-#         at org.glassfish.jersey.servlet.ServletContainer.service(ServletContainer.java:311)
-#         at org.glassfish.jersey.servlet.ServletContainer.service(ServletContainer.java:205)
-#         at org.eclipse.jetty.servlet.ServletHolder.handle(ServletHolder.java:799)
-#         at org.eclipse.jetty.servlet.ServletHandler.doHandle(ServletHandler.java:554)
-#         at org.eclipse.jetty.server.handler.ScopedHandler.nextHandle(ScopedHandler.java:233)
-#         at org.eclipse.jetty.server.session.SessionHandler.doHandle(SessionHandler.java:1624)
-#         at org.eclipse.jetty.server.handler.ScopedHandler.nextHandle(ScopedHandler.java:233)
-#         at org.eclipse.jetty.server.handler.ContextHandler.doHandle(ContextHandler.java:1440)
-#         at org.eclipse.jetty.server.handler.ScopedHandler.nextScope(ScopedHandler.java:188)
-#         at org.eclipse.jetty.servlet.ServletHandler.doScope(ServletHandler.java:505)
-#         at org.eclipse.jetty.server.session.SessionHandler.doScope(SessionHandler.java:1594)
-#         at org.eclipse.jetty.server.handler.ScopedHandler.nextScope(ScopedHandler.java:186)
-#         at org.eclipse.jetty.server.handler.ContextHandler.doScope(ContextHandler.java:1355)
-#         at org.eclipse.jetty.server.handler.ScopedHandler.handle(ScopedHandler.java:141)
-#         at org.eclipse.jetty.server.handler.ContextHandlerCollection.handle(ContextHandlerCollection.java:234)
-#         at org.eclipse.jetty.server.handler.StatisticsHandler.handle(StatisticsHandler.java:181)
-#         at org.eclipse.jetty.server.handler.HandlerWrapper.handle(HandlerWrapper.java:127)
-#         at org.eclipse.jetty.server.Server.handle(Server.java:516)
-#         at org.eclipse.jetty.server.HttpChannel.lambda$handle$1(HttpChannel.java:487)
-#         at org.eclipse.jetty.server.HttpChannel.dispatch(HttpChannel.java:732)
-#         at org.eclipse.jetty.server.HttpChannel.handle(HttpChannel.java:479)
-#         at org.eclipse.jetty.server.HttpConnection.onFillable(HttpConnection.java:277)
-#         at org.eclipse.jetty.io.AbstractConnection$ReadCallback.succeeded(AbstractConnection.java:311)
-#         at org.eclipse.jetty.io.FillInterest.fillable(FillInterest.java:105)
-#         at org.eclipse.jetty.io.ChannelEndPoint$1.run(ChannelEndPoint.java:104)
-#         at org.eclipse.jetty.util.thread.strategy.EatWhatYouKill.runTask(EatWhatYouKill.java:338)
-#         at org.eclipse.jetty.util.thread.strategy.EatWhatYouKill.doProduce(EatWhatYouKill.java:315)
-#         at org.eclipse.jetty.util.thread.strategy.EatWhatYouKill.tryProduce(EatWhatYouKill.java:173)
-#         at org.eclipse.jetty.util.thread.strategy.EatWhatYouKill.run(EatWhatYouKill.java:131)
-#         at org.eclipse.jetty.util.thread.ReservedThreadExecutor$ReservedThread.run(ReservedThreadExecutor.java:409)
-#         at org.eclipse.jetty.util.thread.QueuedThreadPool.runJob(QueuedThreadPool.java:883)
-#         at org.eclipse.jetty.util.thread.QueuedThreadPool$Runner.run(QueuedThreadPool.java:1034)
-#         at java.base/java.lang.Thread.run(Unknown Source)
-# Caused by: org.glassfish.jersey.server.ContainerException: java.lang.IllegalAccessError: class software.amazon.awssdk.awscore.client.builder.AwsDefaultClientBuilder tried to access private field software.amazon.awssdk.core.client.builder.SdkDefaultClientBuilder.overrideConfig (software.amazon.awssdk.awscore.client.builder.AwsDefaultClientBuilder and software.amazon.awssdk.core.client.builder.SdkDefaultClientBuilder are in unnamed module of loader org.apache.kafka.connect.runtime.isolation.PluginClassLoader @5f7b97da)
-#         at org.glassfish.jersey.servlet.internal.ResponseWriter.rethrow(ResponseWriter.java:255)
-#         at org.glassfish.jersey.servlet.internal.ResponseWriter.failure(ResponseWriter.java:237)
-#         at org.glassfish.jersey.server.ServerRuntime$Responder.process(ServerRuntime.java:438)
-#         at org.glassfish.jersey.server.ServerRuntime$1.run(ServerRuntime.java:263)
-#         at org.glassfish.jersey.internal.Errors$1.call(Errors.java:248)
-#         at org.glassfish.jersey.internal.Errors$1.call(Errors.java:244)
-#         at org.glassfish.jersey.internal.Errors.process(Errors.java:292)
-#         at org.glassfish.jersey.internal.Errors.process(Errors.java:274)
-#         at org.glassfish.jersey.internal.Errors.process(Errors.java:244)
-#         at org.glassfish.jersey.process.internal.RequestScope.runInScope(RequestScope.java:265)
-#         at org.glassfish.jersey.server.ServerRuntime.process(ServerRuntime.java:234)
-#         at org.glassfish.jersey.server.ApplicationHandler.handle(ApplicationHandler.java:684)
-#         at org.glassfish.jersey.servlet.WebComponent.serviceImpl(WebComponent.java:394)
-#         ... 35 more
-# Caused by: java.lang.IllegalAccessError: class software.amazon.awssdk.awscore.client.builder.AwsDefaultClientBuilder tried to access private field software.amazon.awssdk.core.client.builder.SdkDefaultClientBuilder.overrideConfig (software.amazon.awssdk.awscore.client.builder.AwsDefaultClientBuilder and software.amazon.awssdk.core.client.builder.SdkDefaultClientBuilder are in unnamed module of loader org.apache.kafka.connect.runtime.isolation.PluginClassLoader @5f7b97da)
-#         at software.amazon.awssdk.awscore.client.builder.AwsDefaultClientBuilder.setOverrides(AwsDefaultClientBuilder.java:205)
-#         at software.amazon.awssdk.core.client.builder.SdkDefaultClientBuilder.asyncClientConfiguration(SdkDefaultClientBuilder.java:212)
-#         at software.amazon.awssdk.services.sqs.DefaultSqsAsyncClientBuilder.buildClient(DefaultSqsAsyncClientBuilder.java:37)
-#         at software.amazon.awssdk.services.sqs.DefaultSqsAsyncClientBuilder.buildClient(DefaultSqsAsyncClientBuilder.java:25)
-#         at software.amazon.awssdk.core.client.builder.SdkDefaultClientBuilder.build(SdkDefaultClientBuilder.java:155)
-#         at io.confluent.connect.sqs.util.SqsClientUtil.createAsyncClient(SqsClientUtil.java:46)
-#         at io.confluent.connect.sqs.source.SqsSourceConfigValidation.performValidation(SqsSourceConfigValidation.java:89)
-#         at io.confluent.connect.utils.validators.all.ConfigValidation.validate(ConfigValidation.java:185)
-#         at io.confluent.connect.sqs.source.SqsSourceConnector.validate(SqsSourceConnector.java:80)
-#         at org.apache.kafka.connect.runtime.AbstractHerder.validateConnectorConfig(AbstractHerder.java:745)
-#         at org.apache.kafka.connect.runtime.AbstractHerder.lambda$validateConnectorConfig$5(AbstractHerder.java:597)
-#         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Unknown Source)
-#         at java.base/java.util.concurrent.FutureTask.run(Unknown Source)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(Unknown Source)
-#         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(Unknown Source)
-#         ... 1 more
