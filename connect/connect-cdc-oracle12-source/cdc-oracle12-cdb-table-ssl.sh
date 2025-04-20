9da76b9103d9bb901ea361230916e1b639aceb41
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-04-20 21:46:25 +0200

    fix

--- connect/connect-cdc-oracle12-source/cdc-oracle12-cdb-table-ssl.sh
+++ connect/connect-cdc-oracle12-source/cdc-oracle12-cdb-table-ssl.sh
@@ -167,7 +167,7 @@ log "Sleeping 60 seconds"
 sleep 60
 
 set_profiles
-docker compose -f ../../environment/plaintext/docker-compose.yml ${KRAFT_DOCKER_COMPOSE_FILE_OVERRIDE} -f "${PWD}/docker-compose.plaintext.cdb-table-ssl.yml" up -d ${profile_control_center_command} ${profile_ksqldb_command} ${profile_zookeeper_command}  ${profile_grafana_command} ${profile_kcat_command} --quiet-pull
+docker compose -f ../../environment/plaintext/docker-compose.yml ${KRAFT_DOCKER_COMPOSE_FILE_OVERRIDE} -f "${PWD}/docker-compose.plaintext.cdb-table-ssl.yml" ${profile_control_center_command} ${profile_ksqldb_command} ${profile_zookeeper_command}  ${profile_grafana_command} ${profile_kcat_command} up -d --quiet-pull
 command="source ${DIR}/../../scripts/utils.sh && docker compose -f ../../environment/plaintext/docker-compose.yml ${KRAFT_DOCKER_COMPOSE_FILE_OVERRIDE} -f ${PWD}/docker-compose.plaintext.cdb-table-ssl.yml ${profile_control_center_command} ${profile_ksqldb_command} ${profile_zookeeper_command}  ${profile_grafana_command} ${profile_kcat_command} up -d"
 playground state set run.docker_command "$command"
 playground state set run.environment "plaintext"
