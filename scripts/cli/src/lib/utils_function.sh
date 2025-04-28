fa950991d8ac863f2865eecaba425d3e7a00468c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-04-28 15:41:03 +0200

    8.x

--- scripts/cli/src/lib/utils_function.sh
+++ scripts/cli/src/lib/utils_function.sh
@@ -113,6 +113,11 @@ function version_gt() {
 
 function set_kafka_client_tag()
 {
+    if [[ $TAG_BASE = 8.0.* ]]
+    then
+      export KAFKA_CLIENT_TAG="4.0.0"
+    fi
+
     if [[ $TAG_BASE = 7.9.* ]]
     then
       export KAFKA_CLIENT_TAG="3.9.0"
