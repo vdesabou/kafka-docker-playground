14bad72c815c94786d9bea78b4b6aee0d57e8849
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-05-12 09:41:00 +0200

    🐛 STRING_TOO_LONG for PUSH_TOPICS_NAME #6603

--- connect/connect-salesforce-bulkapi-sink/salesforce-bulkapi-sink-with-pushtopics-source-proxy.sh
+++ connect/connect-salesforce-bulkapi-sink/salesforce-bulkapi-sink-with-pushtopics-source-proxy.sh
@@ -70,6 +70,9 @@ fi
 
 PUSH_TOPICS_NAME=MyLeadPushTopics${TAG}
 PUSH_TOPICS_NAME=${PUSH_TOPICS_NAME//[-._]/}
+if [ ${#PUSH_TOPICS_NAME} -gt 25 ]; then
+  PUSH_TOPICS_NAME=${PUSH_TOPICS_NAME:0:25}
+fi
 
 sed -e "s|:PUSH_TOPIC_NAME:|$PUSH_TOPICS_NAME|g" \
      ../../connect/connect-salesforce-bulkapi-sink/MyLeadPushTopics-template.apex > ../../connect/connect-salesforce-bulkapi-sink/MyLeadPushTopics.apex
