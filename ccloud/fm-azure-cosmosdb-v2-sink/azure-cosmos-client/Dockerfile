caa1d6b6514e429cf4d15a587f0e67e49281a384
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-05-19 16:51:05 +0200

    👾 Add Azure Cosmos DB Sink V2 Connector fully managed example #6608

---
+++ ccloud/fm-azure-cosmosdb-v2-sink/azure-cosmos-client/Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3
+
+ADD get-data.py /
+
+RUN pip install azure-cosmos
+
+CMD sleep infinity
---
+++ ccloud/fm-azure-cosmosdb-v2-sink/fully-managed-azure-cosmosdb-v2-sink.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+login_and_maybe_set_azure_subscription
+
+
+bootstrap_ccloud_environment
+
+set +e
+playground topic delete --topic hotels
+sleep 3
+playground topic create --topic hotels --nb-partitions 1
+set -e
+
+
+# https://github.com/microsoft/kafka-connect-cosmosdb/blob/dev/doc/CosmosDB_Setup.md
+AZURE_NAME=pgfm${USER}cs${GITHUB_RUN_NUMBER}${TAG}
+AZURE_NAME=${AZURE_NAME//[-._]/}
+if [ ${#AZURE_NAME} -gt 24 ]; then
+  AZURE_NAME=${AZURE_NAME:0:24}
+fi
+AZURE_REGION=westeurope
+AZURE_RESOURCE_GROUP=$AZURE_NAME
+AZURE_COSMOSDB_SERVER_NAME=$AZURE_NAME
+AZURE_COSMOSDB_DB_NAME=$AZURE_NAME
+AZURE_COSMOSDB_CONTAINER_NAME=$AZURE_NAME
+
+set +e
+log "Delete Cosmos DB instance and resource group (it might fail)"
+az cosmosdb delete -g $AZURE_RESOURCE_GROUP -n $AZURE_COSMOSDB_SERVER_NAME --yes
+az group delete --name $AZURE_RESOURCE_GROUP --yes
+set -e
+
+log "Creating Azure Resource Group $AZURE_RESOURCE_GROUP"
+az group create \
+    --name $AZURE_RESOURCE_GROUP \
+    --location $AZURE_REGION \
+    --tags owner_email=$AZ_USER
+
+sleep 10
+
+log "Creating Cosmos DB server $AZURE_COSMOSDB_SERVER_NAME"
+az cosmosdb create \
+    --name $AZURE_COSMOSDB_SERVER_NAME \
+    --resource-group $AZURE_RESOURCE_GROUP \
+    --locations regionName=$AZURE_REGION
+
+function cleanup_cloud_resources {
+    set +e
+    log "Delete Cosmos DB instance"
+    check_if_continue
+    az cosmosdb delete -g $AZURE_RESOURCE_GROUP -n $AZURE_COSMOSDB_SERVER_NAME --yes
+
+    log "Deleting resource group $AZURE_RESOURCE_GROUP"
+    check_if_continue
+    az group delete --name $AZURE_RESOURCE_GROUP --yes --no-wait
+}
+trap cleanup_cloud_resources EXIT
+
+log "Create the database"
+az cosmosdb sql database create \
+    --name $AZURE_COSMOSDB_DB_NAME \
+    --resource-group $AZURE_RESOURCE_GROUP \
+    --account-name $AZURE_COSMOSDB_SERVER_NAME \
+    --throughput 400
+
+log "Create the container"
+az cosmosdb sql container create \
+    --name $AZURE_COSMOSDB_CONTAINER_NAME \
+    --resource-group $AZURE_RESOURCE_GROUP \
+    --account-name $AZURE_COSMOSDB_SERVER_NAME \
+    --database-name $AZURE_COSMOSDB_DB_NAME \
+    --partition-key-path "/id"
+
+# With the Azure Cosmos DB instance setup, you will need to get the Cosmos DB endpoint URI and primary connection key. These values will be used to setup the Cosmos DB Source and Sink connectors.
+AZURE_COSMOSDB_DB_ENDPOINT_URI="https://${AZURE_COSMOSDB_DB_NAME}.documents.azure.com:443/"
+log "Cosmos DB endpoint URI is $AZURE_COSMOSDB_DB_ENDPOINT_URI"
+
+# get Cosmos DB primary connection key
+AZURE_COSMOSDB_PRIMARY_CONNECTION_KEY=$(az cosmosdb keys list -n $AZURE_COSMOSDB_DB_NAME -g $AZURE_RESOURCE_GROUP --query primaryMasterKey -o tsv)
+
+docker compose build
+docker compose down -v --remove-orphans
+docker compose up -d --quiet-pull
+
+connector_name="CosmosDbSinkV2_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+    "connector.class": "CosmosDbSinkV2",
+    "name": "$connector_name",
+    "kafka.auth.mode": "KAFKA_API_KEY",
+    "kafka.api.key": "$CLOUD_KEY",
+    "kafka.api.secret": "$CLOUD_SECRET",
+    "input.data.format": "JSON",
+    "azure.cosmos.account.endpoint": "$AZURE_COSMOSDB_DB_ENDPOINT_URI",
+    "azure.cosmos.account.key": "$AZURE_COSMOSDB_PRIMARY_CONNECTION_KEY",
+    "azure.cosmos.sink.database.name": "$AZURE_COSMOSDB_DB_NAME",
+    "azure.cosmos.sink.containers.topicMap": "hotels#${AZURE_COSMOSDB_DB_NAME}",
+    "azure.cosmos.sink.id.strategy": "FullKeyStrategy",
+    "topics": "hotels",
+    "tasks.max" : "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 180
+
+
+log "Write data to topic hotels"
+playground topic produce -t hotels --nb-messages 3 << 'EOF'
+{"id": "h1", "HotelName": "Marriott", "Description": "Marriott description"}
+{"id": "h2", "HotelName": "HolidayInn", "Description": "HolidayInn description"}
+{"id": "h3", "HotelName": "Motel8", "Description": "Motel8 description"}
+EOF
+
+sleep 10
+
+log "Verify data from Azure Cosmos DB"
+docker exec -e AZURE_COSMOSDB_DB_ENDPOINT_URI=$AZURE_COSMOSDB_DB_ENDPOINT_URI -e AZURE_COSMOSDB_PRIMARY_CONNECTION_KEY=$AZURE_COSMOSDB_PRIMARY_CONNECTION_KEY -e AZURE_COSMOSDB_DB_NAME=$AZURE_COSMOSDB_DB_NAME -e AZURE_COSMOSDB_CONTAINER_NAME=$AZURE_COSMOSDB_CONTAINER_NAME azure-cosmos-client bash -c "python /get-data.py" > /tmp/result.log  2>&1
+
+cat /tmp/result.log
+grep "Motel8" /tmp/result.log
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
