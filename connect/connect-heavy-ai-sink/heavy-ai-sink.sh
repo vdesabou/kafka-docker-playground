af7c13adc7e35795437fcd1feba0a36e35bb76ee
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-07-03 12:14:17 +0200

    fix version

--- connect/connect-heavy-ai-sink/heavy-ai-sink.sh
+++ connect/connect-heavy-ai-sink/heavy-ai-sink.sh
@@ -4,9 +4,9 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
-if [ ! -z "$TAG_BASE" ] && version_gt $TAG_BASE "7.9.99" && [ ! -z "$CONNECTOR_TAG" ] && ! version_gt $CONNECTOR_TAG "0.0.99"
+if [ ! -z "$TAG_BASE" ] && version_gt $TAG_BASE "7.9.99" && [ ! -z "$CONNECTOR_TAG" ] && ! version_gt $CONNECTOR_TAG "1.0.8"
 then
-     logwarn "minimal supported connector version is 1.0.0 for CP 8.0"
+     logwarn "minimal supported connector version is 1.0.9 for CP 8.0"
      logwarn "see https://docs.confluent.io/platform/current/connect/supported-connector-version-8.0.html#supported-connector-versions-in-cp-8-0"
      exit 111
 fi
