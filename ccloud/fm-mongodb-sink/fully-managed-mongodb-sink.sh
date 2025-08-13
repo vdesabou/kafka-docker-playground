0e5062941e16fdf438a73145ebb7dd8234df7406
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-08-13 12:04:53 +0200

    👾 Add Fully Managed Mongo Sink example with MONGODB_SELF_MANAGED #6947

---
+++ ccloud/fm-mongodb-sink/fully-managed-mongodb-sink.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+
+NGROK_AUTH_TOKEN=${NGROK_AUTH_TOKEN:-$1}
+
+display_ngrok_warning
+
+bootstrap_ccloud_environment
+
+set +e
+playground topic delete --topic mongodb.orders
+set -e
+
+playground topic create --topic mongodb.orders
+
+docker compose build
+docker compose down -v --remove-orphans
+docker compose up -d --quiet-pull
+
+sleep 5
+
+log "Waiting for ngrok to start"
+while true
+do
+  container_id=$(docker ps -q -f name=ngrok)
+  if [ -n "$container_id" ]
+  then
+    status=$(docker inspect --format '{{.State.Status}}' $container_id)
+    if [ "$status" = "running" ]
+    then
+      log "Getting ngrok hostname and port"
+      NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+      NGROK_HOSTNAME=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 1)
+      NGROK_PORT=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 2)
+
+      if ! [[ $NGROK_PORT =~ ^[0-9]+$ ]]
+      then
+        log "NGROK_PORT is not a valid number, keep retrying..."
+        continue
+      else 
+        break
+      fi
+    fi
+  fi
+  log "Waiting for container ngrok to start..."
+  sleep 5
+done
+
+
+log "Initialize MongoDB replica set"
+docker exec -i mongodb mongosh --eval 'rs.initiate({_id: "myuser", members:[{_id: 0, host: "mongodb:27017"}]})'
+
+sleep 5
+
+log "Create a user profile"
+docker exec -i mongodb mongosh << EOF
+use admin
+db.createUser(
+{
+user: "myuser",
+pwd: "mypassword",
+roles: ["dbOwner"]
+}
+)
+EOF
+
+sleep 2
+
+log "Sending messages to topic mongodb.orders"
+playground topic produce -t mongodb.orders --nb-messages 1 << 'EOF'
+{
+  "type": "record",
+  "name": "myrecord",
+  "fields": [
+    {
+      "name": "id",
+      "type": "int"
+    },
+    {
+      "name": "product",
+      "type": "string"
+    },
+    {
+      "name": "quantity",
+      "type": "int"
+    },
+    {
+      "name": "price",
+      "type": "float"
+    }
+  ]
+}
+EOF
+
+playground topic produce -t mongodb.orders --nb-messages 1 --forced-value '{"id":2,"product":"foo","quantity":2,"price":0.86583304}' << 'EOF'
+{
+  "type": "record",
+  "name": "myrecord",
+  "fields": [
+    {
+      "name": "id",
+      "type": "int"
+    },
+    {
+      "name": "product",
+      "type": "string"
+    },
+    {
+      "name": "quantity",
+      "type": "int"
+    },
+    {
+      "name": "price",
+      "type": "float"
+    }
+  ]
+}
+EOF
+
+
+connector_name="MongoDbSink_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+  "connector.class": "MongoDbAtlasSink",
+  "name": "$connector_name",
+  "kafka.auth.mode": "KAFKA_API_KEY",
+  "kafka.api.key": "$CLOUD_KEY",
+  "kafka.api.secret": "$CLOUD_SECRET",
+  "topics":"mongodb.orders",
+  "mongodb.instance.type": "MONGODB_SELF_MANAGED",
+  "connection.host": "$NGROK_HOSTNAME:$NGROK_PORT",
+  "connection.user": "myuser",
+  "connection.password": "mypassword",
+  "database": "inventory",
+  "collection": "customers",
+  "input.data.format": "AVRO",
+  "tasks.max": "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 180
+
+sleep 10
+
+log "View record"
+docker exec -i mongodb mongosh << EOF
+use inventory
+db.customers.find().pretty();
+EOF
+
+docker exec -i mongodb mongosh << EOF > output.txt
+use inventory
+db.customers.find().pretty();
+EOF
+grep "foo" output.txt
+rm output.txt
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
