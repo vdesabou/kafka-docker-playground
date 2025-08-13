f15fc1fff5a5f57b010962857562721ca28c4119
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-08-13 15:53:52 +0200

    tabular is now deprecated

--- connect/connect-iceberg-sink/iceberg-sink.sh
+++ connect/connect-iceberg-sink/iceberg-sink.sh
@@ -17,7 +17,7 @@ playground connector create-or-update --connector iceberg-sink  << EOF
 {
   "tasks.max": "1",
   "topics": "payments",
-  "connector.class": "io.tabular.iceberg.connect.IcebergSinkConnector",
+  "connector.class": "org.apache.iceberg.connect.IcebergSinkConnector",
   "iceberg.catalog.s3.endpoint": "http://minio:9000",
   "iceberg.catalog.s3.secret-access-key": "minioadmin",
   "iceberg.catalog.s3.access-key-id": "minioadmin",
@@ -35,6 +35,24 @@ playground connector create-or-update --connector iceberg-sink  << EOF
   "schemas.enable": "false"
 }
 EOF
+
+# https://github.com/apache/iceberg/issues/12507
+
+# [2025-08-13 13:08:47,143] ERROR [iceberg-sink|worker] Failed to start connector iceberg-sink (org.apache.kafka.connect.runtime.Worker:425)
+# java.lang.NoClassDefFoundError: org/apache/iceberg/IcebergBuild
+# 	at org.apache.iceberg.connect.IcebergSinkConfig.version(IcebergSinkConfig.java:109) ~[iceberg-kafka-connect-1.9.1.jar:?]
+# 	at org.apache.iceberg.connect.IcebergSinkConnector.version(IcebergSinkConnector.java:37) ~[iceberg-kafka-connect-1.9.1.jar:?]
+# 	at org.apache.kafka.connect.runtime.WorkerConnector$ConnectorMetricsGroup.<init>(WorkerConnector.java:502) ~[connect_runtime_runtime-project.jar:?]
+# 	at org.apache.kafka.connect.runtime.WorkerConnector.<init>(WorkerConnector.java:103) ~[connect_runtime_runtime-project.jar:?]
+# 	at org.apache.kafka.connect.runtime.Worker.startConnector(Worker.java:411) ~[connect_runtime_runtime-project.jar:?]
+# 	at org.apache.kafka.connect.runtime.distributed.DistributedHerder.startConnector(DistributedHerder.java:2172) ~[connect_runtime_runtime-project.jar:?]
+# 	at org.apache.kafka.connect.runtime.distributed.DistributedHerder.lambda$getConnectorStartingCallable$46(DistributedHerder.java:2178) ~[connect_runtime_runtime-project.jar:?]
+# 	at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:317) ~[?:?]
+# 	at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1144) ~[?:?]
+# 	at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:642) ~[?:?]
+# 	at java.base/java.lang.Thread.run(Thread.java:1583) [?:?]
+# Caused by: java.lang.ClassNotFoundException: org.apache.iceberg.IcebergBuild
+# 	... 11 more
 
 sleep 30
 
