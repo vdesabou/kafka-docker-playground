faf4a90aa77defcdbcb4862f0f587afc9076f146
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-08-13 11:23:42 +0200

    👾 Add Fully Managed Mongo Source example with MONGODB_SELF_MANAGED #6946

---
+++ ccloud/fm-mongodb-source/README.md
@@ -0,0 +1,20 @@
+# Fully Managed MongoDB Source connector
+
+
+
+## Objective
+
+Quickly test [MongoDB Source](https://docs.confluent.io/cloud/current/connectors/cc-mongo-db-source.html) connector with `mongodb.instance.type=MONGODB_SELF_MANAGED`
+
+
+## Prerequisites
+
+See [here](https://kafka-docker-playground.io/#/how-to-use?id=%f0%9f%8c%a4%ef%b8%8f-confluent-cloud-examples)
+
+
+## How to run
+
+Simply run:
+
+```
+$ just use <playground run> command
---
+++ ccloud/fm-mongodb-source/fully-managed-mongodb-source.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+
+NGROK_AUTH_TOKEN=${NGROK_AUTH_TOKEN:-$1}
+
+display_ngrok_warning
+
+bootstrap_ccloud_environment
+
+set +e
+playground topic delete --topic mongo.inventory.customers
+set -e
+
+playground topic create --topic mongo.inventory.customers
+
+docker compose build
+docker compose down -v --remove-orphans
+docker compose up -d --quiet-pull
+
+sleep 5
+
+log "Waiting for ngrok to start"
+while true
+do
+  container_id=$(docker ps -q -f name=ngrok)
+  if [ -n "$container_id" ]
+  then
+    status=$(docker inspect --format '{{.State.Status}}' $container_id)
+    if [ "$status" = "running" ]
+    then
+      log "Getting ngrok hostname and port"
+      NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+      NGROK_HOSTNAME=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 1)
+      NGROK_PORT=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 2)
+
+      if ! [[ $NGROK_PORT =~ ^[0-9]+$ ]]
+      then
+        log "NGROK_PORT is not a valid number, keep retrying..."
+        continue
+      else 
+        break
+      fi
+    fi
+  fi
+  log "Waiting for container ngrok to start..."
+  sleep 5
+done
+
+log "Initialize MongoDB replica set"
+docker exec -i mongodb mongosh --eval 'rs.initiate({_id: "myuser", members:[{_id: 0, host: "mongodb:27017"}]})'
+
+sleep 5
+
+log "Create a user profile"
+docker exec -i mongodb mongosh << EOF
+use admin
+db.createUser(
+     {
+          user: "myuser",
+          pwd: "mypassword",
+          roles: ["dbOwner"]
+     }
+)
+EOF
+
+
+connector_name="MongoDbSource_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+     "connector.class": "MongoDbAtlasSource",
+     "name": "$connector_name",
+     "kafka.auth.mode": "KAFKA_API_KEY",
+     "kafka.api.key": "$CLOUD_KEY",
+     "kafka.api.secret": "$CLOUD_SECRET",
+     "topic.prefix":"mongo",
+     "mongodb.instance.type": "MONGODB_SELF_MANAGED",
+     "connection.host": "$NGROK_HOSTNAME:$NGROK_PORT",
+     "connection.user": "myuser",
+     "connection.password": "mypassword",
+     "database": "inventory",
+     "collection": "customers",
+     "poll.await.time.ms": "5000",
+     "poll.max.batch.size": "1000",
+     "startup.mode": "copy_existing",
+     "output.data.format": "JSON",
+     "tasks.max": "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 180
+
+sleep 5
+
+log "Insert a record"
+docker exec -i mongodb mongosh << EOF
+use inventory
+db.customers.insert([
+{ _id : 1, first_name : 'Bob', last_name : 'Hopper', email : 'thebob@example.com' }
+]);
+EOF
+
+log "Verifying topic mongo.inventory.customers"
+playground topic consume --topic mongo.inventory.customers --min-expected-messages 1 --timeout 60
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
