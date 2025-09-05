8136f45402ac477885f2149b15d1550aa1f1e063
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-09-05 16:27:08 +0200

    minor

--- connect/connect-http-sink/http_ssl_basic_auth.sh
+++ connect/connect-http-sink/http_ssl_basic_auth.sh
@@ -30,30 +30,29 @@ EOF
 playground debug log-level set --package "org.apache.http" --level TRACE
 
 log "Creating http-sink connector"
-playground connector create-or-update --connector http-ssl-basic-auth-sink2  << EOF
+playground connector create-or-update --connector http-ssl-basic-auth-sink  << EOF
 {
-     "topics": "http-messages",
-     "tasks.max": "1",
-     "connector.class": "io.confluent.connect.http.HttpSinkConnector",
-     "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-     "value.converter": "org.apache.kafka.connect.storage.StringConverter",
-     "confluent.topic.bootstrap.servers": "broker:9092",
-     "confluent.topic.replication.factor": "1",
-     "reporter.bootstrap.servers": "broker:9092",
-     "reporter.error.topic.name": "error-responses",
-     "reporter.error.topic.replication.factor": 1,
-     "reporter.result.topic.name": "success-responses",
-     "reporter.result.topic.replication.factor": 1,
-     "reporter.result.topic.value.format": "string",
-     "http.api.url": "https://http-service-ssl-basic-auth:8443/api/messages",
-     "auth.type": "BASIC",
-     "connection.user": "admin",
-     "connection.password": "password",
-     "ssl.enabled": "true",
-     "https.ssl.truststore.location": "/tmp/truststore.http-service-ssl-basic-auth.jks",
-     "https.ssl.truststore.type": "JKS",
-     "https.ssl.truststore.password": "confluent",
-     "https.ssl.protocol": "TLSv1.2"
+	"topics": "http-messages",
+	"tasks.max": "1",
+	"connector.class": "io.confluent.connect.http.HttpSinkConnector",
+	"key.converter": "org.apache.kafka.connect.storage.StringConverter",
+	"value.converter": "org.apache.kafka.connect.storage.StringConverter",
+	"confluent.topic.bootstrap.servers": "broker:9092",
+	"confluent.topic.replication.factor": "1",
+	"reporter.bootstrap.servers": "broker:9092",
+	"reporter.error.topic.name": "error-responses",
+	"reporter.error.topic.replication.factor": 1,
+	"reporter.result.topic.name": "success-responses",
+	"reporter.result.topic.replication.factor": 1,
+	"reporter.result.topic.value.format": "string",
+	"http.api.url": "https://http-service-ssl-basic-auth:8443/api/messages",
+	"auth.type": "BASIC",
+	"connection.user": "admin",
+	"connection.password": "password",
+	"https.ssl.truststore.location": "/tmp/truststore.http-service-ssl-basic-auth.jks",
+	"https.ssl.truststore.type": "JKS",
+	"https.ssl.truststore.password": "confluent",
+	"https.ssl.protocol": "TLSv1.2"
 }
 EOF
 
