2e2fdb7468570da574d278d57017a79aebef2eb4
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-10-02 14:46:36 +0200

    🧠🧞‍♂️⭐ Add playground ai command #7130 add kafka-mcp-server

--- scripts/cli/src/commands/ai.sh
+++ scripts/cli/src/commands/ai.sh
@@ -13,16 +13,26 @@ if [[ "$environment" == "ccloud" ]]
 then
 	if [ -f .ccloud/.env ]
 	then
-		log "🌩️ ccloud environment is used, using mcp-confluent server to interact with confluent cloud"
-		gemini mcp remove mcp-confluent > /dev/null 2>&1 || true
-		gemini mcp add mcp-confluent npx "-y" "@confluentinc/mcp-confluent@latest" -- "-e" "$root_folder/.ccloud/.env"
+		log "🌩️ ccloud environment is used, using mcp-confluent server (https://github.com/confluentinc/mcp-confluent) to interact with confluent cloud"
+		gemini mcp remove mcp-kafka > /dev/null 2>&1 || true
+		gemini mcp remove mcp-ccloud > /dev/null 2>&1 || true
+		gemini mcp add mcp-ccloud npx "-y" "@confluentinc/mcp-confluent@latest" -- "-e" "$root_folder/.ccloud/.env"
 	else
 		logerror "❌ .ccloud/.env file is not present!"
 		exit 1
 	fi
 else
 	# https://github.com/google-gemini/gemini-cli/issues/9766
-	gemini mcp remove mcp-confluent > /dev/null 2>&1 || true
+	gemini mcp remove mcp-ccloud > /dev/null 2>&1 || true
+
+	if [[ "$environment" == "plaintext" ]]
+	then
+		log "🌩️ plaintext environment is used, using kafka-mcp-server (https://docs.tuannvm.com/kafka-mcp-server) to interact with the cluster"
+		gemini mcp remove mcp-kafka > /dev/null 2>&1 || true
+		gemini mcp add mcp-kafka kafka-mcp-server -e "KAFKA_BROKERS=localhost:29092" -e "KAFKA_CLIENT_ID=kafka-mcp-server" -e "MCP_TRANSPORT=stdio"
+	else
+		logwarn "🔐 $environment environment is used, using kafka-mcp-server (https://docs.tuannvm.com/kafka-mcp-server) to interact with the cluster will not be used, only works with plaintext for now"
+	fi
 fi
 
 if [[ -n "$yolo" ]]
