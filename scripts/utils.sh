70e482c613ce18cf6d34e51559b8b66b579c9138
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-10-10 15:38:28 +0200

    👾 Upgrade playground to CP 8.0.2 #7217

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -33,7 +33,7 @@ fi
 if [ -z "$TAG" ]
 then
     # TAG is not set, use default:
-    export TAG=8.0.0 # default tag
+    export TAG=8.0.2 # default tag
     # to handle ubi8 images
     export TAG_BASE="$TAG"
     if [ -z "$CP_KAFKA_IMAGE" ]
