536b0c4ccc33320333e2bf3de65b7ab56b9f2a62
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-10-17 09:22:01 +0200

    fix

--- multi-data-center/mirrormaker2/mirrormaker2-plaintext.sh
+++ multi-data-center/mirrormaker2/mirrormaker2-plaintext.sh
@@ -12,10 +12,10 @@ fi
 ${DIR}/../../environment/mdc-plaintext/start.sh "${PWD}/docker-compose.mdc-plaintext.yml"
 
 log "Sending sales in Europe cluster"
-seq -f "european_sale_%g ${RANDOM}" 10 | docker container exec -i broker-europe kafka-console-producer --bootstrap-server localhost:9092 --topic sales_EUROPE
+seq -f "european_sale_%g ${RANDOM}" 10 | docker container exec -i broker-europe kafka-console-producer --bootstrap-server broker-europe:9092 --topic sales_EUROPE
 
 log "Sending sales in US cluster"
-seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i broker-us kafka-console-producer --bootstrap-server localhost:9092 --topic sales_US
+seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i broker-us kafka-console-producer --bootstrap-server broker-us:9092 --topic sales_US
 
 log "Consolidating all sales (logs are in /tmp/mirrormaker.log):"
 
@@ -32,10 +32,10 @@ sleep 120
 # By default MM2 renames source topics to be prefixed with the source cluster name. e.g. if topic foo came from cluster A then it would be named A.foo on the destination. In the current release (5.4) MM2 does not support any different topic naming strategies out of the box.
 
 log "Verify we have received the data in topic US.sales_US in EUROPE"
-timeout 60 docker container exec broker-europe kafka-console-consumer --bootstrap-server localhost:9092 --topic "US.sales_US" --from-beginning --max-messages 10
+timeout 60 docker container exec broker-europe kafka-console-consumer --bootstrap-server broker-europe:9092 --topic "US.sales_US" --from-beginning --max-messages 10
 
 log "Verify we have received the data in topic EUROPE.sales_EUROPE topics in the US"
-timeout 60 docker container exec broker-us kafka-console-consumer --bootstrap-server localhost:9092 --topic "EUROPE.sales_EUROPE" --from-beginning --max-messages 10
+timeout 60 docker container exec broker-us kafka-console-consumer --bootstrap-server broker-us:9092 --topic "EUROPE.sales_EUROPE" --from-beginning --max-messages 10
 
 log "Copying mirrormaker logs to /tmp/mirrormaker.log"
 docker cp connect-us:/tmp/mirrormaker.log /tmp/mirrormaker.log
