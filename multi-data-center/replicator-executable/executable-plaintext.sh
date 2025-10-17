69c1965df829bbcfea68221c836607ccd949a8b2
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-10-17 14:16:30 +0200

    fix

--- multi-data-center/replicator-executable/executable-plaintext.sh
+++ multi-data-center/replicator-executable/executable-plaintext.sh
@@ -13,10 +13,10 @@ fi
 playground start-environment --environment mdc-plaintext --docker-compose-override-file "${PWD}/docker-compose.mdc-plaintext.replicator.yml"
 
 log "Sending sales in Europe cluster"
-seq -f "european_sale_%g ${RANDOM}" 10 | docker container exec -i broker-europe kafka-console-producer --bootstrap-server localhost:9092 --topic sales_EUROPE
+seq -f "european_sale_%g ${RANDOM}" 10 | docker container exec -i broker-europe kafka-console-producer --bootstrap-server broker-europe:9092 --topic sales_EUROPE
 
 log "Sending sales in US cluster"
-seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i broker-us kafka-console-producer --bootstrap-server localhost:9092 --topic sales_US
+seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i broker-us kafka-console-producer --bootstrap-server broker-us:9092 --topic sales_US
 
 playground container restart -c replicator-europe
 playground container restart -c replicator-us
@@ -27,7 +27,7 @@ wait_container_ready replicator-europe
 sleep 120
 
 log "Verify we have received the data in all the sales_ topics in EUROPE"
-docker container exec broker-europe timeout 120 kafka-console-consumer --bootstrap-server localhost:9092 --include "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000
+docker container exec broker-europe timeout 120 kafka-console-consumer --bootstrap-server broker-europe:9092 --include "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000
 
 log "Verify we have received the data in all the sales_ topics in the US"
-docker container exec broker-us timeout 120 kafka-console-consumer --bootstrap-server localhost:9092 --include "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000
+docker container exec broker-us timeout 120 kafka-console-consumer --bootstrap-server broker-us:9092 --include "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000
--- multi-data-center/replicator-executable/executable-sasl-plain.sh
+++ multi-data-center/replicator-executable/executable-sasl-plain.sh
@@ -13,10 +13,10 @@ fi
 playground start-environment --environment mdc-sasl-plain --docker-compose-override-file "${PWD}/docker-compose.mdc-sasl-plain.replicator.yml"
 
 log "Sending sales in Europe cluster"
-seq -f "european_sale_%g ${RANDOM}" 10 | docker container exec -i broker-europe kafka-console-producer --bootstrap-server localhost:9092 --topic sales_EUROPE --producer.config /etc/kafka/client.properties
+seq -f "european_sale_%g ${RANDOM}" 10 | docker container exec -i broker-europe kafka-console-producer --bootstrap-server broker-europe:9092 --topic sales_EUROPE --producer.config /etc/kafka/client.properties
 
 log "Sending sales in US cluster"
-seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i broker-us kafka-console-producer --bootstrap-server localhost:9092 --topic sales_US --producer.config /etc/kafka/client.properties
+seq -f "us_sale_%g ${RANDOM}" 10 | docker container exec -i broker-us kafka-console-producer --bootstrap-server broker-us:9092 --topic sales_US --producer.config /etc/kafka/client.properties
 
 playground container restart -c replicator-europe
 playground container restart -c replicator-us
@@ -27,7 +27,7 @@ wait_container_ready replicator-europe
 sleep 120
 
 log "Verify we have received the data in all the sales_ topics in EUROPE"
-docker container exec broker-europe timeout 120 kafka-console-consumer --bootstrap-server localhost:9092 --include "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/kafka/client.properties
+docker container exec broker-europe timeout 120 kafka-console-consumer --bootstrap-server broker-europe:9092 --include "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/kafka/client.properties
 
 log "Verify we have received the data in all the sales_ topics in the US"
-docker container exec broker-us timeout 120 kafka-console-consumer --bootstrap-server localhost:9092 --include "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/kafka/client.properties
+docker container exec broker-us timeout 120 kafka-console-consumer --bootstrap-server broker-us:9092 --include "sales_.*" --from-beginning --max-messages 20 --property metadata.max.age.ms 30000 --consumer.config /etc/kafka/client.properties
