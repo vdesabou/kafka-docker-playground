09431a70dc364dc051ada0de8ec18ac322ffcde7
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-10-28 10:39:05 +0100

    fix error message

--- ccloud/connect-centralized-license/redshift.sh
+++ ccloud/connect-centralized-license/redshift.sh
@@ -69,7 +69,8 @@ while [[ ! $(cat /tmp/out.txt) =~ "available" ]]; do
      aws redshift describe-clusters --cluster-identifier $CLUSTER_NAME | jq .Clusters[0].ClusterStatus > /tmp/out.txt 2>&1
      CUR_WAIT=$(( CUR_WAIT+10 ))
      if [[ "$CUR_WAIT" -gt "$MAX_WAIT" ]]; then
-          echo -e "\nERROR: The logs in ${CONTROL_CENTER_CONTAINER} container do not show 'available' after $MAX_WAIT seconds. Please troubleshoot with 'docker container ps' and 'playground container logs --open --container <container>'.\n"
+          logerror "❌ AWS Redshift cluster $CLUSTER_NAME has not started ! See output of aws redshift describe-clusters below:"
+          cat /tmp/out.txt
           exit 1
      fi
 done
