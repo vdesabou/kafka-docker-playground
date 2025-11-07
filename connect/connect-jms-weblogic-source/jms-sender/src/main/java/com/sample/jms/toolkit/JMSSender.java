261b3dd5caf33b2e11eb9e966dd3273436f7caac
Aman Shrivastava <ashrivastava@confluent.io>
2025-11-07 13:03:33 +0530

    Add commitRecord API assertion to JMS Weblogic Source tests

--- connect/connect-jms-weblogic-source/jms-sender/src/main/java/com/sample/jms/toolkit/JMSSender.java
+++ connect/connect-jms-weblogic-source/jms-sender/src/main/java/com/sample/jms/toolkit/JMSSender.java
@@ -1,5 +1,6 @@
 package com.sample.jms.toolkit;
 
+import java.util.Enumeration;
 import java.util.Hashtable;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
@@ -7,6 +8,9 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
+import javax.jms.QueueBrowser;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
 import javax.jms.QueueSession;
 import javax.jms.Session;
 import javax.jms.Topic;
@@ -24,7 +28,11 @@ public class JMSSender {
 	public final static String URL = "t3://weblogic-jms:7001";
 
 	public static void main(String[] args) throws NamingException, JMSException {
-
+		// If "check" argument is provided, check queue status instead of sending messages
+		if (args.length > 0 && "check".equals(args[0])) {
+			checkQueueStatus();
+			return;
+		}
 
 		System.out.println("Sending message to queue");
 		Connection connection = null;
@@ -87,5 +95,47 @@ public class JMSSender {
 		env.put(Context.INITIAL_CONTEXT_FACTORY, JNDI_FACTORY);
 		env.put(Context.PROVIDER_URL, URL);
 		return new InitialContext(env);
+	}
+
+	private static void checkQueueStatus() throws NamingException, JMSException {
+		QueueConnection connection = null;
+		try {
+			// Create initial context with credentials for queue checking
+			Hashtable env = new Hashtable();
+			env.put(Context.INITIAL_CONTEXT_FACTORY, JNDI_FACTORY);
+			env.put(Context.PROVIDER_URL, URL);
+			env.put(Context.SECURITY_PRINCIPAL, "weblogic");
+			env.put(Context.SECURITY_CREDENTIALS, "welcome1");
+			Context context = new InitialContext(env);
+			ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("myFactory");
+			QueueConnectionFactory qcf = (QueueConnectionFactory) connectionFactory;
+			connection = qcf.createQueueConnection();
+			QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			Queue queue = (Queue) context.lookup("myQueue");
+			connection.start();
+
+			// Use QueueBrowser to peek at messages without consuming them
+			QueueBrowser browser = session.createBrowser(queue);
+			Enumeration messages = browser.getEnumeration();
+
+			int messageCount = 0;
+			while (messages.hasMoreElements()) {
+				messages.nextElement();
+				messageCount++;
+			}
+			browser.close();
+
+			if (messageCount == 0) {
+				System.out.println("Queue is empty - messages were successfully consumed and deleted");
+				System.exit(0);
+			} else {
+				System.out.println("Queue still contains " + messageCount + " message(s) - messages were not deleted");
+				System.exit(1);
+			}
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
 	}
 }
