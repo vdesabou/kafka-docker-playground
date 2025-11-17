cb66fb8b8b3182cd56c2e364ce17054cc8ca72ca
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-11-17 14:41:45 +0100

    wip

--- scripts/cli/src/commands/bashly-reload.sh
+++ scripts/cli/src/commands/bashly-reload.sh
@@ -1,5 +1,17 @@
-cd $root_folder/scripts/cli
-bashly generate
+log "💫 generating playground CLI using Bashly (https://bashly.dev/)"
+set +e
+docker pull dannyben/bashly > /dev/null 2>&1
+set +e
+
+cd "$root_folder/scripts/cli"
+docker run --rm -it --user $(id -u):$(id -g) --volume "$PWD:/app" dannyben/bashly generate
 rm -f $root_folder/scripts/cli/completions.bash
-bashly add completions_script
+docker run --rm -it --user $(id -u):$(id -g) --volume "$PWD:/app" dannyben/bashly add completions_script --quiet
+
+log 🎱 "if you updated bahsly.yml with new commands or modified fags, you can reload completions file using"
+echo ""
+echo "source $root_folder/scripts/cli/completions.bash"
+
+docker run --rm -it --user $(id -u):$(id -g) --volume "$PWD:/app" dannyben/bashly render templates/shell-script-command-completion .
+yq -o=json playground.yaml > playground.json
 cd - > /dev/null
