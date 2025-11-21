5e308f3e35077bcd15803a689307308478e41e4d
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-11-21 14:55:25 +0100

    👾⭐ Add fully managed InfluxDB 3 Sink Connector example #7370

---
+++ ccloud/fm-influxdb3-sink/README.md
@@ -0,0 +1,43 @@
+# Fully Managed InfluxDB 3 Sink connector
+
+
+
+## Objective
+
+Quickly test [InfluxDB 3 Sink](https://docs.confluent.io/cloud/current/connectors/cc-influx-db3-sink.html) connector.
+
+## Exposing docker container over internet
+
+**🚨WARNING🚨** It is considered a security risk to run this example on your personal machine since you'll be exposing a TCP port over internet using [Ngrok](https://ngrok.com). It is strongly encouraged to run it on a AWS EC2 instance where you'll use [Confluent Static Egress IP Addresses](https://docs.confluent.io/cloud/current/networking/static-egress-ip-addresses.html#use-static-egress-ip-addresses-with-ccloud) (only available for public endpoints on AWS) to allow traffic from your Confluent Cloud cluster to your EC2 instance using EC2 Security Group.
+
+Example in order to set EC2 Security Group with Confluent Static Egress IP Addresses and port 1414:
+
+```bash
+group=$(aws ec2 describe-instances --instance-id <$ec2-instance-id> --output=json | jq '.Reservations[] | .Instances[] | {SecurityGroups: .SecurityGroups}' | jq -r '.SecurityGroups[] | .GroupName')
+aws ec2 authorize-security-group-ingress --group-name $group --protocol tcp --port 1414 --cidr 13.36.88.88/32
+aws ec2 authorize-security-group-ingress --group-name $group --protocol tcp --port 1414 --cidr 13.36.88.89/32
+etc...
+```
+
+An [Ngrok](https://ngrok.com) auth token is necessary in order to expose the Docker Container port to internet, so that fully managed connector can reach it.
+
+You can sign up at https://dashboard.ngrok.com/signup
+If you have already signed up, make sure your auth token is setup by exporting environment variable `NGROK_AUTH_TOKEN`
+
+Your auth token is available on your dashboard: https://dashboard.ngrok.com/get-started/your-authtoken
+
+Ngrok web interface available at http://localhost:4551
+
+## Prerequisites
+
+See [here](https://kafka-docker-playground.io/#/how-to-use?id=%f0%9f%8c%a4%ef%b8%8f-confluent-cloud-examples)
+
+
+
+## How to run
+
+Simply run:
+
+```
+$ just use <playground run> 
+```
---
+++ ccloud/fm-influxdb3-sink/fully-managed-influxdb3-sink.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+NGROK_AUTH_TOKEN=${NGROK_AUTH_TOKEN:-$1}
+
+display_ngrok_warning
+
+bootstrap_ccloud_environment
+
+set +e
+playground topic delete --topic influx_measures
+sleep 3
+playground topic create --topic influx_measures --nb-partitions 1
+set -e
+
+docker compose build
+docker compose down -v --remove-orphans
+docker compose up -d --quiet-pull
+
+sleep 5
+
+log "Waiting for ngrok to start"
+while true
+do
+  container_id=$(docker ps -q -f name=ngrok)
+  if [ -n "$container_id" ]
+  then
+    status=$(docker inspect --format '{{.State.Status}}' $container_id)
+    if [ "$status" = "running" ]
+    then
+      log "Getting ngrok hostname and port"
+      NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+      NGROK_HOSTNAME=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 1)
+      NGROK_PORT=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 2)
+
+      if ! [[ $NGROK_PORT =~ ^[0-9]+$ ]]
+      then
+        log "NGROK_PORT is not a valid number, keep retrying..."
+        continue
+      else 
+        break
+      fi
+    fi
+  fi
+  log "Waiting for container ngrok to start..."
+  sleep 5
+done
+
+log "Sending messages to topic influx_measures"
+playground topic produce -t influx_measures --nb-messages 1 --forced-value '{"measurement": "influx_measures", "id": 999, "product": "foo", "quantity": 100, "price": 50}' << 'EOF'
+{
+  "fields": [
+    {
+      "name": "measurement",
+      "type": "string"
+    },
+    {
+      "name": "id",
+      "type": "int"
+    },
+    {
+      "name": "product",
+      "type": "string"
+    },
+    {
+      "name": "quantity",
+      "type": "int"
+    },
+    {
+      "name": "price",
+      "type": "float"
+    }
+  ],
+  "name": "myrecord",
+  "type": "record"
+}
+EOF
+
+connector_name="InfluxDB3Sink_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+log "Creating InfluxDB3 admin token"
+INFLUXDB_TOKEN=$(docker exec influxdb3 influxdb3 create token --admin | awk '/Token:/{print $NF}' | tr -d '\r\n')
+
+log "Creating InfluxDB3 database test-db"
+curl -X POST "http://localhost:8181/api/v3/configure/database" \
+  --header "Authorization: Bearer $INFLUXDB_TOKEN" \
+  --header "Content-Type: application/json" \
+  --data '{"db": "test-db"}'
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+  "connector.class": "InfluxDB3Sink",
+  "name": "$connector_name",
+  "kafka.auth.mode": "KAFKA_API_KEY",
+  "kafka.api.key": "$CLOUD_KEY",
+  "kafka.api.secret": "$CLOUD_SECRET",
+  "input.data.format": "AVRO",
+  "influxdb.url": "http://$NGROK_HOSTNAME:$NGROK_PORT",
+  "influxdb.token": "$INFLUXDB_TOKEN",
+  "influxdb.database": "test-db",
+  "topics": "influx_measures",
+  "tasks.max" : "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 180
+
+sleep 10
+
+log "Verify that order is in influxdb3"
+docker exec influxdb3 influxdb3 query "SELECT * FROM influx_measures" --database test-db --token $INFLUXDB_TOKEN > output.txt
+cat output.txt
+grep "foo" output.txt
+rm output.txt
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
