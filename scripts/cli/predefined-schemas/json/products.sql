01414fe3e084710d5dcd27abfd3de0b33a317d28
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-11-27 14:06:31 +0100

    wip faker deprecated

--- scripts/cli/predefined-schemas/json/products.sql
+++ scripts/cli/predefined-schemas/json/products.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "notused"."notused" (
   "id" int PRIMARY KEY,
   "name" varchar COMMENT 'faker.internet.userName()',
-  "merchant_id" int NOT NULL COMMENT 'faker.datatype.number()',
-  "price" int COMMENT 'faker.datatype.number()',
+  "merchant_id" int NOT NULL COMMENT 'faker.number.int()',
+  "price" int COMMENT 'faker.number.int()',
   "status" int COMMENT 'faker.datatype.boolean()'
 );
