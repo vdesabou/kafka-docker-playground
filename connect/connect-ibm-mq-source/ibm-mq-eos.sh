7993fa121f7c19235578e35957de2b4ef0760272
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-11-28 14:47:50 +0100

    👾 Add IBM MQ source example with EOS #7389

---
+++ connect/connect-ibm-mq-source/ibm-mq-eos.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if connect_cp_version_greater_than_8 && [ ! -z "$CONNECTOR_TAG" ] && ! version_gt $CONNECTOR_TAG "12.1.99"
+then
+     logwarn "minimal supported connector version is 12.2.0 for CP 8.0"
+     logwarn "see https://docs.confluent.io/platform/current/connect/supported-connector-version-8.1.html#supported-connector-versions-in-cp-8-1"
+     exit 111
+fi
+
+cd ../../connect/connect-ibm-mq-source
+get_3rdparty_file "IBM-MQ-Install-Java-All.jar"
+
+if [ ! -f ${PWD}/IBM-MQ-Install-Java-All.jar ]
+then
+     # not running with github actions
+     logerror "❌ ${PWD}/IBM-MQ-Install-Java-All.jar is missing. It must be downloaded manually in order to acknowledge user agreement"
+     exit 1
+fi
+
+if [ ! -f ${PWD}/com.ibm.mq.allclient.jar ]
+then
+     # install deps
+     log "Getting com.ibm.mq.allclient.jar and jms.jar from IBM-MQ-Install-Java-All.jar"
+     if [[ "$OSTYPE" == "darwin"* ]]
+     then
+          # workaround for issue on linux, see https://github.com/vdesabou/kafka-docker-playground/issues/851#issuecomment-821151962
+          rm -rf ${PWD}/install/
+     else
+          sudo rm -rf ${PWD}/install/
+     fi
+     docker run --quiet --rm -v ${PWD}/IBM-MQ-Install-Java-All.jar:/tmp/IBM-MQ-Install-Java-All.jar -v ${PWD}/install:/tmp/install eclipse-temurin:8 java -jar /tmp/IBM-MQ-Install-Java-All.jar --acceptLicense /tmp/install
+     cp ${PWD}/install/wmq/JavaSE/lib/jms.jar ${PWD}/
+     cp ${PWD}/install/wmq/JavaSE/lib/com.ibm.mq.allclient.jar ${PWD}/
+fi
+cd -
+
+PLAYGROUND_ENVIRONMENT=${PLAYGROUND_ENVIRONMENT:-"plaintext"}
+playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.eos.yml"
+
+log "Setup IBM MQ for EOS: topic and permissions"
+docker exec -i ibmmq runmqsc QM1 << EOF
+DEFINE TOPIC('MY.MQ.STATE.TOPIC') TOPICSTR('ibm-mq-source-state-topic')
+EOF
+
+docker exec -u 0 ibmmq setmqaut -m QM1 -t topic -n MY.MQ.STATE.TOPIC -p app +sub +pub +dsp
+docker exec -u 0 ibmmq setmqaut -m QM1 -t q -n SYSTEM.DEFAULT.MODEL.QUEUE -p app +get +dsp +inq
+
+docker exec -i ibmmq runmqsc QM1 << EOF
+REFRESH SECURITY TYPE(AUTHSERV)
+EOF
+
+log "Creating IBM MQ source connector"
+playground connector create-or-update --connector ibm-mq-source-eos  << EOF
+{
+    "connector.class": "io.confluent.connect.ibm.mq.IbmMQSourceConnector",
+    "kafka.topic": "MyKafkaTopicName",
+    "mq.hostname": "ibmmq",
+    "mq.port": "1414",
+    "mq.transport.type": "client",
+    "mq.queue.manager": "QM1",
+    "mq.channel": "DEV.APP.SVRCONN",
+    "mq.username": "app",
+    "mq.password": "passw0rd",
+    "max.retry.time": "10000",
+    "jms.destination.name": "DEV.QUEUE.1",
+    "jms.destination.type": "queue",
+
+    "state.topic.name": "ibm-mq-source-state-topic",
+    "transaction.boundary": "connector",
+
+    "confluent.license": "",
+    "confluent.topic.bootstrap.servers": "broker:9092",
+    "confluent.topic.replication.factor": "1"
+}
+EOF
+
+sleep 5
+
+log "Sending messages to DEV.QUEUE.1 JMS queue:"
+docker exec -i ibmmq /opt/mqm/samp/bin/amqsput DEV.QUEUE.1 << EOF
+Message 1
+Message 2
+EOF
+
+sleep 5
+
+log "Verify we have received the data in MyKafkaTopicName topic using --isolation-level read_committed"
+playground topic consume --topic MyKafkaTopicName --min-expected-messages 2 --timeout 60 --isolation-level read_committed
