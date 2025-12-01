e8da7b1dcbdcf79ee69f37e8e0b79e9166bbd416
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2025-12-01 15:29:52 +0100

    added cursor pagination example

---
+++ ccloud/fm-http-v2-source/fully-managed-http-v2-source-cursor-pagination-no-auth.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+NGROK_AUTH_TOKEN=${NGROK_AUTH_TOKEN:-$1}
+
+display_ngrok_warning
+
+bootstrap_ccloud_environment
+
+
+docker compose -f docker-compose.cursor-pagination.no-auth.yml build
+docker compose -f docker-compose.cursor-pagination.no-auth.yml down -v --remove-orphans
+docker compose -f docker-compose.cursor-pagination.no-auth.yml up -d --quiet-pull
+
+sleep 5
+
+log "Waiting for ngrok to start"
+while true
+do
+  container_id=$(docker ps -q -f name=ngrok)
+  if [ -n "$container_id" ]
+  then
+    status=$(docker inspect --format '{{.State.Status}}' $container_id)
+    if [ "$status" = "running" ]
+    then
+      log "Getting ngrok hostname and port"
+      NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+      NGROK_HOSTNAME=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 1)
+      NGROK_PORT=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 2)
+
+      if ! [[ $NGROK_PORT =~ ^[0-9]+$ ]]
+      then
+        log "NGROK_PORT is not a valid number, keep retrying..."
+        continue
+      else 
+        break
+      fi
+    fi
+  fi
+  log "Waiting for container ngrok to start..."
+  sleep 5
+done
+
+set +e
+playground topic delete --topic http-source-topic-v2
+set -e
+
+log "Creating http-source-topic-v2 topic in Confluent Cloud"
+set +e
+playground topic create --topic http-source-topic-v2
+set -e
+
+connector_name="HttpSourceV2_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+    "connector.class": "HttpSourceV2",
+    "name": "$connector_name",
+    "kafka.auth.mode": "KAFKA_API_KEY",
+    "kafka.api.key": "$CLOUD_KEY",
+    "kafka.api.secret": "$CLOUD_SECRET",
+    "output.data.format": "AVRO",
+    "tasks.max" : "1",
+
+    "http.api.base.url": "http://$NGROK_HOSTNAME:$NGROK_PORT",
+    "behavior.on.error": "FAIL",
+    "apis.num": "1",
+    "api1.http.api.path": "/storage/v1/b/test-bucket/o",
+    "api1.topics": "http-source-topic-v2",
+    "api1.http.request.headers": "Content-Type: application/json",
+    "api1.test.api": "false",
+
+    "api1.http.offset.mode": "CURSOR_PAGINATION",
+    "api1.http.request.method": "GET",
+    "api1.http.request.parameters": "pageToken=\${offset}",
+    "api1.http.response.data.json.pointer": "/items",
+    "api1.http.next.page.json.pointer": "/nextPageToken",
+    "api1.request.interval.ms": "1000"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 180
+
+
+log "Verify we have received the data in http-source-topic-v2 topic (expecting 15 messages across 3 pages)"
+playground topic consume --topic http-source-topic-v2 --min-expected-messages 15 --timeout 60
+
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
