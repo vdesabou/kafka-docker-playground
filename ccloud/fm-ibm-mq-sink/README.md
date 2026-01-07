eecc985dce6ae904b42ab0fb5e89a717c393458c
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2026-01-07 15:31:06 +0100

    👾 Add Fully Managed IBM MQ Sink connector example #7500

---
+++ ccloud/fm-ibm-mq-sink/README.md
@@ -0,0 +1,44 @@
+# Fully Managed IBM MQ Sink connector
+
+## Objective
+
+Quickly test [IBM MQ Sink](https://docs.confluent.io/cloud/current/connectors/cc-ibm-mq-sink.html#cc-ibm-mq-sink) connector.
+
+Using IBM MQ Docker [image](https://hub.docker.com/r/ibmcom/mq/)
+
+## Exposing docker container over internet
+
+**🚨WARNING🚨** It is considered a security risk to run this example on your personal machine since you'll be exposing a TCP port over internet using [Ngrok](https://ngrok.com). It is strongly encouraged to run it on a AWS EC2 instance where you'll use [Confluent Static Egress IP Addresses](https://docs.confluent.io/cloud/current/networking/static-egress-ip-addresses.html#use-static-egress-ip-addresses-with-ccloud) (only available for public endpoints on AWS) to allow traffic from your Confluent Cloud cluster to your EC2 instance using EC2 Security Group.
+
+Example in order to set EC2 Security Group with Confluent Static Egress IP Addresses and port 1414:
+
+```bash
+group=$(aws ec2 describe-instances --instance-id <$ec2-instance-id> --output=json | jq '.Reservations[] | .Instances[] | {SecurityGroups: .SecurityGroups}' | jq -r '.SecurityGroups[] | .GroupName')
+aws ec2 authorize-security-group-ingress --group-name $group --protocol tcp --port 1414 --cidr 13.36.88.88/32
+aws ec2 authorize-security-group-ingress --group-name $group --protocol tcp --port 1414 --cidr 13.36.88.89/32
+etc...
+```
+
+An [Ngrok](https://ngrok.com) auth token is necessary in order to expose the Docker Container port to internet, so that fully managed connector can reach it.
+
+You can sign up at https://dashboard.ngrok.com/signup
+If you have already signed up, make sure your auth token is setup by exporting environment variable `NGROK_AUTH_TOKEN`
+
+Your auth token is available on your dashboard: https://dashboard.ngrok.com/get-started/your-authtoken
+
+Ngrok web interface available at http://localhost:4551
+
+## Prerequisites
+
+See [here](https://kafka-docker-playground.io/#/how-to-use?id=%f0%9f%8c%a4%ef%b8%8f-confluent-cloud-examples)
+
+
+## How to run
+
+```
+$ just use <playground run> command and search for fully-managed-ibm-mq-source<use tab key to activate fzf completion (see https://kafka-docker-playground.io/#/cli?id=%e2%9a%a1-setup-completion), otherwise use full path, or correct relative path> .sh in this folder
+```
+
+Note: you can also export the value as environment variable
+
+
---
+++ ccloud/fm-ibm-mq-sink/fully-managed-ibm-mq-sink.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+NGROK_AUTH_TOKEN=${NGROK_AUTH_TOKEN:-$1}
+
+display_ngrok_warning
+
+bootstrap_ccloud_environment
+
+set +e
+playground topic delete --topic sink-messages
+set -e
+
+log "Creating sink-messages topic in Confluent Cloud"
+set +e
+playground topic create --topic sink-messages
+set -e
+
+docker compose build
+docker compose down -v --remove-orphans
+docker compose up -d --quiet-pull
+
+sleep 5
+
+log "Waiting for ngrok to start"
+while true
+do
+  container_id=$(docker ps -q -f name=ngrok)
+  if [ -n "$container_id" ]
+  then
+    status=$(docker inspect --format '{{.State.Status}}' $container_id)
+    if [ "$status" = "running" ]
+    then
+      log "Getting ngrok hostname and port"
+      NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+      NGROK_HOSTNAME=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 1)
+      NGROK_PORT=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 2)
+
+      if ! [[ $NGROK_PORT =~ ^[0-9]+$ ]]
+      then
+        log "NGROK_PORT is not a valid number, keep retrying..."
+        continue
+      else 
+        break
+      fi
+    fi
+  fi
+  log "Waiting for container ngrok to start..."
+  sleep 5
+done
+
+log "Sending messages to topic sink-messages"
+playground topic produce --topic sink-messages --nb-messages 1 << 'EOF'
+This is my message
+EOF
+
+connector_name="IbmMqSink_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+    "connector.class": "IbmMqSink",
+    "name": "$connector_name",
+    "kafka.auth.mode": "KAFKA_API_KEY",
+    "kafka.api.key": "$CLOUD_KEY",
+    "kafka.api.secret": "$CLOUD_SECRET",
+    "input.data.format": "STRING",
+    "topics": "sink-messages",
+    "mq.hostname": "$NGROK_HOSTNAME",
+    "mq.port": "$NGROK_PORT",
+    "mq.transport.type": "client",
+    "mq.queue.manager": "QM1",
+    "mq.channel": "DEV.APP.SVRCONN",
+    "mq.username": "app",
+    "mq.password": "passw0rd",
+    "jms.destination.name": "DEV.QUEUE.1",
+    "jms.destination.type": "queue",
+
+    "tasks.max" : "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 180
+
+sleep 10
+
+log "Verify message received in DEV.QUEUE.1 queue"
+docker exec ibmmq bash -c "/opt/mqm/samp/bin/amqsbcg DEV.QUEUE.1" > /tmp/result.log  2>&1
+cat /tmp/result.log
+grep "my message" /tmp/result.log
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
