168b898fa34d220f9199868519e0badeb91c3655
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2026-01-23 10:16:33 +0100

    wip

--- scripts/cli/src/commands/container/kill-all.sh
+++ scripts/cli/src/commands/container/kill-all.sh
@@ -1,3 +1,3 @@
-log "💀 kill all docker containers"
+log "💀 kill all docker containers (this also removes volumes)"
 docker rm -f $(docker ps -qa) > /dev/null 2>&1
 docker volume prune -f > /dev/null 2>&1
