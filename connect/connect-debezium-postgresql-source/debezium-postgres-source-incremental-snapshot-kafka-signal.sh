85d23f7e22ed035d7ed4e56a50589d12e369b916
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2026-02-16 16:47:09 +0100

    👾 Add Debezium postgres example with incremental snapshot triggered via signal kafka topic #7565

---
+++ connect/connect-debezium-postgresql-source/debezium-postgres-source-incremental-snapshot-kafka-signal.sh
@@ -0,0 +1,243 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if ! version_gt $TAG_BASE "5.9.99" && version_gt $CONNECTOR_TAG "1.9.9"
+then
+    logwarn "connector version >= 2.0.0 do not support CP versions < 6.0.0"
+    exit 111
+fi
+
+if connect_cp_version_greater_than_8 && [ ! -z "$CONNECTOR_TAG" ] && ! version_gt $CONNECTOR_TAG "2.4.99"
+then
+     logwarn "minimal supported connector version is 2.5.0 for CP 8.0"
+     logwarn "see https://docs.confluent.io/platform/current/connect/supported-connector-version-8.1.html#supported-connector-versions-in-cp-8-1"
+     exit 111
+fi
+
+PLAYGROUND_ENVIRONMENT=${PLAYGROUND_ENVIRONMENT:-"plaintext"}
+playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.yml"
+
+
+log "Create customers table:"
+docker exec -i postgres psql -U myuser -d postgres << EOF
+create table customers (
+        id INT PRIMARY KEY,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        email VARCHAR(50),
+        gender VARCHAR(50),
+        club_status VARCHAR(20),
+        comments VARCHAR(90),
+        create_ts timestamp DEFAULT CURRENT_TIMESTAMP,
+        update_ts timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Courtesy of https://techblog.covermymeds.com/databases/on-update-timestamps-mysql-vs-postgres/
+CREATE FUNCTION update_updated_at_column() RETURNS trigger
+    LANGUAGE plpgsql
+    AS \$\$
+  BEGIN
+    NEW.update_ts = NOW();
+    RETURN NEW;
+  END;
+\$\$;
+
+CREATE TRIGGER t1_updated_at_modtime BEFORE UPDATE ON customers FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (1, 'Rica', 'Blaisdell', 'rblaisdell0@rambler.ru', 'Female', 'bronze', 'Universal optimal hierarchy');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (2, 'Ruthie', 'Brockherst', 'rbrockherst1@ow.ly', 'Female', 'platinum', 'Reverse-engineered tangible interface');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (3, 'Mariejeanne', 'Cocci', 'mcocci2@techcrunch.com', 'Female', 'bronze', 'Multi-tiered bandwidth-monitored capability');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (4, 'Hashim', 'Rumke', 'hrumke3@sohu.com', 'Male', 'platinum', 'Self-enabling 24/7 firmware');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (5, 'Hansiain', 'Coda', 'hcoda4@senate.gov', 'Male', 'platinum', 'Centralized full-range approach');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (6, 'Robinet', 'Leheude', 'rleheude5@reddit.com', 'Female', 'platinum', 'Virtual upward-trending definition');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (7, 'Fay', 'Huc', 'fhuc6@quantcast.com', 'Female', 'bronze', 'Operative composite capacity');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (8, 'Patti', 'Rosten', 'prosten7@ihg.com', 'Female', 'silver', 'Integrated bandwidth-monitored instruction set');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (9, 'Even', 'Tinham', 'etinham8@facebook.com', 'Male', 'silver', 'Virtual full-range info-mediaries');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (10, 'Brena', 'Tollerton', 'btollerton9@furl.net', 'Female', 'silver', 'Diverse tangible methodology');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (11, 'Alexandro', 'Peeke-Vout', 'apeekevouta@freewebs.com', 'Male', 'gold', 'Ameliorated value-added orchestration');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (12, 'Sheryl', 'Hackwell', 'shackwellb@paginegialle.it', 'Female', 'gold', 'Self-enabling global parallelism');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (13, 'Laney', 'Toopin', 'ltoopinc@icio.us', 'Female', 'platinum', 'Phased coherent alliance');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (14, 'Isabelita', 'Talboy', 'italboyd@imageshack.us', 'Female', 'gold', 'Cloned transitional synergy');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (15, 'Rodrique', 'Silverton', 'rsilvertone@umn.edu', 'Male', 'gold', 'Re-engineered static application');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (16, 'Clair', 'Vardy', 'cvardyf@reverbnation.com', 'Male', 'bronze', 'Expanded bottom-line Graphical User Interface');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (17, 'Brianna', 'Paradise', 'bparadiseg@nifty.com', 'Female', 'bronze', 'Open-source global toolset');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (18, 'Waldon', 'Keddey', 'wkeddeyh@weather.com', 'Male', 'gold', 'Business-focused multi-state functionalities');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (19, 'Josiah', 'Brockett', 'jbrocketti@com.com', 'Male', 'gold', 'Realigned didactic info-mediaries');
+insert into customers (id, first_name, last_name, email, gender, club_status, comments) values (20, 'Anselma', 'Rook', 'arookj@europa.eu', 'Female', 'gold', 'Cross-group 24/7 application');
+
+EOF
+
+log "Create customers2 table:"
+docker exec -i postgres psql -U myuser -d postgres << EOF
+create table customers2 (
+        id INT PRIMARY KEY,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        email VARCHAR(50),
+        gender VARCHAR(50),
+        club_status VARCHAR(20),
+        comments VARCHAR(90),
+        create_ts timestamp DEFAULT CURRENT_TIMESTAMP ,
+        update_ts timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Courtesy of https://techblog.covermymeds.com/databases/on-update-timestamps-mysql-vs-postgres/
+CREATE FUNCTION update_updated_at_column() RETURNS trigger
+    LANGUAGE plpgsql
+    AS \$\$
+  BEGIN
+    NEW.update_ts = NOW();
+    RETURN NEW;
+  END;
+\$\$;
+
+CREATE TRIGGER t1_updated_at_modtime BEFORE UPDATE ON customers2 FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (1, 'Rica', 'Blaisdell', 'rblaisdell0@rambler.ru', 'Female', 'bronze', 'Universal optimal hierarchy');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (2, 'Ruthie', 'Brockherst', 'rbrockherst1@ow.ly', 'Female', 'platinum', 'Reverse-engineered tangible interface');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (3, 'Mariejeanne', 'Cocci', 'mcocci2@techcrunch.com', 'Female', 'bronze', 'Multi-tiered bandwidth-monitored capability');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (4, 'Hashim', 'Rumke', 'hrumke3@sohu.com', 'Male', 'platinum', 'Self-enabling 24/7 firmware');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (5, 'Hansiain', 'Coda', 'hcoda4@senate.gov', 'Male', 'platinum', 'Centralized full-range approach');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (6, 'Robinet', 'Leheude', 'rleheude5@reddit.com', 'Female', 'platinum', 'Virtual upward-trending definition');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (7, 'Fay', 'Huc', 'fhuc6@quantcast.com', 'Female', 'bronze', 'Operative composite capacity');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (8, 'Patti', 'Rosten', 'prosten7@ihg.com', 'Female', 'silver', 'Integrated bandwidth-monitored instruction set');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (9, 'Even', 'Tinham', 'etinham8@facebook.com', 'Male', 'silver', 'Virtual full-range info-mediaries');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (10, 'Brena', 'Tollerton', 'btollerton9@furl.net', 'Female', 'silver', 'Diverse tangible methodology');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (11, 'Alexandro', 'Peeke-Vout', 'apeekevouta@freewebs.com', 'Male', 'gold', 'Ameliorated value-added orchestration');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (12, 'Sheryl', 'Hackwell', 'shackwellb@paginegialle.it', 'Female', 'gold', 'Self-enabling global parallelism');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (13, 'Laney', 'Toopin', 'ltoopinc@icio.us', 'Female', 'platinum', 'Phased coherent alliance');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (14, 'Isabelita', 'Talboy', 'italboyd@imageshack.us', 'Female', 'gold', 'Cloned transitional synergy');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (15, 'Rodrique', 'Silverton', 'rsilvertone@umn.edu', 'Male', 'gold', 'Re-engineered static application');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (16, 'Clair', 'Vardy', 'cvardyf@reverbnation.com', 'Male', 'bronze', 'Expanded bottom-line Graphical User Interface');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (17, 'Brianna', 'Paradise', 'bparadiseg@nifty.com', 'Female', 'bronze', 'Open-source global toolset');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (18, 'Waldon', 'Keddey', 'wkeddeyh@weather.com', 'Male', 'gold', 'Business-focused multi-state functionalities');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (19, 'Josiah', 'Brockett', 'jbrocketti@com.com', 'Male', 'gold', 'Realigned didactic info-mediaries');
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (20, 'Anselma', 'Rook', 'arookj@europa.eu', 'Female', 'gold', 'Cross-group 24/7 application');
+
+EOF
+
+log "Creating a signaling data collection"
+docker exec -i postgres psql -U myuser -d postgres << EOF
+CREATE TABLE debezium_signal (id VARCHAR(100) PRIMARY KEY, type VARCHAR(100) NOT NULL, data VARCHAR(2048) NULL);
+EOF
+
+log "Show content of customers table:"
+docker exec -i postgres psql -U myuser -d postgres << EOF
+SELECT * FROM customers;
+EOF
+
+log "Show content of customers2 table:"
+docker exec -i postgres psql -U myuser -d postgres << EOF
+SELECT * FROM customers2;
+EOF
+
+log "Show content of debezium_signal table:"
+docker exec -i postgres psql -U myuser -d postgres << EOF
+SELECT * FROM debezium_signal;
+EOF
+
+
+log "Creating Debezium PostgreSQL source connector with customers table"
+playground connector create-or-update --connector debezium-postgres-source  << EOF
+{
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "tasks.max": "1",
+    "database.hostname": "postgres",
+    "database.port": "5432",
+    "database.user": "myuser",
+    "database.password": "mypassword",
+    "database.dbname" : "postgres",
+
+    "_comment": "old version before 2.x",
+    "database.server.name": "asgard",
+    "_comment": "new version since 2.x",
+    "topic.prefix": "asgard",
+
+    "key.converter" : "io.confluent.connect.avro.AvroConverter",
+    "key.converter.schema.registry.url": "http://schema-registry:8081",
+    "value.converter" : "io.confluent.connect.avro.AvroConverter",
+    "value.converter.schema.registry.url": "http://schema-registry:8081",
+    "table.include.list" : "public.customers",
+    "signal.data.collection": "public.debezium_signal",
+
+    "signal.kafka.topic": "dbz-signals",
+    "signal.kafka.bootstrap.servers": "broker:9092",
+    "signal.enabled.channels": "kafka,source",
+
+    "_comment:": "remove _ to use ExtractNewRecordState smt",
+    "transforms": "unwrap",
+    "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState"
+}
+EOF
+
+sleep 5
+
+log "Verifying topic asgard.public.customers"
+playground topic consume --topic asgard.public.customers --min-expected-messages 5 --timeout 60
+
+log "Verifying topic asgard.public.customers2 has no data"
+set +e
+playground topic consume --topic asgard.public.customers2 --min-expected-messages 5 --timeout 60
+set -e
+
+log "Creating Debezium PostgreSQL source connector with customers and customers2 table"
+playground connector create-or-update --connector debezium-postgres-source  << EOF
+{
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "tasks.max": "1",
+    "database.hostname": "postgres",
+    "database.port": "5432",
+    "database.user": "myuser",
+    "database.password": "mypassword",
+    "database.dbname" : "postgres",
+
+    "_comment": "old version before 2.x",
+    "database.server.name": "asgard",
+    "_comment": "new version since 2.x",
+    "topic.prefix": "asgard",
+
+    "key.converter" : "io.confluent.connect.avro.AvroConverter",
+    "key.converter.schema.registry.url": "http://schema-registry:8081",
+    "value.converter" : "io.confluent.connect.avro.AvroConverter",
+    "value.converter.schema.registry.url": "http://schema-registry:8081",
+
+    "table.include.list" : "public.customers,public.customers2",
+    "signal.data.collection": "public.debezium_signal",
+
+    "signal.kafka.topic": "dbz-signals",
+    "signal.kafka.bootstrap.servers": "broker:9092",
+    "signal.enabled.channels": "kafka,source",
+
+    "transforms": "unwrap",
+    "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState"
+}
+EOF
+
+sleep 10
+
+log "insert a record in customers2"
+docker exec -i postgres psql -U myuser -d postgres << EOF
+insert into customers2 (id, first_name, last_name, email, gender, club_status, comments) values (21, 'Anselma2', 'Rook2', 'arook2j@europa.eu', 'Female2', 'gold2', 'Cross-group 24/7 application');
+EOF
+
+log "Verifying topic asgard.public.customers2 : there will be only the new record"
+set +e
+playground topic consume --topic asgard.public.customers2 --min-expected-messages 1 --timeout 60
+set -e
+
+sleep 10
+
+log "Sending snapshot signal via kafka topic  dbz-signals"
+playground topic produce -t dbz-signals --nb-messages 1 --key "asgard" << 'EOF'
+{"id": "captain adhoc 1","type":"execute-snapshot","data": {"data-collections": ["public.customers2"], "type": "incremental"}}
+EOF
+
+sleep 10
+
+log "Checking topic asgard.public.customers2 for data, the 6 records are there"
+playground topic consume --topic asgard.public.customers2 --min-expected-messages 6 --timeout 60
