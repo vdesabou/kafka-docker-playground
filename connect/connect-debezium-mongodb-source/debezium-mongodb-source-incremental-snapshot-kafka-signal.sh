4a7122a59deebe50297aea39b8902f917769e555
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2026-02-16 16:59:27 +0100

    👾 Add Debezium mongo example with incremental snapshot triggered via signal kafka topic #7566

---
+++ connect/connect-debezium-mongodb-source/debezium-mongodb-source-incremental-snapshot-kafka-signal.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+if ! version_gt $TAG_BASE "5.9.99" && version_gt $CONNECTOR_TAG "1.9.9"
+then
+    logwarn "connector version >= 2.0.0 do not support CP versions < 6.0.0"
+    exit 111
+fi
+
+if connect_cp_version_greater_than_8 && [ ! -z "$CONNECTOR_TAG" ] && ! version_gt $CONNECTOR_TAG "2.4.99"
+then
+     logwarn "minimal supported connector version is 2.5.0 for CP 8.0"
+     logwarn "see https://docs.confluent.io/platform/current/connect/supported-connector-version-8.1.html#supported-connector-versions-in-cp-8-1"
+     exit 111
+fi
+
+
+PLAYGROUND_ENVIRONMENT=${PLAYGROUND_ENVIRONMENT:-"plaintext"}
+playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.yml"
+
+log "Initialize MongoDB replica set"
+docker exec -i mongodb mongosh --eval 'rs.initiate({_id: "debezium", members:[{_id: 0, host: "mongodb:27017"}]})'
+
+sleep 5
+
+log "Create a user profile"
+docker exec -i mongodb mongosh << EOF
+use admin
+db.createUser(
+{
+user: "debezium",
+pwd: "dbz",
+roles: ["dbOwner"]
+}
+)
+EOF
+
+sleep 2
+
+log "Insert a record"
+docker exec -i mongodb mongosh << EOF
+use inventory
+db.customers.insert([
+{ _id : 1006, first_name : 'Bob', last_name : 'Hopper', email : 'thebob@example.com' }
+]);
+EOF
+
+log "View record"
+docker exec -i mongodb mongosh << EOF
+use inventory
+db.customers.find().pretty();
+EOF
+
+log "Creating Debezium MongoDB source connector"
+playground connector create-or-update --connector debezium-mongodb-source  << EOF
+{
+    "connector.class" : "io.debezium.connector.mongodb.MongoDbConnector",
+    "tasks.max" : "1",
+    "_comment": "old version before 2.4.x",
+    "mongodb.hosts": "debezium/mongodb:27017",
+    "_comment": "new version since 2.4.x",
+    "mongodb.connection.string": "mongodb://mongodb:27017/?replicaSet=debezium",
+
+    "_comment": "old version before 2.x",
+    "mongodb.name": "dbserver1",
+    "_comment": "new version since 2.x",
+    "topic.prefix": "dbserver1",
+
+    "_signal.data.collection": "inventory.debezium_signal",
+    "signal.kafka.topic": "dbz-signals",
+    "signal.kafka.bootstrap.servers": "broker:9092",
+    "signal.enabled.channels": "kafka",
+
+    "collection.include.list": "inventory.customers",
+
+    "mongodb.user" : "debezium",
+    "mongodb.password" : "dbz",
+
+    "_comment:": "remove _ to use ExtractNewRecordState smt",
+    "_transforms": "unwrap",
+    "_transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState"
+}
+EOF
+
+
+sleep 5
+
+log "Verifying topic dbserver1.inventory.customers"
+playground topic consume --topic dbserver1.inventory.customers --min-expected-messages 1 --timeout 60
+
+
+log "Insert a record in new collection customers2"
+docker exec -i mongodb mongosh << EOF
+use inventory
+db.customers2.insert([
+{ _id : 1010, first_name : 'John', last_name : 'Greek', email : 'greek@example.com' }
+]);
+EOF
+
+log "Updating connector to include new collection"
+playground connector create-or-update --connector debezium-mongodb-source  << EOF
+{
+    "connector.class" : "io.debezium.connector.mongodb.MongoDbConnector",
+    "tasks.max" : "1",
+    "_comment": "old version before 2.4.x",
+    "mongodb.hosts": "debezium/mongodb:27017",
+    "_comment": "new version since 2.4.x",
+    "mongodb.connection.string": "mongodb://mongodb:27017/?replicaSet=debezium",
+
+    "_comment": "old version before 2.x",
+    "mongodb.name": "dbserver1",
+    "_comment": "new version since 2.x",
+    "topic.prefix": "dbserver1",
+
+    "_signal.data.collection": "inventory.debezium_signal",
+    "signal.kafka.topic": "dbz-signals",
+    "signal.kafka.bootstrap.servers": "broker:9092",
+    "signal.enabled.channels": "kafka",
+
+    "collection.include.list": "inventory.customers,inventory.customers2",
+
+    "mongodb.user" : "debezium",
+    "mongodb.password" : "dbz",
+
+    "_comment:": "remove _ to use ExtractNewRecordState smt",
+    "_transforms": "unwrap",
+    "_transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState"
+}
+EOF
+
+
+log "Insert another record in new collection customers2"
+docker exec -i mongodb mongosh << EOF
+use inventory
+db.customers2.insert([
+{ _id : 1011, first_name : 'Peter', last_name : 'Pan', email : 'pan@example.com' }
+]);
+EOF
+
+# FIXTHIS: we can see 2 records here, not sure why?
+log "Verifying topic dbserver1.inventory.customers2 : there will be only the new record"
+playground topic consume --topic dbserver1.inventory.customers2 --min-expected-messages 1 --timeout 60
+
+
+log "Trigger Ad hoc snapshot via kafka signal topic"
+playground topic produce -t dbz-signals --nb-messages 1 --key "dbserver1" << 'EOF'
+{"id": "captain adhoc 1","type":"execute-snapshot","data": {"data-collections": ["inventory.customers2"], "type": "incremental"}}
+EOF
+
+
+sleep 5
+
+log "Verifying topic dbserver1.inventory.customers2: it should have all records"
+playground topic consume --topic dbserver1.inventory.customers2 --min-expected-messages 2 --timeout 60
