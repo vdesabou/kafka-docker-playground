4f9e48a87170583d0fbd77c03c7734e5c52d6e76
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2026-02-18 14:31:38 +0100

    👾 Set default tag to CP 8.1.1 #7572

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -33,7 +33,7 @@ fi
 if [ -z "$TAG" ]
 then
     # TAG is not set, use default:
-    export TAG=8.1.0 # default tag
+    export TAG=8.1.1 # default tag
     # to handle ubi8 images
     export TAG_BASE="$TAG"
     if [ -z "$CP_KAFKA_IMAGE" ]
