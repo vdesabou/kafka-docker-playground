a6b987a1520b723e0d57642306a51d6446562d6e
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2026-04-01 16:04:30 +0200

    added sink exmaple

--- other/schema-references/protobuf.sh
+++ other/schema-references/protobuf.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
 PLAYGROUND_ENVIRONMENT=${PLAYGROUND_ENVIRONMENT:-"plaintext"}
-playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}"
+playground start-environment --environment "${PLAYGROUND_ENVIRONMENT}" --docker-compose-override-file "${PWD}/docker-compose.plaintext.yml"
 
 ###
 # PROTOBUF EXAMPLE
@@ -44,7 +44,7 @@ playground schema register --subject protobuf-alltypes-value << 'EOF'
 EOF
 
 log "Produce records to protobuf-alltypes topic"
-playground topic produce --topic protobuf-alltypes --forced-value '{"firstName":"Gupdqph.","lastName":"Ovck aobggjqdzn.","address":{"street":"Pchhblish rnsvtvwg ozjizdo.","street2":"Oyxp ybbtddn.","city":"Spqkjlihb qqsmiwufn tewcgudxt.","state":"Fwbwky yqvzcw viio fqsehza.","postalCode":"Wooxyyx slnr wmbf.","countryCode":"Ycfpnnh oknlsgup dczjgouyo edevdbuong."}}' --value-schema-id 3 << 'EOF'
+playground topic produce --topic protobuf-alltypes --forced-value '{"firstName":"Gupdqph.","lastName":"Ovck aobggjqdzn.","address":{"street":"Pchhblish rnsvtvwg ozjizdo.","street2":"Oyxp ybbtddn.","city":"Spqkjlihb qqsmiwufn tewcgudxt.","state":"Fwbwky yqvzcw viio fqsehza.","postalCode":"Wooxyyx slnr wmbf.","countryCode":"Ycfpnnh oknlsgup dczjgouyo edevdbuong."}}' --value-schema-id 2 << 'EOF'
 syntax = "proto3";
 
 package com.example;
@@ -61,3 +61,22 @@ EOF
 
 log "Consuming records from this topic"
 playground topic consume --topic protobuf-alltypes
+
+log "Creating FileStream Sink connector"
+playground connector create-or-update --connector filestream-sink  << EOF
+{
+    "tasks.max": "1",
+    "connector.class": "org.apache.kafka.connect.file.FileStreamSinkConnector",
+    "topics": "protobuf-alltypes",
+    "file": "/tmp/output.json",
+    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+    "value.converter": "io.confluent.connect.protobuf.ProtobufConverter",
+    "value.converter.schema.registry.url": "http://schema-registry:8081"
+}
+EOF
+
+
+sleep 5
+
+log "Verify we have received the data in file"
+docker exec connect cat /tmp/output.json
