c94766a361274e913c50c7257115163bee775ec2
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2026-05-13 10:39:18 +0200

    ⭐👾 Add Fully Managed IBM Db2 Source (JDBC) Connector example #7770

---
+++ ccloud/fm-jdbc-ibmdb2-source/README.md
@@ -0,0 +1,15 @@
+# Fully Managed JDBC IBM DB2 source connector
+
+## Objective
+
+Quickly test [Fully Managed  JDBC IBM DB2](https://docs.confluent.io/cloud/current/connectors/cc-ibmdb2-source.html) connector.
+
+## Prerequisites
+
+See [here](https://kafka-docker-playground.io/#/how-to-use?id=%f0%9f%8c%a4%ef%b8%8f-confluent-cloud-examples)
+
+## How to run
+
+```
+$ just use <playground run> command 
+```
---
+++ ccloud/fm-jdbc-ibmdb2-source/fully-managed-ibmdb2-source.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../../scripts/utils.sh
+
+
+NGROK_AUTH_TOKEN=${NGROK_AUTH_TOKEN:-$1}
+
+display_ngrok_warning
+
+bootstrap_ccloud_environment
+
+
+
+set +e
+playground topic delete --topic pg-LOGS
+set -e
+
+playground topic create --topic pg-LOGS
+
+docker compose build
+docker compose down -v --remove-orphans
+docker compose up -d --quiet-pull
+
+playground container logs --container ibmdb2 --wait-for-log "Setup has completed" --max-wait 600
+log "ibmdb2 DB has started!"
+
+log "Waiting for ngrok to start"
+while true
+do
+  container_id=$(docker ps -q -f name=ngrok)
+  if [ -n "$container_id" ]
+  then
+    status=$(docker inspect --format '{{.State.Status}}' $container_id)
+    if [ "$status" = "running" ]
+    then
+      log "Getting ngrok hostname and port"
+      NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+      NGROK_HOSTNAME=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 1)
+      NGROK_PORT=$(echo $NGROK_URL | cut -d "/" -f3 | cut -d ":" -f 2)
+
+      if ! [[ $NGROK_PORT =~ ^[0-9]+$ ]]
+      then
+        log "NGROK_PORT is not a valid number, keep retrying..."
+        continue
+      else 
+        break
+      fi
+    fi
+  fi
+  log "Waiting for container ngrok to start..."
+  sleep 5
+done
+
+docker exec -i ibmdb2 bash << EOF
+su - db2inst1
+db2 connect to sample user db2inst1 using passw0rd
+db2 -x "CREATE TABLE LOGS(ID INT GENERATED ALWAYS AS IDENTITY NOT NULL,MESSAGE VARCHAR(255) NOT NULL,CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY(ID));"
+EOF
+
+docker exec -i ibmdb2 bash << EOF
+su - db2inst1
+db2 connect to sample user db2inst1 using passw0rd
+db2 describe table LOGS
+EOF
+
+# Column name                     schema    Data type name      Length     Scale Nulls
+# ------------------------------- --------- ------------------- ---------- ----- ------
+# ID                              SYSIBM    INTEGER                      4     0 No    
+# MESSAGE                         SYSIBM    VARCHAR                    255     0 No    
+# CREATED_AT                      SYSIBM    TIMESTAMP                   10     6 Yes   
+
+docker exec -i ibmdb2 bash << EOF
+su - db2inst1
+db2 connect to sample user db2inst1 using passw0rd
+db2 -x "INSERT INTO logs(MESSAGE) VALUES('Testing timestamp');"
+EOF
+
+connector_name="IbmDb2Source_$USER"
+set +e
+playground connector delete --connector $connector_name > /dev/null 2>&1
+set -e
+
+log "Creating fully managed connector"
+playground connector create-or-update --connector $connector_name << EOF
+{
+    "connector.class": "IbmDb2Source",
+    "name": "$connector_name",
+    "kafka.auth.mode": "KAFKA_API_KEY",
+    "kafka.api.key": "$CLOUD_KEY",
+    "kafka.api.secret": "$CLOUD_SECRET",
+    "output.data.format": "AVRO",
+    "connection.host": "$NGROK_HOSTNAME",
+    "connection.port": "$NGROK_PORT",
+    "connection.user": "db2inst1",
+    "connection.password": "passw0rd",
+    "db.name": "sample",
+    "mode": "timestamp",
+    "timestamp.column.name": "CREATED_AT",
+    "query": "SELECT ID, CREATED_AT, TRIM(MESSAGE) AS MESSAGE_TRIMMED FROM LOGS",
+    "topic.prefix": "pg-LOGS",
+    "tasks.max": "1"
+}
+EOF
+wait_for_ccloud_connector_up $connector_name 180
+
+
+sleep 15
+
+log "Verifying topic pg-LOGS"
+playground topic consume --topic pg-LOGS --min-expected-messages 1 --timeout 60
+
+log "Do you want to delete the fully managed connector $connector_name ?"
+check_if_continue
+
+playground connector delete --connector $connector_name
