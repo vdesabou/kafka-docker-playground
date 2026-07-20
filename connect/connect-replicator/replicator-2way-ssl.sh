f264cbde624c1dd3cc1e2e5bf6eedd2eea80d4aa
Vincent Saboulin <vsaboulin@confluent.io>
2026-07-20 10:23:16 +0200

    🔥 connect/connect-replicator (cfk) Fixes #8548

--- connect/connect-replicator/replicator-2way-ssl.sh
+++ connect/connect-replicator/replicator-2way-ssl.sh
@@ -4,6 +4,12 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../scripts/utils.sh
 
+if [[ "$PLAYGROUND_ENVIRONMENT" == "cfk" ]]
+then
+     logwarn "this test is not supported with cfk"
+     exit 111
+fi
+
 playground start-environment --environment 2way-ssl --docker-compose-override-file "${PWD}/docker-compose.2way-ssl.yml"
 
 
